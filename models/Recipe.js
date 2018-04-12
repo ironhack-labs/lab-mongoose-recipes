8819a8ab28221397b6fe5bb53ad54dec9c16ae08
Luna Fidalgo <lunafidalgo@gmail.com>
2018-04-12 15:26:11 +0200

    Second iteration

--- models/Recipe.js
+++ models/Recipe.js
@@ -12,10 +12,15 @@ const recipeSchema = new Schema({
   dishType: {
     type: String,
     enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
-  }, 
-  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg."}, 
-  duration: {type: Number, min: 0}, 
-  creator: {type: String}, 
-  created: {type: Date, default: "today"}
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg."
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+ created: { type: Date, default: Date.now() }
 });
 
+const Recipe = mongoose.model("Recipe", recipeSchema);
+module.exports = Recipe;
--- recipes.js
+++ recipes.js
@@ -1,11 +1,32 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
+const Recipe = require("./models/Recipe");
+const dbURL = "mongodb://localhost/recipeApp";
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose
+  .connect(dbURL)
   .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log(`Connected to: ${dbURL}`);
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+Recipe.create(
+  {
+    title: "Macarrones con chorizo",
+    level: "Easy Peasy",
+    ingredients: ["macarrones", "tomate", "chorizo", "queso"],
+    cousine: "mediterranea",
+    dishType: "Dish",
+    image:
+      "https://cocinandoentreolivos.com/2015/01/como-hacer-macarrones-con-chorizo.html",
+    duration: 20,
+    creator: "David & Luna"
+  },
+  (err, recipe) => {
+    if (err) console.log(`An error happened ${err}`);
+    else console.log(`RECIPE CREATED ${recipe}`);
+  }
+);

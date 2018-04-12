2426070bb5884e21f2e593f899b7944a6c1af66e
Tanis Pan <e.pan.gar@gmail.com>
2018-04-12 17:58:17 +0200

    i4...

--- models/schema.js
+++ models/schema.js
@@ -5,12 +5,12 @@ const recipeSchema = new Schema({
   title: {type: String, required: true, unique: true},
   level: {type: String, enum: ["Easy Peasy", "Amateur Chef" , "UltraPro Chef" ] },
   ingredients: {type: Array},
-  cousine: {type: String, required: true},
+  cuisine: {type: String, required: true},
   dishType: {type: String, enum: ["Breakfast", "Dish" , "Snack" , "Drink" , "Dessert", "Other"] },
   image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
   duration: {type: Number, min: 0},
   creator: {type: String},
-  created: {type: Date} 
+  created: {type: Date, default: Date.now} 
 });
 
 const recipe = mongoose.model("Recipe", recipeSchema);
--- recipes.js
+++ recipes.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const data = require('./data.js');
+const Receta = require("./models/schema");
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
+    Receta.collection.drop();
     console.log('Connected to Mongo!')
+    Receta.insertMany(data, (error, docs)=>{
+      if(error){
+        console.log(error)
+      } else {
+        console.log(docs);
+        Receta.collection.updateOne({title:'Rigatoni alla Genovese'}, {level: 'PEPE'});
+      }
+      
+    } )
+
+
+   // console.log(plato);
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
 

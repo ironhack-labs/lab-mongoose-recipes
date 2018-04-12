2ac61f337dd7022ed6202babaa4abe154309a0ef
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-12 14:19:36 +0200

    Schema done

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  title: {type:String},
-  level: {type:String},
+  title: {type:String, required:true, unique: true},
+  level: {type:String, enum: ['Easy Peasy', 'Amateur Ched', 'UltraPro Chef']},
   ingredients: {type:Array},
-  cousine: {type:String},
-  dishType: {type:String},
-  image: {type:String},
-  duration: {type:Number},
+  cousine: {type:String, required:true},
+  dishType: {type:String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+  image: {type:String, default:"https://images.media-allrecipes.com/images/75131.jpg."},
+  duration: {type:Number, min: 0},
   creator: {type:String},
-  created: {type:Date}
+  created: {type:Date, dafault: "$currentDate"}
 });
 
 const Recipe = mongoose.model("Recipe", userSchema);

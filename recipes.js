3fc7b5fcbb0010ae7607e7b8343082b13709b3e3
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-12 14:13:30 +0200

    Iteration 2

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js')
+const Recipe = require('./models/Recipe');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -9,14 +10,13 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err)
   });
 
-const recipeSchema = new Schema({
-  title: {type: String, required:true, unique:true},
-  level: {type: String, enum:["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
-  ingredients: Array,
-  cousine: {type: String, required: true},
-  dishType: {type: String, enum:["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
-  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
-  duration: {type: Number, min: 0},
-  creator: String,
-  created: {type: Date, default: Date.now}
-})
+Recipe.create( { 
+  title: 'Asian Glazed Chicken Thighs',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  cousine: 'Asian',
+  dishType: ['Dish'],
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 40,
+  creator: 'Chef LePapu'
+});

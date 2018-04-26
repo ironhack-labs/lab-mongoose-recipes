568d7117027ad9aa3a2469b475c9fbd8f1b4087d
Harland Lohora <lohoraharland@gmail.com>
2018-04-26 15:07:42 -0500

    done

--- recipes.js
+++ recipes.js
@@ -1,11 +1,38 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js')
+//const Schema   = mongoose.Schema;
+const data = require('./data.js');
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+app.set("view engine", "hbs");
+app.set("views",__dirname+"/views");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:false}));
 
-mongoose.connect('mongodb://localhost/recipeApp')
+const Receta = require("./models/recipes");
+
+
+mongoose.connect('mongodb://localhost:27017/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
 
+app.get("/",(req,res,next)=>{
+  /*const newRecipe = new Receta({title:"Tacos", cousine:"C"});
+  //console.log(newRecipe)
+  newRecipe.save()
+  .then(r => {
+    console.log(r)
+    res.send("Agregada Receta");
+  })
+  .catch(e=>console.log(e));*/
+  console.log(data);
+  console.log(recipeApp);
+
+
+});
+
+
+ app.listen(3000, e=>console.log("listo"));

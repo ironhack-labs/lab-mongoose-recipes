60e4e4dc94dd6b2dddd6ed571a09050f1ecd141b
Daniel Lozano <lozanodaniel02@gmail.com>
2018-04-26 14:44:58 -0400

    done

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js')
+const recipeSchema = require('./schema/recipeSchema.js')
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,4 +9,32 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
+
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+Recipe.create({title: "chicken", level: 'Easy Peasy', ingredients: ['chicken'],
+ cousine: 'International', dishType: ['Dish'], duration: 9, creator: "Me"})
+  .then((recipe) => { console.log('New recipe added: ', recipe.title) })
+  .catch((err) => { console.log(err)} );
+
+Recipe.insertMany(data)
+.then((recipe) => { console.log('New recipe added: ', data.title) })
+.catch((err) => { console.log(err)} );
+
+setTimeout(() => {
+  Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  .then((recipe) => { console.log('change duration!!!') })
+  .catch((err) => { console.log(err)} )
+
+  Recipe.deleteOne({title: 'Carrot Cake'})
+  .then((recipe) => { console.log('Carrot Cake deleted :(') })
+  .catch((err) => { console.log(err)} )
+
+}, 1000);
+
+setTimeout(() => {
+  mongoose.connection.close()
+}, 3000)
 

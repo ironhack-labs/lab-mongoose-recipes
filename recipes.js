c20ec4e4ea3059c417bd37c2c681e8a8bce1328e
Lars Hansen <lars1702@gmail.com>
2018-06-14 14:38:12 +0200

    Done! Lars and Aparna

--- recipes.js
+++ recipes.js
@@ -1,11 +1,60 @@
+/* jshint: esversion 6 */
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const data = require('./data.js');
+
+
+const recipeSchema = new Schema ({
+  title: { type: String, required: true, unique: true},
+  level: { type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
+  ingredients: Array,
+  cousine: {type: String, required: true},
+  dishType: { type: String, enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type: Number, min: 0},
+  creator: String,
+  created: {type: Date, default: Date.now} //sets the date to now, so its always today
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema) //name of model, then name of Schema
+
+
+
+
+// Recipe.insertMany({ data: 
+//   title: "Pizza Aparna", 
+//   level: "Amateur Chef", 
+//   ingredients: ["tomato","mozarella"], 
+//   cousine: "Italian", 
+//   dishType: "Dish",
+//   image: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+//   duration: 20,
+//   creator: "Lars and Aparna",
+  
+//   })
+//   .then((recipe) => { console.log('The recipe is saved and its title is: ', recipe.title) })
+//   .catch((err) => { console.log('An error happened:', err) });
+
+
+
+
+
+
+Recipe.insertMany(data)
+  .then((recipe) => { console.log('The recipe is saved and its title is: ', recipe[0].title) })
+  .catch((err) => { console.log('An error happened:', err) });
+
+
+  //timeout function for iteration 4
+  //new timeout function for iteration 5
+  //connection.close() or something like that for iteration 6
+
+
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 

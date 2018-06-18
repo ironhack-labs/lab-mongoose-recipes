7926f1a156e994f380a7ea21fb34bd90a313248b
Edward Leslie <edwardleslie09@gmail.com>
2018-06-18 18:19:25 -0400

    done

--- recipes.js
+++ recipes.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
 
 const recipeSchema = new Schema({
-  title:{
+  title: {
     type: String,
     unique: true,
     required: true
@@ -12,8 +12,8 @@ const recipeSchema = new Schema({
     type: String,
     enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
   },
-  ingredients: Array ,
-  cuisine:{
+  ingredients: Array,
+  cuisine: {
     type: String,
     required: true,
   },
@@ -21,9 +21,9 @@ const recipeSchema = new Schema({
     type: String,
     enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
   },
-  image:{ 
+  image: {
     type: String,
-    default: 'https://images.media-allrecipes.com/images/75131.jpg' 
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
   },
   duration: {
     type: Number,
@@ -36,15 +36,45 @@ const recipeSchema = new Schema({
   }
 });
 
+
+
 const Recipe = mongoose.model('Recipe', recipeSchema);
-
-
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
-    console.log(Recipe.title);
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
 
+Recipe.insertMany(data).then(() => {
+    console.log('import successful');
+  })
+  .catch(err => {
+    console.error(`Error: could not import data.`, err);
+  });
+
+Recipe.updateOne({
+    title: 'Rigatoni alla Genovese'
+  }, {
+    duration: 100
+  })
+  .then(() => {
+    console.log("update successful");
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    console.log(`Error updating`, err);
+  });
+
+Recipe.deleteOne({
+    title: "Carrot Cake"
+  })
+  .then(() => {
+    console.log("delete successful");
+  })
+  .catch(err => {
+    console.log(`Error deleting`, err);
+  });
+
+// mongoose.disconnect();

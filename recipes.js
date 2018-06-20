31e395dba14ac4c4da9fe806f54c674894742dc3
mafeq03 <mafeq03@gmail.com>
2018-06-20 16:32:47 -0400

    on iteration 3

--- recipes.js
+++ recipes.js
@@ -1,11 +1,39 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const data = require('./data.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
-    console.log('Connected to Mongo!')
+    console.log('Connected to Mongo!');
   }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
+
+const recipeSchema = new Schema ({
+  title: {type: String, required: true, unique: true},
+  level: {type:String, enum:['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: {type: Array},
+  cousine: {type:String, required: true},
+  dishType: {type:String},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type:Number, min: 0},
+  creator: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+Recipe.create({  
+  title: 'Portobello Mushroom Burger',
+  level: 'Amateur Chef',
+  ingredients: ['Mushroom Cap', 'Oregano', 'Olive Oil', 'Salt'],
+  cousine: 'vegan',
+  dishType: 'lunch',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/403928.jpg',
+  duration: 35,
+  creator: 'Bob Cody',
+  // created: {'2016-08-25'}
+})
+  .then((recipe)=>{console.log('The recipe saved!, the title is:', recipe.title)})
+  .catch((err) => {console.log('An error happened:' , err)})
 

76f180f3d7ebf5bd9f4a0f302a52c343fb4fd461
Leismale <leismale@gmail.com>
2018-07-05 16:45:12 +0200

    Done

--- recipes.js
+++ recipes.js
@@ -5,7 +5,70 @@ const data = require('./data.js')
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!')
+    update()
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
 
+  const recipeSchema = new Schema({
+    title: String,
+    level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+    ingredients: Array,
+    cousine: { type: String, required: true},
+    dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] },
+    image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+    duration: { type: Number, min: 0},
+    creator: String,
+    created: { type: Date, default: Date.now}
+  });
+
+
+  const Recipe = mongoose.model('Recipe', recipeSchema);
+
+const pollo ={
+  title: "Pollo asado",
+  level: "Easy Peasy",
+  ingredients: ["Pollo", "Sal", "Pimienta"],
+  cousine: "Carne",
+  dishType: "Dish",
+  image: "",
+  duration: 60,
+  creator: "Ruben",
+  created: ""
+}
+
+const addRecipe = () => {
+  Recipe.create(pollo, function (err) {
+    if (err) {
+      console.log('An error happened:', err);
+    } else {
+      console.log('The user is saved and its value is: ', pollo);
+    }
+  });
+}
+
+const addMany = () => {
+  Recipe.create(data, function (err) {
+    if (err) {
+      console.log('An error happened:', err);
+    } else {
+      console.log('The user is saved and its value is: ', data);
+    }
+  });
+}
+
+const update = () => {
+  Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+  .then(() => console.log("The duration has been changed"))
+  .catch(() => console.log('An error happened'))
+}
+
+const deleteRecipe = () => {
+  Recipe.findByIdAndRemove("5b3e241f75bb5623c258610f")
+  .then(() => console.log("The Carrot cake has been deleted"))
+  .catch(() => console.log('An error happened'))
+}
+
+const close = () => {
+  mongoose.disconnect();
+}

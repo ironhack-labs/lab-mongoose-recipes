84fe03d7eea8bd24a8136441e85f0c88aef6b184
EdMijangos <mloeza2010@hotmail.com>
2018-07-05 13:16:45 -0500

    PP MEX - Eduardo y Ricardo Aaron

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+const lista = 
+
 module.exports =  
     [
       { 
--- recipes.js
+++ recipes.js
@@ -1,11 +1,67 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const app  = express();
 const Schema   = mongoose.Schema;
 const data = require('./data.js')
+const Recipe = require('./models/Recipe')
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose.connect('mongodb://localhost:27017/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
 
+
+app.use('/new', (req,res)=>{
+  Recipe.create({
+    title:'lolol',
+    level:'Easy Peasy',
+    ingredients:['pollo'],
+    cousine:'International',
+    dishType:'Dish',
+    duration:5,
+    creator:'sho',    
+  })
+  .then(recipes=>{
+    console.log('Receta hecha', recipes.title);
+  })
+  .catch(err=>{
+    res.send(err)
+  })
+})
+
+app.get('/newmany', (req,res)=>{
+  Recipe.insertMany(data)
+  .then(res=>{
+    console.log('recetas importadas')
+  })
+  .catch(err=>{
+    console.log(err)
+  });
+})
+
+app.get('/update/:id', (req,res)=>{
+  Recipe.findByIdAndUpdate(req.params.id, {duration: 100})
+  .then(res=>{
+    console.log('receta actualizada')
+  })
+  .catch(err=>{
+    console.log(err)
+  });
+})
+
+app.get('/:id/delete', (req,res)=>{
+  Recipe.findByIdAndRemove(req.params.id)
+  .then(res=>{
+    console.log('receta borrada')
+  })
+  .catch(err=>{
+    console.log(err)
+  });
+})
+
+
+app.listen(3000, ()=>{
+  console.log('Oyendo')
+});

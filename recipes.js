7b48f9440bdc9a3b4c6aaa79de3579b86134ab90
Anna Hogberg <am.hogberg@gmail.com>
2018-07-05 15:34:49 +0200

    first commit

--- recipes.js
+++ recipes.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js')
 
+const  = new Schema({
+  name : String,
+  color: String,
+  age  : Number
+});
+
+
+
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!')

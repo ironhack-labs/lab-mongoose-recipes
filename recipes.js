b01beb3dfdb90efe33839c360eac3b634b5b4ff2
Pere Busquet <perebusquet87gmail.com>
2018-07-14 14:39:51 +0200

    done

--- recipes.js
+++ recipes.js
@@ -9,3 +9,34 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err)
   });
 
+  const recipeSchema = new Schema({
+    title: { type: String, required: true , unique:true},
+    level: { type: String, enum:['Easy Peasy','Amateur Chef','UltraPro Chef'] },
+    ingredients: { type: Array },
+    cousine: { type: String, required: true},
+    dishType:{type:String,  enum: ['Breakdast', 'Dish', 'Snak','Drink','Dessert','Other']},
+    image: {type: String, default:' https://images.media-allrecipes.com/images/75131.jpg'},
+    duration:{ type : Number,min:0},
+    creator:{type:String},
+    created:{type:Date,default:Date.now()},
+
+  });
+  //console.log(recipeSchema);
+
+const recipe = mongoose.model('recipe',recipeSchema);
+
+recipe.create({ title: 'lo que sea', cousine: 'algo' })
+  .then((recipe) => { console.log('The user is saved and its value is: ', recipe) })
+  .catch((err) => { console.log('An error happened:', err) });
+
+  recipe.insertMany(data)
+  .then((recipe) => { console.log('The user is saved and its value is: ', recipe) })
+  .catch((err) => { console.log('An error happened:', err) });
+
+  recipe.updateOne({ title: "Rigatoni alla Genovese"},{duration: 100})
+  .then((recipe) => {console.log('ok',recipe)})
+  .catch((err) => {console.log('error')})
+
+  recipe.deleteOne({ title: " Carrot Cake"})
+  .then((recipe) => {console.log('delete',recipe)})
+  .catch((err) => {console.log('error')})

87e89d902f6d1d258354bbf7add1cdda899d5d9d
Dalina <d.weidinger.16@ucl.ac.uk>
2018-08-23 14:27:25 +0200

    done

--- recipes.js
+++ recipes.js
@@ -1,11 +1,32 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
 
-mongoose.connect('mongodb://localhost/recipeApp')
+const Recipe = require("./models/schemaR.js");
+
+mongoose
+  .connect("mongodb://localhost/recipeApp")
   .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+Recipe.insertMany(data)
+  .then(() => {
+    return Promise.all([
+      Recipe.create({ name: "Sushi", cousine: "Japanese" }),
+      Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 }),
+      Recipe.remove({ title: "Carrot Cake" })
+    ]);
+  })
+  .then(() => {
+    mongoose.connection.close();
+  })
+  .then(() => {
+    console.log("connection was closed");
+  })
+  .catch(err => {
+    console.error(err);
+  });

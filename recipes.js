19ca6ddb0d0943348066deb3bacce0d4b9239a50
Cesar Vinken <cesarvinken@hotmail.com>
2018-08-23 14:17:58 +0200

    done

--- recipes.js
+++ recipes.js
@@ -1,11 +1,31 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
 
-mongoose.connect('mongodb://localhost/recipeApp')
+const Recipe = require("./models/Recipe");
+
+mongoose
+  .connect("mongodb://localhost/recipeApp")
   .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+Recipe.insertMany(data)
+  .then(result => {
+    return Promise.all([
+      Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 }),
+      Recipe.deleteOne({ title: "Carrot Cake" })
+    ]);
+  })
+  .then(result => {
+    mongoose.connection.close();
+  })
+  .then(recipe => {
+    console.log("connection was closed");
+  })
+  .catch(err => {
+    console.log(err);
+  });

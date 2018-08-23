8d72026191d7c41052599354a9a79590689327e5
vazz <elisa.merinovazquez@gmail.com>
2018-08-23 14:31:23 +0200

    DONE

---
+++ model/Recipe.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+//const RecipeSchema = mongoose.Schema;
+
+const Schema = new mongoose.Schema({
+	title: String,
+	level: { type: String, enum: [ 'Easy Peasy', 'Amateur Chef', 'UltraPro Chef' ] },
+	ingredients: [ String ],
+	cousine: { type: String, required: true },
+	dishType: { type: String, enum: [ 'Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other' ] },
+	image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+	duration: { type: Number, min: 0 },
+	creator: String,
+	created: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Recipe', Schema);
--- recipes.js
+++ recipes.js
@@ -1,11 +1,59 @@
+const data = require('./data.js');
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const express = require('express');
+const Recipe = require('./model/Recipe');
 
-mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+const app = express();
 
+mongoose
+	.connect('mongodb://localhost/recipeApp')
+	.then(() => {
+		console.log('Connected to Mongo!');
+	})
+	.catch((err) => {
+		console.error('Error connecting to mongo', err);
+	});
+
+app.get('/add-one', (req, res) => {
+	const recipe = new Recipe({ title: 'soup', cousine: 'german' });
+	recipe
+		.save()
+		.then((result) => {
+			console.log(result);
+			res.send(result);
+		})
+		.catch((err) => console.log(err));
+});
+app.get('/get-many', (req, res) => {
+	Recipe.insertMany(data)
+		.then((recipe) => {
+			res.send(recipe);
+		})
+		.catch((err) => console.log(err));
+});
+
+app.get('/update-one-recipe', (req, res) => {
+	Recipe.findByIdAndUpdate(
+		'5b7ea151338a5368dd975514',
+		{ duration: 100 },
+		{ new: true }
+	).then((recipe) => {
+		res.send(recipe);
+	});
+});
+app.get('/remove-one-recipe', (req, res) => {
+	Recipe.findByIdAndRemove({ _id: '5b7ea503334a6a5d6aaa2b48' }).then((recipe) => {
+		res.send(recipe);
+		console.log('Removed carrot cake!');
+	});
+});
+
+app.listen(4000);
+
+process.on('SIGINT', () => {
+	mongoose.connection.close().then(() => {
+		console.log('closing db');
+		res.send('closed db');
+		process.exit(0);
+	});
+});

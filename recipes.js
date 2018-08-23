3f51a30cc32aa12d2fa023966a79d30da22e4451
adiyathereh <ad.kydy@gmail.com>
2018-08-23 12:01:12 +0200

    created recipe schema

--- recipes.js
+++ recipes.js
@@ -1,11 +1,36 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose
+  .connect("mongodb://localhost/recipeApp")
   .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+const Recipe = mongoose.model(
+  "Recipe",
+  new mongoose.Schema({
+    title: { type: String, required: true, unique: true },
+    level: {
+      type: String,
+      enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+    },
+    ingredients: Array,
+    cusine: { type: String, required: true },
+    dishType: {
+      type: String,
+      enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+    },
+    image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg."
+    },
+    duration: { type: Number, min: 0 },
+    creator: String,
+    created: { type: Date, default: new Date() }
+  })
+);

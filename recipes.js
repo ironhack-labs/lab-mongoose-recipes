853458394fc0618e6c1cb4d350f888bfb626c6e7
trong nhan nguyen <ntnhan30@gmail.com>
2018-08-23 22:27:47 +0200

    done

--- recipes.js
+++ recipes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const data = require('./data.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,4 +8,44 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+  
+  const recipeSchema = new Schema({
+    title: { type: String, required: true, unique: true },
+    level: {
+      type: String,
+      enum: ['Easy Peasy','Amateur Chef' , 'UltraPro Chef'] 
+    },
+    ingredients: {type: Array},
+    cousine: { type: String, required: true},
+    dishType: {
+      type: String,
+      enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] 
+    },
+    image: { type: String, default:' https://images.media-allrecipes.com/images/75131.jpg' },
+    duration: { type: Number, min:0  },
+    creator: { type: String},
+    created: {type: Date, default: new Date()}
 
+  });
+  
+  const Recipe = mongoose.model('Recipe', recipeSchema);
+
+  // Recipe.create({
+  //   title: 'Pizza', level: 'Amateur Chef', cousine: 'something'
+  //   })
+  // .then((recipe) => { console.log('New receipt title is : ', recipe.title) })
+  // .catch((err) => { console.log('An error happened:', err) });
+  
+  Recipe.insertMany(data)
+  .then( () => {
+    Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+    .then( _ => {
+      Recipe.remove({title:"Carrot Cake"})
+      .then( _ => {
+        console.log( "DONE!!!!" )
+        mongoose.connection.close();
+      })
+    })
+  })
+  .catch( err => { console.log(err) } )
+ 

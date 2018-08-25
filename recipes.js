8f9532f00b24f9897c8f633fdde00e8dfae625eb
Paul de Barbeyrac <pauldebarbeyrac@gmail.com>
2018-08-25 15:24:59 +0200

    Iteration 1 DONE

--- recipes.js
+++ recipes.js
@@ -9,3 +9,17 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err)
   });
 
+
+const Recipe = new Schema({
+  title : { type: String, unique: true },
+  level: { type: String, enum: ["Easy Peasy","Amateur Chef","UltraPro Chef"]},
+  ingredients : Array,
+  cousine:{ type: String, required: true },
+  dishType:{ type: String, enum: ["Breaksfast","Dish","Snack","Drink","Dessert","Other"]},
+  image:{ type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration:{ type: Number, min: 0},
+  creator:String,
+  created:{ type: Date, default: Date.now },
+});
+
+// const Cat = mongoose.model('Cat', catSchema);

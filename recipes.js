7501968ea1b30f50890d9ca9a06eeb11a28d966c
Lauren Cahill <laurencahill@live.com>
2018-09-06 12:33:44 -0400

    done

--- recipes.js
+++ recipes.js
@@ -8,4 +8,65 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+  
+const recipeSchema = new Schema ({
 
+title: {type: String, unique: true, required: true},
+level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+ingredients: {type: Array},
+cousine: {type: String, required: true},
+dishType: {type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+duration: {type: Number, min: 0},
+creator: {type: String},
+created: {type: Date, default: Date.now}
+
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema );
+
+Recipe.create({
+
+title: 'Fantastic Fiesta Potatoes',
+level: 'Amateur Chef',
+ingredients: ['Potatoes', 'Imagination', 'Super Skills', 'Garlic'],
+cousine: 'Prima Cuisina',
+dishType: 'Breakfast',
+duration: 1000,
+creator: 'Fernando',  
+ })
+
+.then((response)=>{
+    console.log("Created a recipe", response)
+})
+.catch((err)=>{
+    console.log("Failed horribly", err)
+})
+
+Recipe.insertMany(data)
+
+.then((response) => {
+    console.log(response)
+ 
+})
+
+.catch((err) => {
+    console.log('oh shit', err);
+
+})
+
+Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+.then ((response) => {
+    console.log(response)
+})
+.catch ((err) => {
+    console.log(err)
+})
+
+Recipe.findOneAndRemove({title: "Carrot Cake"})
+.then ((response) => {
+    console.log(response)
+})
+.catch ((err) => {
+    console.log(err)
+})

d732122e86918c8f12b9d3a651fba0c44a385cb4
Axel Garcia <axel.garcia94@gmail.com>
2018-09-12 23:22:09 +0200

    not very seccessful

--- recipes.js
+++ recipes.js
@@ -1,6 +1,47 @@
+'use strict';
+
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js')
+
+
+const recipeSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: Array,
+  },
+  cousine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ['Breakfas', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+  },
+  iamge: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    minimum: 0
+  },
+  creator: {
+    type: String,
+  },
+  created: {
+    type: Date
+  }
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -9,3 +50,52 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err)
   });
 
+  Recipe.create({
+    title: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: Array,
+  },
+  cousine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ['Breakfas', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+  },
+  iamge: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    minimum: 0
+  },
+  creator: {
+    type: String,
+  },
+  created: {
+    type: Date
+  }
+  })
+ 
+  Recipe.insertMany(data,(err, recipes) => {
+    if (err){
+      console.log('ERROR');
+    }
+    for (let i = 0; i < recipes.length; i++) console.log(recipes[i].title);
+  })
+
+Recipe.update({title:'Rigatoni alla Genovese' },{ $set: {duration: 100}});
+
+Recipe.remove()
+.then(console.log('Success!'))
+
+  module.exports = Recipe;

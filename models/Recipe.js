c9da565c436d1e3c2b4156492a3c57dc6544793b
wergilia <weronika.zdrojewska@gmail.com>
2018-09-13 15:50:53 +0200

    First iteration

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,16 +1,7 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
-const data = require('./data.js')
-
-mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
-const Recipe = new Schema({
+const recipesSchema = new Schema({
   title: String,
   level: { type: String, enum: ['Easy Peasy','Amateur Chef', 'UltraPro Chef']},
   ingredients: [],
@@ -21,3 +12,6 @@ const Recipe = new Schema({
   creator: String,
   created: { type: Date, default: Date.now}
 })
+
+const Recipe = mongoose.model('Recipe', recipesSchema);
+module.exports = Recipe;

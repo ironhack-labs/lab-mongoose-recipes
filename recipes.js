8a363e3e2367f06c783d2a3b10c2a6ca7a56b12d
agdwm <almudenaguerraswm@gmail.com>
2018-09-13 15:25:04 +0200

    Connected to mongo

--- recipes.js
+++ recipes.js
@@ -1,11 +1,18 @@
+const express = require('express');
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const app = express();
+const path = require('path');
+const Schema = mongoose.Schema;
+const data = require('./data.js');
+
 
 mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+	.then(() => {
+		console.log('Connected to Mongo!')
+	}).catch(err => {
+		console.error('Error connecting to mongo', err)
+	});
 
+app.listen(3000, () => {
+	console.log('Listening 3000');
+})

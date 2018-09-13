e364b088c2f62c633da49c1154b3ce4c204edb56
wergilia <weronika.zdrojewska@gmail.com>
2018-09-13 16:45:22 +0200

    Second Iteration - Create a recipe

--- recipes.js
+++ recipes.js
@@ -1,23 +1,30 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const data = require('./data.js')
 
+const recipesSchema = new Schema({
+  title: { type: String, required: true },
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+  ingredients: [],
+  cousine: { type: String, required: true },
+  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] },
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration: { type: Number, min: 0 },
+  creator: String,
+  created: { type: Date, default: Date.now }
+})
+
+const Recipe = mongoose.model('Recipe', recipesSchema);
+
 mongoose.connect('mongodb://localhost/recipeApp')
+  .then( () => {
+    return Recipe.collection.drop();
+  })
   .then(() => {
+    Recipe.create({ title : "Arepas ReinaPepiada", cousine : "Venezuelan", level : "Easy Peasy" }) 
     console.log('Connected to Mongo!')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
 
-const Recipe = new Schema({
-  title: String,
-  level: { type: String, enum: ['Easy Peasy','Amateur Chef', 'UltraPro Chef']},
-  ingredients: [],
-  cousine: String,
-  dishType: { type: String, enum: ['Breakfast','Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
-  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
-  duration: {type: Number, min: 0},
-  creator: String,
-  created: { type: Date, default: Date.now}
-})

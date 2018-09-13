b36fef2e3645b61a02b48f971de013a698ee10b0
mfgomezb <mgomezb89@gmail.com>
2018-09-13 17:17:27 +0200

    done

--- recipes.js
+++ recipes.js
@@ -1,11 +1,38 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js')
+const Recipe = require('./models/Recipe.js')
 
-mongoose.connect('mongodb://localhost/recipeApp')
+
+const createRecipe = (data) => {
+  return Recipe.create(data)
+  .then(()=>{
+  }).catch( err => {console.log (err) });
+}
+
+const crazyRecipes = () => {
+  let proms = []
+  for (let i=0; i <data.length; i++) {
+    proms.push(createRecipe(data[i]));
+  }
+  return Promise.all(proms);
+}
+
+
+mongoose.connect('mongodb://localhost:27017/recipeApp', { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log("Connected to db")
+    return Recipe.collection.drop();
+  }).then( () => {
+    return crazyRecipes();
+  }).then( () => {
+    return Recipe.updateOne({"title": 'Rigatoni alla Genovese'}, {duration: 100})
+  }).then( () => {
+    return Recipe.deleteOne({title: "Carrot Cake"});
+  }).then(() => mongoose.disconnect() )
+  .catch(err => {
+    console.error('Error', err)
   });
+
+
 

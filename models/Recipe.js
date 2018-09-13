80c36450ad93224ec3138a86810715370bd9ce74
TeoDiaz <tdiazsan@gmail.com>
2018-09-13 16:35:54 +0200

    iteration 2

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,15 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const data = require("./data.js");
+const data = require("../data.js");
 
-mongoose
-  .connect("mongodb://localhost/recipeApp")
-  .then(() => {
-    console.log("Connected to Mongo!");
-  })
-  .catch(err => {
-    console.error("Error connecting to mongo", err);
-  });
 
 const recipeSchema = new Schema({
   title: { type: String, unique: true },
@@ -23,8 +15,15 @@ const recipeSchema = new Schema({
     type: String,
     enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
   },
-  image:{type:String, default:"https://images.media-allrecipes.com/images/75131.jpg"},
-  duration: {type:Number, min:0},
-  creator:String,
-  created:{type:Date, default:"today"}
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
+  creator: String,
+  created: { type: Date, default: "2018-08-13" }
 });
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
---
+++ new-recipe.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe");
+
+const createRecipe = (name = "Diego con Papas") => {
+  let recipe = new Recipe (
+    { 
+      title: 'Asian Glazed Chicken Thighs',
+      level: 'Amateur Chef',
+      ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+      cousine: 'Asian',
+      dishType: ['Dish'],
+      image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+      duration: 40,
+      creator: 'Chef LePapu'
+    });
+  return recipe.save();
+};
+
+
+
+mongoose
+  .connect("mongodb://localhost/recipeApp")
+  .then(() => {
+    console.log("Connected to Mongo!");
+    createRecipe();
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });

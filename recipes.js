7f1fac482dbfcf1bc0501cbaf55391f69ae88462
Ilham Moutaharik <ilham@prescrypto.com>
2018-09-13 16:35:36 -0500

    No acabamos

--- recipes.js
+++ recipes.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express();
+
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js')
@@ -9,3 +12,35 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err)
   });
 
+  const recipeSchema = new Schema({
+    title:String,
+    level:{type:String, enum:["Easy Peasy",'Amateur Chef', 'UltraPro Chef']},
+    ingredients:Array,
+    cousine:String,
+    dishType:{type:String, enum:['Breakfast', 'Dish','Snack', 'Dessert', 'Other']},
+    image:{
+      type:String,
+      default: 'https://images.media-allrecipes.com/images/75131.jpg'
+    },
+    duration:{
+      type:Number,
+      min:0
+    },
+    creator:String,
+    created:{
+      type:Date,
+      default:'Today'
+    }
+  })
+
+
+
+const Recipe = mongoose.model('recipes', recipeSchema)
+
+
+Recipe.create(data).then((recipe)=>console.log(recipe)).catch()
+
+
+
+
+app.listen(3000)

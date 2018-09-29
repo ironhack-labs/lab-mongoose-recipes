eea91701449e309662a198b08d9799b21b6b758d
Adolfo Vela <vela@tekplanner.com>
2018-09-29 13:45:00 -0500

    Listo

--- recipes.js
+++ recipes.js
@@ -32,10 +32,53 @@ function createNewRecipe() {
 function create(data) {
   RecipeSchema.insertMany(data)
     .then(res => {
-      console.log(res.title)
+      res.forEach(e => {
+        console.log(e.title)
+      });
     })
     .catch(error => {
       console.log(error)
     })
 }
-// create(data)
+ create(data)
+
+//UPDATE - CUARTA ITERACION
+function updateRecipeDuration(title, duration) {
+  RecipeSchema.updateOne({
+      title: title
+    }, {
+      duration: duration
+    })
+    .then(res => {
+      console.log("Sucess message ok!:", res)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}
+
+//updateRecipeDuration("Rigatoni alla Genovese",100)
+
+//REMOVE RECIPE - QUINTA ITERACION
+function deleteOneRecipe(title) {
+  RecipeSchema.remove({
+      title: title
+    })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+//deleteOneRecipe("Carrot Cake")
+
+//CERRAR BASE DE DATOS - SEXTA ITERACION
+  process.on('SIGINT', () => {  
+    mongoose.connection.close(() => { 
+      console.log('Mongoose default connection disconnected through app termination'); 
+      process.exit(0); 
+    }); 
+  }); 
+
+

68d754392a8929103e084174fe62581e18e98c2a
annamrp <annamrp22@gmail.com>
2018-10-12 14:45:45 +0200

    updated

--- recipes.js
+++ recipes.js
@@ -34,52 +34,77 @@ const recipeSchema = new Schema({
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
-Recipe.create({ title: "Tortila de patatas con cebolla", cuisine: "hola" })
+const createRecipe = Recipe.create({
+  title: "Tortila de patatas con cebolla",
+  cuisine: "hola"
+})
   .then(result => {
     console.log("result", result);
-    Recipe.insertMany(data).then(result => {
-      result.forEach(item => {
-        console.log(item.title, item._id);
-      });
-      Recipe.updateOne(
-        { title: "Rigatoni alla Genovese" },
-        { duration: 100 }
-      ).then(result => {
-        console.log("updated", result);
-        Recipe.findOneAndDelete({ title: "Carrot Cake" }).then(result => {
-          console.log("borrado");
-          mongoose.connection.close().then(result => {
-            console.log("Mongoose default connection closed");
-          });
-        });
-      });
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
+
+//versión sugestión ;p
+const insertMany = Recipe.insertMany(data)
+  .then(result => {
+    return result.forEach(item => {
+      console.log(item.title);
     });
   })
   .catch(error => {
     console.log("error", error);
   });
 
-// Recipe.insertMany(data)
-// .then(result => {
-//   return result.forEach( item => {
-//     console.log(item.title)
-//   })
-// })
-// .catch(error => {
-//   console.log('error', error)
-// })
-// Recipe.findByIdAndUpdate('5bc07ad4095b07858b3e5ea0', {duration: 100})
-// .then(result => {
-//   console.log('updated', result.duration)
-// })
-// .catch(error => {
-//   console.log('error', error)
-// })
+const updateOne = Recipe.findByIdAndUpdate("5bc07ad4095b07858b3e5ea0", {
+  duration: 100
+})
+  .then(result => {
+    console.log("updated");
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
 
-// Recipe.findOneAndRemove({title: 'Carrot Cake'})
-// .then(result => {
-//     console.log('borrado')
-// })
-// .catch(error => {
-//   console.log('error', error)
-// })
+const removeOne = Recipe.findOneAndRemove({ title: "Carrot Cake" })
+  .then(result => {
+    console.log("borrado");
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
+
+Promise.all([createRecipe, insertMany, updateOne, removeOne])
+  .then(result => {
+    mongoose.connection.close();
+    console.log("Mongoose default connection closed");
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
+
+//version thor itinerao
+// Recipe.create({ title: "Tortila de patatas con cebolla", cuisine: "hola" })
+//   .then(result => {
+//     console.log("result", result);
+//     Recipe.insertMany(data).then(result => {
+//       result.forEach(item => {
+//         console.log(item.title, item._id);
+//       });
+//       Recipe.updateOne(
+//         { title: "Rigatoni alla Genovese" },
+//         { duration: 100 }
+//       ).then(result => {
+//         console.log("updated", result);
+//         Recipe.findOneAndDelete({ title: "Carrot Cake" }).then(result => {
+//           console.log("borrado");
+//           mongoose.connection.close().then(result => {
+//             console.log("Mongoose default connection closed");
+//           });
+//         });
+//       });
+//     });
+//   })
+//   .catch(error => {
+//     console.log("error", error);
+//   });

a4c538446314469bd33f21fa16a3a95f47232374
annamrp <annamrp22@gmail.com>
2018-10-12 14:26:11 +0200

    fixed

--- recipes.js
+++ recipes.js
@@ -1,59 +1,63 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose
+  .connect("mongodb://localhost/recipeApp")
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
 const recipeSchema = new Schema({
   title: String,
-  level: {type: String, enum: ['Easy Peasy' , 'Amateur Chef' , 'UltraPro Chef']},
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
   ingredients: Array,
-  cuisine: {type: String, required: true},
-  dishType: {type: String, enum: ['Breakfast' , 'Dish' , 'Snack' , 'Drink' , 'Dessert' , 'Other']},
-  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
-  duration: {type: Number, min: 0},
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
   creator: String,
-  created: {type: Date , default: Date.now},
+  created: { type: Date, default: Date.now }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
-Recipe.create({title: 'Tortila de patatas con cebolla', cuisine: 'hola'})
-.then(result => {
-console.log('result', result)
-Recipe.insertMany(data)
-.then(result => {
-result.forEach( item => {
-console.log(item.title, item._id)
-})
-Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
-.then(result => {
-console.log('updated', result)
-Recipe.findOneAndDelete({title: 'Carrot Cake'})
-.then(result => {
-console.log('borrado')
-mongoose.connection.close() 
-.then ((result) => {
-  console.log('Mongoose default connection closed');
-})
-})
-})
-})
-})
-.catch(error => {
-  console.log('error', error)
-})
-
-
-
-
-
-
+Recipe.create({ title: "Tortila de patatas con cebolla", cuisine: "hola" })
+  .then(result => {
+    console.log("result", result);
+    Recipe.insertMany(data).then(result => {
+      result.forEach(item => {
+        console.log(item.title, item._id);
+      });
+      Recipe.updateOne(
+        { title: "Rigatoni alla Genovese" },
+        { duration: 100 }
+      ).then(result => {
+        console.log("updated", result);
+        Recipe.findOneAndDelete({ title: "Carrot Cake" }).then(result => {
+          console.log("borrado");
+          mongoose.connection.close().then(result => {
+            console.log("Mongoose default connection closed");
+          });
+        });
+      });
+    });
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
 
 // Recipe.insertMany(data)
 // .then(result => {

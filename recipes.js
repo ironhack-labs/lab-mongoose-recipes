7784dc7874477d577a1e0d1bf18c3e9cb8d2057a
Teresa Romero <tromerolozano@gmail.com>
2018-11-10 21:18:17 +0100

    done

--- recipes.js
+++ recipes.js
@@ -1,30 +1,23 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 const data = require("./data.js");
+const Recipe = require("./Recipe");
 
-const recipeSchema = new Schema({
-  title: { type: String, required: true, unique: true },
-  level: {
-    type: String,
-    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
-  },
-  ingredients: Array,
-  cuisine: { type: String, required: true },
-  dishType: {
-    type: String,
-    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
-  },
-  image: {
-    type: String,
-    default: "https://images.media-allrecipes.com/images/75131.jpg"
-  },
-  duration: { type: Number, min: 0 },
-  creator: String,
-  created: { type: Date, default: Date.now }
-});
+mongoose
+  .connect(
+    "mongodb://localhost/recipeApp",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
 
-const Recipe = mongoose.model("Recipe", recipeSchema);
+// delete all collection
+Recipe.collection.drop();
 
+// create one recipe
 Recipe.create({
   title: "Mi primera receta",
   level: "Easy Peasy",
@@ -34,44 +27,39 @@ Recipe.create({
   creator: "Teresa"
 })
   .then(recipe => {
-    console.log("The recipe is saved and its value is: ", recipe);
+    console.log("Just added the recipe: ", recipe.title);
+
+    // insert all recipes of data
+    Recipe.insertMany(data)
+      .then(recipe => {
+        for (let i = 0; i < recipe.length; i++) {
+          console.log("Just added recipe " + recipe[i].title);
+        }
+
+        // update the recipe when the insert has finished
+        Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+          .then(recipe => {
+            console.log("The recipe was updated");
+
+            // delete Recipe when the update has ended
+            Recipe.deleteOne({ title: "Carrot Cake" })
+              .then(recipe => {
+                console.log("The recipe was deleted");
+
+                mongoose.disconnect()
+              })
+              .catch(err => {
+                console.log("An error happened:");
+              });
+          })
+          .catch(err => {
+            console.log("An error happened:");
+          });
+      })
+      .catch(err => {
+        console.log("An error happened:");
+      });
   })
   .catch(err => {
     console.log("An error happened:", err);
-  });
-
-Recipe.insertMany(data)
-.then(recipe => {
-  console.log("The recipes were saved");
-
-  Recipe.updateOne({title:"Rigatoni alla Genovese"},{duration:100})
-  .then(recipe => {
-    console.log("The recipe was updated");
-
-    Recipe.deleteOne({ title: "Carrot Cake"})
-    .then(recipe => {
-      console.log("The recipe were deleted");
-    })
-    .catch(err => {
-      console.log("An error happened:");
-    });
-  })
-
-  .catch(err => {
-    console.log("An error happened:");
-  });
-})
-.catch(err => {
-  console.log("An error happened:");
-});
-
-
-
-mongoose
-  .connect("mongodb://localhost/recipeApp")
-  .then(() => {
-    console.log("Connected to Mongo!");
-  })
-  .catch(err => {
-    console.error("Error connecting to mongo", err);
   });

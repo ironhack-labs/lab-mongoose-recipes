375af5b3dab5aebae736dfdad79aaa2281433be1
aemartos <aemartos@gmail.com>
2018-11-10 14:41:08 +0100

    Start

--- recipes.js
+++ recipes.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
+const mongoose = require("mongoose");
+//const Schema = mongoose.Schema;
+const Recipe = require('./recipe');
+const data = require("./data.js");
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose
+  .connect("mongodb://localhost/recipeApp")
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });

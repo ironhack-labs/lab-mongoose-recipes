3d6c75ec0036c8432e1571e2da1cdd496d8ad0c0
Gabriel Cueva <gabrielcueva_10@hotmail.com>
2018-11-10 20:29:37 +0100

    Con mucho esfuezo al iteracion2

--- recipes.js
+++ recipes.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
@@ -14,10 +14,21 @@ mongoose.connect('mongodb://localhost/recipeApp')
     title: {type: String, required: true, unique: true},
     level: { type: String, enum: ['Easy Peasy','Amateur Chef','UltraPro Chef'] },
     ingredients: { type: Array},
-    cuisine: {type: String, required: true, unique: true},
+    cuisine: {type: String, required: true,},
     dishType: {type: String, enum: ['Breakfast','Dish','Snack','Drink','Dessert','Other']},
     image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
     duration: {type: Number, min: 0},
     creator: {type: String},
-    created: {type: Date, default: "Today"}
+    created: {type: Date, default:Date.Today}
   });
+
+  const Recipe =  mongoose.model('Recipe', recipeSchema);
+
+  Recipe.create({title: data[0].title, level: data[0].level, cuisine:data[0].cuisine,image: "https://images.media-allrecipes.com/images/75131.jpg"})
+  .then((recipe) => { console.log('The user is saved and its value is: ', recipe) })
+  .catch((err) => { console.log('An error happened:', err) });
+
+
+
+
+

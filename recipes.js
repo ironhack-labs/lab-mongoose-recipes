8d88c686911ebcc3d1659c93face085049070151
Gabriel Cueva <gabrielcueva_10@hotmail.com>
2018-11-12 18:01:31 +0100

    Terminado

--- recipes.js
+++ recipes.js
@@ -1,38 +1,68 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
 
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect(
+    "mongodb://localhost/recipeApp",
+    { useNewUrlParser: true }
+  )
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
-
-  const recipeSchema = new Schema({
-    title: {type: String, required: true, unique: true},
-    level: { type: String, enum: ['Easy Peasy','Amateur Chef','UltraPro Chef'] },
-    ingredients: { type: Array},
-    cuisine: {type: String, required: true,},
-    dishType: {type: String, enum: ['Breakfast','Dish','Snack','Drink','Dessert','Other']},
-    image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
-    duration: {type: Number, min: 0},
-    creator: {type: String},
-    created: {type: Date, default:Date.now}
-  });
-
-  const Recipe =  mongoose.model('Recipe', recipeSchema);
-
-  Recipe.create({title: data[0].title, level: data[0].level, cuisine:data[0].cuisine,image: "https://images.media-allrecipes.com/images/75131.jpg"})
-  .then((recipe) => { console.log('The recipe is saved!') })
-  .catch((err) => { console.log('An error happened:', err) });
-
-  Recipe.collection.drop();
-
-Recipe.insertMany(data,function(error,docs){ 
-  for(let i = 0;i < data.length ;i++){
-    console.log (data[i].title);
-  }
+const recipeSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: { type: Array },
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now }
 });
 
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+Recipe.collection.drop();
+
+Recipe.insertMany(data, function(error, docs) {
+  if (docs) {
+    console.log("Insertados correctamente");
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].title);
+    }
+
+    Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+
+      .then(console.log("actualizado corractamente campo duration"))
+
+      .catch(console.log("Fallo al actulaizar campo duration"));
+
+    Recipe.deleteOne({ title: "Carrot Cake" })
+
+      .then( () => mongoose.disconnect(), console.log("borrado documento correctamente"))
+
+      .catch( () => mongoose.disconnect(), console.log("problema al borrar documento"));
+      
+  }
+  if (error) {
+    console.log(
+      "no se han podido insertar los documentos correctamente",
+      error
+    );
+  }
+});

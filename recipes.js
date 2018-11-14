64eb568e62cca966cc11f738176a38abd612abf1
Zeswen <zeswentyrael@gmail.com>
2018-11-14 18:15:21 +0100

    Iteration 4

--- recipes.js
+++ recipes.js
@@ -5,14 +5,16 @@ const Recipe = require(`./models/RecipeSchema`)
 
 
 mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
-
-Recipe.collection.drop();
+	.then(() => {
+		console.log('Connected to Mongo!');
+	}).catch(err => {
+		console.error('Error connecting to mongo', err);
+	});
 
 Recipe.insertMany(data)
-  .then(recipe => recipe.forEach((elem) => console.log(`The recipe is created and it is: `, elem.title)))
-  .catch(err => console.log(`An error happened: `, err))
+	.then(recipe => recipe.forEach((elem) => console.log(`The recipe is created and it is: `, elem.title)))
+	.catch(err => console.log(`An error happened: `, err))
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+	.then(recipe => console.log(`Duration of Rigatoni updated`))
+	.catch(err => console.log(`An error has ocurred:`, err))

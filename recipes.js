8c54cb3d352fe023ea13ff8d19c77350cf88582b
Yared Santiago <yaredmyers@gmail.com>
2018-11-14 19:26:30 +0100

    iteration 2

--- recipes.js
+++ recipes.js
@@ -2,24 +2,36 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
 
-mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+
 
   const recipeSchema = new Schema({
-    title : {String, required: true, unique: true},
-    level: {String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
-    ingredients : {Array},
-    cuisine: {String, required: true},
-    dishType: {String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
-    image: {String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
-    duration: {Number, min: 0},
-    creator: {String},
-    created:{Date, default: Date.now},
+    title : {type: String, required: true, unique: true},
+    level: {type:String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+    ingredients : {type:Array},
+    cuisine: {type:String, required: true},
+    dishType: {type:String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+    image: {type:String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+    duration: {type:Number, min: 0},
+    creator: String,
+    created:{type:Date, default: Date.now},
   });
   
   const Recipe = mongoose.model('Recipe', recipeSchema);
   module.exports = Recipe;
+
+
+  mongoose.connect('mongodb://localhost/recipeApp')
+  .then(() => {
+    console.log('Connected to Mongo!');
+    Recipe.create({ title: 'Tallarines', cuisine: "china"}, function (err, user) {
+    if (err) {
+        console.log('An error happened:', err);
+    } else {
+        console.log('The user is saved and its value is: ', user);
+    }
+  });
+  }).catch(err => {
+    console.error('Error connecting to mongo', err);
+  });
+
+  

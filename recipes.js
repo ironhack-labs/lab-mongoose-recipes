4e33c5221576a8185e0da66a28a326668b5a2eba
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-14 21:27:26 +0100

    Iteration 1 Fixedgit add .

--- recipes.js
+++ recipes.js
@@ -22,7 +22,10 @@ mongoose.connect('mongodb://localhost/recipeApp')
       duration: 40,
       creator: 'Chef LePapu'
     })
-  }).then(() => Recipe.collection.drop())
+  }).then((recipe) => {
+    Recipe.collection.drop()
+    return recipe
+  })
   .then((recipe) => {
     console.log(recipe)
     return Recipe.insertMany(data)
@@ -34,10 +37,10 @@ mongoose.connect('mongodb://localhost/recipeApp')
   })
   .then(() => {
     return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
-  }).then(() => console.log("Rigatoni alla Genovese duration updated tog 100"))
+  }).then(() => {console.log("Rigatoni alla Genovese duration updated tog 100")})
   .then(() => {
     return Recipe.deleteOne({ title: 'Carrot Cake' })
-  }).then(() => console.log("Carrot Cake was deleted"))
+  }).then(() => {console.log("Carrot Cake was deleted")})
 
 
 

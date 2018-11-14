a95072929f18845d39ae4f15e6411d9cad59a497
Sonia Ruiz <yune.vk@gmail.com>
2018-11-14 20:20:35 +0100

    Iteration 4 - Update recipe

--- recipes.js
+++ recipes.js
@@ -10,15 +10,20 @@ mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
 
-    Recipe.create({ title: 'Arroz', cuisine: 'Spanish' })
-    .then(recipe => { console.log('The recipe is saved and its value is: ', recipe) })
-    .catch(err => { console.log('An error happened:', err) });
+    Recipe.collection.drop().then(() => {
+      Recipe.create({ title: 'Arroz', cuisine: 'Spanish' })
+      .then(recipe =>  console.log('The recipe is saved and its value is: ', recipe) )
+      .then(() => Recipe.insertMany(recipeData))
+      .then(() => Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 }))
+    }).catch(err => { console.log('An error happened:', err) });
 
+    // Recipe.insertMany(recipeData)
+    // .then(recipes => { console.log('The recipes are saved and its value are: ', recipes) })
+    // .catch(err => { console.log('An error happened:', err) });
 
-    // insertMany
-    Recipe.insertMany(recipeData)
-    .then(recipes => { console.log('The recipes are saved and its value are: ', recipes) })
-    .catch(err => { console.log('An error happened:', err) });
+    // Recipe.updateOne({ name: "Alice"}, { company: "Ironhack" })
+    // .then(recipe => { console.log('The recipes are updated and its value id: ', recipe) })
+    // .catch(err => { console.log('An error happened:', err) });
 
     
   }).catch(err => {

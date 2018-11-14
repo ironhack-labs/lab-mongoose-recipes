e05aada603917777fc2421e854e1e43d6629689a
Jorge Devesa <jrgdevesa@gmail.com>
2018-11-14 20:45:59 +0100

    Iteration 2 - done

--- recipes.js
+++ recipes.js
@@ -1,22 +1,52 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
 
-mongoose.connect('mongodb://localhost/recipeApp')
+const recipeSchema = new Schema({
+  title: { type: String, unique: true, required: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: { type: Array },
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  date: { type: Date, default: Date.now }
+});
+
+const recipe = mongoose.model("recipe", recipeSchema);
+
+mongoose
+  .connect("mongodb://localhost/recipeApp")
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+    recipe.collection.drop()
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
-  mongoose.model('Recipe',{
-    title: {type: string, unique: true},
-    level: {type: string, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
-    ingredients: {type: array},
-    cuisine: {type: string, required: true},
-    dishType: {type: string, enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
-    image: { type: string, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
-    duration: { type: number, min: 0},
-    creator: {type: string},
-    date: {type: date, default: date.now}
+recipe
+  .create({
+    title: "chips with eggs",
+    level: "UltraPro Chef",
+    ingredients: ["eggs", "potatoes", "oil"],
+    cuisine: "???",
+    dishType: "Dish",
+    image:
+      "http://www.lacocinademasito.com/files/huevos-rotos-con-jamon-y-patatas-660x412.jpg",
+    duration: 15,
+    creator: "Giorgio",
+    created: 17 / 17 / 2000
   })
+  .then(() => console.log("ok"))
+  .catch(err => console.log(err));

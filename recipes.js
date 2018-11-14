014be908c2d0030295ede6bcb522f0a71e2b39c1
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-11-14 20:24:55 +0100

    Iteration 2

--- recipes.js
+++ recipes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
-const Cat = require('./models/recipeSchema.js');
+const Recipe = require('./models/recipeSchema.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -9,3 +9,15 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  Recipe.create({ 
+    title    : "Strange recipe",
+    level : `Easy-Peasy`,
+    ingredients: [`Potatoes`, `Tomato`, `Cheese`, `Apples`],
+    cuisine: "Russian",
+    dishType:`Other`,
+    duration: 30,
+    creator: `Anna Zhuravleva`
+  })
+  .then(recipes => console.log(`The recipe's title is: ${recipes.title}`))
+  .catch(err => { console.log('An error happened:', err) });

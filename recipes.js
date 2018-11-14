37f21c30fa8695aca2c009f2d77d6b42e8d00af2
sitobusnan <sito@citytonight.es>
2018-11-14 20:10:20 +0100

    it 5

--- recipes.js
+++ recipes.js
@@ -1,31 +1,52 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
-
-mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
 
 const recipeSchema = new Schema({
-  title: { type: String, required: true, unique: true},
-  level: { type: String, enum:['Easy Peasy','Amateur Chef','UltraPro Chef']},
-  ingredients: {type: Array},
-  cuisine: { type: String, required: true},
-  dishType: { type: String, enum: ['Breakfast','Dish','Snack','Drink','Dessert','Other']},
-  image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
-  duration: { type: Number, min: 0},
-  creator: { type: String},
-  created: { type: Date, default: Date.now}
-})
+  title: { type: String, required: true, unique: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: { type: Array },
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now }
+});
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-module.exports = Recipe;
+var arr = [...data];
+const Recipe = mongoose.model("Recipe", recipeSchema);
+//module.exports = Recipe;
 
-Recipe.collection.drop();
+mongoose.connect("mongodb://localhost/recipeApp")
+  
+  .then(() => {
+    console.log("Connected to Mongo!");
+    return Recipe.collection.drop();
+  })
+  .then(() => {
+    return Recipe.insertMany(arr);
+  })
+  .then(() => {
+    console.log("Update Succesfull");
+    return Recipe.updateOne({ title: "Rigatoni alla Genovese"},{ duration: 100 });
+  })
+  .then(() => {
+    console.log("Recipe Delete!!!");
+    return Recipe.deleteOne({ title:"Carrot Cake"});
+  })
 
-var arr = data;
-Recipe.insertMany(arr);
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
 

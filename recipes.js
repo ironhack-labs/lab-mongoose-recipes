9835aebbb6f2bfea1ce92f543d437567ccf8f33b
cocobilbao <cocobilbao@gmail.com>
2018-11-14 20:03:35 +0100

    iteration 1

--- recipes.js
+++ recipes.js
@@ -8,3 +8,16 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+
+  const recepieSchema = new Schema({
+    title:  { type: String},
+    level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']}, 
+    ingrediets: [],
+    cuisine: [],
+    dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert','Other'] },
+    image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+    duration: { type: Number },
+    creator: { type: String },
+    created: { type: Date, default: Date.now },
+  });

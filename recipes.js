0300f3984645e493659c91bef833d0316fdf6d9f
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-11-14 20:07:24 +0100

    Iteration 1

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Cat = require('./models/recipeSchema.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {

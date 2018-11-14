d49c25625bf86d126c4c8af21371d60b5d5adaf2
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-14 19:53:02 +0100

    Iteration 1

--- recipes.js
+++ recipes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./models/Recipe.js')
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +8,5 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  

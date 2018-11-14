7c990d2f55adcc7b4122d018ffc1d1cc29e29652
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-14 20:34:29 +0100

    Iteration 3 fixed

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-    name: {
+    title: {
         type: String
     },
     level: {
--- recipes.js
+++ recipes.js
@@ -7,17 +7,33 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
+  })
+  .then(() => {
+    Recipe.collection.drop()
+  })
+  .then(() => {
+    return Recipe.create({
+      title: 'Asian Glazed Chicken Thighs',
+      level: 'Amateur Chef',
+      ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+      cuisine: 'Asian',
+      dishType: ['Dish'],
+      image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+      duration: 40,
+      creator: 'Chef LePapu'
+    })
+  })
+  .then((recipe) => {
+    console.log(recipe)
+    return Recipe.insertMany(data)
+  })
+  .then((recipes) => {
+    return recipes.forEach(function (e) {
+      console.log(e.title)
+    })
+  })
+
+
+  .catch(err => {
+    console.error('An error happened', err);
   });
-
-Recipe.create({
-  title: 'Asian Glazed Chicken Thighs',
-  level: 'Amateur Chef',
-  ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
-  cuisine: 'Asian',
-  dishType: ['Dish'],
-  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
-  duration: 40,
-  creator: 'Chef LePapu'
-})
-
-Recipe.insertMany(data)

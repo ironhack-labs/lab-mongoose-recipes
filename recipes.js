0e2ca908313f7802f98cf136f7a89264aeaa5816
cgandiaf <c.gandia.f@gmail.com>
2018-11-14 18:57:54 +0100

    Iteration 1

--- recipes.js
+++ recipes.js
@@ -8,3 +8,22 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+const Recipe = new Schema({
+  title : { type:String, require:true, unique: true},
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+  ingredients  : Array,
+  cuisine: { type:String, require:true },
+  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] },
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration: { type: Number, min: 0},
+  creator: String,
+  created: { 
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+//const Recipe = mongoose.model('Recipe', catSchema);
+//module.exports = Recipe;

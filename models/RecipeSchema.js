196e4a5af23799357305cc0f7a3113bb02f18e9a
Sandra Luengo <luchosandra@gmail.com>
2018-11-14 18:51:22 +0100

    Iteration 3 - Insert Many recipes

--- models/RecipeSchema.js
+++ models/RecipeSchema.js
@@ -5,7 +5,7 @@ const recipeSchema = new Schema({
     title: { type: String, required: true, unique: true },
     level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
     ingredients: { type: Array },
-    cusine: { type: String, required: true },
+    cuisine: { type: String, required: true },
     dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] },
     image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
     duration: { type: Number },
--- recipes.js
+++ recipes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const data = require('./data.js');
+
+//Importamos nuestro esquema, indicando la ruta
 const Recipe = require('./models/RecipeSchema.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
@@ -10,7 +12,13 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err);
   });
 
-Recipe.create({title: 'Recipe 1', level: 'Easy Peasy', ingredients: ['Tomatoe','Fish'], cusine: 'Spanish'
+
+//Primera insercion a la BBDD, si la BBDD no esta creada la crea.
+Recipe.create({title: 'Recipe 1', level: 'Easy Peasy', ingredients: ['Tomatoe','Fish'], cuisine: 'Spanish'
 , dishType : 'Dish',duration: 120, creator: 'Sandra' })
-  .then(user => { console.log('The user is saved and its value is: ', user) })
-  .catch(err => { console.log('An error happened:', err) });
+  .catch(err => { console.log('An error happened:', err) })
+  
+
+//Añadimos varias recetas a la vez del archivo data.js
+
+Recipe.insertMany(data);

90b7edbb17a2c37874682d5ec23b47f16ca8995c
Jorge Devesa <jrgdevesa@gmail.com>
2018-11-14 20:16:33 +0100

    Iteration 1 - done

--- recipes.js
+++ recipes.js
@@ -8,3 +8,15 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  mongoose.model('Recipe',{
+    title: {type: string, unique: true},
+    level: {type: string, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
+    ingredients: {type: array},
+    cuisine: {type: string, required: true},
+    dishType: {type: string, enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
+    image: { type: string, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+    duration: { type: number, min: 0},
+    creator: {type: string},
+    date: {type: date, default: date.now}
+  })

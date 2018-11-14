722d28c7428d7f0387c3ffee4583480fa3897141
Sandra Luengo <luchosandra@gmail.com>
2018-11-14 17:04:23 +0100

    Iteration 2 - Create a recipe

--- recipes.js
+++ recipes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
-const Recipe = require('/models/RecipeSchema.js');
+const Recipe = require('./models/RecipeSchema.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -10,4 +10,7 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err);
   });
 
- 
+Recipe.create({title: 'Recipe 1', level: 'Easy Peasy', ingredients: ['Tomatoe','Fish'], cusine: 'Spanish'
+, dishType : 'Dish',duration: 120, creator: 'Sandra' })
+  .then(user => { console.log('The user is saved and its value is: ', user) })
+  .catch(err => { console.log('An error happened:', err) });

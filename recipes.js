0bf1bfb54224ee52986e35c8917ab6c811b2ae03
Zeswen <zeswentyrael@gmail.com>
2018-11-14 16:36:46 +0100

    Iteration 1

--- recipes.js
+++ recipes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require(`./models/RecipeSchema`)
+
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {

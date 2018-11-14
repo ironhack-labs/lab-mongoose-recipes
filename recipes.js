1ae22fdd9fb7d4f03e0d6b70c5d765b288903b86
Sandra Luengo <luchosandra@gmail.com>
2018-11-14 20:06:41 +0100

    Todas las operaciones en una funcion

--- recipes.js
+++ recipes.js
@@ -1,29 +1,31 @@
 const mongoose = require('mongoose');
 const data = require('./data.js');
 
-//Importamos nuestro esquema, indicando la ruta
 const Recipe = require('./models/RecipeSchema.js');
+
+let operation = () => {
+
+  Recipe.collection.drop()
+    .then(() =>
+      Recipe.create({
+        title: 'Recipe 1', level: 'Easy Peasy', ingredients: ['Tomatoe', 'Fish'], cuisine: 'Spanish'
+        , dishType: 'Dish', duration: 120, creator: 'Sandra'
+      }))
+    .then(() => Recipe.insertMany(data))
+    .then(() => Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 }))
+    .then(() => Recipe.deleteOne({ title: "Carrot Cake" }))
+    .then(() => console.log('OK Giorgio'))
+    .then(() => {
+      mongoose.connection.close();
+      console.log('connection close');
+    })
+    .catch(err => { console.log('An error happened:') })
+
+}
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
-    console.log('Connected to Mongo!');
+    operation();
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
-
-Recipe.collection.drop()
-  
-
-Recipe.create({
-  title: 'Recipe 1', level: 'Easy Peasy', ingredients: ['Tomatoe', 'Fish'], cuisine: 'Spanish'
-  , dishType: 'Dish', duration: 120, creator: 'Sandra'
-})
-  .then(() => Recipe.insertMany(data))
-  .then(() => Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 }))
-  .then(()=> Recipe.deleteOne({ title: "Carrot Cake" }))
-  .then(()=> console.log('OK Giorgio'))
-  .then(()=> {
-    mongoose.connection.close();
-    console.log('connection close');
-  })
-  .catch(err => { console.log('An error happened:') })

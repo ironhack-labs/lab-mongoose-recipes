f3b4ef9c68eaef0acd3d100a1339638f3d1a559d
Yared Santiago <yaredmyers@gmail.com>
2018-11-14 19:01:40 +0100

    iteration 1

--- recipes.js
+++ recipes.js
@@ -8,3 +8,18 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  const recipeSchema = new Schema({
+    title : {String, required: true, unique: true},
+    level: {String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+    ingredients : {Array},
+    cuisine: {String, required: true},
+    dishType: {String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+    image: {String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+    duration: {Number, min: 0},
+    creator: {String},
+    created:{Date, default: Date.now},
+  });
+  
+  const Recipe = mongoose.model('Recipe', recipeSchema);
+  module.exports = Recipe;

f664c11b23a2e0b5d605957a07de8f1c41cb5723
cocobilbao <cocobilbao@gmail.com>
2018-11-15 06:46:56 +0100

    iteration 2

--- recipes.js
+++ recipes.js
@@ -11,13 +11,18 @@ mongoose.connect('mongodb://localhost/recipeApp')
 
 
   const recepieSchema = new Schema({
-    title:  { type: String},
+    title:  { type: String, required: true, unique: true},
     level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']}, 
     ingrediets: [],
-    cuisine: [],
+    cuisine: { type: String, required: true},
     dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert','Other'] },
     image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
-    duration: { type: Number },
+    duration: { type: Number, min:0 },
     creator: { type: String },
     created: { type: Date, default: Date.now },
   });
+
+  const Recepie = mongoose.model('Recepie', recepieSchema);
+    module.exports = Recepie;
+
+    console.log(Recepie);

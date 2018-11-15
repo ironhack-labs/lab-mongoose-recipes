c63d1ce10d219d90053fd9f1e0475ec18ecae8ad
Sonia Bravo <rinbra0@hotmail.com>
2018-11-15 09:17:05 +0100

    done

--- models/recipe.js
+++ models/recipe.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 //aquí abajo hago el esquema con este formato.
 const recipeSchema = new Schema({
-  title: { type: String, required, unique},
+  title: { type: String, required:true, unique:true},
   level : { type: String, enum: ['Easy Peasy', 'Amateur Chef ', 'UltraPro Chef']},
   ingredients: { type: Array},
-  cuisine: { type: String, required},
+  cuisine: { type: String, required:true},
   dishType: { type: String, enum: ['Breakfast','Dish','Snack','Drink','Dessert,Other']},
   image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
   duration: { type: Number, min:0},
--- recipes.js
+++ recipes.js
@@ -6,8 +6,40 @@ const data = require('./data.js');
 const recipeSchema = require('./models/recipe.js')
 //esto de abajo es para conectar con mongo:
 mongoose.connect('mongodb://localhost/recipeApp')
+
+//esto: es una promesa de que si se conecta a mongo haga un drop, que es borrar la coleccion.
   .then(() => {
     console.log('Connected to Mongo!');
-  }).catch(err => {
+    return recipeSchema.collection.drop(); //borro la coleccion
+  })
+
+  //aqui abajo otro then concatenado con el primero, en el que creo una entrada manualmente:
+  .then(() => { 
+    return recipeSchema.create({title:"Pizza", cuisine:"Italian"})
+  })
+  //aqui abajo otra promesa para añadir muchos datos a la vez: data son los datos que le paso
+  .then(() => {
+    return recipeSchema.insertMany(data)////aqui te mete todas las recetas con el insertmany
+  })
+
+  //aquí abajo para actualizar los campos de una promesa, primeros {} llamo a la que quiero, 
+  // y los segundos  {} modifico lo que quiero:
+  .then(() => {
+    return recipeSchema.updateOne({ 
+      title: 'Orange and Milk-Braised Pork Carnitas'},
+      {duration: 60})
+
+  })
+// aqui abajo deleteOne le digo uno que borrar:
+  .then(() => {
+    return recipeSchema.deleteOne({
+      title: 'Carrot Cake'})
+  })
+// aqui abajo me desconecto de moongose:
+  .then(() => {
+    moongose.disconect()
+  })
+  
+  .catch(err => {
     console.error('Error connecting to mongo', err);
   });

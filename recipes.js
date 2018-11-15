1d478f29bf5980576cdf3e08833bbf5b594b5a5f
Zeswen <zeswentyrael@gmail.com>
2018-11-15 11:02:38 +0100

    Done with promises

--- recipes.js
+++ recipes.js
@@ -7,13 +7,22 @@ mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
     return Recipe.collection.drop()
-      .then(() => Recipe.insertMany(data)
-        .then(recipe => recipe.forEach(elem => console.log('The recipe is created and it is: ', elem.title)))
-        .then(() => Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
-          .then(() => console.log('Duration of Rigatoni updated'))
-          .then(() => Recipe.deleteOne({ title: 'Carrot Cake' })
-            .then(() => console.log('Carrot Cake removed'))))
-        .catch(err => console.log('An error happened: ', err)))
+      .then(() => Recipe.create({
+        title: 'La cuisine',
+        level: 'Amateur Chef',
+        ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+        cuisine: 'Asian',
+        dishType: ['Dish'],
+        image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+        duration: 60,
+        creator: 'Chef Super Pro',
+      })
+        .then(() => Recipe.insertMany(data)
+          .then(recipe => recipe.forEach(elem => console.log('The recipe is created and it is: ', elem.title)))
+          .then(() => Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+            .then(() => console.log('Duration of Rigatoni updated'))
+            .then(() => Recipe.deleteOne({ title: 'Carrot Cake' })
+              .then(() => console.log('Carrot Cake removed'))))))
       .catch((err) => {
         console.error('Error connecting to mongo', err);
       });

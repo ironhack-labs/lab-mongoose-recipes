496de4e358ff1454202662e646669565647489a6
danielsuidan <danielsuidan@gmail.com>
2018-11-15 09:50:19 +0100

    Done

--- recipes.js
+++ recipes.js
@@ -2,6 +2,58 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
 
+
+mongoose.connect('mongodb://localhost/recipesApp');
+
+const recipeSchema = new Schema({
+  title: { type: String, required: true },
+  level: { type: String, enum: ['Easy Peasy',  'Amateur Chef', 'UltraPro Chef'] },
+  ingredients:{type:[]},
+  cuisine:{type:String},
+  images: { type: String, default: 'images/default-avatar.png' },
+  dishType: { type: String, enum: ['Breakfast', 'Dish',  'Snack' , 'Drink' , 'Dessert', 'Other'] },
+  duration: { type: Number, min: 0,},
+  creator:{type:String},
+  created: { 
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Recipe=mongoose.model('recipe',recipeSchema)
+module.exports = Recipe;
+
+Recipe.create({
+  title: 'lentejas',
+  level: 'UltraPro Chef',
+  ingredients:['lentejas','mortadela'],
+  cuisine:'italiana',
+  images: null,
+  dishType:'Breakfast',
+  duration:30,
+  creator:'Marcco',
+  created:null,
+  })
+  .then(recipe => { console.log(recipe.title) })
+  .catch(err => { console.log('An error happened:', err) });
+
+
+
+  Recipe.insertMany(data)
+  .then(recipe => { console.log(recipe.title) })
+  .catch(err => { console.log('An error happened:', err) });;
+
+
+
+
+
+
+
+
+
+    
+
+
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');

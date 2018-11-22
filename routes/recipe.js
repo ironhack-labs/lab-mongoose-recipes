1ac5a2bbe614a6aee503175196716dc27a438c27
MontseOrtiz <montse_2908@hotmail.com>
2018-11-21 19:05:31 -0600

    Iteracion3

--- routes/recipe.js
+++ routes/recipe.js
@@ -16,7 +16,23 @@ router.get('/new', (req,res)=>{
     .then(r=>{
         console.log(r.title)
         res.send(r.title)
-        // res.redirect('/')
+        // res.redirect('/recipes')
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+
+router.get('/many', (req,res)=>{
+    Recipe.insertMany(require('../data'))
+    .then(r=>{
+        for(var dish of r){
+            console.log(dish.title)
+        }
+    })
+    .catch(err=>{
+        console.log(err)
     })
 })
 

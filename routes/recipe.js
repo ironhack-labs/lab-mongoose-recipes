bc936882f6d896ae8c54b17e08fe60eca35adece
MontseOrtiz <montse_2908@hotmail.com>
2018-11-21 19:18:14 -0600

    Iteracion5

--- routes/recipe.js
+++ routes/recipe.js
@@ -46,4 +46,14 @@ router.get('/update', (req,res)=>{
     })
 })
 
+
+router.get('/delete', (req,res)=>{
+    Recipe.deleteOne({title: "Carrot Cake"})
+    .then(r=>{
+        res.send(r)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 module.exports = router

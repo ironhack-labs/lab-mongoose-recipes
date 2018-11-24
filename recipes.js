6f6da173516e2eb45887808142e6290a0fd6ab69
nicolas prieur <nicolas.prieurdelacomble@gmaile.com>
2018-11-24 17:05:42 +0100

    mongoos

--- recipes.js
+++ recipes.js
@@ -8,3 +8,40 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+
+  const RecipeSchema = new Schema({
+    title : {type: String, unique: true},
+    level: String,
+    ingredients  : Array,
+    cuisine : String,
+    dishType: String,
+    image : { type : String , default:"https://images.media-allrecipes.com/images/75131.jpg"},
+    duration : { type : Number , min:0},
+    creator : String,
+    created : { type : Date , default: Date.now },
+  });
+
+
+  const Recipe = mongoose.model('Recipe', RecipeSchema);
+  module.exports = Recipe;
+  
+  Recipe.create({ title : "Salade de pâtes", level : "5" , ingredients : [1,4,5] , cuisine : "française" ,
+    dishType : "breakfast", duration : "15"})
+    .then( RecipePate => { console.log('The user is saved and its value is: ',RecipePate.title) })
+    //.catch(err => { console.log('An error happended: ', err)})
+
+  
+  Recipe.insertMany(data)
+    .then( Recips => {console.log('The user is saved and its value is: ', Recips)} )
+
+
+  Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+  .then(successCallback)
+  .catch(errorCallback);
+
+
+
+
+    
+

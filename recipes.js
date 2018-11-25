8a952b48b5175fd5a21c680f3ee44355a297c914
Rita Mernissi <mernissi.rita@gmail.com>
2018-11-25 11:38:53 +0100

    Done

--- recipes.js
+++ recipes.js
@@ -1,39 +1,85 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = require("./data.js");
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose
+  .connect("mongodb://localhost/recipeApp")
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
-
-  //1. Create a schema for the recipes 
+//1. Create a schema for the recipes
 const recipeSchema = new Schema({
-  title: { type: String, required: true, unique: true},
-  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+  title: { type: String, required: true, unique: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
   ingredients: { type: Array },
   cuisine: { type: String, required: true },
-  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] },
-  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
-  duration: { type: Number, min: 0},
-  creator: { type: String},
-  created: { type: Date, default: Date.now}
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now }
+});
+// 2. Create a recipe object that I can crud 
+const Recipe = mongoose.model("Recipe", recipeSchema);
+module.exports = Recipe;
+
+// 3. Create a recipe 
+var createOneRecipe = Recipe.create({
+  title: "Risotto",
+  level: "Easy Peasy",
+  ingredients: ["riz", "wine", "mushrooms"],
+  cuisine: "italian",
+  dishType: "Dish",
+  duration: "30",
+  creator: "Sandra et Rita"
 })
- // 2. Create a recipe object que je peux crud
- const Recipe = mongoose.model('Recipe', recipeSchema);
- module.exports = Recipe;
+  .then(() => console.log("Risotto created"))
+  .catch(() => console.log("error creating the risotto"));
 
- // 3. Create a recipe 
- Recipe.create({ title: 'Risotto', level: 'Easy Peasy', ingredients: ['riz', 'wine', 'mushrooms'], cuisine: 'italian', dishType: 'Dish', duration: '30', creator: 'Sandra et Rita'})
-  .then(() => console.log('success'))
-  .catch(() => console.log('error'));
-  
- 
-// 3. INSERT MANY RECIPES
-Recipe.insertMany(data)
-  .then(() => Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: '100'}))
-  .catch(()=> console.log('Error')); 
+// 3. Insert many recipes. When it's done, update a recipe and remove another one. 
+var insertAll = Recipe.insertMany(data)
+  .then(() => {
+    console.log("All recipes inserted");
+    var updateOneRecipe = Recipe.updateOne({ title: "Rigatoni alla Genovese" },{ duration: "100" })
+      .then(() => console.log("One recipe successfully updated"))
+      .catch(() => console.log("error updating a recipe"));
 
+    var deleteOneRecipe = Recipe.deleteOne({ title: "Carrot Cake" })
+      .then(() => console.log("One recipe successfully deleted"))
+      .catch(() => console.log("Error deleting a recipe"));
+
+    // wait for these 2 promises to end before closing the database
+    return Promise.all([updateOneRecipe, deleteOneRecipe]);
+  })
+  // close the database
+  .then(() => {
+    mongoose.connection.close();
+    console.log("Database closed");
+  })
+  .catch( () => console.log("Error closing the database"))
+
+
+
+  /* 
+  node recipes.js : 
+  _______________________________
+  Connected to Mongo!
+  Risotto created // All recipes inserted
+  All recipes inserted // Risotto created
+  One recipe successfully updated
+  One recipe successfully deleted
+  Database closed
+  */

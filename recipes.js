bd888ffe49ef350e317c5390f2c5b8e69a4e6f53
nirtzur <nirtzur89@gmail.com>
2018-12-13 21:02:37 +0100

    21:00

--- recipes.js
+++ recipes.js
@@ -8,3 +8,21 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+  const recipeSchema = new Schema ({
+    title: String ,
+    level: String ,
+    ingredients: String ,
+    cuisune: String ,
+    dishType: String ,
+    image: String ,
+    duration: Number ,
+    creator: String ,
+    created: Date
+  });
+  const recipe = mongoose.model('recipe', recipeSchema);
+
+  recipe.create({ title: 'Katchup Pasta' , level: 'Drunk' , ingredients: 'pasta and katchup' , cuisune: 'Poor Berliners' , dishType: 'Pasta' , image: 'unwatchable' , duration: 10 , creator: 'Nir'   });
+
+  recipe.insertMany(data)
+  .then(rec => { console.log('The user is saved and its value is: ', rec) })
+  .catch(err => { console.log('An error happened:', err) });  

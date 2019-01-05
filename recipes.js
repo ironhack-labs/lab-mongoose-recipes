c1b702d8c050d546d77c818a5da716a474e33e04
Antonio Colmenares <colmena.anto@gmail.com>
2019-01-05 12:58:27 -0500

    done

--- recipes.js
+++ recipes.js
@@ -8,3 +8,42 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+const recipesSchema = new Schema ({
+  title: {type: String, required: true, unique: true},
+  level: {type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
+  ingredients: {type: Array},
+  cuisine: {type: String, required: true},
+  dishType: {type: String, enum:["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
+  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: {type: Number, min: 0},
+  creator: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
+const Recipe = mongoose.model('Recipe', recipesSchema);
+
+Recipe.create({ title: "Pizza", 
+level: "Easy Peasy",
+ingredients: ['tomatoes, cheese, pizza base'],
+cuisine: "mettre la pizza au four et attendre",
+disType: "Dish",
+duration: 30,
+creator: "Justine"})
+.then(recipe => { console.log('The recipe title is: ', recipe.title)})
+.catch(err => { console.log('An error happened:', err) });
+
+Recipe.insertMany(data)
+.then( recipes => {
+  recipes.forEach(function(recipe, index){console.log("The recipe title is: ", recipe.title)
+})
+.catch(err => { console.log('An error happened:', err) });
+});
+
+Recipe.update({ title: "Rigatoni alla Genovese" }, { $set : { duration: 100}},  { new: true })
+.then(recipe => {console.log("Rigatoni alla Genovese duration updated!")})
+.catch(err => {console.log("there has been an error", err)});
+
+Recipe.findOneAndRemove({title: "Carrot cake"})
+.then(recipe => "Carrot Cake has been removed")
+.catch(err => { console.log('An error happened:', err) });

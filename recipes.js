d40563f3770da4d15ac01b82c141d23fb3802357
DanielGoycochea <danielgoycocheardz@gmail.com>
2019-01-07 21:23:55 -0600

    done

--- recipes.js
+++ recipes.js
@@ -1,6 +1,9 @@
+const express = require('express');
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipes = require('./models/Recipes');
+
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +11,29 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  Recipes.create({
+      title: 'Hot Dog',
+      level: 'Amateur Chef',
+      ingredients: ['Salchichas', 'Pan', 'Cebolla', 'Jitomate'],
+      cuisine: 'American',
+      dishType: ['Dish'],
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Hotdog_-_Evan_Swigart.jpg/1024px-Hotdog_-_Evan_Swigart.jpg',
+      duration: 10,
+      creator: 'Chef Daniel'
+    })
+    .then(Recipes=> {console.log('the recipes is saved and its title is:',Recipes.title)})
+    .catch(err=> {console.log('an error happened', err) });
+    
+ Recipes.insertMany(data)
+    .then(Recipes=> {console.log('Recipes are saved successfully')})
+    .catch(err=> {console.log('an error insert', err) });
+ 
+ Recipes.updateOne({title:"Rigatoni alla Genovese"}, {duration: 100})  
+    .then(Recipes=> {console.log('Update duration')})
+    .catch(err=> {console.log('an error update', err) });
+
+ Recipes.deleteOne({title: "Carrot Cake"}) 
+    .then(recipes=> {console.log('delete Carrot Cake'); mongoose.connection.close()})
+    .catch(err=> {console.log('an error delete', err); mongoose.connection.close() });
+

a19884a98c0256f50ffcef94402985519ed9b837
Moises Garcia <moisesgarcia83@gmail.com>
2019-01-10 14:21:42 +0100

    work finished

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./models/recipes.model');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +9,39 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+
+Recipe.create({ 
+  title: 'Paella',
+  level: "UltraPro Chef",
+  ingredients: ["rice","water","1/4 chicken","1/4 green beans", "colorant", "salt"],
+  cuisine: "Spanish",
+  dishType: "Dish",
+  image: "https://upload.wikimedia.org/wikipedia/commons/e/ed/01_Paella_Valenciana_original.jpg",
+  duration: 40,
+  creator: "Moi",
+})
+  .then(recipe => { console.log('The recipe is saved and is: ', recipe.title) })
+  .catch(err => { console.log('An error happened:', err) });
+
+Recipe.insertMany(data)
+.then(recipe => { console.log('The recipe is saved and is:', recipe.title) })
+.catch(err => { console.log('An error happened:', err) });
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+.then(recipe => { console.log('The recipe update and is:', recipe.title) })
+.catch(err => { console.log('An error happened:', err) });
+
+Recipe.deleteOne({ title: "Carrot Cake"})
+.then(recipe => { console.log('The recipe remove') })
+.catch(err => { console.log('An error happened:', err) });
+
+
+process.on('SIGINT', () => {  
+  mongoose.connection.close(() => { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
+}) 
+
+

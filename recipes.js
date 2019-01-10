3203b553fa4cd9eeba397cfecd5e2ee74df10139
Lucia Cazorla <a.lucia.cazorla@gmail.com>
2019-01-10 12:03:08 +0100

    in progress

--- recipes.js
+++ recipes.js
@@ -8,3 +8,20 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+const recipesSchema = new Schema({
+  title    : { type: String},
+  level : { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: {type: Array},
+  cuisine: {type: Array, required:true},
+  dishType: {type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type: Number, min: 0},
+  creator: {type: String},
+  created: {type: Date, default: Date.now}
+});
+const Recipe = mongoose.model('Recipe', recipesSchema);
+
+Recipe.create({ title:})
+
+module.exports = Recipe;

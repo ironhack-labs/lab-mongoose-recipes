226a7561f4558f86fe97116ee83c84df47bb658a
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2019-01-15 18:03:19 +0100

    second iteration done

--- recipes.js
+++ recipes.js
@@ -10,49 +10,66 @@ mongoose.connect('mongodb://localhost/recipeApp')
   });
 
 
-const recepie = new Schema ({
+const recepieSchema = new Schema ({
 
-  title: {
-    type: String
-    },
+    title: {
+      type: String
+      },
 
-  level: {
-     type: String, 
-     enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] 
-    },
+    level: {
+      type: String, 
+      enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] 
+      },
 
-  ingredients: {
-    type: String
-    },
+    ingredients: {
+      type: String
+      },
 
-  cuisine:{
-    type: String,
-    required: true,
-    },
+    cuisine:{
+      type: String,
+      required: true,
+      },
 
-  dishType:  {
-    type: String, 
-    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'], 
-    },
+    dishType:  {
+      type: String, 
+      enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'], 
+      },
 
-  image: { 
-    type: String, 
-    default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+    image: { 
+      type: String, 
+      default: 'https://images.media-allrecipes.com/images/75131.jpg' },
 
-  duration: {
-    type: Number,
-    min: 0
-    },
+    duration: {
+      type: Number,
+      min: 0
+      },
 
-  creator: {
-    type: String
-    },
+    creator: {
+      type: String
+      },
 
-  created: { 
-    type: Date,
-    default: Date.now
-    }
-  });
+    created: { 
+      type: Date,
+      default: Date.now
+      }
+    });
+
+
+const recepie = mongoose.model('recepie', recepieSchema);
+module.exports = recepie;
+
+recepie.create({ 
+  title: 'Thai green chicken curry',
+  level: 'Amateur Chef',
+  ingredients: [ '1 tbsp vegetable oil', ' 2 tbsp ready-made Thai green curry paste', '6 chicken thighs, skin and bones removed, meat cut into strips', '400ml tin coconut milk', '2 lime leaves (optional)', '2 tbsp Thai fish sauce', '1 tbsp caster sugar', 'handful green beans, trimmed', 'handful asparagus spears', 'salt and freshly ground black pepper'],
+  cuisine: 'Thai',
+  dishType: 'Dish',   
+  image: 'https://rasamalaysia.com/wp-content/uploads/2008/12/green_curry4.jpg',
+  duration: 50,   
+  creator: 'Someone',
+  }) 
+    .then(recepie => { console.log('The recepie is saved and its value is: ', recepie) })
+    .catch(err => { console.log('An error happened:', err) });
 
   
- 
+    

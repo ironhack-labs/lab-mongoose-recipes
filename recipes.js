c1f6fa06d36598bba0969054c25e1c39f53613aa
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2019-01-15 17:17:00 +0100

    first iteration done

--- recipes.js
+++ recipes.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
@@ -8,3 +8,51 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+
+const recepie = new Schema ({
+
+  title: {
+    type: String
+    },
+
+  level: {
+     type: String, 
+     enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] 
+    },
+
+  ingredients: {
+    type: String
+    },
+
+  cuisine:{
+    type: String,
+    required: true,
+    },
+
+  dishType:  {
+    type: String, 
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'], 
+    },
+
+  image: { 
+    type: String, 
+    default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+
+  duration: {
+    type: Number,
+    min: 0
+    },
+
+  creator: {
+    type: String
+    },
+
+  created: { 
+    type: Date,
+    default: Date.now
+    }
+  });
+
+  
+ 

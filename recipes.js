59c7f3dfad850d205a0a0623d8f856e90debbcda
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2019-01-16 00:17:25 +0100

    Done!

--- recipes.js
+++ recipes.js
@@ -1,47 +1,44 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./models/recipes.model');
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
-const schema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  level: {
-    type: String,
-    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
-  },
-  ingredients: {
-    type: Array
-  },
-  cuisine: {
-    type: String,
-  },
-  dishType: {
-    type: String,
-    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
-  },
-  image: {
-    type: String,
-    default: 'https://images.media-allrecipes.com/images/75131.jpg'
-  },
-  duration: {
-    type: Number,
-    min: 0
-  },
-  creator: {
-    type: String
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  }
-});
+Recipe.create({
+  title: 'Good Old Fashioned Pancakes',
+  level: 'UltraPro Chef',
+  ingredients: ['1 1/2 cups all-purpose flour', '3 1/2 teaspoons baking powder', '1 teaspoon salt', '1 tablespoon white sugar', '1 1/4 cups milk', '1 egg', '3 tablespoons butter, melted'],
+  cuisine: 'American',
+  dishType: 'Breakfast',
+  image: 'https://images.media-allrecipes.com/userphotos/560x315/4948036.jpg',
+  duration: 20,
+  creator: 'Javi',
+})
+  .then(recipe => { console.log(`This recipe has been saved as: ${recipe.title}`)})
+  .catch(err => { console.error(`Error ocurred: ${err}`)});
+
+Recipe.insertMany(data)
+  .then(recipe => { console.log(`This recipe has been saved as: ${recipe.title}`)})
+  .catch(err => { console.error(`Error ocurred: ${err}`)});
+
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then(recipe => { console.log(`This recipe has been updated with a duration of: ${recipe.duration}`)})
+  .catch(err => { console.error(`Error ocurred while updating: ${err}`)});
+
+Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(recipe => { console.log(`This recipe has been removed: ${recipe.title}`)})
+  .catch(err => { console.error(`Error ocurred while removing: ${err}`)});
+
+  process.on('SIGINT', () => {  
+	  mongoose.connection.close(() => { 
+	    console.log('Mongoose default connection disconnected through app termination'); 
+	    process.exit(0); 
+	  }); 
+	}); 
+

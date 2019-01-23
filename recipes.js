170b6d07559656f68ed81de2c09217c4758c1b35
mrosas22 <miller.rosas22@gmail.com>
2019-01-23 16:14:25 -0500

    Finished Iteration#2

--- recipes.js
+++ recipes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const data = require('./data.js');
+const data     = require('./data.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +8,32 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+//Iteration#1 create new Schema
+const recipeSchema = new Schema ({
+  title       : {type : String, required : true, unique : true},
+  level       : {type : String, enum : ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients : [],
+  cuisine     : {type : String, required : true},
+  dishType    : {type : String, enum : ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+  image       : {type : String, default : 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration    : {type : Number, min: 0},
+  creator     : {type : String},
+  created     : {type : Date, default : Date.now}
+})
+
+//Iteration#2 Use the Model.create method to pass the info to create a new recipe
+const Recipe = mongoose.model('Recipes', recipeSchema);
+module.exports = Recipe;
+
+Recipe.create({
+  title   : 'Homemade Beef Stroganoff',
+  level   : 'Amateur Chef',
+  ingredients : ['2 Pounds Beef Round Steak', 'Salt & Pepper', '4 Tablespoons Butter', '2 Cups Sliced Mushrooms', 'Onion'],
+  cuisine     : 'American',
+  dishType    : ['Dish'],
+  duration    : 30,
+  creator     : 'Chef Pepin'
+})
+  .then(recipe => { console.log('The new recipe is: ', recipe.title)})
+  .catch(err => { console.log('An error happened: ', err)})

e9aa76f7fe47f037de0fab11877b6db74d2974b0
williamfiallos <wfiallos10@gmail.com>
2019-01-24 22:01:02 -0500

    iteration 1 - recipe schema

--- recipes.js
+++ recipes.js
@@ -1,6 +1,44 @@
 const mongoose = require('mongoose');
+// we use Schema class given by mongoose
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
+
+// based on that Schema we create a blueprint for our students collection
+const recipeSchema = new Schema ({
+  title:{
+    type: String,
+    required: true
+  },
+  level: { 
+    type: String,
+     // values: Easy Peasy - Amateur Chef - UltraPro Chef (ENUM?) 
+  },
+  ingredients: { type: Array },
+  cuisine: { 
+    type: String, 
+    required: true 
+  },
+  dishType: {
+    type: String, 
+    // values: Breakfast - Dish - Snack - Drink - Dessert - Other
+  },
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg.'},
+  duration: { 
+    type: Number,
+    // minValue: 0 
+  },
+  creator: { type: String },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+// we export the model to make it accessible in other files
+module.exports = Recipe;
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {

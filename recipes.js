44d09c68c902c26fd23484a5e47c74117671d2cb
williamfiallos <wfiallos10@gmail.com>
2019-01-26 11:51:59 -0500

    done

--- recipes.js
+++ recipes.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err);
   });
 
-// based on that Schema we create a blueprint for our students collection
+// based on that Schema we create a blueprint for our recipe collection
 const recipeSchema = new Schema ({
   title:{
     type: String,
@@ -46,55 +46,55 @@ const recipeSchema = new Schema ({
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
-// we export the model to make it accessible in other files
+// we export the model to make it accessible in other .js files
 module.exports = Recipe;
 
 // ITERATION 2 - CREATE A RECIPE
 
-Recipe.create({
-  title: "Gallo Pinto",
-  level: "Easy Peasy",
-  ingredients: ['1 (16-ounce) bag dried small red or black beans', 'Salt', '7 garlic cloves, peeled', '1/4 cup vegetable oil, divided', '1 medium yellow onion, finely chopped (about 1 cup), divided', '1 1/2 cups long-grain white rice', '3 cups water or low-sodium chicken broth', '1/2 green bell pepper, cored and seeded'],
-  cuisine: 'Nicaraguan',
-  dishType: 'Breakfast',
-  image: 'https://www.seriouseats.com/recipes/images/2012/05/20120521-127677-LatAmCuisine-Gallopinto-PRIMARY.jpg',
-  duration: 45,
-  creator: 'William Fiallos'
-  // created: Date.now
-})
-.then( (newRecipe) => { 
-  console.log('Recipe title: ', recipe.title)
-})
-.catch( (err) => {
-  console.log('Error ocurred when creating recipe: ', err)
-})
+// Recipe.create({
+//   title: "Gallo Pinto",
+//   level: "Easy Peasy",
+//   ingredients: ['1 (16-ounce) bag dried small red or black beans', 'Salt', '7 garlic cloves, peeled', '1/4 cup vegetable oil, divided', '1 medium yellow onion, finely chopped (about 1 cup), divided', '1 1/2 cups long-grain white rice', '3 cups water or low-sodium chicken broth', '1/2 green bell pepper, cored and seeded'],
+//   cuisine: 'Nicaraguan',
+//   dishType: 'Breakfast',
+//   image: 'https://www.seriouseats.com/recipes/images/2012/05/20120521-127677-LatAmCuisine-Gallopinto-PRIMARY.jpg',
+//   duration: 45,
+//   creator: 'William Fiallos'
+//   // created: Date.now
+// })
+// .then( (newRecipe) => { 
+//   console.log('Recipe title: ', newRecipe.title)
+// })
+// .catch( (err) => {
+//   console.log('Error ocurred when creating recipe: ', err)
+// })
 
 // ITERATION 3 - INSERT MANY RECIPES
 
-Recipe.insertMany( data ) 
-.then( (newRecipe) => {
-  console.log('Recipe title is: ', data.title)
-})
-.catch ( (err) => {
-  console.log('Error ocurred when inserting many recipe: ', err)
-})
+// Recipe.insertMany( data ) 
+// .then( (newRecipe) => {
+//   console.log('Recipes added are: ', newRecipe)
+// })
+// .catch ( (err) => {
+//   console.log('Error ocurred when inserting many recipes: ', err)
+// })
 
-// ITERATION 4 - UPDATAE RECIPE
+// ITERATION 4 - UPDATE RECIPE
 
-Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
-.then( (recipe) => {
-  console.log('Duration has been updated successfully: ', data.duration)
-})
-.catch( (err) => {
-  console.log('Error ocurres updating recipe: ', err)
-})
+// Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+// .then( (updateRecipe) => {
+//   console.log('Duration has been updated successfully: ', updateRecipe.duration)
+// })
+// .catch( (err) => {
+//   console.log('Error ocurres updating recipe: ', err)
+// })
 
 //ITERATION 5 - DELETE A RECIPE
 
-Recipe.findByIdAndRemove('Carrot Cake')
-.then( (recipe) => {
-  console.log('Recipe has been removed from DB.')
-})
-.catch( (err) => {
-  console.log('Error ocurred removing recipe from DB.', err)
-})
+// Recipe.findByIdAndRemove('5c4c8fb6bd34cd1577b7a4fe')
+// .then( (removeRecipe) => {
+//   console.log('Recipe has been removed from DB.', removeRecipe.title)
+// })
+// .catch( (err) => {
+//   console.log('Error ocurred removing recipe from DB.', err)
+// })

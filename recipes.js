b894d84637c74c6e980db0424833cb4abfe53ee0
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-01-28 00:01:42 -0500

    Up to third iteration

--- recipes.js
+++ recipes.js
@@ -1,10 +1,30 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./models/recipe_entries.js');
 
-mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+//Iteration 2
+
+Recipe.create({
+  title: "Ceviche",
+  level: 'UltraPro Chef',
+  ingredients: ['Fish', 'Salt', 'Onion'],
+  cuisine: 'International',
+  dishType: 'Dish',
+  creator: 'Gaston'
+})
+  .then( newRecipe => {
+    console.log(`Recipe ${newRecipe.title} was added`);
+  })
+  .catch(err => {
+    console.log("An error happened ", err);
+})
+
+//Iteration 3
+
+Recipe.insertMany(data)
+.then( (newRecipe) => {
+    console.log('Recipes added are: ', newRecipe)
+   })
+  .catch ( (err) => {
+     console.log('Error ocurred when inserting many recipes: ', err)
+  })
+

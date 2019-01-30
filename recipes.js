dbf943383278de8e3d62b971b6933f9d51e5d9f5
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-01-30 14:50:10 +0100

    done

--- recipes.js
+++ recipes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./model.js')
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +8,27 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+// Drop everything
+Recipe.deleteMany()
+  //Iteration 3
+  .then(() => Recipe.insertMany(data)) 
+  .then(recipes => {
+    console.log("recipes added!")
+    //Iteration 4
+    return Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+  })
+  .then(recipe => {
+    console.log("recipe updated!")
+    //Iteration 5
+    return Recipe.deleteOne({title: "Carrot Cake"}) 
+  })
+  .then(info => {
+    console.log("recipe deleted!", info);
+  }) 
+  //Iteration 6
+  .then(() => mongoose.disconnect())
+  .catch(err => {console.log(err)})
+
+
+

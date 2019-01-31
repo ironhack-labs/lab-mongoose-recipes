5e781f18d234c5cf5f11ffdd83bab6c2c12cbac7
Raphael Huber <raphael.ehuber@gmail.com>
2019-01-30 22:48:59 -0200

    updated iteration 2

--- recipes.js
+++ recipes.js
@@ -30,3 +30,16 @@ const recipeSchema = new Schema({
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
 
+Recipe.create ({
+  title: 'Orange and Milk-Braised Pork Carnitas',
+  level: 'UltraPro Chef',
+  ingredients: ['3 1/2 pounds boneless pork shoulder, cut into large pieces', '1 tablespoon freshly ground black pepper', '1 tablespoon kosher salt, or more to taste', '2 tablespoons vegetable oil', '2 bay leaves', '2 teaspoons ground cumin', '1 teaspoon dried oregano', '1/4 teaspoon cayenne pepper', '1 orange, juiced and zested'],
+  cuisine: 'American',
+  dishType: ['Dish'],
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg',
+  duration: 160,
+  creator: 'Chef John'
+})
+  .then((data) => {
+    console.log(data.title); 
+  });

bc72fb722c9f1037598a7a6d5b542905dd51d1ad
David Casas <cem20903@gmail.com>
2019-01-31 09:43:41 +0100

    Done

--- recipes.js
+++ recipes.js
@@ -8,3 +8,45 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+
+  const SchemaRecipes = new Schema({
+    title : String,
+    level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+    ingredientes: Array,
+    cuisine: { type: String, required: true },
+    dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Dring', 'Dessert', 'Other'] },
+    image: {type: String, default:'https://images.media-allrecipes.com/im{ type:Date, default: today}ages/75131.j' },
+    duration: { type: Number, min:0},
+    creator: String,
+    create: { type:Date, default: Date.now}
+  });
+
+
+  const Recipe = mongoose.model('Recipe',SchemaRecipes)
+
+  Recipe.insertMany(data)
+  .then( ()=>{
+    Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+    .then(() => {
+      Recipe.deleteOne({ title: "Carrot Cake"})
+        .then(()=>{mongoose.disconnect()} )
+    })
+  })
+
+
+
+  
+  // .then(console.log("Bien"))
+  // .catch(console.log("Mal"));
+
+
+  // Recipe.deleteOne({ title: "Carrot Cake"})
+  // .then(console.log("Borrado"))
+  // .catch(console.log("No borrado"));
+
+
+
+  //mongoose.disconnect()
+  
+ 

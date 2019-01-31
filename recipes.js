9be6ef62b4bc904680b1dc40f0f0389bf66f2194
Raphael Huber <raphael.ehuber@gmail.com>
2019-01-30 22:32:20 -0200

    updated iteration 1 and iteration 2 worked

--- recipes.js
+++ recipes.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
@@ -9,14 +9,24 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err);
   });
 
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose default connection open');
-
-  User.create({ title: 'Raphael' })
-    .then((usr) => {
-      console.log('User created!', usr);
-    })
-    .catch((err) => {
-      console.log('Error when user created!', err);
-    });
+const recipeSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+  ingredients: { type: Array },
+  cuisine: { type: String, required: true },
+  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+module.exports = Recipe;
+

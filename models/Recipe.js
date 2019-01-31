ab71f7a50918aee206419d84de5174f547dbbc6d
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-01-31 07:53:54 -0200

    [SAO FT2 - DE - W4D3] Henrique Mendes

---
+++ models/Recipe.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const recipeSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+  ingredients: { type: Array },
+  cuisine: { type: String, required: true },
+  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] },
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now }
+});
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
--- recipes.js
+++ recipes.js
@@ -1,10 +1,77 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./models/Recipe.js');
+
+function recipeCreate() {
+  Recipe.create(
+    { title: 'Recipe 1',
+      level: 'Easy Peasy',
+      ingredients: ['ingr1', 'ingr2', 'ingr3'],
+      cuisine: 'recipeCountry',
+      dishType: 'Dish',
+      image: 'http://image.address',
+      duration: 10,
+      creator: 'Henrique' }
+  )
+    .then((rec) => {
+      console.log('User Criado', rec.title);
+    })
+    .catch((err) => {
+      console.error('Error connecting to mongo', err);
+    });
+}
+
+function insertMany() {
+  Recipe.insertMany(data)
+    .then((rec) => {
+      console.log('Dados inseridos com sucesso', rec.title);
+    })
+    .catch((err) => {
+      console.log('erro ao inserir dados', err);
+    });
+}
+
+function updateOneDuration(recTitle, recDuration) {
+  Recipe.updateOne({ title: recTitle }, { duration: recDuration })
+    .then((rec) => {
+      console.log('Update Sucessful!', rec);
+    })
+    .catch((err) => {
+      console.log('Error', err);
+    });
+}
+
+function removeOneRecipe(recTitle) {
+  Recipe.deleteOne({ title: recTitle })
+    .then((rec) => {
+      console.log('Data sucessfully removed!', rec);
+    })
+    .catch((err) => {
+      console.log('Error', err);
+    });
+}
+
+function closeDadatase() {
+  mongoose.connection.close()
+    .then(() => {
+      console.log('Database closed!');
+    })
+    .catch((err) => {
+      console.log('Error', err);
+    });
+}
 
 mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
+  .then((usr) => {
+    console.log('Connected to Mongo!', usr);
+  }).catch((err) => {
     console.error('Error connecting to mongo', err);
   });
+
+mongoose.connection.on('connected', () => {
+  recipeCreate();
+  insertMany();
+  updateOneDuration('Rigatoni alla Genovese', 100);
+  removeOneRecipe('Carrot Cake');
+  closeDadatase();
+});

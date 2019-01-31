68a4606d40c83f90ad29e09e58524cf0c64bd119
j0nl1 <jonlitech@gmail.com>
2019-01-31 09:43:07 +0100

    Refactored!

--- recipes.js
+++ recipes.js
@@ -6,45 +6,28 @@ var host = `mongodb://localhost/${db}`
 // Fixing my account
 mongoose.connect(host)
     .then(() => {
-    console.log('Connected to Mongo!');
-    App.singleImport()
-    App.manyImport()
-    App.updateRecipe()
-    App.removeRecipe("Carrot Cake")
-    mongoose.disconnect(host)
-  }).catch(err => {
+    console.log('Connected to Mongo!')
+    Recipes.create({ title: 'Patatas fritas con huevo', level:"Easy Peasy", cuisine: 'Old School', dishType: "Dish", duration: 10, creator: "Alfonso" })
+      .then(recipe => {console.log(`The recipe was saved and their title is: ${recipe.title}`)
+      Recipes.insertMany(data)
+        .then(recipe => {recipe.forEach((recipe) => {
+          console.log("The recipe was saved and their title is: " + recipe.title)})
+          Recipes.updateOne({title: "Rigatoni alla Genovese"}, { duration: 100})
+          .then(() => {console.log("You updated the recipe")
+          Recipes.deleteOne({title: "Carrot Cake"})
+          .then(() => {console.log("Remove sucessful")
+          mongoose.disconnect(host)
+          .then(() => console.log("DB Disconnected successful!"))
+          .catch(() => console.log("An error to disconnect db"))
+        })
+        .catch(err => console.log("An error happpened: ", err))
+        })
+        .catch(err => console.log("An error happened: ", err));
+        })
+        .catch(err => console.log("An error happened: ", err))
+    })
+      .catch(err => console.log('An error happened:', err));
+  })
+    .catch(err => {
     console.error('Error connecting to mongo', err);
   });
-
-const App = {
-  init: () => {
-
-  },
-  singleImport: () => {
-    Recipes.create({ title: 'Patatas fritas con huevo', level:"Easy Peasy", cuisine: 'Old School', dishType: "Dish", duration: 10, creator: "Alfonso" })
-      .then(recipe => console.log(`The recipe was saved and their title is: ${recipe.title}`))
-      .catch(err => console.log('An error happened:', err)); 
-  },
-  manyImport: () => {
-    Recipes.insertMany(data)
-    .then(recipe => getTittle(recipe))
-    .catch(err => console.log("An error happened: ", err))
-  },
-  updateRecipe: () => {
-    Recipes.updateOne({title: "Rigatoni alla Genovese"}, { duration: 100})
-  .then(() => console.log("You updated: "))
-  .catch(err => console.log("An error happened: ", err));
-  },
-  removeRecipe: (recipeTitle) => {
-    Recipes.deleteOne({title: recipeTitle})
-    .then(() => console.log("Remove sucessful"))
-    .catch(err => console.log("An error happpened: ", err))
-  },
-
-}
-
-function getTittle(arr) {
-  arr.forEach((arr) => {
-    console.log("The recipe was saved and their title is: " + arr.title)
-  })
-}

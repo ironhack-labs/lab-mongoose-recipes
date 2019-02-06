0393a7dcb1bbd74fa45160259d372a2982fc6330
iamalexrodriguez <alex.rdz.t@gmail.com>
2019-02-06 17:25:38 -0600

    mongo

---
+++ models/Recipe.js
@@ -0,0 +1,50 @@
+//Schemas
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let recipeSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    level:{
+        type: String,
+        enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+    },
+    ingredients: Array,
+    cuisine:{
+        type:String,
+        required: true
+    },
+    dishType: {
+        type: String,
+        enum:["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"],
+        image:{
+          type: String,
+          default: "https://images.media-allrecipes.com/images/75131.jpg"
+        },
+        duration: {
+            type: Number,
+            min: 0,
+        },
+        creator: String,
+        created:{
+            type: Date,
+            default: new Date(),
+        },
+
+
+    }
+});
+
+
+module.exports = mongoose.model('Recipe', recipeSchema);
+
+
+
+
+
+
+
+
--- recipes.js
+++ recipes.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
+let mongoose = require('mongoose');
+//const Schema   = mongoose.Schema;
+//let Recipe = require('./models/Recipe');
+//const data = require('./data.js');
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose.connect('mongodb://localhost:27017/firstDatabase')
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+

fed3c76433e2fba46425ab0e15447ab12114d3af
Victoria Hickam <vhickam@gmail.com>
2019-02-12 16:49:55 -0500

    done

--- recipes.js
+++ recipes.js
@@ -8,3 +8,53 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  const recipeSchema = new Schema({
+    title: {type: String, required: true, unique: true},
+    level: {type: String, enum:['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+    ingredients: Array,
+    cuisine: {type:String, required:true},
+    dishType: {type:String, enum:['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+    image: {type:String, default:' https://images.media-allrecipes.com/images/75131.jpg'},
+    duration: {type:Number, min:0 },
+    creator: String,
+    created: {type:Date, default: Date.now }
+  });
+
+  const Recipe = mongoose.model('Recipe', recipeSchema);
+  module.exports = Recipe;
+
+    Recipe.insertMany(data)
+    .then(recipes=> {
+      for (recipe of recipes){
+        console.log(recipe.title)
+      }
+      //UPDATE RIGATONI
+      Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100}, (err, res) => {
+        console.log("Recipe successfully updated");
+      });
+      //REMOVE CARROT CAKE
+      Recipe.deleteOne({title: "Carrot Cake"}, (err,res)=>{
+        console.log("carrot cake removed");
+      });
+
+      // mongoose.connection.close((err, res)=>{
+      //   console.log("connection closed");
+      // })
+  
+    });
+
+    setTimeout(() => {
+      mongoose.connection.close();
+      console.log("connection closed");
+    }, 5000);
+
+    
+
+    
+    
+  
+   
+
+
+

79e50b450be30996b86ed710c4a9a0532126ce0c
Robin <hahne.robin@gmail.com>
2019-02-22 16:48:54 +0100

    done

--- recipes.js
+++ recipes.js
@@ -10,6 +10,7 @@ mongoose.set('useFindAndModify', false);
 const Recipes = require('./models/recipes')
 const data = require('./data.js')
 
+
 Recipes.create({
   title: 'Asian Glazed Chicken Thighs',
   level: 'Amateur Chef',
@@ -19,37 +20,37 @@ Recipes.create({
   image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
   duration: 40,
   creator: 'Chef LePapu'
-}, (err, newRec) => {
-  if (err) console.log(err)
-  else {
-    console.log("Single Recipe created with Model.create()")
-    console.log(newRec._doc.title)
-  }
 })
-
-Recipes.insertMany(data, (err, allRec)=>{
-  if (err) console.log(err)
-  else {
+.then(newRec => {
+  console.log("Single Recipe created with Model.create()")
+  console.log(newRec._doc.title)
+  Recipes.insertMany(data)
+  .then(allRec => {
     console.log("Added Recipes with Model.insertMany:")
     allRec.forEach(recipe => {
       console.log(recipe.title);
-    });
-  }
-});
+    })
+    Recipes.findOneAndUpdate({title: 'Rigatoni alla Genovese' 
+    }, {duration: 100}, {
+      new: true
+    }, function (err) {
+      if (err) console.log(err)
+      else {
+        console.log("succesfully updated duration time");
+      }
+    })
+    Recipes.findOneAndRemove({title: "Carrot Cake"}, (err) => {
+      if (err) console.log(err)
+      else console.log("Successfully removed Carot Cake")
+    }) 
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+.catch(err => {
+  console.log(err)
+})
 
-Recipes.findOneAndUpdate({title: 'Rigatoni alla Genovese' 
-}, {duration: 100}, {
-  new: true
-}, function (err) {
-  if (err) console.log(err)
-  else {
-    console.log("succesfully updated duration time");
-  }
-});
-
-Recipes.findOneAndRemove({title: "Carrot Cake"}, (err) => {
-  if (err) console.log(err)
-  else console.log("Successfully removed Carot Cake")
-}) 
 
 mongoose.connection.close()

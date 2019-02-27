89a231fe266821e8b3458e1f6d31e97261e42c36
Sofiajor <sofia.jordan2000@gmail.com>
2019-02-27 18:04:07 +0100

    GUac guac guacamole

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
+const Recipe = require('./models/recipe.js')
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +9,32 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+Recipe.deleteMany()
+.then(() => console.log("ALl was deleted"))
+.catch((err)=> console.log(err))
+
+  Recipe.create({title: "Guacamole", cuisine : "Mexican"})
+  .then(recipe =>{console.log(`The recipe has been saved: ${recipe.title}`)})
+  .then(() => Recipe.insertMany(data))
+  .then(() => Recipe.updateOne({ title: "Rigatoni alla Genovese"}, {duration: 100}))
+  .then(()=> Recipe.deleteOne({title:"Carrot Cake" }))
+  .then(()=>mongoose.connection.close())
+  .then(()=> console.log("Mongoose is discconected")) 
+  .catch(err =>{console.log('A shit happened:',err)})
+
+  
+ 
+
+  // Recipe.insertMany(data)
+  // .then(recipe =>{
+  //   recipe.forEach((element) => {
+  //     console.log(element.title)
+  //   })
+  //   console.log('Recipes have been added:', recipe)
+  // })
+  // .catch(err =>{console.log('An error happened:', err)})
+
+  
+// Recipe.updateOne({ title: "Rigatoni alla Genovese"}, {duration: 100})
+// .then(recipe => {console.log('Succes')}) 
+// .catch((err)=> {console.log(err)})

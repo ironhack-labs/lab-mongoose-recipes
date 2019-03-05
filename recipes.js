9e9f56c41762b3ad1400677786ca61d627659fce
Marco Monter <monter.marco@gmail.com>
2019-03-04 18:53:24 -0600

    done

--- recipes.js
+++ recipes.js
@@ -8,3 +8,75 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+
+const recipeSchema = new Schema({
+    title: String,
+    level: String,
+    ingredients: [String],
+    cuisine: String,
+    dishType: String,
+    image: { type: String, default:"https://images.media-allrecipes.com/images/75131.jpg."},
+    duration: Number,
+    creator: String,
+    created: { type: Date, default: Date.now }
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+  const MyRecipe = new Recipe({
+    title: 'My sweet new recipe',
+    level: 'Ninja Level',
+    ingredients: ['Ing1','Ing2','Ing3','Ing4','Ing5'],
+    cuisine: 'Mexican',
+    dishType: 'Dish!',
+    image: "https://images.media-allrecipes.com/images/75131.jpg.", 
+    duration: 45,
+    creator: "Chef Marco monter",
+    created: Date.now()
+  })
+
+  MyRecipe.save({})  
+  .then((recipe) => {
+    console.log(recipe.name)
+  })
+  .catch((err) => {
+    console.log("An error happened:" + err);
+  });
+
+
+  Recipe.insertMany(data)
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log("An error happened:" + err);
+  });
+
+  Recipe.findByIdAndUpdate("5c7dc095be79a82c33d7cec8", { $set: {duration: 100} })
+  .then((data) => {
+    console.log("The document was succesly updated!")
+  })
+  .catch((err) => {
+    console.log("An error happened:" + err);
+  });
+
+  Recipe.findByIdAndRemove("5c7dc095be79a82c33d7cec7")
+  .then((data) => {
+    console.log("The document was succesly removed!")
+  })
+  .catch((err) => {
+    console.log("An error happened:" + err);
+  });
+
+  process.on('SIGINT', () => {  
+    mongoose.connection.close(() => { 
+      console.log('Mongoose default connection disconnected through app termination'); 
+      process.exit(0); 
+    }); 
+  }); 
+
+  
+module.exports = Recipe;
+
+

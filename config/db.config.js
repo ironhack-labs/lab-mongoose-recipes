10d623973f670b8d9ddcf8178667fa1707b5a0b8
eskalona17 <eskalona_17@hotmail.com>
2019-03-28 18:20:23 +0100

    third iteration

--- config/db.config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://localhost:27017/mongoose-models';
-
-mongoose.connect(MONGODB_URI)
-    .then(() => console.info(`Conectado en ${MONGODB_URI}`))
-    .catch(error => {console.error(`NO conectado en ${MONGODB_URI}`), error});
--- recipes.js
+++ recipes.js
@@ -2,9 +2,43 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
 
+const Recipe = require('./models/recipe_schema.js');
+
+const recipeData = require('./data.js');
+
+
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+
+
+  Recipe.collection.drop().then(() => {
+    Recipe.create(
+      { 
+      title: 'Arroz', 
+      cuisine: 'Spanish' 
+    })
+    .then(recipe =>  console.log('The recipe is saved and its value is: ', recipe) )
+    .then(() => Recipe.insertMany(recipeData))
+    .then(() => Recipe.updateOne(
+      { 
+      title: "Rigatoni alla Genovese"
+    }, 
+    { 
+      duration: 100 
+    }))
+    .then(() => Recipe.deleteOne({ 
+      title: "Carrot Cake"
+    }))
+    .then(() => mongoose.connection.close())
+    .then(() => console.log('Mongoose default connection disconnected through app termination'))
+}).catch(err => { 
+  console.log('An error happened:', 
+  err) 
+});
+
+
+    
+}).catch(err => {
+  console.error('Error connecting to mongo', err);
+});

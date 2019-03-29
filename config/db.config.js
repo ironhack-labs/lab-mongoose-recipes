9666e3f36783c25394f65b7b1dfa7ecb51966062
RedBalloon24 <worldtraveler757@gmail.com>
2019-03-29 13:00:58 +0100

    1st commit

---
+++ config/db.config.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const MONGODB_URI = 'mongodb://localhost:27017/lab-mongoose-recipes';
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
+    .then(() => console.info('Successfully connected to the database ${MONGODB_URI}'))
+    .catch(error => console.error('An error occurred trying to connect to the database ${MONGODB_URI}', error))
--- recipes.js
+++ recipes.js
@@ -1,10 +1,51 @@
+const Recipe = require('./models/recipe.model');
+const recipes = require('./data.js');
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
 
-mongoose.connect('mongodb://localhost/recipeApp')
+require('./config/db.config');
+
+const newRecipe =  {
+  title: 'Brownies',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup butter', '1 cup sugar', '2 eggs', '2 teaspoon vanilla extract', '3/4 cup flour', '1/3 cup cocoa powder', '1/4 teaspoon salt', '1/4 teaspoon baking powder', '1 cup chocolate chips', '2/3 cup chopped nuts'],
+  cuisine: 'American',
+  dishType: ['Dessert'],
+  image: 'https://unsplash.com/photos/WPRuXYLNY68',
+  duration: 60,
+  creator: 'Chef Awesome'
+}
+
+const updateDuration = 100;
+const removeRecipe = 'Carrot Cake';
+
+
+Recipe.create(recipes)
+  .then(recipes => console.info(recipes))
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+    console.info('============')
+    console.info('Creating new recipe', newRecipe)
+    return Recipe.insertMany({ newRecipe })
+    .save()
+    .then(recipes => console.info(recipes))
+  })
+  .then(() => {
+    console.info('Updating recipe', updateDuration)
+    return Recipe.findOneAndUpdate({ duration: updateDuration }, { $inc: {title: 'Rigatoni alla Genovese'}}, { new: true }) 
+      .then(recipes => console.info(recipes))
+  })
+  .then(() => {
+    console.info('Removing recipe', removeRecipe)
+    return Recipe.remove({ title: removeRecipe})
+      .then(recipes => console.info(recipes))
+  })
+  .catch(error => console.error(error))
+  
+  .then(() => {
+    console.info('Dropping database!')
+    return mongoose.connection.dropDatabase()
+  })
+  .then(() => {
+    console.info('Disconnecting...')
+    return mongoose.disconnect()
+  })
+  .then(() => console.info('Successfully disconnected, bye!'))

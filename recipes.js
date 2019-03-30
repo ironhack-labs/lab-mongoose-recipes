411c109e33449dafaa5d2fd94234e46e6c6d4ecc
eskalona17 <eskalona_17@hotmail.com>
2019-03-30 18:01:51 +0100

    done

--- recipes.js
+++ recipes.js
@@ -1,44 +1,39 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
 
 const Recipe = require('./models/recipe_schema.js');
 
 const recipeData = require('./data.js');
 
-
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
-
-
-  Recipe.collection.drop().then(() => {
-    Recipe.create(
-      { 
-      title: 'Arroz', 
-      cuisine: 'Spanish' 
+    return Recipe.collection.drop().then(() => {
+      return Recipe.create(
+        {
+          title: 'Spaguetti',
+          cuisine: 'Italian'
+        })
+        .then(recipe => console.log('New recipe is: ', recipe))
+        .then(() => Recipe.insertMany(recipeData))
+        .then(() => Recipe.updateOne(
+          {
+            title: "Rigatoni alla Genovese"
+          },
+          {
+            $set: {
+              duration: 100
+            }
+          }))
+        .then(() => Recipe.deleteOne({
+          title: "Carrot Cake"
+        }))
+        .then(() => mongoose.connection.close())
+        .then(() => console.log('Mongoose default connection disconnected through app termination'))
     })
-    .then(recipe =>  console.log('The recipe is saved and its value is: ', recipe) )
-    .then(() => Recipe.insertMany(recipeData))
-    .then(() => Recipe.updateOne(
-      { 
-      title: "Rigatoni alla Genovese"
-    }, 
-    { 
-      duration: 100 
-    }))
-    .then(() => Recipe.deleteOne({ 
-      title: "Carrot Cake"
-    }))
-    .then(() => mongoose.connection.close())
-    .then(() => console.log('Mongoose default connection disconnected through app termination'))
-}).catch(err => { 
-  console.log('An error happened:', 
-  err) 
-});
+      .catch(err => console.log('An error happened:', err));
 
-
-    
-}).catch(err => {
-  console.error('Error connecting to mongo', err);
-});
+  }).catch(err => {
+    console.error('Error connecting to mongo', err);
+  });

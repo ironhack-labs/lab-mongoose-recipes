b6f533b97920ad9fad87c90fe71b61cd669dc478
Dnox <david@nestrategia.com>
2019-04-04 21:44:01 +0200

    done

--- recipes.js
+++ recipes.js
@@ -1,24 +1,53 @@
-const mongoose  = require('mongoose');
-const Schema    = mongoose.Schema;
-const data      = require('./data.js');
-const Recipe    = require('./models/recipe.js');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const data = require('./data.js');
+const Recipe = require('./models/recipe.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
-  }).catch(err => {
+  })
+  .catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
 Recipe.create(data[0])
-  .then(data => {console.log(data[0] + 'Receta insertada')})
-  .then( () => {
-    return Recipe.find({}, {title:1, _id:0})
+  .then(data => {
+    console.log( data[0] + 'Receta insertada')
   })
-  .catch(err => {console.log(err)});
+  .then(() => {
+    return Recipe.find({}, {
+      title: 1,
+      _id: 0
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  });
 
-Recipe.insertMany([, ...data])
-  .then(data => {console.log('inserted')})
-  .catch(err => {console.log(err)});
+Recipe.insertMany(
+  [...data], { skip: 1 })
+  .then(data => {
+    console.log('inserted')
+  })
+  .then(() => {
+    Recipe.updateOne({
+      "title": "Rigatoni alla Genovese"
+    }, {
+      $set: {
+        "duration": 100
+      }
+    })
+    console.log("success, Rigattoni alla Genovese update at 100")
 
+    Recipe.remove({
+      "title": "Carrot Cake"
+    })
+    console.log('Carrot cake is over')
 
+    mongoose.connection.close()
+  })
+
+  .catch(err => {
+    console.log(err)
+  });

15990c5ffc48a47e3bed8762fc94f8d5ccbc7b1b
Ruben Polo <rpoloredondo@outlook.es>
2019-04-11 00:13:28 +0200

    done

--- recipes.js
+++ recipes.js
@@ -1,10 +1,51 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 const data = require('./data.js');
+// const Recipe = require("./data.js");
 
+const recipeSchema = new Schema ({
+  title: {type:String, required: true, unique: true},
+  level: {type:String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: {type:Array},
+  cuisine: {type:String, required: true},
+  dishType: {type:String, enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
+  image: {type:String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: {type:Number, min: 0},
+  creator: {type:String},
+  created: {type:Date, default: Date.now}
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+  
+  
+  Recipe.create({title:'Raviolis', cuisine:'asas'})
+.then(recipe => { console.log(recipe)})
+.catch(err => { console.log('An error happened:', err) });
+
+
+Recipe.insertMany(data)
+.then(recipe => { console.log("Hemos añadido todas")})
+.catch(err => { console.log('An error happened:', err) });
+
+Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration:  100})
+.then(recipe => { console.log("Hemos actualizado")})
+.catch(err => { console.log('An error happened:', err) });
+
+Recipe.findOneAndDelete({title: "Carrot Cake"})
+.then(recipe => { console.log("Hemos borrado Carrot Cake")})
+.catch(err => { console.log('An error happened:', err) });
+
+Recipe.deleteOne({title: "aa"})
+.then(recipe => { console.log("Hemos borrado aa")})
+.catch(err => { console.log('An error happened:', err) });
+module.exports = Recipe;
+
+// mongoose.connection.close(() => { 
+//   console.log('Mongoose default connection disconnected through app termination'); 
+// }); 

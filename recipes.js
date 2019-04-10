022e9668a374db71d4ce7cfe0a24f7df1b6d4553
Raúl Contreras <raulc98@gmail.com>
2019-04-10 16:45:04 +0200

    First iteration

--- recipes.js
+++ recipes.js
@@ -8,3 +8,21 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  const recipeModel = new Schema({
+    title: {type: String, required: true, unique: true},
+    level: {type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
+    ingredients: {type: Array},
+    cuisine: {type: String, required: true},
+    dishType: {type: String, enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
+    image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+    duration: {type: Number, min: 0},
+    creator: {type: String},
+    created: {type: Date, default: new Date()}
+  });
+  
+  const Recipe = mongoose.model("recipe", recipeModel);
+
+  Recipe.create(data[0])
+  .then(newRecipe => { console.log('The user is saved and its value is: ', newRecipe) })
+  .catch(err => { console.log('An error happened:', err) });

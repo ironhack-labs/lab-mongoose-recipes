f3a6494ac02790fca65438b651c0d03eb0c4e5d6
iMAC <iMAC@MacBook-Pro-de-MACBOOK.local>
2019-04-10 21:39:12 +0200

    Iteracion 1 fin

--- models/Recipe.js
+++ models/Recipe.js
@@ -11,15 +11,15 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  title : String,
-  level: String,
-  ingredients  : String,
-  cuisine : String,
-  dishType : String,
-  image : String,
-  duration : Number,
-  creator : String,
-  created : Date
+  title : {type: String, required:true, unique:true},
+  level: {type: String,enum: ['white', 'black', 'brown']},
+  ingredients  : {type: Array},
+  cuisine : {type:String, required: true},
+  dishType : {type:String, enum: ['Breakfast','Dish','Snack','Drink','Dessert', 'Other']},
+  image : {type:String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration : {type:Number, min:0},
+  creator : {type: String},
+  created : {type:Date, default:Date.now}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

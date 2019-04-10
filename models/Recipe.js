922ee369aa89e3bb9c8eb00b8d47168a051faac6
sanler <sanler110489@gmail.com>
2019-04-10 18:43:03 +0200

    Iteracion 1.1

--- models/Recipe.js
+++ models/Recipe.js
@@ -7,3 +7,31 @@ const recipeSchema = new Schema({
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const recipeSchema = new Schema({
+  title : String,
+  level: String,
+  ingredients  : String,
+  cuisine : String,
+  dishType : String,
+  image : String,
+  duration : Number,
+  creator : String,
+  created : Date
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+module.exports = Recipe;
+
+/*
+title. Type String. It should be required and unique.
+level. Type String. Only can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the ENUM 😉)
+ingredients. Type Array.
+cuisine. Type String. Should be required.
+dishType. Type String. Possible values: Breakfast - Dish - Snack - Drink - Dessert - Other.
+image. Type String. Default value: https://images.media-allrecipes.com/images/75131.jpg.
+duration. Type Number. Min value should be 0.
+creator. Type String
+created. Type Date. By default today.*/

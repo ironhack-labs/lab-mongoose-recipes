217c209bfc5f5bcceae120600fff21c204d9a860
erFedeRuiz <federicoruiz.ruiz@gmail.com>
2019-04-10 17:28:34 +0200

    Done

--- recipes.js
+++ recipes.js
@@ -5,7 +5,7 @@ const data = require('./data.js');
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!');
-
+  //Iteration 1
     const schema = {
       title: {type: String, unique: true, required: true},
       level: {type: String, enum:[ 'Easy Peasy' , 'Amateur Chef' , 'UltraPro Chef' ]},
@@ -18,6 +18,27 @@ mongoose.connect('mongodb://localhost/recipeApp')
       created: {type: Date, default: Date.now}
     }    
 
+    const RecipeBuilder = mongoose.model('recipe', schema);
+
+      // Iteration 2 and 3
+      RecipeBuilder.insertMany(data)
+      .then((success)=>{
+        // Iteration 4
+        console.log(data[0].title);
+        return RecipeBuilder.findOneAndUpdate({title: 'Rigatoni alla Genovese'},{duration:100})        
+      })
+      .then((success)=>{
+        // Iteration 5
+        return RecipeBuilder.findOneAndDelete({title: 'Carrot Cake'});
+      })
+      .then( () => {
+        // Iteration 6
+        mongoose.connection.close();
+      })
+      .catch((err)=>{
+        console.log(err);
+      }); 
+    
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });

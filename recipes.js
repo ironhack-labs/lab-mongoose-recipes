1a066a4d05f55cab8355c57faa20821ecc01cc21
Natasha R. Silva <natashairsilva@gmail.com>
2019-04-10 16:32:19 +0100

    Corrected

--- recipes.js
+++ recipes.js
@@ -10,25 +10,35 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-Recipe.create({title: 'Thai rice', cuisine: 'Asian'})
+// Delete all recipes to clean the collection and avoid "unique" problems
+Recipe.deleteMany()
+.then(() => {
+  Recipe.create({title: 'Thai rice', cuisine: 'Asian'})
   .then (recipe => { console.log('The ' + recipe.title + ' was created') })
   .catch (err => { console.log('An error happened:', err) });
 
 Recipe.insertMany(data)
-  .then (recipe => { console.log('The ' + recipe.title + ' was created') })
-  .catch (err => { console.log('An error happened:', err) });
-
-Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
-  .then (() => {
+  .then (recipes => { 
+    for (let i = 0; i < recipes.length; i++) {
+      console.log('The ' + recipes[i].title + ' was created');
+    }
+    
+    // Asynchronous
+    Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+    .then (() => {
     console.log('The duration of the Rigatoni alla Genovese was updated')
-  })
-  .catch (err => { console.log('An error happened:', err) });
+    })
+    .catch (err => { console.log('An error happened:', err) });
 
-Recipe.deleteOne({title: 'Carrot Cake'})
-  .then (() => {
+    Recipe.deleteOne({title: 'Carrot Cake'})
+    .then (() => {
     console.log('The Carrot Cake was removed')
-  })
+    })
+    .catch (err => { console.log('An error happened:', err) });
+    })
   .catch (err => { console.log('An error happened:', err) });
+})
+
 
 // Close the connection after 2 seconds
 setTimeout(() => {

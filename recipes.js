ccd9ad5e2b187907d5c2ef47183d8d1e61c69fdc
Natasha R. Silva <natashairsilva@gmail.com>
2019-04-10 15:34:14 +0100

    Done by Natasha and Jorge

--- recipes.js
+++ recipes.js
@@ -1,10 +1,36 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
+const Recipe = require('./models/Recipe')
 const data = require('./data.js');
 
-mongoose.connect('mongodb://localhost/recipeApp')
+mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+Recipe.create({title: 'Thai rice', cuisine: 'Asian'})
+  .then (recipe => { console.log('The ' + recipe.title + ' was created') })
+  .catch (err => { console.log('An error happened:', err) });
+
+Recipe.insertMany(data)
+  .then (recipe => { console.log('The ' + recipe.title + ' was created') })
+  .catch (err => { console.log('An error happened:', err) });
+
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then (() => {
+    console.log('The duration of the Rigatoni alla Genovese was updated')
+  })
+  .catch (err => { console.log('An error happened:', err) });
+
+Recipe.deleteOne({title: 'Carrot Cake'})
+  .then (() => {
+    console.log('The Carrot Cake was removed')
+  })
+  .catch (err => { console.log('An error happened:', err) });
+
+// Close the connection after 2 seconds
+setTimeout(() => {
+  mongoose.disconnect()
+}, 2000)

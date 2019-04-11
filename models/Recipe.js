5123aaefa89662e439971fc8ba3529101195b852
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-04-11 17:23:32 +0200

    iteration 1 done

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: {
+    String,
+    enum: ["Easy Peasy", "Amateur Chef", "Ultrapro Chef"]
+  },
+  ingredients: [],
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: {
+    type: String,
+    default: "https: //images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: o
+  },
+  Creator: String,
+  Created: {
+    type: Date,
+    default: Date.now
+  }
+
 });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

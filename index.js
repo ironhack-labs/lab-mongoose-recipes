b7e948f44560d76c9c4baae2a516c675a4960d21
Duc NGUYEN <nguyentduc92@gmail.com>
2019-04-11 18:06:34 +0200

    PARIS Finished Work Duc & Arnaud

--- index.js
+++ index.js
@@ -4,9 +4,38 @@ const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+  // .then(() => {
+  //   console.log('Connected to Mongo!');
+  // }).catch(err => {
+  //   console.error('Error connecting to mongo', err);
+  // });
 
+//Iteration 2
+// Recipe.create(
+//    { title : "Bobun",
+//     level: "Easy Peasy",
+//     ingredients : ["nem","salad"],
+//     cuisine: "Asian",
+//     dishType : "Dish",
+//     duration : 30,
+//     creator: "Chef Duc",
+//     created : new Date(),}
+//   )
+// })
+//Iteration 3
+// Recipe.insertMany(data,(err,recipe)=>{
+//   if (err) {
+//     console.log('An error happened:', err);
+// } else {
+//     console.log('The user is saved and its value is: ', recipe.forEach(el=>console.log(el.title)));
+// }
+// })
+//Iteration 4
+// Recipe.updateOne({_id :"5caf6123146a925c7d735b94"},{$set: {duration :100}})
+// .then(()=> console.log("Updated"))
+// .catch(()=> console.log("Error"))
+
+//Iteration 5
+// Recipe.deleteOne({_id :"5caf6123146a925c7d735b93"})
+// .then(()=> console.log("Deleted"))
+// .catch(()=> console.log("Error"))
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type :String},
+  level : {type :String},
+  ingredients : {type:Array},
+  cuisine: {type:String},
+  dishType: {type:String},
+  image: {type:String,default:'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration : {type :Number, min:0},
+  creator: {type:String},
+  created : {type: String,default : new Date() }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

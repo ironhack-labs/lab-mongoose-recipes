ac97aaa1a75e7cf278d12654846d66f96c648d37
miguelironhack <snoop_S_@hotmail.com>
2019-04-11 17:38:52 +0200

    fc

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
+
+//
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser());
+// Create module
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
-  }).catch(err => {
+  })
+  .catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+const carbonara = new Recipe({ title: 'Carbonara' });
+carbonara.save(err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('mama mia pizzeria');
+  }
+});
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,7 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    ENUM: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+    default: 'Easy Peasy'
+  },
+  ingredients: Array,
+  cuisine: String,
+  dishType: {
+    type: String,
+    enum: ['Breakfast', 'Dish', ' Snack', ' Drink', 'Dessert', 'Other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

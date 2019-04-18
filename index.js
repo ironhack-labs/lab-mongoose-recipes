13fce0ed72e12c5977d9baf43b88eae8758f0679
albertofragoso <hola@albertofragoso.com>
2019-04-17 23:33:41 -0500

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,36 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+const anyRecipe = new Recipe({
+  title: 'Chilaquiles',
+  level: 'UltraPro Chef',
+  ingredients: ['Salsa', 'Tortillas', 'Cebolla', 'Huevos', 'Queso', 'Crema', 'Aguacate'],
+  cuisine: 'Mexican',
+  dishType: 'Dish',
+  image: 'https://www.mylatinatable.com/wp-content/uploads/2018/04/chilaquiles-verdes-in-skilet-6.jpg',
+  duration: 160,
+  creator: 'Chef Alberto'
+})
+
+async function crudRecipes() {
+  try {
+    const recipe = await Recipe.create(anyRecipe)
+    console.log(recipe.title)
+
+    const recipes = await Recipe.insertMany(data)
+    recipes.forEach(recipe => console.log(recipe.title))
+
+    const rigatoni = await Recipe.findOneAndUpdate({'title': 'Rigatoni alla Genovese'},{$set:{'duration': 100}}, {new: true})
+    console.log(`Nice! You've updated ${rigatoni.title} successfully.`)
+
+    const carrotCake = await Recipe.deleteOne({title: 'Carrot Cake'})
+    console.log(`${carrotCake.title}'s been removed from the DB.`)
+  } catch(err) {
+    console.log(err)
+  }
+} 
+
+crudRecipes()
+
+mongoose.connection.close()
+  

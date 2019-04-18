f6c844220d8897688e336225f1a599839f268462
Heitor Godau <heitorgodau@gmail.com>
2019-04-18 16:26:59 -0300

    Done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
-
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
@@ -10,3 +9,16 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+data.forEach(e => {
+  Recipe.create(e)
+  .then(() => console.log('Criado, mano'))
+  .catch(() => console.log('Deu ruim, irmão'));
+});
+  
+Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+.then(() => console.log('Alterado, mano'))
+.catch(() => console.log('Deu ruim, irmão'));
+
+Recipe.deleteOne({title: "Carrot Cake"})
+.then(() => console.log('Já era, mano'))
+.catch(() => console.log('Deu ruim, irmão'));

9453a839538c16e1c3e72af181dec5c8f2605049
kellyjoany <kelly.joany@gmail.com>
2019-04-18 16:44:26 -0300

    done

--- index.js
+++ index.js
@@ -10,3 +10,46 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+function addNewRecipe(title, level, ingredients, cuisine, dishType, duration, creator ){
+  const newRec = new Recipe( { title: title, level: level, ingredients: ingredients, cuisine: cuisine, dishType: dishType, duration: duration, creator: creator} );
+  newRec.save(function (err){
+    if (err){
+      console.log(err);
+    } else{
+      console.log(`Recipe ${title} criado com sucesso!`);
+    }
+  });
+}
+
+//addNewRecipe('bolo','Easy Peasy', 'ovo', 'cuisine', 'Snack', 10, 'Kelly');
+
+/*Recipe.insertMany(data, function(error, docs) {
+    if(error){
+      console.log(error);
+    }else{
+      console.log(`Recipe ${docs} criado com sucesso!`);
+    }  
+});*/
+
+
+  Recipe.updateOne({title: 'Rigatoni alla Genovese'},{duration: 100})
+  .then((res) => {
+    console.log('Registro alterado com sucesso', res)
+  })
+  .catch((err) => {
+    console.log('Erro', err)
+  });
+
+
+  Recipe.deleteOne({ title: 'Carrot Cake' })
+    .then((res) => {
+      console.log('Registro deletado com sucesso', res)
+    })
+    .catch((err) => {
+      console.log('Erro', err) 
+    });
+
+
+mongoose.connection.close(function () {
+  console.log('Mongoose connection disconnected');
+  });

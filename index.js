94b572b5cb26ff7fe21b839effe50894e3b1fba3
Mario Neto <netto.mtx@gmail.com>
2019-04-18 18:19:31 -0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js');// Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
@@ -9,4 +9,29 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+// Recipe
+Recipe.insertMany(data, function(err,data) {
+  if (err) {
+    console.log('An error happened:', err);
+  } else {
+  console.log('The recipe is saved and its value is: ', data);
+}
+});
 
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  .then((response) => {
+    console.log('update', response);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+Recipe.deleteOne({ title: 'Carrot Cake' })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+mongoose.connection.close();

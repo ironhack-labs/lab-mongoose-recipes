3c4354afb008d1798ca9e81a962948a379f92137
deredhuzent <hakurascot@gmail.com>
2019-04-19 08:17:45 -0500

    done

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,7 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String, 
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Che', 'UltraPro Chef'] 
+  },
+  engredients: [String],
+  Cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String, 
+    enum:['Breakfast', 'Dish', 'Drink', 'Dessert', 'Other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    timestamps: true, 
+    versionKey: false
+  }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

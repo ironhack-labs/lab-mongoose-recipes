dc923563346a88814106bfad0a76eeb8b3357dc7
betoiron1988 <galactico10@live.com.mx>
2019-04-19 16:45:47 -0500

    avance

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const express=require('express');
 const mongoose = require('mongoose');
+const path=require
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
+const app=express()
 
+
+app.set('view engine','hbs')
+app.set('views',__dirname +'/views')
+app.use(express.static(path.join(__dirname,'public')))
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
@@ -9,4 +16,11 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+app.get('/-',(req,res)=>{
+  const chilaquiles= 
+})
+
+  app.listen(8000,()=>{
+    console.log('estoy vivo, solo por hoy')
+  })
 
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+ title:{
+   type:String,
+   unique:true
+
+ },
+ level:{
+   type: String,
+   enum:['Easy Peasy' , 'Amateur Chef ',' UltraPro Chef']
+ },
+ ingredients:[String],
+
+cuisine:{
+  type:String,
+  require:true
+},
+dishType:{
+  type:String,
+
+},
+
+image{
+  type:String,
+  require:
+}
+
+
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
+dishType. Type String. Possible values: Breakfast - Dish - Snack - Drink - Dessert - Other.
+image. Type String. Default value: https://images.media-allrecipes.com/images/75131.jpg.
+duration. Type Number. Min value should be 0.
+creator. Type String
+created. Type Date. By default today.

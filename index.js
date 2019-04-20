666d60697e3bf2668fd42bbc158954dcfdb16a28
alex fox <to.alexfox@gmail.com>
2019-04-20 21:48:55 +0200

    working with asynch

--- index.js
+++ index.js
@@ -11,54 +11,43 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-function run() {
-  // Create a new recipe
-  const firstRecipe = Recipe.create(
-    {  
-      title: 'Alex Recipe',
-      level: 'Amateur Chef',
-      ingredients: ['chicken'],
-      cuisine: 'Asian',
-      dishType: 'Dish',
-      image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
-      duration: 20,
-      creator: 'alex'
-    })
-    .then( recipe => { console.log("My new recipe is called: ", recipe.title)} )
-    .catch( err => { console.log("Error: ", err)} )
-
-
-  // Import data from file to create recipes
-  // **** MISSING: NOT SURE HOW TO PRINT ON EACH ITERATION *****
-  Recipe.insertMany( data)
-    .then( data => { console.log("Number of new recipes added: ", data.length)} )
-    .catch( err => { console.log("Error: ", err)} )
-
-  3
-  // Update recipe
-  Recipe.updateOne( { title: "Rigatoni alla Genovese" }, { duration: 100})
-    .then( recipe => { console.log("Recipe has been updated: ", recipe.title)} )
-    // ^^^ why does this line give undefined?
-    .catch( err => { console.log("Error: ", err)} )
-
-
-  // Delete recipe
-  Recipe.deleteOne( { title: "Carrot Cake" } )
-    .then( val => { console.log("Recipe has been removed.")} )
-    .catch( err => { console.log("Error: ", err)} )
-}
-
-var disconnect = new Promise( (resolve, reject) => {
-  run()
-  if (Recipe.count({})){
-    resolve(1)
-  }
-})
-
-
-disconnect.then(() => {
-    console.log('Disconnected from Mongo');
+// Create a new recipe
+const firstRecipe = Recipe.create(
+  {  
+    title: 'Alex Recipe',
+    level: 'Amateur Chef',
+    ingredients: ['chicken'],
+    cuisine: 'Asian',
+    dishType: 'Dish',
+    image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+    duration: 20,
+    creator: 'alex'
   })
-  .catch((err) => {
-    console.error('Error during exec', err);
+  .then( recipe => { console.log("My new recipe is called: ", recipe.title)} )
+  .catch( err => { console.log("Error: ", err)} )
+
+
+// Import data from file to create recipes
+// **** MISSING: NOT SURE HOW TO PRINT ON EACH ITERATION *****
+const insertMany = Recipe.insertMany( data)
+  .then( data => { console.log("Number of new recipes added: ", data.length)} )
+  .catch( err => { console.log("Error: ", err)} )
+
+
+// Update recipe
+const updateOne = Recipe.updateOne( { title: "Rigatoni alla Genovese" }, { duration: 100})
+  .then( val => { console.log("Recipe has been updated: ", val.title)} )
+  // ^^^ why does this line give undefined?
+  .catch( err => { console.log("Error: ", err)} )
+
+// Delete recipe
+const deleteOne = Recipe.deleteOne( { title: "Carrot Cake" } )
+  .then( val => { console.log("Recipe has been removed.")} )
+  .catch( err => { console.log("Error: ", err)} )
+
+Promise.all([firstRecipe, insertMany, updateOne, deleteOne])
+  .then( result => { 
+    console.log("all ops have finished");
+    mongoose.connection.close();
   })
+  .catch(err => console.error(err));

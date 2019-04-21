83272656559dca861d2adf5036d03fb677c57bde
saulscs <saulcoronasantos@ymail.com>
2019-04-21 14:42:53 -0500

    done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const express = require('express'); //
+const express = require('express'); // Import express 
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
-const path = require('path')
+const path = require('path') // Import path 
 const app = express()
 
 
@@ -21,32 +21,39 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-  app.get('/', (req,res) =>{
-    const chilaquiles = {
-      title: 'Chilaquilakillers',
-      level: 'Easy Peasy',
-      ingredients: ['Tortillas','Salsa','Crema','Pollo',],
-      cuisine: 'Mexican',
-      dishType: 'Breakfast',
-      image: '',
-      duration: 20,
-      creator: 'Juan Nieves',
-      
-    }
-
-    Recipe.create(chilaquiles)
-      .then ((doc) => {
-        
-      res.render('index', doc)
-      }) 
-      .catch ((err) => {
-        
-        res.render('index', {err})
-        }) 
+  const anyRecipe = new Recipe({
+    title: 'Chilaquiles',
+    level: 'UltraPro Chef',
+    ingredients: ['Salsa', 'Tortillas', 'Cebolla', 'Huevos', 'Queso', 'Crema'],
+    cuisine: 'Mexican',
+    dishType: 'Dish',
+    image: 'https://www.mylatinatable.com/wp-content/uploads/2018/04/chilaquiles-verdes-in-skilet-6.jpg',
+    duration: 60,
+    creator: 'Saul'
   })
-
-
+  
+  async function crudRecipes() {
+    try {
+      const recipe = await Recipe.create(anyRecipe)
+      console.log(recipe.title)
+  
+      const recipes = await Recipe.insertMany(data)
+      recipes.forEach(recipe => console.log(recipe.title))
+  
+      const rigatoni = await Recipe.findOneAndUpdate({'title': 'Rigatoni alla Genovese'},{$set:{'duration': 100}}, {new: true})
+      console.log(`Nice! You've updated ${rigatoni.title} successfully.`)
+  
+      const carrotCake = await Recipe.deleteOne({title: 'Carrot Cake'})
+      console.log(`${carrotCake.title}'s been removed from the DB.`)
+    } catch(err) {
+      console.log(err)
+    }
+  } 
+  
+  crudRecipes()
+  mongoose.connection.close()
+  
 app.listen(3000, () =>{
-  console.log('Estoy vivo')
+  console.log('Estoy vivo, ps si funciona')
 })
 

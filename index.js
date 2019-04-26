10523f08eda35d9d7e1745a91f3d84685b019e65
“Anghy <anghyf27@hotmail.com>
2019-04-26 11:54:34 -0400

    inital commit

--- index.js
+++ index.js
@@ -1,12 +1,71 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+Recipe.create({
+  title: "Causa de Pollo",
+  level: "Easy Peasy",
+  ingredients: ["Yellow Potatoes", "Yellow-Pepper", "Mayonnaise", "Shredded Chicken"],
+  cuisine: "Peruvian Cuisine",
+  dishType: "Dish",
+  image: "http://acomer.pe/wp-content/uploads/2018/02/causalimeniathumb-1.jpg",
+  duration: 1,
+  creator: "Anghy Pomar"
+  },
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("title:"+ Recipe.title);
+    }
+  }
+);
+
+
+
+
+
+Recipe.insertMany(data,
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("title:"+ Recipe.title);
+    }
+  }
+  );
+
+
+  Recipe.findByIdAndUpdate("5cc322bb7d65024a5aa06522", {duration: 100})
+        .then(
+          function(err) {
+            if (err) {
+              console.log("The Error is:", err);
+            } else {
+              console.log("success!");
+            }
+          }
+        );
+
+          Recipe.findByIdAndDelete("5cc322bb7d65024a5aa06521")
+            .then(
+              function(err) {
+                if (err) {
+                  console.log("The Error is:", err);
+                } else {
+                  console.log("success!");
+                }
+              }
+            );
+
+mongoose.disconnect();

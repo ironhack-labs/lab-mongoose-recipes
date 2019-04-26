e835881138dd639030caaf0a6734ebd521c92665
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-04-26 14:10:09 -0400

    complete

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  Recipe.create({
+    title: 'Chicken Soup',
+    level: 'Easy Peasy',
+    ingredients: ['Chicken', 'Water', 'Salt' ],
+    cuisine: 'yee',
+    dishType: 'Snack',
+    image: 'https://images.media-allrecipes.com/images/75131.jpg',
+    duration: 15,
+    creator: 'Nate',
+
+  }).then(recipe=> {console.log('The recipe is saved', recipe.title)})
+  .catch(err=>{console.log('error happened', err)});
+
+// console.log(data, '$%^&%^&*')
+
+//Below is the commented out code that returns all of our data from the data.js to the Mongocompass
+//Recipe.insertMany(data)
+
+Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration:100})
+  .then(whatverIwanted=>console.log(whatverIwanted))
+  .catch(err=>console.error(err))
+
+Recipe.deleteOne({title: "Carrot Cake"})
+  .then(whatverIwanted=>console.log(whatverIwanted))
+  .catch(err=>console.error(err))
+
+  setTimeout(() => {
+    mongoose.disconnect();
+  }, 1500)
--- models/Recipe.js
+++ models/Recipe.js
@@ -5,5 +5,15 @@ const recipeSchema = new Schema({
   // TODO: write the schema
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model('Recipe', {
+title: String,
+level: String,
+ingredients: Array,
+cuisine: String,
+dishType: String,
+image:  { type: String, default: 'images/default-avatar.png' },
+duration: Number,
+creator: String,
+created: { type: Date, default: Date.now },
+  });
 module.exports = Recipe;

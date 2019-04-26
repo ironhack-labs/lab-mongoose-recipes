43a6dd642762083b5997e0c52285020ed785e20a
jvelez523 <justin.velez0523@gmail.com>
2019-04-26 13:17:43 -0400

    finished

--- index.js
+++ index.js
@@ -10,3 +10,44 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  //If node connection closes, close mongoose
+
+
+//Add individual Recipe
+Recipe.create({title: 'Angel Hair Spaghetti',cuisine: 'Italian', })
+  .then(recipe=>{
+    console.log('The following recipe has been added', recipe)
+  }) .catch(err => {
+    console.log('The recipe was not added.', err)
+  });
+
+//Add all recipes from data.js
+Recipe.insertMany(data)
+  .then(data => {
+    console.log('The following recipes have been added to the DB',data)
+  }).catch(err => {
+    console.log('The recipes were not added to the DB', err)
+  })
+
+
+  //Find Rigatoni alla Genovese and update
+  Recipe.updateOne({title: 'Rigatoni alla Genovese'},{duration: 100})
+  .then(success => {
+    console.log('The following recipe has been updated',success)
+  })
+  .catch(err => {
+    console.log('The recipe was not updated',err)
+  })
+
+  //Delete Carrot Cake
+  Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(suc => {
+    console.log('The following recipe has been deleted',suc)
+  })
+  .catch(err => {
+    console.log('The recipe was not deleted', err)
+  })
+
+  setTimeout(() => {
+    mongoose.disconnect();
+  }, 1500) 

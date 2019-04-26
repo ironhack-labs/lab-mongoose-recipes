39da50a444d504d7861e4096f0af11b52091274b
Daniela Olano <danolan@Danielas-MacBook-Pro-2.local>
2019-04-26 15:58:27 -0400

    done

--- index.js
+++ index.js
@@ -10,3 +10,49 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  Recipe.create({
+    title: 'Choco Chip Cookies',
+    level:'Easy Peasy',
+    ingredients: ['flour','milk','butter','brown sugar','chocolate chips'], 
+    cuisine: 'American', 
+    dishType: 'Dessert', 
+    duration: 45,
+    creator: 'Ruth Graves Wakefield'
+    }, 
+  function(err,recipe){
+  if (err){
+  console.log('error',err);
+  }
+  else{
+  console.log('The recipe is',recipe);
+  }
+  });
+
+Recipe.insertMany(data,
+  function(err, recipe){
+    if (err){
+    console.log('error',err);
+    }
+    else{
+    console.log('title',recipe.title);
+    }
+  }); 
+
+
+Recipe.updateOne({title:'Rigatoni alla Genovese'}, {duration: 100})
+.then(
+  function(err) {
+    if (err) {
+      console.log("The Error is:", err);
+    } else {
+      console.log("success!");
+    }
+  });
+
+  Recipe.deleteOne({ title: "Orange and Milk-Braised Pork Carnitas" })
+  .then(console.log("Sucess"))
+  .catch(console.log("Not Sucess"));
+
+  setTimeout(() => {
+    mongoose.disconnect();
+  }, 1500)

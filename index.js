6949d88edf2e1384f2af16fbb182121b56dc7ee4
gabaldonrico <gabaldon.1617@gmail.com>
2019-05-15 17:55:31 +0200

    It was very dificult

--- index.js
+++ index.js
@@ -6,7 +6,83 @@ const data = require('./data.js');  // Import of the data from './data.js'
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
 
+    addNewRecipe(`pizza`, `Easy Peasy`, [`tomato`,`cheese`, `masa`, `mushrooms`, `anchovis`], `Italian`, `Breakfast`, undefined, 30, `Marcello Criminally`, undefined)
+    
+        .then(pizza => {
+          console.log(pizza.title)
+
+            // Recipe.find({})
+            //   .then((recipes)=> {console.log(`The recipes are:`, recipes)
+            
+              Recipe.insertMany(data)
+              .then(recipes=> {
+                
+                recipes.forEach(recipe => console.log(recipe.title)) 
+              
+                Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+                .then(recipe=> {
+                  console.log(`Update succesful`, recipe) 
+                  Recipe.findOneAndDelete({title: 'Carrot Cake'})
+                  .then(recipe => {
+                    console.log(`Delete succesful`, recipe)
+                    mongoose.connection.close()
+                    .then(() => console.log('BYE!'))
+                    .catch(err => console.log("Algo malo", err))
+                }).catch(err => console.log("Algo malo", err))
+              
+              }).catch(err => console.log("Algo malo", err))
+            
+            }).catch(err => console.log("Algo malo", err))
+
+            })
+            .catch(err => console.log("Algo malo", err))
+    })
+    .catch(err => console.log("Algo malo", err))    
+
+
+    const addNewRecipe = (Rtitle, Rlevel, Ringredients, Rcuisine, RdishType, Rimage, Rduration, Rcreator, Rcreated) => {
+
+      const pizza = new Recipe({ title: Rtitle, level: Rlevel, ingredients: Ringredients, cuisine: Rcuisine, dishType: RdishType, image: Rimage, duration:Rduration, creator: Rcreator, created: Rcreated})
+      
+      return pizza.save()
+    }
+
+
+
+  /* const addNewRecipe = (Rtitle, Rlevel, Ringredients, Rcuisine, RdishType, Rimage, Rduration, Rcreator, Rcreated) => {
+
+    const pizza = new Recipe({ title: Rtitle, level: Rlevel, ingredients: Ringredients, cuisine: Rcuisine, dishType: RdishType, image: Rimage, duration:Rduration, creator: Rcreator, created: Rcreated})
+    
+    pizza.save()
+      .then(pizza => console.log(`recipe saved!`, pizza))
+      .catch(err => console.log(`Error`, err))
+
+  }
+
+  const showRecipe = ()=> {
+
+    Recipe.find({})
+      .then(recipes=> console.log(`The recipes are:`, recipes))
+      .catch(err => console.log(`Error`, err))
+  }
+
+  Recipe.insertMany(data)
+    .then(recipes=> console.log(`The recipes are:`, recipes))
+    .catch(err => console.log(`Error`, err)) */
+
+  
+
+  // addNewRecipe(`pizza`, `Easy Peasy`, [`tomato`,`cheese`, `masa`, `mushrooms`, `anchovis`], `Italian`, `Breakfast`, undefined, 30, `Marcello Criminally`, undefined)
+
+  // Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+  //   .then(recipes=> console.log(`Update succesful`, recipes))
+  //   .catch(err => console.log(`Error`, err))
+  
+  // Recipe.deleteOne({name: 'Carrot Cake'})
+  //   .then(recipes=> console.log(`Update succesful`, recipes))
+  //   .catch(err => console.log(`Error`, err))
+
+  // mongoose.connection.close(()=> {
+  //   console.log('BYE!')
+  // })
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,7 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type: String},
+  level:{type: String, enum: ['Easy Peasy','Amateur Chef', 'UltraPro Chef']},
+  ingredients:{type: Array},
+  cuisine:{type: String, required:true},
+  dishType:{type:String, enum:['Breakfast','Dish','Snack','Drink','Dessert','Other']},
+  image:{type:String, default:'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration:{type:Number,min:0},
+  creator:{type:String},
+  created:{type:Date,default:new Date()},
+
+
+},
+{
+  timestamps: true
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

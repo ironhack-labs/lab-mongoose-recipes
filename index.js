df9ca0381df176703a8865ccd9b21518eb2df4fc
letiescanciano <leticia.escanciano@gmail.com>
2019-05-15 15:53:59 +0200

    Iteration 3. Insert many recipes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js'); // Import of the data from './data.js'
-
+console.log(data)
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', {
     useNewUrlParser: true
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://localhost/recipeApp', {
     console.error('Error connecting to mongo', err);
   });
 
-Recipe.create({
+/* Recipe.create({
     title: 'Orange and Milk-Braised Pork Carnitas',
     level: 'UltraPro Chef',
     ingredients: ['3 1/2 pounds boneless pork shoulder, cut into large pieces', '1 tablespoon freshly ground black pepper', '1 tablespoon kosher salt, or more to taste', '2 tablespoons vegetable oil', '2 bay leaves', '2 teaspoons ground cumin', '1 teaspoon dried oregano', '1/4 teaspoon cayenne pepper', '1 orange, juiced and zested'],
@@ -22,5 +22,9 @@ Recipe.create({
     duration: 160,
     creator: 'Chef John'
   })
+  .then(recipe => console.log(recipe.title))
+  .catch(err => console.log('An error happened', err)) */
+
+Recipe.insertMany(data)
   .then(recipe => console.log(recipe.title))
   .catch(err => console.log('An error happened', err))

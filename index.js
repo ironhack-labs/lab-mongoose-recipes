2b530a0734c3d7946fc07ad97f4c9c97b288adee
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-15 17:12:54 +0200

    Iteration 3

--- index.js
+++ index.js
@@ -10,13 +10,31 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-const addNewRecipe = (titleRecipe, levelRecipe, ingrRecipe, cuisineRecipe, dishRecipe, imageRecipe, durationRecipe, creatorRecipe, createdRecipe) => {
+/* const addNewRecipe = (titleRecipe, levelRecipe, ingrRecipe, cuisineRecipe, dishRecipe, imageRecipe, durationRecipe, creatorRecipe, createdRecipe) => {
 
   const recipe = new Recipe({ title: titleRecipe, level: levelRecipe, ingredients: ingrRecipe, cuisine: cuisineRecipe, dishType: dishRecipe, image: imageRecipe, duration: durationRecipe, creator: creatorRecipe, created: createdRecipe })
 
   recipe.save()
     .then(title => console.log('receta guardada', title))
     .catch(err => console.log('Error', err))
+} */
+
+
+
+
+Recipe.create({
+  title: "Tortilla de patatas Mejorada", level: "Easy Peasy", ingredients: ["huevos", "patatas", "aceite", "sal"], cuisine: "mediterranea", dishType: "Dish", image: undefined, duration: 60, creator: "Cris y Lu", created: undefined
+})
+  .then(recipe => { console.log(recipe.title) })
+  .catch(err => { console.log('An error happened:', err) });
+
+
+const manyRecipes = () => {
+  Recipe.insertMany(data)
+    .then((recipes) => recipes.forEach(batman => console.log(batman.title)))
+    .catch(err => { console.log("An error happened", err) })
+
 }
 
-addNewRecipe("Tortilla de patatas", "Easy Peasy", ["huevos", "patatas", "aceite", "sal"], "mediterranea", "Dish", undefined, 60, "Cris y Lu")
+
+//manyRecipes()

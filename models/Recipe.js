5da7636d833b442a40a23d60749f3ac4d7d7eea3
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-15 15:50:22 +0200

    Ups

--- models/Recipe.js
+++ models/Recipe.js
@@ -3,6 +3,18 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  level: { type: String, enum: ["Easy Pease", "Amateur Chef", "UltraPro Chef"] },
+  ingredients: Array,
+  cuisine: { type: String, required: true },
+  dishType: { type: String, enum: ["Breakfast", "Dish", "Sanck", "Drink", "Dessert", "Other"] },
+  image: {
+    type: String, default: " https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

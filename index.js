c05fb6dd7cb1e75a339be377cc1f75b7209303cd
letiescanciano <leticia.escanciano@gmail.com>
2019-05-15 16:45:07 +0200

    Iteration 6. Close the database

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ mongoose.connect('mongodb://localhost/recipeApp', {
     console.error('Error connecting to mongo', err);
   });
 
-/* Recipe.create({
-    title: 'Orange and Milk-Braised Pork Carnitas',
+Recipe.create({
+    title: 'Carnitas',
     level: 'UltraPro Chef',
     ingredients: ['3 1/2 pounds boneless pork shoulder, cut into large pieces', '1 tablespoon freshly ground black pepper', '1 tablespoon kosher salt, or more to taste', '2 tablespoons vegetable oil', '2 bay leaves', '2 teaspoons ground cumin', '1 teaspoon dried oregano', '1/4 teaspoon cayenne pepper', '1 orange, juiced and zested'],
     cuisine: 'American',
@@ -21,17 +21,33 @@ mongoose.connect('mongodb://localhost/recipeApp', {
     duration: 160,
     creator: 'Chef John'
   })
-  .then(recipe => console.log(recipe.title))
-  .catch(err => console.log('An error happened', err)) */
+  .then(recipe => {
+    console.log(recipe.title)
+    Recipe.insertMany(data)
+      .then(recipes => {
+        recipes.forEach(recipe => console.log(recipe.title))
+        Recipe.updateOne({
+            title: "Rigatoni alla Genovese"
+          }, {
+            duration: 100
+          })
+          .then(recipe => {
+            console.log("Receta actualizada!")
+            Recipe.deleteOne({
+                title: "Carrot Cake"
+              })
+              .then(message => {
+                if (message.deletedCount === 1) {
+                  console.log('Receta borrada correctamente')
+                } else console.log('No se ha borrado')
+                mongoose.connection.close(() => console.log("Mongoose disconnected"))
+              })
+              .catch(err => console.log('Error al intentar borrar', err))
+          })
+          .catch(err => console.log('An error happened', err))
+      })
+      .catch(err => console.log('An error happened', err))
 
-/* Recipe.insertMany(data)
-  .then(recipe => console.log(recipe.title))
-  .catch(err => console.log('An error happened', err)) */
 
-Recipe.updateOne({
-    title: "Rigatoni alla Genovese"
-  }, {
-    duration: 100
   })
-  .then(recipe => console.log("Receta actualizada!"))
   .catch(err => console.log('An error happened', err))

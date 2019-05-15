fa2e4cf38537ba9c5a39788386da9af7e4ed8549
letiescanciano <leticia.escanciano@gmail.com>
2019-05-15 15:45:27 +0200

    Iteration 2. Create a recipe

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({
+    title: 'Orange and Milk-Braised Pork Carnitas',
+    level: 'UltraPro Chef',
+    ingredients: ['3 1/2 pounds boneless pork shoulder, cut into large pieces', '1 tablespoon freshly ground black pepper', '1 tablespoon kosher salt, or more to taste', '2 tablespoons vegetable oil', '2 bay leaves', '2 teaspoons ground cumin', '1 teaspoon dried oregano', '1/4 teaspoon cayenne pepper', '1 orange, juiced and zested'],
+    cuisine: 'American',
+    dishType: 'Dish',
+    image: 'https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg',
+    duration: 160,
+    creator: 'Chef John'
+  })
+  .then(recipe => console.log(recipe.title))
+  .catch(err => console.log('An error happened', err))

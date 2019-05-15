239f0f3d574570c5f5d8e41a8439ddecac7a76d1
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-15 17:43:19 +0200

    Iteration 5

--- index.js
+++ index.js
@@ -6,32 +6,33 @@ const data = require('./data.js');  // Import of the data from './data.js'
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
+    addNewRecipe()
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
-/* const addNewRecipe = (titleRecipe, levelRecipe, ingrRecipe, cuisineRecipe, dishRecipe, imageRecipe, durationRecipe, creatorRecipe, createdRecipe) => {
-
-  const recipe = new Recipe({ title: titleRecipe, level: levelRecipe, ingredients: ingrRecipe, cuisine: cuisineRecipe, dishType: dishRecipe, image: imageRecipe, duration: durationRecipe, creator: creatorRecipe, created: createdRecipe })
-
-  recipe.save()
-    .then(title => console.log('receta guardada', title))
-    .catch(err => console.log('Error', err))
-} */
 
 
 
+const addNewRecipe = () => {
+  Recipe.create({
+    title: "Tortilla de patatas Mejorada", level: "Easy Peasy", ingredients: ["huevos", "patatas", "aceite", "sal"], cuisine: "mediterranea", dishType: "Dish", image: undefined, duration: 60, creator: "Cris y Lu", created: undefined
+  })
+    .then(recipe => {
+      console.log(recipe.title)
+      manyRecipes()
+    })
+    .catch(err => { console.log('An error happened:', err) });
 
-/* Recipe.create({
-  title: "Tortilla de patatas Mejorada", level: "Easy Peasy", ingredients: ["huevos", "patatas", "aceite", "sal"], cuisine: "mediterranea", dishType: "Dish", image: undefined, duration: 60, creator: "Cris y Lu", created: undefined
-})
-  .then(recipe => { console.log(recipe.title) })
-  .catch(err => { console.log('An error happened:', err) });
- */
+}
+
 
 const manyRecipes = () => {
   Recipe.insertMany(data)
-    .then((recipes) => recipes.forEach(batman => console.log(batman.title)))
+    .then((recipes) => {
+      recipes.forEach(batman => console.log(batman.title))
+      updateRecipe()
+    })
     .catch(err => { console.log("An error happened", err) })
 
 }
@@ -40,12 +41,36 @@ const manyRecipes = () => {
 
 const updateRecipe = () => {
   Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
-    .then(() => { console.log("se ha actualizado correctamente yujujuuu!") })
+    .then(() => {
+      console.log("se ha actualizado correctamente yujujuuu!")
+      deleteRecipe()
+    })
     .catch(err => { console.log('An error happened:', err) });
 
 }
 
+const deleteRecipe = () => {
+  Recipe.deleteOne({ title: "Carrot Cake" })
+    .then(() => {
+      console.log("se ha borrado correctamente yujujuuu!")
+      disconnectMongoose()
+    })
+    .catch(err => { console.log('An error happened:', err) });
 
-//manyRecipes()
-updateRecipe()
+}
+
+const disconnectMongoose = () => {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection closed');
+  });
+}
+
+
+
+
+
+
+
+
+
 

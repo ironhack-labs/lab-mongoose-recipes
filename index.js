26ebd929f8d9792bc432362e5106b6cca3f2316c
elinlarsen <elin_larsen1@hotmail.fr>
2019-06-19 17:24:25 +0200

    done

--- index.js
+++ index.js
@@ -10,3 +10,38 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({title: "Recipe Test",
+              level: "Easy Peasy",
+              ingredients:["tomato", "cheese"],
+              cuisine: "American",
+              dishType: "Dish",
+              duration: 10,
+              creator:"Elin",})
+.then( (res) =>{ console.log(" Title of the recipe created:  ", res.title)})
+.catch( (err) =>{console.log("An error occured during the creation of a recipe : ", err)})
+
+
+
+Recipe.insertMany(data)
+.then( (res) =>{ 
+  
+  res.forEach( r => console.log(r.title))
+
+
+  Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese"}, {duration: 100 }, {new: true})
+  .then( res => console.log(res ))
+  // .catch(err => {console.log("erreur during update : ", err)});
+
+  Recipe.deleteOne( {title: "Carrot Cake"})
+  .then( res => {console.log( ` ${res.title} is deleted`)})
+  // .catch(err => {console.log("erreur during deletion : ", err)});
+
+
+})
+.catch( (err) =>{console.log("An error occured during the insertion of many recipes : ", err)})
+
+
+
+// mongoose.connection.close()
+
+

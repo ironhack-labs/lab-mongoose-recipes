ea858fd15b1581ec466f075f0320b9f83e13f3e3
Stefano Vanotti <vanottistefano@gmail.com>
2019-06-20 09:23:25 +0200

    done

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+function createRecipe() {
+  Recipe.create({
+    title: "choco chips",
+    level: "easy",
+    ingredients: ["Chocolate", "Flour"],
+    cuisine: "American",
+    dishType: "Snack",
+    image: "",
+    duration: 20,
+    creator: "fat american mom",
+    created: "<1919-06-19>"
+  }).then(res => {
+    console.log("recipe created");
+  });
+}
+
+Recipe.find({})
+  .then(recipe => {
+    console.log(recipe[0].title);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.insertMany(data)
+  .then(recipe => {
+    recipe.forEach(elem => {
+      console.log(elem.title);
+    });
+
+    Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+      .then(recipe => {
+        console.log("yolo", recipe);
+        console.log("Hello");
+        // console.log("easy rigatoni");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    Recipe.deleteOne({ title: "Carrot Cake" })
+      .then(recipe => {
+        console.log("delete", recipe);
+        // console.log("easy rigatoni");
+        mongoose.disconnect(() => console.log("Closed"));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+console.log("Goodbye");
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: String,
+  ingredients: Array,
+  cuisine: String,
+  dishType: String,
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: { type: Date, default: "<2019-06-19>" }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;

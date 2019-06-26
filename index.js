423fa4cf588f216402d054f631c562a19320c715
Anja Böttcher <anjabottcher@Anja-Bottchers-MacBook-Air.local>
2019-06-26 18:05:33 +0100

    finished exercise

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+mongoose.connect(
+  "mongodb://localhost/recipeApp", {
+    useNewUrlParser: true
+  },
+  () => {
+    console.log("Connected to the database 'recipeApp'");
+  }
+);
 
+Recipe.deleteMany().then(() => {
+    Recipe.insertMany(data)
+      .then(recipe => {
+        for (let n = 0; n < recipe.length; n++) {
+          console.log("The title of the recipe is:", recipe[n].title)
+        }
+        Recipe.updateOne({
+            title: "Rigatoni alla Genovese"
+          }, {
+            duration: 100
+          })
+          .then(value => {
+            console.log("Value updated to:", duration)
+          })
+
+        Recipe.deleteOne({
+            name: "Carrot Cake"
+          })
+          .then(value => {
+            console.log("Recipe deleted");
+
+            mongoose.connection.close();
+          });
+      })
+  })
+  .catch(error => {
+    console.error("Error happened");
+  })

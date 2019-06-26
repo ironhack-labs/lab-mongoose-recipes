d5e7b023e0ceac59dea12366475a5d794ccfb2fe
carlos.trujillo <carlos@riverettecom>
2019-06-26 19:01:39 +0200

    exercise semi done

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: String,
+  ingredients: [],
+  cuisine: String,
+  dishType: {type :String, enum:["Breakfast","Dish","Snack","Drink","Dessert","Other"]},
+  image: {type:  String, default : "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: Number,
+  creator: String ,
+  date :{type : Date , default : Date.now} 
 });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

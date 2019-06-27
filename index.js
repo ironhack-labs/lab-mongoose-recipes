2d89ba003eb343782257b09b51a7629e5964722d
bryanmej <bryanmej@gmail.com>
2019-06-26 21:23:10 -0500

    Done in teamwork

--- index.js
+++ index.js
@@ -10,3 +10,57 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  const recipe1 = {
+    title: 'Taquitos en el espacio',
+    level: 'UltraPro Chef',
+    ingredients: ['tortilla', 'patitos', 'tirpas sin lavar', 'tacos de gato'],
+    cuisine: 'Mexicanil',
+    dishType: 'Snack',
+    duration: 20,
+    creator: 'Todos',
+
+  }
+
+  let a = Recipe.create(recipe1)
+  .then(recipe => {
+    console.log(recipe)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+  let b = Recipe.create(data)
+  .then(recipes =>{
+    console.log(`You just created ${recipes.length} recipes`)
+  })
+  .catch(err =>{
+    console.log(err)
+  })
+
+  let c = Recipe.findOneAndUpdate(
+    {title: 'Rigatoni alla Genovese'}, 
+    {duration: 100}, 
+    { new: true})
+    .then(recipe => {
+    console.log(recipe)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+    let d = Recipe.deleteOne({title: 'Carrot Cake'})
+    .then(cake => {
+      console.log(`bye ${cake}`)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+
+    Promise.all([a, b, c, d])
+    .then(()=>{
+      mongoose.connection.close()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+

b1589ff51b2e7d23146fc00d03e7e84dbf0f1cdd
Adri Gispen <aegispen@gmail.com>
2019-06-27 17:51:25 +0200

    done

--- index.js
+++ index.js
@@ -10,3 +10,35 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+ Recipe.create({
+    title: 'Mississippi Mud Pie',
+    level: 'Amateur Chef',
+    ingredients: ['1 cup all-purpose flour', '1 cup chopped pecans', '1/2 cup butter, softened', '1 package (5.9 ounces) instant chocolate pudding mix', '1 package (8 ounces) cream cheese, softened', '1 cup confectioners\' sugar', '1 container (16 ounces) frozen whipped topping, thawed, divided', 'Toasted chopped pecans and chocolate curls, optional'],
+    cuisine: 'Comfort',
+    dishType: 'Dessert',
+    image: 'https://static01.nyt.com/images/2017/04/11/dining/11COOKING-MISSISSIPPI-MUD-PIE2/11COOKING-MISSISSIPPI-MUD-PIE2-articleLarge.jpg',
+    duration: 25,
+    creator: 'Unknown'
+  }).then(recipe => console.log(recipe.title))
+  .catch(error => console.log(error));  
+
+  let insertRecipes = Recipe.insertMany(data)
+  .then(values => values.forEach(value => console.log(value.title)))
+  .catch(error => console.log(error));
+
+  let updateRecipe = Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+  .then(console.log("successfully updated!"))
+  .catch(error => console.log(error));
+
+ let deleteRecipe = Recipe.deleteOne({title: "Carrot Cake"})
+  .then(console.log("successfully deleted!"))
+  .catch(error => console.log(error));
+
+  Promise.all([insertRecipes, updateRecipe, deleteRecipe])
+  .then(values => {
+    console.log(values);
+    mongoose.connection.close();
+  })
+  .catch(error => console.log(error));
+
+  

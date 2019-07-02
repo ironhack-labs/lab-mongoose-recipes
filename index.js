d2a39340c79f1c0992db2863b6313cef5a72241c
horaleon1 <horaleon1@gmail.com>
2019-07-01 23:17:19 -0500

    connected to mongo and iteration 2 done

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({
+  title: 'Impecable Salmon',
+        level: 'Amateur Chef',
+        ingredients: ['1/2 cup light brown sugar', '1 large egg', '2 tablespoons milk', '1 1/4 teaspoons vanilla extract', '2 cups semisweet chocolate chips'],
+        cuisine: 'French',
+        dishType: 'Dish',
+        image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+        duration: 60,
+        creator: 'Horace Lee'
+})
+.then(user => { console.log(`The Recipe ${Recipe.title} was created succesfully`)})
+.catch(err => { console.log('An error happened:', err) });

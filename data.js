ec68cfde9a3c3abbf96b26957b8330653a11aef6
Sergio Velikopoljski <sergiovelo17@gmail.com>
2019-07-03 16:35:40 -0400

    minor change

--- data.js
+++ data.js
@@ -20,34 +20,34 @@ module.exports =
         duration: 160,
         creator: 'Chef John'
       },
-      // {
-      //   title: 'Carrot Pie',
-      //   level: 'Amateur Chef',
-      //   ingredients: ['6 cups grated carrots', '1 cup brown sugar', '1 cup raisins', '4 eggs', '1 1/2 cups white sugar', '1 cup vegetable oil', '2 teaspoons vanilla extract', '1 cup crushed pineapple, drained', '3 cups all-purpose flour', '1 1/2 teaspoons baking soda', '1 teaspoon salt', '4 teaspoons ground cinnamon'],
-      //   cuisine: 'International',
-      //   dishType: 'Dessert',
-      //   image: 'https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg',
-      //   duration: 130,
-      //   creator: 'Chef Nadia'
-      // },
-      // {
-      //   title: 'Rigatoni alla Genovese',
-      //   level: 'Easy Peasy',
-      //   ingredients: ['2 pounds red onions, sliced salt to taste', '2 (16 ounce) boxes uncooked rigatoni', '1 tablespoon chopped fresh marjoram leaves', '1 pinch cayenne pepper', '2 tablespoons freshly grated Parmigiano-Reggiano cheese'],
-      //   cuisine: 'Italian',
-      //   dishType: 'Dish',
-      //   image: 'https://images.media-allrecipes.com/userphotos/720x405/3489951.jpg',
-      //   duration: 220,
-      //   creator: 'Chef Luigi'
-      // },
-      // {
-      //   title: 'Vanilla Walnut Pie',
-      //   level: 'Amateur Chef',
-      //   ingredients: ['1/2 cup light brown sugar', '1 large egg', '2 tablespoons milk', '1 1/4 teaspoons vanilla extract', '2 cups semisweet chocolate chips'],
-      //   cuisine: 'French',
-      //   dishType: 'Dish',
-      //   image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
-      //   duration: 30,
-      //   creator: 'Chef Jennifer'
-      // }
+      {
+        title: 'Carrot Pie',
+        level: 'Amateur Chef',
+        ingredients: ['6 cups grated carrots', '1 cup brown sugar', '1 cup raisins', '4 eggs', '1 1/2 cups white sugar', '1 cup vegetable oil', '2 teaspoons vanilla extract', '1 cup crushed pineapple, drained', '3 cups all-purpose flour', '1 1/2 teaspoons baking soda', '1 teaspoon salt', '4 teaspoons ground cinnamon'],
+        cuisine: 'International',
+        dishType: 'Dessert',
+        image: 'https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg',
+        duration: 130,
+        creator: 'Chef Nadia'
+      },
+      {
+        title: 'Rigatoni alla Genovese',
+        level: 'Easy Peasy',
+        ingredients: ['2 pounds red onions, sliced salt to taste', '2 (16 ounce) boxes uncooked rigatoni', '1 tablespoon chopped fresh marjoram leaves', '1 pinch cayenne pepper', '2 tablespoons freshly grated Parmigiano-Reggiano cheese'],
+        cuisine: 'Italian',
+        dishType: 'Dish',
+        image: 'https://images.media-allrecipes.com/userphotos/720x405/3489951.jpg',
+        duration: 220,
+        creator: 'Chef Luigi'
+      },
+      {
+        title: 'Vanilla Walnut Pie',
+        level: 'Amateur Chef',
+        ingredients: ['1/2 cup light brown sugar', '1 large egg', '2 tablespoons milk', '1 1/4 teaspoons vanilla extract', '2 cups semisweet chocolate chips'],
+        cuisine: 'French',
+        dishType: 'Dish',
+        image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+        duration: 30,
+        creator: 'Chef Jennifer'
+      }
     ];

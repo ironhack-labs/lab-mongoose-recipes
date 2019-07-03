c46eabf3ec12ec4bd71dd27993ddc7eba8861d41
Gulfiya Isyangulova <gulfiyaisyangulova@Gulfiyas-MacBook-Pro.local>
2019-07-03 16:40:43 -0400

    done

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
+// Recipe.create({title: 'Green Curry', cuisine: 'Thai', duration: 45})
+//  Recipe.insertMany(data)
+  // .then((recipe,eachRec) => {
+  //   recipe.forEach(element => {
+  //     console.log(element.title);
+  //   });
+  // Recipe.updateOne({name:"Rigatoni alla Genovese"},{duration: 100})
+  //     .then(()=>{
+  //       console.log("success");
+      
+  // })
+  Recipe.deleteOne({title:"Carrot Cake"})
+  .then(()=>{
+    console.log("successfully deleted");
+  })
+  .catch(err => {
     console.error('Error connecting to mongo', err);
   });
 

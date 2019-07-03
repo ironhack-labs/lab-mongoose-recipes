347a29077cbc34c57041210389a09c2ea8118338
William B <william.benn11@gmail.com>
2019-07-03 16:41:49 -0400

    MIA-FTWD William Bennett

--- data.js
+++ data.js
@@ -1,15 +1,6 @@
 module.exports =
     [
-      {
-        title: 'Asian Glazed Chicken Thighs',
-        level: 'Amateur Chef',
-        ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
-        cuisine: 'Asian',
-        dishType: 'Dish',
-        image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
-        duration: 40,
-        creator: 'Chef LePapu'
-      },
+     
       {
         title: 'Orange and Milk-Braised Pork Carnitas',
         level: 'UltraPro Chef',
--- index.js
+++ index.js
@@ -10,3 +10,66 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+//CallBack Message Functions
+
+function successCallback(msg){
+    console.log("Entry Accepted");
+}
+
+function errorCallback(err){
+    console.log("Error", err);
+}
+
+  //Insert One Entry
+
+  // Recipe.create( {
+  //   title: 'Asian Glazed Chicken Thighs',
+  //   level: 'Amateur Chef',
+  //   ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  //   cuisine: 'Asian',
+  //   dishType: 'Dish',
+  //   image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  //   duration: 40,
+  //   creator: 'Chef LePapu'
+  // })
+  //  .then(()=>{
+  //     console.log('Entry Created')
+  // })
+  // .catch((err)=>{
+  //     console.log('Entry Rejected', err)
+  // })
+
+
+//Insert Many
+
+// Recipe.insertMany(data)
+// .then(()=>{
+
+// console.log("Entries Added")
+
+// }).catch((err)=>{
+
+// console.log(err)
+
+// })
+
+
+//Update Recipe Entry
+
+// Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+//   .then(successCallback)
+//   .catch(errorCallback);
+
+
+//Delete Recipe Entry
+
+Recipe.deleteOne({title:"Carrot Cake"})
+.then(()=>{
+
+  successCallback();
+  mongoose.connection.close();
+
+})
+.catch(errorCallback);
+

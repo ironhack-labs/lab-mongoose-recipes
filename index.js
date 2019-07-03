6fc28a0f6e30b1e61e4ca2afdfa53398bcb0a69c
Nicolle Gonzalez <nicolle.ngonzalez@gmail.com>
2019-07-03 16:56:24 -0400

    Donzo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
+const RecipeObj = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
@@ -10,3 +10,55 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+  ///////////////////////////////////////////////
+  /*//Iteration 2 - Create a recipe 
+  RecipeObj.create(data[0])
+  .then(()=>{
+    console.log('dot create worked');
+    console.log(data[0].title);
+  })
+  .catch((err)=>{
+    console.log('dot created didnt work',err)
+  })
+  */
+
+  ///////////////////////////////////////////////
+   /*//Iteration 3 - Insert Many recipes
+   RecipeObj.insertMany(data)
+  .then((msg)=>{
+    msg.forEach((element)=>{
+      console.log(element.title);
+
+    })
+    console.log('dot create worked');
+    // console.log(data[index].title);
+  })
+  .catch((err)=>{
+    console.log('dot insert didnt work',err)
+  })
+  */
+
+  ///////////////////////////////////////////////
+  /*//Iteration 4 - Update recipe
+    RecipeObj.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+    .then(()=>{
+        console.log("Update Succesfull");
+    })
+    .catch((err)=>{
+      console.log('dot update didnt work',err)
+    })
+   */
+
+///////////////////////////////////////////////
+/*//Iteration 5 - Remove a recipe
+RecipeObj.deleteOne({title: 'Carrot Cake'})
+    .then((msg)=>{
+        console.log("Delete Succesfull");
+    })
+    .catch((err)=>{
+      console.log('dot delete didnt work',err)
+    })
+  */
+
+  

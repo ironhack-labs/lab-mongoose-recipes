68fed0c9fd546a4a26375ceba99b4e120133507c
yi-ting Chiang <tarynchiang@gmail.com>
2019-07-03 21:17:32 -0400

    done

--- index.js
+++ index.js
@@ -10,3 +10,56 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// create method
+Recipe.create({
+  title: 'omelete',
+  level: 'Easy Peasy',
+  ingredients: ['egg','butter','salt'],
+  cuisine: 'American',
+  dishType: 'Breakfast',
+  image: 'https://receitinhas.s3-sa-east-1.amazonaws.com/wp-content/uploads/2017/09/Depositphotos_19636187_l-2015-848x477.jpg',
+  duration: 15,
+  creator: 'Oliver',
+  created: 7/3
+})
+.then((recipe)=>{
+    console.log('dot create worked');
+    console.log(recipe.title);
+})
+.catch((err)=>{
+    console.log('dot create didnt work', err)
+})
+
+//insertMany Method
+Recipe.insertMany(data)
+.then((recipes)=>{
+    console.log('insertMany worked');
+    recipes.forEach(function(rec){
+      console.log(rec.title);
+    })
+    
+})
+.catch((err)=>{
+    console.log('insertMany failed', err)
+})
+
+//update info
+Recipe.findByIdAndUpdate('5d1d511bbf9a1820050509ce', {
+      duration: 100
+})
+.then(()=>{
+  console.log('succesful');
+})
+.catch((err)=>{
+    console.log('update failed', err)
+})
+
+
+//delete recipe
+Recipe.deleteOne({title: "Carrot Cake"})
+.then(()=>{
+  console.log('succesful');
+})
+.catch((err)=>{
+    console.log('dot create didnt work', err)
+})

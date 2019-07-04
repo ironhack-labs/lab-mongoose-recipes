1db634d068f8f7381e76d9c72dbfa9709cc02d97
Pat Jordao <patriciacj@gmail.com>
2019-07-04 14:19:21 -0300

    done

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+data.forEach(element => {
+  Recipe.create(element)
+    .then(recipe => { console.log('The recipe is saved and its value is: ', recipe) })
+    .catch(err => { console.log('An error happened:', err) });
+
+});
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then(recipe => { console.log('The recipe was updated and its value is: ', recipe) })
+  .catch(err => { console.log('An error happened:', err) });
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then(recipe => { console.log('The recipe was deleted and its value is: ', recipe) })
+  .catch(err => { console.log('An error happened:', err) });
+
+// Recipe.findOne({ title: "Carrot Cake" })
+// .then(recipe => { 
+//   console.log('The recipe is saved and its value is: ', recipe) 
+// })
+//   .catch(err => { console.log('An error happened:', err) });
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});

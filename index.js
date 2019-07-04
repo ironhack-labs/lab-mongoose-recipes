a3ff2edeccb6a90bf889f8fd5eff44371c346b37
Layza Floriano <layzafloriano@gmail.com>
2019-07-04 14:03:28 -0300

    done

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+const recipe = new Recipe({ title: "Teste", level: 'Easy Peasy', ingredients: [], cuisine: 'brasileira', dishType: 'Breakfast', duration: 10, creator: 'Layza' });
+
+
+Recipe.create(recipe);
+
+Recipe.insertMany(data);
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+
+
+Recipe.findByIdAndUpdate('5d1e2bcd78aa5f43e5e728d3', { duration: 100 })
+  .then((res) => {
+    console.log('sucesso', res);
+  })
+  .catch(err => console.log(err));
+
+
+Recipe.deleteOne({ title: 'Rigatoni alla Genovese' })
+  .then((res) => {
+    console.log('sucesso', res);
+  })
+  .catch(err => console.log(err));
+
+mongoose.connection.close();

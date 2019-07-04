bd64145348d5f99f3fcae38c68cfd82daae10c67
Matiny <matinylouis@gmail.com>
2019-07-04 14:13:00 -0400

    Most of tasks complete!

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+
+  // Recipe.create({
+//   title:"Dumplings",
+//   level: "Easy Peasy",
+//   ingredients: ["Dough", "Sauce", "Pork"],
+//   cuisine: "Asian",
+//   dishType: "Snack",
+// }).then((theRecipe)=>{
+//   console.log(theRecipe.title);
+// }).catch((err)=>{
+//   console.log("There was a problem");
+// })
+
+// Recipe.insertMany(recipesList);
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+.then((theRecipe)=>{
+  console.log("Successfully updated!");
+})
+.catch(()=> {
+  console.log("An error has occured!");
+});

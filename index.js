0dda2b3ea1cfd41918887e995eff6f7110f66b59
Stefanie Cespedes <stefanie.davilaa@gmail.com>
2019-07-04 14:30:07 -0300

    done

--- index.js
+++ index.js
@@ -3,10 +3,32 @@ const Recipe = require('./models/Recipe'); // Import of the model Recipe from '.
 const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipe1', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+// data.forEach(element => {
+//   Recipe.create(element)
+//   .then(res => console.log(res.title))
+//   .catch(err => console.log(err));
+// });
+
+Recipe.insertMany(data)
+.then(res => console.log(res))
+.catch(err => console.log(err));
+
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then(() => console.log('ok'))
+  .catch(err => console.log(err));
+
+Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(() => console.log('deleted'))
+  .catch(err => console.log(err))
+
+mongoose.connection.close(() => {
+  console.log('Mongoose default connection disconnected through app termination');
+  process.exit(0);
+});
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: {type: String, required: true},
+  level: {
+    type: String, 
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: { type: Number, min: 0},
+  creator: String,
+  created:  {
+    type: Date,
+    default: Date.now
+  }
+},
+{
+  timestamps: true
+})
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

10e889064fbc213b229d2e52c39025cd7c8d226b
Felipe Chun <felipe.ychun@gmail.com>
2019-07-05 10:21:15 -0300

    done

--- index.js
+++ index.js
@@ -10,3 +10,38 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({
+  title: 'Asian Sweet Noodles',
+  level: 'UltraPro Chef',
+  ingredients: ['1 cup rice vinegar', '3 tablespoons honey', '1/4 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  cuisine: 'Asian',
+  dishType: 'Dish',
+  duration: 60,
+  creator: 'Chef LeChun'
+})
+  .then(res => console.log(res.title))
+  .catch(err => console.log(err));
+
+
+const updateDuration = Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  .then(() => console.log('Update Successful!'))
+  .catch(err => console.log(err));
+
+const deleteCarrot = Recipe.deleteOne({ title: 'Carrot Cake' })
+  .then(() => console.log('Remove Successful!'))
+  .catch(err => console.log(err));
+
+
+Recipe.insertMany(data)
+  .then((dishes) => {
+    dishes.forEach(dish => console.log(dish.title));
+    Promise.all([updateDuration, deleteCarrot])
+      .then(() => {
+        mongoose.connection.close(() => console.log('Connection closed! See ya!'));
+      })
+      .catch(err => console.log(err));
+  })
+  .catch((dishes) => {
+    dishes.forEach(err => console.log(err));
+  });
+
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+  title: { type: String, required: true, unique: true},
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: { type: Array },
+  cuisine: { type: String, required: true},
+  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now },
   // TODO: write the schema
+}, {
+  timestamps: true,
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
+
+// title. Type String. It should be required and unique.
+// level. Type String. Only can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the ENUM 😉)
+// ingredients. Type Array.
+// cuisine. Type String. Should be required.
+// dishType. Type String. Possible values: Breakfast - Dish - Snack - Drink - Dessert - Other.
+// image. Type String. Default value: https://images.media-allrecipes.com/images/75131.jpg.
+// duration. Type Number. Min value should be 0.
+// creator. Type String
+// created. Type Date. By default today.

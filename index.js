227138aab11a170c0f3b471e2be26169f82e21bf
Mitch Malinin <mitchmalinin@gmail.com>
2019-07-04 23:26:41 -0400

    Done

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
-// Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+// User.create({ name: 'Alice', password:"ironhack2018", job: 'Architect' })
+//   .then(user => { console.log('The user is saved and its value is: ', user) })
+//   .catch(err => { console.log('An error happened:', err) });
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+//iteration 2
+// Recipe.create(data[1])
+//   .then(recipie => {
+//     console.log("A recipe was saved and its value is", recipie);
+//   })
+//   .catch(err => {
+//     console.log("An error happened:", err);
+//   });
+
+//Iteration 3
+// Recipe.insertMany(data)
+//   .then(recipie => {
+//     console.log("A recipe was saved and its value is", recipie);
+//   })
+//   .catch(err => {
+//     console.log("An error happened:", err);
+//   });
+
+//Iteration 4
+// Recipe.findByIdAndUpdate("5d1ebcb4895cc33071d3dcb3", { duration: 100 })
+//   .then(recipe => {
+//     console.log("It Worked.");
+//   })
+//   .catch(err => {
+//     console.log("Didn't Work");
+//   });
+
+Recipe.findByIdAndRemove("5d1ebcb4895cc33071d3dcb2")
+  .then(recipe => {
+    console.log("It Worked.");
+  })
+  .catch(err => {
+    console.log("Didn't Work");
+  });

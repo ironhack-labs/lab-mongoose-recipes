79512ccd4aaaee060f275e32f30fb6d2ea088d23
Luis Orihuela <luis.orihuela20@gmail.com>
2019-07-16 19:52:07 -0500

    done

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({
+  title: 'Asian Glazed Chicken Thighs',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  cuisine: 'Asian',
+  dishType: 'Dish',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 40,
+  creator: 'Chef LePapu'
+}).then(recipe =>{console.log("Recipe has been saved: " ,recipe)})
+.catch(err =>{ console.log("An error ocurred: ", err)});
+
+Recipe.insertMany(data,)
+.then(recipes =>{ console.log('All recipes have been added', recipes)})
+.catch(err=>{console.log('There was an error: ', err)})
+
+Recipe.updateOne({title: "Rigatoni alla Genovese"},{duration: "50"})
+  .then(recipe =>{
+    (console.log('Succesfully updated'));    
+  })
+  .catch(err =>{
+      console.log(err)
+  });
+
+Recipe.deleteOne({title: "Carrot Cake"})
+  .then(recipe =>{
+    console.log('Recipe deleted', recipe)
+    mongoose.connection.close();
+})
+  .catch(err=>{ console.log(err)});
+

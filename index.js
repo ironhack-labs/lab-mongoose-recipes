1911b3ff53ed41e309d315fa9a60aaef4ec1bcf3
Lison <lisoncouturier@gmail.com>
2019-07-19 11:52:45 +0200

    done

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ const Recipe = require('./models/Recipe'); // Import of the model Recipe from '.
 const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/kitchen', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
-  const newRecipe = new Recipe({title : "Ratatouille", level : "Easy", ingredients : ["tomatoes", "eggplants", "zucchini", "onions"], cuisine : "French", dishType : "Maindish", image : "#", duration : 10, creator : "Lison", created : "2019-07-19"})
-
-  Recipe.create(newRecipe)
-    .then(recipe1 => {
-      console.log(recipe1.title)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+module.exports = mongoose
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,19 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require('../index');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  title : String,
-  level : String,
+  title : String, required: true,
+  level : String, required : ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
   ingredients : Array,
-  cuisine : String,
+  cuisine : String, required : true,
   dishType : String,
   image : String,
   duration : Number,
-  creator : String,
-  created : Date
+  creator : String, required : [ "Breakfast", "Dish", "Snack",  "Drink", "Dessert", "Other"],
+  created : {type : Date, default : Date.now}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
 
+// CREATE A NEW RECIPE
+const newRecipe = new Recipe({title : "Ratatouille", level : "Easy Peasy", ingredients : ["tomatoes", "eggplants", "zucchini", "onions"], cuisine : "French", dishType : "Dish", image : "#", duration : 10, creator : "Lison", created : "2019-07-19"})
 
+// ADD NEW RECIPE TO DATA BASE
+Recipe.create(newRecipe)
+  .then(recipe1 => {
+    console.log(recipe1.title)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+  // UPDATE RECIPE 
+Recipe.update({title : 'Rigatoni alla Genovese'}, { $set: {duration : 100}})
+.then(recipe2 => {
+  console.log("Recipe updated")
+})
+  .catch((error) => {
+    console.log(error)
+  })
+
+  // REMOVE 1 RECIPE
+Recipe.deleteOne({title : 'Carrot Cake'})
+.then(recipe2 => {
+  console.log("Recipe removed")
+})
+  .catch((error) => {
+    console.log(error)
+  })
+
+  // REMOVE SEVERAL RECIPES 
+Recipe.deleteMany({title : 'Ratatouille'})
+.then(recipe2 => {
+  console.log("Recipes removed")
+})
+  .catch((error) => {
+  console.log(error)
+  })

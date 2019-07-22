e56e1a9e676aa6859da3ee8da116ac4d94e551ff
pujasmint <pujasingh.jmd.jh@gmail.com>
2019-07-22 16:47:26 +0200

    done

--- index.js
+++ index.js
@@ -3,10 +3,54 @@ const Recipe = require('./models/Recipe'); // Import of the model Recipe from '.
 const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/kitchen', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+ // Iteration 2 (Recipe create) //
+
+ Recipe.find({})
+    .then((recipes)=> {
+      console.log(recipes[0].title);
+      })
+    .catch((err)=> {
+      console.log(err)
+    })
+    
+    // Iteration 3 (insertmany- title of each recipe)
+Recipe.find({})
+    .then((recipes)=> {
+      for(var i = 0; i < recipes.length; i++){
+        console.log(recipes[i].title)}
+      })
+    .catch((err)=> {
+      console.log(err)
+    })
+
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {durattion: 100})
+    .then(()=> {console.log("successfully updated")
+      })
+    .catch((err)=> {
+      console.log(err)
+    })   
+  
+Recipe.deleteOne({title: 'Carrot Cake'})
+    .then(()=> {console.log("success")
+      })
+    .catch((err)=> {
+      console.log(err)
+    })      
+
+// mongoose.connection.close()
+//     .then(res => {console.log("success!")
+//     })
+//     .catch((err)=> {
+//     console.log(err)
+//     }) 
+    
+    
+    
 
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,7 +3,17 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title : String,
+  level: String,
+  ingredients: Array,
+  cuisine: String,
+  dishType: String,
+  image: String,
+  duration: Number,
+  creator: String,
+  created: Number,
+
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model('recipes', recipeSchema);
 module.exports = Recipe;

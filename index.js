9ee22ee28bdaaffdfaf98e86c1f6b0177662b812
Lu Suelves <luciasuelves@gmail.com>
2019-07-24 17:37:32 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
+Recipe.collection.drop()
+
+Recipe.create({ 
+  title:  'Roscón de Reyes', 
+  level: 'Amateur Chef',
+  ingredients: ['harina','leche','azucar','almendras','mantequilla','canela'],
+  cuisine:'española',
+  dishType: 'Other',
+  duration: 300,
+  creator: 'Baltasar'
+})
+.then(x=>Recipe.insertMany(data))
+.then(all => all.forEach(elm=>console.log(elm.title)))
+.then(x => Recipe.updateOne({title:"Rigatoni alla Genovese"}, {duration: 100}))
+.then (x => {console.log("yujuuuuu")})
+.then(x => Recipe.deleteOne({title:'Carrot Cake'}))
+.then (x => {console.log("success Elemento borrado")})
+.catch (err => { console.log(err)})
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: String,
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: Array,
+  cuisine: {type: String, required: true},
+  dishType: {type: String, enum: ['Breakfast','Dish', 'Snack','Dessert', 'Other']},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type: String, min: 0},
+  creator: String,
+  creted: {type: Date, default: Date.now}
+},{timestamps: true});
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

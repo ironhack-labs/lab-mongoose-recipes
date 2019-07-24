0ba7245149686629825332eb54bd5891827cd598
Rebeca Lopez <rebecalopezsaelices@MacBook-Air-de-REBECA.local>
2019-07-24 17:51:44 +0200

    done

--- index.js
+++ index.js
@@ -4,9 +4,57 @@ const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
 
+      Recipe.collection.drop()
+
+      .then(x => { 
+            Recipe.create({
+              title: 'New York Cheese Cake',
+              level: 'Amateur Chef',
+              ingredients: ['Cheese', 'Jam', 'Cookies', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+              cuisine: 'Americam',
+              dishType: 'Dessert',
+              image: 'https://www.onceuponachef.com/images/2017/12/NY-Cheesecake-760x574.jpg',
+              duration: 60,
+              creator: 'Chef LePapu'})
+
+          .then(x => {
+            Recipe.insertMany(data)
+            
+            .then(x =>{
+              Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+                .then(x =>console.log(x,'hemos cambiado la duracion'))
+
+                .then(x =>{
+                  Recipe.deleteOne({title: 'Carrot Cake'})
+                    .then(x =>console.log(x,'hemos borrado la tarta'))
+                })
+                .catch(err => console.log('error', err))
+    
+              })
+
+            .catch(err => console.log('error', err))
+          })
+
+          .catch(err => console.log('error', err))
+
+      })
+      .catch(err => console.log('error', err))
+
+   
+ 
+   
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+    

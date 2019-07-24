99c8211acc9ef7a70e42b47f5cac728a0ac37432
medinajr <jrmedinpa@gmail.com>
2019-07-24 18:17:13 +0200

    done

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+  title: String,
+  level: String,
+  ingredients: Array,
+  cuisine: String,
+  dishType: String,
+  image: String,
+  duration: Number,
+  creator: String,
+  created: Date
+
   // TODO: write the schema
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
 module.exports = Recipe;

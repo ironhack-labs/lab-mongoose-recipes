7a7994d9164b075db140936a663fc79461caa961
Randolph Amaya <randolphjr7@gmail.com>
2019-08-07 16:23:42 -0400

    done

--- index.js
+++ index.js
@@ -10,3 +10,81 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// When successfully connected
+mongoose.connection.on('connected', () => console.log('Mongoose default connection open'));
+
+
+  // create a recipe
+  Recipe
+    .create({
+      title: `Tuna Salad`,
+      cuisine: "Seafood",
+      image: "https://ccs1-jl7jxm3ukfwyutip.netdna-ssl.com/wp-content/uploads/2019/03/avocado-tuna-salad-in-white-bowl-long-A.jpg"
+    })
+    .then( recipeDoc => console.log(`Recipe .create() success: `, recipeDoc.title))
+    .catch( error => console.log(`Recipe .create() failure`, error)); 
+
+  
+  // insert array of recipes from data.js
+  Recipe.insertMany(data);
+
+
+// console the title of each recipe
+function showRecipes() {
+  console.log(`ALL THE RECIPES`);
+  Recipe
+    .find()
+    .then(recipesFromDB => {
+      recipesFromDB.forEach(oneRecipe => console.log(`  --> recipe: ${oneRecipe.title}`))
+    })
+    .catch(err => console.log(`Error occured during getting recipe from DB: ${err}`)) 
+  } 
+
+  
+// update recipe
+async function update(recipeName){
+  const res = await Recipe.updateOne({ title: recipeName}, { duration: 100 });
+
+  console.log(`Recipe Update Successful`)
+  res.n; // number of documents matched
+  console.log(res.nModified); // number of documents modified
+  console.log(res);
+}
+
+update(`Rigatoni alla Genovese`);
+
+// remove a recipe
+async function remove(recipeName){
+  const res = await Recipe.deleteOne({ title: recipeName})
+
+  console.log(`Recipe Removal Sucessfull`);
+  res.n; // number of documents matched
+  console.log(res.nModified); // number of documents modified
+  console.log(res);
+}
+
+
+remove(`Carrot Cake`);
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => console.log('Mongoose default connection disconnected'));
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+
+
+// we have to wait for our recipes to save before displaying them
+setTimeout(showRecipes, 1000);
+
+// close the connection
+function close(){
+  mongoose.connection.close();
+}
+setTimeout(close, 9000);
+
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,8 +1,39 @@
 const mongoose = require('mongoose');
+// getting access to Schema class from mongoose
 const Schema = mongoose.Schema;
 
+// define the STRUCTURE of documents in the collection
 const recipeSchema = new Schema({
   // TODO: write the schema
+    title: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    leveL : {
+      type: String,
+      enum: [`Easy Peasy`,`Amateur Chef`,`Ultra Pro Chef`]
+    },
+    ingredients: [{type: String}],
+    cuisine: {
+      type: String,
+      required: true
+    },
+    dishType: {
+      type: String,
+      enum: [`Breakfast`,`Dish`,`Snack`,`Drink`, `Dessert`, `Other`]
+    },
+    image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+    duration: {
+      type: Number,
+      min: 0
+    },
+    creator: {
+      type: String
+    }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

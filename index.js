927f1350afc304838c8b8943929d7b9c385decc0
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-08-06 22:26:54 -0400

    done

--- index.js
+++ index.js
@@ -10,3 +10,57 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+// ITERATION 2
+// Recipe.create({
+//   title: "Cevice",
+//   level: "UltraPro Chef",
+//   ingredients: ["Fish", "Ginger", "Lime", "Aji Limo"],
+//   cuisine: "Peruvian",
+//   dishType: "Dish",
+//   duration: "20",
+//   creator: "Fabricio",
+// })
+//   .then(cev => console.log(`this ${cev} is fire!!!`))
+//   .catch(err => console.log(err))
+
+
+// const Recipe = mongoose.model("Recipe", Recipe)
+
+
+// ITERATION 3:
+// Recipe.insertMany(data)
+
+// ITERATION 4:
+
+async function update(name) {
+  const result = await Recipe.updateOne({ title: name }, {
+    $set: {
+      duration: 100
+    }
+  })
+  console.log(`succesfully updated ${result}`)
+}
+
+
+// update("Rigatoni alla Genovese")
+
+// ITEREATION 5
+
+async function deleteItem(name) {
+  const result = await Recipe.deleteOne({ title: name })
+  console.log(`removed ${result}`)
+}
+
+// deleteItem("Carrot Cake")
+
+mongoose.connection.on('disconnected', function () {
+  console.log("Mongoose default connection is disconnected");
+});
+
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log("Mongoose default connection is disconnected due to application termination");
+    process.exit(0);
+  });
+});

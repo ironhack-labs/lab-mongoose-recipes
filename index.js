1f0923e9bf1ed4f17c301e57c2d9badf77320de5
Alexander Hoinville <ahoin001@gmail.com>
2019-08-07 20:41:41 -0400

    Iteration 2 complete

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+// TODO Iteration 2: Create info to create new recipie
+Recipe.create({
+
+  title: 'Flurry Fluff',
+  level: "Easy Peasy",
+  ingredients: [`Flurry`, `fluff`, `love`, `Pint of Salt`],
+  cuisine: `Lean`,
+  dishType: `Snack`,
+  image: '',
+  duration: 20,
+  creator: `Alex El Sweet`,
+  created: ``
+
+})
+  .then((newRecipe) => { console.log(`New reicpie: ${newRecipe.title} added!`) })
+  .catch((err) => { console.log(`Error adding new recipie: `, err) });
+
+// TODO Iteration 3: Create multiple recipies, by adding from other file
+Recipe.insertMany(data)
+  .then(console.log(`Added the list of new recipes!`))
+  .catch((err) => {console.log(`Sorry chef, there was an error adding the list of recipies`);})
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+  
   // TODO: write the schema
   title: { type: String, required: true },
   // must be one of the 3 vaues in enum
@@ -10,6 +11,9 @@ const recipeSchema = new Schema({
   cuisine: { type: String, required: true },
   dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', `Drink`, `Dessert`, `Other`] },
   image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
+  duration: {type: Number, min:0 },
+  creator: {type: String},
+  created: {type: Date, default: Date.now}
 
 });
 

93d30377725575afed953cdf69761221498f9f63
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-08-09 09:07:48 -0400

    Created a new recipe in the db

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+
+  Recipe.create({
+    title: "Baked Chicken Wings",
+    level: "Amateur Chef",
+    ingredients: ["Chicken Wings","Black Pepper", "Season Salt", "Garlic Powder", "Lipton Onion Soup" ],
+    cuisine: "Soul Food",
+    dishType: "Dish",
+    image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/22/1496171764-baked-chicken-wingsl3.jpg",
+    duration: 70,
+    creator:"Antquanique",
+  })
+  .then(newRecipe => {console.log("Here is your new Recipe"), newRecipe.title} )
+  .catch(err => {console.log("An error happened: The recipe was not added to the db"), err});
+
+  
+

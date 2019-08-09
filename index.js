ff86b98199fbbbe9e81f56c1c2cbb0997c287b24
Alexander Hoinville <ahoin001@gmail.com>
2019-08-08 21:33:52 -0400

    Iteration 4 complete

--- index.js
+++ index.js
@@ -25,23 +25,43 @@ Recipe.create({
   creator: `Alex El Sweet`,
   created: ``
 
-})
-  .then((newRecipe) => { console.log(`New reicpie: ${newRecipe.title} added!`) })
+})   //If recipie created successfully then ->
+  .then((newRecipe) => {
+    // tell user the recipe was created and added safely
+    console.log(`New reicpie: ${newRecipe.title} added!`)
+
+    // and then do ->
+    // TODO Iteration 3: Create multiple recipies, by adding from other file
+
+    Recipe.insertMany(data)
+
+      // TODO Why do objects print in different order of schema? 
+      .then((recipes) => {
+        console.log(`Added the list of new recipes!`, recipes)
+
+        // TODO Iteration 4: Update duration of The Rigatoni alla Genovese 
+        // for the recipe found with name Rigatoni alla, chamge duration to 100
+        Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+          .then((updatedDocument) => {
+            console.log(`Successfully updated`, updatedDocument);
+
+            Recipe.find({title: "Rigatoni alla Genovese" })
+            .then((foundDoc) => console.log(`-------------------------`, foundDoc))
+            
+          })
+
+          .catch((err) => { console.log(`Error updating document`, err); });
+
+      })
+
+      .catch((err) => { console.log(`Sorry chef, there was an error adding the list of recipies`, err); })
+
+  }
+
+  )
   .catch((err) => { console.log(`Error adding new recipie: `, err) });
 
-// TODO Iteration 3: Create multiple recipies, by adding from other file
 
-Recipe.insertMany(data)
-  .then((recipes) => { console.log(`Added the list of new recipes!`, recipes) })
-  .catch((err) => { console.log(`Sorry chef, there was an error adding the list of recipies`, err); })
-
-// TODO Iteration 4: Update duration of The Rigatoni alla Genovese 
-// for the recipe found with name Rigatoni alla, chamge duration to 100
-Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
-  .then((updatedDocument) => {
-    console.log(`Successfully updated`, updatedDocument)
-  })
-  .catch((err) => { console.log(`Error updating document`, err); });
 
 const deleteAllRecipies = () => {
   Recipe.deleteMany({ duration: { $gte: 18 } })

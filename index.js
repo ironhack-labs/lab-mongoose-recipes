af0d522a9349f437ef8d39cecc82242a8f8b6bad
AlinaZakirod <50298876+AlinaZakirod@users.noreply.github.com>
2019-08-08 22:15:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+Recipe
+  .create({ title: "Minestrone", level: "Easy Peasy", ingridients: ["tomatoes", "potatoes", "cabbage", "onion"], cuisine: 'Italian', dishType: "Other", duration: 30, creator: "Me"})
+    .then( newRecipe => console.log("New recipe was created: ", newRecipe))
+    .catch( err => console.log("Something went wrong ", err));
+
+Recipe
+  .insertMany(data)
+  .then(manyMeals => console.log('Meal ${manyMeals.title} is inserted') )
+  .catch( err => console.log("Something went wrong ", err));
+
+Recipe
+    .findOneAndUpdate({title: "Rigatoni alla Genovese"} , { $set: { duration: 100}})
+    .then(newRigatoni => console.log("Success!", newRigatoni))
+    .catch( err => console.log("Error while fixing rigatoni: ", err));
+  
+Recipe
+  .deleteOne({title: "Carrot Cake"}) 
+  .then(byeByeCake => console.log("Oh, no carbs for you! Forget about ", byeByeCake))
+  .catch( err => console.log("Keeping my sugar, because ", err)); 

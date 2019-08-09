4726e57ddedd615d933948b26c1197044dbee5ae
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-08-09 09:30:33 -0400

    updated an item in the recipe db

--- index.js
+++ index.js
@@ -12,22 +12,24 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
 
 
 
-  Recipe.create({
-    title: "Baked Chicken Wings",
-    level: "Amateur Chef",
-    ingredients: ["Chicken Wings","Black Pepper", "Season Salt", "Garlic Powder", "Lipton Onion Soup" ],
-    cuisine: "Soul Food",
-    dishType: "Dish",
-    image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/22/1496171764-baked-chicken-wingsl3.jpg",
-    duration: 70,
-    creator:"Antquanique",
-  })
-  .then(newRecipe => {console.log("Here is your new Recipe"), newRecipe.title} )
-  .catch(err => {console.log("An error happened: The recipe was not added to the db"), err});
+  // Recipe.create({
+  //   title: "Baked Chicken Wings",
+  //   level: "Amateur Chef",
+  //   ingredients: ["Chicken Wings","Black Pepper", "Season Salt", "Garlic Powder", "Lipton Onion Soup" ],
+  //   cuisine: "Soul Food",
+  //   dishType: "Dish",
+  //   image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/22/1496171764-baked-chicken-wingsl3.jpg",
+  //   duration: 70,
+  //   creator:"Antquanique",
+  // })
+  // .then(newRecipe => {console.log("Here is your new Recipe"), newRecipe.title} )
+  // .catch(err => {console.log("An error happened: The recipe was not added to the db"), err});
 
-  Recipe.insertMany(data)
-  .then(newRecipes => {console.log(newRecipes.title), newRecipes})
-  .catch(err => {console.log("An error happened: The recipes were not added to the db", err), err})
+  // Recipe.insertMany(data)
+  // .then(newRecipes => {console.log(newRecipes.title), newRecipes})
+  // .catch(err => {console.log("An error happened: The recipes were not added to the db", err), err})
 
   
-
+Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+.then(recipeUpdate => {console.log("the Recipe duration was updated to 100"), recipeUpdate})
+.catch(err => {console.log("An error happened: The recipes were not added to the db", err), err})

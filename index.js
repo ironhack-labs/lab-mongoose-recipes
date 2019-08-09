fcae05359a394dcfced4c41c1e3d9f53c2165c00
Alexander Hoinville <ahoin001@gmail.com>
2019-08-08 22:01:17 -0400

    Iteration 6 complete(Structure and close connection added

--- index.js
+++ index.js
@@ -5,75 +5,89 @@ const data = require('./data.js');  // Import of the data from './data.js'
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
+
     console.log('Connected to Mongo!');
+
     // Delete all recipes in db before we start doing anything
     deleteAllRecipies()
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
 
-// TODO Iteration 2: Create new recipe following schema
-Recipe.create(
-  {
+    // TODO Iteration 2: Create new recipe following schema
+    Recipe.create(
+      {
 
-    title: 'Flurry Fluff',
-    level: "Easy Peasy",
-    ingredients: [`Flurry`, `fluff`, `love`, `Pint of Salt`],
-    cuisine: `Lean`,
-    dishType: `Snack`,
-    image: '',
-    duration: 20,
-    creator: `Alex El Sweet`,
-    created: ``
+        title: 'Flurry Fluff',
+        level: "Easy Peasy",
+        ingredients: [`Flurry`, `fluff`, `love`, `Pint of Salt`],
+        cuisine: `Lean`,
+        dishType: `Snack`,
+        image: '',
+        duration: 20,
+        creator: `Alex El Sweet`,
+        created: ``
 
-  })   //If recipie created successfully then ->
-  .then((newRecipe) => {
-    // tell user the recipe was created and added safely
-    console.log(`New reicpie: ${newRecipe.title} added!`)
+      })   //If recipie created successfully then ->
+      .then((newRecipe) => {
+        // tell user the recipe was created and added safely
+        console.log(`New reicpie: ${newRecipe.title} added!`)
 
-    // and then do ->
-    // TODO Iteration 3: Create multiple recipies, by adding from other file
+        // and then do ->
+        // TODO Iteration 3: Create multiple recipies, by adding from other file
 
-    Recipe.insertMany(data)
+        Recipe.insertMany(data)
 
-      // TODO Why do objects print in different order of schema? 
-      .then((recipes) => {
-        console.log(`Added the list of new recipes!`, recipes)
+          // TODO Why do objects print in different order of schema? 
+          .then((recipes) => {
+            console.log(`Added the list of new recipes!`, recipes)
 
-        // TODO Iteration 4: Update duration of The Rigatoni alla Genovese 
-        // for the recipe found with name Rigatoni alla, chamge duration to 100
-        Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
-          .then((updatedDocument) => {
+            // TODO Iteration 4: Update duration of The Rigatoni alla Genovese 
+            // for the recipe found with name Rigatoni alla, chamge duration to 100
+            Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+              .then((updatedDocument) => {
 
-            console.log(`Successfully updated`, updatedDocument);
+                console.log(`Successfully updated`, updatedDocument);
 
-            Recipe.find({ title: "Rigatoni alla Genovese" })
-              .then((foundDoc) => {
+                Recipe.find({ title: "Rigatoni alla Genovese" })
+                  .then((foundDoc) => {
 
-                console.log(`-------------------------`, foundDoc);
+                    console.log(`-------------------------`, foundDoc);
 
-                // TODO Iteration 5: Update duration of The Rigatoni alla Genovese 
-                Recipe.deleteOne({ title: "Carrot Cake" })
-                  .then(() => {
-                    console.log(`Document successfully deleted!`);
+                    // TODO Iteration 5: Update duration of The Rigatoni alla Genovese 
+                    Recipe.deleteOne({ title: "Carrot Cake" })
+                      .then(() => {
+                        console.log(`Document successfully deleted!`);
+
+                        mongoose.connection.close()
+                          .then(() => {
+                            console.log(`Successfully closed connection`)
+                          })
+                          .catch((err) => {
+                            console.log(`Error when closing connection`, err)
+                          })
+                      })
+                      .catch((err) => {
+                        console.log(`Error Deleting document`);
+                      })
+
                   })
-                  .catch((err) => {
-                    console.log(`Error Deleting document`);
-                  })
+
+                  .catch((err) => { console.log(`Error getting document that was updated`, err); });
 
               })
 
-              .catch((err) => { console.log(`Error getting document that was updated`, err); });
+              .catch((err) => { console.log(`Error updating document`, err); });
 
           })
 
-          .catch((err) => { console.log(`Error updating document`, err); });
+          .catch((err) => { console.log(`Sorry chef, there was an error adding the list of recipies`, err); })
 
-      })
+      }).catch((err) => { console.log(`Error adding new recipie: `, err) });
 
-      .catch((err) => { console.log(`Sorry chef, there was an error adding the list of recipies`, err); })
+  })
 
-    }).catch((err) => { console.log(`Error adding new recipie: `, err) });
+  .catch(err => {
+    console.error('Error connecting to mongo', err);
+  });
+
 
 
 const deleteAllRecipies = () => {

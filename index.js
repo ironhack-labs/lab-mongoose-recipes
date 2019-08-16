2f6a804aa293227cf36f84412a2e23e52f8b1bc5
luanperosa <luan.perosa@hotmail.com>
2019-08-15 23:31:05 -0300

    Falta estudar melhor o conceito de promisses e disconectar corretamente do banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
+const date = new Date();
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
@@ -10,3 +11,38 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// const newRecipe = { 
+//   title: 'Luan',
+//   level: 'Easy Peasy',
+//   ingredients: 'eggs',
+//   cuisine: '1',
+//   dishType: 'Snack',
+//   duration: 5,
+//   creator: 'Vovo',
+// }
+
+// Recipe.create(newRecipe)
+//   .then((newRecipe) => {console.log(`Recipe created ${newRecipe}`)})
+//   .catch((err) => {
+//     console.log('ERROR =>', err);
+//   });
+
+  // Recipe.insertMany(data)
+  // .then((response) => {
+  //   console.log(response)
+  // })
+  // .catch();
+
+  // Recipe.updateMany({title: 'Rigatoni alla Genovese'}, {duration: '100'})
+  //   .then()
+  //   .catch();
+
+  // Recipe.deleteMany({title: 'Carrot Cake'})
+  //   .then()
+  //   .catch();
+
+  // mongoose.disconnect()
+  //   .then(() => {
+  //     console.log('Disconnect MongoDB')
+  //   })
+  //   .catch();

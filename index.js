a6613e3bbc86a85290a9a848396d17816f5e0247
Your Name <josedelacruz@Joses-MacBook-Air.local>
2019-08-20 15:34:29 -0400

    done

--- index.js
+++ index.js
@@ -10,3 +10,44 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  //interation: 2
+  Recipe.create({ 
+    title: 'Homemade Mangu',
+    level: 'Amateur Chef',
+    ingredients: ['2 Tablespoons of Vinegar', 'Salt & Pepper', '4 Tablespoons Butter', '2 Pantains', 'Onion'],
+    cuisine: 'Dominican',
+    dishType: 'Dish',
+    duration: 30,
+    creator: 'Chef Joes',
+    created: '11/02/1993',
+  })
+  .then(recipe => { console.log('The new recipe is: ', recipe)})
+  .catch(err => { console.log('An error happened: ', err)})
+
+//Interation: 3
+Recipe.insertMany(data)
+.then(newRecipe =>{
+  console.log(`Recipes has been added to DB: ${newRecipe}`);
+  newRecipe.forEach(({title}) =>console.log(`Title is ${title}`));})
+.catch(err => { console.log('An error happened: ', err)});
+
+//Interation: 4
+Recipe.updateOne({title: 'Carrot Cake'}, {cuisine: 'French'})
+.then(updateRecipe => {
+  console.log('Carrot Cake has been updated!') })
+.catch(err =>{
+  console.log( 'An error has occur', err);
+});
+
+//Interation: 5
+Recipe.deleteOne({title: 'Chocolate Chip Cookies'})
+      .then(recipe => { console.log( 'The recipe "Chocolate Chip Cookies" has been deleted successfully')})
+      .catch(err => { console.log( 'An error has occur', err)})
+
+//Interation: 6
+process.on('SIGINT', () => { 
+  mongoose.connection.close(() => { 
+    console.log('Mongoose is Disconnected'); 
+    process.exit(0); 
+  }); 
+}); 

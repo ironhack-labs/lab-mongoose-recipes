3666593b9b08be731c5b2c877426b83da8840473
Rafael Bueno <rafaelbuenolink@gmail.com>
2019-08-21 10:55:50 -0300

    done

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+const createRecipe = Recipe.create(data[0])
+  .then(recipe => console.log(`Receita: ${recipe.title}`))
+  .catch();
+
+const insertRecipe = Recipe.insertMany(data)
+  .then(recipe => {recipe.forEach(eachRecipe => console.log(`Receita: ${eachRecipe.title}`))})
+  .catch()
+
+const updateRecipe = Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then(recipe => { console.log(`Recipe updated`)})
+  .catch()
+
+const deleteRecipe = Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(recipe => {console.log('Recipe removed')})
+  .catch();
+
+Promise.all([createRecipe, insertRecipe, updateRecipe, deleteRecipe])
+  .then(res =>{
+      mongoose.connection.close(() => {
+        console.log('Mongoose default connection disconnected through app termination');
+      });
+  })
+  .catch();
+  

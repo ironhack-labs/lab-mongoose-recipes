4d33e614f6692d0f2372e9571e1912c85aa5ced2
Xavier Crespo <xavier.crespo1@gmail.com>
2019-08-28 15:40:12 +0200

    Done!

--- index.js
+++ index.js
@@ -10,3 +10,41 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({title : "Fries",
+  level : "Easy",
+  ingredients : ["potatoes",'salt','oil','bacon','cheddar'],
+  cuisine: "French",
+  dishType : "Lunch",
+  image : "fries.png",
+  duration : 30,
+  creator : "John Doe",
+  created : Date()});
+
+
+//Recipe.insertMany(data, function(error, docs) {});
+
+function updateRecipe(id){
+  return Recipe.findByIdAndUpdate(id,{duration : 100})
+  .then(res =>{
+    console.log('duration updated');
+  })
+  .catch(err =>{
+    console.error('err');
+  });
+}
+
+function removeRecipe(tit){
+  return Recipe.deleteMany({title:tit})
+  .then(res =>{
+    console.log('recipe removed');
+  })
+  .catch(err =>{
+    console.error('err');
+  });
+}
+
+// updateRecipe("5d66801ce4c64f57588f5348");
+
+removeRecipe("Carrot Cake");
+
+mongoose.connection.close()

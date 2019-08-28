99ff2d3ba9921219134e4537461f330a85beeffb
sophieannebourgine <sophieannebourgine@gmail.com>
2019-08-28 17:14:28 +0200

    fini

--- index.js
+++ index.js
@@ -10,3 +10,37 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+  Recipe.create({ title: 'Gâteau', level:"Easy Peasy", cuisine: "French", }, function (err, recipe) {
+    if (err) {
+        console.log('An error happened:', err);
+    } else {
+        console.log('The recipe is saved and its value is: ', recipe);
+    }
+  });
+  
+  
+  Recipe.insertMany(data)
+  .then(recipe => { console.log('All the recipes: ', recipe) })
+  .catch(err => { console.log('An error happened:', err) });
+
+
+  Recipe.findByIdAndUpdate( "Rigatoni alla Genovese", { duration: "100" })
+  .then(recipe => { console.log('duration:', recipe) })
+  .catch(err => { console.log('An error happened:', err) });
+
+
+  Recipe.deleteOne({ title:"Carrot Cake"})
+  .then(recipe => { console.log('Remove carrot cake', recipe) })
+
+
+ // When the connection is disconnected
+mongoose.connection.on('disconnected', () => console.log('Mongoose default connection disconnected'));
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});

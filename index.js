c79088927b5fb177d64659a4894330afc12c9205
Loup A <loup.aparicio@gmail.com>
2019-08-28 17:15:37 +0200

    done

--- index.js
+++ index.js
@@ -10,3 +10,28 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+
+Recipe.create({title: "ratatouille", level: "Easy Peasy", cuisine: "french"})
+.then(recipe => {console.log("The recipe is reciped :", recipe)} )
+.catch(err => {console.log("An error happened :", err)})
+
+Recipe.insertMany(data)
+.then(res => res.forEach( r => console.log(r.title)))
+.catch(err => console.error("an error has occured", err));
+
+
+
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, {duration: 100})
+.then(res => console.log(res))
+;
+
+Recipe.deleteOne({title: "Carrot Cake"}).then(res => console.log("success"))
+
+//disconnect from mongoose
+mongoose.connection.close(function(){
+  console.log("Mongoose default connection is disconnected due to application termination.", "Connection readyState : ", mongoose.connection.readyState)
+  console.log(mongoose.connection.readyState)})
+  ;
+
+console.log(mongoose.connection.readyState)

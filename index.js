1964ace63687f70a76ca46c96464878a6c959e00
Lena Lau <frau.lena.lau@gmail.com>
2019-08-29 21:38:08 +0200

    interation 4+5 missing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
+const recipeSchema = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
+const Recipe = mongoose.model('recipeApp', recipeSchema);
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
@@ -10,3 +11,19 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+Recipe.deleteMany({}).then(() => {
+  Recipe.create({ title: "i am title", cuisine: "mexican" })
+    .then(data => { console.log('The user is saved and its value is: ', data) })
+    .catch(err => { console.log('An error happened:', err) });
+
+  Recipe.insertMany(data).then(data.forEach((recipe) => console.log('title', recipe.title)))
+})
+
+
+
+  // recipexy.save()
+
+  // Recipexy.find().then(allCatsFromDatabase => {
+  //   console.log(allCatsFromDatabase)
+  // })

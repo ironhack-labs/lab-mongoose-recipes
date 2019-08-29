8dc6756201f97ce468841399838c4450f39830ae
Bana Hagos <banahagos@banas-mbp.localdomain>
2019-08-29 09:07:02 +0200

    done: iterations 1 - 5 (schema created, one recipe inserted, many receipes inserted, recipe updated, recipe deleted)

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// Create a new recipe
+let promise1 = Recipe.create({ title: 'Pasta e Fagioli', level: 'Amateur Chef', ingredients: ['8 oz. dried medium white beans', 'Kosher salt', '4 carrots', '1 leek', '6 garlic cloves', '⅓ cup extra-virgin olive oil', 'Freshly ground black pepper', '1 smoked ham hock', '1 15-oz. can whole peeled tomatoes', '1 bunch Tuscan kale', '1–2 Parmesan rinds', '2 bay leaves', '8 oz. small pasta', 'Finely grated Parmesan, crushed red pepper flakes, and crusty bread (for serving)'], cuisine: 'Italian', dishType: 'Dish', image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg', duration: 40, creator: 'Carla Lalli Music' })
+
+// Insert an array of recipes of data.js
+let promise2 = Recipe.insertMany(data)
+
+// Update recipe
+let promise3 = Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+
+// Remove a recipe
+let promise4 = Recipe.deleteOne({ title: 'Carrot Cake' })
+
+
+//Close the database after all tasks are executed
+Promise.all([promise1, promise2])
+.then(() => {
+  Recipe.find({}, 'title')
+.then(value => {
+  console.log(value)
+  Promise.all([promise3, promise4])
+})
+.then(() => {
+    console.log('Duration for Rigatoni alla Genovese has been updated')
+    console.log('Carrot cake recipe has been deleted')
+  })
+  .catch(err => console.log('Error', err))
+  })
+ 
+
+
+

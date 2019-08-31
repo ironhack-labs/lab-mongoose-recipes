080c881ec6f816b521fca8e2efb66d788800bca3
Chanty Herrmann <chanty@Chantys-MacBook-Air.local>
2019-08-31 10:27:41 +0200

    PTWD Chanty done

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
-
+// Recipe.insertMany(data)
 // Connection to the database "recipeApp"
+const mongoose = require('mongoose');
+const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
+const data = require('./data.js');  // Import of the data from './data.js'
+//Iteration 2 - Create a recipe
+let promise1 = Recipe.create({
+  title: 'Im a new Recipe', level: 'UltraPro Chef', ingredients: 'Love and Air', cuisine: 'French', dishType: 'Snack'
+})
+  .then(recipe => {
+    console.log('The new recipe is saved and its value is:', this.title)
+    //oder recipe?
+  })
+  .catch(err => { console.log('An error happened:', err) });
+
+//Iteration 3 - Insert Many recipes
+Recipe.insertMany(data)
+  .then(recipe => {
+    console.log('New Recipe inserted:', this.title)
+    //Iteration 4 - Update recipe
+    let promise2 = Recipe.updateOne({ name: 'Rigatoni alla Genovese' }, { duration: 100 })
+      .then(
+        recipe => {
+          console.log('The recipe was updated:', recipe)
+        }
+      )
+      .catch(
+        err => {
+          console.log('An error happened:', err)
+        }
+      );
+    //Iteration 5 - Remove a recipe
+
+    let promise3 = Recipe.deleteOne({ title: 'Carrot Cake' })
+      .then(
+        recipe => {
+          console.log('The recipe was deleted:', recipe)
+        }
+      )
+      .catch(
+        err => {
+          console.log('An error happened:', err)
+          //Iteration 6 - Close the Database
+          Promise.all([promise1, promise2, promise3]).then(() => {
+            mongoose.connection.close()
+          })
+        }).catch(err => { console.log('An error happened:', err) });
+  })
+// Connection to the database "recipeApp"
+
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
@@ -10,3 +58,25 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// PREVIOUS mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+//   .then(() => {
+//     console.log('Connected to Mongo!');
+//   }).catch(err => {
+//     console.error('Error connecting to mongo', err);
+//   });
+
+// Recipe.create({
+//   .then(Recipe => console.log(Recipe))
+// })
+// Candy.create({ type: 'jelly bean' }, { type: 'snickers' }, function (err, jellybean, snickers) {
+//   if (err) // ...
+// });
+  // In index.js, using the Model.create method, you should pass
+  // the info to create a new recipe. After the creation, 
+  // you can use MongoDB Compass to check everything goes ok. 
+  // After inserting the recipe, you should console.log the 
+  // title of the recipe.
+
+  // User.create({ name: 'Alice', password:"ironhack2018", job: 'Architect' })
+  // .then(user => { console.log('The user is saved and its value is: ', user) })
+  // .catch(err => { console.log('An error happened:', err) });
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//Iteration 1 - Recipe Schema
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: Sting, required: true },
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+  //TYPE ARRAY?
+  ingredients: { type: [] },
+  cuisine: { type: Sting, required: true },
+  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack ', 'Drink', 'Dessert', 'Other'] },
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now }
 });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
+// function equals(a, b){
+//   return a.toLowerCase() == b.toLowerCase()
+// }
+

8657053bb6367a35f8a6dc9be40698d4571359dc
Sonia Meller <mellersonia@gmail.com>
2019-09-05 18:10:22 +0100

    Solution code

--- index.js
+++ index.js
@@ -9,41 +9,44 @@ mongoose
   .connect(`${MONGODB_URI}/${DATABASE_NAME}`, { useNewUrlParser: true })
   .then(() => {
     console.log("Connected to Mongo!");
+    runRecipeApp()
   })
   .catch(err => {
     console.error("Error connecting to mongo", err);
   });
 
-//Put averything inside delete many
+  function runRecipeApp(){
 
-Recipe.deleteMany({}).then(() => {
-  Recipe.create({
-    title: "Lents' stew",
-    level: "Easy Peasy",
-    ingredients: ["onion", "tomato", "sweet pepper", "red sausage", "bacon"],
-    cuisine: "Argentine",
-    duration: 240,
-    creator: "Joaquin"
-  })
-  .then(createdRecipe => {
-    console.log("The title of the recipe is " + createdRecipe.title);
-
-    Recipe.create(data).then(recipes => {
-      console.log("recipes", recipes.map(recipe => recipe.title));
-
-      Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
-      .then(res => {
-        console.log(`The update was succesful and updated ${res.nModified} recipe`,res);
-          //res or whatever we name it logs an object that is the result of the update
-        Recipe.deleteOne({ title: "Carrot Cake" })
-        .then(res => {console.log(`The Carrot Cake was deleted `);
-            //this is why everything is nested. After creating everythin, the connection is closed
-            mongoose.connection.close();
-            })
-        .catch("Err there was an error");
+    //Putting averything inside a deleteMany method with {} will erase everything in the database before starting any new creations
+    Recipe.deleteMany({}).then(() => {
+      Recipe.create({
+        title: "Lents' stew",
+        level: "Easy Peasy",
+        ingredients: ["onion", "tomato", "sweet pepper", "red sausage", "bacon"],
+        cuisine: "Argentine",
+        duration: 240,
+        creator: "Joaquin"
       })
-      .catch(err => console.log("Err there was an error"));
+      .then(createdRecipe => {
+        console.log("The title of the recipe is " + createdRecipe.title);
+      
+        Recipe.create(data).then(recipes => {
+          console.log("recipes", recipes.map(recipe => recipe.title));
+        
+          Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+          .then(res => {
+            console.log(`The update was succesful and updated ${res.nModified} recipe`,res);
+              //res or whatever we name it logs an object that is the result of the update
+            Recipe.deleteOne({ title: "Carrot Cake" })
+            .then(res => {console.log(`The Carrot Cake was deleted `);
+                //this is why everything is nested. After creating everythin, the connection is closed
+                mongoose.connection.close();
+                })
+            .catch("Err there was an error");
+          })
+          .catch(err => console.log("Err there was an error"));
+        });
+      });
     });
-  });
-});
+  }
 

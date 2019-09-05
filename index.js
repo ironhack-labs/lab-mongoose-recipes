63900ca1a130c97ee589766c18a2b0fd481a9a32
Yayo Del Rey <yayodelrey@MacBook-Pro-de-Andres.local>
2019-09-05 08:30:37 -0500

    First commit

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require('mongoose')
+const Recipe = require('./models/Recipe') // Import of the model Recipe from './models/Recipe'
+const data = require('./data.js') // Import of the data from './data.js'
+
+const newRecipe = {
+  title: 'Chicken Cake',
+  level: 'Easy Peasy',
+  ingredients: ['1 Polloyon', '1kg of flour', '5 Carrots'],
+  cuisine: 'Homade food',
+  dishType: 'Cake',
+  image: 'https://images-na.ssl-images-amazon.com/images/I/61imCCyOjCL._SX466_.jpg',
+  duration: 60,
+  creator: 'Alex Gonzalez'
+}
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
-
+    console.log('Connected to Mongo!')
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  })
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,40 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-});
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: []
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: Date
+})
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-module.exports = Recipe;
+const Recipe = mongoose.model('Recipe', recipeSchema)
+module.exports = Recipe

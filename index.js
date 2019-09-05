20abbe600777b1d401d12fc6d37f80c76796fde1
OttoCodeBerlin <ottoschmidberlin@gmail.com>
2019-09-05 09:46:11 -0500

    Done

--- index.js
+++ index.js
@@ -2,11 +2,50 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
+const myRecipe = {
+  title: 'Ropa Vieja',
+  level: 'UltraPro Chef',
+  ingredients: ['Beef', 'Peppers', 'Beans', 'Colchones'],
+  cuisine: 'Cuban',
+  dishType: 'Dish',
+  duration: 60,
+  creator: 'Uschi Otze',
+  created: '2017-05-13'
+}
+
+
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+
+Recipe.create(myRecipe)
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+    console.log(`Connected to Mongo! Created recipe with title ${myRecipe.title}`)
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    console.error('Error connecting to Mongo', err)
+    mongoose.connection.close()
+  })
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  .then(() => {
+    console.log(`Updated recipe`)
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    console.error('Error connecting to Mongo', err)
+    mongoose.connection.close()
+  })
+
+
+Recipe.deleteOne({ title: 'Carrot Cake' })
+  .then(() => {
+    console.log(`Deleted recipe`)
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    console.error('Error connecting to Mongo', err)
+    mongoose.connection.close()
+  })
+
 
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+//const RecipeData = require('../data.js');
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: { type: String, required: true, unique: true },
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+  ingredients: { type: Array },
+  cuisine: { type: String, required: true },
+  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] },
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now },
+})
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-module.exports = Recipe;
+const Recipe = mongoose.model('Recipe', recipeSchema)
+module.exports = Recipe
+
+// Recipe.create({
+//   title: 'Ropa Vieja',
+//   level: 'UltraPro Chef',
+//   ingredients: ['Beef', 'Peppers', 'Beans'],
+//   cuisine: 'Cuban',
+//   dishType: 'Dish',
+//   duration: 60,
+//   creator: 'Uschi Otze',
+//   created: '2017-05-13'
+// })
+//   .then(recipe => { console.log('Successfully inserted ', recipe.title) })
+//   .catch(err => { console.log('Error: ', err) })
+
+// Recipe.insertMany(RecipeData)
+//   .then(recipe => { console.log('Successfully inserted ', recipe.title) })
+//   .catch(err => { console.log('Error: ', err) })
+
+// Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+//   .then(recipe => { console.log('Successfully updated ', recipe) })
+//   .catch(err => { console.log('Error: ', err) });
+
+// Recipe.deleteOne({ title: 'Carrot Cake' })
+//   .then(recipe => { console.log('Successfully deleted ', recipe.title) })
+//   .catch(err => { console.log('Error: ', err) });

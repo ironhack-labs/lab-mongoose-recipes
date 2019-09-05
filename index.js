90d49afd9d86c4f9db29d1400be954fd228f5052
Prelhaz <54009534+Prelhaz@users.noreply.github.com>
2019-09-05 19:46:16 +0100

    Hope it is closed

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const mongoose = require('mongoose');
+
+const MONGODB_URI = 'mongodb://127.0.0.1:27017';
+const DATABASE_NAME = 'example';
+
+mongoose.connect(`${MONGODB_URI}/${DATABASE_NAME}`, { useNewUrlParser: true })
+  .then(message => {
+    console.log('Mongoose.connected.');
+    // createRecipe();
+    // createManyRecipes();
+    // updateRecipe();
+    deleteRecipe();
+  })
+  .catch(error =>{
+    console.log('Error connecting to database.');
+    console.log(error);
+  });
+
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
@@ -10,3 +27,58 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+function createRecipe() {
+  Recipe.create({
+    title: 'Chocolate Chip Extra Cookies',
+    level: 'Amateur Chef',
+    ingredients: ['1/2 cup light brown sugar', '1 large egg', '2 tablespoons milk', '1 1/4 teaspoons vanilla extract', '5 cups semisweet chocolate chips'],
+    cuisine: 'French',
+    dishType: 'Dish',
+    image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+    duration: 30,
+    creator: 'Chef Jennifer'
+  })
+  .then(recipe => {
+    console.log(`Created the ${recipe.title} recipe!`);
+  })
+  .catch(error => {
+    console.log('Recipe creation failure!');
+  })
+}
+
+function createManyRecipes() {
+  Recipe.insertMany(data)
+    .then(recipes => {
+      for (item of recipes) console.log(`Created the ${item.title} recipe!`);
+    })
+    .catch(error => {
+      console.log('Failure loading data base.')
+    })
+}
+
+function updateRecipe() {
+      Recipe.find({title: 'Rigatoni alla Genovese'})
+      .then(recipe => {
+        recipe[0].duration = 100;
+        recipe[0].save()
+          .then(() => {
+            console.log('Update susseccfull!');
+          })
+          .catch(error => {
+            console.log('Update failure.');
+          })
+      })
+      .catch(error => {
+        console.log('Error retrieving the recipe.', error);
+      })
+  }
+
+  function deleteRecipe() {
+    Recipe.deleteOne({title: 'Carrot Cake'})
+      .then(() => {
+        console.log('Recipe was successfully removed!');
+      })
+      .catch(error => {
+        console.log('Failure removing item.', error);
+      })
+  }

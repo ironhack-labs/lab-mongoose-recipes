7f49cad1853729b6df0887bca8ca8bec4df26016
Till Kundrun <tillkundrun1@yahoo.de>
2019-09-06 13:06:35 +0200

    first pass

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({data})
+
+// Recipe.findOne({
+//   title: "Carrot Cake"
+// }).then(data => {
+//   console.log(data)
+// });
+
+Recipe.create({title: "Curry"}, {cuisine: "Indian"})
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,6 +3,49 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+
+  ingridients: Array,
+
+  cuisine: {
+    type: String,
+    required: true
+  },
+
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+
+  duration: {
+    type: Number,
+    min: 0
+  },
+
+  creator: {
+    type: String
+  },
+
+  created: {
+    type: Date,
+    default: Date.now
+  }
+
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

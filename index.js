d6a4b1e732e30ddd03cdb0c8f214005f9708b08d
Keighley McFarland <mcfarland.ke@gmail.com>
2019-09-06 14:32:43 +0200

    done

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
+const chalk = require("chalk");
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.set("useCreateIndex", true);
+
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log(chalk.blue("Connected to Mongo!"));
+  })
+  .catch(err => {
+    console.error(chalk.bgRed("Error connecting to mongo"), err);
   });
 
+Recipe.create({
+  title: "Human Soup",
+  level: "Easy Peasy",
+  ingredients: ["humans", "water", "salt", "spices"],
+  cuisine: "French",
+  dishType: "Snack",
+  duration: 60,
+  creator: "Keighley und Jan"
+})
+  .then(recipe => {
+    console.log(chalk.green(recipe.title));
+  })
+  .catch(err => {
+    console.log(error(chalk.bgRed("Error at create recipe")), err);
+  });
+
+Recipe.insertMany(data)
+  .then(data => {
+    data.forEach(recipe => console.log(chalk.magenta(recipe.title)));
+  })
+  .catch(err => {
+    console.error(chalk.bgRed("Error at import recipes"), err);
+  });
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then(() => console.log(chalk.cyan("update successful")))
+  .catch(err => {
+    console.error(chalk.bgRed("Error at update recipe"), err);
+  });
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then(() => {
+    console.log(chalk.yellow("delete sucessful"));
+    mongoose.connection.close();
+  })
+  .catch(err => {
+    console.error(chalk.bgRed("Error at delete recipe"), err);
+  });

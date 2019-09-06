c70e0ef6ce2d8fc0c20413ef7c2f646df7da61b6
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-09-05 23:13:04 -0500

    done

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
+const myRecipe = {
+  title: 'Tacos al Pastor',
+  level: 'Easy Peasy',
+  ingredients: ['Pastor', 'Tortilla', 'silantro'],
+  cuisine: 'Mexican',
+  dishType: 'Other',
+  duration: 5,
+  creator: 'guille',
+  created: '30/08/1960'
+}
+
+
+
+
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+
+Recipe.create(myRecipe)
+  .then(() => {
+    console.log(`Connected to Mongo! Created recipe with title ${myRecipe.title}`)
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    console.error('Error connecting to Mongo', err)
+    mongoose.connection.close()
+  })
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+    console.log(`Updated recipe`)
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    console.error('Error connecting to Mongo', err)
+    mongoose.connection.close()
+  })
 
+
+Recipe.deleteOne({ title: 'Carrot Cake' })
+  .then(() => {
+    console.log(`Deleted recipe`)
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    console.error('Error connecting to Mongo', err)
+    mongoose.connection.close()
+  })
+})

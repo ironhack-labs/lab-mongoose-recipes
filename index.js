ad6137df542e0dafdeae7d3ac12916165d956803
EDGAR ALFREDO CHAVERO MOYA <ediechavero@Edie.local>
2019-09-09 22:06:33 -0500

    not finish

--- index.js
+++ index.js
@@ -10,3 +10,40 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+const newRecipe = {
+  title: "Chicken Dynamite",
+  level: "Easy Peasy",
+  ingredients: ["400grs chickpeas", "garlic", "lemon"],
+  cuisine: "Libanesa",
+  dishType: "Dish",
+  duration: 60,
+  creator: "Edie"
+}
+let recipeCreated = Recipe.create(newRecipe)
+.then(recipe => {
+  console.log(recipe)
+})
+.catch(err => {
+  console.log(err)
+})
+let recipeUpdated = Recipe.findOneAndUpdate(
+  {
+    title: "Rigatoni alla Genovese"
+  },
+  {
+    new: true 
+  }
+)
+  .then(recipe => {
+    console.log(err)
+  })
+
+  let recipeDelete = Recipe.deleteOne({
+    title: "Carrot Cake"
+  })
+    .then(cake => {
+      console.log(`${cake} Not available anymore`)
+    })
+    .catch(err => {
+      console.log(err)
+    })

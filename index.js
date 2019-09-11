7a34dda18cde2846f92b2d13882980551cd7d256
Pablo Giral Alonso <pablo@MacBook-Pro-de-Pablo.local>
2019-09-11 19:34:12 +0200

    [MAD PP] Javier Garcia y Pablo Giral

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
-
+Recipe.deleteMany().then(() => {
+  console.log("all deleted");
+  Recipe.create({
+    title: "tortilla",
+    level: "Amateur Chef",
+    ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+    cuisine: "Asian",
+    dishType: "Dish",
+    image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    duration: 40,
+    creator: "Chef LePapu"
+  }).then(() => {
+    console.log("recipe created");
+    Recipe.insertMany(data).then(() => {
+      console.log("many created");
+      Recipe.updateOne(
+        { title: "Rigatoni alla Genovese" },
+        { duration: 100 }
+      ).then(() => {
+        console.log("updated one");
+        Recipe.deleteOne({ title: "Carrot Cake" }).then(() => {
+          console.log("deleted carrot cake");
+          mongoose.disconnect().then(() => {
+            console.log("adios");
+          });
+        });
+      });
+    });
+  });
+});

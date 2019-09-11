a130563bb62cbd15e6000a6f828a56d914866f09
Antonioadv08 <antonioadv08@gmail.com>
2019-09-11 19:39:47 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
@@ -10,3 +10,28 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+
+
+
+Recipe.create({
+  title: "Chicken Dynamite",
+  level: "Easy Peasy",
+  ingredients: ["lemon"],
+  cuisine: "Libanesa",
+  dishType: "Dish",
+  duration: 60,
+  creator: "Edie"
+})
+  .then(recipeCreated => Recipe.insertMany(data))
+  .then(dataAdded => Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, {
+    duration: 100
+  })
+    .then(updatedRecipe => {
+      console.log('Successfully updated recipe!'),
+        Recipe.deleteOne({ title: 'Carrot Cake' })
+          .then(deletedRecipe => {
+            console.log('Successfully deleted recipe!'),
+              mongoose.connection.close();
+          })
+    }));

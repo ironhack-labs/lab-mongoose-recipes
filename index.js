05a79a3ae30768d2a35115343effe3031bf89164
Osheit <chang.osheit@gmail.com>
2019-09-12 14:26:49 -0400

    Missing some features

--- index.js
+++ index.js
@@ -10,3 +10,52 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// let recipe = new Recipe()
+
+let bananaPancakes = {
+  title: "bananaPancakes",
+  ingredients: ["bananas, pancakes"]
+}
+let recipe = new Recipe(bananaPancakes)
+recipe.save()
+Recipe.insertMany(data).then(datas=>{
+})
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, {$set: { "duration": 100} })
+.then(updatedRag => {
+console.log("updated rag === ", updatedRag);
+}).catch(err => {console.log(err)});
+Recipe.deleteOne({ title: "Carrot Cake" })
+.then(successCallback=>{
+  console.log("successCallback----------", successCallback)
+})
+.catch(errorCallback=>{
+  console.log(errorCallback)
+});
+
+
+
+
+
+
+
+
+
+// let bananaPancakes = {
+//   title:'bananaPancakes',
+//   ingredients: ['banana', 'pancakes']
+// }
+
+// let sample = {
+//   name: 'sample',
+//   whatever: 'HELLO'
+// }
+
+// // let recipe = new Recipe(bananaPancakes)
+
+// Recipe.create(bananaPancakes)
+
+// // recipe.save()
+
+// // console.log(recipe)
+
+// // recipe.create(recipe)
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,7 +3,17 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+title: {type: String, required: true, unique: true},
+ingredients: Array,
+cusine: String,
+dishType: String,
+image: {type: String, default: 'Default : https://images.media-allrecipes.com/images/75131.jpg.'},
+duration: Number,
+creator: String,
+created: Date,
+level: {type:String,  enum: ['Easy Peasy', 'Amateur Chef','UltraPro Chef' ]}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+

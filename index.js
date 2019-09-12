e933d742c859d3e597f72841f422dc5d02ebdef0
Cole Mooney <colemooney@Coles-MacBook-Pro.local>
2019-09-12 12:14:49 -0400

    done

--- index.js
+++ index.js
@@ -10,3 +10,33 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  let bananaPancakes = {
+    title: "bananaPancakes",
+    ingredients: ['bananas', 'pancakes'],
+  }
+
+// Recipe.create(bananaPancakes) {
+//   console.log("Created the recipe")
+// }
+
+Recipe.create(bananaPancakes, function (err, recipe) {
+  if (err) {
+      console.log('An error happened:', err);
+  } else {
+      console.log('The recipe we created is: ', recipe);
+  }
+});
+
+Recipe.insertMany(data);
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+  .then("success<><><><><><><>,")
+  .catch("><><><error><><><><><");
+
+Recipe.deleteOne({ title: "Carrot Cake"})
+  .then("success<><><><><><><>,")
+  .catch("><><><error><><><><><");
+
+mongoose.connection.close()
+
+
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,7 +3,19 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {type: String, required: true, unique: true},
+  ingredients: Array,
+  cuisine: String,
+  dishType: String,
+  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: Number,
+  creator: String,
+  created: Date,
+  level: {type: String, enum:["Easy Peasy", "Amateur Chef", "UltraPro Chef" ]}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+console.log(module.exports)
+
+

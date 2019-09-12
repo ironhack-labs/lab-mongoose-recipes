4ac047484d42feec5c100a565ef079df32b98d48
Adrian <adrianivsdk@gmail.com>
2019-09-12 13:53:56 -0400

    This counts as a commit

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
-
-// Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
-
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from "./models/Recipe"
+const data = require("./data.js");  // Import of the data from "./data.js"
+// Connection to the database “recipeApp”
+mongoose.connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
+.then(() => {
+  console.log("Connected to Mongo!");
+}).catch(err => {
+  console.error("Error connecting to mongo", err);
+});
+//  let bananaPancakes = {
+//    title: "BATMAN PANCAKES",
+//    ingredients:["bananas", "pancakes"],
+//  }
+//  let bananaRecipe = new Recipe(bananaPancakes)
+//  bananaRecipe.save()
+let recipes = function(){
+  for(i = 0; i<data.length; i++){
+    let myRecipe = {
+      title:data[i].title,
+      ingredients:data[i].ingredients,
+      cuisine: data[i].cuisine,
+      dishType:data[i].dish,
+      image:data[i].image,
+      duration:data[i].duration,
+      creator:data[i].creator,
+    }
+    let recipe = new Recipe(myRecipe)
+    recipe.save()
+  }
+}
+//  recipes()
+Recipe.findOne({title: "Rigatoni alla Genovese"})
+.then(recipe=>{
+ {recipe.duration = 160}
+ console.log("Success!")
+ recipe.save()
+})
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 const recipeSchema = new Schema({
-  // TODO: write the schema
+ // TODO: write the schema
+ title: String,
+ ingredients: Array,
+ cuisine: String,
+ dishType: String,
+ image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+ duration: Number,
+ creater: String,
+ created: Date,
+ level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro']}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

6d85ae7fd4df1f708bfa506b7ea38f3408ff2d4d
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-09-12 14:11:27 -0400

    done

--- index.js
+++ index.js
@@ -1,12 +1,91 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+let pbSandwich = {
+  title: 'PB Sandwich',
+  ingredient: ['bread', 'jelly', 'peanut butter'],
+  cuisine: 'yes',
+  dishType: 'Breakfast',
+  image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Peanut-Butter-Jelly-Sandwich.png/500px-Peanut-Butter-Jelly-Sandwich.png',
+  duration: 5,
+  creator: 'me',
+  created: 12,
+  level: 'Easy Peasy',
+}
+
+let recipe = new Recipe(pbSandwich);
+recipe.save() // Create a new user and return a promise
+  .then(recipe => {
+    console.log('The recipe was created', recipe.title)
+  })
+  .catch(err => {
+    console.log('An error occured', err)
+  });
+
+
+
+Recipe.insertMany(data)
+  .then(recipe => {
+    console.log('The recipe was created', recipe)
+
+    Recipe.findById('5d7a78efec835810e241faba')
+      .then(recipe => {
+        recipe.duration = 100;
+        return recipe.save(); // Update the user '5a3a7ecbc6ca8b9ce68bd41b' and return a promise
+      })
+      .then(duration => {
+        console.log('The duration was updated: ' + duration)
+      })
+      .catch(err => {
+        console.log('An error occured:', err)
+      });
+
+    // Recipe.findByIdAndRemove("5d7a78efec835810e241fab9")
+    //   .then(recipe => { console.log(recipe +' was deleted')})
+    //   .catch(err => { console.log(err) });
+
+
+    Recipe.deleteOne({
+        title: 'Carrot Cake'
+      })
+      .then(recipe => {
+        console.log(recipe + ' was deleted')
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  })
+  .catch(err => {
+    console.log('An error occured', err)
+  });
+
+
+Recipe.updateOne({
+    title: "Rigatoni alla Genovese"
+  }, {
+    duration: 100
+  })
+  .then(console.log('Success!'))
+  .catch(console.log('An error occured'));
+
+
+
+Recipe.deleteMany({})
+  .then(res => {
+    console.log("Deleted all ", res);
+  })
+  .catch(err => console.error(err))
+
+
+mongoose.disconnect();
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,6 +3,24 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  ingredient: Array,
+  cuisine: String,
+  dishType: {
+    type: String,
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+  },
+  image: {
+    type: String,
+    default: ' https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: Number,
+  creator: String,
+  created: Date,
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

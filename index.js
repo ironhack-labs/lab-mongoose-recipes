379d7148cd8889b8c9752564e4cae37b24755ff9
Lauren Laskerr <llask@me.com>
2019-09-12 11:40:47 -0400

    iterations done. what's the difference between .disconnect and .connection(close)

--- index.js
+++ index.js
@@ -10,3 +10,40 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+  let bananaPancakes = {
+    // TODO: write the schema
+    title: 'Banana Pancakes',
+    ingredients: ['Banana', 'Pancakes'],
+    dishType: 'Breakfast',
+  }
+
+  let recipe = new Recipe(bananaPancakes);
+  recipe.save();
+
+   Recipe.insertMany(data).then(recipe => {
+    recipe.forEach(food => {
+      console.log(food.title)
+    })
+   }).catch(error =>{
+     console.log(error);
+   })
+
+
+
+  Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100}).then(data =>{
+    console.log("success")
+  }).catch(error => {
+    console.log(error)
+  })
+
+
+
+  Recipe.deleteOne({title: 'Carrot Cake'}).then(data => {
+    console.log(`deleted`)
+  }).catch(error => {
+    console.log(error)
+  })
+
+
+  mongoose.disconnect()
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,6 +3,15 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  ingredients: Array,
+  cuisine: String,
+  dishType: {type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: Number,
+  creator: String,
+  created: {type: Date, default: new Date},
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

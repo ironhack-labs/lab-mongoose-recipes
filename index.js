d69ee615c88b5501c11e287b6608df1b9cf4a62d
Jenniferrmuniz <Jennifer.r.muniz@gmail.com>
2019-09-12 12:32:51 -0400

    first commit

--- index.js
+++ index.js
@@ -10,3 +10,64 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+
+
+
+
+function insertRecipes(){
+  Recipe.insertMany(data)
+    .then(dataArray => {
+      for(let i=0; i<dataArray.length; i++){
+        console.log(dataArray[i].title);
+      }
+    })
+    .catch(err => {
+      console.log("Error at creation: ", err);
+    });
+}
+
+
+
+
+function updateRecipe(findObj, change){
+  Recipe.updateOne(findObj, change)
+  .then(() => {
+    console.log('Update Successful!')
+  })
+  .catch(err => {
+    console.log("Error updating recipe", err);
+  });
+}
+
+
+
+
+function deleteRecipe(findObj){
+  Recipe.deleteOne(findObj)
+  .then(() => {
+    console.log('Deletion Successful!')
+  })
+  .catch(err => {
+    console.log("Error deleting recipe", err);
+  });
+}
+
+
+
+let bananaPancakes = {
+  title: 'bananaPancakes',
+  ingredients: ['bananas', 'pancakes']
+}
+
+let recipe = new Recipe(bananaPancakes);
+Recipe.create(recipe);
+console.log(recipe.title);
+
+
+
+insertRecipes();
+updateRecipe({title: 'Rigatoni alla Genovese'}, {duration: 100});
+deleteRecipe({title: 'Carrot Cake'});
+
+

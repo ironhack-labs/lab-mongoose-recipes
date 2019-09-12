fa1158768c64a5017ad1f300908634b5250ae576
Christian Roque <croqu008@fiu.edu>
2019-09-12 12:19:00 -0400

    done

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+mongoose
+	.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+	.then(() => {
+		console.log('Connected to Mongo!');
+	})
+	.catch((err) => {
+		console.error('Error connecting to mongo', err);
+	});
 
+let bananaPancakes = {
+	title: 'BananaSplit'
+};
+
+let recipe = new Recipe(bananaPancakes);
+
+recipe.save();
+
+Recipe.insertMany(data);
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 }).then('successCallback').catch('errorCallback');
+
+Recipe.deleteOne({ title: 'Carrot Cake' }).then('successCallback').catch('errorCallback');
+
+mongoose.connection.close();
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+	// TODO: write the schema
+	title: String,
+	ingredients: Array,
+	cousine: String,
+	dishType: String,
+	image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+	duration: Number,
+	creator: String,
+	created: Date,
+	level: { type: String, enum: [ 'Easy Peasy', 'Amateur Chef', 'UltraPro Chef' ] }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

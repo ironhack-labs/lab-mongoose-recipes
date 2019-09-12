8ce71ed3b75399be42e93975cd2306cf3d2b6ee8
Nick Sherman <ogleffeckt2@yahoo.com>
2019-09-12 11:48:14 -0400

    close

--- index.js
+++ index.js
@@ -2,11 +2,69 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
-// Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
+    console.log('Connected to mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+ 
+  function newRecipe () {
+    Recipe.create( {
+      title: 'Banana Pancakes',
+      level: 'Amateur Chef',
+      ingredients: ['Banana', 'Pancakes'],
+      cuisine: 'Breakfeast',
+      dishType: 'Dish',
+      image: 'none',
+      duration: 10,
+      creator: 'Nick'
+    })
+    .then(recepie => {
+      console.log(`${recepie.title} created`);
+    })
+    .catch(error => {
+      console.log('Error creating recipe');
+    });
+  }
 
+  function insertRecipes() {
+    Recipe.insertMany(data)
+    .then(recipe => {
+      for (let recipes of recipe) {
+      console.log(`${recipes.title} added to the database`);
+    }
+    })
+    .catch(error => {
+      console.log('Error adding recipes to the database');
+    });
+  }
+
+  function loadSingleItem() {
+    Recipe.findOne({ title: "Pizza" })
+    .then(recipe => {
+      console.log(recipe);
+      recipe.duration = 100;
+      recipe.save()
+      .then(() => {
+        console.log('Recipe successfully changed!');
+      })
+      .catch(error => {
+        console.log('Error saving recipe!');
+      });
+    })
+    .catch(error => {
+      console.log('Error loading recipe');
+    });
+  }
+
+  function deleteOneItem() {
+    Recipe.deleteOne({ title: "Carrot Cake"})
+    .then(recipe => {
+      console.log('Recipe deleted');
+    })
+    .catch(error => {
+      console.log('Error loading recipe');
+    });
+
+  }
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,7 +3,30 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-});
+  title: String,
+  level:{
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+    },
+  ingredients: Array,
+  cuisine: String,  
+  dishType:{
+    type: String, 
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+    },
+  image:{
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg.'  
+    },
+  duration: Number,
+  creator: String,
+  created:{
+    type: Date,
+    default: Date.now()
+  }})
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
+
+

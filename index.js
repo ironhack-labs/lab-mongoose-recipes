c598bad28d15fd73ff16c5f2635bd49442995ced
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-09-13 00:18:33 -0500

    done

--- index.js
+++ index.js
@@ -1,55 +1,75 @@
+const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
-
-const myRecipe = {
-  title: 'Tacos al Pastor',
-  level: 'Easy Peasy',
-  ingredients: ['Pastor', 'Tortilla', 'silantro'],
-  cuisine: 'Mexican',
-  dishType: 'Other',
-  duration: 5,
-  creator: 'guille',
-  created: '30/08/1960'
-}
-
-
-
+const mongoose = require('mongoose')
+const Recipe = require('./models/Recipe') // Import of the model Recipe from './models/Recipe'
+const data = require('./data.js') // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-
-Recipe.create(myRecipe)
-  .then(() => {
-    console.log(`Connected to Mongo! Created recipe with title ${myRecipe.title}`)
-    mongoose.connection.close()
-  })
-  .catch(err => {
-    console.error('Error connecting to Mongo', err)
-    mongoose.connection.close()
-  })
-
-Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+mongoose
+  .connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
-    console.log(`Updated recipe`)
-    mongoose.connection.close()
+    console.log('Connected to Mongo!')
   })
   .catch(err => {
-    console.error('Error connecting to Mongo', err)
-    mongoose.connection.close()
+    console.error('Error connecting to mongo', err)
   })
 
+  const myRecipe = 
+  { title: 'Tacos al Pastor',
+   level: 'Easy Peasy',
+    ingredients: ['Pastor', 'Tortilla', 'silantro'], 
+    cuisine: 'Mexican', 
+    dishType: 'Other', 
+    duration: 5, 
+    creator: 'guille', 
+    created: '30/08/1960' 
+  }
 
-Recipe.deleteOne({ title: 'Carrot Cake' })
-  .then(() => {
-    console.log(`Deleted recipe`)
-    mongoose.connection.close()
+let recipeCreated = Recipe.create(newRecipe)
+  .then(recipe => {
+    console.log(recipe)
   })
   .catch(err => {
-    console.error('Error connecting to Mongo', err)
-    mongoose.connection.close()
+    console.log(err)
   })
+
+let recipesCreated = Recipe.create(data)
+  .then(recipes => {
+    console.log(`Recipe's created ${recipes.length} `)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+let recipedUpdated = Recipe.findOneAndUpdate(
+  {
+    title: 'Rigatoni alla Genovese'
+  },
+  {
+    duration: 100
+  },
+  {
+    new: true
+  }
+)
+  .then(recipe => {
+    console.log(recipe)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+let recipeDeleted = Recipe.deleteOne({
+  title: 'Carrot Cake'
 })
+  .then(cake => {
+    console.log(`${cake} Not available anymore`)
+  })
+  .catch(err => {
+    console.log(err)
+  })

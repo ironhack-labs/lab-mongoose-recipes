bc5d9eb4cd5113f346a8517ea53c986952d69ec2
Xavier Martinez <luis.martz7@hotmail.com>
2019-09-15 14:38:24 -0500

    Done

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
+mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true})
+  .then(async () => {
     console.log('Connected to Mongo!');
+    try {
+      const myBarbacoa = await Recipe.create({
+        title: 'Barbacoa',
+        level: 'UltraPro Chef',
+        cuisine: 'Mexican Hidalguense alv Perroooo',
+        ingredients: ['Borrego', 'Pencas de maguey'],
+        dishType: 'Dish',
+        image: 'https://www.entornoturistico.com/wp-content/uploads/2017/08/Barbacoa-al-estilo-Hidalgo-660x330.jpghttps://www.entornoturistico.com/wp-content/uploads/2017/08/Barbacoa-al-estilo-Hidalgo-660x330.jpg',
+        duration: 6000,
+        creator: 'Xavier Martinez'
+      });
+      console.log(myBarbacoa);
+
+      const recipesCreated = await Recipe.create(data);
+      console.log(`All the menu:
+      ${recipesCreated}`);
+
+      recipesCreated.forEach(recipe => console.log(recipe.title));
+
+      const updatedRecipe = await Recipe.findOneAndUpdate({
+        title: 'Rigatoni alla Genovese'
+      }, {
+        duration: 100
+      }, {
+        new: true
+      });
+      console.log(`Rigatoni Updated:
+      ${updatedRecipe}`);
+
+      const deletedRecipe = await Recipe.findOneAndDelete({
+        title: 'Carrot Cake'
+      });
+      console.log('Succefully deleted', deletedRecipe);
+
+      mongoose.connection.close();
+    } catch (e) {
+      console.log(e);
+      mongoose.connection.close();
+    }
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
-
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,35 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const {model, Schema} = require('mongoose')
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredents: [],
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishtype: {
+    type: String,
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now()
+  }
+}, {
+  timestamps: true,
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-module.exports = Recipe;
+module.exports = model('Recipe', recipeSchema)

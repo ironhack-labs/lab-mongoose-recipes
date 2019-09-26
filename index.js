a7cc82ca77c059978c8c8e660c0ea464c5a6e1d9
marcos <marcosaalacerda@gmail.com>
2019-09-26 15:05:35 -0300

    almost done

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const data = require('./data.js');  // Import of the data from './data.js'
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
+    Recipe.insertMany(data)
+    
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({title:"ovo frito", level: "Easy Peasy",ingredients:["ovo","sal","oleo/mantega"],cuisine: "global",dishType:"Breakfast",image:"https://cozinhatecnica.com/wp-content/uploads/2015/02/ovo-frito-e1529422904766.jpg",duration:3,creator:"JOC"})
+.then(recipe => { console.log(recipe.title)})
+.catch(err=> {console.log("An error happened", err)});
+
+//Recipe.insertMany([{title:"ovo cozido", level: "Easy Peasy",ingredients:["ovo","sal","oleo/mantega"],cuisine: "global",dishType:"Breakfast",image:"https://cozinhatecnica.com/wp-content/uploads/2015/02/ovo-frito-e1529422904766.jpg",duration:3,creator:"JOC"},{title:"ovo poche", level: "Easy Peasy",ingredients:["ovo","sal","oleo/mantega"],cuisine: "global",dishType:"Breakfast",image:"https://cozinhatecnica.com/wp-content/uploads/2015/02/ovo-frito-e1529422904766.jpg",duration:3,creator:"JOC"}]);
+// Recipe.insertMany(data);
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title:String,
+  level: {type: String, enum : ['Easy Peasy', 'Amateur Chef']},
+  ingredients: Array,
+  cuisine:String,
+  dishType:{type: String, enum : ['Breakfast', 'Dish','Snack','Drink','Dessert','Other']},
+  image:{type:String, default:'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration:{type:Number, min: 0 },
+  creator:String,
+  
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

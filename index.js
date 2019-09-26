8f5b51a5ebdd7c2f267a6c94c34a2b7e15ab4632
Guilherme Tadanori Mori <gtm.mori@gmail.com>
2019-09-26 15:05:33 -0300

    done

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  Recipe.create({title: `Lasanha`, level: `UltraPro Chef`, ingredients: [`Massa`, `Molho`, `Queijo`, `Carne`, `Presunto`], cuisine:`Italiana`, dishType: `Dish`, image:`http://www.receitasnestle.com.br/images/default-source/recipes/lasanha_a_bolonhesa_saborosa.jpg`, duration: 30, creator:`Filipe`})
+    .then(Recipe => {console.log(Recipe.title)})
+    .catch(err => {console.log(error)})
+
+Recipe.insertMany(data)
+  .then(data => {
+    data.forEach((elem) => console.log(elem.title))
+  })
+  .catch(error => {console.log(error)})
+
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then(recipe => console.log("update sucess!"))
+  .catch(error => console.log(error))
+
+Recipe.deleteOne({title: "Carrot Cake"})
+  .then(recipe => console.log("delete sucess!"))
+  .catch(error => console.log(error))
+
+mongoose.connection.close(() => {
+  console.log('Mongoose default connection disconnected through app termination');
+  process.exit(0);
+});

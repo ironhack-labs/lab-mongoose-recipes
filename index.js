723db33a58b1e6be3dbaf51868803caad7fd8596
Alberto Hernández <albertohdezherrera@gmail.com>
2019-10-26 16:24:37 +0200

    Done

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  Recipe.create({ title: 'Kebab', level: 'mierda', ingredients: ['a saber 1', 'a saber 2', 'a saber 3'], cuisine: 'Turca', dishType: 'Comida rapiada', duration: 30, creator: 'MR. Kebab'  })
+  .then(user => { console.log('The user is saved and its value is: ', user) })
+  .catch(err => { console.log('An error happened:', err) });
+
+  Recipe.insertMany(data)
+    .then( data => {
+      console.log(`Fue guay ${data}`)
+      Recipe.updateOne({ title: 'Rigatoni alla Genovese'}, { duration: 100 })
+      .then(data =>  {
+        Recipe.deleteOne({ title: 'Carrot Cake'})
+        .then(data => {
+          mongoose.connection.close().then(data => console.log(data)).catch(data => console.log(data))
+        })
+        .catch(data => console.log(data));
+      })
+      .catch(data => console.log(data))
+    })
+    .catch( data => console.log(`fue mal: ${data}`))
+
+
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: String,
+  level: String,
+  ingredients: Array,
+  cuisine: String,
+  dishType: String,
+  image: { type: String, default: 'images/default-avatar.png' },
+  duration: { type: Number, min: 0 },
+  creator: String,
+}, {
+    timestamps: true
+  }
+);
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

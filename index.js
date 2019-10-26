be9878fd278d07bcd3bff56af223215ae5ad4bb1
Maria Teresa Barbany Biescas <mt.barbany@gmail.com>
2019-10-26 17:57:27 +0200

    done

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  Recipe.create({ title: 'Apple Pie', cuisine:"American", duration: 20,})
+    .then(recipe => { console.log('The recipe is saved and its value is: ', recipe)})
+    .catch(err => { console.log('An error happened:', err) })
+
+
+Recipe.insertMany(data)
+  .then(recepies => {
+    recepies.forEach(recipe => console.log('The recipe is saved and its value is: ', recipe.title))
+    
+    return Recipe.updateOne({ tile: 'Rigatoni alla Genovese' }, { duration:100 })
+      .then((recipe) => { console.log ('Success!! the recepie has been updated correctly') 
+      
+    return Recipe.deleteOne({title:'Carrot Cake'})
+      .then((recipe)=> { console.log ('You have correctly deleted the recipe') })
+    })
+  })
+  .catch(err => { console.log('An error happened:', err) })

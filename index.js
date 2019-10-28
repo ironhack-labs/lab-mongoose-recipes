7540ce65bb2638f87533b1345f4c5ba1e1f71b4d
Elizabeth Lombardi <elizabeth.lombardi7@gmail.com>
2019-10-28 21:52:57 +0100

    Revisar qye falta esta avanzado

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+// Import of the model Recipe from './models/Recipe'
+const Recipe = require('./models/Recipe'); 
+// Import of the data from './data.js'
+const data = require('./data.js');  
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
@@ -10,3 +12,15 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  recipeSchema.create({
+    title: 'Asian Glazed Chicken Thighs',
+    level: 'Amateur Chef',
+    ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+    cuisine: 'Asian',
+    dishType: 'Dish',
+    image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+    duration: 40,
+    creator: 'Chef LePapu'    
+  })
+  .then(user => { console.log('The user is saved and its value is: ', user) })
+  .catch(err => { console.log('An error happened:', err) });

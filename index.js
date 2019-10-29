77e3742a9775c2ff9dadc24efabe7ee6f73b392d
Alberto Hernández <albertohdezherrera@gmail.com>
2019-10-29 19:28:01 +0100

    cambios de modelo

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
       console.log(`Fue guay ${data}`)
       Recipe.updateOne({ title: 'Rigatoni alla Genovese'}, { duration: 100 })
       .then(data =>  {
+        console.log(`Fue guay ${data}`)
         Recipe.deleteOne({ title: 'Carrot Cake'})
         .then(data => {
+          console.log(`Fue guay ${data}`)
           mongoose.connection.close().then(data => console.log(data)).catch(data => console.log(data))
         })
         .catch(data => console.log(data));

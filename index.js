9ac87cdae53b068462e891028189d01d24d43906
raw-brt <robertodzbt@gmail.com>
2019-10-29 16:17:56 +0100

    done

--- index.js
+++ index.js
@@ -10,3 +10,39 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  // Create a recipe
+Recipe.create({ 
+  title: "Fabada", 
+  level: "Amateur Chef", 
+  ingredients: ["Fabes", "Chorizo", "Morcilla", "Tocino"],
+  cuisine: "Asturiana",
+  dishType: "Dish",
+  duration: 45,
+  creator: "Abuelas asturianas",
+  created: "1800-01-01"
+})
+  .then(recipe => console.log("${this.title}"))
+  .catch(err => console.log("Something went wrong"));
+
+  // Insert many recipes
+  const insertRecipes = Recipe.insertMany(data);
+
+  // Update recipe
+  const updateRecipe = Recipe.updateOne({ title: 'Rigatoni alla Genovese', duration: 100 })
+    .then(updatedRecipe => console.log('Recipe successfully updated'))
+    .catch(err => console.log('Something went wrong'));
+
+  // Remove a recipe
+  const removeRecipe = Recipe.deleteOne({ title: "Carrot Cake" })
+    .then(removedRecipe => console.log('Recipe successfully removed'))
+    .catch(err => console.log('Something went wrong'));
+
+  // Close the database
+  const closeDatabase = mongoose.connection.close()
+    .then(closedDatabase => console.log('Database successfully closed'))
+    .catch(err => console.log('Something went wrong'));
+
+  // Viva la asincronía, pendejo!!
+  Promise.all([updateRecipe, removeRecipe, closeDatabase])
+    .then(promises => console.log('Actions successfully executed'))
+    .catch(err => console.log('Something went wrong'));

b151779dc346590dd133d3d687dc05f5f71700bc
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-30 21:50:46 -0300

    all done but everything is commented so it doesnt repeat every time

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-Recipe.create({
-  title: "Pizza de Guacamole",
-  level: 'Amateur Chef',
-  ingredients: ["Pizza", "Guacamole"],
-  cuisine: "Italiana",
-  dishType: "Dish",
-  duration: 60,
-  creator: "Coe",
-})
+// Recipe.create({
+//   title: "Pizza de Guacamole",
+//   level: 'Amateur Chef',
+//   ingredients: ["Pizza", "Guacamole"],
+//   cuisine: "Italiana",
+//   dishType: "Dish",
+//   duration: 60,
+//   creator: "Coe",
+// })
 
-Recipe.insertMany(data, function(err, doc) {})
+// Recipe.insertMany(data, function(err, doc) {})
+
+// Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, (err, doc)=> console.log(doc))
+// Recipe.find({title: "Rigatoni alla Genovese" }, (err, doc)=>{err ? console.log(err) : console.log(doc)})
+// Recipe.deleteOne({title: "Carrot Cake"})
+mongoose.connection.close()

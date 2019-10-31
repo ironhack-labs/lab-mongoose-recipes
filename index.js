ac4583a9a291ae1aeb68157093222711db5badc8
Solomonrp <matheusrp97@hotmail.com>
2019-10-30 22:25:49 -0300

    Feito

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
@@ -10,3 +12,36 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+function addRecipes(data) {
+
+  Recipe.create(data)
+    .then((recipe) => {
+      console.log('recipe')
+    })
+    .catch(err => { console.log(err) });
+}
+
+function update(tittle, field, newValue) {
+  Recipe.findOneAndUpdate({ title: `S{tittle}` }, { field: newValue })
+    .then((update) => {
+      console.log('Update Feito');
+    })
+    .catch(erro => { console.log(erro) });
+}
+
+function deleteRecipe(title) {
+  Recipe.deleteOne({ title: `${tittle}` })
+    .then((delet) => {
+      console.log('Deletado');
+    })
+    .catch(erro => { console.log(erro) });
+}
+
+function closeConect(){
+  mongoose.connection.close();
+  console.log('Fechado');
+}
+
+setTimeout(closeConect, 1500)
+
+// app.listen(3000);

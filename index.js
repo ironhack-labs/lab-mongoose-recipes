6c36d74ed5197d01d4a787fe4c84b7aa4147debb
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-10-30 21:57:27 -0300

    inserindo dados no banco

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const PORT = 3000;
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
@@ -10,5 +13,8 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-
-  
+  Recipe.create({title: 'teste2', level: 'Easy Peasy', ingredients: ['apple', 'orange'], cuisine: 'something', dishType: 'Dish', image: 'https://images.media-allrecipes.com/images/75131.jpg.', duration: 3, creator: 'Jef'})
+  .then((recipe) => {
+    console.log(`${recipe}!`);
+  })
+  .catch(err => {console.log('An error happened:', err)});

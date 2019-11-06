c00e2d8fadc1ff042f38364c9b3d1cd0ebd5aac8
Louise Marissal <louisemarissal@MacBook-Pro-de-Louise.local>
2019-11-06 18:54:29 +0100

    done with the frank s help

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
-
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
+// iteration 2
+Recipe.create({
+  title: "Waffle",
+  level: "Easy Peasy",
+  ingredients: [
+    "200g of flour",
+    "30g of granulated sugar",
+    "3 eggs",
+    "20g of butter",
+    "25cl of milk",
+    "1 minch of salt"
+  ],
+  cuisine: "Cook in a lightly buttered waffle iron",
+  dishType: "Snack",
+  image:
+    "https://odelices.ouest-france.fr/images/recettes/gaufres-970x1024.jpg",
+  duration: 17,
+  creator: "My mummy",
+  created: Date.now()
+})
+  .then(Recipe => {
+    console.log(Recipe.title);
+  })
+  .catch(dbError => console.error(dbError));
+console.log("One!");
+// iteration 3
+Recipe.insertMany(data);
+console.log("Two!");
+//iteration 4
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 });
+console.log("Three!");
+// iteration 5
+Recipe.findOneAndDelete({ title: "Carrot Cake" });
+console.log("Four!");
 
+// Iteration 6
+mongoose.connection.close();
+console.log("Five!");
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  // TODO: write the schema -----
+  // Iteration 1
+  title: String,
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: Array,
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg."
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;

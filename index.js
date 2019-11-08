58ec58f03be4cb44fbf461a94667e8feb5850472
Noam <noamru22@gmail.com>
2019-11-08 11:45:01 +0100

    done

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './modeldata.js'model
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+const exampleRecipe = {
+  title: "Asian Glazed Chicken Thighs",
+  level: "Amateur Chef",
+  ingredients: ["1/2 cup rice vinegar", "5 tablespoons honey", '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  cuisine: 'Asian',
+  dishType: 'Dish',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 40,
+  creator: 'Chef LePapu'
+}
+
+Recipe.create(exampleRecipe).then(recipes => {
+  console.log(exampleRecipe.title);
+}).catch(err => {
+  console.log(err)
+});
+
+Recipe.insertMany(data).then(recipes => {
+  for (let i = 0; i < recipes.length; i++) {
+    console.log(recipes[i].title)
+  }
+}).catch(err => {
+  console.log(err)
+});
+
+Recipe.updateOne({
+  title: "Rigatoni alla Genovese"
+}, {
+  duration: 100
+}).then(response => {
+  console.log("update success");
+}).catch(err => {
+  console.log(err);
+});
+
+Recipe.deleteOne({
+  title: "Carrot Cake"
+}).then(response => {
+  console.log("delete success");
+  mongoose.disconnect();
+}).catch(err => {
+  console.log(err);
+});
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+    type: String
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"],
+    type: String
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

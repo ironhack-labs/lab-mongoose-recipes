b1f29875720cd24f1c9def07ffc0f7771daedc22
daneklustig <danek.lustig@gmail.com>
2019-11-08 11:52:19 +0100

    done

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+/* ------------------------------- iteration 2 ------------------------------ */
+
+Recipe.create({
+  title: "Spaghetti",
+  level: "Easy Peasy",
+  ingredients: ["spaghetti", "pesto", "salt"],
+  cuisine: "Italian",
+  dishType: "Dish",
+  creator: "Daniel"
+})
+
+/* ------------------------------- iteration 3 ------------------------------ */
+
+Recipe.insertMany(data)
+  .then(resArray => {
+    respArray.forEach((obj) => {
+      console.log(obj.title)
+    })
+  }).catch(err => {
+    console.log(err)
+  })
+
+/* ------------------------------- iteration 4 ------------------------------ */
+
+Recipe.updateOne({
+  title: "Rigatoni alla Genovese"
+}, {
+  duration: 100
+}).then(res => {
+  console.log("UDPATED")
+}).catch(err => {
+  console.log(err)
+})
+
+/* ------------------------------- iteration 5 ------------------------------ */
+
+Recipe.deleteOne({
+  title: "Carrot Cake"
+}).then(res => {
+  console.log("DELETED")
+}).catch(err => {
+  console.log(err)
+})
+
+/* ------------------------------- iteration 6 ------------------------------ */
+
+// since the code is executed in an asynchronous way, the closing command would have to be nested within the other commands, so it is easier to just press CONTROL + C
+//mongoose.connection.close()
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/* ------------------------------- iteration 1 ------------------------------ */
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"],
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
+
+// this exports the recipe, we can import it in index.js
 module.exports = Recipe;

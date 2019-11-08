48277527686fe939e8282b44c33480f1836a39c3
fabianschu <48454910+fabianschu@users.noreply.github.com>
2019-11-08 10:35:23 +0100

    done

--- index.js
+++ index.js
@@ -10,3 +10,41 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  /*
+Recipe.create({
+  title: "abc",
+  level: "Easy Peasy",
+  ingredients: ["a", "b"],
+  cuisine: "exxquisite"
+})
+  .then(document => {
+    console.log(document.title);
+  })
+  .catch(err => {
+    console.log("ERROR: ", err);
+  });*/
+
+Recipe.insertMany(data)
+  .then(document => {
+    document.forEach(dish => {
+    console.log(dish.title);
+    })
+    Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+      .then(document => {
+        console.log("updated successfully");
+      })
+      .catch(err => {
+        console.log("ERROR: ", err);
+      });
+    Recipe.deleteOne({title: "Carrot Cake"})
+      .then(document => {
+        console.log("deleted successfully");
+        mongoose.disconnect();
+      })
+      .catch(err => {
+        console.log("ERROR: ", err);
+      });
+  })
+  .catch(err => {
+    console.log("ERROR: ", err);
+  });
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,7 +3,38 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    unique: true
+  },
+  level: {
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+    type: String
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"],
+    type: String,
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: "11.08.2019"
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+

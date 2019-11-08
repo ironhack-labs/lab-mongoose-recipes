d03e6197eeed57c57388fa41b8286336bb02b3d6
Maria Palonen <mariakristiina@Marias-MacBook-Pro.local>
2019-11-08 11:15:35 +0100

    done

--- index.js
+++ index.js
@@ -10,3 +10,42 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// Recipe.findOneAndDelete({title: "Carrot Cake"})
+//       .then(response => console.log("Successfully deleted!", response))
+//       .catch(err => 
+//         console.log(err));
+
+
+// Recipe.updateOne({title: "Rigatoni alla Genovese"}, { duration : 100})
+//       .then(response => console.log("Success!", response))
+//       .catch(err => 
+//         console.log(err));
+
+  // Recipe.insertMany(data)
+  // .then(response => {
+  //       response.forEach(recipe => console.log(recipe.title));
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+
+
+  // Recipe.create({
+  //   title: "Carelian pie",
+  //   level: "Amateur Chef",
+  //   cuisine: "Finnish",
+  //   ingredients: ["Rice", "Milk", "Water", "Salt", "Butter", "Rye flour", "Eggs"],
+  //   dishType: "Snack",
+  //   image: "https://www.travelgluttons.com/wp-content/uploads/2015/04/Karelian-pie-without-egg-butter1.jpg",
+  //   duration: 180,
+  //   creator: "Grandma",
+  //   created: 06-12-1907,
+  //   })
+  //   .then(response => {
+  //     console.log(response.title);
+  //   })
+  //   .catch(err => {
+    //     console.log(err);
+    //   });
+    
+    mongoose.connection.close();

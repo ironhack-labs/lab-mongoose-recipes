e3c18d0a9205a4de1f5a8375f3c1fbbb77ced6a5
Alexandra Servie <alexservie@Alexandras-MacBook-Pro-2.local>
2019-11-08 11:24:00 +0100

    done

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+const connecting = mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create()
+  .then(recipe => {
+    console.log(recipe.title);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.updateOne({
+    title: 'Rigatoni alla Genovese'
+  }, {
+    duration: 100
+  })
+  .then(request => {
+    console.log('success')
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.deleteOne({
+    title: 'Carrot Cake'
+  })
+  .then(request => {
+    console.log('successfully deleted');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.findOneAndUpdate({
+    title: 'Rigatoni alla Genovese'
+  }, {
+    duration: -3
+  }, {
+    new: true,
+    runValidators: true
+  })
+  .then(request => {
+    console.log(request);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+
+mongoose.connection.close(function () {
+  console.log('Mongoose default connection closed');
+});

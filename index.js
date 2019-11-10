53002fd4e2dcc615491b1488c8a5cbf287be7fad
Mawee33 <mariegreiner33@gmail.com>
2019-11-10 19:55:01 +0100

    iterations 1 to 5 (iteration 6 with Franck during renforcement

--- index.js
+++ index.js
@@ -10,3 +10,31 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+// Recipe.create({
+//   title: 'Chocolat Mousse',
+//   level: 'Easy peasy',
+//   ingredients: ['200g dark chocolate', '10g butter', '8 eggs'],
+//   cuisine: 'french',
+//   dishType: 'Dessert',
+//   image: 'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/basicchocolatemousse_93648_16x9.jpg',
+//   duration: 20,
+//   creator: 'Paul Bocuse'
+// }, function (err, Recipe) {
+//   if (err) {
+//       console.log('An error happened:', err);
+//   } else {
+//       console.log('The recipe is created: ', Recipe.title);
+//   }
+// });
+
+// Recipe.insertMany(data, data.forEach(element => console.log(element.title)));
+
+Recipe.findByIdAndUpdate("5dc2f2fb6f76eb4922de83c8", {duration: 100})
+  .then("The value is successfully changed")
+  .catch("error");
+
+  Recipe.findByIdAndRemove("5dc2f3069c2b24492c2a6552")
+  .then("The value is successfully removed")
+  .catch("error");
+

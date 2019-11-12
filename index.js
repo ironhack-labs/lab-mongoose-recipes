81b65181e072880fafda208200c1bf51e1a55710
Camilosorio <tech@camilo.fr>
2019-11-12 16:50:57 +0100

    done

--- index.js
+++ index.js
@@ -5,8 +5,37 @@ const data = require('./data.js');  // Import of the data from './data.js'
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+    return Recipe.create({
+      title: "Vanille",
+      level: "UltraPro Chef",
+      ingredient: ["chocolate", "Marihuana"],
+      cuisine: "Tibet",
+      dishType: "Breakfast",
+      duration: 25,
+      creator: "Yoooooo Frank"
+    });
+  })
+  .then(everest => {
+    console.log("recipe added");
+    console.log(everest);
+    return Recipe.insertMany(data);
+  })
+  .then(() => {
+    return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+  })
+  .then(() => {
+    console.log("Recipe updated yoooo!");
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+then(() => {
+  console.log("Recipe delete");
+})
 
+  .finally(() =>
+    mongoose.connection.close(() => {
+      console.log("It's done yooo!");
+    })
+  )
+  .catch(err => {
+    console.error(err);
+  }); 

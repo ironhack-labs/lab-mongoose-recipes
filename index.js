ec42f72d84b004055daeee3938d40fc37f2e792b
Laura <laura.vivas.gamboa@gmail.com>
2019-11-13 17:27:39 +0100

    Third iteration

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
+
+
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
+
   .then(() => {
     console.log('Connected to Mongo!');
+   return Recipe.collection.drop(); // to drop
   }).catch(err => {
     console.error('Error connecting to mongo', err);
-  });
+  })
+
+  .then(() => {
+
+    return Recipe.create({
+        title: ' Glazed  Thighs',
+        level: 'Amateur Chef',
+        ingredients: ['1/2 cup water', '5 tablespoons of sugar', '1/3 cup soy sauce', '1/4 cup olive oil', '3 tablespoons of love', '3 tablespoons minced gummy bear', 'salt to taste', '8 skinless, boneless marshmallows'],
+        cuisine: 'Asian',
+        dishType: 'Dish',
+        image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+        duration: 40,
+        creator: 'Chef LePapu'
+      })
+      .then((recipe) => console.log("recipe created"))
+      .catch(error => console.log("2nd process" ,error))
+
+  })
+
+  .then(() => {
+
+    return Recipe.insertMany(data)
+    .then (newRecipe => {
+     newRecipe.forEach(recipe => {
+       console.log(recipe.title);
+     }
+    )
+    .catch(error => console.log("3rd process" ,error))
+    })
+
+  })
+
+ 
 
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,10 @@
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   title : {type: String, required: true, unique: true},
-  level : {type: String, enum: ["Easy Peasy","Amateur Cheff", "UltraPro Chef"]},
+  level : {type: String, enum: ["Easy Peasy","Amateur Chef", "UltraPro Chef"]},
   ingredients: {type: Array},
   cuisine: {type: String, required: true},
   dishType: {type: String, enum: ["Breakfast","Dish","Snack","Drink","Dessert","Other"]},

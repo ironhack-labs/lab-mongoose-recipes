ae198bc355e604b94400c6d0db2b6a5b55cf5756
Daniel Gonzalez <daniel.gonvie@gmail.com>
2019-11-13 18:28:31 +0100

    Finnally done

--- index.js
+++ index.js
@@ -6,7 +6,29 @@ const data = require('./data.js');  // Import of the data from './data.js'
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
+    return Recipe.collection.drop()
   }).catch(err => {
     console.error('Error connecting to mongo', err);
-  });
+  })
+
+  .then(() => {
+    return Recipe.create({
+      title: 'Lentejas',
+      level: 'Amateur Chef',
+      ingredients: ['lentejas', 'chorizo', 'cebolla', 'ajo'],
+      cuisine: 'Spanish',
+      dishType: 'Dish',
+      duration: 50,
+      creator: 'Daniel Vieitez'
+    })
+  }).then(recipe=>console.log(recipe.title))
+  .then(() => { return Recipe.insertMany(data)
+  }).then(recipes=>recipes.forEach(recipes => console.log(recipes.title)))
+  .then(() => {return Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, {duration: 100}).then(recipeUpdated => console.log("Success!"))
+  }).then(() => {return Recipe.deleteOne({title:"Carrot Cake"}).then(recipeRemoved => console.log("Success!"))})
+  .then(() =>  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination')}))  
+  
+  .catch(err=>console.log(err))
+ 
 

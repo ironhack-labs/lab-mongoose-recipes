fe477e0ca9888627f27cf1b9e55a82c06ce171fa
David Daniel Ibarra Reyes <DavidIbarra@MacBook-Pro-de-David.local>
2019-11-14 09:19:38 -0600

    done

--- index.js
+++ index.js
@@ -3,10 +3,43 @@ const Recipe = require('./models/Recipe'); // Import of the model Recipe from '.
 const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+   
+  return Recipe.create({
+    title: "Tartara",
+    level: "Pro",
+    ingredient: ["Galletas Maria", "Amor"],
+    cuisine: "La mia",
+    dishType: "Desayuno",
+    duration: 20,
+    creator: "David San"
   });
+})
+.then(everest => {
+  console.log("Recepta creada");
+  console.log(everest);
+  return Recipe.insertMany(data);
+})
+.then(() => {
+  return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+})
+.then(() => {
+  console.log("Exitooo esta actualizado!");
+  return Recipe.deleteOne({ title: "Carrot Cake" });
+})
+then(() => {
+console.log("Carrot Cake elminada");
+})
 
+.finally(() =>
+  mongoose.connection.close(() => {
+    console.log();
+  })
+)
+.catch(err => {
+  console.error(err);
+}); 

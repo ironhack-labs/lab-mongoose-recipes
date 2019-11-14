6699cae4dd63096d7a76fe1b7a7c2e0f6b54b7fb
Robert <robcg1102@gmail.com>
2019-11-14 02:37:31 -0600

    done

--- index.js
+++ index.js
@@ -6,7 +6,36 @@ const data = require('./data.js');  // Import of the data from './data.js'
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
+
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+setTimeout(function(){ 
+  Recipe.insertMany(data, { useNewUrlParser: true })
+      .then(title => { 
+    
+        data.forEach(element =>{
+          console.log("We add " + element.title)
+        })
+    
+      }).catch(err => { 
+        console.log('Error:', err); 
+}); }, 2000);
+
+setTimeout(function(){  
+  Recipe.updateOne({title: "Rigatoni alla Genovese" }, { duration: 100 })
+    .then(title => { 
+    
+      console.log("We updated the duration" )
+     
+    }).catch(err => { 
+      console.log('Error:', err); 
+}); }, 3000);
+
+setTimeout(function(){ 
+  Recipe.deleteOne({ title: "Carrot Cake" }, function (err) {});
+  console.log("We delete the Carrot Cake recipe")
+}, 5000);
+
 

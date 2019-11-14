c056961a9d118751307cc8b307169c7aaba6a78c
Berna <berna@email.com>
2019-11-14 03:27:47 -0600

    Done

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+mongoose
+	.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+	.then(() => {
+		console.log('Connected to Mongo!');
+	})
+	.catch((err) => {
+		console.error('Error connecting to mongo', err);
+	});
 
+Recipe.create({
+	title: 'A recipe',
+	level: 'Amateur Chef',
+	ingredients: [ 'Ingredient 1', 'Ingredient 2' ],
+	cuisine: 'International',
+	dishType: 'Dish',
+	duration: 120,
+	creator: 'Berna'
+})
+	.then((res) => {
+		console.log(res.title);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+Recipe.insertMany(data)
+	.then((res) => {
+		res.forEach((dish) => {
+			console.log(dish.title);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+	.then((res) => {
+		console.log('title updated');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+Recipe.deleteOne({ title: 'Carrot Cake' })
+	.then((res) => {
+		console.log('deleted');
+		mongoose.disconnect();
+	})
+	.catch((err) => {
+		console.log(err);
+	});

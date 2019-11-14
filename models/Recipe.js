748a2a75ce4eeb2c2bf0d7084466991213eb8c82
Boris Vogt <boris.vogt@posteo.de>
2019-11-14 18:31:16 +0100

    iteration 1 added

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+var uniqueValidator = require('mongoose-unique-validator');
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+    title: { type: String, required: true, unique: true},
+    level: { type: String, enum: [ 'Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+    ingredients: { type: [] },
+    cuisine: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Dessert', 'Other']},
+    image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+    duration: { type: Number, default: 0},
+    creator: { type: String},
+    created: { type: Date, default: Date.now}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
+recipeSchema.plugin(uniqueValidator);

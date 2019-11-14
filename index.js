cc133e313cf1266d30b603a5cf1a4caa4b0db0df
jcazorla2000 <jose.cazorla00@gmail.com>
2019-11-14 00:10:39 -0600

    DE-WDFT-MEX Jose Cazorla

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-//Recipe.create(data[0])
-//console.log(data[0].title)
+Recipe.create({
+  title: 'Asian Glazed Chicken Thighss',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  cuisine: 'Asian',
+  dishType: 'Dish',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 40,
+  creator: 'Chef LePapu'
+})
 
-//Recipe.findById("5dcce37608b90ba30ca3f3a4")
-
+console.log(Recipe.title)
 
 Recipe.insertMany(data)
+
+Recipe.findByIdAndUpdate("5dcceeb008bfcba5ed4c1bc8", {duration: 100})

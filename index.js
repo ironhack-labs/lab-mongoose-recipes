1b2959858ec39400674555528e71be19984e5df6
Johann <johanson88@gmail.com>
2019-11-14 23:19:35 +0100

    Iterations done

--- index.js
+++ index.js
@@ -9,4 +9,37 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+let recipe1 = {
+  title: 'Papas',
+  level: 'Easy Peasy',
+  ingredients: ['papas','water'],
+  cuisine: 'Canaria',
+  dishType: 'Dish',
+  duration: 60,
+  creator:'Johann',
+}
 
+Recipe.create(recipe1)
+.then(()=>{
+  console.log(recipe1.title);
+  Recipe.insertMany(data)
+  .then(result => {
+    result.forEach((recipe) =>{
+      console.log(recipe.title);
+    });
+    Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{$set:{"duration":100}})
+      .then(() => {
+        console.log('Recipe update succesful');
+        Recipe.deleteOne({title:'Carrot Cake'})
+        .then(() => {
+          console.log('Recipe deleted')
+          mongoose.connection.close()
+            .then(() => console.log('Connection Succesful'));
+        })
+        .catch((err) => console.error(err));        
+      })
+      .catch((err) => console.error(err));
+  })
+  .catch(err => console.error(err)); 
+  })
+.catch((err) =>console.error(err));

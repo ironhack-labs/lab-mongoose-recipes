71c355637847494e50a11069089021787733e63b
merrymustard <merryymustard@gmail.com>
2019-11-13 23:00:12 -0600

    finished

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+
+
+Recipe.create({
+  title: 'Asian Glazed Chicken Thighs',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  cuisine: 'Asian',
+  dishType: 'Dish',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 40,
+  creator: 'Chef LePapu'
+})
+console.log(Recipe.title);
+// db.recipe.find({}, {name:1})
+
+Recipe.insertMany(data);
+
+Recipe.updateOne({
+  title: "Rigatoni alla Genovese"
+}, {
+  duration: 100
+}).then(
+  console.log("duration changed")
+);
+
+Recipe.deleteOne({
+  title: "Carrot Cake"
+}).then(
+  console.log("Carrot Cake deleted")
+);

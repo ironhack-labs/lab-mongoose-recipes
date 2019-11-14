d3f62073ac8caf7b4399dc331c9d321deb87cb04
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2019-11-14 23:54:58 +0100

    done

--- index.js
+++ index.js
@@ -10,3 +10,75 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+  
+  
+  // Iteration 2 - Create a recipe
+
+      
+      let recipe1 = {
+          title: 'spanish omelette',
+          level: 'Easy Peasy',
+          ingredients: ['eggs', 'oil', 'potatos'],
+          cuisine: 'European',
+          dishType: 'Dish',
+          image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+          duration: 15,
+          creator: 'Casa Pepe'
+        }
+
+
+      Recipe.create( recipe1 , (err, result) => {
+        if (err) console.log(err);
+        else console.log('Document inserted', result);
+      }); 
+
+
+  //  Iteration 3 - Insert Many recipes
+
+
+        
+        Recipe.insertMany(data)
+          .then( (result) => console.log(result))
+          .catch(err=> console.log(err))
+        
+      
+        
+
+
+   // Iteration 4 - Update recipe
+
+   
+      
+    Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } })
+      .then((result) => console.log('duration successfuly updated', result))
+      .catch(err => console.log(err));
+      
+
+     
+      
+    // Iteration 5 - Remove a recipe
+
+      Recipe.deleteOne({ title: 'Carrot Cake'})
+        .then(( result) => console.log('recipe deleted succesfully',result))
+        .catch(err => console.log(err));
+        
+    
+
+         //  Checking if changes are made in BBDD 
+      
+      Recipe.find()
+      .then( (result) => console.log(result))
+      .catch(err=> console.log(err)); 
+      
+
+
+
+    // Iteration 6 - Close the Database
+
+    mongoose.connection.close()
+    .then(() => {
+      console.log('disConnected to Mongo!');
+    }).catch(err => {
+      console.error('Error disconnecting to mongo', err);
+    });

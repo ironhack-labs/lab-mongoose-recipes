c987bacb413a3290b528d478c5551efa068e0a56
Boris Vogt <boris.vogt@posteo.de>
2019-11-14 19:14:52 +0100

    finished

--- index.js
+++ index.js
@@ -10,3 +10,51 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+
+  let recipe1 = {
+    title: 'Asian Glazed',
+    level: 'Amateur Chef',
+    ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+    cuisine: 'Asian',
+    dishType: 'Dish',
+    image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+    duration: 40,
+    creator: 'Chef LePapu'
+  }
+
+  //2.
+  // Recipe.create( recipe1)
+  //   .then( (result) => console.log('Insertion succesful', recipe1.title))
+  //   .catch( err => console.log(err));
+
+    // 3.
+    // Recipe.insertMany(data)
+    //   .then( (result) => console.log('DATA Inserted -> ', data.title))
+    //   .catch( err => console.log(err));
+
+
+    //4.
+    const filter =  { title: 'Rigatoni alla Genovese'};
+    const update = { duration: 100};
+
+  Recipe.findOneAndUpdate( filter, update, {
+      new: true,
+  })
+  .then( (result) => console.log('Update Succesfull', filter))
+  .catch( err => console.log(err));
+
+  //5.
+  const filter2 = {title: 'Carrot Cake' }
+
+  Recipe.deleteOne( filter2 )
+  .then( (result) => console.log('Delete Succesfull', filter2))
+  .catch( err => console.log(err));
+
+
+  //6. 
+  process.on('SIGNIT', () => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose default connection disconnected ');
+        process.exit(0);
+    });
+})
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-var uniqueValidator = require('mongoose-unique-validator');
 
+
+//1.
 const recipeSchema = new Schema({
   // TODO: write the schema
     title: { type: String, required: true, unique: true},
     level: { type: String, enum: [ 'Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
     ingredients: { type: [] },
-    cuisine: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Dessert', 'Other']},
+    cuisine: { type: String, required: true},
+    dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Dessert', 'Other']},
     image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
     duration: { type: Number, default: 0},
     creator: { type: String},
@@ -17,4 +19,3 @@ const recipeSchema = new Schema({
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
 
-recipeSchema.plugin(uniqueValidator);

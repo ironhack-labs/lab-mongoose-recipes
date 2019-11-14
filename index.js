a358e3cacdc398bf7c1588c95fa17f72e2b81eb5
uaqro <uaqro@protonmail.com>
2019-11-13 20:46:53 -0600

    problems with mongo connection

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const Recipe = require('./models/Recipe'); // Import of the model Recipe from '.
 const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:127.0.0.1/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create(data[0])
+console.log(Recipe.title)
+Recipe.createMany(data)

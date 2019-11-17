273cdf5f29ff321a847cd9cae3db7b11b2137d91
Dennis H <deheeu@gmail.com>
2019-11-17 12:22:01 +0000

    DONE: fixed deleteAll, todo: see previous commit + how to order the console.log / order of running the operation, why does clearing all produce an orror and a success message + how to list everything that is in the database?

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ mongoose
   })
   .then(() => {
     console.log("Connected to Mongo!");
+    Recipe.deleteMany({})
+      .then(console.log("SUCCESS: database has been cleared"))
+      .catch(console.log("ERROR: database could not be cleared"));
     // Create 1 recipe(hardcode):
     Recipe.create({
       title: "Pie14",
@@ -21,17 +24,17 @@ mongoose
       creator: "me"
     })
       .then(document => {
-        console.log("We successfully created the recipe", document.title);
+        console.log("SUCCESS: created the recipe", document.title);
       })
       .catch(error => {
-        console.log("There was an error creating the recipe: ", 
+        console.log("ERROR: There was an error creating the recipe: ", 
         // error
         );
       });
     // Insert many recipes from data:
     Recipe.insertMany(data)
       .then(document => {
-        console.log("Successfully inserted the receipe array from data.js");
+        console.log("SUCCESS: inserted the receipe array from data.js");
       })
       .then(document => {
         for (i = 0; i < data.length; i++) {
@@ -40,7 +43,7 @@ mongoose
       })
       .catch(error => {
         console.log(
-          "There was an error inserting the receipe array from data.js: ",
+          "ERROR: There was an error inserting the receipe array from data.js: ",
           // error
         );
       });
@@ -51,24 +54,32 @@ mongoose
       })
       .catch(error => {
         console.log(
-          "There was an error updating Rigatoni alla Genovese: ",
+          "ERROR: There was an error updating Rigatoni alla Genovese: ",
           error
         );
       });
+    // deleteOne recipe:
     Recipe.deleteOne({ title: "Carrot Cake" })
       .then(document => {
         console.log("SUCCESS: removed: ", document.title);
       })
       .catch(error => {
         console.log(
-          "There was an error removing: Carrot Cake",
+          "ERROR: There was an error removing: Carrot Cake",
           error
         );
       });
+    // // print all documents in the collection:
+    // Recipe.find({})
+    //   .then(document => {
+    //     console.log(document.title);
+    //   })
+    //   .catch(error => {
+    //     console.log("ERROR listing all documents: ", error)
+    //   });
   })
   .catch(err => {
-    console.error("Error connecting to mongo", err);
+    console.error("ERROR: Error connecting to mongo", err);
   });
 
 // mongoose.connection.close()
-  

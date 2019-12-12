a35f031f74591bd38d8b7bcc6556dff4d7d51e6d
Anastasia Schmitke <schmitke@mediamarktsaturn.com>
2019-12-12 15:47:58 +0100

    Iteration 5 finished

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
+const Recipe = require('./models/Recipe'); // Import of the document model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
@@ -10,3 +10,37 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  let newRecipe = {
+    title: 'Bratkartoffeln',
+    level: 'Easy Peasy',
+    ingredients: ['Kartoffeln', 'Öl', 'Zwiebeln', 'Salz', 'Pfeffer'],
+    cuisine: 'German',
+    dishType: 'Dish',
+    image: 'https://www.lecker.de/assets/styles/563x422/public/field/image/bratkartoffeln-rezept-b_0.jpg?itok=6RDuUk1M',
+    duration: 30,
+    creator: 'Anastasia und Dirk'};
+
+Recipe.create(newRecipe)
+      .then(recipeFromDB =>{
+            console.log(`Created recipe: ${recipeFromDB.title}`);
+        }) 
+      .catch(err =>{
+            console.error(`Error while creating recipe: ${err} `);
+        });
+
+Recipe.insertMany(data)
+      .then(recipesFromDB =>{
+        recipesFromDB.map((v) => {console.log('Titel:', v.title);});
+          
+       }) 
+      .catch(err =>{
+         console.error(`Error while creating recipes: ${err} `);
+       });
+
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+      .then(recipesUpdatedInDB =>{
+        console.log(`Successfully updated duration from recipe: ${JSON.stringify(recipesUpdatedInDB)  } `);
+        })
+      .catch(err =>{
+        console.error(`Error while updating duration: ${err} `);
+      });

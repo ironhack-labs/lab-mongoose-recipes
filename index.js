845aa02cfb95f3d8f0d30615b2fec83a3f81a676
Mike Ta <h.michael.ta@gmail.com>
2019-12-12 17:56:17 +0100

    finished

--- index.js
+++ index.js
@@ -1,12 +1,92 @@
+const express = require("express");
+const app = express();
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+let options = { 
+  useNewUrlParser: true,  
+  useUnifiedTopology: true 
+};
+
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/recipeApp', options)
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+
+
+//Iteration 2 - create a recipe.
+Recipe.create({
+  title: 'King Prawn Noodles',
+  level: 'UltraPro Chef',
+  ingredients: ['Noodles', 'Prawns', 'Soya', 'Oil'],
+  cuisine: 'Asian',
+  dishType: 'Dish',
+  duration: 30,
+  creator: 'Michael Ta', 
+})
+
+.then((KingPrawnNoodles)=> {
+  console.log(`Created successfully`);
+})
+.catch((err)=> {
+  console.log("Err", err)
+})
+
+//Iteration 3
+Recipe.insertMany(data)
+  .then((recipe)=> {
+    console.log("Inserted successfully")
+    return Recipe.findOneAndUpdate({ "title": "Rigatoni alla Genovese"}, {"duration": 100}) //Iteration 4 - chaining promises to make it sync instead of async 
+  })
+  .then((recipe)=> {
+    console.log("Inserted successfully")
+    return Recipe.deleteOne({title: "Carrot Cake"}) //Iteration 5 - chaining promises to make it sync instead of async
+  })
+  .catch((error)=> {
+    console.log("Err", error)
+  })
+
+  // https://github.com/Piepongwong/async-javascript
+
+//Iteration 6
+mongoose.connection.close(() => {
+  console.log("Disconnected")
+})
+
+/*
+// Iteration 6 - necessary?
+mongoose.connection.close(() => {
+  console.log('Mongoose disconnected on app termination');
+  process.exit(0);
+});
+
+/*
+//Iteration 4
+Recipe.findOneAndUpdate({ "title": "Rigatoni alla Genovese"}, {"duration": 100})
+  .then((recipe)=> {
+    console.log("updated successfully")
+  })
+  .catch((error)=> {
+    console.log("Err", error)
+  })
+
+//Iteration 5
+Recipe.deleteOne({title: "Carrot Cake"})
+  .then((recipe) => {
+    console.log("Deleted successfully")
+  })
+  .catch((error)=> {
+    console.log("Err", error)
+  })
+
+app.listen(3000,()=> {
+  console.log("Webserver listening");
+})*/
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//Iteration 1 - Recipe Schema
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: { type: String, required: true, unique: true}, // It should be required and unique.
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] }, // Only can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef 
+  ingredients: { type: Array},
+  cuisine: { type:String, required: true}, // Should be required.
+  dishType: { type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'] }, // Possible values: Breakfast - Dish - Snack - Drink - Dessert - Other.
+  image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},  // Default value: https://images.media-allrecipes.com/images/75131.jpg.
+  duration: { type: Number, min: 0}, //Min value should be 0.
+  creator: { type: String},
+  created: { type: Date, default: Date.now()} // By default today.
 });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

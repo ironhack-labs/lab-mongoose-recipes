7858d12a49f7116886d49855b98f780804163119
Jorch39 <jorch.finanzas@gmail.com>
2019-12-15 01:21:37 -0600

    Lab accomplished

--- index.js
+++ index.js
@@ -10,3 +10,49 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+let newRecipe = {
+    title: 'Tacos de cochi',
+    level: 'Easy Peasy',
+    ingredients: ['Suadero', 'Chicharrón', 'Cebolla', 'Longaniza', 'Cochi'],
+    cuisine: 'Mexicana',
+    dishType: 'Dish',
+    image: 'https://images.media-allrecipes.com/images/75131.jpg',
+    duration: 10,
+    creator: 'Don Beto'};
+
+Recipe.create(newRecipe)
+	.then(recipeFromDB =>{
+	    console.log(`Created recipe: ${recipeFromDB.title}`);
+	}) 
+	.catch(err =>{
+	    console.error(`Error while creating recipe: ${err} `);
+	});
+
+
+Recipe.insertMany(data)
+	.then(recipesFromDB =>{
+	recipesFromDB.map((c) => {console.log('Title:', c.title);});
+
+	}) 
+	.catch(err =>{
+	 console.error(`Error while creating recipes: ${err} `);
+	});
+
+
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+	.then(recipesUpdatedInDB =>{
+	console.log(`Successfully updated duration from recipe: ${JSON.stringify(recipesUpdatedInDB)  } `);
+	})
+	.catch(err =>{
+	console.error(`Error while updating duration: ${err} `);
+	});
+
+
+Recipe.deleteOne({title: 'Carrot Cake'})
+	.then(recipesDeletedInDB =>{
+	console.log(`Successfully deleted from recipe: ${JSON.stringify(recipesUpdatedInDB)  } `);
+	})
+	.catch(err =>{
+	console.error(`Error while deleting: ${err} `);
+	});
+
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,7 +3,26 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+	title : { type: String,  required: true, unique: true} ,
+	level : { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+	ingredients: { type: []}, 
+	cuisine : { type: String, required: true}, 
+	dishType : { type: String, enum:['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+	image : { type: String, default:'https://images.media-allrecipes.com/images/75131.jpg'}, 
+	duration : { type: Number, min: 0}, 
+	creator : { type: String}, 
+	created : { type: Date, default: Date.now}
+	// countryCode: { type: String, match: /^[A-Z]{2}$/ },
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});

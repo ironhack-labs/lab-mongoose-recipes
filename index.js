cc0e1766a5e2956442ee0630a88fda31ca30688c
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2019-12-17 15:28:54 -0600

    Iteration 5

--- index.js
+++ index.js
@@ -29,8 +29,17 @@ Recipe.insertMany(data)
   })
   .catch(err => console.log(`An error happend: ${err}`));
 
+
+setTimeout(() => {
   Recipe.updateOne(
     {title: 'Rigatoni alla Genovese'}, 
     { duration: 100 })
   .then(recipe => console.log(`A recipe was updated:`))
   .catch(err => console.log(`An error happend: ${err}`));
+}, 1500);
+  
+setTimeout(() => {
+  Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(recipe => console.log(`A recipe was deleted:`))
+  .catch(err => console.log(`An error happend: ${err}`));
+}, 1500);

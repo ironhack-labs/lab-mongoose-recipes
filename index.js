9adfd7ce8a417efd0f350458cf7d42a517b9c549
Analin Flores <foglzerika@gmail.com>
2019-12-17 02:19:37 -0600

    Create a recipe

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
+Recipe.create({
+  title: "Cherios with milk",
+  level: "Easy Peasy",
+  ingredients: ["milk" , "cherios"],
+  cuisine: "Mix milk with cherios",
+  dishType: "Breakfast",
+  duration: 10,
+  creator: "Analin Flores"
+
+})
+.then (recipe =>{ console.log(`New recipe created ${recipe.title}`)})
+.catch(err => {console.log(`An error happend ${err}`)})
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {

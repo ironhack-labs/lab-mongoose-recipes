124bfce8ac2dcdde26cd33f360f8516bffaff239
Ardash <56517452+ardache@users.noreply.github.com>
2019-12-17 12:03:38 -0600

    Iteration 3 - Insert Many recipes Done!
    Agregue varios metodos para hacer el insert (via schema, con promesa o con funcion de control de errores)

--- index.js
+++ index.js
@@ -9,25 +9,84 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-  Recipe 
-    //poner los campos y valores aa creae
-    function addNewRecipie(){
-      const rcp = new Recipe({
-        title: 'Pozole',
-        level: 'UltraPro Chef',
-        ingredients: ['5lt Agua', '2 Kg de Carne de Cerdo', '3 Kg Maiz','5 Lechugas', '1/2 Kg Oregano','3 Kg de limones','300 gr de sal','1 cda de acite','1/2 Kg de rabanos', '300 gr de chiles'],
-        cuisine: 'Mexicana',
-        dishType: 'Other',
-        duration: '360',
-        creator: 'La Abuela'
+// ::::promise Version ::::
 
-      })
-      rcp
-        .save()
-        .then(create => console.log(`A new recipie has been created: ${create}!`))
-  //   //.then (() => showCats())
-        .catch(err => console.log(`Error while creating a new recipie: ${err}`));
-    }
+Recipe.create({
+  title: 'Pozole',
+  level: 'UltraPro Chef',
+  ingredients: ['5lt Agua', '1 Kg de Carne de Cerdo', '3 Kg Maiz','5 Lechugas', '1/2 Kg Oregano','3 Kg de limones','300 gr de sal','1 cda de acite','1/2 Kg de rabanos', '300 gr de chiles'],
+  cuisine: 'Mexicana',
+  dishType: 'Other',
+  duration: '410',
+  creator: 'Le champs'
+})
+.then(dato => { console.log('The recipe has been saved with next values: ', dato) })
+.catch(err => { console.log('An error happened:', err) });
+
+
+Recipe.insertMany([{
+  title: 'Huevos Benedictinos',
+  level: 'UltraPro Chef',
+  ingredients: ['5 huevos', '1 lt de Agua', '100 ml de Vinagre blanco', '5 panes tostados','Salsa Holandesa','10 gr de sal'],
+  cuisine: 'Mexicana',
+  dishType: 'Breakfast',
+  duration: '45',
+  creator: 'Chef Ardash'},
+  {title: 'Chicharron en salsa Verde',
+  level: 'Easy Peasy',
+  ingredients: ['1lt Agua', '1 Kg de Chicharron', '2 Kg tomate verde', '5 Chiles', '1 Kg de Tortillas','3 cebollas', '1 cda de sal'],
+  cuisine: 'Mexicana',
+  dishType: 'Other',
+  duration: '40',
+  creator: 'Chef Ardash'},
+  {title: 'Lentejas',
+  level: 'Amateur Chef',
+  ingredients: ['1lt Agua', '1 Kg de lentejas', '2 Kg jitomate', '1 cda de sal'],
+  cuisine: 'Mexicana',
+  dishType: 'Other',
+  duration: '35',
+  creator: 'Chef Kaflo'}])
+.then(dato => { console.log('The user is saved with next values: ', dato) })
+.catch(err => { console.log('An error happened:', err) });
+
+
+
+ // // :::::promise mix version::::
+  // Recipe 
+  //   //poner los campos y valores aa creae
+  //   function addNewRecipie(){
+  //     const rcp = new Recipe({
+  //       title: 'Pozole',
+  //       level: 'UltraPro Chef',
+  //       ingredients: ['5lt Agua', '2 Kg de Carne de Cerdo', '3 Kg Maiz','5 Lechugas', '1/2 Kg Oregano','3 Kg de limones','300 gr de sal','1 cda de acite','1/2 Kg de rabanos', '300 gr de chiles'],
+  //       cuisine: 'Mexicana',
+  //       dishType: 'Other',
+  //       duration: '360',
+  //       creator: 'La Abuela'
+
+  //     })
+  //     rcp
+  //       .save()
+  //       .then(create => console.log(`A new recipie has been created: ${create}!`))
+  //       .catch(err => console.log(`Error while creating a new recipie: ${err}`));
+  //   }
   
-    addNewRecipie();
+  //   addNewRecipie();
   
+// //  ::::Create version:::::
+// Recipe.create({
+//     title: 'Pozole',
+//     level: 'UltraPro Chef',
+//     ingredients: ['5lt Agua', '1 Kg de Carne de Cerdo', '3 Kg Maiz','5 Lechugas', '1/2 Kg Oregano','3 Kg de limones','300 gr de sal','1 cda de acite','1/2 Kg de rabanos', '300 gr de chiles'],
+//     cuisine: 'Mexicana',
+//     dishType: 'Other',
+//     duration: '410',
+//     creator: 'La Patrona'
+//   }, function (err, user){
+//       if (err) {
+//         console.log('An error happened:', err);
+//       } else {
+//         console.log('The user is saved and its value is: ', user);
+//       }
+//         });
+

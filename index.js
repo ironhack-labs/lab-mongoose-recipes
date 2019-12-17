fe2b416d3889bf46c0beb74f71b3446fc321bb7f
Ardash <56517452+ardache@users.noreply.github.com>
2019-12-17 12:51:09 -0600

    Iteration 4 - Update recipe Done!
    updateone y updatemany

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ Recipe.create({
   ingredients: ['5lt Agua', '1 Kg de Carne de Cerdo', '3 Kg Maiz','5 Lechugas', '1/2 Kg Oregano','3 Kg de limones','300 gr de sal','1 cda de acite','1/2 Kg de rabanos', '300 gr de chiles'],
   cuisine: 'Mexicana',
   dishType: 'Other',
-  duration: '410',
+  duration: 410,
   creator: 'Le champs'
 })
 .then(dato => { console.log('The recipe has been saved with next values: ', dato) })
@@ -30,21 +30,21 @@ Recipe.insertMany([{
   ingredients: ['5 huevos', '1 lt de Agua', '100 ml de Vinagre blanco', '5 panes tostados','Salsa Holandesa','10 gr de sal'],
   cuisine: 'Mexicana',
   dishType: 'Breakfast',
-  duration: '45',
+  duration: 45,
   creator: 'Chef Ardash'},
   {title: 'Chicharron en salsa Verde',
   level: 'Easy Peasy',
   ingredients: ['1lt Agua', '1 Kg de Chicharron', '2 Kg tomate verde', '5 Chiles', '1 Kg de Tortillas','3 cebollas', '1 cda de sal'],
   cuisine: 'Mexicana',
   dishType: 'Other',
-  duration: '40',
+  duration: 40,
   creator: 'Chef Ardash'},
   {title: 'Lentejas',
   level: 'Amateur Chef',
   ingredients: ['1lt Agua', '1 Kg de lentejas', '2 Kg jitomate', '1 cda de sal'],
   cuisine: 'Mexicana',
   dishType: 'Other',
-  duration: '35',
+  duration: 35,
   creator: 'Chef Kaflo'}])
 .then(dato => { console.log('The user is saved with next values: ', dato) })
 .catch(err => { console.log('An error happened:', err) });
@@ -90,3 +90,7 @@ Recipe.insertMany([{
 //       }
 //         });
 
+let query = {creator: 'Le champs'}
+Recipe.updateMany(query, {duration: 90})
+.then(dato => { console.log('The data has been UPDATED with next value: ', dato) })
+.catch(err => { console.log('An error happened:', err) });

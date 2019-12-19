8e49b3f55a84f90268d8b5a2b9cad956a329846d
Maxwell Martins Lima Monteiro <maxwba@AbelardosiPhone.localdomain>
2019-12-19 14:24:00 -0300

    Complete

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+Recipe.create({
+  title: "Asian Glazed Chicken Thighs",
+  level: "Amateur Chef",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  cuisine: "Asian",
+  dishType: "Dish",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 40,
+  creator: "Chef LePapu"
+})
+  .then(() => {
+    console.log("Sucesso");
+  })
+  .catch(() => {
+    console.log("Deu ruim");
+  });
+
+Recipe.insertMany(data)
+  .then(param => {
+    console.log(param);
+  })
+  .catch(() => {
+    console.log("Se fudeu!");
+  });
+
+Recipe.findByIdAndUpdate("5dfbad0485e619a40df30289", { duration: 100 })
+  .then("Sucesso")
+  .catch("Deu ruim");
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+.then("Sucesso")
+.catch("Deu ruim!")
+
+mongoose.connection.close(() => {
+  console.log('Valeuu farouuu!')
+})
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String },
+  level: { type: String, enum: ['Easy Peasy','Amateur Chef','UltraPro Chef']},
+  ingredients: { type: Array,}, // Nao sei se funciona
+  cuisine: { type: String },
+  dishType: { type: String },
+  image: { type: String },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, value: Date.now } // Nao sei se funciona
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;

fda4fecfa05d2e53098e24422cc238e4ace88186
Reginaldo Barbosa <reginaldobr@gmail.com>
2019-12-19 14:14:10 -0300

    2nd interation

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+let newObj = {
+  title: 'Bolo de Caju',
+  level: 'UltraPro Chef',
+  ingredients: 'flour, caju, eggs, milk and lucky',
+  cuisine: 'North of Brazil',
+  dishType: 'Other',
+  duration: 2,
+  creator: 'Unknown',
+};
+
+Recipe.create(newObj)
+  .then(e => console.log(e))
+  .catch(error => console.error('Error', error));

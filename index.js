caea6d92a6d69f72eaf83ea0e8e0ef026d8da111
Fabio SantAnna <sgfabio@yahoo.com>
2019-12-19 15:03:48 -0300

    DONE until Iteration 6

--- index.js
+++ index.js
@@ -10,3 +10,31 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  // Recipe.create({title: 'Bolinho de bacalhau3', level: 'Amateur Chef',ingredients: ['Bacalhau','Farinha','Ovo'],cuisine:'Portuguesa',dishType:'Snack', 
+  // image: 'https://images.media-allrecipes.com/images/75131.jpg', duration: 30, creator: 'Vini e Fabio'})
+  //   .then(recipe => { console.log(`The recipe is saved and its value is:${title}`) })
+  //   .catch(err => { console.log('An error happened:', err) });
+
+  //   console.log(Recipe)
+
+  // Recipe.insertMany(data)
+  //   .then(recipe => { console.log(`The recipe is saved and its value is:${title}`) })
+  //   .catch(err => { console.log('An error happened:', err) });
+
+    // console.log(Recipe)
+
+    // Recipe.findByIdAndUpdate("5dfbb389c5f82e536f005f95",{duration: 100})
+    // .then(recipe => { console.log(`The document  has been updated is saved and its value is:${duration}`) })
+    // .catch(err => { console.log('An error happened:', err) });
+
+    // Recipe.updateOne({title:"Rigatoni alla Genovese"},{duration:100})
+    // .then(recipe => { console.log(`The document  has been updated is saved and its value is:${duration}`) })
+    // .catch(err => { console.log('An error happened:', err) });
+
+    // Recipe.deleteOne({ title: 'Carrot Cake' })
+    // .then(recipe => { console.log(`The recipe is deleted`) })
+    // .catch(err => { console.log('An error happened:', err) });
+
+    mongoose.connection.close()
+    // .then(recipe => { console.log(`Closed`) })
+    // .catch(err => { console.log('An error happened:', err) });
--- models/Recipe.js
+++ models/Recipe.js
@@ -2,8 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title : {type: String, required: true, unique: true},
+  level : {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']}, 
+  ingredients: {type: Array},
+  cuisine : {type: String, required: true},
+  dishType : {type: String, enum : ['Breakfast', 'Dish', 'Snack', 'Drink',  'Dessert', 'Other']},
+  image : {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration : { type: Number, min: 0},
+  creator : { type: String},
+  created: {type: Date, default: Date.now},
 });
+
+// recipe.create({title: 'Bolinho de bacalhau', level: 'Amateur Chef',ingredients: ['Bacalhau','Farinha','Ovo'],cuisine:'Portuguesa',dishType:'Snack', image: 'https://images.media-allrecipes.com/images/75131.jpg'
+// duration: 30, creator: 'Vini e Fabio'}, function (err,recipe){
+//     if(err) {
+//       console.log('Erro')
+//     } else {
+//       console.log(`O nome da receita é ${title}`)
+//     }
+// });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

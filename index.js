306d24a83d8b47d76aab7c83def2d4076df3f45d
Leonardo Polon <lpolon@gmail.com>
2019-12-19 14:53:31 -0300

    Done

--- index.js
+++ index.js
@@ -12,15 +12,49 @@ mongoose
     console.error('Error connecting to mongo', err);
   });
 
+// create()
+
+const miojoRecipe = {
+  title: 'MiojoScript',
+  level: 'UltraPro Chef',
+  ingredients: ['miojo'],
+  cuisine: 'niponic',
+  dishType: 'Dish',
+  duration: 3,
+  creator: 'Chef Fernando',
+}
+
+Recipe.create(miojoRecipe).then( createReturnOfOnFulfilled => console.log('new recipe: ', createReturnOfOnFulfilled)).catch((err) => console.log(err))
+
+// seed
 const handleFulfiledProm = theArrayIamExpecting => {
   theArrayIamExpecting.forEach(e => console.log(e));
 };
 
-// Recipe.create()
-Recipe.insertMany(data)
+const prom1 = Recipe.insertMany(data)
   .then(handleFulfiledProm)
-  .catch((err) => console.log('erro: ', err))
+  .catch(err => console.log('erro: '));
 
-Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
-.then( (toma) => console.log('success!', toma))
-.catch( (err) => console.log(err))
+// update
+const prom2 = Recipe.updateOne(
+  { title: 'Rigatoni alla Genovese' },
+  { duration: 100 }
+)
+  .then(toma => console.log('update successul!', toma))
+  .catch(err => console.log(err));
+
+// delete
+const prom3 = Recipe.deleteOne({ title: 'Carrot Cake' })
+  .then(fullfiledDeleteOne => console.log('deleted: ', fullfiledDeleteOne))
+  .catch(err => console.log(err));
+
+Promise.all([prom1, prom2, prom3])
+  .then(() => {
+    mongoose.connection.close()
+    .then(() => console.log('conexao fechada'));
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+// nao pode deixar sincrono.
+// mongoose.connection.close()

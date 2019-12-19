738267e8eba7b6c864979934a573dc5a2dfbe352
henricasi <henriquecmandruzato@gmail.com>
2019-12-19 14:25:19 -0300

    Finish exercise

--- index.js
+++ index.js
@@ -10,3 +10,36 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// Iteration 2
+Recipe.create({
+  title: 'Brigadeiro Especial',
+  level: 'Easy Peasy',
+  ingredients: ['Leite Condensado', 'Chocolate em pó', 'Leite de coco'],
+  cuisine: 'Brasileira',
+  dishType: 'Dessert',
+  duration: 25,
+  creator: 'Brigadeiro Luis Antônio',
+  created: new Date(1946)}) //isso gera a data 1970-01-01
+  .then(recipe => console.log(recipe.title))
+  .catch(error => console.log(error));
+
+// Iteration 3
+Recipe.insertMany(data)
+.then((recipes) => {
+  recipes.forEach(recipe => console.log(recipe.title));
+  // Iteration 4
+  Recipe.updateOne({name: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then(() => console.log('Duration updated'))
+  .catch(error => console.log(error));
+  // Iteration 5
+  Recipe.deleteOne({name: 'Carrot Cake'})
+  .then(() => {
+    console.log('Cake deleted');
+    // Iteration 6
+    console.log('All tasks done, closing database connection...')
+    mongoose.connection.close();
+    console.log('Connection closed');
+  })
+  .catch(error => console.log(error));  
+})
+.catch(error => console.log(error));

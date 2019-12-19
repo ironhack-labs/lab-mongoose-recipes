f9c4a9a5a51e2df936f553674011b6451364c5c7
Leonardo Polon <lpolon@gmail.com>
2019-12-19 13:00:03 -0300

    iteration 3 done

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
-  }).catch(err => {
+  })
+  .catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+const handleFulfiledProm = theArrayIamExpecting => {
+  theArrayIamExpecting.forEach(e => console.log(e));
+};
+
+// Recipe.create()
+Recipe.insertMany(data)
+  .then(handleFulfiledProm)
+  .catch((err) => console.log('erro: ', err))
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+.then( (toma) => console.log('success!', toma))
+.catch( (err) => console.log(err))

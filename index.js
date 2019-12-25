432190c90743854e6c6b55f97a65e08a5de968e6
Bialas <bialaska@media-saturn.com>
2019-12-25 18:47:02 +0100

    changes after  recap & iteration 6

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
-mongoose.set('useCreateIndex', true);
 
 // Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
@@ -11,50 +10,66 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
-  /* Recipe.create({  
-      title: 'Bigos',       
-      level: 'UltraPro Chef', 
-      ingredients: ['Sauerkraut', 'Cabbanosi','Fleisch', 'Tomaten', 'Salz', 'Pfeffer', 'Rotwein'],
-      cuisine: 'Polish',
-      dishType: 'Dish', 
-      //image: '', 
-      duration: 4,  
-      creator: '',
-    //created:   
-  
+// Connection monitoring
+mongoose.connection.on('connected', () => console.log('Mongoose default connection open'));
+mongoose.connection.on('error', (err) =>  console.log(`Mongoose default connection error: ${err}`));
+mongoose.connection.on('disconnected', () => console.log('Mongoose default connection disconnected'));
+
+
+
+//Iteration 2 - Create a recipe
+const createRecipe = Recipe.create({
+    title: "Bigos",
+    level: 'Easy Peasy',
+    ingredients: ['smoked bacon', 'onions', 'Polish Sausage', 'beef stew meat', 'mushrooms', 'carrots', 'sauerkraut', 'cabbage', 'herbs'],
+    cuisine: 'Polish',
+    dishType: 'Dish',
+    image: 'https://cdn.pixabay.com/photo/2014/09/14/14/19/bigos-445017_1280.jpg',
+    duration: 120,
+    creator: 'Tomasz & Karin'    
   })
-    .then(doc => {
-      console.log(`Created recipe: ${doc.title}`);
-  }).catch(err => {
-      console.log(`Error at creating recipe ${err}`);
-  }); */
+  .then(recipe => { console.log('Recipe is saved and its title is: ', recipe.title) })
+  .catch(err => { console.log('An error at creating recipe happened:', err) });
 
-/* console.log(data)  
-  Recipe.insertMany(data)  
-  .then(doc => {
-    doc.forEach(e => console.log(e.title))
-    //console.log(`inserted recipes: ${doc.title}`);
-}).catch(err => {
-    console.log(`Error at inserting recipes ${err}`);
-}); */
+//Iteration 3 - Insert Many recipes
+const createManyRecipes = Recipe.insertMany(data)
+  .then(data => {
+      console.log('Recipes are saved and its titles are: ');
+      data.forEach(elem => {
+        console.log(elem.title)
+      });
+    })
+  .catch(err => { console.log('An error at inserting recipes happened:', err) });
+
+//Iteration 4 - Update recipe
+/* const RecipeUpdate = Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+  .then(() => { console.log('Duration of "Rigatoni alla Genovese" was updated successfully'); })
+  .catch(err => { console.log('An error happened:', err); }); */
 
 
+//Iteration 5 - Remove a recipe
+/* const deleteRecipe = Recipe.deleteOne({title: "Carrot Cake"})
+  .then((result) => { console.log('Recipe deleted status', result); })
+  .catch(err => { console.log('An error happened:', err); })
+  //.finally(() => { mongoose.connection.close() }); // Not working here (asynchronous process) */
 
-/* Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})  
-  .then(doc => {
-    console.log(`update duration successful`);    
-}).catch(err => {
-    console.log(`Error in update ${err}`);
-}); */
-  
-Recipe.deleteOne({title: 'Bigos'})  
-  .then(doc => {
-    console.log(`delete was successful`);    
+//Iteration 4 - Update recipe &  Iteration 5 - Remove a recipe
+let updateRecipeDB = createManyRecipes
+  .then(() => {
+    Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+      .then(() => { console.log('Duration of "Rigatoni alla Genovese" was updated successfully') })
+      .catch(err => { console.log('An error happened:', err) });
   })
-  .catch(err => {
-    console.log(`Error in delete ${err}`);
+  .then(() => {
+    Recipe.deleteOne({title: "Carrot Cake"})
+      .then((result) => { console.log('Recipe deleted status', result) })
+      .catch(err => { console.log('An error happened:', err) });
   })
-  .finally(() => {
-    mongoose.connection.close()
-  });
+  .catch(err => { console.log('An error happened:', err) });
 
+  // Iteration 6 - Close the Database
+Promise.all([createRecipe, createManyRecipes, updateRecipeDB])
+  .then(() => {      
+    mongoose.connection.close();
+  })
+  .catch(err => console.error(err));
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,32 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//Iteration 1 - Recipe Schema
 const recipeSchema = new Schema({
   // TODO: write the schema
-    title: {
-      type: String,
-      required: true,
-      unique: true    
-    }, 
-    level: {
-      type: String,
-      enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']     
-    }, 
-    ingredients: {
-      type: Array
-    },
-    cuisine: {
-      type: String,
-      required: true
-    },
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: Array
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
   dishType: {
     type: String,
-    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
-  }, 
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other' ]
+  },
   image: {
     type: String,
     default: 'https://images.media-allrecipes.com/images/75131.jpg'
-  }, 
+  },
   duration: {
     type: Number,
     min: 0
@@ -36,8 +37,9 @@ const recipeSchema = new Schema({
   },
   created: {
     type: Date,
-    default: Date()
-  } 
+    default: Date.now
+    //default: Date()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

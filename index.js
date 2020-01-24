bc6f83531d6e867f994770e26b1d381c8577617e
Kevin Medina <kmedinakm@hotmail.com>
2020-01-24 12:18:34 -0500

    complete iteration 2

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const Recipe = require('./models/Recipe'); // Import of the model Recipe from '.
 const data = require('./data.js');  // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+ })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  // create a recipe
+  Recipe.create(data)
+  .then(recipe => console.log(recipe))
+  .catch(err => console.log(`An error occurred: ${err}`));
 

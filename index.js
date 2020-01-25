96869c9c124b8b214742c3db95b02ba148a2dd99
Michel Maia <dimermichel@gmail.com>
2020-01-25 11:15:55 -0500

    Done

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
+});
+
+//  Iteration 2 - Create a recipe
+// .create() is a mongoose method and under the hood uses insertOne and insertMany
+// Recipe.create(data[Math.floor(Math.random() * data.length)])
+// .then(recipeData => console.log(`Recipe create worked well: ${recipeData}`))
+// .catch(err =>
+//   console.log(`Creating a new recipe went wrong! Try again 😞 ${err}`)
+// );
+
+//  Iteration 3 - Insert Many recipes
+// .create() is a mongoose method and under the hood uses insertOne and insertMany
+// Recipe.create(data)
+// .then(recipeData => console.log(`Recipe create worked well: ${recipeData}`))
+// .catch(err =>
+//   console.log(`Creating a new recipe went wrong! Try again 😞 ${err}`)
+// );
+
+// Iteration 4 - Update recipe
+// Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+//   .then(recipeData => console.log(`Recipe update worked well: ${recipeData}`))
+//   .catch(err => console.log(`Updating the recipe went wrong! Try again 😞 ${err}`));
+
+// Iteration 5 - Remove a recipe
+// Recipe.deleteOne({ title: 'Carrot Cake' })
+//   .then(recipeData => console.log(`Recipe deleted worked well: ${recipeData}`))
+//   .catch(err => console.log(`Deleting the recipe went wrong! Try again 😞 ${err}`));
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () =>
+  console.log('Mongoose default connection disconnected')
+);
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
   });
+});
+
 
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,6 +3,52 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+// title. Type String. It should be required and unique.
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+// level. Type String. Only can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the ENUM 😉)
+  level: {
+    level: { 
+      type: String, 
+      enum : ['Easy Peasy','Amateur Chef', 'UltraPro Chef'],       
+      },     
+  },
+// ingredients. Type Array.
+  ingredients: {
+    type: Array
+  },
+// cuisine. Type String. Should be required.
+  cuisine: {
+    type: String,
+    required: true
+  },
+// dishType. Type String. Possible values: Breakfast - Dish - Snack - Drink - Dessert - Other.
+  dishType: {
+    type: String,
+    enum: ['Breakfast','Dish','Snack','Drink','Dessert','Other']
+  },
+// image. Type String. Default value: https://images.media-allrecipes.com/images/75131.jpg.
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+// duration. Type Number. Min value should be 0.
+  duration: {
+    type: Number,
+    min: 0
+  },
+// creator. Type String
+  creator: {
+    type: String
+  },
+// created. Type Date. By default today.
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

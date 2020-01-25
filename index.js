933402eb41e20262ae747a237856c658d62adb61
Stanley Fils <stanleyfils@gmail.com>
2020-01-25 08:55:18 -0500

    Completed iterations 1-3

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('Connected to Mongo!');
-  }).catch(err => {
+  })
+  .catch(err => {
     console.error('Error connecting to mongo', err);
   });
 
+
+//ITERATION 2: Correct???
+recipe.create({
+    recipe
+  })
+  .then(recipe => console.log(`Title of Recipe: ${recipe.title}`, recipe))
+  .catch(error =>
+    console.log('An error occured while saving your new recipe:', error)
+  );
+
+
+//ITERTION 3:
+recipe.insertMany({
+    recipe
+  })
+  .then(recipe => console.log(`Title of Recipe: ${recipe.title}`, recipe))
+  .catch(error =>
+    console.log('An error occured while saving your new recipe:', error)
+  );

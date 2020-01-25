4fd9d612e3f00137f00bb5e710a1459a373a2b37
Michaela DeCamara <michaela.decamara@gmail.com>
2020-01-24 23:53:48 -0500

    first iteration

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({
+    title: 'Goulash',
+    level: 'UltraPro Chef',
+    ingredients: ['3 1/2 pounds boneless beef, cut into small pieces', '1 tablespoon freshly ground black pepper', '1 tablespoon kosher salt, or more to taste', '2 tablespoons vegetable oil', '2 teaspoons ground cumin', '1/4 teaspoon cayenne pepper'],
+    cuisine: 'Hungarian',
+    dishType: 'Dish',
+    image: 'https://kuchynalidla.sk/img/SK/653x414/20190710064142-10263%2011592%20-%20lidl%20Kuchyna%20KW29%20OBRAZOK%20HLAVNY%20RECEPT%20653x414.jpg',
+    duration: 240,
+    creator: 'Chef Michaela'
+});
+
+.then(() => console.log("Recipe succesfully created!")
+.catch(console.log("Error"))
+
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,45 +1,42 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;
 
-title: {
-  type: String,
-  required: true
-},
+const recipeSchema = new Schema({
 
-level: {
-  type: String,
-  value: [Easy Peasy, Amateur Chef - UltraPro Chef]
-},
- ingredients: {
-   type: []
- },
- cuisine: {
-  type: String,
-  required: true
- },
- dishType: {
-  type: String,
-  values: Breakfast - Dish - Snack - Drink - Dessert - Other
- },
- image: {
-   default: https://images.media-allrecipes.com/images/75131.jpg
- },
- duration: {
-   type: Number, 
-   min: 0
- },
- creator: {
-   type: String
- },
-created: {
-  type: Date,
-  default: Date.now
-}
-
+      title: {
+        type: String,
+        required: true
+      },
+      level: {
+        type: String,
+        value: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+      },
+      ingredients: {
+        type: []
+      },
+      cuisine: {
+        type: String,
+        required: true
+      },
+      dishType: {
+        type: String,
+        values: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']
+      },
+      image: {
+        default: https//images.media-allrecipes.com/images/75131.jpg
+      },
+      duration: {
+        type: Number,
+        min: 0
+      },
+      creator: {
+        type: String
+      },
+      created: {
+        type: Date,
+        default: Date.now
+      })

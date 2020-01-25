97fc59da888541762da181fc9506ab71168dcff4
Kevin Medina <kmedinakm@hotmail.com>
2020-01-24 19:25:37 -0500

    complete iterations 4-6

--- index.js
+++ index.js
@@ -14,13 +14,39 @@ mongoose.connect('mongodb://localhost/recipeApp', {
     console.error('Error connecting to mongo', err);
   });
 
-  // create a recipe
-  Recipe.create(data)
-  .then(recipe => console.log(recipe))
-  .catch(err => console.log(`An error occurred: ${err}`));
+  // Iteration 2 create a recipe
+  Recipe.create({
+    title: 'Oreo Sundae',
+    level: 'Amateur Chef',
+    ingredients: ['ice cream', 'oreo', 'hot fudge', 'whip cream'],
+    cuisine: 'Cream',
+    dishType: 'Dessert',
+    duration: 6,
+    creator: 'Kevin'
+  })
+  .then(recipe => console.log(`${recipe.title} was successfully added: 
+  ${recipe}`))
+  .catch(err => console.log(err));
 
+  // Iteration 3 Insert many recipes 
   Recipe.insertMany(data)
-  .then(recipe => console.log(recipe))
+  .then(recipes => {
+    recipes.forEach(recipe => console.log(recipe.title))
+  })
   .catch(err => console.log(`An error occured: ${err}`));
 
+  // Iteration 4 Update recipe
+  Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  .then(() => console.log(`The duration was successfully updated!`))
+  .catch(err => console.log(`An error occured: ${err}`));
+
+  // Iteration 5 Remove a recipe
+  Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(recipe => console.log(recipe, 'deleted'))
+  .catch(err => console.log(err));
+
+  // Iteration 6 Close the database
+  mongoose.connection.close(() => {
+    console.log('Connection closed!');
+  });
 

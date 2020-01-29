b82a917e066bb36c6f5f704994918a66373700b2
Remy Pham <remy.8P@gmail.com>
2020-01-29 17:00:18 +0100

    done

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+let japanCurryRecipe = [
+  {
+    title: "Japanese Curry",
+    level: "Easy Peasy",
+    ingredients: ["Beef", "Water", "Rice", "Curry mix"],
+    cuisine: "Japanese",
+    dishType: "Dish",
+    duration: 30,
+  }
+]
+
+
+Recipe.create(japanCurryRecipe, function(error, res){})
+
+Recipe.insertMany(data, function(error, docs){})
+
+Recipe.update({title: "Rigatoni alla Genovese"}, {duration: 100})
+  .then(res => console.log("Success!"))
+  .catch(err => console.log("Failed!"))
+
+
+  Recipe.deleteOne({title: "Carrot Cake"})
+    .then(res => console.log("Carrot Cake deleted!"))
+    .catch(err => console.log("ERROR"))
+
+    mongoose.disconnect()

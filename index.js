99369c4988703ff904d76049e831d8e43bd388b1
Pascal Besson <pascalbes@gmail.com>
2020-01-29 16:59:40 +0100

    [PAR-WDFT]-PASCAL-BESSON

--- index.js
+++ index.js
@@ -9,5 +9,42 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+
+    //Création d'une entrée recette
+    const recipeNew = {
+    title : "Parmentier de canard",
+    level: "Amateur Chef",
+    ingredients: ["canard", "pomme de terre", "pomme de terre douce"],
+    cuisine: "FRANCAISE MOSSIEUR!",
+    dishType: "Dish",
+    duration: 30,
+    creator: "Clémence Reiter"
+    }
+
+    Promise.all([Recipe.create(recipeNew), Recipe.insertMany(data)])
+    .then(x => {
+
+      Promise.all([Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 }), Recipe.deleteOne({ title: 'Carrot Cake' })])
+      .then(x => {
+        console.log("connection closed");
+        mongoose.connection.close();
+      })
+      .catch(err => console.error('Error disconnecting from mongo', err));
+      
+    })
+    .catch(err => console.error('Error in createOne or insertMany', err));
+  })
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+
+
+  
+
+
+
+  
+
+  

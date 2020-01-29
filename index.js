ce35736ce22fffcc1dac73fa036834459a16d420
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-01-29 17:23:06 +0100

    [PAR-WDFT0120] Yannick Bourenane

--- index.js
+++ index.js
@@ -2,6 +2,53 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require('./data.js'); // Import of the data from './data.js'
 
+function createRecipe(title, level, ingredients, cuisine, dishType, image, duration, creator, created) {
+  Recipe.create({
+    title,
+    level,
+    ingredients,
+    cuisine,
+    dishType,
+    image,
+    duration,
+    creator,
+    created
+  }).then(res => {
+    console.log(title);
+    mongoose.disconnect();
+  }).catch(err => console.log(err));
+}
+
+function insertManyRecipe(arr) {
+  Recipe.insertMany(arr).then(res => {
+    arr.map(element => console.log(element.title))
+    mongoose.disconnect();
+  }).catch(err => console.log(err));
+}
+
+function updateRecipeDuration(id, newDuration) {
+  Recipe.findByIdAndUpdate(id, {
+    duration: newDuration
+  }).then(res => {
+    console.log("Recipe updated !");
+    console.log(res);
+    mongoose.disconnect();
+  }).catch(err => console.log(err))
+}
+
+function deleteRecipe(title) {
+  Recipe.deleteOne({
+    title: title
+  }).then(res => {
+    console.log("Recipe deleted !");
+    console.log(res);
+    mongoose.disconnect();
+  }).catch(err => console.log(err))
+}
+//deleteRecipe('Carrot Cake');
+//updateRecipeDuration("5e31a40af341217e61cc48a4", 100)
+//insertManyRecipe(data)
+//createRecipe('Test2', 'Easy Peasy', ['banana', 'chocolate'], 'International', 'Dessert', '', 15, 'Yannick');
 // Connection to the database "recipeApp"
 mongoose
   .connect('mongodb://localhost/recipe-app-dev', {

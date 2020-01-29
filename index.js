266a358d97c6ca0f51795bb82f48e82ddcb672a9
Paul Carillion <pcarillion@yahoo.fr>
2020-01-29 16:50:22 +0100

    done

--- index.js
+++ index.js
@@ -11,3 +11,39 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+  // Recipe.create({
+  //     title: "Pot au feu",
+  //     level:"Easy Peasy", 
+  //     ingredients:"meat and vegetables",
+  //     cuisine:"French",
+  //     dishType: "Dish",
+  //     duration: 20,
+  //     creator: "the French",
+  //     created: ""
+  //   }).then(reponse => {console.log(reponse.title)}).catch(err => console.log(err))
+    
+
+  Recipe.insertMany(data)
+
+  
+function updateRecipeDuration(title, newDuration) {
+  
+  Recipe.findOneAndUpdate({title:title}, { duration: newDuration })
+    .then(res => {
+      console.log("Recipe duration updated", res);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+updateRecipeDuration("Rigatoni alla Genovese", 100)
+
+
+Recipe.findOneAndDelete({
+  title: "Carrot Cake"
+}).then(response => {console.log("deleted!!!")}).catch(err => console.log(err))
+
+setTimeout(mongoose.connection.close, 5000)

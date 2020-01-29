6c49e5f82be3f75422fbdc3794eccd55ca6b117e
abrebion <abrebion@gmail.com>
2020-01-29 17:44:42 +0100

    Promise Hell

--- index.js
+++ index.js
@@ -9,65 +9,63 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error("Error connecting to mongo", err));
-
-// Recipe.create({
-//   title: "Asian Glazed Chicken Thighs",
-//   level: "Amateur Chef",
-//   ingredients: [
-//     "1/2 cup rice vinegar",
-//     "5 tablespoons honey",
-//     "1/3 cup soy sauce (such as Silver Swan®)",
-//     "1/4 cup Asian (toasted) sesame oil",
-//     "3 tablespoons Asian chili garlic sauce",
-//     "3 tablespoons minced garlic",
-//     "salt to taste",
-//     "8 skinless, boneless chicken thighs"
-//   ],
-//   cuisine: "Asian",
-//   dishType: "Dish",
-//   image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-//   duration: 40,
-//   creator: "Chef LePapu"
-// })
-//   .then(res => {
-//     console.log("You've just inserted this recipe:", res.title);
-//   })
-//   .catch(err => {
-//     console.log("Error", err);
-//   });
-
-// Recipe.insertMany(data)
-//   .then(res => {
-//     console.log(`Well done, ${res.length} recipes have been added`);
-//   })
-//   .catch(err => {
-//     console.log("Error", err);
-//   });
-
-// Recipe.findOneAndUpdate(
-//   {
-//     title: "Rigatoni alla Genovese"
-//   },
-//   {
-//     duration: 100
-//   }
-// )
-//   .then(res => {
-//     console.log("Great, you've just updated a recipe:", res.title);
-//   })
-//   .catch(err => {
-//     console.log("Error", err);
-//   });
-
-Recipe.deleteOne({
-  title: "Carrot Cake"
-})
-  .then(res => {
-    console.log("A recipe has been deleted:", res);
-    mongoose.connection.close();
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    Recipe.create({
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      cuisine: "Asian",
+      dishType: "Dish",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu"
+    })
+      .then(res => {
+        console.log("You've just inserted this recipe:", res.title);
+        Recipe.insertMany(data)
+          .then(res => {
+            console.log(`Well done, ${res.length} recipes have been added`);
+            Recipe.findOneAndUpdate(
+              {
+                title: "Rigatoni alla Genovese"
+              },
+              {
+                duration: 100
+              }
+            )
+              .then(res => {
+                console.log("Great, you've just updated a recipe:", res.title);
+                Recipe.deleteOne({
+                  title: "Carrot Cake"
+                })
+                  .then(res => {
+                    console.log("A recipe has been deleted:");
+                    mongoose.connection.close();
+                  })
+                  .catch(err => {
+                    console.log("Error", err);
+                  });
+              })
+              .catch(err => {
+                console.log("Error", err);
+              });
+          })
+          .catch(err => {
+            console.log("Error", err);
+          });
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
   })
-  .catch(err => {
-    console.log("Error", err);
-  });
+  .catch(err => console.error("Error connecting to mongo", err));

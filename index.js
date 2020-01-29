6098540fa18897d9935fbc774623035567df3cfb
Nina Gautreau <nina.gautreau@gmail.com>
2020-01-29 17:22:05 +0100

    First commit

--- index.js
+++ index.js
@@ -11,3 +11,76 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+  
+// ============
+// Iteration 2
+// ============
+function createRecipe(title, level, ingredients, cuisine, dishType, duration, creator){
+  Recipe.create({
+    title,
+    level,
+    ingredients,
+    cuisine,
+    dishType,
+    duration,
+    creator
+  })
+  .then(res => {
+    console.log(title);
+    mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(err);
+  });
+};
+
+// createRecipe("Crepes", "Easy Peasy", ["flour", "eggs", "milk", "salt", "butter", "beer", "orange blossom water", "lemon zest"], "French", "Dessert", 60, "Nina")
+
+// ============
+// Iteration 3
+// ============
+function importData(array){
+  Recipe.insertMany(array)
+  .then(res => {
+    array.map(item => console.log(item.title));
+    mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(err);
+  });
+};
+
+// importData(data);
+
+// ============
+// Iteration 4
+// ============
+function updateData(id, newValue){
+  Recipe.findByIdAndUpdate(id, { duration: newValue })
+    .then(res => {
+      console.log("Updated Successfully !", res);
+      mongoose.disconnect();
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+// updateData("5e31aab763c0c55308849753", 100);
+
+// ============
+// Iteration 5
+// ============
+function deleteRecipe(value) {
+  Recipe.deleteOne({ title: value })
+    .then(res => {
+      console.log("Recipe Deleted.", res);
+      mongoose.disconnect();
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+// deleteRecipe("Carrot Cake")

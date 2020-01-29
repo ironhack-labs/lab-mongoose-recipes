e764facd240f5e46e4008c4ac4865d9953ca5192
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-01-29 14:17:54 -0600

    Iteration 4 - Update recipe

--- app.js
+++ app.js
@@ -12,20 +12,28 @@ mongoose
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
     // Iteration 2 - Create a recipe
-    Recipe.create({
-      title:'Recipe one',
-      level:'Easy Peasy',
-      ingredients:['cebolla', 'pimiento', 'sal'],
-      cuisine:'cuisine',
-      dishType:'Dish',
-      image:'',
-      duration:'45',
-      creator:'Carlos Dominguez',
-      created:''
-    })
+    // Recipe.create({
+    //   title:'Recipe one',
+    //   level:'Easy Peasy',
+    //   ingredients:['cebolla', 'pimiento', 'sal'],
+    //   cuisine:'cuisine',
+    //   dishType:'Dish',
+    //   image:'',
+    //   duration:'45',
+    //   creator:'Carlos Dominguez',
+    //   created:''
+    // })
 
     // Iteration 3 - Insert multiple recipes
-    Recipe.create(data)
+    //Recipe.create(data)
+    Recipe.find({},{title:1})
+      .then(recipes => recipes.forEach( recipe=>console.log(recipe.title) ))
+      .catch(err => console.log(err))
+
+      // Iteration 4 - Update recipe
+    Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100},{new: true})
+    .then(response => console.log('Update Success!!'))
+    .catch(err=> console.log(err))
    })
 
   .catch(err => console.error('Error connecting to mongo', err));

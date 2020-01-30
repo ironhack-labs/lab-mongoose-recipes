1ee203d5bc4433596ed4696666fd50d3dd17bbad
jonathansaudhof <jonathan@jonathansaudhof.de>
2020-01-30 18:44:16 +0100

    done

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect('mongodb+srv://ironhack:ironhack@cluster0-29wuy.mongodb.net/test', {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then((x) =>
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`,
+    ),
+  )
+  .catch((err) => console.error('Error connecting to mongo', err));
+
+// Recipe.insertMany(data);
+/* Recipe.findOneAndUpdate(
+  { title: 'Rigatoni alla Genovese' },
+  { duration: 100 },
+  {
+    new: true,
+    upsert: true,
+  },
+);
+ */
+// Recipe.deleteOne({ title: 'Carrot Cake' });

bbc23165c66abfa734ce6fad41b2643f44f310dd
Marcel <marcel.riek@googlemail.com>
2020-01-30 19:19:58 +0100

    added some comments

--- index.js
+++ index.js
@@ -14,38 +14,46 @@ mongoose
   )
   .catch(err => console.error("Error connecting to mongo", err));
 
-// Recipe.create({
+// Iteration 2
+Recipe.create({
+  title: "Asian Glazed Chicken Thighs",
+  level: "Amateur Chef",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  cuisine: "Asian",
+  dishType: "Dish",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 40,
+  creator: "Chef LePapu"
+});
 
-//         title: 'Asian Glazed Chicken Thighs',
-//         level: 'Amateur Chef',
-//         ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
-//         cuisine: 'Asian',
-//         dishType: 'Dish',
-//         image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
-//         duration: 40,
-//         creator: 'Chef LePapu'
+// Iteration 3
+Recipe.insertMany(data)
+  .then(createdRecipe => {
+    console.log(createdRecipe.title);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-// });
+// Iteration 4
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then(result => {
+    console.log("Recipe updated successfully!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-// Recipe.insertMany(data)
-//   .then(createdRecipe => {
-//     console.log(createdRecipe.title);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-// Recipe.updateOne(
-//   { title: "Rigatoni alla Genovese" },
-//   { duration: 100 }
-// )
-//   .then(result => {
-//     console.log("Recipe updated successfully!");
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
+// Iteration 5 + 6
 Recipe.deleteOne({ title: "Carrot Cake" }).then(result => {
   console.log("Recipe has been removed successfully");
   mongoose.connection.close();

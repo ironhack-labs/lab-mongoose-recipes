c3387b514bed05892bc4f33c180f929dcc27a143
Ricardo Leyva Gonz�lez <hola@legoricardo.com>
2020-01-29 20:09:15 -0600

    Const Recipe

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require('./data.js'); // Import of the data from './data.js'
 
+
+const ownRecipe = {
+	title:'Pollo en salsa de guajillo ',
+	level:'Amateur Chef',
+	ingredients:['Pollo','Chile guajillo','vinagre','Aceite de Oliva','Sal','Hojas de Laurel'],
+	cuisine:'Mexicana',
+	dishType:'Dish',
+	image:'https://i1.wp.com/ultimasnoticias.us/wp-content/uploads/2018/04/pollo-en-adobo.jpg',
+	duration:'#0 minutos',
+	creator:'Ricardo Leyva Gonzalez',
+}
+
+
+
+
 // Connection to the database "recipeApp"
 mongoose
   .connect('mongodb://localhost/recipe-app-dev', {
@@ -9,5 +24,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then(x => {
+
+  	  }))
   .catch(err => console.error('Error connecting to mongo', err));

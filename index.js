38b46a8dc6a0b77bd3efc00b46d5cfc3cf9947bf
Ale Ochoa <ale.ochoafi@gmail.com>
2020-01-29 19:40:49 -0600

    crea la base pero no carga nada

--- index.js
+++ index.js
@@ -9,5 +9,40 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then(x =>{ console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  //iteracion2
+  Recipe.create({
+    title:"Sandwich",
+    level:"Easy Peasy",
+    ingredients:['Bread','Jam','Cheese'],
+    cuisine:"Fast food",
+    dishType:"Snack",
+    image:'https://s3.eestatic.com/2016/03/21/cocinillas/Cocinillas_111250498_116247584_1706x960.jpg',
+    duration:5,
+    creator:'Alejandra Ochoa',
+    created:Date.now()
+  })
+
+  //iteracion 3
+  Recipe.create(data)
+
+  Recipe.find({},{})
+  .then(recipes => recipes.forEach( recipe=>console.log(recipe.title) ))
+  .catch(err => console.log(err))
+
+  //iteracion 4
+  Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"},{duration:100},{new:true})
+  .then(recipe=>console.log("Update done!"))
+  .catch(err=>console.log(err))
+
+  //iteracion 5
+  Recipe.deleteOne({title:"Carrot Cake"})
+  .then(recipe=>console.log("Recipe deleted!"))
+  .catch(err=>console.log(err))
+
+  //iteracion 6
+  mongoose.connection.close()
+})
   .catch(err => console.error('Error connecting to mongo', err));
+
+  

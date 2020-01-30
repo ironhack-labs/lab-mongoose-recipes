ca79790f9849e92e03c2f0efb00158e9f1b12e30
Vasiliki Balidou <vasilikibalidou@gmail.com>
2020-01-30 18:48:01 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect("mongodb://localhost/recipe-app-dev", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(x =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch(err => console.error("Error connecting to mongo", err));
+function db() {
+  Recipe.create({
+    title: "Pizza",
+    level: "Easy Peasy",
+    ingredients: ["flour", "yeast", "tomato sauce", "cheese"],
+    cuisine: "Italian",
+    dishType: "Dish",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 30
+  })
+    .then(createdRecipe => console.log(createdRecipe))
+    .catch(err => console.log(err));
+
+  Recipe.insertMany(data);
+}
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then(() => {
+    console.log("Updated!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then(() => {
+    console.log("Deleted!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+setTimeout(() => {
+  console.log("Closing the connection");
+  mongoose.connection.close();
+}, 5000);

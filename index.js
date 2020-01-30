b9bc832086c2a721aabeabb6571d862736d0def0
akups <appiah-lindberg@outlook.com>
2020-01-30 18:51:48 +0100

    all done

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect("mongodb://localhost/recipe-app-dev", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(x =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch(err => console.error("Error connecting to mongo", err));
+
+Recipe.create({
+  title: "Jollof Rice",
+  cuisine: "Ghanaian"
+}).then(created => {
+  console.log(created);
+});
+
+Recipe.insertMany(data)
+  .then(recipes => {
+    console.log(recipes);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.updateOne({ duration: 220 }, { duration: 100 }).then(result => {
+  console.log(result);
+});
+
+Recipe.deleteMany({ title: "Carrot Cake" }).then(result => {
+  console.log(result);
+});
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true
+  },
+  //It should be required and unique.
+  level: {
+    type: String,
+    enum: ["Amateur Chef", "UltraPro Chef", "Easy Peasy"]
+  },
+  //Only can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the ENUM 😉)
+  ingredients: {
+    type: Array
+  },
+  cuisine: {
+    type: String,
+    required: true
+  }, //  Should be required.
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  }, //Possible values:
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg."
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: new Date()
+  }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;

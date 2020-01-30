298cdedcf37658ae0df337426d63e21e7f43642a
Jacqueline Chen <xjacquelinechen@gmail.com>
2020-01-30 18:26:35 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect("mongodb://localhost/recipe-app-dev", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(x =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch(err => console.error("Error connecting to mongo", err));
+
+Recipe.create({
+  title: "cookies",
+
+  level: "Easy Peasy",
+
+  ingredients: [
+    "1 cup salted butter",
+    "1 cup white sugar",
+    "1 cup light brown sugar",
+    "2 tsp pure vanilla extract",
+    "2 large eggs",
+    "3 cups all-purpose flour",
+    "1 tsp baking soda",
+    "1/2 tsp baking powder",
+    "1 tsp sea salt",
+    "2 cups chocolate chips (or chunks)"
+  ],
+
+  cuisine: "American",
+
+  dishType: "Dessert",
+
+  duration: 30,
+
+  creator: "Joy Food Sunshine"
+})
+  .then(() => {
+    console.log("recipe added");
+  })
+  .catch(err => {
+    console.log("Error :" + err);
+  });
+
+Recipe.insertMany(data)
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.log("Error: " + err);
+  });
+
+Recipe.find({})
+  .then(results => {
+    const allTitles = results.map(el => el.title);
+    /* 
+    var allTitles = results.map(function (el) {
+      return el.title
+    }) */
+    console.log(allTitles);
+  })
+  .catch(err => {
+    console.log("Error: " + err);
+  });
+
+Recipe.updateOne({ title: "rigatoni alla genovese" }, { duration: 100 }).then(
+  () => {
+    console.log("Recipe has been updated successfully!");
+  }
+);
+
+Recipe.deleteOne({ title: "carrot cake" }).then(() => {
+  console.log("Recipe has been deleted!");
+});
+
+setTimeout(() => {
+  mongoose.connection.close();
+}, 5000);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    set: str => str.toLowerCase()
+  },
+  level: {
+    type: String,
+    validate: {
+      validator: str => {
+        if (!"Easy Peasy" && !"Amateur Chef" && !"UltraPro Chef") {
+          return false;
+        }
+        return true;
+      },
+      message:
+        "The level can only be Easy Peasy, Amateur Chef, or UltraPro Chef."
+    }
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    validate: {
+      validator: str => {
+        if (
+          !"Breakfast" &&
+          !"Dish" &&
+          !"Snack" &&
+          !"Drink" &&
+          !"Dessert" &&
+          !"Other"
+        ) {
+          return false;
+        }
+        return true;
+      },
+      message:
+        "The dish type can only be dish, snack, drink, dessert, or other."
+    }
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creater: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;

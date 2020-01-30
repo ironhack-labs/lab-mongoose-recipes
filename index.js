c7ed5e49c1ce05741fec9a6228dccc51324af077
Ricardo Leyva Gonz�lez <hola@legoricardo.com>
2020-01-29 20:28:23 -0600

    Create Methods to monngose update, delete, create

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require('./data.js'); // Import of the data from './data.js'
 
-
+ /* Iteration 2 - Create a recipe*/
 const ownRecipe = {
 	title:'Pollo en salsa de guajillo ',
 	level:'Amateur Chef',
@@ -24,7 +24,29 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => {
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err))
+  .then(
 
-  	  }))
-  .catch(err => console.error('Error connecting to mongo', err));
+  	/*iteration 2*/
+  	Recipe.create(ownRecipe)
+  		  .then(ownRecipe=>{ownRecipe
+  		  	.forEach(el =>console.log(el))
+  		  })
+	/*iteration 4*/
+
+  	Recipe.updateOne({title:'Rigatoni alla Genovese'}, {level:"UltraPro Chef"})
+  		  .then(res => console.log("Se actualizó la receta"))
+  		  .catch(error => console.log(error))
+  	/*iteration 5*/
+
+  	Receipe.deleeOne({
+  		title:"Chocolate Chip Cookies"
+  	}).then(res => console.log("Receta Borrada"))
+  	  .catch(error => console.log(error))
+
+  	/*iteration 6*/
+
+  	  mongoose.connection.close()
+
+  	).catch(error => console.error("Error al conectar Mongo", error))

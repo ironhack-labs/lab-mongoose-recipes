b2e2644c9ff2da479a49c77c1a4863cf365055c8
Johnny Burke <johnalbertburke@gmail.com>
2020-01-30 19:08:27 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect("mongodb://localhost/recipe-app-dev", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(x =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch(err => console.error("Error connecting to mongo", err));
+
+Recipe.create({
+  title: "Burger de Bangin",
+  level: "UltraPro Chef",
+  ingredients: [
+    "1 bun",
+    "1 patty",
+    "1 slice of cheese",
+    "Some veggies",
+    "A lil ketchup",
+    "A lil mayo"
+  ],
+  cuisine: "American",
+  dishType: "Dish",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 30,
+  creator: "Chef Boyarjohnny"
+})
+  .then(createdRecipe => {
+    console.log(createdRecipe);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.insertMany(data)
+  .then(createdRecipe => {
+    console.log(createdRecipe);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.deleteOne({ title: "Carrot Cake" }).then(result => {
+  console.log("Recipe has been removed successfully");
+  mongoose.connection.close();
+});
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: 1 / 30 / 2020
+  }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;

baf267318968d1b4bf6924a264ceaf921fb09a1b
Jason Franklin <jfrank325@gmail.com>
2020-01-30 18:52:49 +0100

    Done

--- index.js
+++ index.js
@@ -7,7 +7,38 @@ mongoose
   .connect('mongodb://localhost/recipe-app-dev', {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+// Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 }).then(result => {
+//   console.log('Success');
+// });
+
+// Recipe.deleteOne({ title: 'Carrot Cake' }).then(result => {
+//   console.log('Success');
+// });
+
+// Recipe.create({
+//   title: 'Burger',
+//   level: 'Easy Peasy',
+//   ingredients: ['Bun', 'Beef'],
+//   cuisine: 'American',
+//   dishtype: 'Dish',
+//   duration: 10,
+// })
+//   .then(result => {
+//     console.log(result.title);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// Recipe.insertMany(data)
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });

3875a5b35e04236e1f8562937b0823f948025f0c
israSH <isra-03p@hotmail.com>
2020-01-29 22:29:59 -0600

    iteration 1-3

--- index.js
+++ index.js
@@ -9,5 +9,24 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+ Recipe.create({ 
+  title: 'Hot dog',
+  level: 'Easy Peasy',
+  ingredients: ['8 salchichas viena','8 panes para hot dog','al gusto Mayonesa','al gusto Catsup','al gusto Salsa o chiles en escabeche'],
+  image: 'https://img-global.cpcdn.com/recipes/d47004c4148acee1/751x532cq70/hot-dog-sencillos-foto-principal.jpg',
+  duration: 20,
+  creator: 'Chef Isra'
+ })
+.then(Recipe => console.log(`Title of Recipe: ${Recipe.title}`))
+.catch(error =>
+  console.log('Error', error)
+)
+
+Recipe.insertMany(data)
+  .then(Recipe => console.log(`Title of Recipe: ${Recipe.title}`, Recipe))
+  .catch(error => console.log('Error: ', error))
+
+
+
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .catch(err => console.error('Error connecting to mongo', err))

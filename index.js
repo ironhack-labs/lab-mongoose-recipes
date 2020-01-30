b5acad1e22c9fad68b5626f9eaedc843179f67de
Luis Lozano <luislozano@MacBook-Air.local>
2020-01-29 22:46:48 -0600

    Done

--- index.js
+++ index.js
@@ -9,5 +9,46 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  //Promises - async await.
+  //The then is executed if all works properly.
+  .then(async x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)//Connect to the database. This logs to the console.
+    //Here we create a new recipe with all the keys and respective values established in our Schema.
+    const hotCakes = {
+      title: 'Hot cakes',
+      level: 'Easy Peasy',
+      ingredientes: [
+      'Half cup of milk', 
+      'Two eggs', 
+      'Buttermilk', 
+      'Cupcake powder',
+      'Honey'],
+      cuisine: 'American',
+      dishType: 'Breakfast',
+      image: '/Hotcake/hotcakes.jpg',
+      duration: 20,
+      creator: 'Luis'
+    }
+    //This are the process on await.
+    //Each one will be resolved on their respective time without interrupting the execution of the code.
+    await Recipe.create(hotCakes)//Creates new recipe hotCakes.
+    await Recipe.insertMany(data)//Inserts the data in data.js on our data base.
+    await Recipe.find({}, {title: 1, _id: 0})//Finds only the titles of our recipes.
+      .then(title => {
+       title.forEach(recipeName => (console.log(recipeName.title)))
+      })//Logs only the title of our recipes in the terminal.
+    await Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+    //With the selectors we get the recipe which it's title is the one mentioned and update it's information on the duration key to 100.
+    await Recipe.deleteOne({title: 'Carrot Cake'})//Delete recipe with title Carrot Cake.
+    //We close the connection to the database.
+    mongoose.connections.close()
+  })
+  .catch(err => console.error('Error connecting to mongo', err));//If something goes wrong the catch is executed and the error is logged to the terminal.
+  
+
+  
+ 
+  
+  
+  
+  
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,25 @@
+//Requiring mongoose and saving it to a const.
 const mongoose = require('mongoose');
+//Create a mongoose.Schema saved to a const var named Schema.
 const Schema = mongoose.Schema;
 
+//With what was declared before, we create a new instance of Schema called recipeSchema which will receive all the key-value detailed within of the specific type
+//and conditions.
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type: String, required: true}, //Requires the value.
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']}, //Can just be of these three values.
+  ingredientes: Array, //Shorthand type Array.
+  cuisine: {type: String, required: true} , //Requires the value.
+  dishType: {type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},//Not sure if this is possible value. As far as I know, enum is for strict ones.
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},//Type string value with a default of the corresponding image.
+  duration: {type: Number, min: 0},//Minium value for this number 0.
+  creator: {type: String}, 
+  created: {type: Date, default: Date.now}//Default value is the date for present time.
 });
 
+//Creates a moongose model saved in a const Recipe, that will be called Recipe and with the recipeSchema characteristics.
 const Recipe = mongoose.model('Recipe', recipeSchema);
+//Export the const Recipe that we just created to be used in other files.
 module.exports = Recipe;
+
+

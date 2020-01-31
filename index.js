54d790bedba99196ba8be135483d60c51d777eec
Laurel Wright <wright.laureld@gmail.com>
2020-01-31 09:49:24 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect("mongodb://localhost/recipe-app-dev", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(x =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch(err => console.error("Error connecting to mongo", err));
+
+// Create a Recipe
+Recipe.create({
+  title: "Chocolate Chip Cookies",
+  level: "Easy Peasy",
+  ingredients: [
+    "Flour",
+    "Baking Soda",
+    "Brown Sugar",
+    "Chocolate Chips",
+    "Salt",
+    "Vanilla",
+    "White Sugar"
+  ],
+  cuisine: "American",
+  dishType: "Dessert",
+  image:
+    "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/62298.jpg",
+  creator: "Buzzfeed",
+  created: 31 / 12 / 2020
+})
+  .then(createdRecipe => {
+    console.log(createdRecipe);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// Add multiple recipes
+  Recipe.insertMany(data)
+  .then(recipes => {
+    console.log(recipes);
+
+    Recipe.updateOne({ title: "Rigatoni alla Genovese"}, {duration: 100}).then(info => {
+      console.log(info);
+      Recipe.deleteOne({ title: "Carrot Cake" }).then(info => {
+        console.log("Deleted: ", info);
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  mongoose.connection.close();
+
+
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    validate: str => {
+      if (
+        str.startsWith("Easy Peasy") ||
+        str.startsWith("Amateur Chef") ||
+        str.startsWith("UltraPro Chef")
+      ) {
+        return true;
+      }
+      return false;
+    }
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    require: true
+  },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    minlength: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;

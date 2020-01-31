6a7ec6d9bfb660b347d26eb7306cee0c78bf9dce
andrewnunezbibby <andrew.bibby3@gmail.com>
2020-01-31 09:46:42 +0100

    before the nesting for the synchronicity

--- index.js
+++ index.js
@@ -11,3 +11,36 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+const userSpecs = {
+  title: "Pain aux chocolat",
+  level: "UltraPro Chef",
+  ingredients: ["farine", "sel", "sucre"],
+  cuisine: "Yalready",
+  dishType: "Dessert",
+  duration: 120,
+  creator: "Cecile"
+}
+
+Recipe.create(userSpecs)
+  .then(newUser => console.log('1 - CREATE PROMISE The user is saved and its value is: ', newUser))
+  .catch(newUser => console.log("Couldnt create user: ", newUser))
+
+Recipe.insertMany(data)
+  .then(savedData => console.log(' 2 - INSERT PROMISE The user is saved and its value is: ', savedData))
+  .catch(savedData => console.log("Couldnt create user: ", savedData))
+
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+  .then(success => console.log("3 - UPDATE PROMISE we did it"))
+  .catch(err => console.log(err)
+  )
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then(success => console.log("4 - DELETE PROMISE succes"))
+  .catch(err => console.log("Couldnt delete for some reason"))
+
+
+mongoose.connection.close().then(() => {
+  console.log(" 5 - CLOSE PROMISE closing db");
+}).catch(err => console.log("SERVER STILL OPEN"))

25732cfe59e87e0409c28508aed3f527d2de90bd
Louis MD <louis@margot-duclot.fr>
2020-02-01 14:22:10 +0100

    done

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const Recipe = require('./models/Recipe.model');
+const data = require('./data.js');
+const recipe = {
+  title: "Poulet caramel aux pistaches",
+  level: "Easy Peasy",
+  ingredients: ["poulet", "caramel", "pistaches"],
+  dishType: "Dish",
+  cuisine: "nawak"
+}
 
-// Connection to the database "recipeApp"
 mongoose
   .connect('mongodb://localhost/recipe-app-dev', {
     useCreateIndex: true,
@@ -10,4 +16,26 @@ mongoose
     useUnifiedTopology: true
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .catch(error => console.error('Error connecting to mongo', error));
+
+const createNewRecipe = Recipe.create(recipe)
+  .then(recipe => console.log(`The recipe ${recipe.title} has been saved!`))
+  .catch(error => console.error(`Oh no! An error happened while saving a new recipe: ${error}`));
+
+const importRecipes = Recipe.insertMany(data)
+  .then (data => console.log(`The recipe ${data.map(recipe => recipe.title)} has been saved!`))
+  .catch(error => console.error(`Ouch! An error happened while saving new recipes: ${error}`)); 
+
+const editRecipe = Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then (updatedRecipe => console.log(`${updatedRecipe.title} has been updated!`))
+  .catch(error => console.error(`Oops! An error happened while updating the recipe: ${error}`))
+
+const deleteRecipe = Recipe.deleteOne({title: 'Carrot Cake'})
+  .then (deletedRecipe => console.log(`${deletedRecipe.title} has been deleted!`))
+  .catch(error => console.log(`Ooh! An error happened while deleting the recipe: ${error}`))
+
+Promise.all([createNewRecipe, importRecipes, editRecipe, deleteRecipe])
+  .then(values => {
+    console.log(`Life is magic! All promises were filled: ${values}`);
+    mongoose.connection.close();})
+  .catch(error => console.error(`Sadness, something went wrong resolving all promises: ${error}`))

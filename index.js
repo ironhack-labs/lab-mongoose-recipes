177118ab5da1cf58c336908606353f21e4d7ed7d
Pauline Morsli <pauline.morsli@gmail.com>
2020-02-01 16:16:00 +0100

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require('./data.js'); // Import of the data from './data.js'
 
+
 // Connection to the database "recipeApp"
 mongoose
   .connect('mongodb://localhost/recipe-app-dev', {
@@ -11,3 +12,25 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+// const recette = { title: 'Bolognaise', level: 'Easy Peasy', ingredients: ['pasta','tomato sauce'], cuisine : 'italian'};
+
+// Recipe.create(recette)
+//   .then(recipe => console.log('The recipe is saved and its value is: ', recipe))
+//   .catch(error =>
+//     console.log('An error happened while saving a new recipe:', error)
+//   );
+
+  // Recipe.insertMany(data, function(error, docs) {});
+
+  // Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  // .then()
+  // .catch();
+  
+  Recipe.deleteOne({ title: 'Carrot Cake' })
+  .then(console.log("success, you deleted Carrot Cake !"))
+  .catch();
+
+// mongoose.connection.close()
+
+  

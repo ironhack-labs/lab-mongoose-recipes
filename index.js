a8455b6b5a1a5d617cc7f4604d5b97edee652b86
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-02-05 18:48:07 +0100

    done

--- index.js
+++ index.js
@@ -5,9 +5,39 @@ const data = require('./data.js'); // Import of the data from './data.js'
 // Connection to the database "recipeApp"
 mongoose
   .connect('mongodb://localhost/recipe-app-dev', {
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .catch(err => console.error('Error connecting to mongo', err))
+  .then(() => Recipe.collection.drop())
+  .then(x => {
+    const recipe1 = {
+      title: 'Pollo Tika-Masala',
+      level: 'Amateur Chef',
+      ingredients: ["pollo", "curry", "leche de coco", "almendras", "agua", "comino", "arroz"],
+      cuisine: 'India',
+      dishType: 'Dish',
+      image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-chicken-tikka-masala-jpg-1526059261.jpg?resize=980:*',
+      duration: 40,
+      creator: 'Tika Masala'
+    }
+    return recipe1
+  })
+  .then(recipe1 => Recipe.create(recipe1))
+  .then(newRecipe => console.log(`Nuestra receta es ${newRecipe.title}`))
+  .then(() => Recipe.insertMany(data))
+  .then(newRecipes => newRecipes.forEach(element => console.log(`Añadido ${element.title}`)))
+  .then(() => Recipe.updateOne({
+    title: 'Rigatoni alla Genovese'
+  }, {
+    duration: 100
+  }))
+  .then(updateInfo => updateInfo.nModified ? console.log("Updated Successfully") : console.log("Updated Failed"))
+  .then(() => Recipe.deleteOne({
+    title: 'Carrot Cake'
+  }))
+  .then(deleteInfo => deleteInfo.deletedCount ? console.log("Deleted Successfully") : console.log("Deleted Failed"))
+  .then(() => mongoose.connection.close())
+  .catch(error => console.log(`Se produjo un error: ${error}`))

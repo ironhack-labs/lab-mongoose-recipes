fb652d1e4037913580e4c2233f4c2522b5225c13
Pedro Castañeda <pcramos19@gmail.com>
2020-02-05 19:00:19 +0100

    done

--- index.js
+++ index.js
@@ -9,5 +9,40 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(x => 
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    return Recipe.deleteMany()
+  .catch(err => console.error('Error connecting to mongo', err))
+  .then(() => {
+    const recipe = {
+      title: 'AAAA',
+      level: 'Amateur Chef',
+      ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+      cuisine: 'Asian',
+      dishType: 'Dish',
+      image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+      duration: 40,
+      creator: 'Chef LePapu'
+    }
+    return Recipe.create(recipe)
+    .then(recipe => console.log ("Hemos creado recipe1", recipe))
+    .catch(error => console.log ("Error creando receta", error));
+  })
+  .then(() => {
+    return Recipe.insertMany (data)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {
+      duration: 100
+    })
+  })
+  .then(() => {
+    return Recipe.deleteOne ({title: "Carrot Cake"})
+  })
+  .then(() => {
+   return mongoose.connection.close()
+   
+  })
+  .then(() => console.log("database closed"))
+
+  

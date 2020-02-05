d08bcb1ade56e8d8ea75eae8e395bbcf013cae1d
mfernandezfreire <mfernandezfreire@outlook.com>
2020-02-05 19:46:26 +0100

    Casi terminado

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+Recipe.create({
+  title: 'Fried egg',
+  level: 'UltraPro Chef',
+  ingredients: ["1 or two eggs", "oil", "A little of magic"],
+  cuisine: 'MasterChef',
+  dishType: 'Dish',
+  image: 'https://www.pequerecetas.com/wp-content/uploads/2015/11/huevo-frito-microondas.jpg',
+  duration: 5,
+  creator: 'Jota'
+}).then((recipeCreated => {
+  console.log(recipeCreated.title);
+}))
+
+Recipe.insertMany(data).then(
+  recipesCreated => recipesCreated.forEach(recipe => console.log(recipe.title)
+))
+
+Recipe.findByIdAndUpdate("5e3b0a4d34c7378c459ede1c", {
+  duration: 100}).then(console.log("Your Recipe is updated"))
+
+Recipe.findByIdAndDelete("5e3b0a4d34c7378c459ede1b").then(console.log("OUUUUUH MAMA"))
+
+
+.then(()=> {
+  return mongoose.connection.close()
+})

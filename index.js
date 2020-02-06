2b4ae3685eb0bb7f2a46844fb50471fe78f9da92
Stefan Berk <stefan@k2ff.de#>
2020-02-06 18:42:59 +0100

    insert one receipe, model works

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+const testRecipe = {
+  title: 'Asian Glazed Chicken Thighs',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  cuisine: 'Asian',
+  dishType: 'Dish',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 40,
+  creator: 'Chef LePapu'
+}
+
+Recipe.create(testRecipe)
+.then((result) => {
+  console.log(result.name, " was created")
+}).catch((err) => {
+  console.log("Error occured: ", err)
+});

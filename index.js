59de3b1555511179cc75fb2d77b4f035fc45c699
Jethet <henriette.hettinga@gmail.com>
2020-02-06 19:28:55 +0100

    Create recipe model scheme and create first instance

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+// Create recipe with model
+
+let recipeDessert115 = {
+  title: 'Vegan Chocolate Mousse',
+  level: 'Amateur Chef',
+  ingredients: ['200 gr dark chocolate', '100 gr cane sugar', '150 ml aquafaba', 'spoonful of lemon juice'],
+  cuisine: 'Vegan',
+  dishType: 'Dessert',
+  image: 'https://foodimentaryguy.files.wordpress.com/2012/04/chocolatemousse.jpg',
+  duration: 30,
+  creator: 'Hope Green',
+  created: 2019-10-05
+};
+
+Recipe.create(recipeDessert115)
+  .then((result) => {
+    console.log('Created a document', result);
+    
+  }).catch((err) => {
+    console.log(err);
+  });
+  
+  console.log(recipeDessert115);

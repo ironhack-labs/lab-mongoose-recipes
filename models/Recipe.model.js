3a26a7f70df155f74029fb8dbb1acd5f82b0f1e8
Stefan Berk <stefan@k2ff.de#>
2020-02-06 18:35:23 +0100

    added schema for receipe

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type: String, required: true, unique:true},
+  level: {type: String, required: true, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"], default: "Amateur Chef" },
+  ingredients: {type: [], required: false},
+  cuisine: {type: String, required: true},
+  dishType: {type: String, enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"], default:"Other"},
+  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: {type: Number, default:0},
+  creator: {type: String},
+  created: {type: Date, default:Date.now()}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

d329668d687eb8f0f30a10365aa597e7946e590f
LolaEnBeta <lolitaenbeta@gmail.com>
2020-02-06 19:36:27 +0100

    Use Model.create method to save a recipe in Mongo DB

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+//Model.create
+
+const recipe1 = {
+  title: 'Orange and Milk-Braised Pork Carnitas',
+  level: 'UltraPro Chef',
+  ingredients: ['3 1/2 pounds boneless pork shoulder, cut into large pieces', '1 tablespoon freshly ground black pepper', '1 tablespoon kosher salt, or more to taste', '2 tablespoons vegetable oil', '2 bay leaves', '2 teaspoons ground cumin', '1 teaspoon dried oregano', '1/4 teaspoon cayenne pepper', '1 orange, juiced and zested'],
+  cuisine: 'American',
+  dishType: 'Dish',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg',
+  duration: 160,
+  creator: 'Chef John'
+}
+
+Recipe.create(recipe1)
+  .then(result => {
+    console.log("Recipe created. Title:", result.title);
+  })
+  .catch(err => {
+    console.log(err);
+  });

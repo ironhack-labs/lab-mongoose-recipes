be683038d7d61b85ce6e5a8527259e8db1039729
Jorge Berrizbeitia <jorgeberrizbeitia@gmail.com>
2020-02-07 01:02:08 +0100

    only half done, must sleep

--- index.js
+++ index.js
@@ -11,3 +11,43 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+// Iteration 2 recipes created!
+
+// let newRecipe = {
+//   title: "Lemon Pie",
+//   level: "Amateur Chef",
+//   ingredients: ["Lemon", "Sugar", "Cookies", "Butter", "Eggs", "Condensed Milk"],
+//   cuisine: 'International',
+//   dishType: 'Dessert',
+//   image: '',
+//   duration: 60,
+//   creator: 'Chef Flor'
+// }
+
+
+  // Recipe.create(newRecipe)
+  // .then(result => {
+  //   console.log("Created a recipe", result);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // });
+
+  // Iteration 3 recipes created!
+  // Recipe.insertMany(data)
+  // .then(result => {
+  //   console.log("Created a recipe", result);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // });
+
+  // iteration 4
+  Recipe.findOneAndUpdate({name: "Rigatoni alla Genovese"}, {$set:{duration:100}},function(err, doc){
+    if(err){
+        console.log("Something wrong when updating data!");
+    }
+    console.log(doc);
+});

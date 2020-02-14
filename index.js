ad152f968263c1312014d68a12934843db638cd5
José Carneiro <josemcarneiro@gmail.com>
2020-02-14 11:07:53 +0000

    Clarified exercise, added step that drops the database

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data'); // Import of the data from './data.json'
 
-const MONGODB_URI = 'mongodb://localhost/recipeApp';
+// Import of the model Recipe from './models/Recipe.model.js'
+const Recipe = require('./models/Recipe.model');
+// Import of the data from './data.json'
+const data = require('./data');
 
-// Connection to the database "recipeApp"
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+  .then(self => {
+    console.log(`Connected to the database: "${self.connection.name}"`);
+    // Before adding any documents to the database, let's delete all previous entries
+    return self.connection.dropDatabase();
+  })
   .then(() => {
-    console.log(`Connected to the database: "${x.connections[0].name}"`);
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {

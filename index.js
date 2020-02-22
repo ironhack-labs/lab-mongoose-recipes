cc427fca20d8fafbe1221767846bfa693eb4d5ce
Your Name <hurtadojose89@gmail.com>
2020-02-22 14:11:52 +0100

    [master] - done

--- index.js
+++ index.js
@@ -9,5 +9,29 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(() => {
+    return Recipe.deleteMany({});
+  })
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then( recipes => {
+    recipes.forEach( recipe => console.log(recipe.title));
+
+    const promises = [];
+    promises.push(Recipe.updateOne({title: 'Rigatoni alla Genovese' },  {duration: 100}));
+    promises.push(Recipe.deleteOne({title: 'Carrot Cake' }));
+
+    return Promise.all(promises);
+  })
+  .then(() => {
+    return Recipe.find({});
+  })
+  .then( recipes => console.log(recipes))
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  })
+  .finally(() => {
+    mongoose.connection.close();
+  });
+

f624e4d0f3fa2182ee94c5fce961057e3a0e31a5
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-02-22 13:34:07 +0100

    Iteration 1 done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,7 +3,17 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String, 
+  level: String,
+  ingredients: Array,
+  cuisine: String,
+  dishType: String,
+  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg."},
+  duration: Number,
+  creator: Date
 });
+
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

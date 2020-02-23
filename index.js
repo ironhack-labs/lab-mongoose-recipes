f2c0b9abdc15541562ba9903ccf7bfb109995524
Baenas <dbg94dani@gmail.com>
2020-02-23 13:53:06 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
+// Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect("mongodb://localhost/recipe-app-dev", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  // elimino los datos
+  .then(() => {
+    return Recipe.deleteMany({}, err => {
+      console.log("deleted");
+    });
+  })
+  .then(() => {
+    const RecetaOne = {
+      title: "Pan con tomate",
+      level: "Easy Peasy",
+      ingredients: ["Pan", "Tomnate", "Aceite"],
+      cuisine: "Catalana",
+      dishType: "Others",
+      image: "default",
+      duration: 5,
+      creator: "Daniel Baenas",
+      created: Date.Now
+    };
+
+    return Recipe.create(RecetaOne);
+  })
+
+  //inserto multiples
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  //Update time rigatone
+  .then(() => {
+    return Recipe.updateOne(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .catch(err => {
+    console.log(err);
+  })
+  .finally(() => {
+    mongoose.connection.close();
+  });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String, required: true, unique: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: Array,
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Others"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg."
+  },
+  duration: { type: Number, min: 0 },
+  creator: String,
+  created: { type: Date, default: Date.Now }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;

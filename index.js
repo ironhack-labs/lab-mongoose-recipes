e2d308d46ea205eef2b41fee4748087666c27352
Marc Serra <mserrahidalgo@gmail.com>
2020-02-23 12:40:31 +0100

    done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Recipe = require("./models/Recipe.model"); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require("./data.js"); // Import of the data from './data.js'
 
-// Connection to the database "recipeApp"
 mongoose
   .connect("mongodb://localhost/recipe-app-dev", {
     useCreateIndex: true,
@@ -11,26 +10,23 @@ mongoose
   })
   .then(x => {
     console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+      `Connected to Mongo! Database name: "${x.connections[0].name}". Inserting al data...`
     );
-    console.log("Now insert all data");
     return Recipe.insertMany(data);
   })
   .then(() => {
-    console.log("Insert done. Now update bad receipt: ");
+    console.log("Insert done. Now updating bad receipt...");
     return Recipe.updateOne(
       { title: "Rigatoni alla Genovese" },
-      {
-        duration: 100
-      }
+      { duration: 100 }
     );
   })
-  .then(a => {
-    console.log("Updated done. Now remove.");
+  .then(() => {
+    console.log("Updated done. Now removing one receipt...");
     return Recipe.deleteOne({ title: "Carrot Cake" }, err => {});
   })
   .then(() => {
-    console.log("Finish. Closing the database");
-    mongoose.connection.close();
+    console.log("Removing done. Now closing the database...");
+    return mongoose.connection.close();
   })
-  .catch(err => console.error("Error connecting to mongo", err));
+  .catch(err => console.error("Error: ", err));

3bd1e5322831ed9c8dd6a90ecf86c5269cfc8a31
Michael Mueller <michael@miniguide.co>
2020-02-25 00:07:54 +0200

    Iteration 6

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ mongoose
   .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch((err) => console.error('Error connecting to mongo', err));
 
-Recipe.create(myCheesecake)
+const promise1 = Recipe.create(myCheesecake)
   .then((recipe) => console.log(`Added recipe: ${recipe.title}`))
   .catch((error) => console.log('An error happened while saving a new recipe: ', error));
 
-Recipe.insertMany(data)
+const promise2 = Recipe.insertMany(data)
   .then((recipes) => {
     recipes.forEach((recipe) => {
       console.log(`Added recipe: ${recipe.title}`);
@@ -36,15 +36,22 @@ Recipe.insertMany(data)
   .catch((error) => console.log('An error happened while saving a new recipe: ', error));
 
 // method 1: success callback returns updated record
-Recipe.findByIdAndUpdate('5e5441472b670a7ad22778d7', { $set: { duration: 100 } })
-  .then((recipe) => console.log(`Updated recipe: ${recipe.title} duration: ${recipe.duration}`))
-  .catch((error) => console.log('An error happened while updating a recipe: ', error));
+// const promise3 = Recipe.findByIdAndUpdate('5e5441472b670a7ad22778d7', { $set: { duration: 100 } })
+//   .then((recipe) => console.log(`Updated recipe: ${recipe.title} duration: ${recipe.duration}`))
+//   .catch((error) => console.log('An error happened while updating a recipe: ', error));
 
 // method 2: success callback does not return updated record
-Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } })
+const promise3 = Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } })
   .then((success) => console.log(`Updated recipe. Success message: ${JSON.stringify(success)}`))
   .catch((error) => console.log('An error happened while updating a recipe: ', error));
 
-Recipe.deleteOne({ title: 'Carrot Cake' })
+const promise4 = Recipe.deleteOne({ title: 'Carrot Cake' })
   .then((success) => console.log(`Deleted recipe. Success message: ${JSON.stringify(success)}`))
   .catch((error) => console.log('An error happened while deleting a recipe: ', error));
+
+Promise.all([promise1, promise2, promise3, promise4])
+  .then(() => {
+    console.log('Promises completed, closing Mongo connection');
+    mongoose.connection.close();
+  })
+  .catch((error) => console.log('An error occured handling promises: ', error));

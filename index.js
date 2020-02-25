1e39d0be997a143eccc632ab4a8c5495d0804667
alejandro <alejimenezgitgithub@gmail.com>
2020-02-25 21:26:09 +0100

    iteration 5 and 6

--- index.js
+++ index.js
@@ -36,30 +36,21 @@ mongoose
     return 'Rigatoni alla Genovese';
   })
   .then(titleUpload =>{
-    // Iteration 4
     const query = { title: titleUpload};
-    // https://stackoverflow.com/questions/32811510/mongoose-findoneandupdate-doesnt-return-updated-document
-    Recipe.findOneAndUpdate(query,  { $set: {duration: '201'}}, false, (err,doc) => {
-      if (!err) 
-        doc.save();
-        console.log("Congratulations")
-      else 
-        console.log("Something wrong when updating duration!");
-    });
+    return Recipe.updateOne(query, {duration: 100});
   })
-  .catch(err => console.error('Error connecting to mongo', err));
-
-
-  /*
-
-Iteración 5 - Eliminar una receta
-Oh oh! El pastel de zanahoria ya no está disponible, por lo que debemos
- eliminarlo de la base de datos. Usando el método Model.deleteOne, elimine 
- esa receta de la base de datos y muestre un mensaje de éxito después de hacerlo.
-
-Iteración 6: cierre la base de datos
-Después de hacer toda la tarea, debe cerrar la base de datos. De lo contrario, 
-la conexión se mantendrá abierta. Tenga cuidado con la asincronía de todos los procesos; 
-¡deberías cerrarlo después de que todo esté hecho! guiño
-
-  */
+  .then(update => {
+    console.log('save');
+    return 'Carrot Cake';
+  })
+  .then(titleUpload =>{
+    const query = { title: titleUpload};
+    return Recipe.deleteOne(query);
+  })
+  .then(deleteOne => {
+    console.log('delete');
+    return 'close';
+  })
+  .finally( 
+    mongoose.connection.close()
+  )

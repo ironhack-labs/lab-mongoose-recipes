02e55d1630af475775bcc05c410f80301dfd90fb
javier Varela <javivarela@javiers-MacBook-Pro.local>
2020-02-27 20:52:18 +0100

    Iteration 1 - Recipe Schema

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: String,
+  ingredients: [String],
+  cuisine: { type: String, required: true },
+  dishtype: {
+    type: String,
+    validate: {
+      validator: (text) => {
+        return text === "Breakfast" || text === "Dish" || text === "Snack" || text === "Drink" || text === "Dessert" || text === "Other";
+      },
+      message: "Possible values: Breakfast - Dish - Snack - Drink - Dessert - Other"
+    }
+  },
+  image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
+  duration: {type: String, min: 0},
+  creator: String, 
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

8040fbe379721148a2d8ffcf060d708aa9654738
Pablolo <pablo_olondriz@hotmail.com>
2020-03-02 13:51:02 +0100

    2nd iteration

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+const myRecipe = { 
+  title: 'Patatas a lo Pobre', 
+  level: 'Easy Peasy', 
+  ingredients: 'potatoes', 
+  cuisine: 'Spanish', 
+  dishType: 'Dish', 
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg', 
+  duration: 40, 
+  creator: 'Chef Pablo' 
+};
+
+Recipe.create(myRecipe)
+.then(myRecipe => console.log('The name of the recipe is:', myRecipe.title))
+.catch(error =>
+    console.log('An error happened while adding a recipe:', error)
+);
+
+Recipe.insertMany(data)
+  .then(data.forEach(recipe => console.log('The title of the recipe is:', recipe.title)))
+  .catch(error =>
+    console.log('An error happened while adding a recipe:', error)
+); 

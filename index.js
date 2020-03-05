b67799ef0def0a1f54d6202023d803ce28374439
teo23 <matteoazzola23@gmail.com>
2020-03-05 21:42:17 +0100

    done

--- index.js
+++ index.js
@@ -11,3 +11,39 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+Recipe.create({ 
+  title: 'Carrot cake',
+  cuisine: 'Meh',
+  dishType: 'Breakfast'
+})
+.then (recipe =>  console.log(recipe.title))
+.catch (error => console.log(error));
+
+var arr = [
+  { 
+    title: 'Pizza',
+    cuisine: 'Italian',
+    dishType: 'Snack'
+  },
+  { 
+    title: 'Bitterballen',
+    cuisine: 'Dutch',
+    dishType: 'Other'
+  },
+]
+
+Recipe.insertMany(arr, function(error, docs) {});
+
+let italianMeal = Recipe.find({title: 'Rigatoni alla Genovese'})
+italianMeal.updateOne(
+  {
+    duration: '100',
+  }
+)
+.then (recipe =>  console.log('now it is good', recipe))
+.catch (error => console.log(error))
+
+Recipe.deleteOne({ title: 'Ratatouille' })
+.then (recipe =>  console.log(recipe.title))
+.catch (error => console.log(error));

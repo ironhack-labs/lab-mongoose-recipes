f98a5863a294756d30b19d9b567dee2d76bfa9e2
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-03-04 22:29:34 -0500

    using create

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+  //ITERATION 2 
+// In index.js, using the Model.create method, you should pass the info to create a new recipe. After the creation, you can use MongoDB Compass to check everything went ok. After inserting the recipe, console.log the title of the recipe.
+
+const newRecipe = { title: 'Fruit Salad', cuisine: 'American'};
+
+Recipe.create(newRecipe, (error, Recipe) => {
+  if (error) {
+    console.log('An error happened:', error);
+    return;
+  }
+  console.log('The user is saved and its value is: ', Recipe);
+});
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,48 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+//ITERATION 1 
+// Create a Recipe model inside the file /models/Recipe.model.js. The schema should have the following fields:
+
+// title - type String. It should be required and unique.
+// level - type String. Only can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the ENUM 😉)
+// ingredients - type Array.
+// cuisine - type String. Should be required.
+// dishType - type String. Possible values: Breakfast - Dish - Snack - Drink - Dessert - Other.
+// image - type String. Default value: https://images.media-allrecipes.com/images/75131.jpg.
+// duration - type Number. Min value should be 0.
+// creator - type String
+// created - type Date. By default today.
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true
+  },
+  level: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef' ],
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: String, enum:['Breakfast','Dish', 'Snack', 'Drink', 'Dessert', 'Other'],
+  image: { 
+    type: String, 
+    default: 'https://images.media-allrecipes.com/images/75131.jpg' 
+  },
+  duration: {
+    type: Number, 
+    min: 0
+  },
+  creator: String, 
+  created: {
+    type: Date, 
+    default: Date.today 
+  }
 });
+
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

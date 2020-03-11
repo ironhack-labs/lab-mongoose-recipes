5d8ede228fe7ba439769aa6524c96f56f3e913e3
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-03-11 19:44:32 +0100

    first_schema

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true
+  },
+  level: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: String, enum:['Breakfast','Dish', 'Snack', 'Drink', 'Dessert', 'Other'],
+  image: { 
+    type: String, 
+    default: 'https://images.media-allrecipes.com/images/75131.jpg' 
+  },
+  duration: {
+    type: Number, 
+    min: 0
+  },
+  creator: String, 
+  created: {
+    type: Date, 
+    default: Date.today 
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

a47cb1d7a89448b50841eb697428891d6f7d6364
Alex <alexgarcia1771@outlook.com>
2020-03-11 20:35:12 +0100

    SII

--- index.js
+++ index.js
@@ -11,3 +11,36 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+
+  Recipe.create({ 
+      title: 'ALEX',
+        level: 'Amateur Chef',
+        ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+        cuisine: 'Asian',
+        dishType: 'Dish',
+        image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+        duration: 40,
+        creator: 'Chef LePapu'
+
+  })
+    .then(recipe => { console.log('The recipe is saved and its value is: ' + recipe.title) })
+    .catch(err => { console.log('An error happened:', err)
+});
+
+Recipe.insertMany(data)
+.then(recipe => { recipe.map(data => console.log('The recipe saved is: ' + data.title))})
+.catch(err => { console.log('An error happened:', err)
+});
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: '100'})
+  .then(recipe => console.log('Its update'))
+  .catch(error => console.log(error));
+
+  Recipe.deleteOne({ title: 'Carrot Cake'})
+    .then(recipe => console.log('Eliminada'))
+    .catch(err => console.log(err));
+
+    //Cerrar la conexion
+    mongoose.connection.close()

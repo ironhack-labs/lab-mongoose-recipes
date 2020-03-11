1f0573d9a8ff813c0846ae225029be8334356427
Víctor Ronda <victor.ronda@gmail.com>
2020-03-11 22:20:15 +0100

    DONE

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require('./data.js'); // Import of the data from './data.js'
+const recipe1 = {
+  title: 'Arroz tres delicias',
+  level: 'Amateur Chef',
+  ingredients: ['a lot of rice', '1 omellette', 'a lot of beans', 'ham'],
+  cuisine: 'Mine',
+  dishType: 'Dish',
+  duration: 30,
+  creator: 'Victor Ronda'
+};
 
 // Connection to the database "recipeApp"
 mongoose
@@ -11,3 +20,27 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+ /*  Recipe.create(recipe1)
+      .then(recipe => console.log (recipe.title))
+      .catch(err => console.error('Error connecting to mongo', err)); */
+
+/* Recipe.insertMany(data)
+      .then(recipe => recipe.map(data => console.log(`The recipe is saved and its name is: ${data.title}`)))
+      .catch(err => {console.error('Error connecting to mongo', err)});  */
+
+/* Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+      .then(console.log('Something have been updated! '))
+      .catch(err => {console.error('Error connecting to mongo', err)});  */
+
+ Recipe.deleteOne({title: 'Carrot Cake'})
+      .then(console.log('You have deleted something! '))
+      .catch(err => {console.error('Error connecting to mongo', err)});
+
+
+mongoose.disconnect()
+        .then(console.log('Mongoose disconected'))
+        .catch(err => console.error(err) );
+
+  
+    

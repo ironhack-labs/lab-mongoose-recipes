d295b162d71bb085247bbd933f0a922e8041c45f
Pau Larrea <paularrea12@hotmail.com>
2020-03-11 21:22:26 +0100

    done

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require('./data.js'); // Import of the data from './data.js'
-
+const data1 =       {
+  title: 'Pa amb Tomàquet',
+  level: 'Easy Peasy',
+  ingredients: ['Bread', "1 tomato", 'garlic', 'salt to taste', 'olive oil'],
+  cuisine: 'Catalan',
+  dishType: 'Dish',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 2,
+  creator: 'Pau Larrea'
+}
 // Connection to the database "recipeApp"
 mongoose
   .connect('mongodb://localhost/recipe-app-dev', {
@@ -11,3 +20,21 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+  Recipe.create(data1)
+  .then(recipe => console.log(recipe))
+  .catch(error => console.error(error));
+
+  Recipe.insertMany(data)
+  .then(recipe => recipe.map( data => console.log(data.title)))
+  .catch(error => console.error(error));
+
+  Recipe.update({title: 'Rigatoni alla Genovese'},{ duration: 100}, {new: true})
+  .then(recipe => console.log(recipe))
+  .catch(error => console.error(error));
+
+  Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(recipe => console.log(recipe))
+  .catch(error => console.error(error));
+

ae91691daa5bbf22c63b04cdc79704c09328d933
Jacopo <jacopo.rodighiero@icloud.com>
2020-03-11 20:32:55 +0100

    done

--- index.js
+++ index.js
@@ -2,6 +2,49 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
 const data = require('./data.js'); // Import of the data from './data.js'
 
+
+
+//INTERACTION 2
+
+// Recipe.create(
+//   {
+//     title: 'Orange and Milk-Braised Pork Carnitas',
+//     level: 'UltraPro Chef',
+//     ingredients: ['3 1/2 pounds boneless pork shoulder, cut into large pieces', '1 tablespoon freshly ground black pepper', '1 tablespoon kosher salt, or more to taste', '2 tablespoons vegetable oil', '2 bay leaves', '2 teaspoons ground cumin', '1 teaspoon dried oregano', '1/4 teaspoon cayenne pepper', '1 orange, juiced and zested'],
+//     cuisine: 'American',
+//     dishType: 'Dish',
+//     image: 'https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg',
+//     duration: 160,
+//     creator: 'Chef John'
+//   }
+// )
+// .then(recipe => {console.log('The Recipe has been saved with value: ', recipe)})
+// .catch(err => {console.log('An error happened: ', err)})
+
+
+//INERACTION 3
+
+// Recipe.insertMany(data)
+// .then(data => console.log('Recipies are: ',data))
+// .catch(err => console.log('Error detected: ',err))
+
+//INTERACTION 4
+
+// Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+// .then(data => console.log('Recipies are: ',data))
+// .catch(err => console.log('Error detected: ',err))
+
+//ITERACION 5
+
+Recipe.deleteOne({title: 'Carrot Cake'})
+.then(data => console.log('recipies:', recipies))
+.then(()=>mongoose.disconnect())
+.catch(err => console.log(Recipe[0]))
+
+//ITERACION 6
+
+
+
 // Connection to the database "recipeApp"
 mongoose
   .connect('mongodb://localhost/recipe-app-dev', {
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type: String},
+  level: {type: String},
+  ingredients: {type: Array},
+  cuisine: {type: String},
+  dishType: {type: String},
+  image: {type: String},
+  duration: {type: Number},
+  creator: {type: String},
+  created: {type: Date} 
 });
+
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 module.exports = Recipe;

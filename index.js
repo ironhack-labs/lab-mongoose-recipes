2dd828c786087c9c14b5edb9abd3ed0dfbdab1b2
Yvana Wagter <yvana@wagter.nl>
2020-03-12 14:59:42 +0100

    first commit

--- index.js
+++ index.js
@@ -11,3 +11,52 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+Recipe
+  .create({
+    title: "Falafel",
+    level: "Amateur Chef",
+    ingredients: ["chickpeas", "herbs"],
+    cuisine: "Middle Eastern",
+    dishType: "Snack",
+    duration: 20,
+    creater: "Fabio & Yvana",
+  })
+  .then((recipe) =>{
+    console.log("Recipe created");
+  })
+  .catch((error) =>{
+    console.log("error: ", error);
+  })  
+
+
+Recipe
+  .insertMany(data)
+  .then((recipes) =>{
+    console.log("recipes");
+  })
+  .catch((error) =>{
+    console.log("error: ", error);
+  })
+
+Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+  .then((recipe) =>{
+    console.log("recipe updated");
+  })
+  .catch((error) =>{
+    console.log("error: ", error);
+  })
+
+Recipe
+  .deleteOne({title: "Carrot Cake"})
+  .then((recipe) =>{
+    console.log("recipe removed");
+  })
+  .catch((error) =>{
+    console.log("error: ", error);
+  })
+
+mongoose.connection.close(() => {
+  console.log('Mongoose disconnected');
+  process.exit(0);
+  });

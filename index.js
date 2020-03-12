2f76217560f5919397dff0d5d47591fa74b6771d
Elena Perrone <elenperrone11@gmail.com>
2020-03-12 16:11:10 +0100

    done

--- index.js
+++ index.js
@@ -11,3 +11,36 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+  Recipe.create({
+    title: "Scrambled eggs",
+    level: "Easy Peasy",
+    ingredients: ["eggs", "milk", "salt", "pepper"],
+    cuisine: "American",
+    dishType: "Breakfast",
+    image_url: "https://pngio.com/images/png-a654916.html",
+    duration: "20",
+    creator: "Chef Elena"
+  })
+  .then((recipe) => {
+    console.log("title:", recipe)
+    
+  })
+  .catch((err) => {
+    console.log("Error:", err)
+  });
+  
+  Recipe.insertMany(data) 
+  
+  Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: "100"})
+  .then((recipe) => {
+    console.log('Whoop Whoop!!!')
+  })
+
+
+  Recipe.deleteOne({title: "Carrot Cake"})
+
+  .then(() => {
+    mongoose.connection.close();
+  })
+  // mongoose.disconnect()
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,7 +3,35 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef","UltraPro Chef"]
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ["Breakfast" ,"Dish" ,"Snack" ,"Drink" ,"Dessert" ,"Other"]
+  },
+  image_url: {
+    type: String,
+    default: 'https://github.com/ElenaPerrone/lab-mongoose-recipes.git'
+  },
+  duration: Number,
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-module.exports = Recipe;
+const Recipe = mongoose.model('Recipe', recipeSchema)
+module.exports = Recipe
+

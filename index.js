23ff221bcd5d6d1c1f7975b5acd21f0203f32541
Paula Marsal <paula.mz_376@live.com>
2020-03-12 01:56:19 +0100

    Mision cumplida

--- index.js
+++ index.js
@@ -11,3 +11,49 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+const closeConnection = async () => {
+// Iteration 2: Create a recipe
+const createRecipe = await Recipe.create({
+    title: 'Lemon Mousse',
+    level: 'Easy Peasy',
+    ingredients: ['2 lemons', '1 cup condensed milk', '1 cup cream to assemble'],
+    cuisine: 'Andorran',
+    dishType: 'Dessert',
+    image: 'https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2017/08/moussedelimonylechecondensada.jpg',
+    duration: 15,
+    creator: 'PaulaMZ'
+  })
+  .then(recipe => console.log('The recipe is saved! Title: ' + recipe.title))
+  .catch(error => console.log('An error happened saving a new value: ', error));
+
+// Iteration 3: Insert multiple recipes
+const insertRecipe = await Recipe.insertMany(data)
+  .then(recipe => {
+    recipe.map(recipe => console.log('Many recipes have been saved! Title: ' + recipe.title))
+  })
+  .catch(error => console.log('An error happened saving many recipes: ', error));
+
+
+// Iteration 4: Update recipe
+const updateRecipe = await Recipe.updateOne({
+    title: 'Rigatoni alla Genovese'
+  }, {
+    duration: 100
+  })
+  .then(console.log('Update Succes!!'))
+  .catch(error => console.log('An error happened updating: ', error));
+
+// Iteration 5: Remove a recipe
+const deleteRecipe = await Recipe.deleteOne({
+    title: 'Carrot Cake'
+  })
+  .then(console.log('Remove Succes!!'))
+  .catch(error => console.log('An error happened removing: ', error));
+
+  await mongoose.disconnect();
+};
+
+//Iteration 6: Close the Database
+closeConnection();
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,40 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  // Iteration 1: Recipe Schemaki8
+  title: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+    default: 'Easy Peasy'
+  },
+  ingredients: Array,
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other'],
+    default: 'Breakfast'
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

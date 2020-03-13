20b6029d13aca20614806956cc8db324ae0d94d4
Andre <andre.talpo@gmail.com>
2020-03-12 22:14:53 -0300

    Done

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+// const promise1 = Recipe.create(new Recipe({
+//   title: 'Vai cavalo',
+//   level: 'Amateur Chef',
+//   ingredients: ['cavalo', 'vontade'],
+//   cuisine: ['Brasileira'],
+//   dishType: 'Dish',
+//   duration: 50,
+//   creator: 'Andre Talpo'
+// }));
+
+const recipes = data.map((element) => new Recipe(element));
+const promise2 = Recipe.insertMany(recipes, (err, response) => {
+  if (err) console.log('Deu ruim');
+  else console.log('Sucesso demais');
+});
+
+const promise3 = Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, (err, response) => {
+  if (err) console.log('Deu ruim');
+  else console.log('Sucesso demais');
+});
+
+const promise4 = Recipe.deleteOne({ title: 'Carrot Cake' }, (err, response) => {
+  if (err) console.log('Deu ruim.');
+  else console.log('Sucesso demais.');
+})
+
+Promise.all([promise2, promise3, promise4])
+.finally(() => {
+  mongoose.connection.close();
+  console.log('Fechado');
+});

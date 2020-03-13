34901c50099e460d28fd92a455f90c715e6ce440
Joao Juliatti <joaopjuliatti@gmail.com>
2020-03-12 21:28:35 -0300

    done

--- index.js
+++ index.js
@@ -4,10 +4,39 @@ const data = require('./data.js'); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
 mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
+  .connect('mongodb://localhost:27017/recipe-app-dev', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+const newRecipe = {
+  title : 'My first Recipe',
+  level : 'Easy Peasy',
+  ingredients: ['water','fridge'],
+  cuisine: 'Internacional',
+  dishType: 'Other',
+  duration : 60,
+  creator: 'God',
+};
+
+  // Recipe.create(new Recipe(newRecipe))
+  // .then(recipe =>{console.log(receipe.title)})
+
+  // Recipe.insertMany(data)
+  //   .then(recipes => {
+  //     for(let i =0;i<recipes.length;i++)
+  //       console.log(recipes[i].title)
+  //   })
+
+  // Recipe.findOne({title:'Rigatoni alla Genovese'})
+  //   .then(async recipe => {
+  //     recipe.duration =100
+  //     await recipe.save()
+  //   })
+
+  
+  // Recipe.deleteOne({title:'Carrot Cake'})
+  //   .then( () => console.log('Delete completed'))
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title : { type:String, require:true, unique:true},
+  level : { type:String , enum:['Easy Peasy','Amateur Chef','UltraPro Chef']},
+  ingredients: {type:Array},
+  cuisine: { type:String, require:true},
+  dishType: { type:String, enum:['Breakfast','Dish','Snack','Dessert','Other']},
+  duration : { type:Number, min:0},
+  creator: { type:String},
+  created: { type:Date, default: Date.now()}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

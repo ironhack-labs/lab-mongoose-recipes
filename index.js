684474c2c3962923a6a03c63a80f78807f8a88b3
unknown <ahabey7@gmail.com>
2020-03-20 16:58:02 +0100

    changes

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
-const data = require('./data.js'); // Import of the data from './data.js'
+  const mongoose = require('mongoose');
+  const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe.model.js'
+  const data = require('./data.js'); // Import of the data from './data.js'
 
-// Connection to the database "recipeApp"
-mongoose
-  .connect('mongodb://localhost/recipe-app-dev', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  // Connection to the database "recipeApp"
+  mongoose
+    .connect('mongodb://localhost/recipe-app-dev', {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch(err => console.error('Error connecting to mongo', err));
 
 
-// Recipe.create({ title: 'Red Chicken', 
-//                 level:"Easy Peasy", 
-//                 ingredients: ["salt","pepper"],
-//                 cuisine:"Oriental",
-//                 dishType:"Dish",
-//                 image:"Hello",
-//                 creator:"Aris",}, function (err, user) {
-//   if (err) {  
-//       console.log('An error happened:', err);
-//   } else {
-//       console.log('The recipe is saved and its title is: ', user.title);
-//   }
-// });
+ Recipe.create({ title: 'Red Chicken', 
+                 level:"Easy Peasy", 
+                 ingredients: ["salt","pepper"],
+                 cuisine:"Oriental",
+                 dishType:"Dish",
+                 image:"Hello",
+                 creator:"Aris",}, function (err, user) {
+   if (err) {  
+       console.log('An error happened:', err);
+   } else {
+       console.log('The recipe is saved and its title is: ', user.title);
+   }
+ });
 
 //Iteration 3
 Recipe.insertMany(data) 
@@ -40,8 +40,8 @@ Recipe.insertMany(data)
 //Iteration 4
 Recipe
     .findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
-    .then((Recipe)=> {
-        console.log("updated",Recipe.title);
+    .then((recipe)=> {
+        console.log("recipe was changed");
     })
     .catch((error)=> {
         console.log("error: ", error);
@@ -51,7 +51,7 @@ Recipe
     //Iteration 4
     Recipe
         .deleteOne({ name: 'Carrot Cake' })
-        .then((Recipe)=> {
+        .then((recipe)=> {
           console.log("Carrot Cake DELETED!");
       })
       .catch((error)=> {
@@ -67,6 +67,7 @@ Recipe
         })
         .catch((error)=> {
           console.log("error: ", error);
+          process.exit(0);
       })
 
       

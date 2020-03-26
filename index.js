3e05209df78ef451e92276f28a5abbda1ec7512f
Thiago Pizzutti <tpizzutti@gmail.com>
2020-03-26 18:19:45 -0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const recipe1 = new Recipe({
+  title: 'Coffee',
+  level: 'Easy Peasy',
+  ingredients: [
+    "4 cups of coffee",
+    "1L of Water",
+    "4 teaspoons of sugar",
+    "Paper Filter",
+  ],
+  cuisine: "world cuisine",
+  dishType: "drink",
+  image: "https://en.wikipedia.org/wiki/Coffee",
+  duration: 15,
+  creator: "Massao & Thiago",
+}
+)
+
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+
+
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -20,8 +42,48 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+    
+    recipe1.save()
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+        
+    let recipes = []
+    data.forEach(rec => {
+      recipes.push(new Recipe(rec))
+    })
+    Recipe.insertMany(recipes)
+      .then(response => {
+        Recipe.findOneAndUpdate({
+            title: 'Rigatoni alla Genovese'
+          }, {
+            duration: 100
+          })
+          .then(response => console.log("Ok"))
+          .catch(err => console.log(err))
+
+        Recipe.deleteOne({
+            title: "Carrot Cake"
+          })
+          .then(response => console.log("OK"))
+          .catch(err => console.log(err))
+      })
+      .catch(err => console.log(err))
+})
+
+    
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log('Mongoose default connection disconnected through app termination');
+      process.exit(0);
+    });
+  });
+
+
+
+
+

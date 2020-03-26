a028999b9adb9b07d8b0b37cb04317713e149369
Daniel Paiva <danielcspaiva@gmail.com>
2020-03-26 18:11:57 -0300

    done

--- index.js
+++ index.js
@@ -19,8 +19,49 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+        title: 'Bolo da vovó',
+        level: 'Easy Peasy',
+        ingredients: ['farinha', 'ovos', 'amor', 'carinho'],
+        cuisine: 'Comida da vovó',
+        dishType: 'snack',
+        image: 'https://cdn.guiadacozinha.com.br/wp-content/uploads/2019/10/bolo-da-vovo-43692.jpg',
+        duration: 30,
+        creator: 'Vovó Marlete',
+        created: "1950-01-26"
+      })
+      .then(recipe => {
+        Recipe
+          .insertMany(data)
+          .then(recipes => {
+            Recipe
+              .updateOne({
+                title: 'Rigatoni alla Genovese'
+              }, {
+                duration: 100
+              })
+              .then(recipe => {
+                Recipe
+                  .deleteOne({
+                    title: 'Carrot Cake'
+                  })
+                  .then(recipe => {
+                    console.log(`${recipe.title} was deleted`)
+                    mongoose.connection.close()
+                  })
+                  .catch(error => console.log(error))
+                console.log(`${recipe.title} was updated`)
+              })
+              .catch(error => console.log(error))
+            console.log(`${recipes.length} recipes added`)
+          })
+          .catch(error => console.log(error))
+        console.log(recipe.title)
+      })
+      .catch(err => console.log(err))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

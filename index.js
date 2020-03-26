b25596a3da373dff3c3f57d4031c4eb01fdf232d
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-03-26 18:32:08 -0300

    Finish mongoose exercise

--- index.js
+++ index.js
@@ -21,7 +21,58 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Espetinho de Gato",
+      level: "Easy Peasy",
+      ingredients:[
+        "gato",
+        "carvão",
+        "sal grosso"
+      ],
+      cuisine:"Ogrosine",
+      dishType:"Main Course",
+      image: "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.naoentreaki.com.br%2F8067984-churrasquinho-de-gato.htm&psig=AOvVaw398Qi_lGTqEKR9xZiEG7x3&ust=1585342687884000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJiooPWDuegCFQAAAAAdAAAAABAD",
+      duration: 100,
+      creator: "Gustavo & Rafael",
+      created: new Date
+      
+    })
+    Recipe.insertMany(data);
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  //Print all recipe's title
+  Recipe
+    .find({},{title: 1, _id:0})
+    .then(response => response.forEach(a => console.log(a.title)))
+    .catch(error => console.log(error))
+
+  //Find Rigatoni recipe and update duration
+  Recipe
+    .findOneAndUpdate({title: /riga/i},{$set: {duration: 100}}, {useFindAndModify: false})
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => console.log(error))
+
+  // Print Rigatoni with new duration
+  Recipe
+    .find({title: /riga/i},{duration: 1, _id:0})
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
+  // Remove Carrot Cake
+  Recipe
+    .deleteOne({title: /cake/i})
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+// Close database
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Terminated Node server and Disconnected from Database');
+    process.exit(0);
+  });
+});
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: Array,
+  cuisine: String,
+  dishType: String,
+  image: String,
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created:{
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

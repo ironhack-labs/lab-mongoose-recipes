4fbb5d367aa0d5d92148422186a65437f6531e95
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-03-29 16:40:16 -0300

    change then/catch to async/await

--- index.js
+++ index.js
@@ -1,79 +1,88 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
+// Our custom recipe
 const strogonoff = new Recipe({
-  title: "Strogonoff",
+	title: "Strogonoff",
 	level: "Easy Peasy",
 	ingredients: ["Chicken", "Champignom", "Ketchup", "Garlic", "Mustard", "Onions", "Table Cream", "Pepper"],
 	cuisine: "Brazillian",
 	dishType: "Main_course",
 	duration: 30,
-	creator: "Chef Bruno",
-})
+	creator: "Chef Bruno"
+});
 
-const insertStrogonoff = () => {
-  Recipe
-    .create(strogonoff)
-    .then(recipe => console.log(recipe.title))
-    .catch(error => console.log(error));
-}
+const run = async () => {
+	// Connecting to the database
+	try {
+		const self = await mongoose.connect(MONGODB_URI, {
+			useCreateIndex: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+		console.log(`Connected to the database: "${self.connection.name}"`);
+		await self.connection.dropDatabase();
+	} catch (err) {
+		console.log("Error connecting to the database", err);
+	}
 
-const deleteRecipe = () => {
-  Recipe
-    .deleteOne({title: "Carrot Cake"})
-    .then(() => {
-      console.log("Recipe deleted")
-      mongoose.disconnect();
-    })
-    .catch(error => console.log(error))
-}
+	// Adding our own recipe
+	try {
+		const recipe = await Recipe.create(strogonoff);
+		console.log(`New recipe added: ${recipe.title}`);
+	} catch (err) {
+		console.log("Error adding new recipe to the database", err);
+	}
 
-const uptadeRecipe = () => { 
-  Recipe
-    .findOneAndUpdate({
-      title: "Rigatoni alla Genovese"
-    }, {
-      duration: 100
-    }, {
-      new: true
-    })
-    .then(updatedRecipe => {
-      console.log("Updated Recipe:", updatedRecipe)
-      deleteRecipe();
-    })
-    .catch(error => console.log(error))
-}
+	// Adding all given recipes
+	try {
+		const recipes = await Recipe.insertMany(data);
+		recipes.forEach(recipe => console.log(`New recipe added: ${recipe.title}`));
+	} catch (err) {
+		console.log("Error adding new recipes to the database", err);
+	}
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    insertStrogonoff()
-    Recipe
-    .insertMany(data)
-    .then(recipes => {
-      recipes.forEach(recipe => console.log(recipe.title));
-      uptadeRecipe();
-    })
-    .catch(error => console.log(error));
-    })
-    .catch(error => {
-      console.error('Error connecting to the database', error);
-    });
-    
+	// Updating a recipe
+	try {
+		const updatedRecipe = await Recipe.findOneAndUpdate(
+			{
+				title: "Rigatoni alla Genovese"
+			},
+			{
+				duration: 100
+			},
+			{
+				new: true
+			}
+		);
+		console.log("A recipe has been updated:", updatedRecipe);
+	} catch (err) {
+		console.log("Error updating recipe in the database", err);
+	}
+
+	// Deleting a recipe
+	try {
+		await Recipe.deleteOne({
+			title: "Carrot Cake"
+		});
+		console.log("A recipe has been deleted");
+	} catch (err) {
+		console.log("Error deleting recipe in the database", err);
+	}
+
+	// Disconnecting from the database
+	try {
+		await mongoose.disconnect();
+		console.log(`Disconnected to the database: "${self.connection.name}"`);
+	} catch (err) {
+		console.log("Error disconnecting to the database", err);
+	}
+};
+
+run();

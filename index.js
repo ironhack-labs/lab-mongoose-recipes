4056b1975e5743102d41323aa1e7765708c7a755
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-03-30 11:02:35 -0300

    reduce  many try/catch  to just one

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require("./models/Recipe.model");
+const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require("./data");
+const data = require('./data');
 
-const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Our custom recipe
 const strogonoff = new Recipe({
-	title: "Strogonoff",
-	level: "Easy Peasy",
-	ingredients: ["Chicken", "Champignom", "Ketchup", "Garlic", "Mustard", "Onions", "Table Cream", "Pepper"],
-	cuisine: "Brazillian",
-	dishType: "Main_course",
+	title: 'Strogonoff',
+	level: 'Easy Peasy',
+	ingredients: ['Chicken', 'Champignom', 'Ketchup', 'Garlic', 'Mustard', 'Onions', 'Table Cream', 'Pepper'],
+	cuisine: 'Brazillian',
+	dishType: 'Main_course',
 	duration: 30,
-	creator: "Chef Bruno"
+	creator: 'Chef Bruno'
 });
 
 const run = async () => {
@@ -28,31 +28,19 @@ const run = async () => {
 		});
 		console.log(`Connected to the database: "${self.connection.name}"`);
 		await self.connection.dropDatabase();
-	} catch (err) {
-		console.log("Error connecting to the database", err);
-	}
 
-	// Adding our own recipe
-	try {
+		// Adding our own recipe
 		const recipe = await Recipe.create(strogonoff);
 		console.log(`New recipe added: ${recipe.title}`);
-	} catch (err) {
-		console.log("Error adding new recipe to the database", err);
-	}
 
-	// Adding all given recipes
-	try {
+		// Adding all given recipes
 		const recipes = await Recipe.insertMany(data);
 		recipes.forEach(recipe => console.log(`New recipe added: ${recipe.title}`));
-	} catch (err) {
-		console.log("Error adding new recipes to the database", err);
-	}
 
-	// Updating a recipe
-	try {
+		// Updating a recipe
 		const updatedRecipe = await Recipe.findOneAndUpdate(
 			{
-				title: "Rigatoni alla Genovese"
+				title: 'Rigatoni alla Genovese'
 			},
 			{
 				duration: 100
@@ -61,27 +49,19 @@ const run = async () => {
 				new: true
 			}
 		);
-		console.log("A recipe has been updated:", updatedRecipe);
-	} catch (err) {
-		console.log("Error updating recipe in the database", err);
-	}
+		console.log('A recipe has been updated:', updatedRecipe);
 
-	// Deleting a recipe
-	try {
+		// Deleting a recipe
 		await Recipe.deleteOne({
-			title: "Carrot Cake"
+			title: 'Carrot Cake'
 		});
-		console.log("A recipe has been deleted");
-	} catch (err) {
-		console.log("Error deleting recipe in the database", err);
-	}
+		console.log('A recipe has been deleted');
 
-	// Disconnecting from the database
-	try {
+		// Disconnecting from the database
 		await mongoose.disconnect();
 		console.log(`Disconnected to the database: "${self.connection.name}"`);
 	} catch (err) {
-		console.log("Error disconnecting to the database", err);
+		console.log('Error:', err);
 	}
 };
 

3d0c60c8abb119dcf14ca1041eb5a63f7bcd4174
Manuel <mbuergel@gmx.de>
2020-04-04 17:55:08 +0200

    done

--- index.js
+++ index.js
@@ -12,16 +12,41 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
+
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    Recipe.create({title: "tofu", level: "Easy Peasy", ingredients: ["tofu", "oil"], cuisine: "global", dishType: "other"}).then((recipe) => {
+      console.log(recipe)
+    })
+
+    Recipe.insertMany(data).then((result) =>  {
+      
+      console.log(result.map(el => el.title) )
+
+      Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {$set: {duration: 100}}, {new: true}).then(() => {
+        console.log("recipe updated")
+      } ) 
+
+      Recipe.deleteOne({title: "Carrot Cake"}).then(() => {
+        console.log("Carrot Cake deleted")
+
+        mongoose.connection.close(() => {
+          console.log("connection closed")
+      } )
+      })
+    })
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

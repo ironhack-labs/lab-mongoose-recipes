004328fe199624a596aec7382290d57def5a392b
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-04-06 23:01:44 -0500

    Updating and deleting recipes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose
   })
   .then(() => {
     // Iteration 2 Create a recipe
-    createRecipe("Shrimp", "Easy Peasy", ["shrimp", "garlic", "butter"], "Mexican", "main_course")
+    createRecipe("Shrimp", "Easy Peasy", ["shrimp", "garlic", "butter"], "Mexican", "main_course", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.foodnetwork.com%2Frecipes%2Frachael-ray%2F4-minute-spicy-garlic-shrimp-recipe-1915190&psig=AOvVaw2NHoYQZ4a1DrodTuh73cpi&ust=1586317051332000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIiBxtWx1egCFQAAAAAdAAAAABAD", 20, "Chef Gladys")
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
@@ -35,3 +35,26 @@ function createRecipe(title, level, ingredients, cuisine, dishType, image, durat
   .catch((err) => console.error(`Error adding recipe`));
 }
 
+// Iteration 3 Insert multiple recipes -example
+
+// const doc1 = { "name": "basketball", "category": "sports", "quantity": 20, "reviews": [] };
+// const doc2 = { "name": "football",   "category": "sports", "quantity": 30, "reviews": [] };
+
+// return itemsCollection.insertMany([doc1, doc2])
+//   .then(result => {
+//     console.log(`Successfully inserted ${result.insertedIds.length} items!`);
+//     return result
+//   })
+//   .catch(err => console.error(`Failed to insert documents: ${err}`))
+
+// Iteration 4 - Update recipe
+
+Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  .then((x) => console.log("Recipe updated"))
+  .catch((err) => console.error("Error updating recipe"));
+
+// Iteration 5 - Remove a recipe
+
+Recipe.deleteOne({ title: 'Carrot Cake' })
+.then((y) => console.log("Recipe deleted"))
+.catch((err) => console.error("Error deleting recipe"));
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -42,3 +42,5 @@ const recipeSchema = new Schema({
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+
+

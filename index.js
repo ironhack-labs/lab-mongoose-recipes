c83a4a20d518fb5d7c4b2a4aa33178e989339dc5
dkoushha <dimakoushha@gmail.com>
2020-04-07 18:34:29 +0200

    update.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model.js');
+const Recipe = require("./models/Recipe.model.js");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
@@ -31,34 +31,37 @@ mongoose
         "3 tablespoons Asian chili garlic sauce",
         "3 tablespoons minced garlic",
         "salt to taste",
-        "8 skinless, boneless chicken thighs"
+        "8 skinless, boneless chicken thighs",
       ],
       cuisine: "Asian",
       dishType: "main_course",
       image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
       duration: 40,
-      creator: "Chef LePapu"
+      creator: "Chef LePapu",
     }).then(() => {
-      console.log(recipe);
-    })
+      Recipe.insertMany(data).then(() => {
+        Recipe.findOneAndUpdate({
+            title: "Rigatoni alla Genovese",
+          }, {
+            duration: 100,
+          })
+          .then(() => {
+            return Recipe.findOneAndDelete({
+              title: "Carrot Cake",
+            });
+          })
+          .then(() => {
+            mongoose.connection.close(() => {
+              console.log(
+                "Mongoose default connection disconnected through app termination"
+              );
+            });
+          });
+      });
+    });
 
-    Recipe.insertMany(data).then(() => {
-      Recipe.findOneAndUpdate({
-        title: 'Rigatoni alla Genovese'
-      }, {
-        duration: 100
-      }).then(() => {
 
-      })
-      Recipe.findOneAndDelete({
-        title: 'Carrot Cake'
-      }).then(() => {
-        mongoose.connection.close(() => {
-          console.log('Mongoose default connection disconnected through app termination');
-        })
-      })
-    })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

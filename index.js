cd0d4ea34da387844459aea1c59fd6c9fbcf7886
PiaG <piagmdf@gmail.com>
2020-04-07 19:30:54 +0200

    insert improvement from recap

--- index.js
+++ index.js
@@ -22,36 +22,71 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    // create just one document inside db
-    Recipe.create({
-      title: 'Pancake',
-      level: 'Amateur Chef',
-      ingredients: ['eggs', 'milk', 'flour'],
-      cuisine: 'Berlin',
-      dishType: 'dessert', 
-      image: '',
-      duration: 30,
-      creator: 'me'
-    })
+      // Run your code here, after you have insured that the connection was made
+      // create just one document inside db
+      Recipe.create({
+        title: 'Pancake',
+        level: 'Amateur Chef',
+        ingredients: ['eggs', 'milk', 'flour'],
+        cuisine: 'Berlin',
+        dishType: 'dessert',
+        image: '',
+        duration: 30,
+        creator: 'me'
+      })
 
-    // insert the whole data.json into db
-    Recipe.insertMany(data)
-    console.log('log data inside db:' + data)
+      /*     // insert the whole data.json into db
+          Recipe.insertMany(data)
+          console.log('log data inside db:' + data)
 
-    // update an existing document inside db
-    Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true }).then(() => {
-      console.log('successfully updated Rigatoni' + data.title)
-    })
-    
-    // remove recipe
-    Recipe.deleteOne({ title: "Carrot Cake"}).then(() => {
-      console.log('Carrot Cake deleted - what a shame')
-    })
-    
+          // update an existing document inside db
+          Recipe.findOneAndUpdate({
+            title: "Rigatoni alla Genovese"
+          }, {
+            duration: 100
+          }, {
+            new: true
+          }).then(() => {
+            console.log('successfully updated Rigatoni' + data)
+          })
+
+          // remove recipe
+          Recipe.deleteOne({
+            title: "Carrot Cake"
+          }).then(() => {
+            console.log('Carrot Cake deleted - what a shame')
+          }) */
+
+      // better: wait for the insert to happen AND findbyID
+      // insert the whole data.json into db
+      Recipe.insertMany(data).then(() => {
+        console.log('log data inside db:' + data)
+
+        // update an existing document inside db
+        let promise1 = Recipe.findOneAndUpdate({
+          title: "Rigatoni alla Genovese"
+        }, {
+          duration: 100
+        }, {
+          new: true
+        }).then(() => {
+          console.log('successfully updated Rigatoni' + data)
+        })
+
+        let promise2 = Recipe.deleteOne({
+          title: "Carrot Cake"
+        }).then(() => {
+          console.log('Carrot Cake deleted - what a shame')
+        })
+
+        // close the connection to the DB      
+
+        Promise.all([promise1, promise2]).then(() => {
+          mongoose.connection.close()
+          console.log(`disconnected from database`);
+        })
+      })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
-
-  
+.catch(error => {
+  console.error('Error connecting to the database', error);
+});

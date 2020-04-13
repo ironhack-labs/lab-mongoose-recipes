a9c4301bf802ff6f780359498660d38c48509eee
Yuri Jaen <yurimarkjaen@gmail.com>
2020-04-13 22:07:47 +0100

    Completed all iterations, but without functioning, because could not fix iteration 2...

--- index.js
+++ index.js
@@ -19,9 +19,61 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+  //why do we have to have this in a .then? why can't it exist separately like in the example?
+  /* .then(data => { // it's asking us to pull the very first recipe of the json file?
+      Recipe.create(data[0))
+        .then(recipes => {
+        console.log('Recipe has been made: ', recipe)
+        }).catch(error => {
+        console.log('An error happened while saving the recipe:', error)
+        }) */
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .then(data => {
+      Recipe.create(data)
+        .then(recipe => {
+          console.log('Recipe has been made: ', recipe)
+        }).catch(error => {
+          console.log('An error happened while saving the recipe:', error)
+        })
+
+      // model has been exported, and schema has been defined.
+      // model static is essentially a method? 
+      // we are getting our "recipe" from data.json and passing it on to .create, but we only get the first recipe from data.json, since it only says one? 
+
+      //just gonna do it piecemeal instead...since it's not working when nested all together...
+
+      Recipe.insertMany(data) //for each? 
+      then.(recipes => {
+        recipes.forEach(recipe => {
+          console.log(`recipe title: ${recipe.title}`)
+        })
+      });
+
+      Recipe.findOneAndUpdate({
+        title: 'Rigatoni alla Genovese'
+      }, {
+        duration: 100
+      })
+      then.(recipe => {
+        console.log('Recipe has been sucesfully updated!', recipe)
+      })
+      catch.(error => {
+        console.error('Error, could not update recipe duration', error);
+      });
+
+      Recipe.deleteOne({
+          title: 'Carrot Cake'
+        })
+        .then(recipe => {
+          console.log('The carrot cake recipe is now deleted')
+        })
+        .catch(error => {
+          console.log('Error, the carrot cake recipe could not be deleted', error)
+        });
+
+
+      mongoose.connection.close(() => {
+        console.log('Mongoose default connection disconnected through app terminal.')
+      });

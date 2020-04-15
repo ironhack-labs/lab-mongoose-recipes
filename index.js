41739a5b25cdb0ac61a58ed4519f76535a04d0d2
Anne Patana <apatana@hotmail.com>
2020-04-16 00:20:39 +0200

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -21,7 +22,62 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+  
+    Recipe.create({
+    title: "Omelette",
+    level: "Easy Peasy",
+    ingredients: ["eggs", "butter", "salt", "pepper", "cooking oil"],
+    cuisine: "International",
+    dishType: "breakfast",
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((dbResponse) => {
+    console.log(dbResponse);
+
+    Recipe.insertMany(data)
+    .then((dbResponse) => {
+      data.forEach((element) => {
+        console.log(element.title + " has been added to the database");
+      });
+  
+      Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+      .then((dbResponse) => {
+        console.log("Duration has been successfully updated in the database for " + dbResponse.title);
+
+        Recipe.deleteOne({title: 'Carrot Cake'})
+        .then((dbResponse) => {
+          console.log("Carrot Cake has been removed from the database");
+        })
+
+        .catch((dbErr) => {
+          console.log(dbErr);
+        });
+      
+      })
+      .catch((dbErr) => {
+        console.log(dbErr);
+      });
+  
+    })
+    .catch((dbErr) => {
+      console.log(dbErr);
+    });
+
+  })
+  .catch((dbErr) => {
+    console.log(dbErr);
   });
+  
+})
+// .catch((dbErr) => {
+//   console.log(dbErr);
+// });
+
+.catch(error => {
+  console.error('Error connecting to the database', error);
+});
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});

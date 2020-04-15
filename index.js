7c1031906b50db6816546b0c391b9f0d5eddd7cd
Julian Kizeno <julianquiceno20@gmail.com>
2020-04-15 18:32:16 +0200

    almost done

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    console.log('La base de datos está vacía');
+    return Recipe.create({ title: "Las Recetas de Juli y Gaby" })
+  })  
+  .then((theRecipe) => {
+    console.log("El titulo de la receta es:", theRecipe.title);
+    return Recipe.create(data)
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((allTheRecipes) => {
+    console.log("Esto es:", allTheRecipes);
   });
+  

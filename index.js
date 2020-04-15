7c728945029b9e0ec8a2dd8c6d7351fab666243d
William Hermozo <william.hermozo@protonmail.com>
2020-04-15 17:48:12 +0200

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
@@ -21,7 +21,30 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    databaseCycle();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+async function databaseCycle() {
+  try {
+    await Recipe.create({
+      title: "Pâtes au beurre",
+      level: "UltraPro Chef",
+      ingredients: ["pâtes", "beurre", "eau qui bout un peu mais pas top"],
+      cuisine: "Etudiant fauché",
+      dishType: "main_course",
+      duration: 13,
+      creator: "Mr. Pâtes",
+    }).then(dbRes => console.log(`The "${dbRes.title}" recipe has been added`))
+    await Recipe.insertMany(data).then(dbRes => { dbRes.forEach(e => { console.log(`Recipe title: ${e.title}`) }) })
+    await Recipe
+      .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+      .then(console.log("The Rigatoni alla Genovese recipe has been updated!"))
+    await Recipe.deleteOne({ title: "Carrot Cake" }).then(console.log("The Carrot Cake recipe has been deleted!"))
+    await mongoose.connection.close(() => console.log("Database update successful, connection closed"))
+  } catch (dbErr) {
+    console.log("something went wrong:" + dbErr);
+  }
+}

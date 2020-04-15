1c587583a0d12675321315e49c5d79eff6af7759
Bruno <bpais@outlook.pt>
2020-04-15 18:33:37 +0100

    done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 9*/
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -21,6 +22,56 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    // Iteration 2:
+    console.log(`Conected to MongoDB`); // insures the connection was made
+    mongoose.connection.db.dropDatabase(); // resets the data base
+    Recipe.create({
+      title: 'Sweet poatato, carrot and pumpkin soup',
+      level: 'Amateur Chef',
+      ingredients: ['sweet potato', 'carrots', 'pumpkin','"xuxu"', 'french garlic', 'garlic', 'onion', 'olive oil', 'salt', 'pepper'],
+      cuisine: 'mediteranian',
+      dishType: 'soup',
+      image: '',
+      duration : 45,
+      creator: 'Michel Martinho',
+      created: Date.now
+    }) // Recipe created
+    .then((recipe) => {
+      console.log(`The recipe was created and saved as: ${recipe}`) // Recipe creation confirm
+
+      // Iteration 3:
+      Recipe.insertMany(data, (error, recipes) => {
+        recipes.forEach(dataTitle => {
+          console.log(`Created new recipe: ${dataTitle.title}`);
+        });
+      })
+      .then((recipes) => {
+        console.log(`The recipes were created and they'r values are: ${recipes}`); // Recipes addition confirmation
+
+        // Iteration 4:
+        Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese', duration: 100, new: true })
+        .then((recipe) => {
+          console.log(`The ${recipe} was updated`); // Update confirmation
+
+          // Iteration 5:
+          Recipe.deleteOne({ title: 'Carrot Cake' })
+          .then((recipe) => {
+            console.log(`Removed the recipe: ${recipe}`); // Deletion confirmation
+
+            // Iteration 6:
+            mongoose.connection.close(() => {
+              console.log('Mongoose conection closed!'); // Closed conection confirmation
+            })
+            .catch((err) => {console.log(`An error as ocoured: ${err}`);}); // Error on closing conection
+          })
+          .catch((err) => {console.log(`An error as ocoured: ${err}`);}); // Error on recipe deletion
+        })
+        .catch((err) => {console.log(`An error as ocoured: ${err}`);}); // Error on updating recipe
+      })
+      .catch((err) => {console.log(`An error as ocoured: ${err}`);}); // Error on inserting recipes
+    })
+    .catch((err) => {console.log(`An error as ocoured: ${err}`);}); // Error on creating recipe
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

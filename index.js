531951dbd887711bb15423fdc91e231c9408bd4c
Rebecca Norén <rebecca.noren@iteam.se>
2020-04-15 16:44:45 +0200

    done

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+let rebeccasRecipe = {
+  title: "A simple cake",
+  level: "Easy Peasy",
+  ingredients: ["Water", "Sugar", "Oatmeal"],
+  cuisine: "Swedish",
+  dishType: "dessert",
+  duration: 30,
+}
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -20,8 +28,43 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create(rebeccasRecipe) //  Iteration 2  Create a recipe
+      .then(recipe => {
+        console.log(recipe.title)
+        Recipe.insertMany(data) //  Iteration 3 – Insert multiple recipes
+          .then(recipesInDb => {
+            data.map(recipe => console.log(recipe.title))
+            Recipe.updateOne({ //  Iteration 4 - Update recipe
+                title: "Rigatona alla Genovese"
+              }, {
+                duration: 100
+              }, {
+                new: true
+              })
+              .then(() => {
+                console.log("Duration changed")
+                Recipe.deleteOne({ //  Iteration 5 - Remove a recipe
+                    title: "Carrot Cake"
+                  })
+                  .then(recipe => {
+                    console.log("Carrot Cake is removed from DB!")
+                  }).catch(error => {
+                    console.error(error);
+                  })
+              }).catch(error => {
+                console.error(error);
+              })
+          }).catch(error => {
+            console.error(error);
+          })
+      }).catch(error => {
+        console.error(error);
+      })
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.error(error);
+  })
+//  Iteration 6 - Close the Database
+// .finally(() => {
+//   mongoose.connection.close();
+// });

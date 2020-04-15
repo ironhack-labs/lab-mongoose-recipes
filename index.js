e5eed13ae45b59068d518f5a5457c2a6d8890b3e
vlp90 <vlp90@DESKTOP-VUTPP2C.localdomain>
2020-04-15 15:30:45 +0200

    lab done

--- index.js
+++ index.js
@@ -9,19 +9,69 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+	.connect(MONGODB_URI, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then((self) => {
+		console.log(`Connected to the database: "${self.connection.name}"`);
+		// Before adding any documents to the database, let's delete all previous entries
+		return self.connection.dropDatabase();
+	})
+	.then(() => {
+		// ITERATION 2 - CREATE RECIPE
+		Recipe.create({
+			title: 'Pasta Bolognaise',
+			level: 'Amateur Chef',
+			ingredients: [ 'pasta', 'meat', 'cheese', 'salt', 'pepper', 'onions' ],
+			cuisine: 'Italian',
+			dishType: 'main_course',
+			duration: 30,
+			creator: 'Vladimir'
+		})
+			.then((dbResponse) => {
+				console.log(`* Recipe : ${dbResponse.title} has been CREATED`);
+				// Recipe.insertMany(data).then((dbResponse) => { }).catch((dbErr) => {});
+
+				// ITERATION 3 - INSERT RECIPE FROM DATA BASE
+				Recipe.insertMany(data)
+					.then((dbResponse) => {
+						data.forEach((element) => {
+							console.log(`** Recipe : ${element.title} -- has been ADDED to the dataBase`);
+						});
+
+						// ITERATION 4 - FIND AND UPDATE
+						Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true })
+							.then((dbResponse) => {
+								console.log(`*** Recipe : ${dbResponse.title} -- has been UPDATED to the dataBase`);
+
+								// ITERATION 5 - FIND AND REMOVE
+								Recipe.deleteOne({ title: 'Carrot Cake' })
+									.then((dbResponse) => {
+										//console.log(dbResponse);
+										console.log(`**** No more Carrot Cake, sorry...`);
+									})
+									.catch((dbErr) => {
+										console.log(dbErr);
+									});
+							})
+							.catch((dbErr) => {
+								console.log(dbErr);
+							});
+					})
+					.catch((dbErr) => {
+						console.log(dbErr);
+					});
+			})
+			.catch((dbErr) => {
+				console.log(dbErr);
+			});
+	})
+	.catch((error) => {
+		console.error('Error connecting to the database', error);
+	})
+	.finally(() => {
+		// ITERATION 6 - CLOSE DATABASE
+		//mongoose.connection.close();
+	});

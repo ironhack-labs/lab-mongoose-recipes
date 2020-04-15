9be133d1a97d3165a88f22dc1259bebc663af36a
Guillaume Marcellin <marcellinguillaume@hotmail.fr>
2020-04-15 15:10:02 +0200

    done

--- index.js
+++ index.js
@@ -20,7 +20,66 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+        title: "Crêpes",
+        level: "Amateur Chef",
+        ingredients: ["farine", "oeufs", "sucre", "huile", "beurre", "lait", "rhum"],
+        cuisine: "French",
+        dishType: "dessert",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg-3.journaldesfemmes.fr%2FwQeFgucHiq17gshoWww70k9TkpM%3D%2F748x499%2Fsmart%2Fa5181741d55b4b3d8b54bda593ff87f5%2Frecipe-jdf%2F10021667.jpg&imgrefurl=https%3A%2F%2Fcuisine.journaldesfemmes.fr%2Frecette%2F333415-recette-de-crepes-la-meilleure-recette-rapide&tbnid=9tiCJbxY8Zk4PM&vet=12ahUKEwiWhYbrmOroAhVa04UKHSElDVUQMygAegUIARCsAg..i&docid=z8Z1U8UenuTgbM&w=748&h=499&q=recette%20crepes&ved=2ahUKEwiWhYbrmOroAhVa04UKHSElDVUQMygAegUIARCsAg",
+        duration: 30,
+        creator: "Guillaume M",
+      }).then((dbResponse) => {
+        Recipe.find().select({
+          title: 1,
+          _id: 0
+        }).then((dbResponse) => {
+          Recipe.insertMany(data).then((dbResponse) => {
+            Recipe.find().select({
+              title: 1,
+              _id: 0
+            }).then((dbResponse) => {
+              console.log(dbResponse)
+              Recipe.findOneAndUpdate({
+                title: "Rigatoni alla Genovese"
+              }, {
+                duration: 100
+              }, {
+                new: true
+              }).then((dbResponse) => {
+                console.log(dbResponse)
+                console.log("Update successful")
+                Recipe.deleteOne({
+                  title: "Carrot Cake"
+                }).then((dbResponse) => {
+                  console.log("Delete successful")
+                  mongoose.disconnect().then(() => {
+                    console.log("Disconnected")
+                  }).catch((dbErr) => {
+                    console.log(dbErr)
+                  })
+                }).catch((dbErr) => {
+                  console.log(dbErr)
+                })
+              }).catch((dbErr) => {
+                console.log(dbErr)
+              })
+            }).catch((dbErr) => {
+              console.log(dbErr)
+            })
+            console.log(dbResponse)
+          }).catch((dbErr) => {
+            console.log(dbErr)
+          });
+          console.log(dbResponse)
+        }).catch((dbErr) => {
+          console.log(dbErr)
+        })
+        console.log(dbResponse);
+      })
+      .catch((dbErr) => {
+        console.log(dbErr);
+      });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

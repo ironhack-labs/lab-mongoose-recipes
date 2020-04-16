2d298334de375e458f73791d0e6dfc8f6b6e1c7f
edu93 <mailing_93@hotmail.com>
2020-04-16 00:26:48 -0500

    MEX-WDFT-Eduardo

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require('./models/Recipe.model')
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data')
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app'
 
+mongoose.set('useFindAndModify', false)
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
+  .then((self) => {
+    console.log(`Connected to the database: "${self.connection.name}"`)
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    return self.connection.dropDatabase()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    // Iteration 2
+    Recipe.create({ title: 'pizzas', cuisine: 'itali' }).then((newRecipe) => {
+      console.log(newRecipe)
+    })
+
+    //iteration 3
+    Recipe.insertMany(data).then(() => {
+      data.forEach((el) => {
+        console.log(el.title)
+      })
+      Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 }).then(() => {
+        console.log('Actualizado con exito!')
+        Recipe.deleteOne({ title: 'Carrot Cake' }).then(() => {
+          console.log('Borrado con Exito').then(() => mongoose.disconnect())
+        })
+      })
+    })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  //   //iteration 4
+  // Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  //   .then(() => {
+  //     console.log('Actualizado con exito!')
+  // })
+
+  //   //iteration 5
+  // Recipe.deleteOne({ title: 'Carrot Cake' })
+  //   .then(() => {
+  //     console.log('Borrado con Exito')
+  // })
+
+  //iteration 6
+  // .then(() => mongoose.disconnect())
+
+  .catch((error) => {
+    console.error('Error connecting to the database', error)
+  })

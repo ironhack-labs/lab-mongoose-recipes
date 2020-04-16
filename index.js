bc8570bcc925c13892a5ac34f75c93f9290c8cce
agusgutierrezz <agusgutierrezperez@gmail.com>
2020-04-16 18:42:18 +0200

    iteration 5

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
-
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -25,3 +24,49 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  // const recipe = { title: 'Lemon Pie', level :"Easy Peasy", ingredients : ["flour", "lemons","butter"], cuisine : "French"};
+  // ITERATION 2
+  // Mongoose allows us to use a callback pattern
+  // to handle the completion of the asynchronous operation
+  // Recipe.create(recipe, (error, user) => {
+  //   if (error) {
+  //     console.log('An error happened:', error);
+  //     return;
+  //   }
+  //   console.log('The recipe is saved and its name is: ', recipe.title);
+  // });
+
+
+  // ITERATION 3
+
+  Recipe.insertMany(data,(error, user) => {
+    if (error) {
+      console.log('An error happened:', error);
+      return;
+    }
+    data.forEach(element => {
+      console.log('The recipe is saved and its name is: ', element.title);
+    });
+  })
+
+  // ITERATION 4
+  // Recipe.findOneAndUpdate({title : "Rigatoni alla Genovese"}, {duration : 100},(error, doc) => {
+  //   if (error) {
+  //     console.log('An error happened:', error);
+  //     return;
+  //   }
+  //   console.log("You just updated the duration!")
+  // });
+
+  // ITERATION 5
+  Recipe.find({title:"Rigatoni alla Genovese"}, 'title')
+    .then(recipe => {
+      console.log(recipe.title)
+    })
+    .catch(error => {
+      console.log('An error happened:', error);
+      return;
+    });
+  
+
+  

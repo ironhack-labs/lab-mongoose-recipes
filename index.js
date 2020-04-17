9df8030fade96fb7b7c2ab0979a3489f99f40fad
paulamoellendorf <61729055+paulamoellendorf@users.noreply.github.com>
2020-04-17 11:46:25 +0200

    done

--- index.js
+++ index.js
@@ -15,13 +15,51 @@ mongoose
     useUnifiedTopology: true
   })
   .then(self => {
+    
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: 'Fried Egg', 
+      level:'Easy Peasy', 
+      Ingredients:['eggs', 'herbs'], 
+      cuisine: 'traditional',
+       dishType: 'breakfast', 
+       image:'https://tse4.mm.bing.net/th?id=OIP.UG6pKGyoHB_SDKneZ2sQeAHaFj&pid=Api&P=0&w=218&h=164', 
+       duration: 5, 
+       creator:"Paula",  
+      })
+      .then((recipe) => console.log("Created:", recipe.title))
+      .then(() => {
+        Recipe.insertMany(data)
+        .then(() =>{
+          data.forEach((recipe) => {
+            console.log('Added:', recipe.title);
+          });
+          Recipe.findOneAndUpdate( { title: "Rigatoni alla Genovese" }, {duration:100})
+          .then(() => {
+            console.log(`Perfect, you updated the duration of 'Rigatoni alla Genovese'`);
+          })
+
+          Recipe.deleteOne({title: "Carrot Cake"})
+          .then(()=> {
+            console.log("You were able to delete the Carrot Cake Recipe");
+          })
+          .then(() => {
+            mongoose.connection.close(()=> {
+              console.log("You are now disconnected");
+            });
+          })
+
+
+        })
+       
+      });  
+ // Run your code here, after you have insured that the connection was made
   })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

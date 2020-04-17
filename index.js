83e05728d8c2f4fe1ae519c012103082fcef354a
dionisio ugalde <dionisiougalde@iMac-de-dionisio-8.local>
2020-04-17 12:07:42 +0200

    Completed lab

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const Recipe = require("./models/Recipe.model");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+// Import of the data from './data.json'
+const data = require("./data");
+
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
+  .then((self) => {
+    console.log(`Connected to the database: “${self.connection.name}“`);
+    // Before adding any documents to the database, let’s delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((addedRecipes) => {
+    console.log("data created", addedRecipes);
+    // mongoose.connection.close();
+  })
+  .then(() => {
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      {
+        new: true,
+      }
+    ).then((successCallback) => console.log(successCallback, "sucess MARKUS"));
+    // .catch(errorCallback);
+  })
+
+  .then(() => {
+    Recipe.deleteOne({ title: "Carrot Cake" }).then((successCallback) =>
+      console.log(successCallback, "sucess Carrot")
+    );
+    // .catch(errorCallback);
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

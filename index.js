2bc2243be757d92dbdb983f3e80e204015403d38
Alba-Tros <samipcpoolberlin@yahoo.fr>
2020-04-17 11:36:15 +0200

    Iteration 2 Done

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(self => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+        // Before adding any documents to the database, let's delete all previous entries
+        return self.connection.dropDatabase();
+    })
+    .then(() => {
+        // Run your code here, after you have insured that the connection was made
+        return Recipe.create({
+            title: "Pasta alla carbonara",
+            level: "Amateur Chef",
+            ingredients: [
+                "Spaghetti",
+                "Crème Fraîche",
+                "Bacon cubes",
+                "Egg",
+                "Cheese",
+                "Herbs"
+            ],
+            cuisine: "Italian",
+            dishType: "main_course",
+            image: "https://en.wikipedia.org/wiki/Carbonara",
+            duration: 30,
+            creator: "Unknown"
+        });
+    })
+    .then(recipe => {
+        console.log("This is the first recipe's title: ", recipe.title);
+    })
+    .catch(error => {
+        console.error("Error connecting to the database", error);
+    });

068e884196abdb230658687faf191a95c5e75f9c
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-17 11:11:54 +0200

    Done with the lab

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
-const mongoose = require('mongoose');
+/*jshint esversion: 6 */
+
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Godly Pancakes",
+      level: "God",
+      ingredients: [
+        "1 1/2 cups all-purpose flour",
+        "1 1/4 cups milk",
+        "3 1/2 teaspoons baking powde",
+        "1 egg",
+        "1 teaspoon salt",
+        "3 tablespoons butter, melted",
+        "1 tablespoon white sugar",
+        "Godly touch",
+      ],
+      cuisine: "The heavens",
+      dishType: "Desert",
+      image:
+        "https://www.kingarthurflour.com/sites/default/files/recipe_legacy/48-3-large.jpg",
+      duration: 20,
+      creator: "God",
+      created: Date.now(),
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    return Recipe.create([...data]);
+  })
+  .then((recipe) => {
+    console.log("the recepies has been insterted.", recipe);
+  })
+  .then(() => {
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true },
+      (err) => {
+        if (err) {
+          console.log("Something wrong when updating data!");
+        }
+      }
+    );
+  })
+  .then(() => {
+    Recipe.deleteOne({ title: "Carrot Cake" }, (err) => {
+      if (err) {
+        console.log("The Carrot Cake was not deleted.");
+      }
+      console.log("The Carrot Cake was deleted.");
+      console.log("Shutting down connection to DB!");
+      mongoose.connection.close();
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,17 @@
+/*jshint esversion: 6 */ 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title:  String,
+  level: String,
+  ingredients: [String],
+  cuisine: String,
+  dishType: String,
+  image: String,
+  duration: Number,
+  creator: String,
+  created: Date
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

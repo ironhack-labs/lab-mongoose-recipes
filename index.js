29240fa240de6dfcd6feb7ba04a472e3b6847287
yung-ting-chang <tina.yt.chang@protonmail.com>
2020-04-17 11:54:32 +0200

    done

--- index.js
+++ index.js
@@ -21,7 +21,39 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.insertMany([{title: 'mapo tofu', cuisine: 'taiwanese'}, {title: 'Nudeln mit Tomatensoße', cuisine: 'deutsch'}, {title: 'Rigatoni alla Genovese', cuisine: 'italian', duration: 20}, {title: 'carrot cake', cuisine: 'lebanese'}])
+    .then(recipeFromDB => {
+      console.log('title of recipe: ', recipeFromDB)
+    })
+    .then(() => {
+      const promise1 = Recipe.updateOne({ duration: 20 }, { duration: 100 })
+      .then(result => {
+        console.log('it is updated!', result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      const promise2 = Recipe.deleteOne({title: 'carrot cake' })
+        .then(result => {
+          console.log('it is removed!', result);
+        });
+      // Promise.all([promise1, promise2])
+      //   .then (() => {
+      //     mongoose.connection.close();
+      //   })
+
+      return Promise.all([promise1, promise2])
+    })
+    .then(() => {
+      mongoose.connection.close();
+    })
+    .catch(err => {
+          console.log('error', err);
+    });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+

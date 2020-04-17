f7a0103f551164fdc49c6ef49404e14c9350fca0
Nate <mcfwesh@gmail.com>
2020-04-17 12:36:22 +0200

    done

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then(() => {
+    const updatePromise = Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } }
+    );
+    const deletePromise = Recipe.deleteOne({ title: "Carrot Cake" });
+
+    return Promise.all([updatePromise, deletePromise]);
+  })
+  .then((document) => {
+    console.log("Duration updated and Carrot cake deleted");
+    console.log(document);
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log("Error", err));
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: [String],
+  cuisine: String,
+  dishType: String,
+  image: String,
+  duration: Number,
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

4f1646594b3597b08d6832d5cddc551aab4d56b8
Karl Secen <karl.secen@gmail.com>
2020-04-18 22:33:33 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -19,9 +24,54 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+  
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Rigatoni alla Genovese",
+      level: "Easy Peasy",
+      ingredients: [
+        "2 pounds red onions, sliced salt to taste",
+        "2 (16 ounce) boxes uncooked rigatoni",
+        "1 tablespoon chopped fresh marjoram leaves",
+        "1 pinch cayenne pepper",
+        "2 tablespoons freshly grated Parmigiano-Reggiano cheese"
+      ],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/3489951.jpg",
+      duration: 220,
+      creator: "Chef Luigi"
+    });
+   })
+
+   .then((recipe) => {
+    console.log("one recipe created", recipe.title);
+  }) 
+
+  .then(() => {
+    Recipe.insertMany(data, (error, recipes) => {
+      
+      if (error) return console.log('error:', error);
+      recipes.forEach(recipe => {
+        console.log('recipe saved:', recipe.title);
+      });
+    });
   })
+
+
+  //Working on running the below after the above. 
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: 'true'})
+  })
+
+  .then((recipe) => {
+    console.log('recipe updated:', recipe.title)
+  })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  

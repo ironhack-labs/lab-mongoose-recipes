f64c42c3917026aa5033484df644bd8bc1fc674b
sdubois92 <seb.dubois15@free.fr>
2020-04-20 22:12:31 +0200

    done

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
+// PROBLEM WITH THIS METHOD: TRYING TO RENDER A VIEW: SCOPE OF THE DATABASE RESULT IS IN THE THEN BLOCK, SO
+// I CANNOT ACCESS THE SCOPE OF THE PREVIOUS THEN BLOCK
+
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
+
+mongoose                      // to connect to the database
+  .connect(MONGODB_URI, {     // variable of the address of the database, we connect to that path
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -18,10 +23,93 @@ mongoose
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
+    // Iteration 2 - Creating a new recipe
+  })
+  .then(() => {             // First operation: creating the newRecipe using a promise (then + catch)
+    const newRecipe = {
+      title:"my new recipe",
+      level:"beginner",
+      ingredients:["1 bar of chocolate","1 banana"],
+      cuisine:"French",
+      dishType:"dessert",
+      duration:2,
+      creator:"Chef Sébastien",
+    }
+    return Recipe.create(newRecipe)
+  })
+  .then((dbResultCreatePromise) => {
+    console.log(dbResultCreatePromise)
+    return Recipe.insertMany(data)
+  })
+  .then((dbResultInsertManyPromises) => {
+    const onlyTitlesArray = dbResultInsertManyPromises.map((data) => {
+    return data.title;
+    })
+    console.log("insertMany was made", onlyTitlesArray)
+    return Recipe.findOneAndUpdate( {title:'Rigatoni alla Genovese'}, {duration:100}, {new:true} );
+  })
+  .then((dbResultUpdate) => {
+    console.log(dbResultUpdate)
+    return Recipe.deleteOne( {title:'Carrot Cake'} );
+  })
+  .then((dbResDelete) => {
+    console.log(dbResDelete)
+    return mongoose.connection.close();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log('Database closed!');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
+// Meilleure méthode tous les .then() suivis de tous les .catch()
+// Ou bien async await
+
+
+
+  // WITH ASYNC AWAIT
+
+async function seeds() {
+  try {                               // TRY AND CATCH
+  const self = await mongoose
+    .connect(MONGODB_URI, {     // variable of the address of the database, we connect to that path
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log(`Connected to the database: "${self.connection.name}"`);
+    
+  await self.connection.dropDatabase();
+  const newRecipe = {
+    title: "my new recipe",
+    level: "beginner",
+    ingredients: ["1 bar of chocolate", "1 banana"],
+    cuisine: "French",
+    dishType: "dessert",
+    duration: 2,
+    creator: "Chef Sébastien",
+  }
+  const createRecipe = await Recipe.create(newRecipe)
+
+  console.log(dbResultCreatePromise)
+  const createdRecipes = await Recipe.insertMany(data)
+
+  const updatedRecipes = await Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true });
+
+  const deletedRecipe = await Recipe.deleteOne({ title: 'Carrot Cake' });
+
+} catch (error) {
+    console.log(error);
+  }
+
+}
+
+seeds();
+
+
+
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Iteration 1 - Creating the Schema
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title:{type:String, unique:true},
+  level:String,
+  ingredients:[String],
+  cuisine:String,
+  dishType:String,
+  image: {type:String, default:"https://images.media-allrecipes.com/images/75131.jpg"},
+  duration:{type:Number, min:0},
+  creator:String,
+  created: {type:Date, default: Date.now},
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

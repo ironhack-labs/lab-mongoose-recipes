f14418f1775066d2425bb66483a5d0ae14989182
Hugo Suarez <hugo.suarez92@yahoo.com>
2020-05-11 18:03:04 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -21,7 +20,32 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const myRecipe = new Recipe({
+      "title": "Bandeja Paisa",
+    "cuisine": "Colombian"
+    })
+    myRecipe.save()
+    .then((myRecipe) => console.log("Inserted"))
+
+    Recipe.insertMany(data).then(res => {
+      
+      
+
+      Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"},{duration: 100})
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+
+      Recipe.deleteOne({title: "Carrot Cake"})
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+
+       setTimeout(()=> mongoose.connection.close(), 0)
+    
+    }).catch(err => console.log(err))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+
+

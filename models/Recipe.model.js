820d878982075e82502752d79fb467801970fd5b
Alberto Correia <alberto_cpv@hotmail.com>
2020-05-24 22:27:22 +0200

    itereation 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+ title: {
+   type: String,
+   unique: true,
+   required: true
+ },
+
+ level: {
+   type: String,
+   enum: ["easy", "intermedidate","difficult"]
+} ,
+ ingredients: [String],
+ cuisine: {
+   type: String,
+   required: true
+ },
+ dishtype: String,
+ image: {
+   type: String,
+   default:"https://images.media-allrecipes.com/images/75131.jpg"
+ },
+ duration:{
+   type: Number,
+   min: 0
+ },
+ creator: String,
+ created: {
+   type: Date,
+   default: 24/5/2020
+ }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

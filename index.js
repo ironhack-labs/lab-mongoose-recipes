cd6f705f0f59a45a3bd2c2e7194f61d49ab9217e
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-05-25 17:07:07 +0200

    iteration 4-5-6

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const chickenRecipe = {
+  title: "Pollo al Horno",
+  level: "Amateur Chef",
+  ingredients: ["1 pollo","Salsa soja","Salsa picante","Vinagre de manzana","Pimienta","Sal","Ajo","Salsa de tomate","Tomillo"],
+  cuisine: "Antoine's",
+  dishType: "main_course",
+  duration: 45,
+  creator: "Antoine Rolland",
+  created: "2020-05-25"
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -22,10 +33,20 @@ mongoose
   .then(() => {
     // Run your code here, after you have insured that the connection was made
     db => console.log(`Connected to database ${db.connection.name}`)
-    // Recipe.create(data[1]).then().catch()
+    Recipe.create(chickenRecipe).then(rec => console.log(rec.title)).catch()
     Recipe.insertMany(data).then().catch()
-    Recipe.find().then().catch()
+    .then(() => {
+      Recipe.updateOne({"duration":220},{"duration":100}).then(console.log("Updated !")).catch()
+      Recipe.deleteOne({"title":"Carrot Cake"}).then(console.log("Deleted !")).catch()
+    }).catch()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  setTimeout(() => {
+    mongoose.connection.close(() => {
+      console.log("Connection closed")
+      process.exit(0)
+    })
+  },1500)

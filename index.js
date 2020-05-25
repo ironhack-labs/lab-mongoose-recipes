2f6bc40e0ed10aa17cec1bd309336c143993712c
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-05-25 12:00:08 +0200

    add recipe

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const myRecipe = new Recipe({//create a new recipe
+  title: "Pão de Queijo",
+  level: "Easy Peasy",
+  ingredients: ["250g cheese", "1 1/2 egg", "300ml milk", "10g salt", "75ml oil, 250g Polvilho Doce", "250g Polvilho Azedo"],
+  cuisine: "Mineira",
+  dishType: "snack",
+  image: "https://t1.rg.ltmcdn.com/pt/images/6/7/3/img_pao_de_queijo_de_liquidificador_376_orig.jpg",
+  duration: 30,
+  creator: "Marcela Vilas Boas",
+})
+
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,6 +33,10 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    myRecipe
+      .save()
+      .then(newRecipe => console.log(`New recipe created: ${newRecipe.title}`))
+      .catch(err => console.log(`Error creating the recipe: ${err}`))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -6,7 +6,7 @@ const recipeSchema = new Schema({
   title: {
     type: String,
     required: true,
-    unique: true,
+    //unique: true,
   },
 
   level: {

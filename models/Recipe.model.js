c1ba437641057905dada79a7bae80e337bdb90a3
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-05-25 14:04:55 +0200

    iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy","Amateur Chef","UltraPro Chef"]
+  },
+  ingredients: [String],
+  dishType: String,
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: Date
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

1139afdc9fe94f3e890e2c846c06107f58920f1e
Alba Olveros <x8a.018@gmail.com>
2020-05-26 13:00:27 +0200

    Iteration 1 - 6 done

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const customRecipe = {
+  title: "Arròs a la cassola",
+  level: "Easy Peasy",
+  ingredients: ["1 red bell pepper", "2 green red bell peppers", "1 onion", "3 cloves of garlic", "250gr tomato", "1L vegetable broth", "200gr rice", "Salt", "Pepper", "Parsley"],
+  cuisine: "Catalana",
+  duration: 120,
+  creator: "Joana Bautista"
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,6 +30,23 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(customRecipe)
+    .then(() => console.log(`My recipe is ${customRecipe.title}.`))
+  })
+  .then(() => {
+    Recipe.insertMany(data)
+    .then(recipes => recipes.forEach(el => console.log(`${el.title}`)))
+    .then(() => {
+      Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 }})
+      .then(() => console.log(`The "Rigatoni alla Genovese" recipe has been updated successfully.`))
+    })
+    .then(() => {
+      Recipe.deleteOne({title: "Carrot Cake"})
+      .then(() => {
+        console.log(`The "Carrot Cake" recipe has been deleted successfully.`)
+        mongoose.connection.close(() => console.log("Closing connection with mongoose..."))
+      })
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

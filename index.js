5327b6d7e8e1c8aacec83c54e013597d896b9167
LucyJunior <53796420+LucyJunior@users.noreply.github.com>
2020-05-28 16:29:59 +0200

    Lab Done

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    // return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.insertMany(data)
+    .then((res) => {
+      Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } })
+        .then((res) => {
+          console.log('Successfully updated')
+        })
+
+      Recipe.deleteOne({ title: 'Carrot Cake' })
+        .then((res) => {
+          console.log('Successfully deleted')
+        })
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

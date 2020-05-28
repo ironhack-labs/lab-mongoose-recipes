23f92f54d14e1f08e59d07214486cd126169d8b2
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-05-28 15:57:06 +0100

    completed

--- index.js
+++ index.js
@@ -21,7 +21,58 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+
+    // ITERATION 2
+  //   Recipe.create({
+  //     "title": "Asian Glazed Chicken Thighs",
+  //     "level": "Amateur Chef",
+  //     "ingredients": [
+  //       "1/2 cup rice vinegar",
+  //       "5 tablespoons honey",
+  //       "1/3 cup soy sauce (such as Silver Swan®)",
+  //       "1/4 cup Asian (toasted) sesame oil",
+  //       "3 tablespoons Asian chili garlic sauce",
+  //       "3 tablespoons minced garlic",
+  //       "salt to taste",
+  //       "8 skinless, boneless chicken thighs"
+  //     ],
+  //     "cuisine": "Asian",
+  //     "dishType": "main_course",
+  //     "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  //     "duration": 40,
+  //     "creator": "Chef LePapu"
+  //   }).then(() => {
+  //     Recipe.find({})
+  //     .then((res) => {console.log(res)});
+  //   });
+
+  // })
+  // .catch(error => {
+  //   console.error('Error connecting to the database', error);
+  // });
+
+  //ITERATION 3
+  Recipe.insertMany(data).then(() => {
+    Recipe.find({}, {title: 1})
+    .then((res) => {
+      console.log(res);
+
+      // ITERATION 4
+      Recipe.updateOne({title: "Rigatoni alla Genovese"}, {$set: {duration: 100}}).then(() => {
+        console.log("Successfully updated!");
+        //ITERATION 5
+        Recipe.deleteOne({title: "Carrot Cake"}, {$set: {duration: 100}}).then(() => {
+        console.log("Deleted Carrot Cake!");
+        //ITERATION 6
+        mongoose.disconnect(() => {console.log('closed connection');});
+      });
+      });
+
+    });
+   
   });
+})
+.catch(error => {
+  console.error('Error connecting to the database', error);
+});
+

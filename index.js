3b1ecec42b1e9a7346e06ba9bcdcbd11a990d4c5
Amruta Karanjekar <karanjekar.amruta@gmail.com>
2020-05-29 16:13:19 +0200

    initial commit

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+var recipeToCreate = {
+  title : "Asian Glazed Chicken Thighs",
+  level : "Amateur Chef",
+  ingredients : [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  cuisine:"Asian",
+  dishType : "main_course",
+  image:"https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration:40,
+  creator:"Chef LePapu"
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -20,7 +40,41 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe
+    .create(recipeToCreate)
+    .then((newRecipe)=>{console.log(newRecipe.title);})
+    .catch(error=>{console.log(error);
+    })
+  })
+  .then(()=>{
+    return Recipe
+    .insertMany(data)
+    .then(recipesAdded=>{
+      recipesAdded.forEach(recipe=>{
+        console.log(recipe.title)
+      })
+    })
+    .catch(error=>{console.log(error);})
+  })
+  .then(()=>{
+    const query = {title:"Rigatoni alla Genovese"};
+    const update = {duration:100};
+    return Recipe
+      .findOneAndUpdate(query,update)
+      .then(updated=>{console.log("updated successfully", updated)})
+      .catch(error=>{console.log(error);})
+  })
+  .then(()=>{
+    var query = {title:"Carrot Cake"}
+      return Recipe
+        .deleteOne(query)
+        .then(deleted=>{console.log("deleted record successfully")})
+        .catch(error=>{console.log(error);})
+  })
+  .then(()=>{
+   return  mongoose.connection.close(()=>{
+      console.log("connection closed successfully");
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title : String,
+  level: String,
+  ingredients : [String],
+  cuisine : String,
+  dishType : String,
+  image : {type : String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration : {type:Number, default: 0},
+  creator : String,
+  created : {type:Date, default:new Date()}
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

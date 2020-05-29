c1eb3ed1d4ba70e595f4a335f4df39fd002ed873
Melanie N <melanie.nachef@gmail.com>
2020-05-29 14:07:05 +0200

    done

--- index.js
+++ index.js
@@ -19,9 +19,55 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(()=> {
+    return Recipe
+    .create({
+      title: "Brownie",
+      level: "Easy Peasy",
+      ingredients: [
+        "4 eggs",
+        "80g of flour",
+        "150g of sugar",
+        "125g of butter",
+        "200g of dark chocolate"
+      ],
+      cuisine: "American",
+      dishType: "dessert",
+      duration: 15,
+      creator: "Ironhacker"
+    })
+    .then((recipe)=>{console.log(recipe.title)})
+    .catch(err => { console.log('An error happened:', err) })
+  })
+  .then(()=> {
+    return Recipe
+    .insertMany(data)
+    .then((recipe)=> {
+      for (const property in recipe) {
+        console.log(recipe[property].title);
+      }
+    })
+    .catch(error => {console.error('An error happened:', error)})
+  })
+  .then(()=> {
+    return Recipe
+    .findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+    .then(() =>console.log('Updated successfully'))
+    .catch(error => {console.error('An error happened:', error)})
+  })
+  .then(()=> {
+    return Recipe
+    .deleteOne({title: "Carrot Cake"})
+    .then(() => console.log('Deleted successfully'))
+    .then(() => {mongoose.connection.close()})
+    .then(() => console.log('Database closed'))
+    .catch(error => {console.error('An error happened:', error)})
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  
+
+
+
+

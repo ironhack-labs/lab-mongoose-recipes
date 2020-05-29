06ecf231464cf00b7c3dbf3e620bc72ab5b6a59b
Aida <aida.loemba@outlook.com>
2020-05-29 15:39:39 +0200

    done

--- index.js
+++ index.js
@@ -20,8 +20,68 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+    Recipe
+      .create({
+        title: "Baguette French Toast",
+        level: "Amateur Chef",
+        ingredients: [
+          "1 stale baguette",
+          "2 eggs",
+          "1 1/2 cups of milk",
+          "1 teaspoon of baking powder",
+          "1 pinch of salt",
+          "2 tablespoons of sugar",
+          "Vanilla extract",
+          "Sunflower oil"
+        ],
+        cuisine: "French",
+        dishType: "Breakfast",
+        image: "https://cac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcac.2F2018.2F09.2F25.2F5c694217-aeac-400a-98ac-eb4539670c2d.2Ejpeg/748x372/quality/90/crop-from/center/pain-perdu.jpeg",
+        duration: "40",
+        creator: "Mom ♥️"
+      })
+      .then((newRecipe) => {
+        console.log("A new recipe was created: ", newRecipe.title);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+    Recipe
+      .insertMany(data)
+      .then((dataProjected)=> {
+        return Recipe.find()
+          .select("title")
+          .then((recipesProjected)=> {
+            console.log("The following recipes were added: ", recipesProjected);
+          })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+    .then(() => {
+      return Recipe
+      .findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+      .then((updatedRecipe) => {
+        console.log(updatedRecipe.title, "was updated")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    })
+
+    .then(() => {
+      return Recipe
+      .deleteOne({title: "Carrot Cake"}, {new: true})
+      .then((deletedRecipe) => {
+        console.log("A recipe was deleted")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+});

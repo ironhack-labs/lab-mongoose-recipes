c245302c0e142a3a7ebbbb35b2ceb1217bc89a79
Victor Sanchez <victor1305@icloud.com>
2020-06-24 18:47:32 +0200

    Codigo refactorizado

--- index.js
+++ index.js
@@ -20,29 +20,39 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
+
+  .then(() => 
     Recipe
     .create({title: "Lasaña", cuisine: "italian"})
     .then(newRecipe => console.log("The new recipe is: ", newRecipe.title))
-    .then(() =>
+
+  )
+
+  .then(() =>
     Recipe
     .create(data)
     .then(data.forEach(elem => {
       console.log(elem.title)
     }))
-    )
-    .catch(err => console.log("There was an error", err))
-    .then(()  =>
+  )
+
+  .then(()  =>
     Recipe
     .findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
-    .then(recipeUpdated => console.log(`The following recipe has been modificated: ${recipeUpdated.title}. The new duration is: ${recipeUpdated.duration}` )))
-    .then(() =>
+    .then(recipeUpdated => console.log(`The following recipe has been modificated: ${recipeUpdated.title}. The new duration is: ${recipeUpdated.duration}` ))
+    
+  )
+
+  .then(() =>
     Recipe
     .deleteOne({title: "Carrot Cake"})
-    .then(deleted => console.log("The recipe has been deleted"))
-    .then(() =>
-    mongoose.connection.close())
-  )})
+    .then(console.log("The recipe has been deleted"))
+    
+  )
+
+  .then(() =>
+  mongoose.connection.close())
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

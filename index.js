c9f17a3aae9e177e6929ec6690d0767b793ef008
cristianvl8 <cristianvinualeslobera@gmail.com>
2020-06-24 19:11:05 +0200

    finalizado

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -20,7 +21,103 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+    return Recipe
+
+      .create({
+
+        "title": "Chicken",
+        "level": "Amateur Chef",
+        "ingredients": [
+          "500g chicken thighs",
+          "1 lemon juiced",
+          "1 cup raisins",
+          "2 tsp paprika",
+          "200g natural yogurt",
+        ],
+        "cuisine": "International",
+        "dishType": "main_course",
+        "image": "",
+        "duration": 45,
+        "creator": "Jennifer Joyce"
+      })
+
+      .then(newRecipes => console.log('Los nuevos elementos creados:', newRecipes))
+      .catch(err => console.log('Hubo un error', err))
+
+
+
+      .then(() => {
+
+        return Recipe
+          .insertMany(data)
+          .then((newRecipes) => {
+            newRecipes.forEach((elem) => console.log(`Added ${elem.title}`))
+          })
+      })
+
+      .then(() => {
+
+        return Recipe
+          .findOneAndUpdate({
+            duration: 100
+          }, {
+            duration: 110
+          }, {
+            useFindAndModify: false
+          })
+          .then(updateRecipe => {
+            console.log(updateRecipe)
+
+          })
+      })
+      //OTROS MÉTODOS A USAR, TENÍA PROBLEMAS CON LOS DEPRECATED, ASÍ QUE HE PROBADO VARIOS.
+
+      // .then(() => {
+      //   Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+
+      //   .then(updated => {
+      //       console.log("updated successfully", updated)
+      //     })
+      //     .catch(error => {
+      //       console.log(error);
+
+      // })
+
+      // })
+
+      // .then(() => {
+      //   Recipe.findByIdAndUpdate('5ef37b8ebb4524a9395f76db', {
+      //     duration: 100
+      //   }, { useFindAndModify: false })
+
+      //   .then(updated => {
+      //     console.log("updated successfully", updated)
+      //   })
+      //   .catch(error => {
+      //     console.log(error);
+      //   })
+
+      //   })
+
+      .then(() => {
+
+        return Recipe
+
+          .deleteOne({
+            title: "Carrot Cake"
+          })
+          .then(deleteRecipe => {
+            console.log(deleteRecipe)
+          })
+
+      })
+      .then(() => {
+        return mongoose.connection.close(() => {
+          console.log("connection closed successfully");
+        })
+
+      })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

cbe2dc6fa54099d895141fbbac6d40344184588f
Miguel Serrano <miguelserrano.sp@gmail.com>
2020-06-24 19:32:35 +0200

    done

--- index.js
+++ index.js
@@ -12,16 +12,48 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then(() => Recipe.create({
+            title: "Asian Glazed Chicken Thighs",
+            level: "Amateur Chef",
+            ingredients: [
+              "1/2 cup rice vinegar",
+              "5 tablespoons honey",
+              "1/3 cup soy sauce (such as Silver Swan®)",
+              "1/4 cup Asian (toasted) sesame oil",
+              "3 tablespoons Asian chili garlic sauce",
+              "3 tablespoons minced garlic",
+              "salt to taste",
+              "8 skinless, boneless chicken thighs"
+            ],
+            cuisine: "Asian",
+            dishType: "main_course",
+            image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+            duration: 40,
+            creator: "Chef LePapu"
+          })  
+          
+          .then(newRecipe => console.log('La nueva receta creada es', newRecipe.title))
+  )
+  .then(() => Recipe.create(data)
+  .then(data.forEach(elm => console.log(elm.title))))
+  .then(() => Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true }))
+  .then(recipe => console.log('Actualizada la duración de', recipe.title))
+  .then(() => Recipe.findOneAndRemove({ title: 'Carrot Cake' })
+  .then(details => console.log('Ya no tenemos disponible:', details.title, ':(')))
+  .catch(error => console.error('Error connecting to the database', error))
+
+  
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    })
+})

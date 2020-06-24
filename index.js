c5d78e65e8ddc3b60106efdf6054cede627e3d38
Diego Ortiz <diegortiz988@gmail.com>
2020-06-24 19:05:13 +0200

    casi terminado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -21,6 +22,35 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({
+        title:"Tortilla de Patata",
+        level:"Amateur Chef",
+        ingredients: ['huevos', 'cebolla', 'aceite', 'perejil', 'sal', 'aceite de oliva' ],
+        cuisine: "Spanish",
+        dishType: "snack",
+        image: "imagen",
+        duration: 30,
+        creator: "Diego",
+      })
+      .then(newRecipe => console.log('The new recipe is', newRecipe.title))
+      .catch(err => console.log('Error finding recipe', err))
+
+    Recipe
+      .create(data)
+      .then(newData => console.log('The recipes are:', newData))
+      // .catch(err => console.log('Error finding recipes', err))
+
+    Recipe
+      .updateOne({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
+      .then(newUpdate => console.log('The information changed is:', newUpdate.title))
+      // .catch(err => console.log('Error finding recipes', err))
+
+    Recipe
+      .deleteOne({title: "Carrot Cake"})
+      .then(newDelete => console.log('The recipe has been deleted:', newDelete))
+    //   .catch(err => console.log('Error deleting Recipes', err))
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

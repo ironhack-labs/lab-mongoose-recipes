621e78fa469e2495cb09a45f39cfbac81eefd3e8
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-06-24 18:27:04 +0200

    done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require('./models/Recipe.model')
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data')
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app'
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,13 +16,36 @@ mongoose
     useUnifiedTopology: true
   })
   .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
+    console.log(`Connected to the database: "${self.connection.name}"`)
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      "title": "Pizza",
+      "cuisine": "Italian"
+    })
+      .then(newRecipe => console.log('La nueva receta creada es:', newRecipe.title))
   })
+
+  .then(() => Recipe.create(data))
+  .then(newRecipe => (newRecipe.forEach(recipe => {
+    console.log(recipe.title)
+  })))
+  .then(() =>
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true })
+  )
+  .then(updatedRecipe => console.log("The recipe ", updatedRecipe.title, "has been updated and the new duration is", updatedRecipe.duration))
+  .then(() =>
+    Recipe.deleteOne(
+      { title: "Carrot Cake" })
+  )
+  .then(updatedRecipe => console.log("The recipe has been removed."))
+  .then(() => mongoose.connection.close(() =>
+    console.log('Mongoose default connection disconnected through app termination')))
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.error('Error connecting to the database', error)
+  })

d0048bafc703190c04d2fc89f38cf3a591d543f9
SiEr0212 <simonertel@gmx.de>
2020-06-25 19:10:11 +0200

    Iterations 1 and 2 done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -19,9 +20,34 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+  .then( () => {
+    //Iteration 2 - Create a recipe
+    Recipe
+      .create({
+        title: "Whiskey Sour",
+        level: "Amateur Chef",
+        ingredients: [
+          "2 parts bourbon",
+          "1 part lemon juice",
+          "½ part sugar syrup",
+          "½ part egg white",
+          "Cubed ice",
+          "To garnish: 1 cherry and an orange slice",
+        ],
+        cuisine: "World",
+        dishType: 'drink',
+        image: "https://images.unsplash.com/photo-1541546006121-5c3bc5e8c7b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80",
+        duration: 5,
+        creator: "GOD OF ALL COKTAILS"
+      })
+      .then(recipe => {
+        console.log("You're recipe got added", recipe);
+      })
+      .catch(error => {
+        console.log("Error you're recipe did not got added", error);
+      })
+    
+    })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

af568b6edf73389e85679fee09549bebb5d9586c
Majd Sabik <majdsabik.ms@gmail.com>
2020-06-25 21:43:27 +0200

    All iterations done

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
+  .connect("mongodb://localhost/Recipes", {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Cherry Tiramisu",
+      cuisine: "Italian",
+      duration: 60,
+    }).then((recipe) => {
+      console.log(recipe.title);
+    });
+    Recipe.insertMany(data).then((el) => {
+      el.forEach((recipe) => console.log(recipe.title));
+      const filter = { title: "Rigatoni alla Genovese" };
+      const update = { duration: 100 };
+      const promise1 = Recipe.findOneAndUpdate(filter, update, {
+        new: true,
+      }).then(console.log("Updated duration of Rigatoni alla Genovese recipe"));
+      const promise2 = Recipe.deleteOne({ title: "Carrot Cake" }).then(
+        console.log("Deleted Carrot Cake recipe")
+      );
+      Promise.all([promise1, promise2]).then(() => {
+        console.log('All commands executed successfully. Closing database connection now...');
+        mongoose.connection.close();
+      });
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

31bcdbbf6caa34bbb3c10aed3cd55fc021a4f16f
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-06-25 17:53:24 +0200

    Iteration #1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,43 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    validate: {
+      message: 'Every Word needs to be with capital letter',
+      enum: ['Easy Peasy', 'Amateur Chef ', 'UltraPro Chef']
+    }
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    // Possible values
+    
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.prototype.toTimeString()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

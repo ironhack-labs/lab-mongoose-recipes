ac5be1e00df7b295946e22872eeafdc6ee144465
Izquiano <izquiano@hotmail.com>
2020-06-29 23:58:34 +0200

    Iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,8 +3,53 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: { 
+    type: String,
+    unique: true,
+    required: true
+  },
+  level: { 
+    type: { enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]}
+  },
+  ingredients: { 
+    type: [String]
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: { enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert" , "other"]}
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: new Date()
+  }
+    
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+
+
+// title - Type String. It should be required and unique.
+// level - Type String. Can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the enum validator wink).
+// ingredients - Type Array of Strings (represented as [ String ]).
+// cuisine - Type String. Should be required.
+// dishType - Type String. Possible values: breakfast, main_course, soup, snack, drink, dessert or other.
+// image - Type String. Default value: "https://images.media-allrecipes.com/images/75131.jpg".
+// duration - Type Number. The minimum value should be 0.
+// creator - Type String.
+// created - Type Date. By default, today

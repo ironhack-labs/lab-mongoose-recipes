e0de328c080aeac7a97322c80bd01a947d71b68a
Pumpel <romanbaos@gmail.com>
2020-06-29 21:28:33 +0200

    Iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,25 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  required: true,
+  unique: true,
+  level: {
+    type: String,
+    enum: ['Easy Peasy','Amateur Chef','UltraPro Chef']
+  },
+  ingredients: [String],
+  dishType: {
+    type: String,
+    enum:['breakfast','main_course','soup','snack','drink','dessert','other']
+  },
+  image: 'https://images.media-allrecipes.com/images/75131.jpg',
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: Date.now()
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

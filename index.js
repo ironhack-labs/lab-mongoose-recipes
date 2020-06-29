7be10afd92550d09bdbc5231a6d1050f84b39f3f
Pumpel <romanbaos@gmail.com>
2020-06-29 21:19:01 +0200

    pull request

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+//f
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {

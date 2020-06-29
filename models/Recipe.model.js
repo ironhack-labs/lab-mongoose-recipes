a56f415606853e07bb611bb338c64705761de445
TL Garcia <tomas.bl.garcia@gmail.com>
2020-06-29 21:33:46 +0200

    It1 done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String, 
+  level: String,
+  ingredients: [ String],
+  cuisine: {
+    type: string,
+    required: true
+  },
+  dishType: {
+    type: string,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image : {
+    type: string,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: string,
+  created: {
+    type: Date,
+    default: new Date()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

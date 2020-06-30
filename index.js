20185252adedab2711735cc346bf4a394a1693b7
maguirrezabala <marta@FONETIC-MSI93.fonetic.local>
2020-06-30 16:21:35 +0200

    done

--- index.js
+++ index.js
@@ -16,12 +16,42 @@ mongoose
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const myRecipe = { title: 'Lentejas' };
+    Recipe.create(myRecipe)
+      .then((recipe) => console.log(`The ${recipe.title} recipe is created`))
   })
+
+  //insert data from JSON
+  .then(() => Recipe.insertMany(data))
+  .then(() => {
+    data.forEach((el) => console.log(`${el.title} added`))
+  })
+
+  //update data
+  .then(() =>
+    Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true })
+  )
+  .then((recipe) => console.log(`The ${recipe.title} recipe has been updated`))
+
+  //delete
+  .then(() =>
+    Recipe.deleteOne({ title: 'Carrot Cake' }, function (err) { })
+  )
+  .then((recipe) => console.log(`The recipe has been deleted`))
+
+
+  .then(() => {
+    mongoose.connection.close(() => {
+      console.log('Mongoose default disconnected');
+      process.exit(0);
+    });
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+

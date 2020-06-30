f043832f380c604444e766e99a4f41d9f99c5fa3
Izquiano <izquiano@hotmail.com>
2020-06-30 22:31:34 +0200

    Done

--- index.js
+++ index.js
@@ -1,50 +1,84 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+// ***** Mongoose: `findOneAndUpdate()` and `findOneAndDelete()` without the `useFindAndModify` option set to false are deprecated.
+mongoose.set(`useFindAndModify`, false);
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+
+const papasAlinas = {
+  title: "Papas Aliñás",
+  level: "Amateur Chef",
+  ingredients: [
+    "Patatas",
+    "Sal",
+    "Vinagre",
+    "Atún en aceite de oliva",
+    "Perejil",
+    "Cebolla",
+  ],
+  cuisine: "Mediterranean",
+  dishType: "main_course",
+  duration: 30,
+  creator: "Daniel",
+};
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    const recipe = new Recipe ({
-      title: "Papas Aliñás",
-      level: "Amateur Chef",
-      ingredients: ["Patatas", "Sal", "Vinagre", "Atún en aceite de oliva", "Perejil"],
-      cuisine: "Mediterranean",
-      dishType: "main_course",
+    Recipe.create(papasAlinas)
+      .then((newRecipe) =>
+        console.log(`A new recipe is created: ${newRecipe.title}!`)
+      )
+      .catch((err) => console.log(`Error while creating a new recipe: ${err}`))
+      .then(() => Recipe.insertMany(data))
+      .then(() =>
+        Recipe.find({}).then((recipes) => {
+          recipes.map((el) => console.log(el.title));
+        })
+      )
+      // ***** Mongoose: `findOneAndUpdate()` and `findOneAndDelete()` without the `useFindAndModify` option set to false are deprecated.
+      .then(() =>
+        Recipe.findOneAndUpdate(
+          { title: "Rigatoni alla Genovese" },
+          { duration: 100 },
+          { new: true }
+        )
+      )
+      .then((recipe) =>
+        console.log(
+          `${recipe.title} was updated: (duration: ${recipe.duration})`
+        )
+      )
+      .then(() => {
+        Recipe.deleteOne({ title: 'Carrot Cake' })
+        .then((recipe)=>  console.log(`${recipe.n} recipe deleted`))
+        .then(()=>{
+          mongoose.connection.close()
+          console.log('Database closed!')
+          process.exit(0)
+         }) 
+           
+      })
       
-      duration: 30,
-      creator: "Daniel",
-      
-  
-    })
-    recipe.save()
-      .then(newRecipe => console.log(`A new recipe is created: ${newRecipe.title}!`))
-      .catch(err => console.log(`Error while creating a new recipe: ${err}`));
-      
-   
-    
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
-
-
-
-
  
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  })
+  

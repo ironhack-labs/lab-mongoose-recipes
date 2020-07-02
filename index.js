726ec4c38d986f49f6a61269c5c68610345721ad
Jose Roberto <juniorferreirarob@gmail.com>
2020-07-02 16:22:02 -0300

    done

--- index.js
+++ index.js
@@ -19,9 +19,38 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async () => {
+
+    const resultCreate = await Recipe.create({
+      title: "Camarao ao Molho",
+      ingredients: ['Camarao', 'Tomate'],
+      cuisine: 'Haute',
+      dishType: 'snack',
+      duration: 120
+    });
+    console.log('Recipe created => ', resultCreate)
+
+    const resultManyRecipe = await Recipe.create([
+      { title: 'Churros', cuisine: 'Fusion', dishType: 'snack', duration: 20 },
+      { title: 'Cha Mate', cuisine: 'Vegetarian', dishType: 'other', duration: 5 },
+      { title: 'French Fries', cuisine: 'Fusion', dishType: 'snack', duration: 10 },
+      { title: 'Rigatoni alla Genovese', cuisine: 'Fusion', dishType: 'main_course', duration: 500 },
+      { title: 'Carrot Cake', cuisine: 'Vegetarian', dishType: 'dessert', duration: 50 }
+    ]);
+    console.log('MANY Recipe created => ', resultManyRecipe)
+
+    const updateRigatoni = await Recipe.updateOne(
+      {title: 'Rigatoni alla Genovese'},
+      {duration: 100}
+    );
+    console.log(`Updated Rigatoni!!`, updateRigatoni)
+
+    const deleteCake = await Recipe.deleteOne({title: 'Carrot Cake'});
+    console.log('Deleted Cake! ', deleteCake)
+  
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

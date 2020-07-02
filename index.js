d7ea21ae4b460aa61a9850a39f5ee975c0e10e3c
Axel Chauvin <axelchauvin@gmail.com>
2020-07-02 18:45:16 +0200

    Created recipe schema and completed all iterations

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+
+const newRecipe = {
+  title: "Cabage soup",
+  level: "Amateur Chef",
+  ingredients: ["cabage", "soup"],
+  cuisine: "Chinese",
+  dishType: "soup",
+  durection: 15,
+  creator: "Axel",
+};
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // =====Iteration 2 =====
+    Recipe.create(newRecipe)
+      .then((dbRes) => {
+        console.log("Adding custom recipe ===>" + newRecipe.title);
+        // =====Iteration 3 =====
+        Recipe.insertMany(data)
+        .then((dbRes) => {
+          dbRes.forEach((recipe) => console.log("Adding existing recipes ===>" + recipe.title));
+          // =====Iteration 4 =====
+          Recipe.updateOne(
+            { title: "Rigatoni alla Genovese" },
+            { duration: 100 }
+          ).then(() => {
+            // =====Iteration 5 =====
+            Recipe.deleteOne({ title: "Carrot Cake" })
+            .then((dbRes) => {
+              console.log("Carrot cake recipe was removed");
+              // =====Iteration 6 =====
+              mongoose.connection.close();
+            });
+          });
+        });
+      })
+      .catch((dbRes) => {
+        console.log("Error removing Carrot Cake recipe");
+      });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

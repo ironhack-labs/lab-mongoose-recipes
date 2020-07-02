c1733409cb16d69f6558622c744fd3ee9f5ab606
Maya Despretz <mayadespretz@gmail.com>
2020-07-02 17:51:31 +0200

    lab done

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const welsh = {
+  title: "Welsh",
+  level: "Easy Peasy",
+  type: [ "cheddar", "beer", "ham", "bread", "eggs" ],
+  cuisine: "Chti",
+  dishType: "main_course",
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,7 +30,33 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    Recipe.create(welsh)
+    .then(sucess => {
+      console.log(welsh.title);
+      Recipe.insertMany(data)
+      .then(sucess => {
+        data.forEach((el)=> console.log (el.title))
+        Recipe.findOneAndUpdate(
+          {title: "Rigatoni alla Genovese"},
+          { duration: 100})
+        .then(success => {
+          console.log ("receipe updated")
+          Recipe.deleteOne( {title: "Carrot Cake"})
+          .then(success => {
+            console.log("Receipe deleted")
+            mongoose.connection.close(()=> {
+              console.log("ouaiiis")
+            })
+          })
+        })
+      }) 
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+  

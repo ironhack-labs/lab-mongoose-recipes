8b923d7587c9a017f23e1fe6dc30f8e970aa894b
mateomp <mateomadariaga96@gmail.com>
2020-07-06 18:30:47 +0200

    All iterations completed

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const pizza = {
+  title: 'Italian Pizza',
+  level: 'Easy Peasy',
+  ingredients: [ 'Dough', 'Tomatoes', 'Olives', 'Ham', 'Cheese' ],
+  cuisine: 'italian',
+  dishType: 'main_course',
+  image: 'https://cdn.shortpixel.ai/client/to_webp,q_glossy,ret_img/https://www.recetasdesbieta.com/wp-content/uploads/2018/09/Como-hacer-pizza-casera-rapida-con-masa-de-pizza-sin-repos-1-860x380.jpg',
+  duration: 45,
+  creator: 'Chef Mateo'
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,6 +32,20 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(pizza)
+    .then(newRecipe => console.log(`A new recipe is created: ${newRecipe.title}`))
+    .then (() => Recipe.insertMany (data))
+    .then (() => data.forEach(recipe => {console.log(`A new recipe is created: ${recipe.title}`)}))
+    .then (() => Recipe.findOneAndUpdate({ title : 'Rigatoni alla Genovese' }, { duration : 100 }))
+    .then(recipe => console.log(`Recipe updated: ${recipe.title}`))
+    .then(() => Recipe.deleteOne( {title: 'Carrot Cake'} ))
+    .then(() => console.log(`Recipe deleted`))
+    .then(()=> {
+      console.log('Database Exit')
+      mongoose.connection.close()
+      process.exit(0)
+    })
+    .catch(err => console.log(`Error while creating a new recipe: ${err}`))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

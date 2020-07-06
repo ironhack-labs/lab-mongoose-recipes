337fa2e2903a0c4bd91bf7965740b7ef3028a755
Old <onelifedesigning@gmail.com>
2020-07-06 02:28:57 +0200

    revision iteration 2

--- index.js
+++ index.js
@@ -7,13 +7,27 @@ const Recipe = require(path.join(__dirname, 'models/Recipe.model'))
 
 // Import of the data from './data.json'
 const data = require(path.join(__dirname, 'data'))
+const newRecipe = {
+  "title": "Papas with Chorizooo",
+  "level": "Amateur Chef",
+  "ingredients": [
+    "1k papas",
+    "1 chorizo dredge",
+    "1 lauren leaf"
+  ],
+  "cuisine": "Spanish",
+  "dishType": "starter_dish",
+  "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flasrecetasdemicasa.files.wordpress.com%2F2013%2F04%2Fpatatas_con_chorizo.jpg&f=1&nofb=1",
+  "duration": 20,
+  "creator": "Chef LaAbuela"
+}
 
 // Connection to the database "recipe-app"
 Recipe.deleteMany({})
 .then( () => {
-  Recipe.create(data[1])
-  .then(() => {
-    console.log('Iteration 1', data[1].title);
+  Recipe.create(newRecipe)
+  .then(data => {
+    console.log('Iteration 1', data.title);
   })  
   .catch((err) => console.log(err))
 })

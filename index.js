a82ac9e99aab51b821399cb07a93496d560af26d
Irma Blanco <irblancofdz@gmail.com>
2020-07-06 18:17:58 +0200

    feat: Iteration 1+2 WIP

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const process = require('process');
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+
+const chicken = {
+    "title": "Asian Glazed Chicken Thighs",
+    "level": "Amateur Chef",
+    "ingredients": [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+    "cuisine": "Asian",
+    "dishType": "main_course",
+    "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    "duration": 40,
+    "creator": "Chef LePapu"
+  };
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -17,11 +38,23 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
+    Recipe.deleteMany({})
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(chicken)
+      .then(recipe => 
+      console.log(`Created new recipe: ${recipe}`)
+      )   
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    process.exit(0);
+  });
+});

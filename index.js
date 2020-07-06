3075656c77a74eea05fc38e26e59a84637b9872e
Old <onelifedesigning@gmail.com>
2020-07-06 02:21:24 +0200

    iteration 3

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const mongoose = require('mongoose');
+const path = require('path')
+require(path.join(__dirname, 'config/db.config'))
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const Recipe = require(path.join(__dirname, 'models/Recipe.model'))
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+// Import of the data from './data.json'
+const data = require(path.join(__dirname, 'data'))
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
+Recipe.deleteMany({})
+.then( () => {
+  Recipe.create(data[1])
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.log('Iteration 1', data[1].title);
+  })  
+  .catch((err) => console.log(err))
+})
+Recipe.deleteMany({})
+.then( () => {
+  Recipe.insertMany(data)
+    .then(data => {
+      data.forEach(el => {
+        console.log('Iteration 2', el.title);
+      });
+    })
+    .catch((err) => console.log(err))
+})

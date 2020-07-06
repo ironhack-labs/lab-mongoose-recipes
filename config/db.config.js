5c84ed81b727dff68320f20437e4714eb37fdd44
Old <onelifedesigning@gmail.com>
2020-07-06 02:21:24 +0200

    iteration 2

---
+++ config/db.config.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const process = require('process');
+
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+// Connection to the database "recipe-app"
+mongoose
+  .connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(self => {
+    console.log(`Connected to the database: "${self.connection.name}"`);
+    // Before adding any documents to the database, let's delete all previous entries
+    return self.connection.dropDatabase();
+  })
+  .catch(error => {
+    console.error('Error connecting to the database', error);
+  });
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log('Mongoose default connection disconnected through app termination');
+      process.exit(0);
+    });
+  });

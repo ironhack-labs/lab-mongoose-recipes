504e9d5f5beb22be5ccd3e28d06361944f74e68e
Sergi Botargues <sergibotargues@MacBook-Air-de-Sergi.local>
2020-07-23 00:04:27 +0200

    Sergi Y Lourdes

--- index.js
+++ index.js
@@ -19,9 +19,36 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+  
+  Recipe.create({ title: "empanadas", level: "UltraPro Chef", ingredients: ["Carne Molida", "Cebolla", "Pimiento Rojo", "Aceitunas", "Comino", "Masa"], cuisine: "Argentina", dishType: "dinner", imagen: "https://cdn2.cocinadelirante.com/sites/default/files/images/2017/08/empanadasargentinas.jpg", duration: 60, creator: "Lourdes&Sergi" })
+  .then((recipe) => console.log(`This Recipe was saved ${recipe}`))
+  
+  .then(() => Recipe.insertMany(data))
+      .then((dataRecipe) =>
+        dataRecipe.forEach((recipes) => {
+          console.log("Las recetas son:", recipes.title);
+        })
+      )
+
+      Recipe.findOneAndUpdate(
+        { title: "Rigatoni alla Genovese" }, 
+      { duration: 100 }, 
+      { new: true })
+      .then((recipe) => console.log("Subido con exito!",recipe))
+      
+      Recipe.deleteOne({ title: "Carrot Cake" })
+      .then((recipe) => console.log("Receta eliminada",recipe))
+
+      .then(() =>{
+        mongoose.connection.close(() => {
+        console.log(
+          "Database cerrada "
+        );
+        process.exit(0);
+      })
+    })
+   
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+ 

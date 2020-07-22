7c87838a764598d48f58d946a806d76d8171c4ff
Renzo Pederzoli <rdp73@miami.edu>
2020-07-22 14:06:17 -0400

    done

--- index.js
+++ index.js
@@ -20,8 +20,20 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Iteration 2
+    // Recipe.create(data[0]).then(res => console.log(res.title))
+
+    // Iteration 3
+    Recipe.insertMany(data).then(res => {res.forEach(x => {console.log(x.title)})}).then(() => {
+      // Iteration 4
+      Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"},{duration:100},{new:true}).then(() => console.log("Succes!"))
+    }) // Iteration 5
+    .then(() =>Recipe.deleteOne({title:"Carrot Cake"})).then(() => console.log("remove carrot"))
+    
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  // Iteration 6
+mongoose.connection.close()

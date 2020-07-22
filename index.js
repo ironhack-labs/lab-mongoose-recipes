f06aaf4980528a3d5c972d7013373a476b610f49
krankie <foresvives10@gmail.com>
2020-07-22 23:46:30 +0200

    lab

--- index.js
+++ index.js
@@ -7,21 +7,55 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const spaguetti = {
+  "title": "Spaguetti",
+  "level": "Easy Peasy",
+  "ingredients": [
+    "150 gr spaguetti pasta",
+    "Tomate",
+    "Cheese",
+    "Meat balls",
+  ],
+  "cuisine": "Italian",
+  "dishType": "main_course",
+  "image": "",
+  "duration": 10,
+  "creator": "Chef Fer"
+}
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify:false
   })
-  .then(self => {
+
+   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create(spaguetti).then(newPasta => console.log(`${newPasta.title}`))
+    Recipe.insertMany(data).then((dataNew) => {
+      dataNew.forEach((recipe) => console.log(`${recipe.title}`));
+    })
+    Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration:100},{new:true})
+    .then(recipeUpdated =>console.log(`${recipeUpdated.title} new duration ${recipeUpdated.duration}`))
+    Recipe.deleteOne({title:'Carrot Cake'})
+    .then(deletedRecipe => console.log(`${deletedRecipe.title} has been deleted succesfully`))
+   
+    .then(()=> {
+      console.log(`Mongoose closing`)
+      mongoose.connection.close()})
+    .catch(err =>{
+      console.error('Error in CRUD',err)
+    })
+   
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  }); 
+
+ 

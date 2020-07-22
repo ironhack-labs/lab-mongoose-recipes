b88052d33368426b58a51d2919dceb400098e0f8
Noiret <sergio.ko@icloud.com>
2020-07-22 19:18:33 +0200

    Lab done

--- index.js
+++ index.js
@@ -1,27 +1,69 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
-// Connection to the database "recipe-app"
+const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe'
+const data = require('./data.json'); // Import of the data from './data.js'
+// Connection to the database "recipeApp"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  .connect('mongodb://localhost/recipeApp', {
+    useNewUrlParser: true
   })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+  .then(() => console.log('Connected to Mongo!'))
+  .catch(err => console.error('Error connecting to mongo', err));
+
+
+const lasagna = {
+  title: "Lasagna",
+  level: "UltraPro Chef",
+  ingredients: ["pasta sheets", "minced meet", "vegetables", "bechamel", "love"],
+  cuisine: "Italian",
+  dishType: "main_course",
+  image: "https://www.recetasdesbieta.com/wp-content/uploads/2018/10/lasagna-original.-860x380.jpg",
+  duration: 120,
+  creator: "Sergio",
+}
+/*.then(() => {*/
+// Run your code here, after you have insured that the connection was made
+//Añadimos 1 sola recipe 
+/*Recipe.create(lasagna)
+  .then(newRecipe => console.log(`Creating a new recipe: ${lasagna.title}.`))
+  .catch(err => console.log(`Error at creating the lasagna`, err))*/
+
+/*})*/
+/*.then(() => {
+  //Ahora varias recipes. insertMany se usa para añadir varios docuemntos a la vez
+  //Las recetas están en un array en data.json
+  //El parámetro de insertMany es el nombre del archivo que contiene esas recetas
+  //Como es un array, iteramos con un forEach e imprimimos cada título
+  Recipe.insertMany(data)
+    .then(data.forEach(recipe => {
+      //Se debe poner el elemento recipe en el console.log para que salgan los títulos
+      console.log(`Creating a new recipe: ${recipe.title}`)
+    }))
+    .catch(err => console.log(`Error at inserting many recipes`, err))
+})*/
+
+//Encontramos un documento para actualizarlo
+/*Recipe.findOneAndUpdate({
+    title: "Rigatoni alla Genovese"
+  }, {
+    duration: 100
+  }, {
+    new: true
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  //Para comprobar que haya cambiado, hacemos un console.log
+  .then(recipe => console.log(`Recipe updated`, recipe))
+  .catch(err => console.log(`Error at updating the Rigatoni`, err))
+*/
+
+/*Recipe.deleteOne({
+    title: "Carrot Cake"
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => console.log(`Recipe deleted`))
+  .catch(err => console.log(err))
+  */
+
+process.on('SIGINT', function () {
+  Mongoose.connection.close(function () {
+    console.log('Mongo Database disconnected through app termination');
+    process.exit(0);
   });
+});

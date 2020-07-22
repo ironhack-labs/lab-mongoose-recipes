3dd194fe5db1f33b4da218e7a0ecb4bbf4735ec7
Marta Jankowska <martajan218@gmail.com>
2020-07-22 21:59:01 +0200

    lab-complete

--- index.js
+++ index.js
@@ -17,11 +17,35 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    //return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    console.log('Connection successful')
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+const myRecipe = {title: 'Kartoffelpuffer', level: 'Easy Peasy', ingredients: ['potatoes', 'onion', 'eggs', 'salt', 'pepper'], cuisine: 'German', dishType: 'main_course', duration: 30};
+
+
+Recipe.insertMany(data)
+  .then((recipe) => console.log(`This recipe was saved ${recipe}`))
+  .catch((err) => console.log(err));
+
+Recipe.create(myRecipe)
+  .then((recipe) => console.log(`This recipe was saved ${recipe}`))
+  .catch((err) => console.log(err));
+
+
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+  .then((user) => console.log(user))
+  .catch((err) => console.log(err));
+
+Recipe.deleteOne({title: "Carrot Cake"})
+  .then((user) => console.log(user))
+  .catch((err) => console.log(err));
+
+
+mongoose.connection.close()

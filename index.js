835a86eeaf2ce0e4bd21e6089bd52679f0d7f157
srtagalicia <srtagalicia@gmail.com>
2020-07-30 18:47:20 +0200

    Completed lab Britta and Galicia

--- index.js
+++ index.js
@@ -8,20 +8,78 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+  
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+    title: 'Potatoes omelette', 
+    level: 'Easy Peasy',
+    ingredients:  ['potatoes, eggs, onion, olive oil'],
+    cuisine: 'Spanish',
+    dishType: 'main_course',
+    duration:  30,
+    creator: 'Britta & Galicia'
+    })
+
+    .then((result) => {
+      console.log(result.title)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    
   })
+
+  .catch(error => {
+    console.error('Error connecting to the database', error);
+  })
+
+  .then(() => {
+    Recipe.insertMany(data)  
+      .then((result) => {
+      console.log(result)
+      })
+      .catch((error) => {
+      console.log(error)
+    })
+    .then(() => {
+      Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {$set: {duration: 100} })
+      .then(() => {
+        console.log('Hooray! You have updated successfully your recipe!')
+      })
+      .catch((error) => {
+        console.log('Sorry, the recipe remains untouched. A chef never changes his recipes!')
+      })
+    })
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    })
+    .then(() => {
+      Recipe.findOneAndDelete({title: 'Carrot Cake'})
+      .then(() => {
+        console.log('Hooray! You have deleted successfully your recipe!')
+      })
+      .catch((error) => {
+        console.log('Sorry, the recipe remains untouched. A chef never deletes his recipes!')
+      })
+    })
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    })
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+MONGODB_URI.disconnect();

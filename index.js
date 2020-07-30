ee95cd316ec8d940a388bb6a8502b40e63182e55
Bodei Brouwer <bodeibrouwer@gmail.com>
2020-07-30 18:44:14 +0200

    By Alvaro, Lanette, Bodei

--- index.js
+++ index.js
@@ -20,8 +20,52 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({title: "Asian Glazed Chicken Thighs",
+    level: "Amateur Chef",
+    ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+    cuisine: "Asian",
+    dishType: "main_course",
+    image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    duration: 40,
+    creator: "Chef LePapu"})
+      .then((result) => {
+        console.log(result.title)
+      })
+      .catch ((err) => {
+        console.log('Loser!', err)
+      })
+
+    Recipe.insertMany(data)
+    .then((thing) => {
+      thing.forEach((elem) => {
+        console.log(elem.title)
+      })
+      Recipe.deleteOne({title: 'Carrot Cake'})
+      .then(() => {
+          console.log('Hooraaaaay! We are alive')
+    })
+    Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {$set: {duration: 100}})
+    .then(() => {
+        console.log('We are the champions, my friend!')
+    })
+    .catch ((err) => {
+      console.log('Loser!', err)
+    })
   })
+  })
+
+    // Run your code here, after you have insured that the connection was made
+    
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

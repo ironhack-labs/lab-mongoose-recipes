d6c214721c5f510d312c458a5e37223d6b0b338d
Michel Ribbens <michelribbens@gmail.com>
2020-07-31 15:37:49 +0100

    done but totally confused by this LAB

--- index.js
+++ index.js
@@ -12,16 +12,69 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
+  // Iteration 2 - Create a recipe
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Bahn Mi",
+      level: "Amateur Chef",
+      ingredients: [ 
+        "1 cup White Rice Vinegar CBT",
+        "1 cup Water, municipal",
+        "1 cup Sugars, granulated",
+        "1 cup Carrots, raw",
+        "1 Chicken, broilers or fryers, breast, meat only, raw",
+        "1 pinch Garlic Salt GL 0130 HS",
+        "1 pinch Spices, pepper, black",
+        "1 (12 inch) baguette"
+      ],
+      cuisine: "Vietnamese",
+      dishType: "other",
+      image: "https://prods3.imgix.net/images/articles/2016_05/Featured-Article-Herb-Sandwich-Banh-Bahn-Mi-Dinner-Party-Vietnamese-Sandwiches-Fired-Fish-Recipes.jpg",
+      duration: 20,
+      creator: "Chef Kim"
+    })
+    .then(recipe => console.log(recipe.title))
+    .catch(err => console.log('error: ', err));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+
+  // Iteration 3 - Insert multiple recipes
+  .then(() => {
+    Recipe.insertMany(data)
+    .then(allRecipes => allRecipes.forEach(recipe => {console.log(`This is recipe is called: ${recipe.title}`)}))
+    .catch(err => console.log('error: ', err));
+  })
+
+
+  // Iteration 4 - Update recipe
+  Recipe.findOneAndUpdate(
+    { title: "Rigatoni alla Genovese" },
+    { duration: 100 },
+    { new: true }
+  )
+    .then((updatedRecipe) => console.log(`The recipe ${updatedRecipe.title} has been updated succesfully!`, updatedRecipe))
+
+
+  // Iteration 5 - Remove a recipe
+  Recipe.deleteOne(
+    { title: "Carrot Cake" }
+  )
+    .then((deletedRecipe) => console.log(`The recipe ${deletedRecipe.title} has been deleted!`, deletedRecipe))
+
+  // Iteration 6 - Close the Database
+  // .then(() => {
+  //   console.log(`Close DB`);
+  //   mongoose.connection.close();
+  // })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,42 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    match: /^https?:\/\//,
+    default: 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

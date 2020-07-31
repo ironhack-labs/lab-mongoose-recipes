9d63b1bd2504bbba2c410f3fa17213e8f4bfe55d
Nuno Pacheco <nunorpacheco@gmail.com>
2020-08-01 00:50:50 +0100

    Done

--- index.js
+++ index.js
@@ -21,7 +21,55 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    Recipe.create(
+      { title: 'Derf Burger', 
+      level: "Amateur Chef", 
+      ingredients: [
+        "Beef",
+        "Bacon",
+        "Sausage",
+        "Ham",
+        "Cheese",
+        "Bread,",
+        "Onion",
+        "Tomato",
+        "Wisky",
+        "Beer",
+        "Salt",
+        "Spicy"
+      ], 
+      cuisine:"Portuguese", 
+      dishType:"main_course", 
+      duration:60, 
+      creator:"Daniel David Silva" 
+    }
+  )
+      .then(myRecipe => console.log(`Recipe created: `, myRecipe.title))
+      .catch(error =>
+        console.log(`Creating a new recipe went wrong! Try again 😞 ${error}`)
+      );
+
+// Insert Multiple Recipes
+.then(() => Recipe.insertMany(data))
+
+// Update Recipe
+.then(() =>
+     Recipe.findOneAndUpdate(
+       { title: "Rigatoni alla Genovese" },
+       { duration: 100 },
+       { new: true }
+     )
+   )
+   .then((recipe) => console.log("The recipe update succesfully!", recipe))
+
+// Remove a recipe
+.then(() => Recipe.deleteOne({ title: "Carrot Cake" }))
+.then(console.log("Oh oh! The Carrot Cake is no longer available."))
+
+// Close the Database
+.then(() => mongoose.connection.close())
+.then(console.log("The DB it's disconnected."))
+
+.catch(error => {
+  console.error('Error connecting to the database', error);
   });

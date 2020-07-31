cbf938aea8c26ac47959192a4784911c478a256f
Willem Prins <willemprins93@gmail.com>
2020-07-31 13:58:44 +0100

    done

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,8 +21,29 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({title: 'Bolognese', level: 'Easy Peasy', cuisine: 'Italian', creator: 'Willem Prins'})
+    .then(newRecipe => console.log(`New recipe: ${newRecipe.title}`))                                    
+    .catch(err => console.log(`Failed: ${err}`));                                                          
+
+    Recipe.create(data)
+    .then(newRecipes => newRecipes.forEach(item => console.log(`New recipe created: ${item.title}`)))
+    .then(() => {
+      Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {$set: {duration: 100}}, {new:true})
+      .then((updatedRecipe) => console.log(`Succes! ${updatedRecipe.title} updated.`))
+      .catch(err => console.log(`Update failed: ${err}`));
+
+      Recipe.deleteOne({title: 'Carrot Cake'})
+      .then(() => console.log(`Sucesfully deleted recipe.`))
+      .catch(err => console.log(`Could not delete recipe: ${err}`))
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+  setTimeout(() => {
+    mongoose.connection.close()
+  }, 2000);
+
+  // I honestly could not figure out how to properly, so I just used a timeout function. 
+  // Also, I thought .then chaining would work but it didn't. Can we get a refresher on this in class?

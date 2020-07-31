8415e8c85f180cbfa5df8920acf082bacba5c601
Lynn W-B <l.welshboucard@yahoo.co.uk>
2020-08-01 00:37:45 +0200

    All iterations completed

--- index.js
+++ index.js
@@ -20,8 +20,29 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+    Recipe.create({title:'Cold Chocolate Drink', level:'Easy Peasy', ingredients: ['oat milk','raw cacao'], cuisine:'Raw Foods', dishType: 'drink', duration: 2, creator: 'LWB'})
+      .then(recipe => console.log(`The recipe was saved and its title is: ${recipe.title}`))
+      .catch(error => console.log(`An error happened: ${error}`));
+    
+    Recipe.insertMany(data)
+      .then (recipes => recipes.forEach(recipe => console.log(`Title: ${recipe.title}`)))
+      .catch (error => console.log(`Error occurred adding recipes to database:${error}`));
+      
+    Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{$set:{duration:100}}, {new:true})
+      .then (success => console.log(`Recipe successfully updated`))
+      .catch( error => console.log(`Error occurred updating recipe: ${error}`));
+    
+    Recipe.deleteOne({title:'Carrot Cake'})
+      .then (deleted => console.log(`Recipe successfully deleted`))
+      .catch(error => console.log(`Error occurred deleting recipe: ${error}`));
+    })
+
+    .then(() => {
+      mongoose.connection.close()
+      .then(() => console.log('Connection with database closed successfully'))
+      .catch(error => console.log(`Error disconnecting from Mongoose:${error}`));
+        })
+      
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

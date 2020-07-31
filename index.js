447661d93cbe8dfef3568ece009b0a0f49ab56ac
Michel Ribbens <michelribbens@gmail.com>
2020-07-31 22:20:53 +0100

    updated complete version

--- index.js
+++ index.js
@@ -23,58 +23,56 @@ mongoose
 
   // Iteration 2 - Create a recipe
   .then(() => {
-    Recipe.create({
-      title: "Bahn Mi",
-      level: "Amateur Chef",
-      ingredients: [ 
-        "1 cup White Rice Vinegar CBT",
-        "1 cup Water, municipal",
-        "1 cup Sugars, granulated",
-        "1 cup Carrots, raw",
-        "1 Chicken, broilers or fryers, breast, meat only, raw",
-        "1 pinch Garlic Salt GL 0130 HS",
-        "1 pinch Spices, pepper, black",
-        "1 (12 inch) baguette"
-      ],
-      cuisine: "Vietnamese",
-      dishType: "other",
-      image: "https://prods3.imgix.net/images/articles/2016_05/Featured-Article-Herb-Sandwich-Banh-Bahn-Mi-Dinner-Party-Vietnamese-Sandwiches-Fired-Fish-Recipes.jpg",
-      duration: 20,
-      creator: "Chef Kim"
-    })
-    .then(recipe => console.log(recipe.title))
-    .catch(err => console.log('error: ', err));
-  })
+    Recipe.create(
+      {
+        title: "Bahn Mi",
+        level: "Amateur Chef",
+        ingredients: [ 
+          "1 cup White Rice Vinegar CBT",
+          "1 cup Water, municipal",
+          "1 cup Sugars, granulated",
+          "1 cup Carrots, raw",
+          "1 Chicken, broilers or fryers, breast, meat only, raw",
+          "1 pinch Garlic Salt GL 0130 HS",
+          "1 pinch Spices, pepper, black",
+          "1 (12 inch) baguette"
+        ],
+        cuisine: "Vietnamese",
+        dishType: "other",
+        image: "https://prods3.imgix.net/images/articles/2016_05/Featured-Article-Herb-Sandwich-Banh-Bahn-Mi-Dinner-Party-Vietnamese-Sandwiches-Fired-Fish-Recipes.jpg",
+        duration: 20,
+        creator: "Chef Kim"
+      }
+    )
+      .then(newRecipe => console.log(`Recipe: ${newRecipe.title}`))
+      .catch(err => console.log('error: ', err));
 
-  // Iteration 3 - Insert multiple recipes
-  .then(() => {
+    // Iteration 3 - Insert multiple recipes
     Recipe.insertMany(data)
-    .then(allRecipes => allRecipes.forEach(recipe => {console.log(`This is recipe is called: ${recipe.title}`)}))
-    .catch(err => console.log('error: ', err));
+      .then(allRecipes => allRecipes.forEach(recipe => {console.log(`Recipe: ${recipe.title}`)}))
+      .catch(err => console.log('error: ', err))
+
+      // Iteration 4 - Update recipe
+      .then(() => {
+        Recipe.findOneAndUpdate(
+          { title: "Rigatoni alla Genovese" },
+          { $set: { duration: 100 } },
+          { new: true }
+        )
+          .then(updatedRecipe => console.log(`The recipe ${updatedRecipe.title} has been updated succesfully!`))
+          .catch(err => console.log('error: ', err))
+      })
+      
+      // Iteration 5 - Remove a recipe
+      .then(() => {
+        Recipe.deleteOne(
+          { title: "Carrot Cake" }
+        )
+          .then(deletedRecipe => console.log(`The recipe ${deletedRecipe.title} has been deleted!`))
+          .catch(err => console.log('error: ', err))
+      })
   })
-
-
-  // Iteration 4 - Update recipe
-  Recipe.findOneAndUpdate(
-    { title: "Rigatoni alla Genovese" },
-    { duration: 100 },
-    { new: true }
-  )
-    .then((updatedRecipe) => console.log(`The recipe ${updatedRecipe.title} has been updated succesfully!`, updatedRecipe))
-
-
-  // Iteration 5 - Remove a recipe
-  Recipe.deleteOne(
-    { title: "Carrot Cake" }
-  )
-    .then((deletedRecipe) => console.log(`The recipe ${deletedRecipe.title} has been deleted!`, deletedRecipe))
-
-  // Iteration 6 - Close the Database
-  // .then(() => {
-  //   console.log(`Close DB`);
-  //   mongoose.connection.close();
-  // })
 
   .catch((error) => {
     console.error("Error connecting to the database", error);
-  });
+  });  

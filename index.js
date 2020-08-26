fcd4718cd2d47f277bcfee8999e41d915e825900
Daniel Zamarron <daniel.zamarron23@gmail.com>
2020-08-26 18:14:54 -0500

    Terminado

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,7 +22,81 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    agregarReceta();
+    agregarRecetasJson();
+    actualizarReceta('Rigatoni alla Genovese');
+    borrarReceta('Carrot Cake');
+    
+    setTimeout(() => {
+      mongoose.connection.close() 
+      console.log("Cerre la DB con connection.close()")
+    }, 2000)
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+//Nueva Receta
+async function agregarReceta(){
+  let carneRecipe = await Recipe.create({
+    title: "Carne Asada a la Tampiqueña",
+    level: "Amateur Chef",
+    ingredients: ["Carne Filete", "Frijoles", "Aguacate", "Queso", "Tortilla", "Salsa"],
+    cuisine: "Mexicana",
+    dishType: "main_course",
+    duration: 30,
+    creator: "Daniel"
+  })
+  .then(() => {
+    console.log('Adding - Carne Asada');
+  })  
+  .catch(() => {
+    console.log('Error Adding - Carne Asada');
+  });
+}
+
+//Receta from file
+async function agregarRecetasJson(){
+  await Recipe.insertMany(data)
+  .then(() => {
+    console.log('Adding - Multiple Data');
+  })  
+  .catch(() => {
+    console.log('Error Adding - Multiple Data');
+  });
+}
+
+//Update Titulo
+async function actualizarReceta(title)
+{
+  await Recipe.findOneAndUpdate({title}, {duration: 100})
+  .then(() => {
+    console.log(`Update - ${title}`);
+  })  
+  .catch(() => {
+    console.log(`Error Update - ${title}`);
+  });
+}
+
+//Borrar Titutlo
+async function borrarReceta(title)
+{  
+  await Recipe.deleteOne({title})
+  .then(() => {
+    console.log(`Delete - ${title}`);
+  })  
+  .catch(() => {
+    console.log(`Error Delete - ${title}`);
+  });
+}
+
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log(
+      "Cerre la DB por Ctrl+C"
+    )
+    process.exit(0)
+  })
+})

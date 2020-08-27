3af9cd65cf9205c354bfadca93d0a332a27fa667
Alba Mulero Otón <albamulero@MacBook-Pro-de-Alba.local>
2020-08-26 21:11:28 -0500

    paella

--- index.js
+++ index.js
@@ -21,7 +21,56 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    async function controlador(){
+      create()
+      await
+      agregar(data)
+      await
+      actualizar()
+      await
+      borrar()
+      await mongoose.connection.close()
+    }
+    controlador()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  async function create() {
+    const newReceta = await Recipe.create({
+      title: "Paella",
+      level: "UltraPro Chef",
+      ingredients: ["Pollo", "Arroz", "Azafran"],
+      cuisine: "Española",
+      dishType:"main_course",
+      image: "paella",
+      duration: 40,
+      creator: "Alba Mulero"
+    })
+  }
+
+  async function agregar(data) {
+    const newAgregar = await Recipe.insertMany(data)
+    //console.log(newAgregar)
+    newAgregar.forEach(element => {
+      console.log(element.title)
+    });
+  }
+
+  async function actualizar(){
+    const filtro = {title:"Rigatoni alla Genovese"}
+    const cambio = {duration: 100}
+    const newActualizar = await Recipe.findOneAndUpdate(filtro, cambio, {new:true})
+    console.log("Exitoso")
+  }
+
+  async function borrar(){
+    const filtro = {title:"Orange and Milk-Braised Pork Carnitas"}
+    const newBorrar = await Recipe.deleteOne(filtro)
+    console.log("Se borro")
+  }
+
+
+  

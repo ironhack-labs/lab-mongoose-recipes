2b9793010e0f4e866b40d553d742c032b329d9e9
eliot <eliot@pop-os.localdomain>
2020-08-31 00:14:27 -0500

    done

--- index.js
+++ index.js
@@ -9,19 +9,37 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(self => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+        // Before adding any documents to the database, let's delete all previous entries
+        return self.connection.dropDatabase();
+    })
+    .then(async() => {
+        // Run your code here, after you have insured that the connection was made
+        await Recipe.create({
+            title: "Pan de proteina",
+            level: "Easy Peasy",
+            ingredients: ["Proteina en polvo", "Huevo", "Polvo Royal", "Platano"],
+            cuisine: "Fit",
+            dishType: "dessert",
+            image: "https://i.pinimg.com/736x/6c/c7/98/6cc7985973020a4e5e91bbdad89c40f2.jpg",
+            duration: 5,
+            creator: "FitMan",
+        });
+        await Recipe.insertMany(data);
+        await Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 });
+        await Recipe.deleteOne({ title: "Carrot Cake" });
+    })
+    .then(disconnect)
+    .catch(error => {
+        console.error('Error connecting to the database', error);
+    });
+
+function disconnect() {
+    mongoose.connection.close();
+}

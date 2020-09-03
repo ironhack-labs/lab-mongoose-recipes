2003c8b7024222b741979f1f3eaf06d0e4324aa3
Kevin Klein <pkevinklein88@gmail.com>
2020-09-03 21:52:58 +0200

    recipes-app no connection with db

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+//const MONGODB_URI = 'mongodb://localhost:27017/lab-mongoose-recipes';
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,7 +23,24 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+// Recipe.insertMany(
+//     data
+// ).then(recipe => {
+//     console.log(recipe);
+// })
+Recipe.findOneAndUpdate(
+  {title:"Rigatoni alla Genovese"},{duration: 100}
+).then(update => {
+  console.log(update);
+}).catch(err => console.log(err))
+
+Recipe.deleteOne({title: "Carrot Cake"})
+.then(deleted => console.log(deleted))
+.catch(err => console.log("error: ",err))

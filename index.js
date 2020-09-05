65ce7e5359f5db54d095fdc07df72f9f1ff0db07
Rodrigo Muniz <ramunizbh@yahoo.com.br>
2020-09-05 10:38:18 -0300

    finalizado

--- index.js
+++ index.js
@@ -21,7 +21,35 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    const recipe = { title: 'Nova Receita', level: 'Easy Peasy', ingredients: ['açucar', 'farinha'], cuisine:'French',dishType: 'breakfast', duration: 50, creator: 'Rodrigo' };
+
+    const promise1 = Recipe.create(recipe)
+    .then(user => console.log(`Título da Receita: ${recipe.title}`))
+    .catch(error => console.log('An error happened while saving a new user:', error));
+
+    const recipes = require('./data.json');
+
+    const promise2 = Recipe.insertMany(recipes)
+    .then(function(recipe) {
+      for(let i=0;i < recipe.length;i++){
+        console.log(`Título da Receita: ${recipe[i].title}`)
+      }
+    })
+    .catch(error => console.log('An error happened while saving a new user:', error));
+
+    const promise3 = Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration:100})
+    .then(recipe => console.log(`Atualização efetuada com sucesso!`))
+    .catch(error => console.log('An error happened while saving a new user:', error));
+
+    const promise4 = Recipe.deleteOne({title: 'Carrot Cake'})
+    .then(result => console.log(`Registro removido com sucesso!`))
+    .catch(err => console.error(`Erro na exclusão do registro!`))    
+
+    Promise.all([promise1, promise2, promise3, promise4])
+    .then(values => {
+      console.log('Conexão Encerrada');
+      mongoose.connection.close();
+    })
+    .catch(err => console.error(err));
+
   });

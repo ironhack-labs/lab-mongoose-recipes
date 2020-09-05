74c074d2c8c0fd9670325f89a0a71e78bd12ab1e
Diana <d.thueroff@gmail.com>
2020-09-06 00:44:42 +0200

    iteration 1 - 4

--- index.js
+++ index.js
@@ -20,7 +20,46 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+    // Recipe.create({
+    //   "title": "Asian Glazed Chicken Thighs",
+    //   "level": "Amateur Chef",
+    //   "ingredients": [
+    //     "1/2 cup rice vinegar",
+    //     "5 tablespoons honey",
+    //     "1/3 cup soy sauce (such as Silver Swan®)",
+    //     "1/4 cup Asian (toasted) sesame oil",
+    //     "3 tablespoons Asian chili garlic sauce",
+    //     "3 tablespoons minced garlic",
+    //     "salt to taste",
+    //     "8 skinless, boneless chicken thighs"
+    //   ],
+    //   "cuisine": "Asian",
+    //   "dishType": "main_course",
+    //   "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //   "duration": 40,
+    //   "creator": "Chef LePapu"
+    // }).then((result)=>{ console.log(result.title); });
+    Recipe.insertMany(data)
+      .then((result) => {
+        const recipeTitle = result.map((recipe) => {
+          return recipe.title;
+        });
+        console.log(recipeTitle);
+
+        Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+          .then(() => {
+            console.log('success');
+          });
+
+      });
+
+
+
+
+
+    console.log('heyhey');
+    // Run your code here, after you have ensured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,45 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: [String],
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    //this is not correct yet
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    required: true,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg',
+    // validate: {
+    //   message: '',
+    //   validator: value => {
+    //     return value.toLowerCase() === value && value.includes('https://');
+    //   }
+    // }
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: new Date()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

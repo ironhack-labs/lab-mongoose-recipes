3e6729efa2156257f3b2b779f2247be3c6cc15ae
unimatrix2 <dnny.nis@gmail.com>
2020-09-05 10:31:12 -0300

    finished everything

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -9,19 +11,60 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
+    .connect(MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then((self) => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+        // Before adding any documents to the database, let's delete all previous entries
+        return self.connection.dropDatabase();
+    })
+    .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+        // Iteration 2
+
+        /* Recipe.create({
+            title: 'Asian Glazed Chicken Thighs',
+            level: 'Amateur Chef',
+            ingredients: [
+                '1/2 cup rice vinegar',
+                '5 tablespoons honey',
+                '1/3 cup soy sauce (such as Silver Swan®)',
+                '1/4 cup Asian (toasted) sesame oil',
+                '3 tablespoons Asian chili garlic sauce',
+                '3 tablespoons minced garlic',
+                'salt to taste',
+                '8 skinless, boneless chicken thighs',
+            ],
+            cuisine: 'Asian',
+            dishType: 'main_course',
+            image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+            duration: 40,
+            creator: 'Chef LePapu',
+        })
+            .then((doc) => console.log(doc.title)); */
+
+        // Iteration 3
+
+        Recipe.insertMany([...data])
+            .then(() => {
+                // Iteration 4
+                Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true }, (err, doc) => {
+                    if (err) { console.log(err); }
+                    console.log(doc);
+                });
+            })
+            .then(() => {
+                // Iteration 5
+                Recipe.deleteOne({ title: 'Carrot Cake' }, (err) => console.log(err))
+                    // Iteration 6
+                    .then(() => mongoose.connection.close());
+            });
+    })
+    .catch((error) => {
+        console.error('Error connecting to the database', error);
+    });

bfb232f011891e5da46bbbe208fae811354d130c
Leonardo Mezzanotti <leomezza@gmail.com>
2020-09-05 11:24:44 -0300

    All done, better coding and using async/await

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
+  .then(async () => {
     const newRecipe = {
       title: 'Pipoca',
       level: 'Amateur Chef',
@@ -32,31 +32,24 @@ mongoose
       creator: 'Chef Leo',
     };
 
-    Recipe.create(newRecipe)
-      .then((recipe) => {
-        console.log('Recipe', recipe.title, 'created');
-      })
-      .then(() => {
-        Recipe.insertMany(data)
-          .then((recipes) => {
-            recipes.forEach((recipe) => {
-              console.log('Recipe', recipe.title, 'created');
-            });
-          })
-          .then(() => {
-            Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } }, { new: true })
-              .then((updatedRecipe) => {
-                console.log('Duration from recipe', updatedRecipe.title, 'updated to', updatedRecipe.duration);
-              });
-          })
-          .then(() => {
-            Recipe.deleteOne({ title: 'Carrot Cake' })
-              .then(() => {
-                console.log('Recipe Carrot Cake removed successfully');
-                mongoose.connection.close();
-              });
-          });
-      });
+    const myRecipe = await Recipe.create(newRecipe);
+    console.log(`Recipe "${myRecipe.title}" created`);
+
+    const recipesFromData = await Recipe.insertMany(data);
+    recipesFromData.forEach((recipe) => {
+      console.log(`Recipe "${recipe.title}" created`);
+    });
+  })
+  .then(async () => {
+    const updatedRecipe = await Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } }, { new: true });
+    console.log(`Duration from recipe "${updatedRecipe.title}" updated to ${updatedRecipe.duration} minutes`);
+
+    await Recipe.deleteOne({ title: 'Carrot Cake' });
+    console.log('Recipe "Carrot Cake" removed successfully');
+  })
+  .then(() => {
+    mongoose.connection.close();
+    console.log('Database connection closed');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

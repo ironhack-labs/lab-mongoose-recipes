30720c1e06aa58355f5a62f1f3d65cf4868e0266
Tami Pipoca <tamisakita@hotmail.com>
2020-09-05 23:47:29 -0300

    done

--- index.js
+++ index.js
@@ -12,15 +12,47 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async () => {
+    const newRecipe = {
+      title: 'Tapioca',
+      level: 'Easy Peasy',
+      ingredients: ['tapioca', 'cheese', 'eggs'],
+      cuisine: 'brasilian',
+      dishType: 'breakfast',
+      image: 'https://varginhadigital.com.br/wp-content/uploads/2018/10/tapioca-queijo-ovos-FILEminimizer.jpg',
+      duration: 10,
+      creator: 'Portugueses',
+    };
+
+    try {
+      const createdRecipe = await Recipe.create(newRecipe);
+      console.log(`Inserted one new Recipe => ${createdRecipe.title}`);
+
+      const manyNewRecipe = await Recipe.insertMany(data);
+      console.log('Finished inserting many Recipes');
+      manyNewRecipe.forEach(recipe => console.log(recipe.title));
+
+      const updatedRecipe = await Recipe.findOneAndUpdate(
+        { title: 'Rigatoni alla Genovese' },
+        { $set: { duration: 100 } },
+        { new: true, useFindAndModify: false },
+      );
+      console.log(`Successfully updated recipe ${updatedRecipe.title}`);
+
+      const deletedRecipe = await Recipe.deleteOne({ title: 'Carrot Cake' });
+      console.log('Successfully deleted recipe');
+
+      mongoose.connection.close();
+    } catch (error) {
+      console.log(error);
+    }
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

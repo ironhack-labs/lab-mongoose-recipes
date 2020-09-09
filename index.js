00bab864f8da8025e91f9df20724ca735d9e2eea
Juan Pestana <juan.pestana82@gmail.com>
2020-09-09 16:44:00 +0200

    esta todo y parece que funciona

--- index.js
+++ index.js
@@ -17,11 +17,46 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    Recipe.create({
+                  title: "Salmorejo", 
+                  type: "Easy Peasy", 
+                  ingredients: ["tomato", "bread", "garlic", "olive oil", "jerez vinegar"], 
+                  cuisine: "mediterranean", 
+                  dishType: "soup", 
+                  duration: 20, 
+                  creator: "Juan Pestana", 
+                  created: Date.now()
+                })
+    .then(newRecipe => console.log(newRecipe.title))
+
+
+
+    .then(() => Recipe.create(data))
+    .then(newRecipes => console.log(newRecipes.length))
+
+
+    .then(() => Recipe.updateOne({title: "Rigatoni alla Genovese"},{duration: 100}))
+    .then(changes => console.log("los cambios qque se han hecho son:  ", changes))
+
+
+    .then(() => Recipe.deleteOne({title: "Carrot Cake"}))
+    .then(deleted => console.log('se ha eliminado el Carrot Cake  ', deleted))
+
+    .then(()=> mongoose.connection.close())
+      
+    .then(() => console.log("cerramos la conexión con la base de datos"))
+
+    .catch(err => console.log("Hubo algún fallo en alguna de las operaciones", err))
+
+
   })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

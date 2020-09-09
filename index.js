df71d0ee3655a72a7479e6822e048bafb15c1c30
Oliver <oli.b.alonso@gmail.com>
2020-09-09 17:47:18 +0200

    done

--- index.js
+++ index.js
@@ -15,13 +15,34 @@ mongoose
     useUnifiedTopology: true
   })
   .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
+    // console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+
+  .then(() => Recipe.create({
+    title: "Asian  Chicken Thighs",
+    level: "Amateur Chef",
+    ingredients: ["1/2 cup rice vinegar", "5 tablespoons honey"],
+    cuisine: "Asian",
+    dishType: "main_course",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 60,
+    creator: "Chef LePapu"
+  }))
+
+  .then((Arecipe) => console.log(Arecipe.title))
+  .then(() => Recipe.create(data))
+  .then(recipes => recipes.forEach(elm => console.log(elm.title)))
+  .then(() => Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 }))
+  .then(theRecipes => theRecipes.n === 1 && theRecipes.nModified === 1 && theRecipes.ok === 1 ? console.log('oh yeah') : null)
+  .then(() => Recipe.deleteOne({ title: "Carrot Cake" }))
+  .then(allRecipes => allRecipes.n === 1 && allRecipes.ok === 1 && allRecipes.deletedCount === 1 ? console.log('Here We Go!') : null)
+  .then(() => mongoose.disconnect())
+
+
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

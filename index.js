cd92f9fd3f6664f0b1756437b7f761d3e59f25a3
Guira <guira@Guiras-MacBook-Pro.local>
2020-09-09 15:51:34 +0200

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
+
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
@@ -8,20 +9,41 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+//mongoose.set('useFindAndModify', false)
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
-  .then(self => {
+  
+   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+const pepe= {
+  title: "tortilla de patata",
+  level: "Amateur Chef",
+  ingredients: [
+    "huevos",
+    "patata",
+    "chorizo",
+    "ketchuip",
+  ],
+  cuisine: "International",
+  dishType: "dessert",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
+  duration: 180,
+  creator: "Chef Nadia"
+}
+/*Recipe.create(pepe).then(newRecipes => {console.log('Los nuevos elementos creados:', newRecipes)})*/
+  .then(() => Recipe.insertMany(data))
+  .then(newRecipes => console.log('Los nuevos elementos creados:', newRecipes))
+  .then(()=>Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, { $set: { duration: 100 }}, {new:true} ))
+  .then(() => Recipe.deleteOne({title: 'Carrot Cake'},console.log("recipe deleted")))
+  .then(()=>mongoose.connection.close())
+  .catch(err => console.error("no hemos podido borrar el elemento"))
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,46 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    default: 'Desconocido',
+    trim: true,
+},
+  level: {
+    type: String,
+    enum:[`Easy Peasy`, `Amateur Chef` , `UltraPro Chef`]
+},
+ ingredients: {
+    type: [String]
+},
+  cuisine: {
+      type: String,
+      required: true         // Array de strings
+  },
+  dishtype: {
+      type: String,
+      enum:['breakfast', 'main_course', 'soup,' ,'snack', 'drink', 'dessert' , 'other']
+  },
+  image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min:0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date
+  },
+},
+{
+timestamps: true
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

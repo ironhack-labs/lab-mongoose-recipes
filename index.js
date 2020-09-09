dcc8b3af0eadf27b94559f30af34667147d3034a
Pedro Conde <condepedro968@gmail.com>
2020-09-09 21:59:16 +0200

    adding changes

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { update } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+mongoose.set('useNewUrlParser', true);  // Fix all deprecation warnings
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 // Connection to the database "recipe-app"
 mongoose
@@ -20,8 +26,34 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create([{
+        title: "mi receta",
+        level: "Easy Peasy",
+        ingredientes: ['Soja', 'Apio'],
+        cuisine: "Alemana",
+        dishType: "breakfast",
+        duration: 15,
+        creator: "Pedro Conde",
+        created: Date.now()
+    }])
   })
+  .then(newRecipe => console.log(newRecipe[0]))
+  .then(() => Recipe.insertMany(data))
+  .then(data => data.forEach(element => console.log(element.title)))
+  .then(() => Recipe.findOneAndUpdate({
+      title: "Rigatoni alla Genovese"
+    }, {
+        duration: 100
+    }, {
+        new: true
+    }))
+    .then(update => console.log("Actualizado!", update))
+    .then(() => Recipe.deleteOne({
+        title: "Carrot Cake"
+    }))
+    .then(el => console.log("Borrado correctamente", el))
+    .then(() => mongoose.connection.close())
+    .then(() => console.log("Cerramos la conexión"))
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

cff480a66a3d2e5975f201cda1043d293a7ab49a
root <root@DESKTOP-53RFEB6.localdomain>
2020-09-09 18:08:05 +0200

    [WEB-MAD DE] Amanda Ordonez

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+mongoose.set('useFindAndModify', false); // Add to eliminate a warning message from mongoose, as the documentation indicate
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -20,8 +22,46 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Add manually a recipe. I use my curry spice mix ;-) For cook, stir-fry 1/2tbsp of garlic, 1/2tbsp of fresh ginger and an onion. Add the mix and tomato sauce, cream or whatever. Better use sunflower oil
+    const manualRecipe = {
+      title: "Curry Spices mix",
+      level: "Easy Peasy",
+      ingredients: ["1/2tbsp clove", "1tsp curcuma", "1-1/2tbsp cumin", "1/2tbsp nutmeg", "2tbs coriander", "1 cup tomato", "1/8tbsp cayenne (optional)"],
+      cuisine: "Indian",
+      dishType: "other",
+      duration: 0,
+    }
+    Recipe.create(manualRecipe)
+      .then(oneRecipe => console.log(`The recipe ${oneRecipe.title} is now in the database!`))
+      .catch(err => console.log(`Ups, error: ${err}`))
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then(() => {
+    // Add all the data and return to manipulate later
+    return Recipe.create(data)
+  })
+  .then(() => Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })) // Replace manually the duration of the Rigatoni alla Genovese. 
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+  }) // Remove the Carrot Cake manually. with the return, the array with all the recipes not longer exists, but allow indicate the remove with a console.log in the next line
+  .then(removedRecipe=> console.log(removedRecipe)) // Delete must be the last operation because with the return, allRecipes are no longer aviable in the memory
+  .catch(error => console.error('Error connecting to the database', error));
+
+
+
+// Mongooose events
+// When successfully connected
+mongoose.connection.on('connected', () => console.log('Mongoose default connection open'));
+
+// If the connection throws an error
+mongoose.connection.on('error', err => console.log(`Mongoose default connection error: ${err}`));
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => console.log('Mongoose default connection disconnected'))
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  })
+})
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,58 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+
+  ingredients: {
+    type: [String],
+    trim: true
+  },
+
+  cuisine: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"],
+    trim: true,
+    lowercase: true
+  },
+
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+
+  duration: {
+    type: Number,
+    min: 0
+  },
+
+  creator: {
+    type: String
+  },
+
+  created: {
+    type: Date,
+    default: Date.now()
+  }
+
+}, {
+  timestamps: true
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

07071d501bc3ad9ec2c27d442ed9130fd0ead5e3
anabermudezmonteagudo <anabermudezmonteagudo@gmail.com>
2020-09-09 17:31:22 +0200

    done

--- index.js
+++ index.js
@@ -12,16 +12,50 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false 
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
+    return self.connection.dropDatabase()})
+  //.catch(err => console.log('error connecting', err))
+    
+  //-------Iteración 2: Crear una nueva receta
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  return Recipe.create({
+    title: 'cachopo',
+    ingredients:['ternera', 'jamon serrano', 'queso'],
+    cuisine: 'asturiana',
+    dishType: 'main_course',
+    image: "https://recetasdecocina.elmundo.es/wp-content/uploads/2017/04/cachopo-asturiano.jpg",
+    duration: 45,
+    creator: 'Chef Ana'})
   })
+  .then(newRecipe => console.log('el título de la nueva receta es', newRecipe.title))
+
+  //-------Iteración 3: insertar multiples recetas
+  .then(() => Recipe.insertMany(data))
+  .then(data => data.forEach(elm => console.log(`${elm.title}`)))
+      
+  //-------Iteración 4: update 
+  .then(() => Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true}))
+  .then(newDuration => console.log('el nuevo tiempo de Rigatoni alla Genovese es', newDuration.duration))
+
+  //-------Iteración 5: Remove
+  .then(() => Recipe.deleteOne({title: 'Carrot Cake'}))
+  .then(console.log('La Receta no está disponible'))
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+//---------Iteración 6: cerrar database
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    })
+})

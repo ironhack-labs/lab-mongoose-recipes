6d688c3c5f8fb97d47a0ebbb8f21fa35cf5b529d
aretius <jonarechalde@gmail.com>
2020-09-09 16:51:10 +0200

    Done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -21,7 +22,59 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    //----- ITERATION 2 | CREATION -----
+    return Recipe.
+      create({
+        title: 'Spaghetti carbonara',
+        level: 'Easy Peasy',
+        ingredients: ['250g de spaghetonnis', '3 huevos', '75g de guancciale', '30g parmesano', '3 ajos', 'Pimienta', 'Sal'],
+        cuisine: 'Italiana',
+        dishType: 'main_course',
+        duration: 20,
+        creator: 'Davide Contrino'
+      })
+
+  })
+  .then((createdRecipe) => {
+
+    console.log('\n----- ITERATION 2 -----\nThe created recipe is:\n')
+    console.log(createdRecipe)
+
+    //----- ITERATION 3 | MULTI-CREATION -----
+
+    return Recipe.
+      create(data)
+
+  })
+  .then(Recipe.find())
+  .then((allRecipes) => {
+
+    console.log('\n----- ITERATION 3 -----\nThe name of the recipes are:\n')
+    allRecipes.forEach(elm => console.log(elm.title))
+
+    //----- ITERATION 4 -----
+
+    return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+
+  })
+  .then(() => {
+
+    console.log('\n----- ITERATION 4 -----\nSuccess!!!!!\n')
+
+    //----- ITERATION 5 -----
+
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
+
+  })
+  .then(() => {
+
+    console.log('\----- ITERATION 5 -----\nSuccess!!!!!')
+
+    //----- ITERATION 6 -----
+    mongoose.connection.close()
+
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.error('Error connecting to the database', error)
+  })
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -49,6 +49,6 @@ const recipeSchema = new Schema({
   timestamps: true
 })
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const recipe = mongoose.model('recipe', recipeSchema);
 
-module.exports = Recipe;
+module.exports = recipe;

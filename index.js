1b10c0a5108da4a26773d8981e70797508f1c0fc
Javier de Salas <chinostrike@gmail.com>
2020-09-11 20:02:12 +0200

    finished

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const recipeObj = data[0] 
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -19,9 +21,36 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+  
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data)  // Iteration 3
   })
+  
+  .then (recipe => { // Iteration 4
+    recipe.forEach(elm => console.log(elm.title))
+    return Recipe.updateOne({ title: 'Rigatoni alla Genovese'},{duration: 100})
+  })
+  
+  .then (details => {// Iteration 5
+    console.log(details)
+    return Recipe.deleteOne({title: 'Carrot Cake'})
+  })
+     
+  .then (recipe => { // Iteration 6
+    console.log("Recipe Deleted!", recipe)
+    return mongoose.connection.close()
+  })
+  .then()   
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  //  //no Run your code here, after you have insured that the connection was made
+  //  // Iteration 2
+  //  Recipe
+  //  .create(recipeObj)
+  //  .then(recipe => console.log(recipe.title))
+  //  .catch(error =>
+  //    console.log('An error happened while saving a new recipe:', error))
+

388dbf58701d6c02e5ce3e6aabf6f6b260328c52
Lisa Poncet <poncet.lisa@gmail.com>
2020-09-17 16:01:20 +0200

    iteration 2 done

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
+    console.log("hellow");
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Moelleux au chocolat",
+      level: "Easy Peasy",
+      ingredients: ["eggs", "chocolate", "flour"],
+      cuisine: "french",
+      dishType: "dessert",
+      duration: 30,
+      creator: "Lisa",
+    }).then((newRecipe) => {
+      console.log(newRecipe);
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

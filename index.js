28278af0818977ac4857fff2966c74e5916aa2f2
MaxJGruber <maxjgruber@gmail.com>
2020-09-17 17:22:16 +0200

    Lab Solved

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    launchCRUDSequence();
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+async function launchCRUDSequence() {
+  const createdRecipe = await Recipe.create(data[0])
+    .then((recipe) => console.log(recipe.title))
+    .catch((err) => console.log("error in iteration", err));
+
+  const insertedAll = await Recipe.insertMany(data)
+    .then(data.forEach((element) => console.log(element.title)))
+    .catch((err) => console.log("error in iteration", err));
+
+  const updatedRecipe = await Recipe.findOneAndUpdate(
+    { title: "Rigatoni alla Genovese" },
+    { duration: 100 },
+    { new: true }
+  );
+  console.log(updatedRecipe);
+
+  const deletedRecipe = await Recipe.deleteOne({ title: "Carrot Cake" }).then(
+    console.log("SUCCESS!!!")
+  );
+  mongoose.connection.close();
+}

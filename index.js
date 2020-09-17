04bddad9839a11f734028bc56f541d4c71118ef3
AyloSrd <aylo.srd@gmail.com>
2020-09-17 16:44:29 +0200

    iteration-5 done

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-console.log(Recipe.prototype)
+//console.log(Recipe.prototype)
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -23,9 +24,33 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    Recipe.create(data[0])
-      .then(recipe => console.log(recipe.title))
-    
+
+    // Iteration-2
+    // Recipe.create(data[0])
+    //   .then(recipe => console.log(recipe.title))
+    //   .catch(err => console.error('error in iteration-2', err))
+    //Iteration-3
+    Recipe.insertMany(data)
+      .then(coll => coll.forEach(x => console.log(x.title)))
+      .catch(err => console.error('error in iteration-3', err))
+      .then(() => {
+        //Iteration-4
+        Recipe.findOneAndUpdate(
+          {title: 'Rigatoni alla Genovese'},
+          {duration: 100},
+          {new: true}
+        )
+        .then(updated => console.log(updated))
+        .catch(err => console.error('error in iteration-4', err))
+        .then(() => {
+          //Iteration-5
+          Recipe.deleteOne(
+            {title: 'Carrot Cake'}
+          )
+          .then(deleted => console.log(deleted))
+          .catch(err => console.error('error in iteration-5', err))
+        })
+      })    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -34,13 +34,3 @@ const recipeSchema = new Schema({
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
-
-/*
-  const IronhackerSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  favoriteLanguage: String,
-});
-
-const IronHackerModel = mongoose.model("ironhackers", IronhackerSchema);
-*/

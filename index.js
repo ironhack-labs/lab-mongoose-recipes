576b8a0d4c6048c3a624ee1f76b743382cc280f9
Rico <rasim.rico@gmail.com>
2020-09-17 17:44:27 +0200

    iteration 5 & 6

--- index.js
+++ index.js
@@ -8,43 +8,38 @@ const data = require("./data");
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((self) => {
+
+(async function () {
+  try {
+    const self = await mongoose.connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    Recipe.create(data[0]).then(() => {
-      console.log(data[0].title);
-    });
-  })
-  .then(() => {
-    Recipe.insertMany(data)
-      .then(() => {
-        data.forEach((recipe) => {
-          console.log(recipe.title);
-        });
-      })
-      .catch((err) => console.log(err));
-  })
-  .then(() => {
-    const updatedRecipe = Recipe.findByIdAndUpdate(
+    self.connection.dropDatabase();
+
+    const res = await Recipe.create(data[0]);
+    console.log(res.title);
+
+    const res2 = await Recipe.insertMany(data);
+    res2.forEach((r) => console.log(">", r.title));
+
+    const res3 = await Recipe.findOneAndUpdate(
       { title: "Rigatoni alla Genovese" },
       { duration: 100 },
       { new: true }
     );
-    Recipe.deleteOne({
+
+    const res4 = await Recipe.deleteOne({
       title: "Carrot Cake",
     });
-    console.log(updatedRecipe);
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database", error);
-  });
+
+    mongoose.connection.close();
+  } catch (err) {
+    console.error(err);
+  }
+})();

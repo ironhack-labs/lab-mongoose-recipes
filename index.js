e8a69f534b417e25bada63dfa7cc6f327572c056
salim ameur <salim.ameur@gmail.com>
2020-09-18 09:13:06 +0200

    lab done

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify:false
   })
   
   .then(self => {
@@ -20,18 +21,41 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
+  .then(async() => {
     
     // Run your code here, after you have insured that the connection was made
-    // Recipe.create(data[0])
-    // .then(recipe => console.log(recipe.title))
 
-    Recipe.insertMany(data).then((reciepes) =>
-    reciepes.forEach((element) => console.log(element.title))
-  );
+      const newRecipe = {
+        title: 'tagine',
+        level: 'UltraPro Chef',
+        ingredients: ['onion', 'chiken', 'olive', 'potato'],
+        cuisine: 'morroco',
+        duration: 15,
+        creator: 'Amine',
+      };
+  
+      const myRecipe = await Recipe.create(newRecipe);
+      console.log(`Recipe "${myRecipe.title}" created`);
+  
+      const  insertMultipleRecipes = await Recipe.insertMany(data);
+      insertMultipleRecipes.forEach((recipe) => {
+        console.log(`Recipe "${recipe.title}" created`);
+      });
+      const updatedRecipe = await Recipe.findOneAndUpdate({ $set: { duration: 100 } }, { new: true });
+      console.log(`Duration from recipe updated to ${updatedRecipe.duration} minutes`);
+  
+      await Recipe.deleteOne({ title: 'Carrot Cake' });
+      console.log('Recipe "Carrot Cake" removed successfully');
 
-
-  })
+    })
+  
+    .then(() => {
+   
+      mongoose.connection.close();
+      console.log('Database connection closed');
+    })
+    
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

b4a493d5ad2a9f84767e3b6b683cb755664b6bac
carolinacervelaf <carolinacervelaf@gmail.com>
2020-09-29 11:39:20 -0300

    done

--- index.js
+++ index.js
@@ -14,14 +14,38 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Tapioca with egg",
+      level: "Easy Peasy",
+      ingredients: ["2 tbsp. of tapioca", "one egg", "spices"],
+      cusine: "Brasilian",
+      dishType: "breakfast",
+      image: "https://corpoacorpo.com.br/upload/imagens_upload/tapioca_com_ovo.jpg",
+      duration: 10
+    })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .then((result) => {
+      console.log('CREATE RECIPE TITLE', result)
+    }).catch((err) => console.error(err)); 
+
+      Recipe.insertMany(data).then(result => {
+      result.map(recipe => console.log(recipe.title)).catch((err) => console.error(err));
+      
+      Recipe.updateOne({title: 'Rigatoni alla Genovese'},
+      {$set: {diration: 100}}).then((result) => {
+        console.log("update succesful!", result);
+      }).catch((err) => console.error(err));
+
+      Recipe.deleteOne({title: 'Carrot Cake'}).then((result) => {
+      console.log("deletion successful", result).catch((err) => console.error(err));
+
+    }).catch((err) => console.error(err));
+   
+    });

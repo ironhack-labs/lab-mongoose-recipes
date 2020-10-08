b8a99822e38a91d79a37e88b69edefd76f6678a3
Lovro <hodak.lovro@gmail.com>
2020-10-08 17:45:58 +0200

    vykintas und lovro

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,7 +23,55 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+   /* THIS IS ITERATION 2!!!!!!!!!
+    Recipe.create( data[0] )
+      .then((recipe) => {
+        console.log(recipe.title)
+
+      })
+      .catch(()=> {
+        console.log()
+      })
+      */
+
+     Recipe.insertMany(data)
+        .then((recipes) => {
+          //console.log(recipes)
+          for(let recipe in recipes){
+            console.log(recipes[recipe].title)
+          }
+
+          let updatedPromise = Recipe.updateOne({title: "Rigatoni alla Genovese"}, {$set: {duration: 100}})
+
+            updatedPromise.then(() => {
+                console.log('Updated yo!')
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+            let deletedPromise = Recipe.deleteOne({title: 'Carrot Cake'})
+
+              deletedPromise.then(() => {
+                console.log('Deleted yo!')
+            })
+              .catch((err) => {
+                console.log(err)
+            })
+          Promise.all([updatedPromise, deletedPromise])
+            .then(() => {
+              console.log('Connection closed!')
+              mongoose.connection.close()
+            }) 
+            .catch((err) => {
+                console.log('Throw an error: ', err)
+                
+            }) 
+       })
+        .catch((err)=> {
+          console.log(err)
+    })
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

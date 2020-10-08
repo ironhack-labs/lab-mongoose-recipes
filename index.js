b224f2064619e5bfb4c21d2e7ff6f61bd9153942
Aleksandra Tanasijenko <alekstana@gmail.com>
2020-10-08 17:15:52 +0200

    Completed lab

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+// const { model } = require('./models/Recipe.model');
+
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -20,8 +23,47 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+   
+    Recipe.create({title:"Pizza", level: "Amateur Chef", cuisine: "Italian", ingredients: ["flour", 'cheese', 'tomato sauce', 'pure magic']})
+    .then((recipe) => {
+      console.log(recipe.title)
+    })
+    .catch()
+
+    Recipe.insertMany(data)
+    .then((allRecipes) =>{
+
+      allRecipes.forEach((recipe) => {
+        console.log(recipe.title)
+      })
+      Recipe.updateOne({title:"Rigatoni alla Genovese"}, {duration:100})
+      .then(()=>{
+        console.log("updated")
+      })
+      Recipe.deleteOne({title:"Carrot Cake"})
+      .then(() =>{
+        console.log("SUCCESSSSS")
+      })
+    })
+    .catch((recipe) =>{
+      console.log(recipe.title)
+    })
+
+    mongoose
+  .disconnect(MONGODB_URI, {
+    useCreateIndex: false,
+    useNewUrlParser: false,
+    useUnifiedTopology: false
+  })  
+  .then(() => {
+    console.log("Yippie yay closed")
+  })
+  .catch(() => {
+    console.log("nope")
+  })
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
+    
   });

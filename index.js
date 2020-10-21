611c918fcf907c975819dc06a9873fbd8734297f
erifylo <erif.evangelou@gmail.com>
2020-10-21 18:08:42 +0200

    done

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+ const promise1 = Recipe.insertMany(data);
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+Promise.all([promise1])
+  .then(values => {
+    console.log('New recipes have been inserted');
+    console.log(values);
+    mongoose.connection.close();
+  })
+  .catch(err => console.error(err));
+
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app'; 
+
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +28,7 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+  //  return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -25,3 +36,50 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  /* (async () => {
+    try {
+      const recipe = await Recipe.create({
+        title: 'Tzatziki',
+        level: 'Easy Peasy',
+        ingredients: 'Yogurt, cucumber, Olive oil, garlic, salt',
+        cuisine: 'Greek',
+        dishType: 'other',
+        duration: 15,
+        creator: 'Eri',
+
+      });
+      console.log(`The recipe is ${recipe.title}`);
+    } catch (error) {
+      console.log(error.message);
+    }
+  })(); */
+
+
+  (async () => {
+  try {
+    const update = await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+    console.log(update);
+  } catch (error) {
+    console.log(error.message);
+  }
+})(); 
+  
+
+(async () => {
+  try {
+    const deleted = await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log(deleted);
+  } catch (error) {
+    console.log(error.message);
+  }
+})(); 
+
+mongoose.connection.close(() => {
+  console.log('Mongoose default connection disconnected through app termination');
+});

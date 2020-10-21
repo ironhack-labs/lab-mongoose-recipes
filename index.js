4bad57f0a846fa98c9686da6eb756d31a191d315
Cristina Castro <cristinacastro87@gmail.com>
2020-10-21 18:28:03 +0200

    done

--- index.js
+++ index.js
@@ -17,11 +17,81 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+   // return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+
+/*     //ITERATION 2
+
+  (async () => {
+    try {
+      const recipe = await Recipe.create({
+        title: "Crema de calabaza",
+        level: 'Easy Peasy',
+        ingredients: ['calabaza', 'agua', 'sal', 'aceite'],
+        cuisine: "Mediterránea",
+        dishType: 'soup',
+        image: "https://images.media-allrecipes.com/images/75131.jpg",
+        duration: 20,
+        creator: "Cristina Castro",
+        created: 22/10/2020
+      });
+      //console.log(recipe)
+      console.log(`The name of this recipe is ${recipe.title}`);
+    } catch (error) {
+      console.log(error.message);
+    }
+  })(); 
+ 
+  //console.log(data);
+
+
+  //ITERATION 3
+
+   (async () => {
+    try {
+      const insert = await Recipe.insertMany(data);
+      console.log(insert.map(recipes => recipes.title));
+    } catch (error) {
+      console.log(error.message);
+    }
+  })();  
+
+  //ITERATION 4
+
+ (async () => {
+  try {
+    const update = await Recipe.findByIdAndUpdate("5f905d35cb7b3a7ccd5fb881", 
+      { duration: 100},
+      { new: true }
+    );
+    console.log(update);
+  } catch (error) {
+    console.log(error.message);
+  }
+})();   */
+
+
+//ITERATION 5
+
+/*   (async () => {
+  try {
+    const deleted = await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log(deleted);
+  } catch (error) {
+    console.log(error.message);
+  }
+})();    
+ 
+
+//ITERATION 6
+
+var close = mongoose.connection.close()
+console.log(close) */
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  

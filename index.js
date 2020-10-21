7a0da495898b7d3c2ef1a4d4df5eb6724a2f647b
Raiza Escobar <raizaescobar@gmail.com>
2020-10-21 20:31:06 +0200

    [FT-BCN-0920] Raiza Escobar

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -19,9 +19,41 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async () => {    
+    await addRecipe();
+    await addAllRecipes();
+    await updateRecipe();
+    await closeConnection();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  async function addRecipe(){
+    const myRecipe={title: "Pasta alla boscaiola",
+  level:"Easy Peasy" ,
+  ingredients:["pasta", "mushrooms", "ham", "sausage", "cream"],
+  cuisine: "Italian",
+  dishType:"main_course",
+  duration:20};
+  Recipe.create(myRecipe).then(element=>console.log(element.title))
+  }
+
+  async function addAllRecipes(){
+    Recipe.insertMany(data).then(elements=>{
+      elements.forEach(el=>{
+        console.log(el.title);  
+      })
+    })
+  }
+
+  async function updateRecipe(){
+    Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration: 100}).then(el=> console.log("Updated!"));
+  }
+  async function removeRecipe(){
+    Recipe.deleteOne({title: "Carrot Cake"}).then(el=> console.log("Success!"));
+  }
+
+  async function closeConnection(){
+    mongoose.connection.close();
+  }

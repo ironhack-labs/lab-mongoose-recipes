4f06fcd26221d4b1a913afb91a31ff18fbc3706a
Sara Amantegui <saiduna@gmail.com>
2020-10-21 20:02:08 +0200

    done

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -25,3 +27,54 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+// create recipe
+  (async () => {
+    try {
+      const recipe = await Recipe.create({
+        title: "spaghetti",
+        cuisine: "italian"
+    
+      });
+      console.log(`This recipe was save: ${recipe.title}`);
+    } catch (error) {
+      console.log(error.message);
+
+    }
+  })();
+//insertMany
+(async () => {
+  try {
+    const dataRecipes = await require(__dirname+"/data.json")
+    const recipes = await Recipe.insertMany(dataRecipes)
+    console.log
+    console.log("Recipes added");
+  } catch (error) {
+    console.log(error.message);
+
+  }
+})();
+
+//update recipe
+(async () => {
+  try {
+    const recipie = await Recipe.findOneAndUpdate({title:"rigatoni alla genovese"}, {duration: 100})
+    console.log("Rigatoni alla Genovese was updated");
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
+
+//remove one
+
+(async () => {
+  try {
+    const recipie = await Recipe.deleteOne({title:"Carrot Cake"})
+    console.log("Carrot Cake was removed");
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
+
+//close the database
+
+

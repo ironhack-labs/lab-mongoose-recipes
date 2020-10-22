c74bb31f7fc93bcb7bbc358b2eb1674ac7e3387c
Oscar M <empanaisimo@gmail.com>
2020-10-22 02:12:35 +0100

    Completed lab

--- index.js
+++ index.js
@@ -15,26 +15,74 @@ mongoose
     useUnifiedTopology: true
   })
   .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
+    console.log(`# Connected to the database: '${self.connection.name}'`);
     return self.connection.dropDatabase();
   })
-  .then(() => {
+.then(()=>{
+    // Iteration 2
+    (async()=> {
     try {
-      // /* Iterarion 2 */
-      // const recipe = Recipe.create(data[0]);
-      // console.log(`This recipe was saved ${recipe}`);
-
-      // /* Iteration 3 */
-      // Recipe.insertMany(data, function(error, docs) {});
-      // console.log(`This recipes were saved ${data}`);
-
-
-
+        const newRecipe = {
+        "title": "Omelette",
+        "level": "Amateur Chef",
+        "ingredients": [
+            "4 Eggs",
+            "Oil",
+            "Salt",
+            "1 Kg. Potatos",
+        ],
+        "cuisine": "Spanish",
+        "dishType": "main_course",
+        "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+        "duration": 35,
+        "creator": "Chef JohnDoe"
+        };
+        const recipe = await Recipe.create(newRecipe);
+        console.log(`# '${recipe.title}' has been created and added to the database`);
+        // Iteration 3
+        (async()=>{
+            try {
+            const recipes = data;
+            const insert = await Recipe.insertMany(recipes);
+            console.log(`# ${insert.length} new recipes have been added to the database.`);
+            // Iteration 4
+            ( async ()=> {
+                try {
+                const query = { title: "Rigatoni alla Genovese" };
+                const update = await Recipe.findOneAndUpdate(query,{ $set: { duration: 100 }}, {new: true});
+                console.log(`# ${query.title}: duaration changed to `, update.duration);
+                // Itereation 5
+                (async()=>{
+                    try {
+                    const query = {title: "Carrot Cake"};
+                    const del = await Recipe.deleteOne(query);
+                    console.log(`# Successfully deleted: `, query.title);
+                    // Iteration 6
+                    (async()=>{
+                        try{
+                            mongoose.connection.close();
+                            console.log('# Connection closed');
+                        } catch (error){
+                            console.log('# Error disconnecting from database.')
+                        }
+                    })();
+                    } catch (error) {
+                    console.log(error);
+                    }
+                })();
+                } catch (error) {
+                console.log(error);
+                }
+            })();
+            } catch (error) {
+            console.log(error);
+            }
+        })();
     } catch (error) {
-      console.log(error.message);
+        console.log(error);
     }
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    })();
+})
+.catch(error => {
+console.error('Error connecting to the database', error);
+});

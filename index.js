dde10c7fa3aa26fa614fa84d744b7404817fc714
Oscar M <empanaisimo@gmail.com>
2020-10-22 07:58:41 +0100

    Removed deprecation warning in console output

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`# Connected to the database: '${self.connection.name}'`);

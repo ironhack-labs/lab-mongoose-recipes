437665e280e621be07723982af38393ac795e20b
Ute Wiemer <upwiemer@gmail.com>
2020-10-24 16:57:37 +0200

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -8,6 +9,32 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+// mongoose
+//   .connect(MONGODB_URI, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(self => {
+//     console.log(`Connected to the database: "${self.connection.name}"`);
+
+//     Recipe.deleteMany().then(() => {
+//     // Before adding any documents to the database, let's delete all previous entries
+//     return self.connection.dropDatabase();
+//   })
+//   .then(() => {
+//     Recipe.create({title: 'Porridge', dishType: 'breakfast', duration: 5, cuisine: 'American'})
+//    .then((rec) => {
+//     console.log(rec)
+//    })
+//     // Run your code here, after you have insured that the connection was made
+//   })
+
+//   .catch(error => {
+//     console.error('Error connecting to the database', error);
+//   })})
+
+
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
@@ -16,12 +43,29 @@ mongoose
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+
+    Recipe.deleteMany().then(() => {
+      return self.connection.dropDatabase();
+    })
+
+      .then(() => {
+        Recipe.insertMany(data)
+          .then(() => {
+            Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+              .then((theRecipe) => { console.log(theRecipe) })
+          })
+          .then(() => {
+            Recipe.deleteOne({ title: "Carrot Cake" })
+              .then(() => { mongoose.disconnect() })
+          })
+
+          .then((rec) => {
+            console.log(rec)
+          })
+      })
+      .catch(error => {
+        console.error('Error connecting to the database', error);
+      })
+
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+

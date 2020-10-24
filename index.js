d62493bf7f8a30c46733ec8efc36041220dbcc95
phil-legal-tech <philtu1287@gmail.com>
2020-10-24 19:45:45 +0200

    Completed lab

--- index.js
+++ index.js
@@ -21,7 +21,41 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const recipe = {
+      title: 'ApplePie',
+      level: 'Easy Peasy',
+      ingredients: ['Apple', 'Pie'],
+      cuisine: 'American',
+      dishType: 'dessert',
+      image: 'https://static.backen-mit-spass.de/images/content/369/01.jpg',
+      duration: 40,
+      creator: 'Chef',
+      // created:      { type: Date, default: Date.now },
+    };
+
+    Recipe.create(recipe).then(() => {
+      // console.log(recipe.title)
+    });
+    Recipe.insertMany(data).then((arrayOfCreatedRecipe) => {
+      arrayOfCreatedRecipe.forEach(el => {
+        console.log(el.title)
+      });
+
+      Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true }).then((e) => {
+        // console.log('sucess', e)
+      })
+      Recipe.findOneAndRemove({ title: 'Carrot Cake' }).then((e) => {
+        console.log('removed', e)
+      }).catch(err => {
+        console.log(err)
+        mongoose.disconnect()
+      });
+
+    });
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  

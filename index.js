3b36ef2492c3a1f31574a7a0c7c2c9a1a9134874
Alicja Slupska <ala.slupska@gmail.com>
2020-10-24 17:01:23 +0200

    LAB done

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const firstRecipe = {
+  title: 'Pasta Carbonara',
+  level: 'Amateur Chef',
+  ingredients: ['Eggs', 'Cream', 'Pasta'],
+  cuisine: 'Italian',
+  dishType: 'main_course',
+  image: 'https://images.media-allrecipes.com/images/75131.jpg',
+  duration: 30,
+  creator: 'Alicja'
+}
+
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -20,8 +32,26 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Recipe.create(firstRecipe);
+    // console.log(firstRecipe.title);
+    return Recipe.insertMany(data);
+
+    /*for (let i = 0; i < data.length; i++) {
+      console.log(data[i].title)
+    }
+    */
+
+  }).then(() => {
+
+    return Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  }).then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
   })
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  }).then(() => {
+    mongoose.disconnect()
+    console.log('success')
+  })

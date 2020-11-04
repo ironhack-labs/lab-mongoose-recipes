c9a2a659ddaf9a0ace5e7ee2a55d261195b84b9a
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-04 16:07:16 -0600

    added schema

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,25 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  // title - Type String.It should be required and unique.
+  title: String,
+  // level - Type String.Can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef(remember the enum validator).
+  level: String,
+  // ingredients - Type Array of Strings(represented as[String]).
+  ingredients: [],
+  // cuisine - Type String.Should be required.
+  cuisine: String,
+  // dishType - Type String.Possible values: breakfast,  main_course,  soup,  snack,  drink,  dessert or other.
+  dishType: String,
+  // image - Type String.Default value: "https://images.media-allrecipes.com/images/75131.jpg".
+  image: String,
+  // duration - Type Number.The minimum value should be 0.
+  duration: Number,
+  // creator - Type String.
+  creator: String,
+  // created - Type Date.By  default,  today.
+  created: Date
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

88af426eb6f859a864e24f206711e4a2813bd5bc
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-11-04 18:51:57 -0600

    lab done

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,7 +22,55 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const recipe1 = {
+      title: "Asian Glazed Chicken Thighs NO HONEY",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      duration: "35",
+      creator: "Chef LePapu"
+    }
+
+    //Iteration 2 - Create a recipe
+    // Recipe.create(recipe1).then(() => console.log(`${recipe1.title} added to db!`))
+
+    Recipe.insertMany(data).then((recipesArray => {
+      recipesArray.forEach(recipe => {
+        console.log(`The following recpie was inserted: ${recipe.title}`)
+      })
+      Recipe.findOneAndUpdate({
+        title: "Asian Glazed Chicken Thighs"
+      }, {
+        level: "Easy Peasy"
+      }, {
+        new: true
+      }).then((e) => {
+        console.log("update :D " + e.title)
+      })
+
+      Recipe.findOneAndDelete({
+        title: "Orange and Milk-Braised Pork Carnitas"
+      }).then((e) => {
+        console.log("deletion:D " + e.title)
+      })
+
+      Recipe.find().then((e) => console.log("SELECT * FROM ... AFTER the deletion " + e)).then(() => {
+        console.log("disconnecting from db..")
+        mongoose.disconnect()
+      })
+
+    }))
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })

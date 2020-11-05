eb5bf7f3cec90ca0e06744c503a1943612701a43
Tomi Scattiniclear <tomasscattini@gmail.com>
2020-11-04 19:52:22 -0500

    done

--- index.js
+++ index.js
@@ -9,19 +9,36 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(self => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+        // Before adding any documents to the database, let's delete all previous entries
+        return self.connection.dropDatabase();
+    })
+    .then(() => {
+        Recipe.create({
+                title: 'Guiso de lentejas vegetariano',
+                level: 'Easy Peasy',
+                ingredients: ['Papas', 'Zapallo', 'Zanahoria', 'Cebolla', 'Ajo', 'Morron', 'Salsa de tomate', 'Lentejas', 'Agua', 'Sal', 'Hojas de laurel', 'Pimenton dulce'],
+                cuisine: 'Guisuli',
+                dishType: 'main_course',
+                image: `https://guisosyguisados.com/wp-content/uploads/2016/08/2.-guiso-de-lentejas-tradicional-receta-argentina-300x225.jpg`,
+                duration: 60,
+                creator: 'El senor de los guisos'
+            })
+            .then((recipe) => {
+                console.log(`Receta creada: ${recipe.title}`)
+            })
+        Recipe.insertMany(data).then(recipe => recipe.forEach((el) => console.log(`Receta creada: ${el.title}`)))
+        Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true }).then(() => console.log(`Updated`))
+        Recipe.deleteOne({ title: "Carrot Cake" }).then(() => console.log(`Succesfully removed Carrot Cake`))
+
+        // Run your code here, after you have insured that the connection was made
+    })
+    .catch(error => {
+        console.error('Error connecting to the database', error);
+    })

49514491efd26dda774344176d6d560937d63900
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2020-11-05 13:01:33 +0100

    Completed iteration 1 and 2

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
@@ -22,6 +22,24 @@ mongoose
   .then(() => {
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// const scrambledEggs = {
+//   title: "Scrambled Eggs",
+//   level: "Easy Peasy",
+//   ingredients: ["Eggs", "Milk", "Salt", "Pepper", "Corriander"],
+//   cuisine: "English",
+//   dishType: "breakfast",
+//   duration: 15,
+//   creator: "Chef Roy-Arne Brussel",
+// };
+
+// Recipe.create(scrambledEggs)
+//   .then((recipe) =>
+//     console.log("The recipe has been saved and its value is: ", recipe)
+//   )
+//   .catch((error) =>
+//     console.log("An error happened while saving a new recipe: ", error)
+//   );

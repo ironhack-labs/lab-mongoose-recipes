472ab26c08a9339e411aec0899b680e32d576073
Gabriela Polanco <gabrielapolancoferreyra@gmail.com>
2020-11-04 19:15:29 -0500

    Completed lab

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
+
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
@@ -19,9 +21,45 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+  // ITERACION 2
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+   Recipe.create({
+      title: "empanadas",
+      level: "Amateur Chef",
+      ingredients: [
+        "1kg de Carne picada", 
+        "1kg Cebolla",
+         "6 Huevos", 
+         "Tapas de empanadas",
+         "3 cucharadas de comino"
+      ],
+      cuisine: "Argentinian",
+      dishType: "main_course",
+      duration: 120,
+      creator: "Unkown",
+    })
+    .then(() => {console.log(`New recipe`);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  recipe.insertMany(data)
+.then(recipe =>{ 
+  recipe.forEach(recipe =>{
+    console.log(recipe.title)
+  })
+  .then(() => {
+    recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+    })
+    .then(() => {
+      console.log("Succesfully updated")
+    })
+    .then(() => {
+      recipe.deleteOne({title: "Carrot Cake"})
+        })
+        .then(() => {
+          console.log("Successfully removed")
+        })
+        .then(() => {
+          mongoose.disconnect();
+        })
+    .catch(error => {
+      console.error('Error connecting to the database', error)
+    })

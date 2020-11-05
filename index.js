bb7e8e6736a0fb4570dc23d2a5869944a00f3d82
ElfGod <albertogx@icloud.com>
2020-11-05 00:48:39 -0500

    lab mongoose recipes

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
+const App = async () => {
+  // Connection to the database "recipe-app"
+  let db;
+  try {
+    db = await mongoose.connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    db.connection.on("success", function (event) {
+      console.log(`Mongo did connect successfully ${event}`);
+    });
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    await db.connection.dropDatabase();
+  } catch (error) {
+    console.error("Error connecting to the database", error);
+  }
+  if (!db) return;
+  // Iteration 2 - Create a recipe
+  try {
+    const recipe = await Recipe.create({
+      title: "Hawaiian pizza",
+      level: "Amateur Chef",
+      ingredients: ["pineapple", "ham", "tomato sauce", "cheese"],
+      cuisine: "American",
+      dishType: "snack",
+      creator: "PizzaMan",
+      duration: 25,
+    });
+    console.log(
+      `Added a new recipe called: ${recipe.title}, from the ${recipe.cuisine} cuisine.`
+    );
+    const manyRecipes = await Recipe.insertMany(data);
+    console.log(`We added all new recipes ${manyRecipes}`);
+  } catch (error) {
+    console.log(error);
+  }
+  // this will update the first object with title
+  const updatedRecipe = await Recipe.findOneAndUpdate(
+    { title: "Rigatoni alla Genovese" }, // Dónde cambiamos algo
+    {
+      duration: 100, // Qué cambiamos (de 220 a 100)
+    },
+    { new: true } // Obligatorio para que imprima el nuevo objeto
+  );
+
+  const deleteOne = await Recipe.deleteOne({ title: "Carrot Cake" });
+
+  db.connection.close(() => {
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
   });
+};
+
+App();

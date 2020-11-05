620465a48f90807cfbcecae33a92100159ec9a0f
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2020-11-05 18:24:57 +0300

    iterations done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -16,23 +16,51 @@ mongoose
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    //return self.connection.dropDatabase();
+    return self.connection.dropDatabase();
   })
+
+  //ITERATION 2- Create a recipe
   .then(() => {
-    //ITERATION 2
-  Recipe.create({
-  title: "Manti",
-  level: "UltraPro Chef",
-  ingredients: ["flour", "salt", "egg", "ground beef"],
-  cuisine: "Turkish",
-  dishType: "main_course",
-  image:"https://i0.wp.com/turkishfoodchef.com/wp-content/uploads/2017/02/Turkish-Ravioli-Manti-Recipe.jpg",
-  duration: 30,
-  creator:"cu",
-  created: Date.now()
+    return Recipe.create({
+      title: "Manti",
+      level: "UltraPro Chef",
+      ingredients: ["flour", "salt", "egg", "ground beef"],
+      cuisine: "Turkish",
+      dishType: "main_course",
+      image: "https://i0.wp.com/turkishfoodchef.com/wp-content/uploads/2017/02/Turkish-Ravioli-Manti-Recipe.jpg",
+      duration: 30,
+      creator: "Ceyda",
+    })
   })
+
+  //ITERATION 3- Insert multiple recipes
+  .then(recipe => {
+    return Recipe.insertMany(data)
+      .then((results) => console.log(`Saved new recipe: ${results}`))
+      .catch((saveErr) => console.error(`Save failed: ${saveErr}`))
+    console.log(recipe.title)
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  //ITERATION 4- Update recipe
+  .then(recipe => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+      .then(() => console.log(`Duration is updated`))
+      .catch((saveErr) => console.error(`Save failed: ${saveErr}`));
+  })
+
+//ITERATION 5- Remove a recipe
+  .then(recipe => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+    .then(() => console.log(`Carrot Cake is deleted`))
+    .catch((saveErr) => console.error(`Save failed: ${saveErr}`))
+  })
+
+//ITERATION 6- Close the Database
+  .then(recipe => {
+  return mongoose.connection.close()
+      .then(() => console.log(`Database is closed!`))
+      .catch(() => console.error('Delete Failed'))
+    })
+    .catch (error => {
+      console.error('Error connecting to the database', error)
+    })
+  

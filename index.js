9fd19fe94cb0a8adb9a5a8f5636e96ba15c5d320
Francisco Javier Vivas Astudillo <francisco.vivas95@gmail.com>
2020-11-04 19:08:07 -0500

    Solved lab

--- index.js
+++ index.js
@@ -46,8 +46,4 @@ mongoose
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  })
-  // .finally(() => {
-
-  // });
-
+  });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -32,15 +32,6 @@ const recipeSchema = new Schema({
     type: Date,
     default: new Date(),
   }
-  /*title - Type String.It should be required and unique.
-    level - Type String.Can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef(remember the enum validator wink).
-    ingredients - Type Array of Strings(represented as [String]).
-    cuisine - Type String.Should be required.
-    dishType - Type String.Possible values: breakfast, main_course, soup, snack, drink, dessert or other.
-    image - Type String.Default value: "https://images.media-allrecipes.com/images/75131.jpg".
-    duration - Type Number.The minimum value should be 0.
-    creaor - Type String.
-    created - Type Date.By default, today*/
 });
 
 module.exports = model('Recipe', recipeSchema);

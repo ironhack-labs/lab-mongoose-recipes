e3bac732ded0e37bd9e50cc3acfb3a42ca53ae98
Héctor Carramiñana <hector.crm.hcca@gmail.com>
2020-11-11 19:41:16 +0100

    done con código cambiado

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/recipe.model.js'
@@ -21,46 +23,26 @@ mongoose
 
     Recipe.create({
 
-        title: 'Bacon Burger',
-        level: 'Easy Peasy',
-        ingredients: ['bacon', 'cheese', 'meat burger'],
-        cuisine: 'american',
-        dishType: 'other',
-        image: 'https://images.media-allrecipes.com/images/75131.jpg',
-        duration: 15,
-        creator: 'Phector27'
+      title: 'Bacon Burger',
+      level: 'Easy Peasy',
+      ingredients: ['bacon', 'cheese', 'meat burger'],
+      cuisine: 'american',
+      dishType: 'other',
+      image: 'https://images.media-allrecipes.com/images/75131.jpg',
+      duration: 15,
+      creator: 'Phector27'
 
-      })
+    })
 
-      .then(newRecipe => {
+      .then(newRecipe => console.log('El método .create() retorna el objeto de la BBDD:', newRecipe, 'La nueva receta se llama', newRecipe.title))
+      .then(() => Recipe.insertMany(data))
+      .then(newRecipes => console.log('El método .insertMany() inserta el documento en la BBDD:', newRecipes, 'Las nuevas recetas introducidas se llaman:', newRecipes.title))
+      .then(() => Recipe.updateOne({ duration: 220 }, { duration: 100 }, { new: true }))// EN MONGODB COMPASS SE ACTUALIZA EL TIEMPO DE LA RECETA Y EN LA CONSOLA NO.
+      .then(updateDuration => console.log('El método .findOneAndUpdate() actualiza el dato indicado en la BBDD:', updateDuration, 'Los nuevos minutos son:', 100))
+      .then(() => Recipe.deleteOne({ title: 'Carrot Cake' }))
+      .then(deleteRecipe => console.log('El método .deleteOne () elimina el dato indicado en la BBDD', deleteRecipe))
+      .then(() => mongoose.connection.close());
 
-        console.log('El método .create() retorna el objeto de la BBDD:', newRecipe, 'La nueva receta se llama', newRecipe.title);
-        return Recipe.insertMany(data);
-
-      })
-    
-
-      .then(newRecipes => {
-
-        console.log('El método .insertMany() inserta el documento en la BBDD:', newRecipes, 'Las nuevas recetas introducidas se llaman:', newRecipes.title);
-        return Recipe.updateOne({ duration: 220 }, { duration: 100 }, { new: true }); // EN MONGODB COMPASS SE ACTUALIZA EL TIEMPO DE LA RECETA Y EN LA CONSOLA NO. 
-        
-      })
-    
-      .then(updateDuration => {
-
-        console.log('El método .findOneAndUpdate() actualiza el dato indicado en la BBDD:', updateDuration, 'Los nuevos minutos son:', 100);
-        return Recipe.deleteOne({ title: 'Carrot Cake' });
-
-      })
-    
-      .then(deleteRecipe => {
-        
-        console.log('El método .deleteOne () elimina el dato indicado en la BBDD', deleteRecipe);
-        return mongoose.connection.close();
-
-      });
-      
     })
       
   .catch(error => console.error('Error connecting to the database', error));

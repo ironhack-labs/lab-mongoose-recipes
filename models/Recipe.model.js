4d59ab05cb2c80253b71be8d4fac71d60b1aaa53
manuelbarreda <manubarreda69@gmail.com>
2020-11-11 16:16:47 +0100

    Iteration 1 done, created Recipe Schema

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,54 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const listOfLevels = ["Easy Peasy", "Amateur Chef", "UltraPro Chef"];
+const dishTypes = ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  // ToDo: write the schema
+
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    default: "Without name"
+  },
+  level: {
+    type: String,
+    enum: listOfLevels
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  dishType: {
+    type: String,
+    enum: dishTypes
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0,
+    max: 525600 // Si vas a tardar más de un año en hacer una receta, apuntate a un curso no te leas una receta cutre ;)
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now() // No estoy seguro de esto, ¿Puedo meter el metodo now() aquí?, como he puesto { timestamps: true } ¿No necesitaría created verdad?
+  }
+
+}, { timestamps: true }
+);
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 

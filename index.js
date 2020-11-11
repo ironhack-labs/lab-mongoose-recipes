5c62ad25bffd5a0500e8168f1e559a4fdac007d3
Jordi Boronat <jorx.bro@gmail.com>
2020-11-11 19:47:48 +0100

    DONE

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -12,7 +13,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,8 +22,45 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe
+      .create([{ title: "Fabada Asturiana", cuisine: "Asturian", duration: 120, }])
+      .then(newRecipe => {
+        console.log(newRecipe[0].title)
+        return Recipe.create(data)
+      })
+      .then(newRecipes =>
+        newRecipes.forEach(elm => { console.log(elm.title) })
+      )
+      .then(() => {
+
+        return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+          .then(details => console.log("Has actualizado los siguientes datos:", details))
+          .catch(err => console.log('Hubo un error', err))
+
+      })
+      .then(() => {
+        return Recipe
+          .deleteOne({ title: "Carrot Cake" })
+          .then(response => console.log("Has eliminado una receta:", response))
+          .catch(err => console.log('Hubo un error', err))
+
+          .catch(err => console.log('Error creando el registro:', err))
+      })
+
+
   })
+
+  .then(() => {
+    return mongoose.connection.close()
+      .then(() => console.log(`Connection closed!`))
+      .catch(() => console.error('delete Failed'));
+
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+

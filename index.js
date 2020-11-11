6ebd58b5a5d5f7f0cb140793cb6be0a48ab1ffd6
Sergio <srosduran@gmail.com>
2020-11-11 19:19:34 +0100

    done

--- index.js
+++ index.js
@@ -17,11 +17,63 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    // return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  //   Recipe.create(
+  //     {
+  //       title: 'Il bocadillo',
+  //       level: 'Easy Peasy',
+  //       ingredients: ['bread', 'tomato', 'jam', 'cheese'],
+  //       cuisine: 'Spanish',
+  //       dishType: 'snack',
+  //       image: "https://www.enriquetomas.com/es/blog/wp-content/uploads/2018/02/hacer-el-mejor-bocadillo-de-jamon-e1519833440102-1280x720.jpg",
+  //       duration: 3,
+  //       creator: 'Steve Jobs',
+  //       created: '1986-05-04'
+  //     }
+  //   )
+  // .then((result)=>{
+  //     console.log(result);
+  // })
+  // .catch((err)=>{
+  //     console.log(err);
+  // })
+
+  // Recipe.insertMany(data)
+  //       .then((result)=>{
+  //         console.log(result);
+  //         })
+  //       .catch((err)=>{
+  //         console.log(err);
+  //       });
+
+  // Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+  // .then((result)=>{
+  //     console.log(result);
+  // })
+  // .catch((err)=>{
+  //     console.log(err);
+  // });
+
+    // Recipe.deleteOne({title: "Carrot Cake"})
+    //       .then((result)=>{
+    //         console.log(result);
+    //       })
+    //       .catch((err)=>{
+    //         console.log(err);
+    //       });
+
+    
   })
-  .catch(error => {
+  // .then().connection.close(()=>{
+  //   console.log('Mongoose default connection disconnected through app termination');
+  //   // process.exit(0);
+  // })
+  .catch((error) => {
     console.error('Error connecting to the database', error);
+  });
+
+  mongoose.disconnect(()=>{
+    console.log('Disconnected!');
   });

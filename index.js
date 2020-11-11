4774aea87f21122db6e129439156e18e305b7e63
Roma Dorado <doradoroma@gmail.com>
2020-11-11 19:47:38 +0100

    [WEB-MAD-DE]Jose Dorado

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { db } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
@@ -20,8 +22,41 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'ajonesa',
+      level: 'Easy Peasy',
+      ingredients: ['eggs', 'oil', 'garlic', 'lemon juice'],
+      cuisine: 'Spanish',
+      dishType: 'Sauce',
+      image: '',
+      duration: 5,
+      creator: 'Roma'
+    })
+  .then(recipe => {
+    console.log('La nueva receta se llama', recipe.title)
+      })
+  .then(() => {
+    return Recipe.insertMany(data)
+      .then(() => {
+        data.forEach((res) => {
+          console.log('El titulo de la receta es:', res.title)
+        })
+      })
+    })
+  .then(() => {
+    return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+      })
+      .then(() => {
+        return Recipe.deleteOne({ title: 'Carrot Cake' })
+      })
   })
+
+  .then(() => { mongoose.connection.close()
+  .then(() => console.log(`¡TRESCIENTOS INTENTOS DESPUES HE CONSEGUIDO CERRARLO!`))
+  .catch(() => console.error('delete Failed'));
+  })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

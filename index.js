0d5ef8ecbe8fbc16298c6487b20461754ecb7046
Mark <mark.sanjuan.sole@gmail.com>
2020-11-11 17:55:51 +0100

    done

--- index.js
+++ index.js
@@ -7,6 +7,54 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+// Create ONE RECIPE
+
+const createOneRecipe = () => {
+    Recipe.create(data[0])
+    .then((data)=>{
+      console.log(`Recipe created: ${data.title}`)
+      mongoose.disconnect()
+      .then(data => console.log('mongoose disconnected'))
+      .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err))
+}
+
+const createManyRecipes = () => {
+  Recipe.insertMany(data)
+  .then((recipes) => {
+    recipes.forEach(recipe => {
+      console.log(`Recipe created: ${recipe.title}`)
+    })
+    mongoose.disconnect()
+    .then(data => console.log('mongoose disconnected'))
+    .catch(err => console.error(err))
+  })
+  .catch(err => console.error(err))
+}
+
+const updateRecipe = (title) => {
+    Recipe.updateOne({ title: title }, { duration: 100 })
+    .then((data) => {
+      console.log(data)
+    mongoose.disconnect()
+      .then(data => console.log('mongoose disconnected'))
+      .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err))
+}
+
+const deleteRecipe = (title) => {
+      Recipe.deleteOne({ title: title })
+    .then(data => {
+      console.log((data))
+      mongoose.disconnect()
+      .then(data => console.log('mongoose disconnected'))
+      .catch(err => console.error(err))
+    }) 
+    .catch(err => console.error(err))
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -17,11 +65,27 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    // return self.connection.dropDatabase();
   })
   .then(() => {
+    // createOneRecipe()
+
+    createManyRecipes()
+
+    // updateRecipe('Rigatoni alla Genovese')
+
+    // deleteRecipe('Carrot Cake')
+
+
+
+
+
+
+
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+

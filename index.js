7a15c94a8b21dde2695e45a78e784d0f4c3a8442
Jorge Martin <jorgemaram@gmail.com>
2020-11-11 18:50:55 +0100

    done

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,8 +21,47 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+      Recipe.create({
+      title: 'Cheescake',
+      level: 'Amateur Chef',
+      ingredients: ['eggs', 'flour', 'cheese'],
+      cuisine: 'French cuisine',
+      dishType: 'dessert',
+      image: 'https://i2.wp.com/www.sugarspunrun.com/wp-content/uploads/2019/01/Best-Cheesecake-Recipe-2-1-of-1-4.jpg',
+      duration: 120,
+      creator: 'Jorge Martín'
+    })
+      .then(thisRecipe => {
+        console.log(`El título de la receta es ${thisRecipe.title}`)
+      })
+    
+  })
+  .then(() => {
+    return Recipe
+      .insertMany(data)
+      .then(data => {
+        data.forEach (elm =>
+        console.log(`El título de la receta es ${elm.title}`))
+      })
+  })
+  .then(() => {
+    return Recipe
+      .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+      .then(thisRecipe => {console.log(`Has actualizado la duración de la receta, ahora es de: ${thisRecipe.duration}`)})
+  })
+  .then(() => {
+    return Recipe
+      .deleteOne({ title: "Carrot Cake" })
+      .then(info => console.log('La información de la eliminaciónes:', info))
+  })
+
+  .then(() => {
+    return mongoose.connection.close(() => {
+      console.log('Se ha cerrado la base de datos.')
+    })
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

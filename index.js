d4dbbfde83f49b88e932524ba3eca54695c518f9
Carlos Dávila <casedaro15@gmail.com>
2020-11-11 21:38:13 +0100

    completed lab

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { updateMany, db } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -12,7 +13,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false        //  findOneAndUpdate ESTA DEPRECATED TENIA QUE METER ESTA OPTION EN EL .connect
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,8 +22,20 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log('database vacia')
+    return Recipe
+      .create({ title: 'leon come gamba', level: 'UltraPro Chef', ingredients: ['salt', 'pepper', 'lemon'], cuisine: 'rare', dishType: 'other', image: 'https://www.lavanguardia.com/r/GODO/LV/p3/WebSite/2015/12/10/Recortada/img_mgonzaleza_20151210-111441_imagenes_lv_terceros_1_515de56147-kimE--572x270@LaVanguardia-Web.jpg', duration: 1, creator: 'Crazy guy' })
   })
+  .then(theRecipe => console.log(theRecipe.title))
+  .then(() => Recipe.insertMany(data))
+  .then(theData => theData.forEach(elm => console.log(elm.title)))
+  .then(() => Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true }))
+  .then(updatedRecipe => console.log(`Update done! ${updatedRecipe.title} duration is now : ${updatedRecipe.duration}.`))
+  .then(() => Recipe.deleteOne({ title: 'Carrot Cake' }))
+  .then(deletedRecipe => console.log('Element deleted!', deletedRecipe))
+  .then(() => mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+

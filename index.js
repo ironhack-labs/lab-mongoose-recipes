bcc2a15a492230922f2710d52ae00c4a7880753f
manuelbarreda <manubarreda69@gmail.com>
2020-11-11 17:27:43 +0100

    Iteration 3, 4 and 5 Done

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
+  // ITERATION 2
   .then(() => {
     // Run your code here, after you have insured that the connection was made
     return Recipe.create({
@@ -31,9 +33,37 @@ mongoose
       duration: 45,
       creator: "François Pierre de La Varenne",
     })
-    .then (res => {console.log("El título es:", res.title)})
+      .then(res => { console.log("The title is: ", res.title) })
+      .catch((err) => { console.error("Some error occurred with Iteration 2: ", err) })
   })
-  
+
+  // ITERATION 3
+  .then(() => {
+    return Recipe.insertMany(data)
+      .then(() => console.log("You saved some new recipes"))
+      .catch((err) => { console.error("Some error occurred with Iteration 3: ", err) })
+      .then(() => {
+        data.forEach((res) => {
+          console.log("The title of the recipe is: ", res.title)
+        })
+      })
+      .catch((err) => { console.error("Some error occurred with Iteration 3: ", err) })
+  })
+
+  // ITERATION 4
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+      .then((res) => console.log("You updated: ", res.title))
+      .catch((err) => { console.error("Some error occurred with Iteration 4: ", err) })
+  })
+
+  // ITERATION 5
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+      .then((res) => console.log("You deleted: ", res.title)) // Return undefined, ¿Como puedo hacer que me retorne el título?
+      .catch((err) => { console.error("Some error occurred with Iteration 6: ", err) })
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

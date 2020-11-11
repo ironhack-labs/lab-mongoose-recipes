751b1a5f35992f37e9a47715ed941b3c921a6f5b
Teresa Torrente <mttorrente.o@gmail.com>
2020-11-11 20:30:33 +0100

    Completed Lab

--- index.js
+++ index.js
@@ -19,9 +19,38 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create(
+      { title: 'Spanish omelette', level: 'Easy Peasy', ingredients: ['eggs', 'potatoes', 'onion', 'salt', 'olive oil'], cuisine: 'Spanish', dishType: 'main_course', image: "https://cdn1.cocina-familiar.com/recetas/thumb/tortilla-de-patata-con-cebolla.jpg", duration: 30, creator: 'Señor Tortilla', created: Date('2020-18-10') }
+    )
+      .then(theNewRecipe => console.log('La nueva receta es:', theNewRecipe.title))
+      .catch(err => console.log('Error creando el registro:', err))
+
+    
   })
+  .then(() => {
+    Recipe.insertMany(data)
+      .then(Recipes => Recipes.forEach(e => console.log('Las nuevas recetas son:', e.title)))
+      .catch(err => console.log('Error nuevas recetas:', err))
+  })
+
+
+  Recipe
+    .findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true })
+    .then(data => console.log("Bien!! El nuevo dato es:", data))
+    .catch(err => console.log('Error cambiando dato', err))
+
+    
+  Recipe
+    .deleteOne({ title: 'Carrot Cake' })
+    .then(recipe => console.log('Ya no existe la receta', recipe))
+    .catch(err => console.log('Error al borrar receta', err))
+  
+    
+  mongoose.connection.close(console.log('Bye, Bye!!'))
+    
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

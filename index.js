8558707f530efc08385c277b15f8e3c93e22aaad
Loreto AVoces <loretoavoces@gmail.com>
2020-11-11 20:24:52 +0100

    DONE

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require('./models/Recipe.model')
+
+
+
 // Import of the data from './data.json'
 const data = require('./data');
+const { insertMany } = require('./models/Recipe.model');
+
+
+
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -14,14 +21,52 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+
+
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'Sushi',
+      level: 'UltraPro Chef',
+      ingredients: ['rice', 'salmon', 'etc'],
+      cuisine: 'japanesse',
+      dishType: 'main_course',
+      image: '',
+      duration: 40,
+      creator: 'Loreto',
+      created: ''
+    })
+      
+    .then(response => console.log('El titulo es:', response.title))
   })
+
+    .then(() => {
+      return Recipe.insertMany(data)
+      .then(recipies => recipies.forEach(elm => console.log('Los titulos son:', elm.title)))
+    })
+
+  
+    .then(() => {
+      return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, {duration: 100}, {new: true})
+        .then(response => console.log('En verdad tardan:', response.duration))
+    })
+
+    .then(() => {
+
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
+        .then(res => console.log('Oh vaya, no nos queda:', res))
+      
+    })
+ 
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  // mongoose.connection.close(console.log('Adeuuus'))
+

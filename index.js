85fa35b9c606cc440520009cdf7146f766183557
Alicia Rojo <aliciarojovera@hotmail.com>
2020-11-11 18:23:55 +0100

    WEV DEV MAD

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -16,12 +16,46 @@ mongoose
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    Recipe.create({
+      title: "Carbonara",
+      level: "Peasy",
+      ingredients: ["pasta", "eggs", "bacon", "parmesano"],
+      cuisine: "ITA",
+      dishType: "main_course",
+      image: "https://www.pequerecetas.com/wp-content/uploads/2018/01/pasta-carbonara-receta.jpg",
+      duration: 30,
+      creator: "Alicia Rojo"
+
+      })
+      .then(theRecipe=>{
+        console.log(theRecipe.title)  
+        Recipe
+          .create(data)
+              .then(newRecipes => {
+              newRecipes.forEach(recipe => console.log(recipe.title))
+              Recipe
+                .findOneAndUpdate({ duration: 220 }, { duration: 100 },{new:true})
+                .then(changes => {
+                  console.log("changes:", changes)
+                  Recipe
+                    .deleteOne({ title: "Carrot Cake" })
+                    .then(deleted => {
+                      console.log(deleted)
+                      mongoose.connection.close()
+                    })
+                   .catch(err => console.log('Hubo un error', err))
+                  })
+                .catch(err => console.log('Hubo un error', err))
+
+               })
+              .catch(err => console.log('Hubo un error', err))
+          })
+        .catch(err => console.log('Hubo un error', err))
+    })
+  .catch (err => console.log('Hubo un error', err))
+  
+
+

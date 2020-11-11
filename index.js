10906a554b44b59c63d71ce9e22d6422adb397c5
Adrian Casas <adrian.casas.fernandez@gmail.com>
2020-11-11 18:02:49 +0000

    Done

--- index.js
+++ index.js
@@ -12,16 +12,62 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndUpdate: true
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    //return self.connection.dropDatabase();
   })
+  // .then(() => {
+  //   // Run your code here, after you have insured that the connection was made  IT2
+  //   return Recipe.create({
+  //       title: 'Papas con mojo',
+  //       level: 'Easy Peasy',
+  //       ingredients: ['potatoes', 'water', 'salt', 'garlic', 'pepper', 'cumin', 'vinegar', 'oliveoil'],
+  //       cuisine: 'Canaria',
+  //       dishType: 'snack',
+  //       image: 'https://mojopicon.com.es/img/papas-arrugadas-con-mojo-picon-241.jpg',
+  //       duration: 20,
+  //       creator: 'Adrian',
+  //   })
+  //   .then(recipe => {
+  //     console.log(recipe.title)
+  //   })
+  // })
+
+  //IT 3
+  // .then(() => {
+  //   return Recipe.insertMany(data)
+  //     .then((result) => {
+  //       data.forEach((recipe) => {
+  //         console.log(recipe.title)
+  //       })
+  //     })
+  // })
+
+  //IT4
+  // .then(() => {
+  //   return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+  //     .then(() => console.log('Rigatoni alla Genovese is updated'))
+  //     .catch((saveErr) => console.error(`Save failed: ${saveErr}`))
+  // })
+
+  //IT5
+  // .then(() => {
+  //   return Recipe.deleteOne({ title: "Carrot Cake" })
+  //       .then(() => console.log("Carrot Cake is deleted"))
+  //       .catch((saveErr) => console.error(`Save failed: ${saveErr}`))
+  // })
+
+  //IT6 
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return mongoose.connection.close(() => {
+        console.log("database is closed")
+    })
   })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

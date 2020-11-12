65406a70d6bedbf5da0030509ff47eee45b3775c
Monique de Haard <moniquedehaard@msn.com>
2020-11-12 18:17:25 +0100

    done

--- index.js
+++ index.js
@@ -17,11 +17,59 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    return self.connection.dropDatabase(); // delete all database entries
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    console.log('Database Cleared! 🧹');
+
+    // create new recipe
+    return Recipe.create({
+      "title": "Asian Glazed Chicken Thighs",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      "cuisine": "Asian",
+      "dishType": "main_course",
+      "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      "duration": 40,
+      "creator": "Chef LePapu"
+    })
+  })
+  .then((recipe) => {
+    console.log("Title of the recipe: ", recipe.title);
+    return Recipe.insertMany(data)
+  })
+  .then((recipes) =>{
+    console.log("it worked!")
+    recipes.forEach(recipe => console.log(recipe.title));
+    // Update duration to 100
+    return Recipe.findOneAndUpdate(
+      {title:"Rigatoni alla Genovese"},
+      {duration:100},
+      {new: true}
+    );
+  })
+  .then(updatedRecipe => {
+    console.log('Updated recipe: ', updatedRecipe)
+    // Delete carrot cake
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(deleteRecipe => {
+    console.log("Recipe is deleted", deleteRecipe)
+    mongoose.disconnect(); //asynch
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
+    mongoose.disconnect(); //asynch
   });
+
+ 
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,43 @@
+// Import packages and get function
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Make schema for recipes
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: [ String ],
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: new Date(2020, 11, 12),
+  }
 });
 
+// Make new collection
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;

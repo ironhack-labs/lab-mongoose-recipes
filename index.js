975b3a398b57b0610f30f9dbfa02d9ae53eb3d9c
ararita <munjizavesna@gmail.com>
2020-11-12 19:07:18 +0100

    Iteration 3 done

--- index.js
+++ index.js
@@ -19,37 +19,38 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+  // .then(() => {
+  // Run your code here, after you have insured that the connection was made
+  // const recipe = Recipe.create({
+  //   title: "Fish pie",
+  //   level: "Easy Peasy",
+  //   ingredients: [
+  //     "Fish filets",
+  //     "Potatoes",
+  //     "Milk",
+  //     "Salt",
+  //     "Pepper",
+  //     "Butter",
+  //     "Cheddar",
+  //   ],
+  //   cuisine: "International",
+  //   dishType: "main_course",
+  //   image:
+  //     "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSwES3x1LnAtNPusRrU4oIXzTD3B90YSvQZig&usqp=CAU",
+  //   duration: 90,
+  //   creator: "Ottolenghi",
+  // });
+  // })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    // const recipe = Recipe.create({
-    //   title: "Fish pie",
-    //   level: "Easy Peasy",
-    //   ingredients: [
-    //     "Fish filets",
-    //     "Potatoes",
-    //     "Milk",
-    //     "Salt",
-    //     "Pepper",
-    //     "Butter",
-    //     "Cheddar",
-    //   ],
-    //   cuisine: "International",
-    //   dishType: "main_course",
-    //   image:
-    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSwES3x1LnAtNPusRrU4oIXzTD3B90YSvQZig&usqp=CAU",
-    //   duration: 90,
-    //   creator: "Ottolenghi",
+    //importing an array of recipes form the data.json file:
+    console.log(data);
+    return Recipe.insertMany(data);
+  })
+  .then((data) => {
+    data.forEach((el) => {
+      console.log(el.title);
     });
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
-
-//   In index.js, after the connection to the database has been established, you should add a new recipe document to the database by calling the Model.create static, passing it the recipe details as an object. After inserting the recipe, you should console.log the title of the recipe.
-
-// You can use MongoDB Compass to double check that everything is working as intended.
-
-// To run your code, remember you should use:
-
-// $ node index.js
-// Tip: When you have successfully created a new recipe (you see it in the database using Compass tool), you might want to comment out this step. The reason for this is that next time when you run $ node index.js, it will try to create a new recipe with the same name and you will get an error in the terminal related to the duplicate keys - the title should be unique, and the dish with that title already exists in the database.

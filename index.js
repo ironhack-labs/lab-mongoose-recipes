f147a1007fd8632aa22aa7159775e1aa14e70a18
Ana Milena Martinez Caceres <67890212+MilenaMar@users.noreply.github.com>
2020-11-12 16:58:10 +0000

    completed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -12,16 +11,49 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify:false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+ .then(() => {
+   return Recipe.create({
+     title:"Caserole",
+     level: "Easy Peasy",
+     ingredients:["potatoes","meat","tomatoes"],
+     cuisine: "Colombian",
+     dishType:"main_course",
+     duration: 90,
+     creator: "Milena"
+   });
+ }).catch(error => {
+  console.error('Error connecting to the database', error);
+})
+ .then(recipe => {
+   console.log('This is our newly created recipe:', recipe);
+   return Recipe.insertMany(data)})
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .then(recipes=>{
+    console.log('This is our newly created recipes:',recipes)
+    return Recipe.findOneAndUpdate(
+      {title: "Rigatoni alla Genovese"},
+      { duration: 100 },
+      { new: true }
+    );
+  }).catch(error=>{console.error('Error connecting to the database', error);})
+  .then(updatedRecipe =>{
+    console.log("this was a Success",updatedRecipe)
+    return Recipe.findOneAndDelete({title:"Carrot Cake" });})
+    .then (deleteitem => {
+      console.log('This is the Item we delete',deleteitem)
+      mongoose.connection.close()
+    })
+  .catch(error=>{console.error('Error connecting to the database', error);});
+
+
+  

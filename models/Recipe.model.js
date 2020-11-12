e9bc3951b08dea8f26a09db3b2fe8962df76431c
Marc Gotzens <marcgotzens@hotmail.com>
2020-11-12 18:14:58 +0100

    Finished up to iteration 4. 5 and 6 pending

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: [String],
+  cuisine: String,
+  dishType: {
+    type: String,
+    enum: [
+      "Breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: "2020-11-12",
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

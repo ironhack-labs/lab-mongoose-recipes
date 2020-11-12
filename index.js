fa4b09c50935de3a6b7cf7d985e681943fc4f410
Mirko Siddi <mirko.siddi@gmail.com>
2020-11-12 19:51:38 +0100

    Lab delivered by Mirko & Juanmi

--- index.js
+++ index.js
@@ -1,27 +1,77 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
+  .then((result) => {
     // Run your code here, after you have insured that the connection was made
+    const pr = Recipe.create({
+      title: "Tiramisu",
+      level: "UltraPro Chef",
+      ingredients: [
+        "4 large egg yolks",
+        "½ cup/100 grams granulated sugar, divided",
+        "¾ cup heavy cream",
+        "1 cup/227 grams mascarpone (8 ounces)",
+        "1 ¾ cups good espresso or very strong coffee",
+        "2 tablespoons rum or cognac",
+        "2 tablespoons unsweetened cocoa powder",
+        "About 24 ladyfingers (from one 7-ounce/200-gram package)",
+      ],
+      cuisine: "Italian",
+      dishType: "dessert",
+      image:
+        "https://static01.nyt.com/images/2017/04/05/dining/05COOKING-TIRAMISU1/05COOKING-TIRAMISU1-articleLarge.jpg",
+      duration: 25,
+      creator: "Chef Mirko",
+    });
+    return pr;
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((created) => {
+    console.log("Recipe created");
+    const pr = Recipe.insertMany(data);
+    return pr;
+  })
+  .then((createdMany) => {
+    console.log("Recipes created:");
+    createdMany.forEach((elem) => {
+      console.log(elem.title);
+    });
+    const pr = Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } }
+    );
+    return pr;
+  })
+  .then((updateOne) => {
+    console.log("Recipe updated");
+    const pr = Recipe.deleteOne({ title: "Carrot Cake" });
+    return pr;
+  })
+  .then((deleteOne) => {
+    console.log("Recipe deleted");
+    mongoose.connection.close(() => {
+      console.log("Mongoose connection disconnected due to app termination");
+    });
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

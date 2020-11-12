d49f8b0b9e917c42efcbc7d6cb6f52863980167f
ararita <munjizavesna@gmail.com>
2020-11-12 19:33:13 +0100

    Iteration 4 done

--- index.js
+++ index.js
@@ -13,43 +13,53 @@ mongoose
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  // .then(() => {
-  // Run your code here, after you have insured that the connection was made
-  // const recipe = Recipe.create({
-  //   title: "Fish pie",
-  //   level: "Easy Peasy",
-  //   ingredients: [
-  //     "Fish filets",
-  //     "Potatoes",
-  //     "Milk",
-  //     "Salt",
-  //     "Pepper",
-  //     "Butter",
-  //     "Cheddar",
-  //   ],
-  //   cuisine: "International",
-  //   dishType: "main_course",
-  //   image:
-  //     "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSwES3x1LnAtNPusRrU4oIXzTD3B90YSvQZig&usqp=CAU",
-  //   duration: 90,
-  //   creator: "Ottolenghi",
-  // });
-  // })
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Fish pie",
+      level: "Easy Peasy",
+      ingredients: [
+        "Fish filets",
+        "Potatoes",
+        "Milk",
+        "Salt",
+        "Pepper",
+        "Butter",
+        "Cheddar",
+      ],
+      cuisine: "International",
+      dishType: "main_course",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSwES3x1LnAtNPusRrU4oIXzTD3B90YSvQZig&usqp=CAU",
+      duration: 90,
+      creator: "Ottolenghi",
+    });
+  })
   .then(() => {
     //importing an array of recipes form the data.json file:
-    console.log(data);
+    // console.log(data);
     return Recipe.insertMany(data);
   })
-  .then((data) => {
-    data.forEach((el) => {
-      console.log(el.title);
+  .then((recipes) => {
+    recipes.forEach((recipe) => {
+      // console.log(recipe.title);
     });
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+  })
+  .then(() => {
+    return Recipe.deleteOne();
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);

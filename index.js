20f965c72693d31e1024142cab55d342bf7429ca
ARMAGH <alv.gomez@outlook.com>
2020-11-12 22:54:36 +0100

    Initial commit - Recipe Update to Refactor

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,6 +22,42 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    // Iteration 2 - Create a Recipe
+    const recipe1 = {
+      title: "Chilli Crab",
+      level: "UltraPro Chef",
+      ingredients: [ "Crab", "Chilli Peppers", "Ginger", "Shallots", "Taucu", "Garlic" ],
+      cuisine: "Singaporean",
+      dishType: "main_course",
+      image: "https://www.seriouseats.com/recipes/images/2013/02/20130224-242112-singapore-chili-crab-edit.jpg",
+      duration: 30,
+      creator: "‪Zafrullah Abdul",
+      created: Date.now() ,
+    }
+    const pr = Recipe.create(recipe1, (err, result) => {
+      if (err) console.log(err);
+      else console.log(`Recipe for ${recipe1.title} was added to the Database`);
+    });
+
+    // Iteration 3 - Insert Multiple Recipes
+    Recipe.insertMany(data)
+      .then( (result) => {
+        result.forEach( (entry) => 
+        {console.log(`Recipe for ${entry.title} was added to the Database`)})
+      })
+      .catch( (err) => console.log( err ) );
+
+    // Iteration 4: Update recipe
+
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } },
+    )
+    .then( ( result ) => console.log(`Rigatoni alla Genovese was updated ` + result))
+    .catch( ( err ) => console.log(err) );
+
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

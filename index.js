4224d7b3e81279a80ca457925d81c72cca188894
GosiaMas <malgorzata.maslowska81@gmail.com>
2020-11-12 17:15:36 +0000

    Finished lab

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then(() => {
+    return Recipe.findOneAndDelete({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log("Success");
+  })
+  // .then(() => {
+  //   return Recipe.create({
+  //     title: "Chicken Drama",
+  //     level: "Amateur Chef",
+  //     ingredients: ["chicken", "tomatoes"],
+  //     cuisine: "fantasy",
+  //     dishType: "other",
+  //     duraion: 60,
+  //     creator: "GM",
+  //   });
+  // Run your code here, after you have insured that the connection was made
+  // })
+  //Need to disconnect mongoose before catch as errors are executed if everything fails and it could run long time before it disconnects. So my LAST INSTRUCTION is my last THEN and not .catch
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

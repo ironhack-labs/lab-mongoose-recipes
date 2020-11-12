e17274b0086a8932b5b5172d050a0198b856cc8b
sarastackhouse <sarapalacios7@gmail.com>
2020-11-12 09:33:54 +0100

    recipe model fix and done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -7,12 +7,12 @@ const recipeSchema = new Schema({
   level: {type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
   ingredients: [String],
   cuisine: String,
-  dishType: {type: String, enum ["breakfast", "main_course", "soup", "snack", "drink", "dessert or other"]
-  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' }, 
+  dishType: {type: String, enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert or other"]},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'}, 
   duration: {type: Number, min: 0},
   creator: String,
-  created:  {type: Date, default: Date.now}, 
-});
+  created:  {type: Date, default: Date.now}
+})
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 

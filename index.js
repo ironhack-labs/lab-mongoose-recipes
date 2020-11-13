5211163c8c7fc77864b8dba37d95f71abc27dfcb
adria66 <a.salvadorcazorla@gmail.com>
2020-11-13 18:27:31 +0100

    done

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,7 +22,72 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+     const recipeFriedEggs = 
+    {
+      title: "Fried eggs with french fries",
+      level: "Amateur Chef",
+      ingredients: [
+        "2 potatoes",
+        "1 cup olive oil",
+        "4 eggs",
+        "water",
+        "salt",
+      ],
+      cuisine: "worldwide",
+      dishType: "main_course",
+      image: "https://www.hogarmania.com/archivos/201703/huevos-fritos-patatas-668x400x80xX.jpg",
+      duration: 15,
+      creator: "Karlos Arquiñano",
+    }
+    
+    Recipe.create(recipeFriedEggs)
+    .then((result)=>{
+      console.log(`Recipe created: ${result.title}`)
+    })
+    .catch((err)=>{
+      console.log(err)
+      res.send(err)
+    })
+
+    Recipe.insertMany(data)
+    .then((result) => {
+      result.forEach((result) => {
+        console.log(`Recipe imported from data: ${result.title}`)
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  
+    .then(()=>{
+      Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+      .then((result)=>{
+        console.log(`Duration updated to: ${result.duration}`)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    })
+
+    .then(()=>{
+      Recipe.deleteOne({title: 'Carrot Cake'})
+      .then((result)=>{
+        console.log(`The selected recipe has been removed: ${result}`)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    })
   })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  // mongoose.connection.close()
+  // .then(()=>{
+  //   console.log('closed')
+  // })
+  // .catch((err)=>{
+  //   console.log(err)
+  // })

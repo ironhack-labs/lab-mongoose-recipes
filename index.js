70f31aa2dac63ba70273de2289120f33828648f0
Zhenni Qian <zhenni.qian913@gmail.com>
2020-11-14 23:39:16 +0100

    lab solved

--- index.js
+++ index.js
@@ -19,9 +19,41 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+  // .then(() => {
+  //   const recipeOne = data[0];
+  //   Recipe.create(recipeOne)
+  //     .then(recipe => console.log(recipe.title))
+  //     .catch(err => console.log(`An error occurred while saving a new recipe: ${err}`));
+  // })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+async function manipulateRecipeDb() {
+  await insertRecipes(data);
+  await updateRecipe();
+  await removeOneRecipe();
+  await mongoose.connection.close();
+}
+
+async function insertRecipes(data) {
+  const recipes = await Recipe.insertMany(data);
+  recipes.forEach(recipe => {
+    console.log(recipe.title);
+  });
+} 
+
+async function updateRecipe() {
+  await Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100});
+  console.log("Successfully updated the duration of the recipe");
+}
+
+async function removeOneRecipe() {
+  await Recipe.deleteOne({title: "Carrot Cake"});
+  console.log("Successfully removed Carrot Cake");
+}
+
+manipulateRecipeDb()
+  .then(()=>console.log("Success!"))
+  .catch(err => console.error(err));
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,45 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: {
+    type: [ String ]
+  },
+  cuisine: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"],
+    trim: true,
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: {
+    type: String,
+    trim: true,
+  },
+  created: {
+    type: Date,
+    default: new Date().toLocaleDateString(),
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

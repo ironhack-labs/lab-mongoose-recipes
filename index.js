478eeee9b6cc241509b2a63890d21d86a7f675d3
Dennis Deuling <dennis.deuling@gmail.com>
2020-11-15 13:58:41 +0100

    done

--- index.js
+++ index.js
@@ -9,19 +9,36 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(self => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+        // Before adding any documents to the database, let's delete all previous entries
+        return self.connection.dropDatabase();
+    })
+    .then(() => {
+        // Run your code here, after you have insured that the connection was made
+    })
+    .catch(error => {
+        console.error('Error connecting to the database', error);
+    });
+
+
+/* Recipe.create(data)
+    .then(recipe => console.log('The recipes are created: ', recipe))
+    .catch(error => console.log('An error happened: ', error)); */
+
+async function recipes(data) {
+    const recipes = await Recipe.insertMany(data);
+    recipes.map(recipe => console.log(recipe.title));
+    await Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+    console.log('Updated');
+    await Recipe.deleteOne({ title: 'Carrot Cake' });
+    console.log('Deleted');
+    mongoose.connection.close();
+}
+
+recipes(data);

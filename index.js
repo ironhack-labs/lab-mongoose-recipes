490724ac4058b5cadf6a6bccac4d3d4ec3740309
Daniela Gutiérrez <dangtz14@gmail.com>
2020-11-14 18:19:06 -0600

    Lab Completed Web-Mex-PT2 Daniela

--- index.js
+++ index.js
@@ -25,3 +25,63 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+//RECIPE
+
+const recipe = {
+
+    title: "Pizza",
+    level: "Amateur Chef",
+    ingredients: ["Marinara", "Pepper", "Onion", "Cheese"],
+    cuisine: "Italian",
+    dishType: "Main_course",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 20,
+    creator: "Southern Italy",
+    created: Date.now
+}
+
+Model.create(recipe, (error, recipe) => {
+  if (error) {
+    console.log("Error occurred.", error);
+    return
+}
+console.log(`The recipe was created:`, recipe)
+})
+
+Model.insertMany([], (error, recipe) => {
+  if (error) {
+    console.log("Error occurred.", error);
+    return
+}
+console.log(`The recipe's name is:`, recipe)
+})
+
+Model.findOneAndUpdate([], (error, recipe) => {
+  if (error) {
+    console.log("Error occurred.", error);
+    return
+}
+
+Model.updateOne({ name: 'Rigatoni alla Genovese' }, { duration: '100' })
+.then(self => {
+  console.log(`Connected to the database: "${self.connection.name}"`);
+  return self.connection.dropDatabase();
+})
+.catch(error => {
+  console.error('Error connecting to the database', error);
+});
+console.log(`The recipe's been updated:`, recipe)
+})
+
+User.deleteMany({ name: "Carrot Cake" })
+.then(self => {
+  console.log(`Connected to the database: "${self.connection.name}"`);
+  return self.connection.dropDatabase();
+})
+.catch(error => {
+  console.error('Error connecting to the database', error);
+});
+
+console.log(`The recipe's been deleted:`, recipe)

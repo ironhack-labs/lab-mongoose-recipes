9fb6bbd36724cb516fcf3a2807ce99375b9030a6
ZuzanaAK <kubatovazuzana@gmail.com>
2020-11-16 15:02:01 +0000

    done

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+let newRecipe = {
+  title: "Asian Glazed Chicken Thighs",
+  level: "Amateur Chef",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  cuisine: "Asian",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 40,
+  creator: "Chef LePapu"
+};
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,6 +41,61 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+      //for iteration 2
+    /*
+    Recipe
+      .create(newRecipe)
+      .then((result)=> {
+        console.log(`recipe added: ${result.title}`)
+      })
+      .catch ((err) => {
+        console.log(err)
+      })
+    */
+      //for iteration 3
+      /*
+      Recipe
+        .insertMany(data)
+        .then((result) => {
+          result.forEach((item) =>{
+            console.log(`recipe for ${item.title} inserted`)
+          })
+  
+      });
+      */
+      //for interation 4
+        /*
+        Recipe
+          .findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+          .then ((result) => {
+            console.log(`recipe updated`)
+          })
+          .catch ((err) => {
+            console.log(err)
+          })
+        */
+      //for itereation 5
+          /*
+          Recipe
+            .deleteOne({title: "Carrot Cake"})
+            .then(() => {
+              console.log("carrot cake has been deleted")
+            })
+            .catch((err)=>{
+              console.log(err)
+            })
+          */
+      //for interation 6
+              /*
+              mangoose.connection
+                .close()
+                .then(() => {
+                  console.log ("connection has been closed")
+                })
+                .catch((err) => {
+                console.log(`Error with closing the database ${err}`)
+              })
+              */
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

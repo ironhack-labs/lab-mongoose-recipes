2b080dba3812b7cff9ebe73695b9f94e44c62f26
JuanBatkis <juanbatkis@gmail.com>
2020-11-17 02:07:52 -0300

    All done

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+let newRecipe = {
+  title: 'Quick and Easy Pizza Crust',
+  level: 'Amateur Chef',
+  ingredients: [
+    '1 (.25 ounce) package active dry yeast',
+    '1 teaspoon white sugar',
+    '1 cup warm water (110 degrees F/45 degrees C)',
+    '2 1/2 cups bread flour',
+    '2 tablespoons olive oil',
+    '1 teaspoon salt'
+  ],
+  cuisine: 'Italian',
+  dishType: 'main_course',
+  image: 'https://images.media-allrecipes.com/userphotos/2326147.jpg',
+  duration: 30,
+  creator: 'CHEF RIDER'
+};
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -20,7 +38,57 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    let promise1, promise2, promise3, promise4;
+
+    // Iteration 2
+    promise1 = Recipe
+      .create(newRecipe)
+      .then((recipe) => {
+        console.log(`Recipe title: ${recipe.title}`)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+
+    //Iteration 3
+    promise2 = Recipe
+      .insertMany(data)
+      .then((recipes) => {
+        recipes.forEach(recipe => {
+          console.log(`Recipe title: ${recipe.title}`)
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+
+    //Iteration 4
+    promise3 = Recipe 
+      .findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+      .then((recipe) => {
+        console.log(`Recipe "${recipe.title}" duration updated to ${recipe.duration}`)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+
+    //Iteration 5
+    promise4 = Recipe
+      .deleteOne({ title: 'Carrot Cake' })
+      .then(() => {
+        console.log('"Carrot Cake" has been removed')
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+
+    Promise.all([promise1, promise2, promise3, promise4])
+      .then(() => {
+        mongoose.connection.close();
+      })
+      .catch(() => {
+        mongoose.connection.close();
+      });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

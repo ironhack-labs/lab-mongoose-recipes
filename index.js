d308b9e2094f885aadbb98992225d1ff6d71687e
Gonzalo Loza <cg.lozarojas@gmail.com>
2020-11-19 20:17:52 -0500

    Done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { db } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -21,6 +22,64 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const madeUpRecipe = {
+      title: "Ceviche",
+      level: "Easy Peasy",
+      ingredients: [
+        "Fish",
+        "Lemon",
+        "Onions",
+        "Parsley",
+        "A couple spoons of salt",
+        "Fist Stock",
+        "Red Pepper"
+      ],
+      cuisine: "Peruvian",
+      dishType: "main_course",
+      image: "https://www.thespruceeats.com/thmb/4hiFhzfzR7mG275rp6prJv2dj84=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ceviche-fresco-845267908-5abd993b642dca0036ec8b3b.jpg",
+      duration: 35,
+      creator: "Gonzalo Loza",
+      created: 2020-11-22,
+    };
+
+    // ITERATION 2
+    
+    /*
+    Recipe.create(madeUpRecipe)
+    .then(recipe => {
+      console.log(recipe.title)
+    })
+    .catch(err => {
+      console.log(`Everything failed in the following error ${err}`)
+    });
+    */
+
+    // ITERATION 3
+
+/*
+    Recipe.insertMany(data, (error, docs) => {})
+*/ 
+
+    // ITERATION 4
+
+    Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {$set: {duration: 100}}, {new: true}, (err, doc) => {
+      if (err) {
+        console.log("error", err);
+        return;
+      };
+      console.log("Success", doc)
+    })
+
+    //ITERATION 5
+
+
+
+
+    //ITERATION 6
+    db.close()
+      .then(success => console.log(`The database has been closed: ${success}`))
+      .catch(err => console.log(`Problems closing the database: ${err}`))
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

5770ca41214fbfff45c330eff3fde97760ab9504
ThSchweers <th.schweers@googlemail.com>
2020-11-26 18:36:42 +0100

    [Remote WD PT 261120] Thomas Schweers

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -20,8 +20,36 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
   })
+  Recipe.create({    
+    title: 'Lasgne',
+    level: 'Easy Peasy',
+    ingredients: ['Hackfleisch', 'Tomaten', 'Zwiebel(n)'],
+    cuisine: 'Italien',
+    dishType: 'main_course',
+    image: 'https://img.chefkoch-cdn.de/rezepte/745721177147257/bilder/668335/crop-600x400/lasagne.jpg',
+    duration: 60,
+    creator: 'Chef Italino',
+    created: '2020-11-11'
+  })
+
+  .then(theNewRecipeCreated => {
+    console.log('The new recipe is:', theNewRecipeCreated.title)
+    return Recipe.insertMany(data)     
+  })
+
+  .then(theNewsRecipesInsert => {
+    theNewsRecipesInsert.forEach(elm => console.log('The news recipes are:', elm.title))
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })     
+  })
+
+  .then(theRecipeUpdate => {
+    console.log('The recipe that has been updated is:', theRecipeUpdate.title)
+    return Recipe.deleteOne({ title: 'Carrot Cake' })    
+  })
+
+
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

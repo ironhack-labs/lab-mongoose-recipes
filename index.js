acd1b8353a41d648158eb99721861bbcc76dcfbd
ayline <ayline1695@hotmail.com>
2020-11-28 09:46:06 +0100

    Int 4

--- index.js
+++ index.js
@@ -21,7 +21,98 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+   
+    const NewSchema = mongoose.Schema;
+    const newRecipe = new NewSchema(
+      {
+        // TODO: write the schema
+        title:{ 
+          type: String,
+          required: true, // validación
+          unique: true,},
+        level : {
+          type: String,
+          enum: ['Easy Peasy','Amateur Chef','UltraPro Chef']
+        },
+        ingredients: {
+          type: [String]},
+        cuisine: {
+          type: String},
+        dishType: {
+          type: String,
+          enum: ['main_course','dessert']
+        },
+        image: {
+          type: String,
+          default: 'https://images.media-allrecipes.com/images/75131.jpg'
+        },
+        duration: {
+          type: Number,
+          min: 0
+        },
+        creator: {
+          type: String},
+        created: {
+          type: Date,
+          default: ''
+        }
+      }
+      )
+    const RecipeAdd = mongoose.model('RecipeAdd', newRecipe);
+    module.exports = RecipeAdd;
+    const receta = {
+        "title": "Fondant au chocolat",
+        "level": "Amateur Chef",
+        "ingredients": [
+          "220g dark chocolate",
+          "200g butter",
+          "100g white sugar",
+          "5 eggs",
+          "1 level tbsp flour",
+          "A little butter for the mould"
+        ],
+        "cuisine": "French",
+        "dishType": "dessert",
+        "image": "https://cdn.cnn.com/cnnnext/dam/assets/181127111211-24-50-sweets-travel-gateau-fondant-au-chocolat.jpg",
+        "duration": 40,
+        "creator": "Michel Bras"
+      }
+    RecipeAdd.create(receta, (error, recipeadd) => {
+      if(error){
+        console.log('An error happened', error)
+        return;
+      }
+      console.log('The title is: ', recipeadd.title )
+    })
+
+   
   })
+  .then(() =>{
+    Recipe.insertMany(data).then(() =>{
+      console.log(`Title recipes: ${title}`)
+    }).catch((err) => {
+      console.log(err)
+      }
+     
+    )  
+  }).then(() => {
+
+    data.findByIdAndUpdate({"5fc16df6e0d23d13905548be"},
+    {
+      duration : 100
+  },
+  {
+      new: true // actualiza el nombre
+  
+  }).then((data) =>{
+    if(data === null){
+        throw new Error('Title Not Found');
+    }
+    res.json({ message: 'Duration updated!' })
+    console.log("New Duration", data);
+})
+
+})
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,36 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title:{ 
+    type: String,
+    required: true, // validación
+    unique: true,},
+  level : {
+    type: String,
+    enum: ['Easy Peasy','Amateur Chef','UltraPro Chef']
+  },
+  ingredients: {
+    type: [String]},
+  cuisine: {
+    type: String},
+  dishType: {
+    type: String,
+    enum: ['main_course','dessert']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String},
+  created: {
+    type: Date,
+    default: ''
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

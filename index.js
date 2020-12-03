9ef179fbb5f42b89fd51868229695175ec04c806
lauraferraz5 <lauraferraz5@gmail.com>
2020-12-03 19:15:05 -0300

    done

--- index.js
+++ index.js
@@ -21,7 +21,56 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    Recipe.create({
+      title: "Banana and Chocolate Cake",
+      level: "Amateur Chef",
+      ingredients: [
+        "2 cups Oatmeal flour",
+        "4 Bananas",
+        "4 Eggs",
+        "1 tablespoon Cake yeast",
+        "Cinnamon powder",
+        "1 Chocolate bar"
+      ],
+      cuisine: "Brazilian",
+      dishType: "dessert",
+      duration: 60,
+      creator: "A Brazilian person",
+    })
+    .then((result) => {
+      console.log(result.title);
+    })
+    .catch((err) => {
+      console.error(err);
+    });   
+     
+    Recipe.insertMany(data)
+      .then((result) => {
+        result.map((result) => console.log(result.title));
+          Recipe.findOneAndUpdate(
+            {title: "Rigatoni alla Genovese"},
+            {duration: 100}
+          )
+          .then((result) => {
+            console.log(`Duration updared...? ${result.duration}`)
+          })
+          .catch((err) => {
+            console.error(err);
+          })
+            
+          Recipe.deleteOne({title: "Carrot Cake"})
+            .then((result) => {
+              console.log(`Recipe deleted...? ${result}`)
+
+              mongoose.connection.close();
+            })
+            .catch((err) => {
+              console.error(err)
+            })      
+    .catch((err) => {
+      console.error(err)
+    })  
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+})})

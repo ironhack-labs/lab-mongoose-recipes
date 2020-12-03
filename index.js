418dbe6d12beb7ad583cd8d2308c7ac8b89cdaef
Charlotte Oury <charlotte.ouryb@gmail.com>
2020-12-03 18:01:18 +0100

    lab done

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async () => {
+    const banana = await Recipe.create({
+      title: "Banana Bread",
+      level: "Amateur Chef",
+      ingredients: [
+        `3 bananas`,
+        `100g of sugar`,
+        `15g of almond powder`,
+        `one pinch of salt`,
+        `2g of cinnamon`,
+        `3g of ginger`,
+        `1 teaspoon of vanilla`,
+        `60g of a neutral oil`,
+        `165g of flour`,
+        `5g of baking powder`,
+      ],
+      cuisine: "Vegan",
+      dishType: "dessert",
+      image: "./images/banana-bread.jpg",
+      duration: 60,
+      creator: "Chacha",
+    });
+    console.log(banana.title);
+    await Recipe.insertMany(data);
+    data.forEach((element) => console.log(element.title));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(async () => {
+    const filter = { title: "Rigatoni alla Genovese" };
+    const update = { duration: 100 };
+    await Recipe.findOneAndUpdate(filter, update);
+    console.log("Duration updtated");
+  })
+  .then(async () => {
+    await Recipe.deleteOne({
+      title: "Carrot Cake",
+    });
+    console.log("Carrot Cake successfully removed");
+  })
+  .then(async () => {
+    mongoose.connection.close();
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

9f21ec8640add4e0734139998f59c5daee1bb78c
Thomas Lamy <thomaslamy75011@gmail.com>
2020-12-03 17:34:41 +0100

    done

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
+  });
+
+//  Recipe.create(data[0])
+//   .then((recipe) => console.log(recipe.title))
+//   .catch((error) =>
+//     console.log("An error happened while saving a recipe", error)
+//    );
+
+Recipe.insertMany(data)
+  .then((recipe) =>
+    recipe.forEach((element) => {
+      console.log(element.title);
+    })
+  )
+  .catch((error) =>
+    console.log("An error happened while saving a recipe", error)
+  )
+
+  .then(() => {
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    )
+      .then(console.error("success"))
+      .catch((error) => console.log("An error happened", error));
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.deleteOne({ title: "Carrot Cake" })
+      .then(console.error("success"))
+      .catch((error) => console.log("An error happened", error));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

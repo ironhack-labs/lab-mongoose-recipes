634a3d115369402ad7fdfe1a26daf3e6c04923d2
Intan Paoli <intankhairunnisa@gmail.com>
2020-12-03 18:18:49 +0100

    first commit

--- index.js
+++ index.js
@@ -7,21 +7,34 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  //.then (async() => {
+  //const resdb = await Recipe.create(data[0]);
+  // console.log(data[0].title);
+  //})
+  .then(async() =>{
+    const resAll = await Recipe.insertMany(data);
+    const resUpdate = await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+    );
+    const resDel = await Recipe.deleteOne({title:"Carrot Cake"});
+    console.log("Success");
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+});
+

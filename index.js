51fe54a829274e8d693908a065c780a46a299330
Anojan Mylvaganam <m.anojan@live.fr>
2020-12-03 23:14:54 +0100

    all done

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async () => {
+    const newRecipe = {
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs",
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image:
+        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef Anojan",
+    };
+    await Recipe.create(newRecipe);
+    // console.log(newRecipe.title);
+
+    // iteration 3
+    data.forEach((recipe) => console.log(recipe.title));
+
+    // iteration 4
+    await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+    data.forEach((recipe) => console.log(recipe.duration));
+
+    // iteration 5
+    await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log("deleted successful");
+
+    // iteration 6
+    mongoose.connection.close();
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

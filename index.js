3fdec241af3c72c237fef4a90a3354bd599c3880
Milande Teisserenc bonesteve <jf.milande@gmail.com>
2020-12-05 19:16:12 +0100

    lab done

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
+    Recipe.create({ title: "Chicken curry", cuisine: "india" }).then(
+      (recipe) => {
+        console.log(recipe.title);
+      }
+    );
+    Recipe.insertMany(data).then((recipes) => {
+      recipes.forEach((recipe) => {
+        console.log(recipe.title);
+      });
+      Recipe.findOneAndUpdate({
+        title: 'Rigatoni alla Genovese'
+      }, {$set:{duration:100}}).catch((error) => {
+        console.log(error);
+      });
+      Recipe.deleteOne({
+        title: 'Carrot Cake'
+      }).then((recipe) => {
+        console.log("great, Carrot Cake is deleted");
+        mongoose.connection.close();
+      })
+
+    });
+   
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

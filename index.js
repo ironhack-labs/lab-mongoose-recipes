9e41865944420050d2db1a3e8d55bbeb8a5893cb
Sebastian Montoya <seb246@gmail.com>
2020-12-24 16:13:34 -0500

    finished except giving issue closing connection

--- index.js
+++ index.js
@@ -19,35 +19,55 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  // .then(() => {
-  //   Recipe.create({
-  //     title: `Sebas's recipe`,
-  //     level: `Easy Peasy`,
-  //     ingredients: [`Bread`, `Cheese`],
-  //     cuisine: `colombian`,
-  //     dishType: `snack`,
-  //     image: ``,
-  //     duration: 30,
-  //     creator: `Sebastian`,
-  //     created: ``
-  //   })
-  //   .then(newRecipe => {
-  //     console.log(`new recipe created by ${newRecipe.title}`)
-  //   })
-  //   .catch(error => {
-  //     console.log(`error creating website ${error}`)
-  //   })
-  //   // Run your code here, after you have insured that the connection was made
-  // })
-  // Recipe.insertMany(data)
-  // .then(dataResult => {
-  //   dataResult.forEach((eachData) => {
-  //     console.log(`New recipe added by ${eachData.title}`);
-  //   });
-  // })
-  // .catch(error => {
-  //   console.log(`error adding recipe ${error}`);
-  // })
+  .then(() => {
+    Recipe.create({
+      title: `Sebas's recipe`,
+      level: `Easy Peasy`,
+      ingredients: [`Bread`, `Cheese`],
+      cuisine: `colombian`,
+      dishType: `snack`,
+      image: ``,
+      duration: 30,
+      creator: `Sebastian`,
+      created: ``
+    })
+    .then(newRecipe => {
+      console.log(`new recipe created by ${newRecipe.title}`)
+    })
+    .catch(error => {
+      console.log(`error creating website ${error}`)
+    })
+    // Run your code here, after you have insured that the connection was made
+  })
+  Recipe.insertMany(data)
+  .then(dataResult => {
+    dataResult.forEach((eachData) => {
+      console.log(`New recipe added by ${eachData.title}`);
+    });
+  })
+  .catch(error => {
+    console.log(`error adding recipe ${error}`);
+  })
+  Recipe.findOneAndUpdate({title: `Rigatoni alla Genovese`}, {duration: 100})
+  .then(updated => {
+    console.log(`Success in updating duration to ${updated}`);
+  })
+  .catch(err => {
+    console.log(`error updating duration due to ${error}`);
+  })
+  Recipe.deleteOne({title: `Carrot Cake`})
+  .then(removed => {
+    console.log(`successfully removed ${removed}!`)
+  })
+  .catch(notremoved => {
+    console.log(`error removing due to ${notremoved}!`)
+  })
+
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  mongoose.connection.close();
+
+

ee43b09d335f2c0f8a5802d104d4403e08e1e5ff
ida <so.kida@hotmail.com>
2020-12-26 11:58:04 -0500

    done

--- index.js
+++ index.js
@@ -21,7 +21,48 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({title:'pizza', cuisine:'italian'})
+    .then(recepie=>console.log (`The recepie: ${recepie.title} is saved`))
+    .catch(error=>console.log('An error happened while saving recepie.', error))
+
+    Recipe.insertMany(data)
+    .then(data=>data.forEach(recepie=>console.log(recepie.title)))
+    .catch(error=>console.log('An error happened while saving recepie.', error))
+
+    setTimeout(() => {
+      Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true },
+      (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Successfully updated', data);
+        }
+      }
+    );
+    }, 2000)
+    
+
+setTimeout(() => {
+  Recipe.deleteOne({title:'Carrot Cake'})
+  .then( 
+    console.log(`Successfully deleted`)
+  )
+  .catch(error => {
+    console.error('Error while deleting', error)
+  });
+}, 2500)
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  
+
+  process.on("SIGINT", () => {
+    mongoose.connection.close(() => {
+      console.log("Connection disconnected");
+      process.exit(0);
+    })
+  })

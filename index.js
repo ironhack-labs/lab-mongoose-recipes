f2833d9120ff103984310797f3b75ba8f91c5046
Llorenc Palliser <palliser@palliser.es>
2021-01-09 12:13:03 +0100

    Iterations 1, 2, 3 done

--- index.js
+++ index.js
@@ -25,3 +25,74 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  //const Recipe = mongoose.model('recipes', recipeSchema)
+
+  // Iteration 2
+
+const recipe = new Recipe({
+    title: "Pizza",
+    level: "Easy Peasy",
+    ingredients: ['cheese', 'tomatoe', 'jam'],
+    cuisine: 'italian',
+    dishType: 'main_course',
+    image: 'https://images.media-allrecipes.com/images/75131.jpg',
+    duration: 20,
+    creator: 'Llorenc'
+})
+
+recipe.save()
+.then(recipe => {
+    console.log(`Recipe created ${recipe.title}`)
+})
+.catch(err => {
+    console.error(err)
+  
+})
+
+
+// Iteration 3
+const promise1 = Recipe.insertMany([{ 
+  title: "Spaguetties",
+  level: "Easy Peasy",
+  ingredients: ['cheese', 'tomatoe', 'jam'],
+  cuisine: 'italian',
+  dishType: 'main_course',
+  image: 'https://images.media-allrecipes.com/images/75131.jpg',
+  duration: 20,
+  creator: 'Llorenc'
+}, 
+{
+  title: " Rigatoni alla Genovese",
+  level: "Easy Peasy",
+  ingredients: ['cheese', 'tomatoe', 'jam'],
+  cuisine: 'italian',
+  dishType: 'main_course',
+  image: 'https://images.media-allrecipes.com/images/75131.jpg',
+  duration: 20,
+  creator: 'Francesco'
+}])
+;
+
+// Promise.all([promise1])
+//   .then(values => {
+//     console.log('students and cities have been inserted');
+//     console.log(values);
+//     mongoose.connection.close();
+//   })
+//   .catch(err => console.error(err));
+
+
+
+
+  // Iteration 4
+
+ const promise2 =  Recipe.updateOne({title:'Rigatoni alla Genovese'}, {duration: 100}) 
+
+ Promise.all([promise1, promise2])
+ .then(values => {
+   console.log('students and cities have been inserted');
+   console.log(values);
+   mongoose.connection.close();
+ })
+ .catch(err => console.error(err));

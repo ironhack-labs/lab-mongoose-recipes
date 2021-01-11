3e259521df09b11fdeae1a20d3e501b2409d9044
MkAlba <albacastello74@gmail.com>
2021-01-11 19:36:22 +0100

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -19,9 +20,80 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+  //se pone aquí dentro para que no se ejecute antes de uqe se conecte
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+    //Iteration 2
+    const recipe = new Recipe({
+      title: "Orange and Milk-Braised Pork Carnitas",
+      level: "UltraPro Chef",
+      ingredients: [
+        "3 1/2 pounds boneless pork shoulder, cut into large pieces",
+        "1 tablespoon freshly ground black pepper",
+        "1 tablespoon kosher salt, or more to taste",
+        "1 orange, juiced and zested"
+      ],
+      cuisine: "American",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
+      duration: 160,
+      creator: "Chef John"
+    })
+
+    recipe.save()
+      .then(recipe => {
+        console.log(`Recipe: ${recipe.title} created!!`)
+      })
+
+      .catch(err => {
+        console.error(err)
+      })
+
+    //Iteration 3
+    Recipe.insertMany(data)
+      .then(recipes => {
+        recipes.forEach(recipe => {
+          console.log(`recipe ${recipe.title} created`)
+        })
+
+        //Iteration 4
+        Recipe.findOneAndUpdate({
+            title: "Rigatoni alla Genovese"
+          }, {
+            duration: 100
+          })
+          .then(recipe => {
+            if (recipe) {
+              console.log(`Recipe: ${recipe.title} updated!!`)
+            } else {
+              console.log('recipe not found')
+            }
+          })
+          .catch(err => {
+            console.error(err)
+          })
+
+        //Iteration 5
+        Recipe.deleteOne({
+            title: "Carrot Cake"
+          })
+          .then(recipe => {
+            console.log(`Recipe  deleted!!`)
+
+            mongoose.connection.close()
+          })
+          .catch(err => {
+            console.error(err)
+          })
+      })
+
+
+
+      .catch(err => {
+        console.error(err)
+      })
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

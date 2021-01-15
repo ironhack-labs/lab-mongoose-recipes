0f17d51cb7d918bda5fc586b28dea2b12f36b024
Dardannya Patente <dardannyapatente@hotmail.com>
2021-01-15 15:38:54 +0100

    final changes

--- index.js
+++ index.js
@@ -21,6 +21,45 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    console.log('Connection has been established.');
+    return Recipe.create({
+      title: 'Strogonoff',
+      level: 'Easy Peasy',
+      ingredients: ['rice, mushrooms, garlic, oions, beef, tomato sauce and heavy cream'],
+      cuisine: 'Brazilian',
+      dishType: 'main_course',
+      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcybercook.com.br%2Freceitas%2Fcarnes%2Freceita-de-strogonoff-de-carne-14165&psig=AOvVaw01kuyDPAAMQ1LZzLCzwG7M&ust=1610734851374000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPiiooyFnO4CFQAAAAAdAAAAABAD',
+      duration: '40',
+      creator: 'Dardannya Patente',
+    });
+  })
+  .then(recipe => {
+    console.log(recipe.title);
+  })
+  .then(recipes => {
+  return Recipe.insertMany(data)
+  })
+  .then(recipes => {
+    recipes.forEach(recipe => {
+      console.log(`Recipe ${recipe.title} created!`)
+    });
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate( { title: 'Rigatoni alla Genovese'}, { duration: 100 });
+  })
+  .then(recipe => {
+    console.log('The Italian Recipe was updated');
+  })
+  .then(() => {
+    return Recipe.deleteOne( { title: 'Carrot Cake'} );
+  })
+  .then(recipe => {
+    console.log('The Carrot Cake Recipe was deleted');
+
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Connection has been destroyed.');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

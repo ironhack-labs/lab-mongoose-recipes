02de9d275f055334264a7ef324ce1a573290df31
Thiarhes <injesusservice@hotmail.com>
2021-01-22 13:17:36 -0300

    files werent saved, now its ok

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,13 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  title: String,
-  ingredients: [String],
+  title: {
+    tye: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+  },
+  ingredients: {
+    type: [String],
+  },
+  cuisine: {
+    type: String,
+    required: true,
+  },
   dishType: String,
   image: {
     type: String,
     default: `https://images.media-allrecipes.com/images/75131.jpg`,
-  }
+  },
+  durantion: {
+    type: Number,
+    min: 0,
+  },
+  creator: String,
+  created: Date,
+  default: Date.now,
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

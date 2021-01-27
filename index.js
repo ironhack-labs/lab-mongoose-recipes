0b4ff78d8111a797d761e2b88e71077d4f5022c7
monteromario <59968975+monteromario@users.noreply.github.com>
2021-01-27 19:27:07 +0100

    Done

--- index.js
+++ index.js
@@ -5,26 +5,8 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
-// Connection to the database "recipe-app"
-
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  /*
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-
-  .then(() => {
-    const firstData = {
-      title: 'Nothing',
+const firstData = {
+      title: 'Food for dummies',
       level: 'Easy Peasy',
       ingredients: ['water', 'salt'],
       cuisine: 'Single',
@@ -32,39 +14,53 @@ mongoose
       duration: 1,
       creator: 'mario',
     };
+
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+// Connection to the database "recipe-app"
+mongoose.set('useFindAndModify', false)
+
+mongoose
+  .connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(self => {
+    console.log(`1-Connected to the database: "${self.connection.name}"`);
+    // Before adding any documents to the database, let's delete all previous entries
+    return self.connection.dropDatabase();
+  })
+  .then(() => {
     Recipe.create(firstData)
-    .then(recipe => console.log('First recipe is saved and its value is: ', recipe.title))
-    .catch(error => console.log('An error happened while saving a new recipe:', error));
-  }) 
-  
-  .then(() => {
-    Recipe.insertMany(data)
-    .then(recipe => console.log('Recipes are saved and values are: ', data.map(doc => doc.title).sort()))
-    .catch(error => console.log('An error happened while saving a new recipe:', error));
-  })
+    .then(recipe => console.log('2-First recipe is saved and its value is: ', recipe.title))
+    .then(() => {
+      Recipe.insertMany(data)
+      .then(recipe => console.log('3-Recipes are saved and values are: ', data.map(doc => doc.title).sort()))
+      .then(() => {
+        let newValue = 100;
+        let updateTitle = "Rigatoni alla Genovese";
+        let deleteValue = 'Carrot Cake'
 
-  .then(() => {
-    let newValue = 100;
-    let updateTitle = "Rigatoni alla Genovese";
-    Recipe.updateOne({ title: updateTitle }, { duration: newValue })
-    .then(recipe => console.log(`Recipe ${updateTitle} updated, new value is: `, newValue))
-    .catch(error => console.log(`An error happened while updating ${updateTitle} recipe:`, error));
-  })
-  */
-  .then(() => {
-    let deleteValue = 'Carrot Cake'
-    Recipe.deleteOne({ title: deleteValue })
-      .then(recipe => console.log(`Recipe ${deleteValue} successfully deleted`))
-      .finally(() => {
-        mongoose.connection
-          .close()
-          .then(() => console.log("DB disconnected"))
-          .then(() => process.exit());
+        Recipe.updateOne({ title: updateTitle }, { duration: newValue })
+        .then(recipe => console.log(`4-Recipe ${updateTitle} updated, new value is: `, newValue))
+        .catch(error => console.log(`4-An error happened while updating ${updateTitle} recipe:`, error));
+
+        Recipe.deleteOne({ title: deleteValue })
+        .then(recipe => console.log(`5-Recipe ${deleteValue} successfully deleted`))
+        .finally(() => {
+          mongoose.connection
+            .close()
+            .then(() => console.log("6-All tasks finished. DB disconnected"))
+            .then(() => process.exit());
+        })
+      })
+      .catch(error => console.log('3-An error happened while saving a new recipe:', error));
     })
+    .catch(error => console.log('2-An error happened while saving a new recipe:', error));
   })
-
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error('1-Error connecting to the database', error);
   })
 
 process.on("SIGINT", () => {

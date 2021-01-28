6fe986e45c372bcf59ce712108b49aea2638e1aa
karuvsentor <karuvsentor@gmail.com>
2021-01-28 19:21:24 +0100

    Iteration 2 Done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
-
+console.log(data)
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -21,6 +21,26 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: `Three chocolate cake`,
+      level: `Amateur Chef`,
+      ingredients: [`black chocolate`, `milk chocolate`, `white chocolate`, `cookies`, `butter`],
+      cuisine: `International`,
+      dishType: `dessert`,
+      image: `https://www.google.com/search?q=tarta+tres+chocolates&sxsrf=ALeKk0078xoNWcTPfj4HYlZvG5MfdAIL3A:1611854409773&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjVmpjMkb_uAhViQkEAHcO-BeoQ_AUoAnoECAkQBA&biw=1920&bih=976#imgrc=t6HGkmOiZcp2WM`,
+      duration: 45,
+      creator: `Carlos and my beloved thermomix`,
+      created:``
+    })
+
+    .then((recipe) =>{
+      console.log(recipe.title)
+    })
+
+    .catch((error)=> {
+      console.log(`error in ${error}` )
+    })
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

d7096f22b3bf461432fe0879ee504e591211d5c2
Pedro <pedroscampoy@gmail.com>
2021-01-31 12:40:06 +0100

    lab done

--- index.js
+++ index.js
@@ -20,8 +20,60 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    //Run your code here, after you have insured that the connection was made
+    console.log('Iteration 2 - Create a recipe')
+    Recipe
+      .create({
+        title: "Chilli con carne",
+        level: "Amateur Chef",
+        ingredients: ['onion',
+          'red pepper',
+          'garlic cloves',
+          'oil',
+          'chilli powder',
+          'paprika',
+          'minced beef',
+          'chopped tomatoes',
+          'red kidney beans',
+        ],
+        cuisine: "Mexican",
+        dishType: "main_course",
+        image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001451_6-c8d72b8.jpg?quality=90&webp=true&resize=440,400",
+        duration: 60,
+        creator: "Chef Pedro Sola",
+      })
+      .then(recipe => {
+        console.log('Successfully created recipe:', recipe);
+        console.log('Iteration 3 - Insert multiple recipes')
+        Recipe
+          .insertMany(data)
+          .then(recipes => {
+            recipes.forEach((recipe) => {
+              console.log(`Successfully added recipe: ${recipe.title}`)
+            })
+            console.log('Iteration 4 - Update recipe')
+            Recipe
+              .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { useFindAndModify: false })
+              .then(recipe => {
+                console.log(`The recipe has been updated: ${recipe.title}`)
+                console.log('Iteration 5 - Remove a recipe')
+                Recipe
+                  .deleteOne({ title: 'Carrot Cake' })
+                  .then(() => {
+                    console.log(`Successfully Deleted!`)
+                    mongoose.connection.close(() => {
+                      console.log('All done, closing mongoose');
+                      process.exit(0)
+                    })
+                  })
+                  .catch(e => console.error(e))
+              })
+          })
+
+
+      })
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

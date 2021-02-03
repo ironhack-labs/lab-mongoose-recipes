d3fbd9b585fd5794e7560189ca37c2b6a8f1037e
Veronica Alcala <veronica.alcala@gmail.com>
2021-02-03 20:23:51 +0100

    iterarion 4, done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+mongoose.set('useFindAndModify', false);
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -20,17 +21,20 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    Recipe
-      .create({ title: "Arepa", level: "Easy Peasy", ingredients: ["Harina Pan", "sal", "agua"], cuisine: "venezolana", dishType: "breakfast", image: "https://www.recetasderechupete.com/wp-content/uploads/2019/11/Tipos-de-arepa-2-768x527.jpg", duration: 30}
-      )
+    Recipe.create({ title: "Arepa", level: "Easy Peasy", ingredients: ["Harina Pan", "sal", "agua"], cuisine: "venezolana", dishType: "breakfast", image: "https://www.recetasderechupete.com/wp-content/uploads/2019/11/Tipos-de-arepa-2-768x527.jpg", duration: 30})
       .then(theNewRecipe => console.log("New recipe: ", theNewRecipe.title))
-      Recipe
-        .insertMany(data, function(error,result) {
-          if (error) {console.log(error)
-          } else {
-            data.forEach(elm => console.log("New recipe: ", elm.title ))
-          }
+
+      .then(() => {
+        Recipe.insertMany(data)
+        .then((recipes) => {
+          recipes.forEach((recipe)=> console.log("New recipe: ", recipe.title))
+          Recipe.findOneAndUpdate( { title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+            .then(newDuration => {
+              console.log(`Duration of ${newDuration.title} updated to`, newDuration.duration)
+              .then()
+            })
         })
+      })
   })
 
   .catch(error => {

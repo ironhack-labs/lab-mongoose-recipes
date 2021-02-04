4c887a1e97d5e5d1c1d911247c2fc621c711b140
Sotiria Dialeti <soti.dialeti@gmail.com>
2021-02-04 17:52:32 +0100

    Finished

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+const data = require("./data");
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then((self) => {
+    return Recipe.create({
+      title: "Risotto",
+      level: "Amateur Chef",
+      ingredients: [
+        "1 cup of Rice",
+        "2 and a half cup of water",
+        "curcum",
+        "salt",
+      ],
+      cuisine: "main_course",
+      dishType: "main_course",
+      image:
+        "https://ohmydish.com/wp-content/uploads/2015/03/Risotto-Milanese.jpg",
+      duration: 30,
+      creator: "Unknown",
+    }).then((titleBack) => {
+      return console.log(titleBack.title);
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  .then((created) => {
+    return Recipe.insertMany(data).then((inserted) =>
+      inserted.forEach((inserted) => console.log(inserted.title))
+    );
+  })
+
+  .then((inserted) => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } }
+    );
+  })
+
+  .then((updated) => {
+    console.log("Success");
+
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+
+  .then(() => {
+    console.log("Delete success");
+    const pr = mongoose.connection.close();
+    return pr;
+  })
+
+  .catch((error) => console.log(error));

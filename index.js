3da2fe328e1a6f7984dd0379756e63ae9f2f40bf
Javi Sastre <javisastrecal@gmail.com>
2021-02-04 15:54:03 +0100

    finished lab

--- index.js
+++ index.js
@@ -1,27 +1,75 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
+const { db } = require("./models/Recipe.model");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then((response) => {
+    const newRecipe = {
+      title: "Crêpe avec du fromage",
+      level: "Professional Chef",
+      ingredients: ["Crêpe", "Fromage"],
+      cuisine: "Français",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 20,
+      creator: "Monsieur Crêpe",
+    };
+    const pr = Recipe.create(newRecipe);
+    return pr;
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  //.then((response) => {
+  //console.log(response);
+  //})
+  .then((response) => {
+    const pr = Recipe.insertMany(data);
+    return pr;
+  })
+  .then((response) => {
+    const pr = Recipe.find({}, { title: 1, _id: 0 });
+    return pr;
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .then((response) => {
+    const pr = Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } }
+    );
+    console.log("Bon appetit!");
+    return pr;
+  })
+  .then((response) => {
+    const pr = Recipe.deleteOne({ title: "Carrot Cake" });
+    return pr;
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+process.on("SIGINT", () => {
+  mongoose.connection
+    .close()
+    .then((data) => console.log("Connection closed"))
+    .catch((err) => console.log(err));
+});

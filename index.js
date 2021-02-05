c4b440c34c499c5fa2557b5fc907949d72d9baea
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-02-05 12:32:15 +0100

    Adam Tomczyk

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+Recipe.create({
+  title: "How to cook the best pasta in the world"
+}).then((data) => {
+  console.log('the data has been created', data)
+}).catch((err) => {
+  console.log('Sth went wrong', err)
+})
+
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,7 +30,16 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    console.log(Recipe.title)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  Recipe.insertMany(data).then((data) => {
+    data.forEach((el) =>{
+      console.log(el.title)
+    })
+  }).catch((err) => {
+    console.log('Sth went wrong', err)
+  })

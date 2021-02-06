b8939b64d60e0496af8b7b02a8d85f909f9ad425
Simone Capuano <scapuano19@gmail.com>
2021-02-06 16:12:57 +0100

    done

--- index.js
+++ index.js
@@ -9,19 +9,55 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(self => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+        // Before adding any documents to the database, let's delete all previous entries
+        return self.connection.dropDatabase();
+    })
+    .then(() => {
+        // Run your code here, after you have insured that the connection was made
+        console.log('successfully connected')
+    })
+    .catch(error => {
+        console.error('Error connecting to the database', error);
+    })
+
+Recipe.create({
+        title: 'Pizza',
+        level: "Amateur Chef",
+        ingredients: ["tomato", "mozzarella", "meal"],
+        cuisine: "italian",
+        dishType: "main_course",
+        duration: 20,
+        creator: "Hai and Simone",
+    })
+    .then(recipes => {
+        console.log(recipes.title)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+Recipe.insertMany(data)
+
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+    .then(recipes => {
+        console.log('duration updated', recipes)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+Recipe.deleteOne({ title: 'Carrot Cake' })
+    .then(recipes => {
+        console.log('deleted', recipes)
+    })
+    .catch(error => {
+        console.log(error)
+    })

c0742ef5a9d0cd1afd78eb136ea69e7b14678b18
cucabel <i.m.martinez@hotmail.es>
2021-02-07 10:52:28 +0100

    commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require("./models/Recipe.model");
+
 // Import of the data from './data.json'
 const data = require("./data");
+
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -17,42 +21,64 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    const allRecipes = Recipe.insertMany(data);
-    return allRecipes;
+    const ironChicken = {
+      title: "Ironhack Beer Glazed Chicken",
+      level: "UltraPro Chef",
+      ingredients: [
+        "At least 5 bottles of beer (for the Chef :) )",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "2 tablespoons Asian chili garlic sauce",
+        "salt to taste",
+        "8 skinless, boneless chicken breasts"
+      ],
+      cuisine: "Web Dev",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Bob"
+    };
+
+    const createPr = Recipe.create(ironChicken);
+    return createPr;
   })
-  .then(() => {
-    const updatedRecipe = Recipe.findOneAndUpdate(
-      { title: "Rigatoni alla Genovese" },
-      { $set: { duration: 100 } },
-      { new: true }
+  .then((createdRecipeDocument) => {
+    console.log('Recipe.create()', createdRecipeDocument);
+
+    const insertManyPr = Recipe.insertMany(data);
+    return insertManyPr;
+  })
+  .then((createdRecipesArr) => {
+    // Print the title of each recipe to the console | i + 1 to avoid the one we created at the 1st iteration
+    createdRecipesArr.forEach((recipeDocument, i) => {
+      console.log(i + 1, recipeDocument.title);
+    })
+
+    const updatePr = Recipe.findOneAndUpdate(
+      {title: "Rigatoni alla Genovese"}, 
+      {$set: {duration: 100}}, // {duration: 100}, it also modifies the duration without the set      
+      {new: true}
     );
-    return updatedRecipe;
+    return updatePr;
+  })
+  .then((updatedRecipeDocument) => {
+    console.log("UPDATED RECIPE ->", updatedRecipeDocument);
+
+    const deletePr = Recipe.deleteOne(
+      {title: "Carrot Cake"}
+      );
+    return deletePr;
+  })
+  .then((deleteOneResult) => {
+    console.log("DELETED -> ", deleteOneResult);
+
+    const closePr = mongoose.connection.close();
+    return closePr;
   })
   .then(() => {
-    console.log("YAY you've been updated!");
-  })
-  .then(() => {
-    const delete1 = Recipe.deleteOne({ title: "Carrot Cake" });
-    return delete1;
-  })
-  .then((deletedRecipe) => {
-    console.log("Yay you deleted it", deletedRecipe);
+    console.log("DATABASE CLOSED!")
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
-process.on("SIGINT", () => {
-  mongoose.connection
-    .close()
-    .then((data) => console.log("Connection closed"))
-    .catch((err) => console.log(err));
-}); // Ctrl + C, the process ends and also the conecxion
-
-// ------ Iteration 2 - find title -------- //
-// .then(() => {
-//   const title = Recipe.find({}, { title: 1, _id: 0 });
-//   return title;
-// })
-// .then((recipeName) => {
-//   console.log(recipeName);
-// })

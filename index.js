3a228625051ea120ad465678d53e95d2ed107231
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-02-24 22:51:00 +0100

    iterations 1 to 5 -4 and 5 do not work-

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,7 +23,86 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+// 2 create a recipe
+
+  function createRecipe(infos){
+    const {title, level, ingredients, cuisine, dishType, image, duration, creator, created } = infos; 
+    Recipe.create({ 
+      title, level, ingredients, cuisine, dishType, image, duration, creator, created
+    })
+    .then((dbSuccess) => {
+      console.log(dbSuccess);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  createRecipe({
+
+    title: "pizza",
+    level: "Amateur Chef",
+    ingredients: ["farine", "eau", "tomate", "champignon", "basilic"],
+    cuisine: "Italian",
+    dishType: "main_course",
+    image: "https://www.papa-cool.fr/wp-content/uploads/2020/09/comment-faire-une-pizza-napolitaine-1024x684.jpg",
+    duration: 20,
+    creator: "Luigi",
+    created: "2020-06-15"
+
+  })
+
+//3 insert the recipes
+
+const recipesList = require('./data.json');
+console.log(recipesList);
+
+function insertRecipes(arr){
+  Recipe.insertMany(arr)
+  .then((dbSuccess) => {
+    console.log(dbSuccess);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
+insertRecipes(recipesList);
+ 
+//4 findOneAndUpdate
+
+function findOneUpdate(condition, update){
+  Recipe.findOneAndUpdate(condition, update)
+  .then((dbSuccess) => {
+    console.log(dbSuccess);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
+findOneUpdate({title : "Rigatoni alla Genovese"}, {$set: {duration: 100 }})
+
+//5 remove a recipe
+
+function deleteOneItem(itemToDelete){
+  Recipe.deleteOne(itemToDelete)
+  .then((dbSuccess) => {
+    console.log(dbSuccess);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
+deleteOneItem({title : 'Carrot Cake'});
+  
+
+//6 Close the database
+

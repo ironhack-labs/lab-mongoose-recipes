bc9a41fcca5c8f0b6328f5ee79ee8d9d96a611b0
Cl Sayart <clsayart@gmail.com>
2021-02-24 18:30:09 +0100

    done

--- index.js
+++ index.js
@@ -21,7 +21,54 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+const gateau = {
+  title: 'Gateau',
+  level: 'Easy Peasy',
+  ingredients: ["oeufs", "farine", "sucre"],
+  cuisine: "French",
+  dishType: "dessert",
+  image: "default",
+  duration: 20,
+  creator: "Bibi"
+};
+
+
+Recipe.create(gateau, (error, recipe) => {
+  if (error) {
+    console.log('An error happened:', error);
+    return;
+  }
+  console.log('The recipe is saved and its value is: ', recipe);
+});
+
+Recipe.create(gateau)
+  .then(recipe => console.log('The recipe is saved and its value is: ', recipe))
+  .catch(error => console.log('An error happened while saving a new recipe:', error));
+
+  Recipe.insertMany(data, (error, recipe) => {
+    if (error) {
+      console.log('An error happened:', error);
+      return;
+    }
+    console.log('The recipe is saved and its value is: ', recipe);
+  });
+
+  Recipe.insertMany(data)
+  .then(recipe => console.log('The recipe is saved and its value is: ', recipe))
+  .catch(error => console.log('An error happened while saving a new recipe:', error));
+
+ 
+  Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then(recipe => console.log('The recipe is updated and its value is: ', recipe))
+  .catch(error => console.log('An error happened while updating a recipe:', error));
+
+  Recipe.deleteOne({ title: "Carrot Cake" })
+  .then(recipe => console.log(recipe, 'is deleted'))
+  .catch(error => console.log('An error happened while deleting a recipe:', error));

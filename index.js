2cda27ed4a2422b73de76ef7436395bea2cbe2eb
Rowan Williams <rmtwilliams@hotmail.co.uk>
2021-02-24 20:21:12 +0100

    done

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const eggRecipe = {
+  title: "Fried egg",
+  level: "Easy Peasy",
+  ingredients: ["2 eggs", "tbsp olive oil"],
+  cuisine: "Global",
+  dishType: "breakfast",
+  duration: 5,
+  creator: "Chef Rowan"
+}
+
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,7 +32,33 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    
+    Recipe.create(eggRecipe).then((recipe) => console.log(recipe.title))
+  
+      .then(() => {
+        Recipe.insertMany(data).then((recipes) => recipes.forEach((recipe) => console.log(recipe.title)))
+      
+          .then(()=> {
+            Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}).then(() => console.log("successfully changed the durations"))
+          
+              .then(()=> {
+                Recipe.deleteOne({title: "Carrot Cake"}).then(console.log("successfully deleted Carrot Cake"))
+                 
+                  .then(() => {
+                    mongoose.connection.close()
+                    
+                  })
+              })
+          })
+      })     
   })
+  
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+  
+
+

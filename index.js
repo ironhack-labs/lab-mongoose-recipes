7f4c9d360ec12550637bca7d7d1e275492c4e332
maudfabrello <ma.fabrello@gmail.com>
2021-02-24 18:34:45 +0100

    stuck at two

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
@@ -19,9 +20,50 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
+  then(user => console.log('newRecipe', user))
+.then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    //ITERATION 2 : CREATE A RECIPE
+    Recipe.create({
+    "title": "Carrot Cake",
+    "level": "Amateur Chef",
+    "ingredients": [
+      "6 cups grated carrots",
+      "1 cup brown sugar",
+      "1 cup raisins",
+      "4 eggs",
+      "1 1/2 cups white sugar",
+      "1 cup vegetable oil",
+      "2 teaspoons vanilla extract",
+      "1 cup crushed pineapple, drained",
+      "3 cups all-purpose flour",
+      "1 1/2 teaspoons baking soda",
+      "1 teaspoon salt",
+      "4 teaspoons ground cinnamon"
+    ],
+    "cuisine": "International",
+    "dishType": "dessert",
+    "image": "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
+    "duration": 130,
+    "creator": "Chef Nadia"
+    })
+    .then(user => console.log('newRecipe', user))
+      })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  // ITERATION 3
+
+  Recipe.insertMany(recipes) 
+//const arr = [{ name: 'Star Wars' }, { name: 'The Empire Strikes Back' }];
+//Movies.insertMany(arr, function(error, docs) {});
+
+      .then((result) => {
+        result.map((recipeSchema) => {
+          console.log(recipeSchema.title);
+        })
+        .catch(error => {
+          console.error('Error connecting to the database', error);
+        });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,44 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ["easy peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: [String],
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default: "http://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now()
+  }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

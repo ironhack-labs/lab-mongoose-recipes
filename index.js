20f3aa9b1cae7c1e23802179bcd8d1b8d56052e0
Florian Jam <jam.florian@gmail.com>
2021-02-24 23:32:52 +0100

    done

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { model } = require('./models/Recipe.model');
+const dataJson = require("./data.json");
+
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+mongoose.set('returnOriginal', false);
+
+
+const firstRecipeData = {
+  title: "peperroni pizza",
+  level: "Easy Peasy",
+  ingredients: ["dough", "tomato", "chesse", "pepperoni"],
+  cuisine: "italian",
+  dishType: "main_course",
+  image:"https://www.istockphoto.com/fr/photo/pizza-au-pepperoni-savoureux-et-le-cuisson-ingr%C3%A9dients-tomates-basilic-sur-fond-noir-gm1083487948-290669894", 
+  duration: 45,
+  creator: "Pompeo",
+  created: Date.now()
+};
 
 // Connection to the database "recipe-app"
 mongoose
@@ -20,7 +37,23 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create(firstRecipeData).then(recipe => console.log(recipe.title))
+    
+    .then(() => { 
+      Recipe.insertMany(dataJson).then(recipes => console.log(recipes.map(recipe => recipe.title)))
+      
+      .then(() => {
+        Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true}).then(update => console.log(update))
+        
+        .then( () => {
+          Recipe.deleteOne({title: "Carrot Cake"}).then(result => console.log(result)).catch(err => console.log(err))
+
+          .then(() => {
+            mongoose.connection.close()
+          })
+        })
+      })
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -17,7 +17,7 @@ const recipeSchema = new Schema({
   },
   dishType: {
     type: String,
-    enum: ['breakfast', 'main ourse', 'soup', 'snack', 'drink', 'dessert', 'other']
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
   },
   image: {
     type: String,
@@ -37,4 +37,5 @@ const recipeSchema = new Schema({
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+
 

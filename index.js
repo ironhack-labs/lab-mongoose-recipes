3c1bd5674471ca14fd2bb136429284d005e6bd91
Antoine Stouff <antoine-78280@hotmail.fr>
2021-02-24 22:53:27 +0100

    I realy don't know what to do for updating

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+// const recip = {
+//   title: "Boeuf Bourguignon",
+//   level: "Amateur Chef",
+//   ingredients: ["boeuf", "vin"],
+//   cuisine: "sorry ?",
+//   dishType: "main_course",
+//   image: "https://odelices.ouest-france.fr/images/recettes/boeuf-bourguignon.jpg",
+//   duration: 240,
+//   creator: "jean Bourguignon",
+//   created: "1975-06-22"
+// }
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -20,8 +30,21 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Recipe.create(recip, (error, recipe) => {
+    //   if (error) {
+    //     console.log("an error happened:", error);
+    //     return;
+    //   }
+    //   console.log("The new recipe is saved:", recipe);
+    // })
+    Recipe.insertMany(data)
+    .then(recipe => console.log("Here are your new recipes!:", recipe))
+    .catch(error => console.log("Oups, something wrong happened:", error));
+    Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, {duration: 100})
+    .then(recipe => console.log("here's your update", recipe.duration))
+    .catch(error => console.log("Oups:", error));
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  console.log(Recipe);

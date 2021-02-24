9251cdf34b16afb5767032202569d500def12b12
Victoria V <virlan.victoria@gmail.com>
2021-02-24 23:27:32 +0100

    iteration3

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data.json');
-//console.log(data);
+const data = require("./data.json");
+//console.log("this is what im printing ", data);
 //console.log("this is it", Recipe);
 //const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-const recipe1 = { title: "Asian Glazed Chicken Thighs",
-level: "Amateur Chef",
-ingredients: [
-  "1/2 cup rice vinegar",
-  "5 tablespoons honey",
-  "1/3 cup soy sauce (such as Silver Swan®)",
-  "1/4 cup Asian (toasted) sesame oil",
-  "3 tablespoons Asian chili garlic sauce",
-  "3 tablespoons minced garlic",
-  "salt to taste",
-  "8 skinless, boneless chicken thighs"
-],
-cuisine: "Asian",
-dishType: "main_course",
-image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-duration: 40,
-creator: "Chef LePapu"
+const recipe1 = {
+  title: "Asian Glazed Chicken Thighs",
+  level: "Amateur Chef",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs",
+  ],
+  cuisine: "Asian",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 40,
+  creator: "Chef LePapu",
 };
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect('mongodb://localhost:27017/recipe-app', {
+  .connect("mongodb://localhost:27017/recipe-app", {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    
-    
+//Iteration 2
     //Recipe.create(recipe1)
-   // .then(user => console.log('The recipe is : ', {recipe1})
+    // .then(user => console.log('The recipe is : ', {recipe1})
     //.catch(error => console.log('An error happened while saving a new recipe:', error)));
-    Recipe.insertMany([
-    
-
-    ]);
-
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    //ITERATION 3
+   // Recipe.insertMany(data)
+    //  .then(function () {
+    //    data.forEach((object) => console.log(object.title));
+    //  })
+   //   .catch((error) => {
+    //    console.error("No title updated", error);
+   //   });
+ })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

fc15950127e53533ded4722b671d63a6d7420b7b
Mohammed Adib <adibmohammed95@gmail.com>
2021-02-26 16:32:01 +0100

    Recipes Lab

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { insertMany } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -18,10 +19,25 @@ mongoose
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
+    
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+   Recipe.create({
+     title: "Tanjia",
+
+   });
+
+Recipe.insertMany(data, function (err) {
+  if(err){
+    console.error('error' + err)
+  }else{
+    console.log("Goooood")
+  }
+})
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+ 

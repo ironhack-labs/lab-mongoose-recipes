5831bb4d645e3600dd3a4a202a03909c89c70d0e
Alexis Verthuy <alexis.verthuy@gmail.com>
2021-02-28 14:47:03 +0100

    Lab Done

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const myFirstRecipe = {
+  title: "Chocolate Mousse",
+  level: "Easy Peasy",
+  ingredients: ["Chocolate", "Eggs"],
+  cuisine: "French",
+  dishType: "dessert",
+  duration: 25,
+  creator: "John Doe",
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -19,8 +29,18 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async() => {
+    const resFromDb = await Recipe.create(myFirstRecipe)
+    console.log(resFromDb.title)
+    const resFromDb2 = await Recipe.insertMany(data)
+    resFromDb2.forEach((recipe) => {console.log(recipe.title)})
+    const resFromDb3 = await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
+    console.log(resFromDb3)
+    const resFromDb4 = await Recipe.deleteOne({title: "Carrot Cake"})
+    console.log(resFromDb4)
+    await mongoose.connection.close()
+    console.log('Db is disconnected')
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

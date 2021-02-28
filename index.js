6745b036e4d5d960d2d3ff437d9629c792a71a77
Fatoumata Diaby <fatoumata.diaby@outlook.com>
2021-02-28 22:33:35 +0100

    done

--- index.js
+++ index.js
@@ -20,24 +20,44 @@ mongoose
     return self.connection.dropDatabase();
   })
 
-  function createRecipe(infos) {
-    const {title, level, ingredients, cuisine, dishType, image, duration, creator, created } = infos;
-    Recipe.create({
-      title,
-      level,
-      ingredients,
-      cuisine,
-      dishType,
-      image,
-      duration,
-      creator,
-      created
-    })
-  .then((recipeSuccess) => {
+  .then(async(recipeSuccess) => {
+    try {
     // Run your code here, after you have insured that the connection was made
-    console.log(title)
+    //console.log(title)
+      //iteration 2
+    const responsefromdb = await Recipe.create(myRecipe);
+    console.log("Creation succeded: ", responsefromdb)
+  //iteration 3
+    const resipe3 = await Recipe.insertMany(data);
+    resipe3.forEach((recipe) => console.log(recipe.title))
+  //iteration 4
+    const resdbite4 = await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true});
+    console.log("ite3", resdbite4)  
+  //iteration 5
+    const resdbite5 = await Recipe.findOneAndDelete({title: "Carrot Cake"});
+    console.log("ite4", resdbite5)
+  "Carrot Cake"
+  //iteration 6
+    await mongoose.connection.close();
+    console.log("Db disconnected")
+    } catch(err){
+      console.log("error")
+    }
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  
+
+  const myRecipe = {
+    "title": "myRecipe",
+    "level": "Amateur Chef",
+    "ingredients": [
+      "1 large egg" 
+    ], 
+    "cuisine": "French",
+    "dishType": "dessert",
+    "image": "https://images.media-allrecipes.com/images/75131.jpg",
+    "duration": 30,
+    "creator": "Fatou"
   }

76669704be6c124986e6375a8480f70881005382
Tomsylva <Tomsylvanz@gmail.com>
2021-03-11 21:18:39 +0100

    forgot to save...

--- index.js
+++ index.js
@@ -20,28 +20,28 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    Recipe.create({
-      title: "Asian Glazed Chicken Thighs",
-      level: "Amateur Chef",
-      ingredients: [
-        "1/2 cup rice vinegar",
-        "5 tablespoons honey",
-        "1/3 cup soy sauce (such as Silver Swan®)",
-        "1/4 cup Asian (toasted) sesame oil",
-        "3 tablespoons Asian chili garlic sauce",
-        "3 tablespoons minced garlic",
-        "salt to taste",
-        "8 skinless, boneless chicken thighs",
-      ],
-      cuisine: "Asian",
-      dishType: "main_course",
-      image:
-        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-      duration: 40,
-      creator: "Chef LePapu",
-    }).then((recipe1) => {
-      console.log(recipe1.title);
-    });
+    // Recipe.create({
+    //   title: "Asian Glazed Chicken Thighs",
+    //   level: "Amateur Chef",
+    //   ingredients: [
+    //     "1/2 cup rice vinegar",
+    //     "5 tablespoons honey",
+    //     "1/3 cup soy sauce (such as Silver Swan®)",
+    //     "1/4 cup Asian (toasted) sesame oil",
+    //     "3 tablespoons Asian chili garlic sauce",
+    //     "3 tablespoons minced garlic",
+    //     "salt to taste",
+    //     "8 skinless, boneless chicken thighs",
+    //   ],
+    //   cuisine: "Asian",
+    //   dishType: "main_course",
+    //   image:
+    //     "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //   duration: 40,
+    //   creator: "Chef LePapu",
+    // }).then((recipe1) => {
+    //   console.log(recipe1.title);
+    // });
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);

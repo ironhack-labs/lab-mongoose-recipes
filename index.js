357106e71957abef393b9c934ec0500f301c4d9a
Tomsylva <Tomsylvanz@gmail.com>
2021-03-11 21:17:18 +0100

    iteration 2 hard coded

--- index.js
+++ index.js
@@ -20,51 +20,29 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs",
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image:
+        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu",
+    }).then((recipe1) => {
+      console.log(recipe1.title);
+    });
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
-
-const recipeSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  level: {
-    type: String,
-    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
-  },
-  ingredients: [String],
-  cuisine: String,
-  dishType: {
-    type: String,
-    enum: [
-      "breakfast",
-      "main_course",
-      "soup",
-      "snack",
-      "drink",
-      "dessert",
-      "other",
-    ],
-  },
-  image: {
-    type: String,
-    default: "https://images.media-allrecipes.com/images/75131.jpg",
-  },
-  duration: {
-    type: Number,
-    min: 0,
-  },
-  creator: String,
-  created: {
-    type: Date,
-    default: Date.now(),
-  },
-});
-
-const Recipe = mongoose.model("Recipe", recipeSchema);
-
-Recipe.create({});

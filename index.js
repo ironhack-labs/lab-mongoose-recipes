14e6d9bc15fb9a78bb35ab8657f11cf2d9f6eb1f
Luis Henestrosa <ljoaquincuenca@gmail.com>
2021-03-12 12:32:59 +0100

    iterations 3 and 5 are not working... yet ;)

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+const data = require("./data");
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+  //Iteration 1
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Pasta",
+      level: "Easy Peasy",
+      ingredients: ["pasta", "pomodoro"],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 20,
+      creator: "Luis&Luca",
+      created: new Date(),
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  //Iteration 2
+  .then((recipe) => {
+    console.log(recipe.title);
+  })
+  //Iteration 3 (not working)
+  .then(() => {
+    Recipe.insertMany(data);
+    return Recipe;
+  })
+  .then((recipes) => {
+    console.log(recipes.title);
+  })
+  //Iterastion 4
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  //Iteration 5 (not working, probably because of iteration 3)
+  .then(() => {
+    Recipe.findOneAndDelete({ title: "Carrot Cake" });
+    //Iteration 6
+    return mongoose.disconnect();
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

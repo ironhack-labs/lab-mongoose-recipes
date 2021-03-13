0d6fa31a7dcaf914e100d15d37ace47ed9d5d1d0
Tsherculano <tico.herculano@gmail.com>
2021-03-13 11:34:58 -0300

    even interation 4 done

--- index.js
+++ index.js
@@ -19,8 +19,32 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
+  .then(async () => {
     // Run your code here, after you have insured that the connection was made
+    const recipeArrumadinho = {
+      title: 'Arrumadinho com Charque',
+      level: 'Easy Peasy',
+      ingredients: ['Cuscuz', 'Charque', 'Tomate', 'Cebola', 'Coentro'],
+      cuisine: 'Brasileira',
+      dishType: 'main_course',
+      duration: 10,
+      creator: 'Thiago Herculano'
+    };
+
+    try {
+      const includeArrumadinho = await Recipe.create(recipeArrumadinho);
+      console.log(includeArrumadinho.title);
+
+      const newRecipeMany = await Recipe.insertMany(data);
+      newRecipeMany.forEach(recipe => console.log(recipe.title));
+
+      const updateRecipe = await Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese'},{ $set: {duration: 100 }});
+      console.log(`Verifição de código acima. O value de duration tem que ser 100 ==> ${updateRecipe.duration}`)
+
+    } catch (error) {
+      console.log(error)
+    }
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

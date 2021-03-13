3e6cf1b8b5cc7b0af718ae634732056a1d4f4d13
mmanso <mayara.manso@mercadolivre.com>
2021-03-13 12:06:02 -0300

    Iteration 2 - Create a recipe

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const newRecipe = {
+  title: 'FAROFA DE BANANA',
+  level: 'Easy Peasy',
+  ingredients: [
+    '1 ½ xícara (chá) de farinha de mandioca torrada',
+    '2 bananas-prata',
+    '½ cebola roxa',
+    '1 dente de alho',
+    '2 colheres (sopa) de manteiga',
+    'sal e pimenta-do-reino moída na hora a gosto'
+  ],
+  cuisine: 'Brasileira',
+  dishtype: 'acompanhamento',
+  duration: 15,
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,6 +37,10 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(newRecipe)
+      .then(response => console.log(response.title))
+      .catch(error => console.log('erro ao criar receita: ', error))
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

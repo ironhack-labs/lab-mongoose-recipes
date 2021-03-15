e871e59170a97454222cdb7875249a4fd5227526
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-03-15 16:53:31 -0300

    init commit

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const chocolate = {
+  title: "Chocolate",
+  level: "Easy Peasy",
+  ingredients: [ "Chocolate" ],
+  duration: 30,
+  creator: "Someone",
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -21,6 +29,14 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(chocolate)
+      .then(chocolate => console.log('Recipe: ', chocolate.title))
+      .catch(error => console.log('Error while creating!', error))
+  })
+  .then(() => {
+    Recipe.deleteOne(chocolate)
+      .then(console.log("Deleted"))
+      .catch(error => console.log("Removal failed", error))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

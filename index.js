2ef83fa37f9f7f95a4a3e123ba4cc10c75b4acb4
Keith <keithrichardmcgowan@gmail.com>
2021-03-19 14:14:46 -0400

    Still need work on Iterations 4 and 5.  Not connecting to db.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
@@ -22,6 +22,44 @@ mongoose
   .then(() => {
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// Iteration 2
+const newRecipe = {
+  title: "Peanut Butter and Jelly",
+  level: "Easy Peasy",
+  ingredients: ["peanut butter", "jelly", "bread"],
+  cuisine: "American",
+  dishType: "snack",
+  image:
+    "https://www.verywellfit.com/thmb/cefWxbGige9RrMbHiwK38DhPFJo=/2002x2002/smart/filters:no_upscale()/peanut-butter-and-jelly-sandwich-b5f1d5cae12d46c68df3446df000826e.jpg",
+  duration: 5,
+  creator: "Everybody's mom",
+};
+
+Recipe.create(newRecipe)
+  .then((recipe) =>
+    console.log(`${recipe.title} has been added to the database!`)
+  )
+  .catch((err) => console.log(err));
+
+// Iteration 3
+Recipe.insertMany(data)
+  .then((data) => data.forEach((element) => console.log(element.title)))
+  .catch((err) => console.log(err));
+
+// Iteration 4
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then((recipe) =>
+    console.log(
+      `Success! ${recipe.title} now has a duration of ${recipe.duration} minutes!`
+    )
+  )
+  .catch((err) => console.log(err));
+
+// Iteration 5
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then((recipe) => console.log(`${recipe.title} has been removed!`))
+  .catch((err) => console.log(err));

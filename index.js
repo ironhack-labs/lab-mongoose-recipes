33c4f2c18b604805bd8fb86573865324a5d39e80
Natalia Canales <nataliacanales95@outlook.com>
2021-04-14 17:36:29 +0200

    Done

--- index.js
+++ index.js
@@ -9,19 +9,45 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+	.connect(MONGODB_URI, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+
+	})
+	.then(self => {
+		console.log(`Connected to the database: "${self.connection.name}"`);
+		// Before adding any documents to the database, let's delete all previous entries
+		return self.connection.dropDatabase();
+	})
+	.then(() => {
+		return Recipe.syncIndexes();
+	})
+	.then(() => {
+		return Recipe.create({ title: "Prueba", cuisine: "Prueba" });
+	})
+	.then(newRecipe => {
+		console.log("La nueva receta se llama ", newRecipe.title)
+		return Recipe.insertMany(data);
+	})
+	.then(recipes => {
+		recipes.forEach(recipe => {
+			console.log("Añadida la receta ", recipe.title);
+		});
+		return Recipe.findOneAndUpdate(
+			{ title: "Rigatoni alla Genovese" },
+			{ duration: 100 },
+			{ new: true }
+		);
+	})
+	.then(recipe => {
+		console.log("La duración de", recipe.title, "ahora es de", recipe.duration);
+		return Recipe.deleteOne({ title: "Carrot Cake" });
+	})
+	.then(() => {
+		console.log("La receta se ha borrado correctamente");
+		mongoose.connection.close();
+	})
+	.catch(error => {
+		console.error('Error connecting to the database', error);
+	});

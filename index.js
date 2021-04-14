3be30f309c97dfe9da444a60cade8bd42b84237c
Jaime Bastos-mendes armijo <jaimebastos-mendesarmijo@MacBook-Air-de-Jaime.local>
2021-04-14 21:53:08 +0200

    done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { create } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -20,8 +21,84 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return  Recipe.syncIndexes()
   })
+
+
+// ------- ITERACION 2 ------------
+
+
+  .then(() => {
+    
+  return Recipe
+    .create([{
+       title: "Pork Carnitas",
+       level: "UltraPro Chef",
+       ingredients: [
+          "3 1/2 pounds boneless pork shoulder, cut into large pieces",
+          "1 tablespoon freshly ground black pepper",
+          "1 tablespoon kosher salt, or more to taste",
+          "2 tablespoons vegetable oil",
+          "2 bay leaves",
+       ],
+        cuisine: "American",
+        dishType: "main_course",
+        image: "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
+        duration: 160,
+        creator: "Chef John"
+    }])
+
+    //console.log('La nueva receta es:', )
+    //.then(newRecipe => console.log('La nueva receta es:', newRecipe))
+    
+  })
+
+
+// ------- ITERACION 3 ------------
+  
+  .then((newRecipe) => {
+    //console.log(newRecipe)
+    return Recipe
+      .insertMany(data)
+      .then((allRecipes) => {
+        allRecipes.forEach((element) => {
+          console.log('New recipe created:', element.title)
+        });
+      })
+  })
+
+
+// ------- ITERACION 4 ------------
+
+.then((allIn) => {
+    //console.log('Recipe has been succesfully', allIn)
+
+    return Recipe
+    .findOneAndUpdate({title: "Rigattoni alla Genovese"}, {duration: 100})
+
+
+})
+
+
+
+// ------- ITERACION 5 ------------
+
+
+.then(() => {
+
+  return Recipe
+  .deleteOne({title: "Carrot Cake"})
+  //console.log('Deleted succesfully')
+})
+
+
+
+// ------- ITERACION 6 ------------
+
+mongoose.disconnect()
+
+
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

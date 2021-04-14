bc4906067fb65e7e0911ba6f4201428d73e19e3d
MrHose <josemiguel.uk@gmail.com>
2021-04-14 18:52:42 +0200

    Done

--- index.js
+++ index.js
@@ -21,6 +21,32 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.syncIndexes();
+  })
+  .then(() => {
+    return Recipe
+    .create({ title: 'Colacao', level: 'Easy Peasy', ingretidents: ['milk', 'colacao'],
+     cuisine: 'mediterranean', dishType: 'drink',
+     image: "https://ipmark.com/wp-content/uploads/2019/05/ColaCao_grumitos_5.jpg",
+     duration: 2, creator: 'MrHose'})
+    })
+  .then(newRecipe => {
+    console.log('Se ha añadido la receta:', newRecipe.title)
+    return Recipe.insertMany(data)
+  })
+  .then(moreRecipes => {
+    moreRecipes.forEach(all => console.log('Receta de:', all.title))
+    return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}, { new: true })
+  })
+  .then(fixRecipe => {
+    console.log('Se ha actualizado correctamente la receta:', fixRecipe.title)
+    return Recipe.findOne({ title: 'Carrot Cake'})
+  })
+  .then(remRecipe => {
+    return Recipe.deleteOne(remRecipe)
+  })
+  .then(info => {
+    console.log('Este es un mensaje informativo sobre la eliminacion de receta:', info)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

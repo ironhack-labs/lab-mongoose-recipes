3ee1b61cac721c8462b09fe72a8df41d2660ff1f
Thornnk <Thornnk1@gmail.com>
2021-04-14 19:24:40 +0200

    Done Iteration 3

--- index.js
+++ index.js
@@ -21,41 +21,39 @@ mongoose
 })
 .then(() => {
   // Run your code here, after you have insured that the connection was made
-  Recipe.create({
-    "title": "Asian Glazed Chicken Thighs",
-    "level": "Amateur Chef",
-    "ingredients": [
-      "1/2 cup rice vinegar",
-      "5 tablespoons honey",
-      "1/3 cup soy sauce (such as Silver Swan®)",
-      "1/4 cup Asian (toasted) sesame oil",
-      "3 tablespoons Asian chili garlic sauce",
-      "3 tablespoons minced garlic",
-      "salt to taste",
-      "8 skinless, boneless chicken thighs"
-    ],
-    "cuisine": "Asian",
-    "dishType": "main_course",
-    "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-    "duration": 40,
-    "creator": "Chef LePapu"
-  })
-  .then((result)=>{
-        console.log(result.title)
-  })
-  .catch((err)=>{
-      console.log(err)
-  })
-
-  // Recipe.insertMany({
-  //   data
+  // Recipe.create({
+  //   "title": "Asian Glazed Chicken Thighs",
+  //   "level": "Amateur Chef",
+  //   "ingredients": [
+  //     "1/2 cup rice vinegar",
+  //     "5 tablespoons honey",
+  //     "1/3 cup soy sauce (such as Silver Swan®)",
+  //     "1/4 cup Asian (toasted) sesame oil",
+  //     "3 tablespoons Asian chili garlic sauce",
+  //     "3 tablespoons minced garlic",
+  //     "salt to taste",
+  //     "8 skinless, boneless chicken thighs"
+  //   ],
+  //   "cuisine": "Asian",
+  //   "dishType": "main_course",
+  //   "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  //   "duration": 40,
+  //   "creator": "Chef LePapu"
   // })
   // .then((result)=>{
-  //   console.log(result.title)
+  //       console.log(result.title)
   // })
   // .catch((err)=>{
-  // console.log(err)
+  //     console.log(err)
   // })
+
+  Recipe.insertMany(data)
+  .then((result)=>{
+    result.forEach((recipe)=>{
+      console.log(recipe.title)
+    })
+  })
+
 })
 .catch(error => {
   console.error('Error connecting to the database', error);

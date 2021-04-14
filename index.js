5ddcd8f21347ef966eb8dca0cad8c601c043a7fb
Juanjo Prada <juanjopradaalfaro@gmail.com>
2021-04-14 19:31:43 +0200

    done

--- index.js
+++ index.js
@@ -20,7 +20,60 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.syncIndexes()
+  })
+  // .then(() => {
+  //   // Run your code here, after you have insured that the connection was made
+  //   return Recipe.create({
+
+  //       title: "Asian Glazed Chicken Thighs",
+  //       level: "Amateur Chef",
+  //       ingredients: [
+  //         "1/2 cup rice vinegar",
+  //         "5 tablespoons honey",
+  //         "1/3 cup soy sauce (such as Silver Swan®)",
+  //         "1/4 cup Asian (toasted) sesame oil",
+  //         "3 tablespoons Asian chili garlic sauce",
+  //         "3 tablespoons minced garlic",
+  //         "salt to taste",
+  //         "8 skinless, boneless chicken thighs"
+  //       ],
+  //       cuisine: "Asian",
+  //       dishType: "main_course",
+  //       image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  //       duration: 40,
+  //       creator: "Chef LePapu"
+  //   })
+  // })
+  // .then(theRecipe => {
+  //   console.log('Hemos incorporado la siguiente receta', theRecipe.title)
+  //   return Recipe.find()
+  // })
+  
+  .then(() => {
+    return Recipe.insertMany(data)
+    return Recipe.syncIndexes()
+  })
+  .then(allRecipes => {
+    allRecipes.forEach(recipes => {
+      console.log('Se ha añadido la receta', recipes.title)
+    })
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate ( { title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+    return Recipe.syncIndexes()
+  })
+  .then(updatedRecipe => {
+    console.log(`Actualizada la siguiente receta, ${updatedRecipe.title}, que ahora tiene una duración de ${updatedRecipe.duration}`)
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" }, {new: true})
+    return Recipe.syncIndexes()
+  })
+  .then(deletedRecipe => {
+    console.log('Se ha eliminado la receta "Carrot Cake"')
+    mongoose.connection.close()
+    console.log('conexión con BBDD cerrada')
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

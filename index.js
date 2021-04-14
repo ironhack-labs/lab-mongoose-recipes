b836415a732f095fc6176aaf77867faf6ed77209
Achraf <Achrafarazzouk@gmail.com>
2021-04-14 18:29:39 +0200

    done

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+//const myData = JSON.parse(data)
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    returnOriginal: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,8 +26,52 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.syncIndexes()
+  })
+  .then(() => {
+    return Recipe.create({ title: 'Lasagna', level: 'UltraPro Chef', ingredients: ['Tomatoes', 'meat', 'Bechamel'], cuisine: 'Italian', dishType: 'main_course', duration: 4, creator: 'Me' })
+  })
+  .then(recipe => {
+    console.log('El titulo de la receta es: ', recipe.title)
+    return Recipe.find()
+  })
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then(recipes => {
+    recipes.forEach(elm => console.log('El titulo de la receta es: ', elm.title))
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
+  })
+  .then(() => {
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
+/**
+ * Cat.collection.drop()
+  .then(() => console.log("Colección vaciada"))
+  .catch(err => console.log("Hubo un error!", err))
+
+
+Cat.create({ name: 'Michifú' })
+  .then(theCat => {
+      console.log('El método .create() retorna el objeto de la BBDD:', theCat, 'El gato se llama', theCat.name)
+      return Cat.find()
+  })
+  .then(allTheCats => {
+      console.log("El método .find() retorna un Array", allTheCats)
+      allTheCats.forEach(cat => console.log("Hay un gato llamado", cat.name))
+      return Cat.create([{ name: 'Belcebú' }, { name: 'Garfield' }])
+  })
+  .then(allNewCats => console.log("Los gatos nuevos son:", allNewCats))
+  .catch(err => console.log("Hubo un error!", err))
+
+ */

822b8622c134d799e3eee1d44c2676d0e7286579
Thornnk <Thornnk1@gmail.com>
2021-04-14 19:17:46 +0200

    Done Iteration 1

--- index.js
+++ index.js
@@ -9,19 +9,54 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+.connect(MONGODB_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(self => {
+  console.log(`Connected to the database: "${self.connection.name}"`);
+  // Before adding any documents to the database, let's delete all previous entries
+  return self.connection.dropDatabase();
+})
+.then(() => {
+  // Run your code here, after you have insured that the connection was made
+  Recipe.create({
+    "title": "Asian Glazed Chicken Thighs",
+    "level": "Amateur Chef",
+    "ingredients": [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+    "cuisine": "Asian",
+    "dishType": "main_course",
+    "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    "duration": 40,
+    "creator": "Chef LePapu"
   })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+  .then((result)=>{
+        console.log(result.title)
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .catch((err)=>{
+      console.log(err)
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  // Recipe.insertMany({
+  //   data
+  // })
+  // .then((result)=>{
+  //   console.log(result.title)
+  // })
+  // .catch((err)=>{
+  // console.log(err)
+  // })
+})
+.catch(error => {
+  console.error('Error connecting to the database', error);
+});

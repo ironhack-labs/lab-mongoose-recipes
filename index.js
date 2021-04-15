2ee0b5b41371b1204d2ba8a6fddbd52d8322d175
Sisco Falcó <siscofalcop@gmail.com>
2021-04-15 15:22:42 +0200

    iterations 1 and 2 done

--- index.js
+++ index.js
@@ -20,8 +20,26 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  const recipe1 = {
+    title: "Pizza Ironhack",
+    level: "Easy Peasy",
+    ingredients: ["Flour", "Tomato", "Cheese", "Beacon", "Oregano", "Basil"],
+    cuisine: "Italian",
+    dishType: "main_course",
+    image: "https://www.currizzas.co.nz/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/h/cheese_and_bacon_1.png",
+    duration: 40,
+    creator: "Ironhack"
+  }
+
+  Recipe.create(recipe1)
+  .then(recipes => {
+    console.log(`Recipe created: ${recipe.title}`)
+  })
+  .catch(error => console.error(error));
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: {type: String, required: true, unique: true},
+  level: {type: String, enun: ["Easy Peasy", "Amateur Chef", "UltraPro chef"]},
+  ingredients: [{type: String}],
+  cuisine: {type: String, required: true},
+  dishType: {type: String, enun: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]},
+  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: {type: Number, min: 0},
+  creator: {type: String},
+  created: {type: Date, default: Date.now},
+})
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 

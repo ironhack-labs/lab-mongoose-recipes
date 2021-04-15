3fec4e903d262acf6e108938b7916d04fb7aa00a
Andrea Sforza <andrearobertosforza@gmail.com>
2021-04-15 16:03:27 +0200

    Completed lab

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
@@ -21,6 +22,26 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.insertMany(data, (err, recipesList) => {
+      if(err !== null)
+        return console.error(`Error on instertMany: ${err}`);
+      
+      recipesList.map(recipe => console.log(recipe.title));
+
+      Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}, (err) => {
+        if(err !== null)
+          return console.error(`Error on findOneAndUpdate: ${err}`);
+        
+        console.log(`Update successful!`);
+
+        Recipe.deleteOne({title: 'Carrot Cake'}, (err) => {
+          if(err !== null)
+            return console.error(`Error on deleteOne: ${err}`);
+
+          console.log(`Deletion successful!`);
+        })
+      })
+    })    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

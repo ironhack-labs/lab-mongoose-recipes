055ad2d60e1eb91b97911e2ccfe1254ae71bd000
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-04-15 21:34:35 +0200

    Nested then refactored

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
+
   .then(() => {
     const recipe1 = {
       title: "Lasagna",
@@ -28,38 +29,31 @@ mongoose
       dishType: "main_course",
       duration: 70,
       creator: "Jesi"
-    }
-    Recipe.create(recipe1)
-      .then(recipe => {
-        console.log(`Recipe created: ${recipe.title}`)
-      })
-    Recipe.insertMany(data)
-      .then(recipes => {
-        console.log(`Recipes creted from data`)
-        Recipe.find({}, {
-            title: 1,
-            _id: 0
-          })
-          .then(recipes => console.log(recipes))
-          .catch(error => console.error(error));
-        Recipe.findOneAndUpdate({
-            title: "Rigatoni alla Genovese"
-          }, {
-            $set: {
-              duration: 100
-            }
-          })
-          .then(recipes => console.log(`${recipes.title} updated succesfully`))
-          .catch(error => console.error(error))
-
-        Recipe.deleteOne({
-            title: "Carrot Cake"
-          })
-          .then(recipes => console.log(`Recipe deleted succesfully`))
-          .catch(error => console.error(error))
-      })
+    };
+    return Recipe.create(recipe1)
   })
-
+  .then(recipe => {
+      console.log(`Recipe created: ${recipe.title}`)
+      return Recipe.insertMany(data)
+    })
+  .then( () => {
+    console.log(`Recipes created from data`)
+    return Recipe.find({}, { title: 1, _id: 0}) 
+    })
+    .then(recipes => {
+      console.log(recipes)
+      return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {$set: {duration: 100}}, {new: true})
+    })  
+    .then(recipes => { 
+      console.log(`${recipes.title} updated succesfully`)
+      return Recipe.deleteOne({ title: "Carrot Cake" })
+    })
+    .then(recipes => {
+        console.log(`Recipe deleted succesfully`);
+        mongoose.connection.close()
+          .then( () => console.log("Connection closed"))
+          .catch(error => console.error(error))
+    })
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error('Error connecting to the database');
   });

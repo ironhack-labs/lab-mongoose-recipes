c41dd524841180fc9a2e07a893296d11b55adbea
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-04-15 12:11:49 -0400

    Done

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,8 +20,48 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    /* Run your code here, after you have insured that the connection was made 
+      Recipe.create({
+      title: "ceviche",
+      level: "UltraPro Chef",
+      ingredients: [
+        "1 kg fish",
+        "6 lemons",
+        "two purple onions",
+        "yellow pepper",
+      ],
+      cuisime: "Peru",
+      dishType: "main_course",
+      duration: 30,
+      creator: "Junior",
+      created: "15 abril",
+    })
+      .then((result) => {
+        console.log(result.title);
+      })
+      .catch((err) => {
+        console.log(err);
+      }); */
+
+    Recipe.insertMany(data).then((result) => {
+      result.forEach((recipe) => {
+        console.log(recipe.title);
+      });
+
+      Recipe.findOneAndUpdate(
+        { title: "Rigatoni alla Genovese" },
+        { duration: 100 }
+      ).then((result) => {
+        console.log("Change made"); /* , result) */
+        Recipe.deleteOne({ title: "Carrot Cake" }).then((result) => {
+          console.log(result);
+          mongoose.connection.close().then(() => {
+            console.log("Closed");
+          });
+        });
+      });
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String, required: true, unique: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: { type: [String] },
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now() },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
+
+
+
+ const recipePeru = {
+  title: "ceviche",
+  level: "UltraPro Chef",
+  ingredients: ["1 kg fish", "6 lemons", "two purple onions", "yellow pepper"],
+  cuisime: "Peru",
+  dishType: "main_course",
+  duration: 30,
+  creator: "Junior",
+  created: "15 abril",
+}; 

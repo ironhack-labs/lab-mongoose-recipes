cd68ba1bee651c5c0fd38ed5f93bda5630e1ede3
Sam Alafar <alafar.hesam@gmail.com>
2021-04-15 18:05:08 +0200

    done iteration 3, 4, 5

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 const mongoose = require("mongoose");
-
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
 const data = require("./data");
-
-const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
-
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((self) => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database", error);
-  });
-
 const recipe1 = {
   title: "Asian Glazed Chicken Thighs",
   level: "Amateur Chef",
@@ -45,11 +22,53 @@ const recipe1 = {
   duration: 40,
   creator: "Chef LePapu",
 };
-
-Recipe.create(recipe1)
-.then(recipe => {
-  console.log(`Recipe name: ${recipe1.title}`)
-})
-.catch(error => {
-  console.error(error)
-})
+const newDuration = 100;
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+// Connection to the database "recipe-app"
+mongoose
+  .connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((self) => {
+    console.log(`Connected to the database: "${self.connection.name}"`);
+    // Before adding any documents to the database, let's delete all previous entries
+    return self.connection.dropDatabase();
+  })
+  .then(() => {
+    // Iteration 2
+    Recipe.create(recipe1)
+      .then((recipe) => {
+        console.log(`Recipe name: ${recipe1.title}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    //Iteration 3
+    Recipe.insertMany(data)
+      .then((data) => {
+        data.forEach((data) => {
+          console.log(`Recipe name is: ${data.title}`);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    //Iteration 4
+    Recipe.findOneAndUpdate(
+      { _id: "60784ba40fb1d42ee8b2aa10" },
+      { $set: { duration: 100 } },
+      { new: true }
+    )
+      .then((recipe) => console.log("Success!"))
+      .catch((error) => console.error(error));
+    //Iteration 5
+    Recipe.deleteOne({ _id: "60784ba40fb1d42ee8b2aa0f" })
+      .then((recipe) => console.log("Success"))
+      .catch((error) => console.error(error));
+    //Iteration 6
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  });

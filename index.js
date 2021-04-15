82f86a8f1d6d20f173cbdde353cdc6dbe29bd425
Sisco Falcó <siscofalcop@gmail.com>
2021-04-15 16:07:04 +0200

    iteration 4 done

--- index.js
+++ index.js
@@ -17,40 +17,37 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-
+    // return self.connection.dropDatabase();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
 
-  Recipe.insertMany(data)
-  .then(recipes => {
-    console.log(`Recipes added: ${recipes.length}`)
-  })
-  .catch(error => console.error(error));
-
-  // const recipe1 = {
-  //   title: "Pizza Ironhack",
-  //   level: "Easy Peasy",
-  //   ingredients: ["Flour", "Tomato", "Cheese", "Beacon", "Oregano", "Basil"],
-  //   cuisine: "Italian",
-  //   dishType: "main_course",
-  //   image: "https://www.currizzas.co.nz/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/h/cheese_and_bacon_1.png",
-  //   duration: 40,
-  //   creator: "Ironhack"
-  // }
-
-  // Recipe.create(recipe1)
+  // Recipe.insertMany(data)
   // .then(recipes => {
-  //   console.log(`Recipe created: ${recipe.title}`)
+  //   console.log(`Recipes added: ${recipes.length}`)
   // })
   // .catch(error => console.error(error));
 
+const recipe1 = {
+  title: "Pizza Ironhack",
+  level: "Easy Peasy",
+  ingredients: ["Flour", "Tomato", "Cheese", "Beacon", "Oregano", "Basil"],
+  cuisine: "Italian",
+  dishType: "main_course",
+  image: "https://www.currizzas.co.nz/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/h/cheese_and_bacon_1.png",
+  duration: 40,
+  creator: "Ironhack"
+}
+
+// Recipe.create(recipe1)
+// .then(recipes => {
+//   console.log(`Recipe created: ${recipes.title}`)
+// })
+// .catch(error => console.error(error));
+
 const newDuration = 100;
 
-Recipe.findOneAndUpdate({ _id: "607844c3e861c21c29ed5b2c"}, {$set: {duration: newDuration}}, {new: true})
+Recipe.findOneAndUpdate({ _id: "607846234241611cc7f218c6"}, {$set: {duration: newDuration}}, {new: true})
 .then(updatedRecipe => {console.log(`Success: ${updatedRecipe}`)})
 .catch(error => console.error(error));

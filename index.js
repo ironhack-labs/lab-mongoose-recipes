58fedae9d52ee1f53eff42887e629e0799df730b
Sisco Falcó <siscofalcop@gmail.com>
2021-04-15 15:32:47 +0200

    iteration 3 done

--- index.js
+++ index.js
@@ -20,26 +20,32 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-  
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
 
-  const recipe1 = {
-    title: "Pizza Ironhack",
-    level: "Easy Peasy",
-    ingredients: ["Flour", "Tomato", "Cheese", "Beacon", "Oregano", "Basil"],
-    cuisine: "Italian",
-    dishType: "main_course",
-    image: "https://www.currizzas.co.nz/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/h/cheese_and_bacon_1.png",
-    duration: 40,
-    creator: "Ironhack"
-  }
-
-  Recipe.create(recipe1)
+  Recipe.insertMany(data)
   .then(recipes => {
-    console.log(`Recipe created: ${recipe.title}`)
+    console.log(`Recipes added: ${recipes.length}`)
   })
   .catch(error => console.error(error));
+
+  // const recipe1 = {
+  //   title: "Pizza Ironhack",
+  //   level: "Easy Peasy",
+  //   ingredients: ["Flour", "Tomato", "Cheese", "Beacon", "Oregano", "Basil"],
+  //   cuisine: "Italian",
+  //   dishType: "main_course",
+  //   image: "https://www.currizzas.co.nz/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/h/cheese_and_bacon_1.png",
+  //   duration: 40,
+  //   creator: "Ironhack"
+  // }
+
+  // Recipe.create(recipe1)
+  // .then(recipes => {
+  //   console.log(`Recipe created: ${recipe.title}`)
+  // })
+  // .catch(error => console.error(error));
 

c21f009514cd0dffbf7c5dcb901a26cf77692e5f
Ruben Karlsson <neburgordon@gmail.com>
2021-04-16 12:48:16 +0200

    done

--- index.js
+++ index.js
@@ -7,21 +7,35 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
+const connectToDb = async () => {
+
+  try {
+    const Connection = await mongoose.connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    console.log(`Connected to the database: "${Connection.connection.name}"`);
+
+    await Connection.connection.dropDatabase();
+
+    // iteration 2
+    // Recipe.create(data[0])
+    // iteration 3
+    await Recipe.create(data);
+    // iteration 4
+    await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+    );
+    await Recipe.deleteOne({ title: "Carrot Cake" });
+
+    await Connection.disconnect();
+  } catch (error) {
     console.error('Error connecting to the database', error);
-  });
+  }
+}
+
+connectToDb();

33ad797c15fdf7caf3952871bff3a65856b11241
Marcia Faria <mfndnc@gmail.com>
2021-04-16 08:32:49 +0200

    prep trying Promise.all

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require("./models/Recipe.model");
+const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require("./data");
+const data = require('./data');
 
-const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -18,6 +18,54 @@ mongoose
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
+  })
+  .then(() => {
+    // testing with Promise.all()
+
+    Promise.all([
+      Recipe.create(data[0])
+        .then((recipe) => {
+          console.log(recipe.title);
+          return 'create done';
+        })
+        .catch((err) => {
+          console.log(err);
+          return 'create error';
+        }),
+      Recipe.insertMany(data)
+        .then((recipes) => {
+          recipes.forEach((recipe) => console.log(recipe.title));
+          return 'insertMany done';
+        })
+        .catch((err) => {
+          console.log(err);
+          return 'insertMany error';
+        }),
+      Recipe.findOneAndUpdate(
+        { title: 'Rigatoni alla Genovese' },
+        { duration: 100 },
+        { useFindAndModify: false }
+      )
+        .then(() => {
+          console.log('findOneAndUpdate sucess');
+          return 'findOneAndUpdate done';
+        })
+        .catch((err) => {
+          console.log(err);
+          return 'findOneAndUpdate error';
+        }),
+      Recipe.deleteOne({ title: 'Carrot Cake' })
+        .then(() => {
+          console.log('deleteOne sucess');
+          return 'deleteOne done';
+        })
+        .catch((err) => {
+          console.log(err);
+          return 'deleteOne error';
+        }),
+    ]).then(() => {
+      mongoose.connection.close();
+    });
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -36,16 +84,16 @@ mongoose
         recipes.forEach((recipe) => console.log(recipe.title));
         // iteraction 4
         Recipe.findOneAndUpdate(
-          { title: "Rigatoni alla Genovese" },
+          { title: 'Rigatoni alla Genovese' },
           { duration: 100 },
           { useFindAndModify: false }
         )
           .then((_) => {
-            console.log("findOneAndUpdate sucess");
+            console.log('findOneAndUpdate sucess');
             // iteraction 5
-            Recipe.deleteOne({ title: "Carrot Cake" })
+            Recipe.deleteOne({ title: 'Carrot Cake' })
               .then((_) => {
-                console.log("deleteOne sucess");
+                console.log('deleteOne sucess');
                 // iteraction 6
                 mongoose.connection.close();
               })
@@ -62,5 +110,5 @@ mongoose
       });
   })
   .catch((error) => {
-    console.error("Error connecting to the database", error);
+    console.error('Error connecting to the database', error);
   });

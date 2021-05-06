d1caac0c82c4db88e59d5a023ab08851d1b1516d
hisamparker <hisamparker@gmail.com>
2021-05-06 09:45:05 +0200

    tried with async await

--- index.js
+++ index.js
@@ -8,20 +8,25 @@ const { model } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+mongoose.set('useFindAndModify', false);
+
+// Connection to the database "recipe-app"
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany();
   })
-  .then(() => {
-      Recipe.create({
+  //trying async
+  .then(async () => {
+    try {
+      await  Recipe.create({
         title: 'BLT',
         level: "Amateur Chef",
         ingredients: ["sourdough bread", "bacon", "tomato", "mayo", "crisp lettuce", "salt", "pepper"],
@@ -30,23 +35,26 @@ mongoose
         image:
         "https://static01.nyt.com/images/2020/08/18/dining/27Diaryrex4/27Diaryrex4-articleLarge.jpg",
         duration: 10,
-        creator: "pops",
-    });
-  }) 
-  .then(() => {
-    Recipe.create(data)
-    .then(recipe => console.log('The recipe is saved and its title is: ', recipe))
-    .catch(error => console.log('An error happened while saving a new recipe:', error));
+        creator: "pops"
+      });
+      const allRecipes = await Recipe.insertMany(data);
+      console.log("Recipes added");
+      allRecipes.forEach(recipe => {
+        console.log(recipe.title);
+      });
+      await Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, {new: true});
+      console.log("duration updated");
+      await Recipe.deleteOne({ title: "Carrot Cake" });
+      console.log("Recipe deleted");
+      // close connection
+      process.exit();
+    } catch (e) {
+      console.log(`oops, we got this error: ${e}`);
+    }
   })
-  .then(() => {
-  Recipe.find({}, 'title')
-    .then(recipe => console.log(recipe))
-    .catch(e => console.log(`can't title ${e}`));
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
   
-  Recipe.find({}, 'title')
-  .then(recipe => console.log(recipe))
-  .catch(e => console.log(`oops ${e}`));
+  
+ 

c82a14e4909495e10e9b4043ba6f7adf13f6d909
danpinon <danielpformoso@gmail.com>
2021-05-06 19:30:10 -0500

    recipe mongoose lab done

--- index.js
+++ index.js
@@ -17,11 +17,59 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // const data = {
+    //   title: "Asian Glazed Chicken Thighs",
+    //   level: "Amateur Chef",
+    //   ingredients: [
+    //     "1/2 cup rice vinegar",
+    //     "5 tablespoons honey",
+    //     "1/3 cup soy sauce (such as Silver Swan®)",
+    //     "1/4 cup Asian (toasted) sesame oil",
+    //     "3 tablespoons Asian chili garlic sauce",
+    //     "3 tablespoons minced garlic",
+    //     "salt to taste",
+    //     "8 skinless, boneless chicken thighs"
+    //   ],
+    //   cuisine: "Asian",
+    //   dishType: "main_course",
+    //   image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //   duration: 40,
+    //   creator: "Chef LePapu"
+    // }
+  
+    // Recipe.create(data, (error, newRecipe) => {
+    //   if(error){
+    //     console.log(error)
+    //   }
+    //   console.log(newRecipe)
+    // })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+    // Recipe.insertMany(data, (error, newRecipes) => {
+    //   newRecipes.forEach(recipe => console.log(recipe.title))
+    // })
+
+Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, { duration: 100})
+.then(() => {
+  console.log('findOneSuccesful')
+})
+
+Recipe.deleteOne({title: 'Carrot Cake'})
+.then(() => console.log('deleteOne Succesful'))
+.then(() => {
+  console.log('disconected')
+  mongoose.connection.close();
+})
+
+// function rejected(result) {
+//   console.error(result);
+// }
+

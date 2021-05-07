1f2d153c13d52904a8c4bef95d30bffbba5e5542
fereber1294 <freyesber@gmail.com>
2021-05-06 19:20:15 -0500

    lab done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { model } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -17,7 +18,7 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -25,3 +26,34 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  // Recipe.create(data, (err, newRec) => {
+  //   if(err){
+  //     console.log(err);
+  //   }
+  //   console.log(newRec);
+  // })
+Recipe.insertMany(data).then((value) => {
+  console.log(value);
+}).catch(err => {
+  console.log(err);
+});
+
+Recipe.updateOne({
+  title: "Rigatoni alla Genovese"
+}, {
+  duration: 100
+})
+.then((catUpdated) => {
+  console.log(catUpdated)
+})
+.catch(e => console.log(e))
+
+Recipe.deleteOne({
+  title: "Rigatoni alla Genovese"
+})
+.then((catDeleted) => {
+  console.log(catDeleted);
+  mongoose.connection.close();
+})

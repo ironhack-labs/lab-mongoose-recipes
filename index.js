2e937a7908097b56dc893c987f40da8eaa92422d
Eva Evike <evikeprog@gmail.com>
2021-05-07 12:42:01 +0200

    done til iteration4

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
+//console.log(data);
+const { model } = require("./models/Recipe.model");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
-    useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useCreateIndex: true,
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then(async () => {
+    //iteration 2 - Create a recipe OK
+     Recipe.create({
+      title: "Magret de canard",
+      level: "Amateur Chef",
+      ingredients: ["duck", " haricots verts"],
+      cuisine: "french",
+      dishType: "main_course",
+      duration: 10,
+      creator: "eva",
+    });
+     //iteration 3 - Insert multiple recipes Ok 
+     //(on line6 from index.js heres the "link" on const data = require("./data") is taking all the array data of recipes taken from data.json file)
+     Recipe.insertMany(data);
+
+  }); 
+   // iteration 4 - Update Rigatoni alla Genovese duration field and set it to 100   
+    Recipe.findOneAndUpdate( 
+     {title: "Rigatoni alla Genovese", 
+       duration: 100}
+   );
+
+

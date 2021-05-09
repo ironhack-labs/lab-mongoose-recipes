bba8116f561c1e9c151208a3b084e56aeacdff02
Melisa KMNV <melisakmnv@gmail.com>
2021-05-09 23:27:54 +0200

    done, except 4-5

--- index.js
+++ index.js
@@ -19,9 +19,57 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
+  // .then(() => { // Iteration 2 // 
+  //   Recipe.create({
+  //     "title": "Asian Glazed Chicken Thighs",
+  //     "level": "Amateur Chef",
+  //     "ingredients": [
+  //       "1/2 cup rice vinegar",
+  //       "5 tablespoons honey",
+  //       "1/3 cup soy sauce (such as Silver Swan®)",
+  //       "1/4 cup Asian (toasted) sesame oil",
+  //       "3 tablespoons Asian chili garlic sauce",
+  //       "3 tablespoons minced garlic",
+  //       "salt to taste",
+  //       "8 skinless, boneless chicken thighs"
+  //     ],
+  //     "cuisine": "Asian",
+  //     "dishType": "main_course",
+  //     "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  //     "duration": 40,
+  //     "creator": "Chef LePapu"
+  //   }) 
+  //   .then((newrecipe) => {
+  //     console.log(newrecipe.title)
+  //   })
+  // })
+
+  .then(() => { // Iteration 3 // 
+   Recipe.insertMany(data)
+   .then((dbSuccess) => {
+    console.log("data has been added on DB")
+   })
+  })
+
+  // .then(() => { // Iteration 4 //
+  //   Recipe.updateOne({title:"Rigatoni alla Genovese"}, {duration: 100})
+  //   .then((dbSuccess) => {
+  //     console.log("updated is done...")
+  //   })
+  // })
+
+  // .then(() => { // Iteration 5 // 
+  //   Recipe.deleteOne({title:"Carrot Cake"})
+  //   .then((dbSuccess) => {
+  //     console.log("Carrot Cake is out of order")
+  //   })
+  // })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    mongoose.connection.close();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

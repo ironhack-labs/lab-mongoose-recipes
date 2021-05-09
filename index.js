8efb5dea33c27af330ab883f0aab9f636c23534a
ZsokaM <zsokamontvai@gmail.com>
2021-05-09 18:28:44 +0200

    done but connection.close is acting up

--- index.js
+++ index.js
@@ -20,8 +20,39 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    //ITERATION 2 - adding 1 new recipe to database
+    const newRecipe = { 
+      title: 'Lemon and Garlic Salmon', 
+      level: 'Easy Peasy', 
+      ingredients: ['salmon', 'olive oil', 'garlic', 'Italian herb mix', 'lemon', 'salt and pepper'],
+      cuisine: 'Mediterrenean',
+      dishType: 'main_course',
+      duration: 25,
+      creator: 'Zsoka', 
+     };
+		 
+		Recipe.create(newRecipe)
+		  .then(newRecipe => console.log(`The ${newRecipe.title} is saved`))
+		  .catch(error => console.log('An error happened while saving the dish:', error));
+		
+    //ITERATION 3 - inserting multiple recipes
+    Recipe.insertMany(data)
+      .then(console.log("Recipes inserted succesfully"))
+		  .catch(error => console.log('An error happened while saving the dishes:', error));
+    
+    //ITERATION 4 - update recipe
+    Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration:100}, {new: true})
+     .then(updatedRecipe => console.log(`${updatedRecipe.title}'s duration was updated`))
+     .catch(error => console.log('An error happened while updating the recipe:', error));
+    
+    //ITERATION 5 - remove a recipe
+    Recipe.deleteOne({ title: 'Carrot Cake' })
+      .then(console.log("Recipe deleted"))
+      .catch(error => console.log('An error happened while deleting a recipe:', error));
   })
+  .then(mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  

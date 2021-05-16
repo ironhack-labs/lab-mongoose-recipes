5c11c1a415c47460d1b2a190ba019848a516d812
Irene Suarez <irenesuarezalvarez@gmail.com>
2021-05-16 22:15:36 +0200

    Make iteration 1 to 5

--- index.js
+++ index.js
@@ -21,7 +21,48 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+  /*   const mydata = {
+      "title": "Asian Chicken",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup rice vinegar",
+        "1/3 cup soy sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste"
+      ],
+      "cuisine": "Asian",
+      "dishType": "main_course",
+      "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      "duration": 40,
+      "creator": "Chef LePapu"
+    }
+
+    Recipe.create(mydata)
+    .then(recipe => console.log('The recipe title is: ', recipe.title))
+    .catch(error => console.log('An error happened while saving a new user:', error));
+ */
+    Recipe.insertMany(data)
+      .then(recipes => {
+      recipes.forEach(recipe => {
+        console.log('The recipe title is: ', recipe.title)
+      });
+    })
+    .catch(error => console.log('An error happened while saving a new user:', error));
+ 
+    Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+      .then(data => console.log('Rigatoni alla Genovese was successfully updated!'))
+      .catch(err => console.log(err))
+
+    Recipe.deleteOne({ title: 'Carrot Cake' })
+      .then(data => console.log('Carrot cake was deleted successfully!'))
+      .catch(err => console.log('Oh, no! The document could not be deleted! The error: ', err))
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
+

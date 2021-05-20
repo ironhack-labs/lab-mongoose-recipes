07985bc2818dfb8ac8d53365770636153653b663
Mohamud Issa <mohamud.issa@ymail.com>
2021-05-20 18:11:02 +0200

    done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -21,7 +20,33 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    // add one recipe
+    const data = { title: 'Pasta with cheese', level: 'Easy Peasy', ingredients:['tomato', 'meat'], cuisine: 'indian',dishType:'main_course', duration: 1, creator: 'M. Issa'};
+    Recipe.create(data)
+    .then(recipe => console.log('The recipe is saved and its value is: ', recipe.title))
+    .catch(error => console.log('An error happened while saving recipe:', error));
+
+    // add many recipies to the db
+    Recipe.insertMany(data)
+    .then(recipe => console.log('The recipies have been saved', recipe))
+    .catch(error => console.log('An error happened while saving recipies :', error));
+    
+    // update one recipe
+    Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {$set: {duration: 100 }}, function(err, doc){
+      if(err){
+        console.log("Something wrong when updating data!");
+      }
+      console.log(doc)
+    })
+
+    // Delete one recipe
+    Recipe.deleteOne({title: 'Carrot Cake'})
+    .then(() => console.log("data deleted"))
+    .catch(err => console.log(err))
+    
   })
+  // .then(() => mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

464b96534ef51b52aa21b7c72016bb7dfd9a5dcb
Mohamud Issa <mohamud.issa@ymail.com>
2021-05-20 21:06:26 +0200

    separated my code into multiple then blocks

--- index.js
+++ index.js
@@ -16,35 +16,37 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
     
     // add one recipe
-    const data = { title: 'Pasta with cheese', level: 'Easy Peasy', ingredients:['tomato', 'meat'], cuisine: 'indian',dishType:'main_course', duration: 1, creator: 'M. Issa'};
-    Recipe.create(data)
+    const data1 = { title: 'Pasta with cheese', level: 'Easy Peasy', ingredients:['tomato', 'meat'], cuisine: 'indian',dishType:'main_course', duration: 1, creator: 'M. Issa'};
+    Recipe.create(data1)
     .then(recipe => console.log('The recipe is saved and its value is: ', recipe.title))
     .catch(error => console.log('An error happened while saving recipe:', error));
-
+  })
+  .then(() => {
     // add many recipies to the db
     Recipe.insertMany(data)
     .then(recipe => console.log('The recipies have been saved', recipe))
     .catch(error => console.log('An error happened while saving recipies :', error));
-    
+  })
+  .then(()=> {
     // update one recipe
-    Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {$set: {duration: 100 }}, function(err, doc){
+    Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, {duration: 100 }, function(err, doc){
       if(err){
         console.log("Something wrong when updating data!");
       }
-      console.log(doc)
+      console.log('=>',doc)
     })
-
+  })
+  .then(()=> {  
     // Delete one recipe
     Recipe.deleteOne({title: 'Carrot Cake'})
     .then(() => console.log("data deleted"))
     .catch(err => console.log(err))
-    
   })
   // .then(() => mongoose.connection.close())
   .catch(error => {

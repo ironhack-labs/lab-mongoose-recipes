3d798edca687651d60a2bd1adef95178ba5902ca
Mauricio Munoz <mauripedroza89@gmail.com>
2021-06-08 18:56:20 -0500

    almost done

--- index.js
+++ index.js
@@ -20,8 +20,50 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+    
+    console.log("Orange and Milk-Braised Pork Carnitas")
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  function createRecipe (data){
+      Recipe.create(data)
+      .then(recipeCreated => console.log(`Recipe created: ${recipeCreated}`)) 
+      .catch(error=>console.log("Recipe not found",error))
+  }
+//Iteracion 3
+Recipe.insertMany(data)
+.then(recipeCreated => console.log(`Recipe created: ${recipeCreated}`)) 
+.catch(error=>console.log("Recipe not found",error))
+
+//Iteracion 4
+
+
+
+  let recipe = {
+    //Iteracion 2
+      
+    title: "Orange and Milk-Braised Pork Carnitas",
+    level: "UltraPro Chef",
+    ingredients: [
+      "3 1/2 pounds boneless pork shoulder, cut into large pieces",
+      "1 tablespoon freshly ground black pepper",
+      "1 tablespoon kosher salt, or more to taste",
+      "2 tablespoons vegetable oil",
+      "2 bay leaves",
+      "2 teaspoons ground cumin",
+      "1 teaspoon dried oregano",
+      "1/4 teaspoon cayenne pepper",
+      "1 orange, juiced and zested"
+    ],
+    cuisine: "American",
+    dishType: "main_course",
+    image: "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
+    duration: 160,
+    creator: "Chef John"
+  
+}
+
+createRecipe(recipe)
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,7 +3,45 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-});
+
+  //Iteracion 1
+  title: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  level:  {
+    type:String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: [String],
+  cuisine: {
+    type: String, 
+    required: true},
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    num:0
+  },
+  creator: {
+    type:String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+
+},
+
+{timestamps:true}
+);
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 

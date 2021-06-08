1d8b874ea10f229a2636c6deaf89f781f29ad836
Cesar Carreras <cesar.carreras95@gmail.com>
2021-06-08 18:38:16 -0500

    Done with bonus

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+   //return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -25,3 +25,103 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+//ITERATION 2
+  function createRecipe(newRecipe){
+    Recipe.create(newRecipe)
+    .then(createRecipe => {
+      console.log(`A new recipe has been added: ${createRecipe}`)
+      mongoose.connection.close();
+    })
+    .catch(error => console.log('Something went wrong', error))
+}
+
+let newRecipe = {
+    "title": "Tacos",
+    "level": "Easy Peasy",
+    "ingredients": [
+      "Tortilla",
+      "Whatever you wish on your Taco",
+      "Napkins",
+      "Una coquita bien fría",
+    ],
+    "cuisine": "Mexican",
+    "dishType": "main_course",
+    "duration": 40,
+    "creator": "Mario Taquero"
+  };
+
+  //Uncomment this line to add new recipe
+  //createRecipe(newRecipe)
+
+//ITERATION 3
+function addMultipleRecipes(data){
+  Recipe.insertMany(data)
+  .then(manyInserted => {
+    console.log(`New recipes have been added: ${manyInserted}`)
+    mongoose.connection.close();
+  })
+  .catch(error => console.log('Something went wrong', error))
+}
+//This will add the recipes from the data file
+//addMultipleRecipes(data);
+
+let newRecipes = [{
+  "title": "Tacos",
+  "level": "Easy Peasy",
+  "ingredients": [
+    "Tortilla",
+    "Whatever you wish on your Taco",
+    "Napkins",
+    "Una coquita bien fría",
+  ],
+  "cuisine": "Mexican",
+  "dishType": "main_course",
+  "duration": 40,
+  "creator": "Mario Taquero"
+},{
+  "title": "Tacos x2",
+  "level": "Easy Peasy",
+  "ingredients": [
+    "Tortilla",
+    "Whatever you wish on your Taco",
+    "Napkins",
+    "Una coquita bien fría",
+  ],
+  "cuisine": "Mexican",
+  "dishType": "main_course",
+  "duration": 40,
+  "creator": "Mario Taquero"
+}
+];
+
+  //Uncomment this line to add new recipes
+  //addMultipleRecipes(newRecipes);
+
+//ITERATION 4
+function updateOneRecipe(title, update){
+  Recipe.findOneAndUpdate({title}, {duration:update}, {new:true})
+  .then(oneUpdated => {
+    console.log(`The recipe ${oneUpdated} was successfully updated`)
+    mongoose.connection.close();
+  })
+  .catch(error => console.log('Something went wrong', error))
+}
+
+// Uncomment to update the data
+//updateOneRecipe('Rigatoni alla Genovese', '100');
+
+//ITERATION 5
+function removeOneRecipe(title,){
+  Recipe.deleteOne({title})
+  .then((oneDeleted) => {
+    console.log(`A single recipe has been removed: ${oneDeleted}`)
+    mongoose.connection.close();
+  })
+  .catch(error => console.log('Something went wrong', error))
+}
+
+//Uncomment this to remove one single recipe
+//removeOneRecipe('Carrot Cake');
+
+//ITERATION 6 COMPLETED (ALL CONNECTIONS CLOSE)

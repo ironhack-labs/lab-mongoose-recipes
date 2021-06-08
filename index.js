a1a8c745981deab5ab26f0854e714889825f4837
Yani666 <yanis051989@gmail.com>
2021-06-08 18:57:33 -0500

    half lab

--- index.js
+++ index.js
@@ -20,8 +20,45 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+    console.log('Chocolate Chip Cookies')
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error('Error connecting to the database', error)
   });
+
+  function createRecipe(data){
+    Recipe.create(data)
+    .then(recCreated => console.log(`se creo una nueva receta: ${recCreated}`))
+    .catch(error=>console.log("algo salio mal",error))
+  }
+
+  
+    Recipe.insertMany(data)
+    .then(recCreated => console.log(`se insertaron varias recetas: ${recCreated}`))
+    .catch(error=>console.log("algo salio mal",error))
+  
+   
+
+  
+
+  let cookie = {
+    
+      title: "Chocolate Chip Cookies",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup light brown sugar",
+        "1 large egg",
+        "2 tablespoons milk",
+        "1 1/4 teaspoons vanilla extract",
+        "2 cups semisweet chocolate chips"
+      ],
+      cuisine: "French",
+      dishType: "dessert",
+      image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4398987.jpg&w=596&h=399.32000000000005&c=sc&poi=face&q=85",
+      duration: 30,
+      creator: "Chef Jennifer"
+    
+  }
+
+  createRecipe(cookie)

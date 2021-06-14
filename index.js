9d4279198a87519ac0d016474e75b1541b984662
Paloma Borro <palomabs93@gmail.com>
2021-06-14 16:50:16 +0200

    lab completo

--- index.js
+++ index.js
@@ -17,43 +17,73 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(self => {
+
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
-  .then(() => {
-    // return Recipe.create({
-    //   "title": "Carrot Cake",
-    //   "level": "Amateur Chef",
-    //   "ingredients": [
-    //     "6 cups grated carrots",
-    //     "1 cup brown sugar",
-    //     "1 cup raisins",
-    //     "4 eggs",
-    //     "1 1/2 cups white sugar",
-    //     "1 cup vegetable oil",
-    //     "2 teaspoons vanilla extract",
-    //     "1 cup crushed pineapple, drained",
-    //     "3 cups all-purpose flour",
-    //     "1 1/2 teaspoons baking soda",
-    //     "1 teaspoon salt",
-    //     "4 teaspoons ground cinnamon"
-    //   ],
-    //   "cuisine": "International",
-    //   "dishType": "dessert",
-    //   "image": "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
-    //   "duration": 130,
-    //   "creator": "Chef Nadia"
-    // })
-    // .then((recipe) => {
-    //   console.log(`You added the ${recipe.title} recipe`);
 
-      return Recipe.insertMany(recipes)
+  .then(() => {
+
+    return Recipe.create({
+      "title": "Curry Japones",
+      "level": "UltraPro Chef",
+      "ingredients": [
+        "1 cup of water",
+        "2 tablets of japanese curry",
+        "1 apple",
+        "50 grams of mushrooms",
+        "1 steak",
+        "1 carrot",
+      ],
+      "cuisine": "Japanese",
+      "dishType": "main_course",
+      "image": "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
+      "duration": 60,
+      "creator": "Chef Paloma"
     })
-    .then(recipes => {
-      recipes.forEach(recipe => console.log(`You added the ${recipe.title} recipe`))
+  })
+
+    .then((recipe) => {
+      console.log(`You added the ${recipe.title} recipe`);
+
+    return Recipe.insertMany(recipes)
     })
+
+      .then((recipes) => {
+        recipes.forEach((recipe) => console.log(`You added the ${recipe.title} recipe`)
+      );
+  
+    return Recipe.findOneAndUpdate({
+    title: "Rigatoni alla Genovese"
+  }, {
+    duration: 100
+  }, {
+    new: true
+  })
+})
+
+  .then((recipeUpdated) => {
+    console.log(`You updated the ${recipeUpdated.title} successfully`)
+  })
+
+return Recipe.deleteOne({
+    title: 'Carrot Cake'
+  })
+
+  .then((recipeDeleted) => {
+    console.log(`You deleted the ${recipeDeleted.title} successfully`);
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
+  })
+
+  .finally(() => {
+    mongoose.connection.close(() => {
+      console.log('Disconnected from database')
+      process.exit(0)
+    })
+    
   });

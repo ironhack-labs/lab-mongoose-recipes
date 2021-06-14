4c5689e7cd62a82024805778f871d67c95fad152
Paloma Borro <palomabs93@gmail.com>
2021-06-14 15:55:17 +0200

    Hasta la iteración 3

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express()
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const recipes = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -20,8 +23,37 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+    // return Recipe.create({
+    //   "title": "Carrot Cake",
+    //   "level": "Amateur Chef",
+    //   "ingredients": [
+    //     "6 cups grated carrots",
+    //     "1 cup brown sugar",
+    //     "1 cup raisins",
+    //     "4 eggs",
+    //     "1 1/2 cups white sugar",
+    //     "1 cup vegetable oil",
+    //     "2 teaspoons vanilla extract",
+    //     "1 cup crushed pineapple, drained",
+    //     "3 cups all-purpose flour",
+    //     "1 1/2 teaspoons baking soda",
+    //     "1 teaspoon salt",
+    //     "4 teaspoons ground cinnamon"
+    //   ],
+    //   "cuisine": "International",
+    //   "dishType": "dessert",
+    //   "image": "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
+    //   "duration": 130,
+    //   "creator": "Chef Nadia"
+    // })
+    // .then((recipe) => {
+    //   console.log(`You added the ${recipe.title} recipe`);
+
+      return Recipe.insertMany(recipes)
+    })
+    .then(recipes => {
+      recipes.forEach(recipe => console.log(`You added the ${recipe.title} recipe`))
+    })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

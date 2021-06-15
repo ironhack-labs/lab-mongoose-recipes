210af1786b32e39b7a5ff30a0f29cb84f809018d
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2021-06-15 10:17:40 +0200

    Iteration2

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Lemon Popsicles",
+      level: "Easy Peasy", 
+      ingredients: ["water", "lemon juice", "sugar", "lemon zest"],
+      cuisine: "English",
+      dishType: "dessert",
+      image: "https://www.courtneyssweets.com/lemonade-popsicles/",
+      duration: 20,
+      creator: "Luci"
+    })
+
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

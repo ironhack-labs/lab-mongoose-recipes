425675a5174092fa84b0fc234c7300441891c36a
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2021-06-15 10:23:09 +0200

    Iteration3

--- index.js
+++ index.js
@@ -21,17 +21,25 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    return Recipe.create({
-      title: "Lemon Popsicles",
-      level: "Easy Peasy", 
-      ingredients: ["water", "lemon juice", "sugar", "lemon zest"],
-      cuisine: "English",
-      dishType: "dessert",
-      image: "https://www.courtneyssweets.com/lemonade-popsicles/",
-      duration: 20,
-      creator: "Luci"
-    })
-
+    //   return Recipe.create({
+    //     title: "Lemon Popsicles",
+    //     level: "Easy Peasy",
+    //     ingredients: ["water", "lemon juice", "sugar", "lemon zest"],
+    //     cuisine: "English",
+    //     dishType: "dessert",
+    //     image: "https://www.courtneyssweets.com/lemonade-popsicles/",
+    //     duration: 20,
+    //     creator: "Luci",
+    //   });
+    // })
+    // .then((recipe) => {
+    //   console.log(`"${recipe.title}" has been added to database`);
+    return Recipe.insertMany(data);
+  })
+  .then((recipes) => {
+    recipes.forEach((recipe) =>
+      console.log(`Recipe with the title ${recipe.title} has been created`)
+    );
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);

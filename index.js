956bf6e5ea3dd4ca391f8a9666420972eab98420
Shirley Gianina <shirley.cd853f@gmail.com>
2021-06-15 13:56:01 +0200

    done

--- index.js
+++ index.js
@@ -20,13 +20,44 @@ mongoose
     return Recipe.deleteMany()
   })
 
-  .then(async () => {
-    // Run your code here, after you have insured that the connection was made
-    // itenacion 2
-    const newRecipe = await Recipe.create({ title: "Chunky Pancakes",cuisine: "USA"})
-    console.log(newRecipe.title)
+    .then(async () => {
+      // Run your code here, after you have insured that the connection was made
+      try {
+      // iteracion 2
+        const newRecipe = await Recipe.create({
+          title: "Pancakes",
+          cuisine: "USA"
+        })
+        console.log(`The ${newRecipe.title} has been created`)
+        
+        //Iteracion 3
+        const recipes = await Recipe.insertMany(data)
+        recipes.forEach(recipe => {
+        console.log(`${recipe.title}`)
+        })
 
-    
+        // Iteracion 4
+        const recipeUpdate = await Recipe.findOneAndUpdate(
+          { title: "Rigatoni alla Genovese" },
+          { duration: 100 })
+        console.log(`The duration of the recipe ${recipeUpdate.title} was updated to ${recipeUpdate.duration}`)
 
-  });
+       // Iteracion 5 
+        const recipeDelete = await Recipe.findOneAndDelete(
+          { title: "Carrot Cake" }
+        )
+        console.log(`The recipe ${recipeDelete.title} was deleted`)
 
+
+      } catch(err) {
+        console.error('the new recipes has not been created' );
+      }
+
+    });
+    // Iteracion 6
+    process.on('SIGINT', () => {
+      mongoose.connection.close(() => {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+      });
+    });

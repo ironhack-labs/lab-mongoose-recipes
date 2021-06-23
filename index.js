b37f441eb9c2245193b677e4426f13a1369c8810
juanfuerteshdz@gmail.com <juanfuerteshdz@gmail.com>
2021-06-24 00:35:40 +0200

    done

--- index.js
+++ index.js
@@ -20,8 +20,55 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.syncIndexes()
   })
+
+  //Iteration 2 : Create a recipe
+
+  // .then(() => {
+
+  //   Recipe
+
+  //     .create({
+  //       title: "Chocolate Chip Cookies",
+  //       level: "Amateur Chef",
+  //       ingredients: [
+  //         "1/2 cup light brown sugar",
+  //         "1 large egg",
+  //         "2 tablespoons milk",
+  //         "1 1/4 teaspoons vanilla extract",
+  //         "2 cups semisweet chocolate chips"
+  //       ],
+  //       cuisine: "French",
+  //       dishType: "dessert",
+  //       image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4398987.jpg&w=596&h=399.32000000000005&c=sc&poi=face&q=85",
+  //       duration: 30,
+  //       creator: "Chef Jennifer",
+  //     })
+
+  // Iteration 3 : Insert multiple recipes
+  .then(() => Recipe.create(data))
+  .then(recipes => recipes.forEach((recipe) =>
+    console.log('Se creó la siguiente receta: ', recipe.title)))
+
+  // Iteration 4 : Update recipe
+  .then(() => Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }))
+  .then(recipe => console.log('Se modificó correctamente la receta de: ', recipe.title))
+
+  //Iteration 5 : Remove a recipe
+  .then(() => Recipe.deleteOne({ title: 'Carrot Cake' }))
+  .then(() => console.log('La receta se suprimió de la base de datos'))
+
+
+  //Iteration 6 : Close the Database
+  .then(() => mongoose.connection.close(() => {
+    console.log('Mongoose disconnected')
+  }))
+
+  .catch(err => console.log('Se produjo un error.... =>', err))
+
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

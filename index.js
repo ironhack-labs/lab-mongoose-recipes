330a30ae61ac8aaf5043a948e4934ad0c76065a2
javierceballosaltuna <javierceballosaltuna@gmail.com>
2021-06-23 18:21:45 +0200

    done!

--- index.js
+++ index.js
@@ -19,9 +19,70 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.syncIndexes()
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  // .then(() => {
+
+  //   Recipe.create({
+
+  //     title: "Asian Glazed Chicken Thighs",
+  //     level: "Amateur Chef",
+  //     ingredients: [
+  //       "1/2 cup rice vinegar",
+  //       "5 tablespoons honey",
+  //       "1/3 cup soy sauce (such as Silver Swan®)",
+  //       "1/4 cup Asian (toasted) sesame oil",
+  //       "3 tablespoons Asian chili garlic sauce",
+  //       "3 tablespoons minced garlic",
+  //       "salt to taste",
+  //       "8 skinless, boneless chicken thighs"
+  //     ],
+  //     cuisine: "Asian",
+  //     dishType: "main_course",
+  //     image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  //     duration: 40,
+  //     creator: "Chef LePapu"
+  //   }
+  //   )
+  //     .then(theNewRecipe => console.log('¡La nueva receta fué creada!:', theNewRecipe))
+  //     .catch(err => console.log("Hubo un error!", err))
+  // })
+
+  .then(() => {
+
+    return Recipe
+
+      .create([...data])
+      .then(theNewRecipes => console.log('las nuevas recetas ya están listas:', theNewRecipes))
+      .catch(error => console.log('error aquí', error))
+
+  })
+
+
+  .then(() => {
+
+    return Recipe
+
+      .findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } }, { new: true })
+      .then(theNewDuration => console.log('duration is now ok at:', theNewDuration))
+      .catch(error => console.log('Error connecting to the database', error)
+
+      )
+  })
+
+  .then(() => {
+
+    return Recipe
+      .deleteOne({ _id: '60d35dca43bc3d0360d5529f' })
+
+      .then(carrotDeleted => console.log('Recipe deleted is:', carrotDeleted))
+      .catch(error => console.log('Error connecting to the database', error)
+      )
+  })
+
+  .then(() => {
+    mongoose.disconnect()
+  })

80017a9e7af27c42700fc8134e65190b99e037e9
kevinu27 <kevin.sm89@gmail.com>
2021-06-23 17:36:25 +0100

    commit

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,44 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  //enum: ['Marrón', 'Negro', 'Blanco', 'Varios']
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    type: String
+  },
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    default: 'Nombre desconocido'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String,
+  },
+  created: {
+    type: String,
+    default: Date.now
+  },
+
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

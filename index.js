8d65e58fa6144fa5e2f3aa23019901d9c97bf74d
pablorodri <pablorodri.r@gmail.com>
2021-06-23 19:54:24 +0200

    ejercicio day3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // requiero la conexion a la base de datos
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
@@ -14,14 +14,48 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany() // borro las recetas
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.syncIndexes()
   })
+
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({
+        title: 'pastel', level: "Easy Peasy", ingredientes: ["agua", "harina", "azucar"],
+        cuisine: "internacional", dishType: "breakfast", image: "https://images.media-allrecipes.com/images/75131.jpg",
+        duration: 10, creator: "Pablo", created: "2021/06/23 "
+      })
+      .then(() => {
+        return Recipe
+          .create(data)
+          .then(recipe => console.log('las recetas son:', recipe))
+      })
+
+      .then(() => {
+        return Recipe
+          .findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } }, { new: true })
+          .then(theNewDuration => console.log('la nueva duracion es:', theNewDuration))
+
+
+      })
+
+      .then(() => {
+        return Recipe.deleteOne({ title: 'Carrot Cake' })
+
+      })
+      .then(() => {
+        mongoose.disconnect()
+      })
+
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
---
+++ models/Model.create.js
@@ -0,0 +1,16 @@
+/*require('./data'); // conexion a base de datos ???
+
+// Model
+const Recipe = require('./models/Recipe.model')
+
+Recipe
+    .create({
+        title: 'pastel', level: "Easy Peasy", ingredientes: ["agua", "harina", "azucar"],
+        cuisine: "internacional", dishType: "breakfast", image: "https://images.media-allrecipes.com/images/75131.jpg",
+        duration: 10, creator: "Pablo", created: "2021/06/23 "
+    })
+    .then(theNewRecipe => console.log("nueva receta creada", theNewRecipe))
+    .catch(err => console.log('Se produjo un error.... =>', err))*/
+
+
+

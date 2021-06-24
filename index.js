cbd489022c108b8e1d32d2b4492d89f4400c57d7
Your Name <you@example.com>
2021-06-24 17:36:31 +0200

    lab done

--- index.js
+++ index.js
@@ -20,8 +20,49 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    /* return Recipe.create({
+               "title": "Asian Glazed Chicken Thighs",
+               "level": "Amateur Chef",
+               "ingredients": [
+                 "1/2 cup rice vinegar",
+                 "5 tablespoons honey",
+                 "1/3 cup soy sauce (such as Silver Swan®)",
+                 "1/4 cup Asian (toasted) sesame oil",
+                 "3 tablespoons Asian chili garlic sauce",
+                 "3 tablespoons minced garlic",
+                 "salt to taste",
+                 "8 skinless, boneless chicken thighs"
+               ],
+               "cuisine": "Asian",
+               "dishType": "main_course",
+               "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+               "duration": 40,
+               "creator": "Chef LePapu"
+             }) */
+
+
+
+    return Recipe.insertMany(data)
+  })
+  .then((result) => {
+    console.log(result.map((elem) => elem.title))
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+  })
+  .then(() => {
+    console.log('Update successful')
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+  })
+  .then(() => {
+    console.log("Delete was successful")
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+Promise.all([Recipe.deleteMany(), Recipe.insertMany(data), Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true }), Recipe.deleteOne()])
+  .then(([deleteMany, insert, update, deleteOne]) => {
+    console.log("All promises passed")
+  })
+
+

c86cbde0b2a4058917419efc381fe4017c6199dc
Ellen Children <ellenchildren@Ellens-iMac.home>
2021-06-24 17:12:44 +0200

    lab done

--- index.js
+++ index.js
@@ -20,8 +20,50 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+      // return Recipe.create({
+      //     "title": "Asian Glazed Chicken Thighs",
+      //     "level": "Amateur Chef",
+      //     "ingredients": [
+      //       "1/2 cup rice vinegar",
+      //       "5 tablespoons honey",
+      //       "1/3 cup soy sauce (such as Silver Swan®)",
+      //       "1/4 cup Asian (toasted) sesame oil",
+      //       "3 tablespoons Asian chili garlic sauce",
+      //       "3 tablespoons minced garlic",
+      //       "salt to taste",
+      //       "8 skinless, boneless chicken thighs"
+      //     ],
+      //     "cuisine": "Asian",
+      //     "dishType": "main_course",
+      //     "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      //     "duration": 40,
+      //     "creator": "Chef LePapu"
+      //   })
+
+        return Recipe.insertMany(data)  
+        
     // Run your code here, after you have insured that the connection was made
   })
+  .then(() => {
+      return Recipe.findOneAndUpdate(
+        {title: "Rigatoni alla Genovese"},
+        {duration: 100},
+        {new: true},
+      )
+  })
+  .then(() => {
+    console.log('Success!')
+  })
+  .then(() => {
+    return Recipe.deleteOne(
+          {title: "Carrot Cake"}
+    )
+  })
+  .then(() => {
+    mongoose.connection.close(() => {
+      console.log('The connection is closed')
+    })
+  })  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

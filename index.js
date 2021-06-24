5eebdb3ae19f5f3d31f8b242ffc2f50d7fb31ff2
“Zhanna“ <maekot@googlemail.com>
2021-06-25 01:02:37 +0200

    lab done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -14,14 +14,44 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
+  // .then(self => {
+  //   console.log(`Connected to the database: "${self.connection.name}"`);
+  //   // Before adding any recipes to the database, let's remove all existing ones
+  //   return Recipe.deleteMany()
+  // })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.deleteOne({ title: "Carrot Cake"})
+    .then(deletedRecipe => {
+      console.log(deletedRecipe);
+      mongoose.connection.close();
+    });
   })
+  // .then(() => {
+  //   Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese"}, {duration: 100},
+  //   {new: true})
+  //   .then(updatedRecipe => {
+  //     console.log(updatedRecipe);
+  //     mongoose.connection.close();
+  //   });
+  // })
+  // .then(() => {
+  //   Recipe.insertMany(data)
+  //   .then(recipe => {
+  //     recipe.forEach(recipe => console.log(recipe.title));
+  //     mongoose.connection.close();
+  //   });
+  // })
+  //.then(() => {
+    // Recipe.create({
+    //   title: 'Bla',
+    //   cuisine: 'Hr Bu',
+    //   dishType: "soup"
+    // })
+    // .then(recipe => {
+    //   console.log(recipe.title)})
+  
+    // Run your code here, after you have insured that the connection was made
+ // })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })

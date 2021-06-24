0c63b2109ca36d15abc15ef055d82acc17035216
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-06-24 17:38:55 +0200

    LAB Done

--- index.js
+++ index.js
@@ -17,33 +17,33 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     return Recipe.create({title: "Scrambled Eggs", cuisine: "International"})
   })
   .then((res)  => {
-    console.log(res.title)
+    console.log(res.title);
     return Recipe.insertMany(data);
   })
   .then((result) => {
     result.forEach((elem)=>{
-      console.log(elem.title)
+    console.log(elem.title);
     });
-    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true} )
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true});
   })
   .then(() => {
     console.log("Duration updated!");
-    return Recipe.deleteOne({title: "Carrot Cake"})
+    return Recipe.deleteOne({title: "Carrot Cake"});
   })
   .then(() => {
-    console.log("Carrot cake successfully deleted!")
-    return mongoose.connection.close()
+    console.log("Carrot cake successfully deleted!");
+    return mongoose.connection.close();
   })
   .then(() => {
-    console.log('Mongoose connection disconnected')
+    console.log('Mongoose connection disconnected');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  })
+  });
   

52bfaabc2cd6e0505a48668bcd9825db8b929248
beta <beta@kusarigama.local>
2021-06-24 20:43:51 +0200

    Completed lab

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,11 +21,60 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
+
     return Recipe.deleteMany()
   })
+
+  //------------------Add a signle document in the DB-------------------
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({ title: 'CurryWurst', cuisine: 'German' })
   })
+
+  //------------------ Add multiple documents in the DB--------------
+  .then((findResults) => {
+    // let recipes = [{ title: 'Rigatoni alla Genovese', cuisine: 'Italian', duration: 220 }, { title: 'Orange and Milk-Braised Pork Carnitas', cuisine: 'American' }, { title: 'Carrot Cake', cuisine: 'International' }, { title: 'Chocolate Chip Cookies', cuisine: 'French' }, { title: 'NY Cheese Cake', cuisine: 'Desert' }]
+    return Recipe.insertMany(data)
+
+      // //---------------------update-------------------------
+      .then((findResults) => {
+        findResults.forEach((elem) => {
+          console.log(elem.title)
+        })
+        return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+          .then(() => {
+
+            console.log('success!!')
+          })
+      })
+  })
+
+  // Delete the cake!!!   
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+      .then(() => {
+        console.log('yeaaaaaaaaahhhhhhhhhhhhhhhhhhh!!!!')
+
+      })
+
+  })
+
+
+  .then(() => {
+    return mongoose.disconnect()
+      .then(() => {
+        console.log(mongoose.connection.readyState);
+      })
+
+  })
+
+
+
+  // Run your code here, after you have insured that the connection was made
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+// mongoose.connection.close()

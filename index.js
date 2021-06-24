35b219f08e978039e78e3ff8b922b423191e3643
Gabriel Santos Dev <selvagedev@gmail.com>
2021-06-24 15:55:25 +0100

    iron hack may-21 Completed lab

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    updateDatabase();
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -25,3 +26,35 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  async function updateDatabase(){
+    try {
+      const recipeCreated = await Recipe.create({
+        title: "Barbecue", 
+        level: "Easy Peasy", 
+        ingredients: ["meat", "salt", "fire"],
+        cuisine: "Gaúcha",
+        dishType: "other",
+        image: "https://famintas.com.br/wp-content/uploads/2018/05/pic1.jpg",
+        duration: 300,
+        creator: "Gabriel",
+      });
+      console.log(recipeCreated.title);
+      const recipeArray = await Recipe.insertMany(data);
+      recipeArray.forEach(element => {
+        console.log(element.title);
+      });
+      
+      await Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 });
+      console.log("changed sucess");
+    
+      await Recipe.deleteOne({title:"Carrot Cake"})
+    } catch (error) {
+      console.log('oh oh error!')
+    } finally{
+      mongoose.connection.close();
+    }
+}
+
+

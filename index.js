0658391a9f94d41dbbedeca63a6007622186bdca
Tiago Pascoa <tiagompascoa@gmail.com>
2021-06-24 14:57:51 +0100

    Completed Lab

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    updateDatabase();
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+async function updateDatabase() {
+  try {
+    const recipeCreated = await Recipe.create({
+      title: "Vegetarian Lasanha",
+      level: "Amateur Chef",
+      ingredients: [
+        "fresh pasta",
+        "onions",
+        "spinach",
+        "riccota",
+        "bechamel",
+        "mozzarella",
+      ],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image:
+        "https://www.celeiro.pt/media/contentmanager/content/cache/720x473/receitas/lasanha-de-espinafres.jpg",
+      duration: 15,
+      creator: "Tiago Páscoa",
+    });
+    console.log(recipeCreated.title);
+
+    await Recipe.insertMany(data);
+    await Recipe.updateOne(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+    await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log("Carrot cake deleted successfuly");
+  } catch (e) {
+    console.log("error occurred", e);
+  } finally {
+    mongoose.connection.close();
+  }
+}

fcdaa2e1e58872431dd1da5dbdf0e3bfa4a9946c
Margarida Pereira <margarida.arq@gmail.com>
2021-06-24 14:52:30 +0100

    Solved

--- index.js
+++ index.js
@@ -12,16 +12,74 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
+    updateDatabase();
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
+ async function updateDatabase() {
+   try { //everything inside the try catch error
+    //  const recipeOne = await Recipe.create({
+    //    title: "Asian Glazed Chicken Thighs",
+    //    level: "Amateur Chef",
+    //    ingredients: [
+    //      "1/2 cup rice vinegar",
+    //      "5 tablespoons honey",
+    //      "1/3 cup soy sauce (such as Silver Swan®)",
+    //      "1/4 cup Asian (toasted) sesame oil",
+    //      "3 tablespoons Asian chili garlic sauce",
+    //      "3 tablespoons minced garlic",
+    //      "salt to taste",
+    //      "8 skinless, boneless chicken thighs"
+    //    ],
+    //    cuisine: "Asian",
+    //    dishType: "main_course",
+    //    image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //    duration: 40,
+    //    creator: "Chef LePapu"
+    //  });
+     
+    //  console.log('Recipe created', recipeOne.title);
+
+     const insertRecipes = await Recipe.insertMany(data);
+     for(let i = 0; i < insertRecipes.length; i++){
+    console.log('Recipes inserted', insertRecipes[i].title);
+     }
+     
+     const updatedRecipe = await Recipe.findOneAndUpdate({
+       title: 'Rigatoni alla Genovese'
+     }, {
+       duration: 100
+     }, { new:true });
+     console.log('Success message', updatedRecipe);
+
+    const deleteRecipe = await Recipe.deleteOne({title: 'Carrot Cake'});
+    console.log('Success message');
+     
+
+   } catch (e) {
+     console.log('error occurred', e);
+   } finally {
+     mongoose.connection.close(); //closes the connection to the database
+     //good practise to close the connection
+   }
+ }
+ 
+
+
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,9 +2,49 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+  title: {
+      type: String,
+      required: true //validations: we have to have a name otherwise we will have an error
+    },
+    level: {
+      type: String,
+      enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] //validations: the color has to be one of these otherwise we will have an error
+    },
+    ingredients: {
+      type: [String]
+    },
+    cuisine: {
+      type: String,
+      required: true
+    },
+    dishType: {
+      type: String,
+      enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+    },
+    image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+    duration: {
+      type: Number,
+      min: 0
+    },
+    creator: {
+      type: String
+    },
+    created: {
+      type: Date,
+      default: Date.now()
+    }
+
+  
   // TODO: write the schema
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+
+
+
+

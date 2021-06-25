bf307e5a65f90107ca7d5cf02677a86fe9f3862c
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-06-25 13:57:13 +0200

    lab-done

--- index.js
+++ index.js
@@ -8,20 +8,38 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
+const promise4 = mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+})
+
+const promise5 = Recipe.create({
+  title: "pastelito",
+  level: "Easy Peasy",
+  ingredients: ["??","???"],
+  cuisine: "apañola",
+  dishType: "breakfast",
+  image: "https://images.media-allrecipes.com/images/75131.jpg",
+  duration: 20,
+  creator: "yo"
+})
+.then((results)=>console.log(results.title))
+.catch((error)=>console.log(error))
+  
+  
+const promise1 = Recipe.insertMany(data)
+.then((recetas)=> console.log(recetas))
+.catch((error)=> console.log(error))
+
+const promise2 = Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+// Client.findOneAndUpdate({ name: 'Alberto Marcos'}, {age: 50})//we find the client object and then we update his age
+// .then( (updatedClient) => console.log(updatedClient))
+// .catch( error => console.log(error))
+const promise3 = Recipe.findOneAndDelete({title:"Carrot Cake"}
+)
+.then(()=>console.log("Element deleted"));
+
+Promise.all([promise1, promise2, promise3, promise4, promise5])
+  .then(()=> mongoose.connection.close(()=> console.log("connection closed!!")))

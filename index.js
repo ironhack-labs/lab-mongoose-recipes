2f79c51e6e76802ac033f9189e4bd680a69d6ecd
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-06-27 18:35:41 +0200

    done

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const DB_NAME = 'recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
+  .connect(`mongodb://localhost:27017/${DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -20,8 +19,45 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log(`All the recipes should be deleted now`);
+    //iteration 2
+    Recipe.create(iterationTwoRecipe).then(recipe => console.log(recipe.title))
+    //iteration 3
+    Recipe.create(data).then(recipesArray => recipesArray.forEach(recipe => console.log(recipe.title))).then(() => {
+      //iteration 4
+      Recipe.findOneAndUpdate(
+        {title: "Rigatoni alla Genovese"}, 
+        {duration: 100},
+        {new: true}
+      ).then(() => console.log("Recipe Updated!"))
+      //iteration 5 and 6 
+      Recipe.deleteOne({ title: "Carrot Cake" }).then(() => console.log("Carrot Cake removed!"))
+      .then(() => mongoose.disconnect())
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  
+
+
+let iterationTwoRecipe = {
+  title: "Iteration 2 - Asian Glazed Chicken Thighs",
+  level: "Amateur Chef",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  cuisine: "Asian",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 40,
+  creator: "Chef LePapu"
+}
+

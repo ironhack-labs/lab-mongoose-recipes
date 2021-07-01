118d5e7f2675a95fd4428cc7b9fb6c42c1c2d011
doris hutter <doris.hutter@gmx.de>
2021-07-01 13:26:14 +0200

    Missing the last two iterations

--- index.js
+++ index.js
@@ -21,7 +21,59 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+
+    //Iteration 2 - Create a recipe
+
+    //Recipe.create({ 
+         /*title: 'Asian Glazed Chicken Thighs',
+         level: 'Amateur Chef',
+         ingredients: [
+           '1/2 cup rice vinegar',
+           '5 tablespoons honey',
+           '1/3 cup soy sauce (such as Silver Swan®)',
+           '1/4 cup Asian (toasted) sesame oil',
+           '3 tablespoons Asian chili garlic sauce',
+           '3 tablespoons minced garlic',
+           'salt to taste',
+           '8 skinless, boneless chicken thighs'
+         ],
+         cuisine: 'Asian',
+         dishType: 'main_course',
+         image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+         duration: 40,
+         creator: 'Chef LePapu'
+       })
+         .then(recipe => {
+       	  console.log(recipe.title);
+      
+       })
+       .catch(err => console.log(err))
+ })*/
+
+  // ITERATION 3: INSERT MULTIPLE RECIPES
+
+        Recipe.insertMany(data)
+
+	     .then(recipe => {
+
+	   	  recipe.forEach(element => {
+         console.log(element.title); 
+       }); 
+	
+	 })
+	 .catch(err => console.log(err))
+ })
+
+   //ITERATION 4: UPDATE RECIPE
+     Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' },
+     
+     { duration: 100 }, {new: true}).then(updatedRecipe => {console.log(updatedRecipe.duration);
+         
+    })
+
+   .catch(err => console.log(err))
+  
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

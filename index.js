0ad80c671de9af0b19e2414449bd683ca432fdfe
Chenjia Li <cligrenet@gmail.com>
2021-07-21 15:46:22 +0200

    init commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -12,16 +13,83 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+
+    mongoose.connection.db
+      .dropDatabase()
+      .then(() => {
+        const recipe1 = {
+          title: 'Asian Glazed Chicken Thighs',
+          level: 'Amateur Chef',
+          ingredients: [
+            '1/2 cup rice vinegar',
+            '5 tablespoons honey',
+            '1/3 cup soy sauce (such as Silver Swan®)',
+            '1/4 cup Asian (toasted) sesame oil',
+            '3 tablespoons Asian chili garlic sauce',
+            '3 tablespoons minced garlic',
+            'salt to taste',
+            '8 skinless, boneless chicken thighs',
+          ],
+          cuisine: 'Asian',
+          dishType: 'main_course',
+          image:
+            'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+          duration: 40,
+          creator: 'Chef LePapu',
+        };
+
+        Recipe.create(recipe1)
+          .then(() => {
+            console.log(recipe1.title);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+        Recipe.insertMany(data)
+          .then((newDoc) => {
+            newDoc.forEach((i) => console.log(i.title));
+
+            Recipe.findOneAndUpdate(
+              { title: 'Rigatoni alla Genovese' },
+              { duration: 100 }
+            )
+              .then(() => {
+                console.log(
+                  'Rigatoni alla Genovese recipe updated successfully!'
+                );
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+
+            Recipe.deleteOne({ title: 'Carrot Cake' })
+              .then(() => {
+                console.log('Carrot Cake deleted successfully!');
+                mongoose.connection.close();
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    //return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });

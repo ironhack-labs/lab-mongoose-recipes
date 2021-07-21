ad00c98284e1541448426b19915dd6eb5f606e7e
Chenjia Li <cligrenet@gmail.com>
2021-07-21 21:27:21 +0200

    added async/await

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+const recipe1 = {
+  title: 'Asian Glazed Chicken Thighs2',
+  level: 'Amateur Chef',
+  ingredients: [
+    '1/2 cup rice vinegar',
+    '5 tablespoons honey',
+    '1/3 cup soy sauce (such as Silver Swan®)',
+    '1/4 cup Asian (toasted) sesame oil',
+    '3 tablespoons Asian chili garlic sauce',
+    '3 tablespoons minced garlic',
+    'salt to taste',
+    '8 skinless, boneless chicken thighs',
+  ],
+  cuisine: 'Asian',
+  dishType: 'main_course',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 40,
+  creator: 'Chef LePapu',
+};
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+/*
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
@@ -19,31 +40,13 @@ mongoose
   .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-
+    return Recipe.deleteMany();
+  })
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
     mongoose.connection.db
       .dropDatabase()
       .then(() => {
-        const recipe1 = {
-          title: 'Asian Glazed Chicken Thighs',
-          level: 'Amateur Chef',
-          ingredients: [
-            '1/2 cup rice vinegar',
-            '5 tablespoons honey',
-            '1/3 cup soy sauce (such as Silver Swan®)',
-            '1/4 cup Asian (toasted) sesame oil',
-            '3 tablespoons Asian chili garlic sauce',
-            '3 tablespoons minced garlic',
-            'salt to taste',
-            '8 skinless, boneless chicken thighs',
-          ],
-          cuisine: 'Asian',
-          dishType: 'main_course',
-          image:
-            'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
-          duration: 40,
-          creator: 'Chef LePapu',
-        };
-
         Recipe.create(recipe1)
           .then(() => {
             console.log(recipe1.title);
@@ -85,11 +88,49 @@ mongoose
       .catch((error) => {
         console.log(error);
       });
-    //return Recipe.deleteMany();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
   })
   .catch((error) => {
     console.error('Error connecting to the database', error);
   });
+*/
+
+//Aync await
+
+async function seedDatabase() {
+  try {
+    const dbconnection = await mongoose.connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    //Delete all the recipes in the DB
+    await Recipe.deleteMany();
+    //Create one recipe
+    const createOneRecipe = await Recipe.create(recipe1);
+    console.log(createOneRecipe.title);
+    //Create many recipes
+    const createdRecipes = await Recipe.create(data);
+    createdRecipes.forEach((recipe) => {
+      console.log(recipe.title);
+    });
+    //Update one recipe
+    const updatedRecipe = await Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 },
+      { useFindAndModify: false, new: true }
+    );
+    console.log(updatedRecipe.duration);
+    //Delete one recipe
+    const deletedRecipe = await Recipe.deleteOne({ title: 'Carrot Cake' });
+    console.log(deletedRecipe);
+    console.log('Recipe deleted successfully!');
+
+    //close DB connection
+    mongoose.connection.close();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+seedDatabase();

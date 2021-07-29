f143c271b2801f592630812e16e2f425631bd5b9
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-07-29 16:37:40 +0300

    complete iteration 1-3

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    createRecipe();
+    includeDataJson();
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+function createRecipe() {
+  const recipeData = {
+    title: "Chicken Wings",
+    cuisine: "Asian",
+  };
+
+  Recipe.create(recipeData)
+    .then((recipeFromDb) => console.log(recipeFromDb.title))
+    .catch((err) => console.log(err));
+}
+
+function includeDataJson() {
+  Recipe.insertMany(data)
+    .then((recipeFromDb) => {
+      recipeFromDb.forEach((recipe) => {
+        console.log(recipe.title);
+      });
+    })
+    .catch((err) => console.log(err));
+}

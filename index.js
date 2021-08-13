62bdf65158961616272c393006862375a511f7e0
kpla001 <kpla001@fiu.edu>
2021-08-13 14:57:17 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set("useFindAndModify", false);
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -19,9 +19,63 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  /////////////////// Iteration 2 /////////////////////
+  // .then(() => {
+    //   Recipe
+    // .create(
+      //   {
+        //     title: "Asian Glazed Chicken Thighs",
+        //     level: "Amateur Chef",
+        //     ingredients: [
+          //       "1/2 cup rice vinegar",
+          //       "5 tablespoons honey",
+          //       "1/3 cup soy sauce (such as Silver Swan®)",
+          //       "1/4 cup Asian (toasted) sesame oil",
+          //       "3 tablespoons Asian chili garlic sauce",
+          //       "3 tablespoons minced garlic",
+          //       "salt to taste",
+          //       "8 skinless, boneless chicken thighs"
+          //     ],
+          //     cuisine: "Asian",
+          //     dishType: "main_course",
+          //     image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+          //     duration: 40,
+          //     creator: "Chef LePapu"
+          //   }
+          // )
+          
+          //   .then(( newlyCreatedRecipe ) => console.log(`Newly Created Recipe: ${newlyCreatedRecipe.title}`))
+          // })
+
+
+
+  /////////////////// Iteration 3 /////////////////////
+          Recipe.insertMany(data)
+          .then((array) => array.forEach((recipe) => console.log(`Title: ${recipe.title}`)) )
+          
+          
+
+/////////////////// Iteration 4 /////////////////////
+          .then(() => { 
+              Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 } )
+              .then(() => console.log(`Document has been updated`))
+          })
+
+
+
+/////////////////// Iteration 5 /////////////////////
+          .then(() => {
+            Recipe.deleteOne({ title: 'Carrot Cake'}, (err, item) => {
+              if (err) console.log(`Could not remove record`);
+              else console.log(`Record was removed successfully - Deleted ${item.deletedCount} item(s).`);
+            })
+/////////////////// Iteration 6 /////////////////////
+            .then(() => { 
+            mongoose.connection.close()
+          })
+})
+
+          .catch(error => {
+            console.error('Error connecting to the database', error);
+          });

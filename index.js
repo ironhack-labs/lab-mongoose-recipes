561111dc0edff68bf885e2a442675843b1af0535
vloparevich <vadim.loparevich@gmail.com>
2021-08-12 20:58:59 -0700

    Implement Recipes Lab

--- index.js
+++ index.js
@@ -12,16 +12,49 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: 'Carrot Cookie',
+      level: 'Kitchen Killer',
+      ingredients: ['6 cups grated carrots', '4 teaspoons ground cinnamon'],
+      cuisine: 'International',
+      dishType: 'dessert',
+      image:
+        'https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg',
+      duration: 130,
+      creator: 'Chef Nadia',
+    })
+      .then((insertion) => console.log(`Insertion: ${insertion.title}`))
+      .catch((err) => console.log(`Create failed with the error ${err}`));
+
+    Recipe.insertMany(data)
+      .then((arrOfObjects) =>
+        arrOfObjects.forEach((obj) => {
+          console.log(`Title: ${obj.title}`);
+        })
+      )
+      .then(() =>
+        Recipe.findOneAndUpdate(
+          { title: 'Rigatoni alla Genovese' },
+          { duration: 111 }
+        )
+      )
+      .then(() =>
+        Recipe.deleteOne({ title: 'Carrot Cake' }, (err, item) => {
+          if (err) console.log(`Record was not removed 😱`);
+          else console.log(`Records removed: ${item.deletedCount}`);
+        })
+      )
+      .then(() => mongoose.connection.close())
+      .catch((err) => console.error(`Error: ${err} 💀`));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .catch((err) => console.error('Error connecting to the database', err));

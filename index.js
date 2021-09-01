7cce8e8a6d9c27725ae48e624b8c3b934077ec78
dany <dacr63@gmail.com>
2021-09-01 16:59:37 +0200

    lab done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { create } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -19,9 +20,30 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  // Iteracion 1
+  Recipe
+    .create({title: 'Meat Pie', level: 'Amateur Chef', ingredients: ['Potatoes', 'Tomato Sauce', 'Meat', 'Olive oil'], cuisine: 'American', dishType: 'main_course', image: 'https://canalcocina.es/medias/publicuploads/2017/03/05/162735/148002322958bb486de934f9.48228749.jpg', duration: 60, creator: 'Chef Daniel'})
+    .then(console.log("ok"))
+    .then(addedOwnRecipe => {
+      console.log("Added own recipe", addedOwnRecipe.title);
+      return Recipe.create(data)
+    })
+    .then (addedData => {
+      addedData.map((data) => console.log("Added", data.title))
+      return Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
+    })
+    .then(updatedData => {
+      console.log("Updated data");
+      return Recipe.deleteOne({title: 'Carrot Cake'})
+    })
+    .then(deletedRecipe => {
+      console.log("Deleted entry")
+      return mongoose.disconnect();
+    })
+    .catch(err => console.log('Error detected ---', err))
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+

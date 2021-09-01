9a824ed6c472ef39cf0bef3a8bdcf9ecfa649e67
Sara Mansori <sara.mansori.amar@gmail.com>
2021-09-01 16:50:47 +0200

    lab done

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany()        
   })
   .then(() => Recipe.syncIndexes())
 
@@ -25,18 +25,46 @@ mongoose
 
   //   ITERATION 2. Create new recipe
 
-  //   Recipe
-  //   .create({title: 'IronBurgers', level: 'UltraPro Chef', ingredients: ['meat', 'bread', 'love'], cuisine: 'spanish', dishType: 'main_course', duration: 1, creator: 'Sara' })
-  //   .then(newRecipe => console.log('The new recipe is', newRecipe))
-  //   .catch(err => console.log('EEERROOORR', err))
+    Recipe
+    .create({
+      title: 'IronBurgers', 
+      level: 'UltraPro Chef', 
+      ingredients: ['meat', 'bread', 'love'], 
+      cuisine: 'spanish', 
+      dishType: 'main_course', 
+      image: 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      duration: 1, 
+      creator: 'Sara' 
+      })
 
+    .then(newRecipe => {
+    console.log('The new recipe is', newRecipe.title)
+    return Recipe.create(data)
+    })
 
-  //  ITERATION 3. Insert multiple recipes
+  // //  ITERATION 3. Insert multiple recipes
 
-      Recipe
-      .create(data)
-      .then(newRecipes => newRecipes.map((recipe) => console.log('The title of the recipe is', recipe.title)))
-      .catch(err => console.log('EEERROOORR', err))
+    .then(newRecipes => {
+      newRecipes.map((recipe) => console.log('The title of the recipe added is', recipe.title))
+      return Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100}, { new: true })
+    }) 
+
+  // //  ITERATION 4. Updating a recipe
+
+    .then(updatedRecipe =>  {
+      console.log('The recipe was updated succesfully!')
+      return Recipe.deleteMany({title: 'Carrot Cake'})
+    })
+
+  // //  ITERATION 5. Remove a recipe
+
+   .then(removedRecipe => {
+     console.log('Success! The recipe was removed')
+     return mongoose.connection.close()
+   })
+    
+   .catch(err => console.log('Error at recipe update', err))
+
   })
 
 

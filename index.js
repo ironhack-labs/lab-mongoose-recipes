6db6707de2e6e0e2f387888b5e4d896765dcf9f1
MATS <waanmedia_mats@MacBook-Pro-de-WaanMedia-MATS.local>
2021-09-01 18:28:43 +0200

    done

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+//1. Si está fuera de un then está descontrolado
+//2. Recibe como parametro el return del anterior
+//3. Espera a que se complete el return del anterior
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -19,9 +23,27 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe
+      .create(
+        { title: "Tarta de queso", level: 'Amateur Chef', ingredients: ["queso", "huevos", "mermelada"], cuisine: "española", dishType: "other", image: "https://www.recetasderechupete.com/wp-content/uploads/2019/06/tarta-de-queso-mejor-Espa%C3%B1a.jpg", duration: 10, creator: "Miguel", created: Date.now() }
+      )
   })
+
+  .then((recipe) => {
+    console.log(`Este postre se titula ${recipe.title}`)
+    return Recipe.create(data)
+  })
+
+  .then(recipes => recipes.forEach(elm => console.log(`el nombre del postre es ${elm.title}`)))
+
+  .then(() => Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }))
+
+  .then(() => Recipe.deleteOne({ title: "Carrot Cake" }))
+
+  .then(() => mongoose.connection.close())
+
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error('Error connecting to the database este es el ERROR => OJITO', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,48 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+
+  title: {
+    type: String,
+    unique: true,
+    default: 'Nombre desconocido'
+  },
+
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'Blanco', 'UltraPro Chef']
+  },
+
+  ingredients: [String],
+
+  cuisine: {
+    type: String,
+    required: true,
+    default: 'Nombre desconocido'
+  },
+
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+
+  },
+
+  duration: {
+    type: Number,
+    min: 0
+  },
+
+  creator: String,
+
+  created: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

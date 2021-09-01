d2cda0906768ef35a4dd26e84392f359efd45fd9
Vanessa Zagone <vanessa.zgn@gmail.com>
2021-09-01 16:57:15 +0200

    Everything done, nice and smooth!

--- index.js
+++ index.js
@@ -12,16 +12,56 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+
+    return Recipe.deleteMany();
+  })
+  .then(() => Recipe.syncIndexes())
+  .then(() => {
+    Recipe.create({
+      title: 'Risotto de Boletus',
+      level: 'Amateur Chef',
+      ingredients: [
+        'Boletus',
+        'Arroz',
+        'Mantequilla',
+        'Grana Padano',
+        'Perejil',
+        'Vino Blanco',
+        'Cebolla',
+        'Pimienta negra',
+      ],
+      cuisine: 'Italiana',
+      dishType: 'main_course',
+      image:
+        'https://www.kenwoodworld.com/Global/Countries/Spain/Pasta/RISOTTO-BOLETUS.jpg',
+      duration: 45,
+      creator: 'Sybil Vane',
+    }).then(newRecipe => console.log(newRecipe.title));
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create(data);
   })
+  .then(importedResults =>
+    importedResults.forEach(recipe => {
+      console.log(recipe.title);
+    })
+  )
+  .then(() =>
+    Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 },
+      { new: true }
+    )
+  )
+  .then(updatedRecipe => console.log('Recipe updated!', updatedRecipe.title))
+  .then(() => Recipe.deleteOne({ title: 'Carrot Cake' }))
+  .then(deletedRecipe => console.log('Recipe deleted!'))
+  .then(() => mongoose.connection.close())
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error('Recipe Error!', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,79 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  //   title - Type String. It should be required and unique.
+
+  title: {
+    unique: true,
+    type: String,
+    required: true,
+    default: 'Unknown',
+    trim: true,
+  },
+
+  // level - Type String. Can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (remember the enum validator 😉).
+
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+  },
+
+  // ingredients - Type Array of Strings (represented as [ String ]).
+
+  ingredients: {
+    type: [String],
+  },
+
+  // cuisine - Type String. Should be required.
+
+  cuisine: {
+    type: String,
+    required: true,
+    default: 'Unknown',
+  },
+
+  // dishType - Type String. Possible values: breakfast, main_course, soup, snack, drink, dessert or other.
+
+  dishType: {
+    type: String,
+    enum: [
+      'breakfast',
+      'main_course',
+      'soup',
+      'snack',
+      'drink',
+      'dessert',
+      'other',
+    ],
+  },
+
+  // image - Type String. Default value: "https://images.media-allrecipes.com/images/75131.jpg".
+
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg',
+  },
+
+  // duration - Type Number. The minimum value should be 0.
+
+  duration: {
+    type: Number,
+    min: 0,
+  },
+
+  // creator - Type String.
+
+  creator: {
+    type: String,
+    default: 'Unknown',
+  },
+
+  // created - Type Date. By default, today.
+
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

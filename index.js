06da3f50e383f8190f358958fd4cd5da4eebbd30
Paula Kamisato <paukamisato@gmail.com>
2021-09-01 18:27:49 +0200

    completed lab

--- index.js
+++ index.js
@@ -19,9 +19,39 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
+    Recipe
+      .create({ title: 'Lemon Pie', level: 'Amateur Chef', ingredients: 'sugar,egs,milk,flour,lemonjuice', cuisine: 'american', dishtype: 'dessert', image: 'lemonpie', duration: '40', creator: 'Lucia', created: '01/09/2021' },)
+      .then(newRecipeFromDB => console.log('The new recipe is', newRecipeFromDB))
     // Run your code here, after you have insured that the connection was made
+  })
+  .then(() => {
+    Recipe
+      .create(data)
+      .then(allNewRecipes => {
+        allNewRecipes.forEach(obj => console.log(`title: ${obj.title}`))
+        Recipe
+          .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+          .then(info => console.log("Los detalles de la modificación son:", info))
+        Recipe
+          .deleteOne({ title: "Carrot Cake" })
+          .then(info => console.log('Este es un objeto informativo sobre una elimiación', info))
+          .catch(error => { console.error('Error delete', error); })
+
+          .catch(error => {
+            console.error('Error', error);
+          });
+      })
+
+      .catch(error => {
+        console.error('Error ', error);
+      });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+mongoose.connection.close();
+
+
+

e2434937433f7937477cee4223f2c7d3f0641b45
Sergio Puente <sergio.leones@hotmail.com>
2021-09-01 17:19:40 +0200

    Finish result

--- index.js
+++ index.js
@@ -19,9 +19,49 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
+
     // Run your code here, after you have insured that the connection was made
+    // Recipe
+    // .create({
+    //   title: "Tortilla de patata",
+    //   level: "Amateur Chef",
+    //   ingredients: [
+    //     "patatas",
+    //     "aceite",
+    //     "cebolla",
+    //     "amor"
+    //   ],
+    //   cuisine: "Española",
+    //   dishType: "main_course",
+    //   image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //   duration: 40,
+    //   creator: "Sergio el chef"
+    // })
+    // .then(newRecipeFromDB => console.log('La nueva receta es', newRecipeFromDB) )
+
+    Recipe
+    // Creamos las recetas
+    .create()
+    .then(() => Recipe.create(data))
+
+    // Sacamos los titulos de las recetas añadidas
+    .then(recipes => recipes.forEach ((recipe) => console.log('Se crearon las siguientes recetas', recipe.title)))
+
+    // Encontramos la receta y modificamos la duración
+    .then(() => Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}))
+    .then(recipe => console.log('Se modificó la receta:', recipe.title , 'Con la duración de', recipe.duration))
+
+    // Borramos la receta que ya no tiene que estar en la BD
+    .then(() => Recipe.deleteOne({title: 'Carrot Cake'}))
+    .then(recipe => console.log('Receta eliminada'))
+
+    // Cerramos la BD
+    .then(() => mongoose.connection.close())
+
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

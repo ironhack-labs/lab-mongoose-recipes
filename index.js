23efad4b79ad103bb8f4260b6b02dda3a1ad2364
Juan De Santis <juanbds66@gmail.com>
2021-09-01 18:49:29 +0200

    [MAD DE] JUAN DE SANTIS

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+
+    return Recipe.deleteMany();
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({ title: "Cheescake", cuisine: "xd" })
+      .then((newRecipe) => {
+        console.log(newRecipe.title);
+      })
+      .catch((error) => {
+        console.error("no titulo xd", error);
+      });
+    //iteration 3
+
+    Recipe.create(data).then((newArray) => {
+      newArray.forEach((Recipe) => {
+        console.log(Recipe.title);
+      }).then((Recipe) => {
+        Recipe.findOneAndUpdate({
+          title: "Rigatoni alla Genovese",
+          duration: 100
+          
+        });
+      })
+    });
+
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: [String],
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: String,
+  imageUrl: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: String,
+  created: Date,
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

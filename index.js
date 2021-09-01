1a47495fcec2502323741a743af1120268248d13
Laura <lauradecc@gmail.com>
2021-09-01 17:16:02 +0200

    done

--- index.js
+++ index.js
@@ -19,8 +19,36 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
+
+  // Create a recipe
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({ 
+      title: 'Tortilla de patata', 
+      level: 'Easy Peasy',
+      ingredients: ['huevos', 'patatas', 'aceite', 'sal'],
+      cuisine: 'Española',
+      dishType: 'main_course',
+      image: 'https://yolandanutricionista.com/wp-content/uploads/2019/02/tortilla.jpg',
+      duration: 60,
+      creator: 'Laura',
+      })
+    .then(newRecipe => console.log(newRecipe.title))
+
+    // Insert multiple recipes
+    .then(() => Recipe.create(data))
+    .then(newRecipes => newRecipes.forEach(recipe => console.log(recipe.title)))
+
+    // Update recipe
+    .then(() => Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, {duration: 100}))
+    .then(() => console.log('La duración de la receta se ha actualizado con éxito.'))
+
+    // Remove a recipe
+    .then(() => Recipe.deleteOne({ title: 'Carrot Cake' }))
+    .then(() => console.log('La receta se ha eliminado con éxito.'))
+  
+    // Close the Database
+    .then(() => mongoose.connection.close(() => console.log('Mongoose default connection closed')))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

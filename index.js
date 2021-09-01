e865f0bb2aef2e7288b74248e2bc47e258261116
Ivan Espinoza <icaro_19@hotmail.com>
2021-09-01 17:17:59 +0200

    finish work

--- index.js
+++ index.js
@@ -19,9 +19,37 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // console.log(data)
   })
+  .then(() => Recipe.create(
+    {
+      title: "Rice and Chicken",
+      level: "Amateur Chef",
+      ingredients: [
+        "2 pounds red onions, sliced salt to taste",
+        "2 (16 ounce) boxes uncooked rigatoni",
+        "1 tablespoon chopped fresh marjoram leaves",
+        "1 pinch cayenne pepper",
+        "2 tablespoons freshly grated Parmigiano-Reggiano cheese"
+      ],
+      cuisine: "Peruvian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/3489951.jpg",
+      duration: 120,
+      creator: "Chef Luigi"
+    }
+  ))
+  .then(() => Recipe.create(data))
+  .then(() => Recipe.updateOne({ duration: 220 }, { duration: 100 }, { new: true }))
+  .then(() => Recipe.deleteOne({ title: 'Carrot Cake' }))
+  .then(() => {
+    console.log("Connection closed")
+    mongoose.connection.close()
+  }
+  )
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

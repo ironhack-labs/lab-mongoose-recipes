44681e87e8a25f7bb0087459c705053a6ad4e64b
Ninabaldo <nbaldorousseau@gmail.com>
2021-09-02 17:52:28 +0200

    iteration 1,2,3 done

--- index.js
+++ index.js
@@ -10,18 +10,49 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  // iteratión 2 - insert one recipe
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    
+
+    Recipe.create({
+      "title": "Healthy oat chocolate cake",
+      "level": "Easy Peasy",
+      "ingredients": [
+        "1 cup oats",
+        "2 eggs",
+        "2 bananas",
+        "1/4 cup coconut oil",
+        "1/4 vanilla esence",
+        "3 tablespoons minced garlic",
+        "1/4 cup yeast",
+        "100 grams chocolate 100%",
+        "apricot to decorate",
+      ],
+      "cuisine": "French",
+      "dishType": "snack",
+      "image": "",
+      "duration": 35,
+      "creator": "Chef Nina",
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+
+  // Iteration 3 - Insert multiple recipes 
+  .then  (() => {
+    return Recipe.insertMany(data)
+  })
+  .then((recipes) => {
+    recipes.forEach(recipe => {
+      console.log(recipe.title)
+    });
+  })

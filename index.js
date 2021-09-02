28441d7e4343cbdb2b81ff3539ba1478da4cc0df
lauraAlabau <arlaura3@gmail.com>
2021-09-02 18:15:38 +0200

    lab done

--- index.js
+++ index.js
@@ -18,47 +18,65 @@ mongoose
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
-  })
     .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    /**** Iteration 2 - Create a recipe ****/
-    Recipe.create({
-      "title": "Lemon and Milk-Braised Pork Carnitas",
-      "level": "UltraPro Chef",
-      "ingredients": [
-        "3 1/2 pounds boneless pork shoulder, cut into large pieces",
-        "1 tablespoon freshly ground black pepper",
-        "1 tablespoon kosher salt, or more to taste",
-        "2 tablespoons vegetable oil",
-        "2 bay leaves",
-        "2 teaspoons ground cumin",
-        "1 teaspoon dried oregano",
-        "1/4 teaspoon cayenne pepper",
-        "1 lemon, juiced and zested"
-      ],
-      "cuisine": "American",
-      "dishType": "main_course",
-      "image": "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
-      "duration": 160,
-      "creator": "Chef John"
-    })
-    .then(recipe => console.log(recipe.title))
-  }) 
+      // Iteration 2 - Create a recipe //
+      Recipe.create({
+        "title": "Lemon and Milk-Braised Pork Carnitas",
+        "level": "UltraPro Chef",
+        "ingredients": [
+          "3 1/2 pounds boneless pork shoulder, cut into large pieces",
+          "1 tablespoon freshly ground black pepper",
+          "1 tablespoon kosher salt, or more to taste",
+          "2 tablespoons vegetable oil",
+          "2 bay leaves",
+          "2 teaspoons ground cumin",
+          "1 teaspoon dried oregano",
+          "1/4 teaspoon cayenne pepper",
+          "1 lemon, juiced and zested"
+        ],
+        "cuisine": "American",
+        "dishType": "main_course",
+        "image": "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
+        "duration": 160,
+        "creator": "Chef John"
+      })
+      .then(recipe => console.log(recipe.title))
+      
+      .catch(error => {
+        console.error('Error connecting to the database', error);
+      });
+    }) 
+  })
 
 /**** Iteration 3 - Insert multiple recipes ****/
-
   Recipe.insertMany(data)
     .then((recipes) => {
       recipes.forEach(recipe => {
-        console.log(recipe.title)        
+        console.log(recipe.title) 
+        
       });
-  })
 
 /**** Iteration 4 - Update recipe ****/
+      Recipe.updateMany({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+        .then(recipe => {
+          console.log('Update users: ', recipe)
 
 
+/**** Iteration 5 - Remove a recipe ****/          
+          Recipe.deleteOne({ title: 'Carrot Cake' })
+          .then(() => {
+            console.log('Carrot Cake deleted')
+
+/**** Iteration 6 - Close the Database ****/            
+            mongoose.connection.close(() => {
+              console.log('Mongoose default connection disconnected through app termination');
+              process.exit(0);
+            });
+          })
+      })
 
 
+  })
 
   .catch(error => {
     console.error('Error connecting to the database', error);

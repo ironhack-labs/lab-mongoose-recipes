eecda24719bdfd68f16d92771963fd2de691a259
Noemi Radvan <radvanno@gmail.com>
2021-09-02 22:08:12 +0200

    Done till iteration 3

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Lasagne",
+      level: "Amateur_Chef",
+      ingredients: [
+        "meat",
+        "onion",
+        "garlic",
+        "tomatoes",
+        "lasagne noodles",
+        "cheese",
+      ],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "https://www.allrecipes.com/recipe/23600/worlds-best-lasagna/",
+      duration: 120,
+      creator: "John Chandler",
+      created: 2 / 9 / 2021,
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  })
+  .finally(() => {
+    return mongoose.disconnect();
   });

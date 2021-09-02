0a1a56fd5bf2df3b8ee4fb1c3d77adc41be692bc
Alejandro Mallo <mallomanzi@gmail.com>
2021-09-02 08:10:13 +0200

    easy peasy

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
@@ -19,9 +20,31 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe
+          .create({ title: 'Pascualina', level: 'Amateur Chef', ingredients: ['harina', 'huevos', 'aceite', 'agua', 'espinacas', 'cebolla', 'pimiento', 'sal', 'pimienta', 'salsa bechamel', 'nueces',], cuisine: 'Uruguayan', dishType: 'snack', image: 'https://elgourmet.s3.amazonaws.com/recetas/share/tarta_qZEDFgPHQsGelh37irYd1njc0M9BWy.png', duration: 60, creator: 'Panda',})
+          .then(newRecipe => console.log('La nueva receta es', newRecipe))
+          .catch(err => console.log('ERROooooor ', err))
+
+    Recipe
+          .create(data)
+          .then(theNewRecipe => {
+            theNewRecipe.forEach(elm => console.log(`Esta receta se titula ${elm.title}`))
+            return Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration: 100},{new:true})
+          })
+          .then(reciepe => {
+            console.log(reciepe)
+            return Recipe.deleteOne({title:'Carrot Cake'})
+          })
+          .then(deleteRecipe => {
+            console.log('Se ha elimiado', deleteRecipe)
+            mongoose.connection.close()})
+          
+          .catch(err => console.log('ERROoooooor ', err))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

bd1d7cb04c3e1a32751f5c788961485fabee86c2
Adrian Fernandez Carmona <adrianfdezbcn@gmail.com>
2021-09-02 18:08:59 +0200

    WD082021- Adrian Fernandez

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
-
+const newRecipe = {title: "Tortilla", level: "Easy Peasy", ingredients: ["eggs", "oil"], cuisine: "Spanish", dishType:"main_course", duration: 30, creator: "me"}
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+let i=0
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useCreateIndex: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,8 +21,35 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    //Iteration 2
+    Recipe.create(newRecipe)
+    .then(recipe => console.log('The recipe is saved and its title is: ', recipe.title))
+    .catch(error => console.log('An error happened: ', error))
+    //Iteration 3
+    Recipe.insertMany(data)
+    .then(recipe => 
+      console.log('The recipe is saved '))
+    // .catch(error => 
+    //   console.log('An error happened: ', error))
+     
+    })
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    });
+  //3
+  Recipe.find()
+    .then(titlesRecipe => {
+      titlesRecipe.forEach(recipe => console.log('Title :', recipe.title))
+    })
+  //4 setTimeout porq me cargaba antes que la llamada al documento en si.
+setTimeout(() => {
+       Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100}, {new:true})
+        .then(console.log('Duration time changed!'))
+        .catch(error => console.log('Error', error))
+     }, 1000);
+  //Delete carrot cake
+setTimeout(() => {
+      Recipe.deleteOne({ title: 'Carrot Cake' })
+      .then(recipe => console.log('Deleted!!!!!', recipe))
+      .catch(error => console.log('Error, cant delete', error));
+}, 1000);

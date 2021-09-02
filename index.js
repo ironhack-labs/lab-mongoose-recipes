49f978083853cfa4bfecd73ca7528b618b7aa130
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-02 18:05:26 +0200

    first

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    //useCreateIndex: true,
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,6 +21,32 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+   /* Recipe.create({
+      "title": "European Chicken Thighs",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      "cuisine": "Asian",
+      "dishType": "main_course",
+      "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      "duration": 40,
+      "creator": "Chef LePapu"
+    })
+   .then(recipe => console.log(recipe.title))*/
+   return Recipe.insertMany(data)
+  })
+  .then((recipes) => {
+    recipes.forEach(recipe => {
+      console.log(recipe.title)
+    });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

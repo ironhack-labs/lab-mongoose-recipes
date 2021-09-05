10643916296ce42384b4d20920291af56beb5fc0
simplePac <rober_alicante@hotmail.es>
2021-09-05 17:03:08 +0200

    WD082021 - Roberto Lorente
    Lab acabado, he tenido que pedir un poco de ayuda al final para que el index.js finalizará cuando realizara todos los procesos

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
+  .connect(MONGODB_URI, {})
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "San Agustin Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs",
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image:
+        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu",
+    });
+    return Recipe.insertMany(data)
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  .then((recipes) => {
+    recipes.forEach(recipe => {
+      console.log(recipe.title)
+    })
+    return recipes
+  })
+
+  .then((recipes) => {
+    Recipe.findOneAndUpdate( {name: 'Rigatoni alla Genovese'}, {duration: 100}).then(() => {
+      console.log("Succes Updating Duration!!")
+    })
+
+    Recipe.deleteOne({name: 'Carrot Cake'}).then(() => {
+      console.log('Deleted')
+      mongoose.disconnect().then(() => {
+        console.log('Disconnected')
+      })
+    })
+  })
+
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  })
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", {
+  title: {type: String, required: true, unique: true},
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef','UltraPro Chef']},
+  ingredients: {type: [String]},
+  cuisine: {type:String, required: true},
+  dishType: {type: String, enum: ["breakfast", "main_course", "soup", "nack", "drink", "dessert", "other"]},
+  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: {type: Number, min: 0},
+  creator: {type: String},
+  created: {type: Date, default: Date.now}
+});
 
 module.exports = Recipe;

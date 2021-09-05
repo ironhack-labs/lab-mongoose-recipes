bace31e0c25bd525a08ae3a08c3afcf915fd9b1d
Rahaf <r.abualhassan@gmail.com>
2021-09-05 19:34:52 +0200

    connection to the database established

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -12,7 +12,7 @@ const recipeSchema = new Schema({
     enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
 	},
 	ingredients: {
-		type: Array[ String ]
+		type:[{ type: String }]
 	},
 	cuisine: {
 		type: String,
@@ -21,18 +21,18 @@ const recipeSchema = new Schema({
 	dishType: {
 		type: String,
 		enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
-	}
+	},
   image: {
     type: String,
-    default: "https://images.media-allrecipes.com/images/75131.jpg"
-  }
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
   duration: {
     type: Number,
     min: 0
-  }
+  },
   creator: {
     type: String
-  }
+  },
   created: {
     type: Date,
     default: Date.now

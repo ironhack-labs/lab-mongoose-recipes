ff409bdf4eb90fd86f402be086d7c331e48e2b56
Tiago R <tiagobiagirebelato@gmail.com>
2021-09-09 08:27:59 -0300

    interacao-1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title:{
+    type:String,
+    requied:true,
+    unique:true,
+  },
+  level:{
+    type:String,
+    enum:['Easy Peasey','Amateur Chef','UltraPro Chef']
+  },
+  ingredients:{
+    type:[String]
+  },
+  cuisine:{
+    type:String,
+    required:true,
+  },
+  dishType:{
+    type:String,
+    enum:['breakfast','main_course','snack','drink','dessert','other']
+  },
+  image:{
+    type:String,
+    default:"https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration:{
+    type:Number,
+    min:0,
+  },
+  creator:{
+    type:String,
+  },
+  created:{
+    type:Date,
+    default:Date.now,
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

02311f06e28d71e1e24f8704e055f56ffed0c8d9
Arthur Melo <arthurmelo1000@gmail.com>
2021-09-09 01:31:23 -0300

    Interations Completed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb+srv://arthur_melo:P4lmeiras2021@cluster0.jx5nn.mongodb.net/recipe-app?retryWrites=true&w=majority';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -21,7 +21,25 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const recipe = { title: 'feijoada', ingredients: ['carne de porco', 'feijão preto', 'linguiça defumada', 'carne seca'], creator:'slaves', created:Date('1870')}
+
+    Recipe.create(recipe)
+      .then(recipe => console.log("A receita está salva no banco"))
+      .catch(error => console.log("Um erro ocorreu na conexão"))
+    
+    Recipe.insertMany(data)
+      .then(() => {console.log("Dados inseridos no banco")})
+      .catch(() => {console.log("Erro no acréscimo")})
+
+    Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration:100})
+      .then(() => {console.log("Alteração realizada no banco")})
+      .catch(() => {console.log("Erro na atualização")})
+    
+    Recipe.deleteOne({title:"Carrot Cake"})
+      .then(() => {console.log("Eliminação realizada no banco")})
+      .catch(() => {console.log("Erro na eliminação")})
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

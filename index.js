7288af3544abf6ec2d272434471ccae977097c88
John Doe <gustavo.sena0@gmail.comGustavo Barbosa Sena>
2021-09-10 10:14:48 -0300

    done up to an interaction 3

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+const newRecipe = {title: "torta", level: "Easy Peasy", ingredients: ["eggs", "oil"], cuisine: "portuguese", dishType:"main_course", duration: 30, creator: "me"}
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
-    useCreateIndex: true,
+   
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
@@ -24,4 +24,15 @@ mongoose
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  
+  Recipe.create(newRecipe)
+  .then(recipe => console.log('The recipe is saved and its title is: ', recipe.title))
+  .catch(error => console.log('An error happened: ', error))
+  
+  //Iteration 3
+  Recipe.insertMany(data)
+    .then(recipe => console.log('The recipe is saved '))
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    })

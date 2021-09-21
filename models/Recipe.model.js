5eb8045557ce1bda13901024c745e2aa14353d1e
Daniel td <danieltd@gmail.com>
2021-09-21 14:55:43 +0200

    update

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  level: String,
+  ingredients: [String],
+  cuisine: String,
+  dishType: String,
+  image: String,
+  duration: Number,
+  creator: String,
+  created: Date,
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

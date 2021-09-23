b61572c3624b2a06ac2e034e203a8f1a3ae35db7
sarebro <sarebro77@gmail.com>
2021-09-23 17:42:59 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
-const { deleteOne } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -21,13 +20,40 @@ mongoose
 		return Recipe.deleteMany();
 	})
 	.then(async () => {
+		await Recipe.create({
+			title: 'Tiramisu',
+			level: 'Easy Peasy',
+			ingredients: [ 'Mascarpone', 'Chocolate', 'Cream', 'Eggs' ],
+			cuisine: 'Italian',
+			dishType: 'dessert',
+			image: 'https://www.askchefdennis.com/wp-content/uploads/2011/04/Tiramisu10b.jpg',
+			duration: 10,
+			creator: 'Carminantonio Iannaccone',
+			created: new Date('12/24/1969')
+		});
+
 		await Recipe.insertMany(data);
 
-		Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }).then((res) => console.log(res));
+		let res = await Recipe.find({ title: 'Tiramisu' });
+		console.log(res[0].title);
 
-		Recipe.deleteOne({ title: 'Carrot Cake' }).then((res) => console.log(res));
+		//Iteration 4 - Update recipe
 
-		return mongoose.connection.dropDatabase();
+		let response = await Recipe.findOneAndUpdate(
+			{ title: 'Rigatoni alla Genovese' },
+			{ duration: 100 },
+			{ new: true }
+		);
+		console.log(response);
+
+		// Iteration 5 - Remove a recipe
+
+		let deleted = await Recipe.deleteOne({ title: 'Carrot Cake' });
+		console.log(deleted);
+
+		// Iteration 6 - Close the Database
+
+		mongoose.disconnect();
 	})
 	.catch((error) => {
 		console.error('Error connecting to the database', error);

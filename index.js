3f2282b256d23ca29409d65c4b066c55ea314c10
Dragan Cicic <your3mail>
2021-09-23 17:57:08 -0400

    Complited lab

--- index.js
+++ index.js
@@ -9,52 +9,56 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(async() => {
-    
-    await Recipe.insertMany(data)
+	.connect(MONGODB_URI, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then((self) => {
+		console.log(`Connected to the database: "${self.connection.name}"`);
+		// Before adding any recipes to the database, let's remove all existing ones
+		return Recipe.deleteMany();
+	})
+	.then(async () => {
+		await Recipe.create({
+			title: 'Tiramisu',
+			level: 'Easy Peasy',
+			ingredients: [ 'Mascarpone', 'Chocolate', 'Cream', 'Eggs' ],
+			cuisine: 'Italian',
+			dishType: 'dessert',
+			image: 'https://www.askchefdennis.com/wp-content/uploads/2011/04/Tiramisu10b.jpg',
+			duration: 10,
+			creator: 'Carminantonio Iannaccone',
+			created: new Date('12/24/1969')
+		});
 
-    await Recipe
-  
-// Update Recipe
+		await Recipe.insertMany(data);
 
+		let res = await Recipe.find({ title: 'Tiramisu' });
+		console.log(res[0].title);
 
-  Recipe.findOneAndUpdate(
-    { title: "Rigatoni alla Genovese" },
-    { duration: 100 }
-  )
-  .then((updated) => {
-    console.log("updatedRecipe")
-      return updated;
-    }
-    
-  )
+		//Iteration 4 - Update recipe
 
-  // Remove a recipe
+		let response = await Recipe.findOneAndUpdate(
+			{title: "Rigatoni alla Genovese"},
+			{duration: 100},
+			{new:true}
+			
+			)
+			console.log(response)
+		
+			// Iteration 5 - Remove a recipe
 
-  Recipe.deleteOne({title: "Carrot Cake"})
-  .then((recipe) => console.log("Deleted"))
+		let deleted = await Recipe.deleteOne(
+			{title: "Carrot Cake"}
+		)
+		console.log(deleted)
 
+		// Iteration 6 - Close the Database
 
+		mongoose.disconnect()
 
-  //Close the Database
-
-  .then(() => {
-    console.log("allRecipes")
-    return mongoose.connection.dropDatabase();
-  })
-
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
-  
+	})
+	.catch((error) => {
+		console.error('Error connecting to the database', error);
+	});
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,10 +3,18 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   title: String,
-  level: String,
+  level: {type: String, enem: ["Easy Peasy","Amateur Chef", "UltraPro Chef"]}
   ingredients: [ String ],
   cuisine: String,
-  dishType: String,
+  dishType: {type:String, enum:[
+    "breakfast",
+    "main course",
+    "soup",
+    "snack",
+    "drink",
+    "dessert",
+    "other"
+  ]}
   image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
   duration: { type: Number, min: 0} ,
   creator: String,

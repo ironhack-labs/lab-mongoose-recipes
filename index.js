d5b88092f3630fa07949be1c8db46f506399bb11
Dominika Lipka <dominika.lipka96@gmail.com>
2021-10-14 20:17:04 +0200

    Iteration 3 done

--- index.js
+++ index.js
@@ -17,23 +17,24 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
 
-    const newRecipe = {
-      title: 'Cacio e pepe',
-      level: 'Amateur Chef',
-      ingredients: ['spaghetti', 'butter', 'pepper', 'pecorino cheese', 'salt' ],
-      cuisine: 'italian',
-      dishType: 'main_course',
-      image: 'https://www.giallozafferano.com/images/228-22822/Spaghetti-Cacio-e-Pepe-Pecorino-and-black-pepper-spaghetti_1200x800.jpg',
-      duration: 20,
-      creator: 'Dominika',
-    }
-    console.log(newRecipe.title)
-    Recipe.create(newRecipe);
+    // const newRecipe = {
+    //   title: 'Cacio e pepe',
+    //   level: 'Amateur Chef',
+    //   ingredients: ['spaghetti', 'butter', 'pepper', 'pecorino cheese', 'salt' ],
+    //   cuisine: 'italian',
+    //   dishType: 'main_course',
+    //   image: 'https://www.giallozafferano.com/images/228-22822/Spaghetti-Cacio-e-Pepe-Pecorino-and-black-pepper-spaghetti_1200x800.jpg',
+    //   duration: 20,
+    //   creator: 'Dominika',
+    // }
+    // console.log(newRecipe.title)
+    // Recipe.create(newRecipe);
     // Before adding any recipes to the database, let's remove all existing ones
-    //return Recipe.deleteMany()
+
+    return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.insertMany(data)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

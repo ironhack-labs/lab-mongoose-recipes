0d7e55b90ada49f17bd47652ba8f04bca8b9eece
Kati Fe <k-felgitsch@hotmail.com>
2021-10-15 09:07:14 +0100

    Iteration 2 done

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
 let testRecipe = {
-name: "Mi Goreng"
+  name: "Mi Goreng"
 };
 
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
+// Connection to the database "recipe-app" // connection takes some time / async function
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
@@ -25,16 +25,11 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    Recipe
-    .create(testRecipe)
-    .then((testRecipe) =>{
-      console.log('our new recipe was successfully saved...testRecipe');
-    })
-    .catch((errorMsg) => {
-      console.log('ooops something went wrong')
-    });
-    })
-  
+    return Recipe.create(testRecipe);
+  })
+  .then((testRecipeObj) => {     // or when from DB--> testRecipeFromDB
+    console.log("our new recipe was successfully saved...testRecipe");
+  })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

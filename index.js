3e76d8d9bdb3ebdff540fd161a3e325e31194f85
andrekleine <andrekleine@gmail.com>
2021-10-28 09:39:37 -0300

    It 3

--- index.js
+++ index.js
@@ -23,33 +23,45 @@ mongoose
     // Run your code here, after you have insured that the connection was made
 
     //-----------------------------    ITERATION 2    -------------------------------
-    const myRecipe = new Recipe({
-      title: "Homus",
-      level: "Easy Peasy",
-      ingredients: [
-        "300 g de grão-de-bico",
-        "caldo de 1 limão",
-        "1 dente de alho descascado",
-        "100 g de tahine",
-        "azeite a gosto",
-        "sal e pimenta-do-reino a gosto",
-      ],
-      cuisine: "Arabe",
-      dishType: "main_course",
-      image: "https://cdn.panelinha.com.br/receita/994388400000-Homus.jpg",
-      duration: 120,
-      creator: "Andre",
-    });
+    // const myRecipe = new Recipe({
+    //   title: "Homus",
+    //   level: "Easy Peasy",
+    //   ingredients: [
+    //     "300 g de grão-de-bico",
+    //     "caldo de 1 limão",
+    //     "1 dente de alho descascado",
+    //     "100 g de tahine",
+    //     "azeite a gosto",
+    //     "sal e pimenta-do-reino a gosto",
+    //   ],
+    //   cuisine: "Arabe",
+    //   dishType: "main_course",
+    //   image: "https://cdn.panelinha.com.br/receita/994388400000-Homus.jpg",
+    //   duration: 120,
+    //   creator: "Andre",
+    // });
 
-    Recipe.create(myRecipe)
-      .then((recipe) =>
-        console.log("The recipe is saved and its value is: ", recipe)
-      )
-      .catch((error) =>
-        console.log("An error happened while saving a new recipe:", error)
-      );
+    // Recipe.create(myRecipe)
+    //   .then((recipe) =>
+    //     console.log(`The recipe "${recipe.title}" is saved.`)
+    //   )
+    //   .catch((error) =>
+    //     console.log("An error happened while saving a new recipe:", error)
+    //   );
 
     //-----------------------------    ITERATION 3    -------------------------------
+
+    // Recipe.insertMany(data)
+    //   .then((recipes) => {
+    //     recipes.forEach((recipe) => {
+    //       console.log(`The recipe "${recipe.title}" is saved.`);
+    //     });
+    //   })
+    //   .catch((error) =>
+    //     console.log("An error happened while saving the recipes:", error)
+    //   );
+
+    //-------------------------------------------------------------------------------
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);

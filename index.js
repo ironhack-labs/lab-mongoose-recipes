83c349e59bf4a119d4c02e9fca8b47280c261be9
Carol Nisaka <carolnisaka@gmail.com>
2021-10-30 15:24:30 -0300

    mudei a estrutura e fiz passo a passo do que eu precisava pra testar aprendizado

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
+//1) importando Mongoose
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
+//8) iportando o modelo
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const listRecipes = require('./data.json');
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+// console.log(listRecipes);
+//2)Fazer a conexao com banco
+const initConnection = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/model-create');
+    console. log('contectado ao banco')
+
+    
+
+    //criando a primeira receita - so depois disso aquii é que é criado automaticamente no Compass o database, a coleçao e o documento
+    const recipeTodatabase = new Recipe(listRecipes[5]);
+    await recipeTodatabase.save();
+    
+
+    //criar uma nova de outro jeito mais curto
+    const recipeTodatabase2 = await Recipe.create(listRecipes[6]);
+    console.log(recipeTodatabase2);
+
+    //criar varias receitas ao mesmo tempo - nao deu certo. Ver como fazer
+    // const recipesTodatabase = await Recipe.createCollection(listRecipes[2], listRecipes[3]);
+    // console.log(recipesTodatabase)
+
+    //procurando todas as receita
+    const recipiesFromDatabase = await Recipe.find();
+    console.log(recipiesFromDatabase)
+
+    //editando uma receita (busquei por titulo e alterei o titulo)
+    const upDateARecipe = await Recipe.updateOne({title:"Asian Glazed Chicken Thighs"}, {title: "Asian"});
+    
+    // //deletando uma receita - nao deu certo - ver como fazer
+    // const deleteARecipe = await Recipe.deleteMany({title: "Orange and Milk-Braised Pork Carnita" });
+
+    //deletando uma receita por ID
+    const deleteById = await Recipe.deleteOne({_id:'617d6e634fc7eb5eacf2f86f'});
+
+  } catch (error) {
+    console.log('erro ao conectar', error)
+  }
+};
+
+
+//3) Invocando a funão para conexao com banco
+initConnection();
+
+
+// Import of the model Recipe from './models/Recipe.model.js'
+// const Recipe = require('./models/Recipe.model');
+// // Import of the data from './data.json'
+// const data = require('./data');
+
+// const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+// // Connection to the database "recipe-app"
+// mongoose
+//   .connect(MONGODB_URI, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(self => {
+//     console.log(`Connected to the database: "${self.connection.name}"`);
+//     // Before adding any recipes to the database, let's remove all existing ones
+//     return Recipe.deleteMany()
+//   })
+//   .then(() => {
+//     // Run your code here, after you have insured that the connection was made
+//   })
+//   .catch(error => {
+//     console.error('Error connecting to the database', error);
+//   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,24 @@
+//4) importar Mongoose
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
+// const Schema = mongoose.Schema;
+
+//5) Fazer o modelo 
+const recipeSchema = new mongoose.Schema({
+  title: {type: String, require},
+  level: {type: String, enum:['Easy', 'Peasy', 'Amateur Chef','UltraPro Chef']},
+  ingredients: [String],//é uma array que recebe strings 
+  cuisine: {type: String, require},
+  dishType: {type: String, enum:['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']},
+  image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: {type: Number, min: 0},
+  creator: {type: String}
+}, {
+  timestamps: true,
 });
 
+//6)declarando que a coleçao Recipe vai seguir o recipeSchema
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
+//7) exportar o modelo
 module.exports = Recipe;

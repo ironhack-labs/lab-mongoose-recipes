22abe24bc11b6b4d68b8ee895a068c51b1e4d657
Piero de Sá <desa.piero@gmail.com>
2021-11-03 23:43:07 -0300

    Lab done following Henrique's video

--- index.js
+++ index.js
@@ -7,20 +7,74 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+// Iterations 2 & 3 can happen in parallel
+async function insertRoutines() {
+  const newRecipe = {
+    title: 'Henriques Pasta with sauce and meat',
+    level: 'Easy Peasy',
+    ingredients: ['Pasta', 'Tomatoes', 'Meat'],
+    cuisine: 'Italian',
+    dishType: 'main_course',
+    duration: 20,
+    creator: 'Henrique Mendes',
+  };
+
+  try {
+  const createRecipePromise = Recipe.create(newRecipe);
+  const manyNewRecipesPromise = Recipe.insertMany(data);
+  
+  const results = await Promise.all([createRecipePromise, manyNewRecipesPromise]);
+
+  console.log(`Inserted one new Recipe => ${results[0].title}`);
+  console.log('Finished inserting many Recipes');
+  results[1].forEach(recipe => console.log(recipe.title));
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Iterations 4 & 5 can happen in parallel
+async function updateAndDeleteRoutines() {
+  try {
+    // Iteration 4
+    const updatedRecipe = await Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { $set: { duration: 100 } },
+      { new: true, useFindAndModify: false },
+    );
+    console.log(`Successfully updated recipe "${updatedRecipe.title}" duration to ${updatedRecipe.duration} minutes`);
+  
+    // Iteration 5
+    await Recipe.deleteOne({ title: 'Carrot Cake' });
+    console.log('Recipe Carrot Cake deleted');
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // Before adding any documents to the database, let's delete all previous entries
+    return self.connection.dropDatabase();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async () => {
+    try {
+      // Iterations 2 & 3
+      await insertRoutines();
+      // Iterations 4 & 5
+      await updateAndDeleteRoutines();
+      // Iteration 6
+      mongoose.connection.close();
+    } catch (error) {
+      console.log(error)
+    }
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

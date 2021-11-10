e10a6aab825e3ce75dc986b3e805c0b43dd74b87
Lisa Medina Gutiérrez <lisa.medinag@gmail.com>
2021-11-10 17:08:51 +0100

    Final Submission

--- index.js
+++ index.js
@@ -5,23 +5,33 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(self => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+        // Before adding any recipes to the database, let's remove all existing ones
+        return Recipe.deleteMany()
+    })
+    .then(() => Recipe.syncIndexes())
+    .then(() => Recipe.create({ title: "papas arrugás", cuisine: "Canaria" }))
+    .then((recipesReturned) => console.log("wow que receta chula, es:", recipesReturned.title))
+    .then(() => Recipe.create(data))
+    .then((recipesCreated) => recipesCreated.forEach(eachRecipe => console.log("mira mis recetitas, atent@, me presento, soy : ", eachRecipe.title)))
+    .then(() => Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true }))
+    .then((updatedRecipe) => console.log("Succes, has updateado la receta :", updatedRecipe.title))
+    .then(() => Recipe.deleteOne({ title: "Carrot Cake" }, {new: true}))
+    .then((deletedRecipe) => console.log("Comilón, te comiste el Carrot Cake"))
+    .then(() => mongoose.connection.close())
+    .catch(error => console.error('Error connecting to the database', error));
+
+// Model 
+
+

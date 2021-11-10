6b3b8d76ffdda5ea5db5d0984959298c6a5e3832
AlexVonrez <alexvonrez1@gmail.com>
2021-11-10 16:19:13 +0100

    done

--- index.js
+++ index.js
@@ -19,7 +19,39 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  .then(() => Recipe.syncIndexes())
+  .then(() => { Recipe.create([{
+
+    "title": "Spaguetti Carbonara",
+    "level": "Amateur Chef",
+    "ingredients": [
+      "200 grs. Spaguetti",
+      "50 grs. Guanciale",
+      "200 grs. Grana Padano Cheese",
+      "4 eggs",
+      "1 teaspoon salt",
+      "4 teaspoons ground black pepper"
+    ],
+    "cuisine": "Italian",
+    "dishType": "main_course",
+    "image": "https://elmundoenrecetas.s3.amazonaws.com/uploads/recipe/picture/108/IMG_500px.jpg",
+    "duration": 100,
+    "creator": "Chef Guiseppe"
+  }])
+  .then((newrecipe)=> console.log(newrecipe))
+
+  .then(()=> Recipe.create(data))
+  .then((newrecipe)=> newrecipe.forEach(elm => console.log(elm.title)))
+
+  .then(()=> Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration:100},{new:true}))
+  .then(()=> (console.log('Tu receta ha sido actualizada!')))
+
+  .then(()=> Recipe.deleteOne({title:'Carrot Cake'}))
+  .then(()=> console.log('Se ha eliminado tu receta correctamente'))
+
+  .then(()=> mongoose.connection.close())
+  .then(()=> console.log('Hasta la vista,babe..!!'))
+
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {

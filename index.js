ccb1dc0e61a29db24c32b4092c8d0abc4ce638a0
Fernando Cardona <fernandocardona@MacBook-Pro-de-FernandoCardona.local>
2021-11-10 17:07:23 +0100

    Lab Done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,11 +17,35 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
+     
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      // TODO: write the schema
+      title :'Garlic chicken',
+      level:'Amateur Chef',
+      ingredients:[ '1 kg. frozen or fresh shrimp', '20 garlic cloves', '2 dried chillies (cayenne peppers also work)', 'Extra virgin olive oil (about 20 tablespoons)', 'Salt and freshly ground black pepper (to taste) '],
+      cuisine:'Tipical Spanish',
+      dishType:'main_course',
+      image:'https://www.recetasderechupete.com/wp-content/uploads/2017/08/Gambas-al-ajillo-768x527.jpg',
+      duration: 20,
+      creator:'SomeOne long... longTime a go' 
+    };
+    console.log(newRecipe.title);
+    return Recipe.create(newRecipe)
   })
+  .then (() => console.log('Created a new recipe', Recipe))
+  .then(recipe => {
+    console.log(data);
+    return Recipe.create(data)
+  })
+  .then(() => Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, { duration:100 }, {new: true} ))
+  .then(recipe => console.log('update Rigatoni alla Genovese duration', recipe))
+  .then(() => Recipe.deleteOne({title:'Carrot Cake'}))
+  .then(recipe => console.log(' Deleted Carrot Cake', recipe))
+  .then(() => mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

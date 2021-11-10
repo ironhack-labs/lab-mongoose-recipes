2469892136d07e8b3ea93d66f3d4829109daa124
Analia Lopez <analialopezrodriguez@gmail.com>
2021-11-10 16:13:05 +0100

    Lab terminado

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
+
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
@@ -19,9 +20,38 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+
+  .then(() => Recipe.syncIndexes())
+
+  .then(() => Recipe.create({
+     title: 'Pure de coliflor', 
+     level: 'Easy Peasy', 
+     ingredients: ['coliflor', 'caldo', 'nata', 'especias' ], 
+     cuisine: 'Española', 
+     dishType: 'breakfast', 
+     duration: 1, 
+     creator: 'Analía López' 
+    }))
+
+  .then((newrecipe) => console.log(newrecipe))
+
+  .then(() => Recipe.create (data))
+
+  .then((newrecipe) => newrecipe.forEach(elm => console.log(elm.title)))
+
+  .then(() => Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 }))
+
+  .then(() => console.log("exito, su receta ha sido actualizada en la duracion"))
+
+  .then(() => Recipe.deleteOne({ title: "Carrot Cake" }))
+
+  .then(() => console.log("Se ha eliminado correctamente la receta"))
+
+  .then(() => mongoose.connection.close())
+
+  .then(() => console.log("Hasta Pronto...."))
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

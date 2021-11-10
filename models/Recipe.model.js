26d2b81d3168de9b2d45fe9045dcd9af0688343a
RaselJam <Rasel.jam.h@gmail.com>
2021-11-10 12:20:51 +0100

    Added Recipe  Model

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,43 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+	// TODO: write the schema
+	title: String,
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+	level: {
+		type: String,
+		enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+	},
+	ingredients: {
+		type: Array[String],
+	},
+	cusine: {
+		type: String,
+		required: true,
+	},
+	dishType: {
+		type: String,
+		enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'],
+	},
+	image: {
+		type: String,
+    default :'https://images.media-allrecipes.com/images/75131.jpg",
 
-module.exports = Recipe;
+	},
+  duration  :{
+    typ:Number,
+    min : 0
+  },
+  creator :{
+    type:String
+  },
+created :{
+  type : Date,
+  default: Date.now()
+}
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema)
+
+module.exports = Recipe

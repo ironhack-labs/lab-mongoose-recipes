510ef24ca5e67baf83589f82694c29a46884c5e2
Adrian Ramirez <ramiire@gmail.com>
2021-11-10 17:20:48 +0100

    done

--- index.js
+++ index.js
@@ -19,9 +19,32 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    return Recipe.create(
+      {
+        title: "Lemon duck",
+        level: "Amateur Chef",
+        ingredients: "Duck, lemon and salt",
+        cuisine: "chinese",
+        dishType: 'main_course',
+        duration: 30,
+        creator: 'Adrian'
+      })
   })
+  .then(duckRecipe => {
+    console.log('la receta es', duckRecipe.title)
+    return Recipe.create(data)
+  })
+  .then(recipes => {
+    console.log("el nombre es", recipes)
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese'}, {duration: 100})
+  })
+  .then(() => Recipe.deleteOne({title: "Carrot Cake"}))
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+mongoose.connection.close()

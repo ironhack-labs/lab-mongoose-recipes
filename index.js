f780909cf387f1071d7cabd7c3ee6d428118f53f
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-10 14:05:44 +0100

    Complete iterations 1 to 5. Pause for lunch time

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require('./models/Recipe.model')
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data')
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app'
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
+  .then((self) => {
+    console.log(`Connected to the database: "${self.connection.name}"`)
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // ITERATION 2
+    return Recipe.create({
+      title: 'Espaguetis a la Carbonara',
+      level: 'Easy Peasy',
+      ingredients: ['Espaguetis', 'Cebolla', 'Bacon', 'Nata', 'Huevo', 'Sal'],
+      cuisine: 'Italian',
+      dishType: 'main_course',
+      duration: 45,
+    })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  // ITERATION 3
+  .then(() => Recipe.create(data))
+  .then((recipes) =>
+    recipes.forEach((recipe) =>
+      console.log("You've created the following new recipe: ", recipe.title)
+    )
+  )
+  // ITERATION 4
+  .then(() =>
+    Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 }
+    )
+  )
+  .then((recipe) =>
+    console.log("Success! You've modified the recipe: ", recipe.title)
+  )
+  // ITERATION 5
+  .then(() => Recipe.deleteOne({ title: 'Carrot Cake' }))
+  .then((info) => console.log('Success deleting item', info))
+  // ITERATION 6
+
+  .catch((error) => {
+    console.error('Error connecting to the database', error)
+  })

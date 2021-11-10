0bef89e6db7f82f3e981f9a772b30af5a2dd2733
Carlos P <carlos_dic1985@hotmail.com>
2021-11-10 17:26:13 +0100

    done

--- index.js
+++ index.js
@@ -19,9 +19,41 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe
+      .create([
+        {
+          title: 'Fried eggs',
+          level: 'Amateur Chef',
+          ingredients: [
+            '2 Egg',
+            'olive oil',
+            '1 garlic clove'
+          ],
+          cuisine: 'Spanish',
+          disthType: 'other',
+          image: 'https://cocina-casera.com/wp-content/uploads/2011/10/como-hacer-huevo-frito.jpg',
+          duration: 5,
+          creater: 'Carlos P',
+          created: '10/11/2021'
+        }
+      ])
   })
+
+  .then(theNewRecipe => console.log('Se ha creado esta receta:', theNewRecipe))
+
+  .then(() => Recipe.create(data))
+  .then(theNewRecipes => console.log('Se han creado estas recetas:', theNewRecipes))
+
+  .then(() => Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true }))
+  .then(theUpdateRecipe => console.log("The update recipe it is:", theUpdateRecipe))
+
+  .then(() => Recipe.findOneAndDelete({ title: 'Carrot Cake' }))
+  .then(theDeleteRecipe => console.log("The delete recipe it is:", theDeleteRecipe))
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+mongoose.connection.close()

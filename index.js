3032a82bf3fb9129159ff8bf3347114c4f40fe8b
besaga <bsgallardo93@gmail.com>
2021-11-10 22:52:51 +0100

    final

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -19,9 +19,40 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
+  .then(() => Recipe.syncIndexes())
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+    Recipe
+      .create([
+        {
+          title: 'Spanish Omelette',
+          level: 'Easy Peasy',
+          ingredients: ['Potatoes', 'eggs', 'olive oil'],
+          cuisine: 'Spanish',
+          dishtype: 'breakfast',
+          image: "https://images.media-allrecipes.com/images/75131.jpg",
+          duration: 60,
+          creator: 'Begonia',
+          created: 20,
+        }
+      ])
+
   })
+  .then(theNewRecipe => console.log('Se ha creado estos registros:', theNewRecipe))
+  .then(() => Recipe.create(data))
+  .then(theNewRecipes => console.log('Se ha creado estos registros:', theNewRecipes))
+  .then(() => Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true }))
+  .then(duck => console.log("El perro modificado es:", duck))
+  .then(() => Recipe.findOneAndDelete({ title: 'Carrot Cake' }))
+  .then(duck => console.log("El perro modificado es:", duck))
+
+
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+mongoose.connection.close()
+

9d0364d500c7589ecf983bd153e123aad3d5e535
Alex Lopez <alexlopezz21@gmail.com>
2021-11-10 17:19:58 +0100

    done

--- index.js
+++ index.js
@@ -19,9 +19,55 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe
+      .create([
+          { "title": "Donald Trump", "level": "Easy Peasy", "ingredients": ["5 carrot", "1 human", "1 cup of patriotism"], cuisine: "American", dishType: "breakfast", image: "https://images.media-allrecipes.com/images/75131.jpg", duration: 6 , creator: 'Alejandro' },
+      ])
   })
+  .then(newRecipe => console.log('Se ha creado estos registros:', newRecipe))
+  .then(() => Recipe.create(data))
+  //.then(() => Recipe.findByIdAndUpdate('618be79ea8a4e05e1108d43e', { $inc: { duration: 100 } }, { new: true }))
+  .then(()=> Recipe.updateOne({ duration: 220 }, { duration: 100 }, { new: true }))
+  .then(info => console.log("Los detalles de la modificación son:", info))
+  .then(()=> Recipe.deleteOne({ title : "Carrot Cake"}))
+  .then(() => console.log("Receta eliminada"))
+ 
+  .then(() => mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /* { name: 'Lucas', age: 12, city: 'Madrid', color: 'Blanco' },
+        { name: 'Juanito', age: 14, city: 'Estepona', color: 'Grisito' },
+        { name: 'Jorgito', age: 1, city: 'Salamanca', color: 'Marroncito' },
+        { name: 'Jaimito', age: 16, city: 'Bilbao', color: 'Toffe y caramelo' },
+        { name: 'Foie Gras', age: 20, city: 'Bilbao', color: 'Paté' }
+
+    ])
+    .then(theNewPatos => console.log('Se ha creado estos registros:', theNewPatos))
+    .catch(err => console.log('Se ha producido un error:', err))
+ */

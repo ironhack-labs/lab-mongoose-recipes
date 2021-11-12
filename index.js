5f50794f694d528dc07bb081de4c69e30a75deca
Luisfdearaujo <90002192+Luisfdearaujo@users.noreply.github.com>
2021-11-12 17:55:16 +0000

    Lab finished

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
+    const newRecipe = {
+      title: "Bacalhau",
+      level: "Easy Peasy",
+      ingredients: ["ovos", "batatas"],
+      cuisine: "Portuguesa",
+      dishType: "fish",
+      image:
+        "https://fh-sites.imgix.net/sites/2712/2020/07/16140235/3.-bacalhau-a-bras.jpg?auto=compress%2Cformat&w=1000&h=1000&fit=max",
+      duration: 30,
+      creator: "chef Antonio",
+      created: new Date(),
+    };
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create(newRecipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((createdRecipe) => {
+    console.log(createdRecipe);
+    const pr = Recipe.insertMany(data);
+    return pr;
+  })
+  .then((manyRecipes) => {
+    console.log(manyRecipes);
+    const pr = Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then(() => {
+    const pr = Recipe.deleteOne({ title: "Carrot Cake" });
+    return pr;
+  })
+  .then(() => {
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    console.log("connection closed");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

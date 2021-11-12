f0d61de6e1528f1b0740596359a978cb1e9f2ecb
nachbr91 <nabero91@gmail.com>
2021-11-12 21:48:55 +0100

    Uncomment iteration 2

--- index.js
+++ index.js
@@ -24,37 +24,37 @@ connectToMongo();
 
 // ITERATION 2
 
-// const createRecipe = async () => {
-//   try {
-//     await Recipe.deleteMany();
-//     const response = await Recipe.create({
-//       title: 'Asian Glazed Chicken Thighs',
-//       level: 'Amateur Chef',
-//       ingredients: [
-//         '1/2 cup rice vinegar',
-//         '5 tablespoons honey',
-//         '1/3 cup soy sauce (such as Silver Swan®)',
-//         '1/4 cup Asian (toasted) sesame oil',
-//         '3 tablespoons Asian chili garlic sauce',
-//         '3 tablespoons minced garlic',
-//         'salt to taste',
-//         '8 skinless, boneless chicken thighs',
-//       ],
-//       cuisine: 'Asian',
-//       dishType: 'main_course',
-//       image:
-//         'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
-//       duration: 40,
-//       creator: 'Chef LePapu',
-//     });
-//     const closeMongoose = await mongoose.connection.close( () => {
-//     console.log('Mongoose default connection closed')
-//     });
-//     console.log(response.title);
-//   } catch (err) {
-//     console.log('Error:', err);
-//   }
-// };
+const createRecipe = async () => {
+  try {
+    await Recipe.deleteMany();
+    const response = await Recipe.create({
+      title: 'Asian Glazed Chicken Thighs',
+      level: 'Amateur Chef',
+      ingredients: [
+        '1/2 cup rice vinegar',
+        '5 tablespoons honey',
+        '1/3 cup soy sauce (such as Silver Swan®)',
+        '1/4 cup Asian (toasted) sesame oil',
+        '3 tablespoons Asian chili garlic sauce',
+        '3 tablespoons minced garlic',
+        'salt to taste',
+        '8 skinless, boneless chicken thighs',
+      ],
+      cuisine: 'Asian',
+      dishType: 'main_course',
+      image:
+        'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+      duration: 40,
+      creator: 'Chef LePapu',
+    });
+    const closeMongoose = await mongoose.connection.close( () => {
+    console.log('Mongoose default connection closed')
+    });
+    console.log(response.title);
+  } catch (err) {
+    console.log('Error:', err);
+  }
+};
 // createRecipe();
 
 // ITERATION 3

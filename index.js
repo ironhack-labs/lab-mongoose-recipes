5dc4edd60351a7b77cbc60394558be6321bfca18
nachbr91 <nabero91@gmail.com>
2021-11-12 20:23:47 +0100

    Iteration 3 done

--- index.js
+++ index.js
@@ -22,59 +22,49 @@ const connectToMongo = async () => {
 };
 connectToMongo();
 
-// const deleteManyRecipe = async () => {
+// ITERATION 2
+
+// const createRecipe = async () => {
 //   try {
-//     const response = Recipe.deleteMany()
-//     console.log(response)
-//   } catch(err) {
-//     console.log('Error:', err)
+//     await Recipe.deleteMany();
+//     const response = await Recipe.create({
+//       title: 'Asian Glazed Chicken Thighs',
+//       level: 'Amateur Chef',
+//       ingredients: [
+//         '1/2 cup rice vinegar',
+//         '5 tablespoons honey',
+//         '1/3 cup soy sauce (such as Silver Swan®)',
+//         '1/4 cup Asian (toasted) sesame oil',
+//         '3 tablespoons Asian chili garlic sauce',
+//         '3 tablespoons minced garlic',
+//         'salt to taste',
+//         '8 skinless, boneless chicken thighs',
+//       ],
+//       cuisine: 'Asian',
+//       dishType: 'main_course',
+//       image:
+//         'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+//       duration: 40,
+//       creator: 'Chef LePapu',
+//     });
+//     console.log(response.title);
+//   } catch (err) {
+//     console.log('Error:', err);
 //   }
-// }
-// deleteManyRecipe();
+// };
+// createRecipe();
+
+// ITERATION 3
 
 const createRecipe = async () => {
   try {
     await Recipe.deleteMany();
-    const response = await Recipe.create({
-      title: 'Asian Glazed Chicken Thighs',
-      level: 'Amateur Chef',
-      ingredients: [
-        '1/2 cup rice vinegar',
-        '5 tablespoons honey',
-        '1/3 cup soy sauce (such as Silver Swan®)',
-        '1/4 cup Asian (toasted) sesame oil',
-        '3 tablespoons Asian chili garlic sauce',
-        '3 tablespoons minced garlic',
-        'salt to taste',
-        '8 skinless, boneless chicken thighs',
-      ],
-      cuisine: 'Asian',
-      dishType: 'main_course',
-      image:
-        'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
-      duration: 40,
-      creator: 'Chef LePapu',
-    });
-    console.log(response.title);
+    const response = await Recipe.create(data);
+    response.forEach((recipe) => {
+      console.log(recipe.title)
+    })
   } catch (err) {
     console.log('Error:', err);
   }
 };
 createRecipe();
-
-// mongoose.connect(MONGODB_URI, {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(self => {
-//     console.log(`Connected to the database: "${self.connection.name}"`);
-//     // Before adding any recipes to the database, let's remove all existing ones
-//     return Recipe.deleteMany()
-//   })
-//   .then(() => {
-//     // Run your code here, after you have insured that the connection was made
-//   })
-//   .catch(error => {
-//     console.error('Error connecting to the database', error);
-//   });

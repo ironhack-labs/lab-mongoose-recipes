d5a4e8ea32e895e4cba245716e39149214aca13b
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-12 15:47:24 +0100

    Iteration 6. Refactor and lab done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,14 +15,14 @@ mongoose
     useUnifiedTopology: true,
     // useFindAndModify: false
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     // return Recipe.deleteMany()
   })
   .then(() => {
     const createRecipe = async () => {
-      try{
+      try {
         const newRecipe = await Recipe.create({
           title: "Asian Glazed Chicken Thighs",
           level: "Amateur Chef",
@@ -34,58 +34,60 @@ mongoose
             "3 tablespoons Asian chili garlic sauce",
             "3 tablespoons minced garlic",
             "salt to taste",
-            "8 skinless, boneless chicken thighs"
+            "8 skinless, boneless chicken thighs",
           ],
           cuisine: "Asian",
           dishType: "main_course",
-          image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+          image:
+            "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
           duration: 40,
-          creator: "Chef LePapu"
-        })
-        console.log(newRecipe.title)
-      } catch(err) {
-        console.log('err', err)
+          creator: "Chef LePapu",
+        });
+        console.log(newRecipe.title);
+      } catch (err) {
+        console.log("err", err);
       }
-    }
-  
+    };
+
     // createRecipe()
 
     const createManyRecipes = async () => {
-      try{
-        const newRecipes = await Recipe.insertMany(data)
+      try {
+        const newRecipes = await Recipe.insertMany(data);
         // console.log(newRecipes)
-      } catch(err) {
-        console.log('err', err)
+      } catch (err) {
+        console.log("err", err);
       }
     };
 
     // createManyRecipes();
 
     const updateDuration = async (name, newDuration) => {
-      try{
-        const updatedRecipe = await Recipe.findOneAndUpdate({title: name}, {duration: newDuration}, {new: true})
-        console.log(updatedRecipe)
-      } catch(err) {
-        console.log('err', err)
+      try {
+        const updatedRecipe = await Recipe.findOneAndUpdate(
+          { title: name },
+          { duration: newDuration },
+          { new: true }
+        );
+        console.log(updatedRecipe);
+      } catch (err) {
+        console.log("err", err);
       }
     };
 
     // updateDuration("Rigatoni alla Genovese", 100);
 
     const removeRecipe = async (name) => {
-      try{
-        const remove = await Recipe.deleteOne({title: name})
-        console.log(`${remove.deletedCount} recipe removed => ${name}`)
-      } catch(err) {
-        console.log('err', err)
+      try {
+        const remove = await Recipe.deleteOne({ title: name });
+        console.log(`${remove.deletedCount} recipe removed => ${name}`);
+      } catch (err) {
+        console.log("err", err);
       }
     };
 
     removeRecipe("Carrot Cake");
-    
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
-
-

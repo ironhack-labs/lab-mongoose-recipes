8c2a408e916f6c8f6a6d4eba2375932c0a09be4d
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-11-12 15:57:08 +0100

    Iteration 2 completed

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+// Import mongoose to index.js 
+
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -8,20 +11,45 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+const connectToMongo = async()=>{
+  try{
+  await  mongoose.connect('mongodb://localhost:27017/recipe-app',{
+   useCreateIndex:true,
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+}) 
+console.log('Connected to the database: recipe-app 🥘 ')
+
+   }catch(err){
+       console.log ('Error connecting to the database', error)
+   }
+
+}
+
+connectToMongo()
+
+
+// Create recipe 
+
+const createRecipe = async()=>{
+  try{
+    const recipe = await Recipe.create({
+      title: "Carrot Cake",
+      level:"Easy Peasy",
+      ingredients:"flour, eggs, sugar, carrots, and baking powder ",
+      cuisine: "English",
+      dishType:"dessert",
+      image:"https://www.llopartec.com/sites/default/files/styles/12col169custom_user_lg_1x/public/imatges/blog/carrot-cake.jpg?itok=QKx6xn6q&timestamp=1620719367",
+      duration:120,
+      creator:"Antoine Beauvilliers",
+      created:08/05/1814,
+    })
+    console.log (recipe.title)
+  } catch(error){
+    console.log ('Error connecting to the database', error)
+}
+}
+
+// createRecipe()
+

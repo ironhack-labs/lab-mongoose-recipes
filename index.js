21d8998779faf82f0ab9dbbd6d07e38768af3057
Aarón López <n_aaron5@hotmail.com>
2021-11-12 15:34:41 +0100

    Iteration 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const chalk = require('chalk')
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
@@ -8,20 +9,68 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+// mongoose
+//   .connect(MONGODB_URI, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(self => {
+//     console.log(`Connected to the database: "${self.connection.name}"`);
+//     // Before adding any recipes to the database, let's remove all existing ones
+//     return Recipe.deleteMany()
+//   })
+//   .then(() => {
+
+//   })
+//   .catch(error => {
+//     console.error('Error connecting to the database', error);
+//   });
+  
+  const connectToMongo = async ()=>{
+    try {
+      await mongoose.connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+      })
+      
+      console.log(`Connected to the database`)
+    
+    } catch(err) {
+      console.log(chalk.bgRed('Error connecting to the database', err))
+    }
+  }
+
+  connectToMongo()
+
+  const createRecipe = async ()=>{
+    try {
+      await Recipe.deleteMany()
+      const recipe = await Recipe.create({
+        "title": "Asian Glazed Chicken Thighs",
+        "level": "Amateur Chef",
+        "ingredients": [
+          "1/2 cup rice vinegar",
+          "5 tablespoons honey",
+          "1/3 cup soy sauce (such as Silver Swan®)",
+          "1/4 cup Asian (toasted) sesame oil",
+          "3 tablespoons Asian chili garlic sauce",
+          "3 tablespoons minced garlic",
+          "salt to taste",
+          "8 skinless, boneless chicken thighs"
+        ],
+        "cuisine": "Asian",
+        "dishType": "main_course",
+        "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+        "duration": 40,
+        "creator": "Chef LePapu"
+      })
+      console.log(recipe)
+    } catch(err){
+      console.log(chalk.bgRed(err))
+    }
+  }
+
+  createRecipe()
+  
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  name: {type: String, required: true, unique: true},
-  level: {type: String, enum:['Easy Peasy', 'Amateur Chef-UltraPro Chef']},
+  title: {type: String, required: true, unique: true},
+  level: {type: String, enum:['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
   ingredients: {type: [String]},
   cuisine: {type: String, required: true},
   dishType: {type: String, enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']},

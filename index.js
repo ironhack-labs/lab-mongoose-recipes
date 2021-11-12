33876583b68518618617e00617cba0d56f1964c2
Lilli Reilly <lilli.reilly@gmail.com>
2021-11-12 20:10:59 +0100

    Completed lab

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  .then(async() => {
     // Run your code here, after you have insured that the connection was made
-     Recipe.create({
+    /* Recipe.create({
       title: "Kimchi Fried Rice",
       level: "Easy Peasy",
       ingredients: ["kimchi", "rice", "carrot", "soy sauce", "sesame oil", "egg"],
@@ -29,8 +29,18 @@ mongoose
       dishType: "main_course",
       duration: 15,
       creator: "unknown",
-    })
-    console.log(Recipe.title)
+    })*/
+
+    const addRecipes = await Recipe.insertMany(data);
+    const brokenRecipe = {title: "Rigatoni alla Genovese"};
+    const fixedRecipe = await Recipe.findOneAndUpdate(brokenRecipe, {$set: {duration: 100 }},
+      { new: true }
+      );
+      console.log(fixedRecipe)
+      const carrotCake = {title: "Carrot Cake"};
+      const deleteCake = await Recipe.deleteOne(carrotCake);
+      console.log("Carrot Cake deleted")
+  
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

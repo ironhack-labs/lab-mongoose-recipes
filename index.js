2777bc256f88b1f476bd88fb2eb6e6a51573a513
JosePedro <guedes.jose@gmail.com>
2021-11-13 10:57:56 +0000

    finallabmongoose

--- index.js
+++ index.js
@@ -20,8 +20,60 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const recipe1= {
+    title: "Lasanha", 
+    level: "Easy Peasy", 
+    ingredients: "pasta",
+    cuisine: "italian", 
+    dishType: "main_course",
+    image: "https://images.media-allrecipes.com/images/75131.jpg", 
+    duration: 5, 
+    creator: "josé"
+  };
+  const pr = Recipe.create(recipe1);
+    return pr;
+})
+  .then((createRecipe1) => {
+    console.log(createRecipe1)
   })
+  .then((updatedRecipe) => {
+    //console.log(updatedRecipe.title)
+    const allRecipes = Recipe.insertMany(data);
+    return allRecipes;
+  })
+  .then((insertedResults)=> {
+    console.log(insertedResults);
+
+    const pr = Recipe.find({ tile: "The Rigatoni alla Genovese" });
+    return pr;
+  })
+  .then((foundRecipe)=>{
+    console.log(foundRecipe)
+    const pr = Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"},
+    {duration: 100},{ new:true})
+    return pr;
+  })
+  .then((updatedRecipe) => {
+    console.log(updatedRecipe)
+    const pr = Recipe.deleteOne({ title: "Carrot Cake"});
+    return pr;
+  })
+  .then((deletedRecipe)=> {
+    console.log("deletedRecipe")
+  })
+  .then((result) => {
+    console.log(`result`, result);
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    console.log('connection closed!');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

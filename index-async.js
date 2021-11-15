334e742a210106dd2939e7c2a346bbb5f928bcfd
Judyta Filipkowska <judytafilipkowska@gmail.com>
2021-11-15 09:06:33 +0000

    done

--- index-async.js
+++ index-async.js
@@ -1,24 +1,20 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 const data = require('./data.json');
-
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+async function runCode() {
+    try {
+        await mongoose
+            .connect(MONGODB_URI, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+    
+        await Recipe.deleteMany()
+  
   })
   /*.then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -43,7 +39,7 @@ mongoose
     console.log('foundRecipe', foundRecipe);
 
     const prz = Recipe.findByIdAndUpdate(
-      foundRecipe[0]._id, {duration: 220}, { new: true}
+      foundRecipe[0]._id, {duration: 220} 
     );
     return prz;
   })

d9671ae447ee5a26f2dd925705262a321b08e4be
Miguel Garcia <gunsandgamer@hotmail.com>
2021-11-19 00:30:57 -0600

    Resuelto hasta iteracion 3

--- index.js
+++ index.js
@@ -9,19 +9,49 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(self => {
+        console.log(`Connected to the database: "${self.connection.name}"`);
+        // Before adding any recipes to the database, let's remove all existing ones
+        return Recipe.deleteMany()
+    })
+
+const nuevaReceta = {
+    title: "Rancher Eggs",
+    level: "Beginner Cook",
+    ingredients: ["4 eggs",
+        "1 Onion",
+        "3 Chilies",
+        "3 Tomatos",
+        "1 Spoon with a pinch of salt",
+        "1 Garlic clove",
+        "3-4 Tortillas",
+        "Cooking oil"
+    ],
+    cuisine: "Mexican",
+    dishType: "Breakfast",
+    image: "https://www.guacamouly.com/wp-content/uploads/2021/05/Huevos-rancheros-750x394.jpg.webp",
+    duration: 30,
+    creator: "Cook Miguel",
+    created: "19-11-21",
+}
+
+const createMultipleRecipes = async() => {
+    try {
+        const recipes = await Recipe.insertMany(data)
+        console.log(recipes.title)
+    } catch (error) {
+        console.log('Error connecting to the database', error)
+    }
+}
+
+then(() => {
+        // Run your code here, after you have insured that the connection was made
+    })
+    .catch(error => {
+        console.error('Error connecting to the database', error);
+    });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
+const { stringify } = require('querystring');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+    // TODO: write the schema
+    title: String,
+    level: String,
+    ingredients: [String],
+    cuisine: String,
+    dishType: String,
+    image: String,
+    duration: Number,
+    creator: String,
+    created: Date,
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

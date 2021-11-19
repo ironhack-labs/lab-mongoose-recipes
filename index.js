7f3cd8018fa65ad41060eccdfe88a2c4cb44d2c8
KuraiTsukino <sandym3000@hotmail.com>
2021-11-19 00:10:50 -0600

    Iteración 2

--- index.js
+++ index.js
@@ -18,9 +18,35 @@ mongoose
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
-  })
-  .then(() => {
+  }) 
+  .then( async (req, res) => {
     // Run your code here, after you have insured that the connection was made
+      const newRecipe = ({
+        "title": "Carrot Cake",
+        "level": "Amateur Chef",
+        "ingredients": [
+          "6 cups grated carrots",
+          "1 cup brown sugar",
+          "1 cup raisins",
+          "4 eggs",
+          "1 1/2 cups white sugar",
+          "1 cup vegetable oil",
+          "2 teaspoons vanilla extract",
+          "1 cup crushed pineapple, drained",
+          "3 cups all-purpose flour",
+          "1 1/2 teaspoons baking soda",
+          "1 teaspoon salt",
+          "4 teaspoons ground cinnamon"
+        ],
+        "cuisine": "International",
+        "dishType": "dessert",
+        "image": "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
+        "duration": 130,
+        "creator": "Chef Nadia"
+      }) 
+      
+      const recipeCreated = await Recipe.create(newRecipe)
+        console.log (newRecipe.title)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

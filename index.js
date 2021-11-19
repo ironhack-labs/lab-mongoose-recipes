f3f1f5d46273b3ef33f99d7bf174945480d99f54
SinuheJardinez <sinuhe.jh@gmail.com>
2021-11-18 22:54:34 -0600

    resuelto hasta iteracion 3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
-
+const Schema = mongoose.Schema;
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -16,11 +16,38 @@ mongoose
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
+
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    //return Recipe.deleteMany()
+    
   })
-  .then(() => {
+  .then(async(req, res) => {
     // Run your code here, after you have insured that the connection was made
+    //agregando una nueva receta
+    const myNewRecipe = {
+      title: "Lemon Pie",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup light brown sugar",
+        "1 large egg",
+        "2 Philadelphia Cheese",
+        "1 1/4 Marias Cookies",
+        "15 lemon slices"
+      ],
+      cuisine: "French",
+      dishType: "dessert",
+      image: "https://paydelimon.org/wp-content/uploads/2017/12/10.-relleno-de-lemon-pie.jpg",
+      duration: 50,
+      creator: "Chef Sinuhe"
+    }
+    //const recipeCreated = await Recipe.create(myNewRecipe);
+    //console.log(recipeCreated.title);
+    //agregando todas las recetas a partir del Recipe y obteniendo el data.json
+
+    const allRecipes = await Recipe.insertMany(data)
+    
+    console.log(allRecipes.title); //no puedo imprimir los titles aun 
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+  title:{type:String},
+  level:{type:String},
+  ingredients:[ String ],
+  cuisine:{type:String,required: true},
+  dishType:{type:String},
+  image: {type:String, default:'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration:{type:Number, min:0},
+  creator: {type:String},
+  created:{type:Date}
+
   // TODO: write the schema
 });
 

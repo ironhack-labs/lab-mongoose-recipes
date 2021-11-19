b7fa9b5801b4ddf7d65984add9ceb8201a659c39
Martin Giura <martin.giura@gmail.com>
2021-11-19 00:11:10 -0600

    Iteration 1 to 3. Returns an undefined at console log of recipe title that needs to be checked

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+const Schema = mongoose.Schema
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -19,8 +21,35 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async(req,res) => {
+
+  // Agregamos nueva receta
+
+  const myNewRecipe = {
+      title: "Chocolate Chip Cookies",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup light brown sugar",
+        "1 large egg",
+        "2 tablespoons milk",
+        "1 1/4 teaspoons vanilla extract",
+        "2 cups semisweet chocolate chips"
+      ],
+      cuisine: "French",
+      dishType: "dessert",
+      image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4398987.jpg&w=596&h=399.32000000000005&c=sc&poi=face&q=85",
+      duration: 30,
+      creator: "Chef Jennifer"
+    }
+
+    const recipeCreated = await Recipe.create(myNewRecipe);
+    console.log(recipeCreated.title);
+  
+
+// Agregamos todas las recetas
+
+const allRecipes = await Recipe.insertMany(data)
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+
+/**
+ * MODELOS
+ * Los modelos son moldes que nos permiten determinar todas las propiedades que debe de tener una petición de creación hacia la base de datos.
+ * Los requisitos para generar un gato, en este caso.
+ */

9caea68c9a46412090ce8b43a61eca9ac47a1146
carloscordon82 <carloscordon82@gmail.com>
2021-12-10 14:51:53 -0500

    Done

--- index.js
+++ index.js
@@ -1,23 +1,156 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const Recipe = require("./models/Recipe.model");
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost/recipe-app";
+
+const oneRecipe = {
+  title: "Pie",
+  level: "Easy Peasy",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+  ],
+  cuisine: "American",
+  dishType: "dessert",
+  image: "https://images.media-allrecipes.com/images/75131.jpg",
+  duration: 40,
+  creator: "Chef Carlos",
+};
+
+const manyRecipes = [
+  {
+    title: "Pie 2",
+    level: "Easy Peasy",
+    ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+    ],
+    cuisine: "American",
+    dishType: "dessert",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 40,
+    creator: "Chef Carlos",
+  },
+  {
+    title: "Pie 3",
+    level: "Easy Peasy",
+    ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+    ],
+    cuisine: "American",
+    dishType: "dessert",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 40,
+    creator: "Chef Carlos",
+  },
+  {
+    title: "Pie 4",
+    level: "Easy Peasy",
+    ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+    ],
+    cuisine: "American",
+    dishType: "dessert",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 40,
+    creator: "Chef Carlos",
+  },
+];
 
 // Connection to the database "recipe-app"
+
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// Recipe.create(data)
+//   .then(() => {
+//     console.log("Success adding items");
+//   })
+// .then(() => {
+//   mongoose.disconnect();
+// })
+//   .catch((error) => {
+//     console.log(
+//       "Error creating items. Error code:",
+//       error.code);
+//   });
+
+//ITERATION 2
+
+// Recipe.create(oneRecipe)
+//   .then(() => {
+//     console.log("Success adding item");
+//   })
+// .then(() => {
+//   mongoose.disconnect();
+// })
+//   .catch((error) => {
+//     console.log(
+//       "Error creating item. Error code:",
+//       error.code);
+//   });
+
+// ITERATION 3
+
+// Recipe.insertMany(manyRecipes)
+//   .then((x) => {
+//     console.log("Success adding items:");
+//     x.forEach((element) => {
+//       console.log(element.title);
+//     });
+//   })
+// .then(() => {
+//   mongoose.disconnect();
+// })
+//   .catch((error) => {
+//     console.log(
+//       "Error creating items. Error code:",
+//       error.code);
+//   });
+
+// ITERATION 4
+
+// Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+//   .then(() => {
+//     console.log("Success changing item");
+//   })
+// .then(() => {
+//   mongoose.disconnect();
+// })
+//   .catch((error) => {
+//     console.log(
+//       "Error changing item. Error code:",
+//       error.code);
+//   });
+
+// ITERATION 5
+
+// Recipe.deleteOne({ title: "Carrot Cake" })
+//   .then(() => {
+//     console.log("Success erasing item");
+//   })
+// .then(() => {
+//   mongoose.disconnect();
+// })
+//   .catch((error) => {
+//     console.log("Error erasing item. Error code:", error.code);
+//   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+//ITERATION 1
+const recipeSchema = new Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    level: { type: String },
+    ingredients: { type: [String] },
+    cuisine: { type: String },
+    dishType: { type: String },
+    image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg",
+    },
+    duration: { type: Number },
+    creator: { type: String },
+  },
+  {
+    timestamp: true,
+  }
+);
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

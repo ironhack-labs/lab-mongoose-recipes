0480c3bcfb52b82f1fa2e8e8c8cc679b9b0f0268
Chun Lam <Chun01@hotmail.fr>
2022-01-11 20:42:42 +0100

    Solvelab

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useCreateIndex: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
   })
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
@@ -42,31 +42,11 @@ mongoose
 Recipe.findOneAndUpdate( {title: "Rigatoni alla Genovese"},{ duration: 100 },
 { new: true },)
 
-Recipe.deleteOne({
-  "title": "Carrot Cake",
-  "level": "Amateur Chef",
-  "ingredients": [
-    "6 cups grated carrots",
-    "1 cup brown sugar",
-    "1 cup raisins",
-    "4 eggs",
-    "1 1/2 cups white sugar",
-    "1 cup vegetable oil",
-    "2 teaspoons vanilla extract",
-    "1 cup crushed pineapple, drained",
-    "3 cups all-purpose flour",
-    "1 1/2 teaspoons baking soda",
-    "1 teaspoon salt",
-    "4 teaspoons ground cinnamon"
-  ],
-  "cuisine": "International",
-  "dishType": "dessert",
-  "image": "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
-  "duration": 130,
-  "creator": "Chef Nadia"
+ await Recipe.deleteOne({
+  title: "Carrot Cake"
 })
-.then(successCallback)
-.catch(errorCallback);
+.then()
+.catch();
 
 mongoose.connection.close();
 

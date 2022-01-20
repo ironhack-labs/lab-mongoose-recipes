d2a495b12b7ded32890eed55601146a2b656b8e6
Rachouan Rejeb <info@rachouan.com>
2022-01-20 20:36:32 +0000

    Completed lab

--- index.js
+++ index.js
@@ -1,23 +1,75 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+const rec = {
+  title: "Asian Glazed Chicken Thighs",
+  level: "Amateur Chef",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs",
+  ],
+  cuisine: "Asian",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 40,
+  creator: "Chef LePapu",
+};
+
+
+Recipe.create(rec)
+  .then((recipe) => console.log("Recipe was create with title: ", recipe.title))
+  .catch((error) =>
+    console.log("An error happened while saving a new recipe:", error)
+  );
+
+Recipe.insertMany(data)
+  .then((recipes) =>
+    recipes.forEach((recipe) => console.log("Recipe title:", recipe.title))
+  )
+  .catch((error) =>
+    console.log("An error happened while saving a new recipes:", error)
+  );
+
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then((recipe) => console.log("Updated Recipe"))
+  .catch((error) => console.log("Was not able to update recipe:", error));
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then(() => console.log("Removed Recipe"))
+  .catch((error) => console.log("Was not able to remove recipe:", error));
+
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
+    process.exit(0);
+  });
+});

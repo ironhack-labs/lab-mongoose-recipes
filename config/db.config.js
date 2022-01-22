345ad93890a5071037d1b86cbc746fc5a51ce0ef
Maria Gallego <mariagestionredes@gmail.com>
2022-01-22 12:58:28 +0100

    iteration 1 done

---
+++ config/db.config.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+// Connection to the database "recipe-app"
+mongoose
+  .connect(MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+    // Before adding any recipes to the database, let's remove all existing ones
+    return Recipe.deleteMany()
+  })
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+  })
+  .catch(error => {
+    console.error('Error connecting to the database', error);
+  });

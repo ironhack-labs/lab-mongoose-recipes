a8bf92b3ada2d8f1f7d0f7c0eabc7d84bb00d3c1
Maria Gallego <mariagestionredes@gmail.com>
2022-01-22 14:06:56 +0100

    done iteration 2

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Recipe = require("../models/Recipe.model");
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -7,7 +8,7 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+  
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -15,3 +16,5 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  
--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
+require("./config/db.config");
+
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
+
 const data = require('./data');
+
+mongoose.connection.once('open', () => {
+  mongoose.connection.db.dropDatabase()
+  .then(() => console.log ("Consola has been cleared"))
+  .then(() => {
+
+  const newRecipe = {
+    title: "Pan fried tofu",
+    level: "Easy Peasy",
+    ingredients: 
+    [
+      "1 (16 ounce) package water-packed firm tofu, drained and rinsed",
+      "6 tablespoons nutritional yeast",
+      "½ teaspoon garlic powder, or to taste (Optional)",
+      "salt and ground black pepper to taste",
+      "1 ½ tablespoons olive oil"
+    ],
+
+    cuisine: "Plant based",
+    dishType: "main_course",
+    duration: 20,
+    creator: "Chef María"
+  }
+  return Recipe.create(newRecipe)
+
+  })
+  .then(newRecipe => console.log ("New recipe sucessfully created", newRecipe))
+  .catch(err => console.log(err))
+  .finally(() => mongoose.connection.close())
+})
+
+
 
 
 

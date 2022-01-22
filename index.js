5cf3f83381d6ff392565325c8c1205b3e51cb4b6
Robert Cabrera <cabrera.rch@gmail.com>
2022-01-22 13:45:18 +0100

    Iteration 3 done

--- index.js
+++ index.js
@@ -2,55 +2,24 @@ const mongoose = require('mongoose');
 require('./config/db.config');
 
 const Recipe = require('./models/Recipe.model');
-/* const AllRecipes = require('./data.json'); */
+const AllRecipes = require('./data.json');
 
 
-const recipe = {
-  title: "Asian Glazed Chicken Thighs",
-    level: "Amateur Chef",
-    ingredients: [
-      "1/2 cup rice vinegar",
-      "5 tablespoons honey",
-      "1/3 cup soy sauce (such as Silver Swan®)",
-      "1/4 cup Asian (toasted) sesame oil",
-      "3 tablespoons Asian chili garlic sauce",
-      "3 tablespoons minced garlic",
-      "salt to taste",
-      "8 skinless, boneless chicken thighs"
-    ],
-    cuisine: "Asian",
-    dishType: "main_course",
-    image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-    duration: 40,
-    creator: "Chef LePapu"
-} 
-
-Recipe.create(recipe)
-  .then(createdRecipe => console.log(recipe.title))
+mongoose.connection.once('open', () => {
+  mongoose.connection.db.dropDatabase()
+  .then(() => console.log('Database has been cleared'))
+  .then(() => Recipe.create(AllRecipes))
+  .then((recipes) => {
+    for (let i = 0; i < AllRecipes.length; i++) {
+      console.log(recipes[i].title)
+    }
+  })
   .catch(err => console.log(err))
+  .finally(() => mongoose.connection.close())
+})
 
 
+/* Recipe.create(AllRecipes)
+  .then(createdRecipes => console.log(createdRecipes[0].title))
+  .catch(err => console.log(err)) */
 
-
-/* // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
- */

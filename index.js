4b09d6f343a8cc458dc21e01cab90d4d727465c8
Maria Gallego <mariagestionredes@gmail.com>
2022-01-23 18:07:49 +0100

    done iteration 3

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 require("./config/db.config");
 
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 
-const data = require('./data');
+const data = require("./data");
 
-mongoose.connection.once('open', () => {
-  mongoose.connection.db.dropDatabase()
-  .then(() => console.log ("Consola has been cleared"))
-  .then(() => {
+mongoose.connection.once("open", () => {
+  mongoose.connection.db
+    .dropDatabase()
+    .then(() => console.log("Consola has been cleared"))
+    .then(() => {
+      const newRecipe = {
+        title: "Pan fried tofu",
+        level: "Easy Peasy",
+        ingredients: [
+          "1 (16 ounce) package water-packed firm tofu, drained and rinsed",
+          "6 tablespoons nutritional yeast",
+          "½ teaspoon garlic powder, or to taste (Optional)",
+          "salt and ground black pepper to taste",
+          "1 ½ tablespoons olive oil",
+        ],
 
-  const newRecipe = {
-    title: "Pan fried tofu",
-    level: "Easy Peasy",
-    ingredients: 
-    [
-      "1 (16 ounce) package water-packed firm tofu, drained and rinsed",
-      "6 tablespoons nutritional yeast",
-      "½ teaspoon garlic powder, or to taste (Optional)",
-      "salt and ground black pepper to taste",
-      "1 ½ tablespoons olive oil"
-    ],
+        cuisine: "Plant based",
+        dishType: "main_course",
+        duration: 20,
+        creator: "Chef María",
+      };
 
-    cuisine: "Plant based",
-    dishType: "main_course",
-    duration: 20,
-    creator: "Chef María"
-  }
-  return Recipe.create(newRecipe)
+      const promise1 = Recipe.create(newRecipe);
+      const promise2 = Recipe.insertMany(data);
 
-  })
-  .then(newRecipe => console.log ("New recipe sucessfully created", newRecipe))
-  .catch(err => console.log(err))
-  .finally(() => mongoose.connection.close())
+      return Promise.all([promise1, promise2])
+      .then(
+        promise2.forEach = () => {
+          for(let i = 0; i < data.length; i++) {
+           console.log(`${data[i].title}`)
+          }
+          console.log(`New recipe ${newRecipe.title} sucessfully created`);
+        }
+        
+      )
+
+    .catch((err) => console.log(err))
+    .finally(() => mongoose.connection.close());
+  }) 
+
 })
-
-
-
-
-
 

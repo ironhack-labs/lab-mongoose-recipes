22ea0d531d4f069afe5c53be08e1e178ac3d6395
David Vekony <dave.vekony@gmail.com>
2022-01-27 14:59:43 +0000

    delete extra data for testing

--- index.js
+++ index.js
@@ -7,26 +7,6 @@ const data = require("./data");
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-const dataOne = {
-  title: "Asian Glazed Chicken Thighs",
-  level: "Amateur Chef",
-  ingredients: [
-    "1/2 cup rice vinegar",
-    "5 tablespoons honey",
-    "1/3 cup soy sauce (such as Silver Swan®)",
-    "1/4 cup Asian (toasted) sesame oil",
-    "3 tablespoons Asian chili garlic sauce",
-    "3 tablespoons minced garlic",
-    "salt to taste",
-    "8 skinless, boneless chicken thighs",
-  ],
-  cuisine: "Asian",
-  dishType: "main_course",
-  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-  duration: 40,
-  creator: "Chef LePapu",
-};
-
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {

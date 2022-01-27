69a6421a4cee42b404ef54888b723f7b71e1439d
Clara Villa <Clara.Villa@lego.com>
2022-01-27 16:37:50 +0000

    delete unused data

--- index.js
+++ index.js
@@ -5,48 +5,6 @@ const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
 const data = require("./data");
 
-const manyTest = [
-  {
-    title: "Asian Glazed Chicken Thighs",
-    level: "Amateur Chef",
-    ingredients: [
-      "1/2 cup rice vinegar",
-      "5 tablespoons honey",
-      "1/3 cup soy sauce (such as Silver Swan®)",
-      "1/4 cup Asian (toasted) sesame oil",
-      "3 tablespoons Asian chili garlic sauce",
-      "3 tablespoons minced garlic",
-      "salt to taste",
-      "8 skinless, boneless chicken thighs",
-    ],
-    cuisine: "Asian",
-    dishType: "main_course",
-    image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-    duration: 40,
-    creator: "Chef LePapu",
-  },
-  {
-    title: "Orange and Milk-Braised Pork Carnitas",
-    level: "UltraPro Chef",
-    ingredients: [
-      "3 1/2 pounds boneless pork shoulder, cut into large pieces",
-      "1 tablespoon freshly ground black pepper",
-      "1 tablespoon kosher salt, or more to taste",
-      "2 tablespoons vegetable oil",
-      "2 bay leaves",
-      "2 teaspoons ground cumin",
-      "1 teaspoon dried oregano",
-      "1/4 teaspoon cayenne pepper",
-      "1 orange, juiced and zested",
-    ],
-    cuisine: "American",
-    dishType: "main_course",
-    image: "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
-    duration: 160,
-    creator: "Chef John",
-  },
-];
-
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
@@ -96,6 +54,7 @@ mongoose
       for (let i = 0; i < insertedRecipies.length; i++) {
         console.log("those are the new recipes:" + insertedRecipies[i].title);
       }
+
       //ITERATION 4 - waiting for all recipies to be added before updating one
 
       Recipe.findOneAndUpdate(
@@ -117,9 +76,10 @@ mongoose
         .catch((error) => {
           console.log(`this is not working ${error}`);
         });
-    });
+    }); //CLOSING INSERTMANY .THEN
   }) //CLOSING .THEN AFTER DATABASE HAS CONNECTED
-
+  //  //ITERATION 6
+  // .then(() => mongoose.connection.close())
   //CATCH FOR DATABASE CONNECTION FAILURE
 
   .catch((error) => {

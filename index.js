d321f1ac2f05c4aada42b9d9b5b202d10e39e29b
Diego V <egovaldel@gmail.com>
2022-02-02 17:18:58 +0100

    done

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    //return Recipe.deleteMany();
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Natillas",
+      level: "Easy Peasy",
+      ingredients: "leche azucar canela huevos",
+      cuisine: "Parla",
+      dishType: "dessert",
+      duration: 20,
+      creator: "Diego",
+    }).then((newRecipe) => console.log("Nueva receta", newRecipe.title));
+    // Recipe.insertMany(data)
+    // .then((allrecipies) =>
+    //   allrecipies.forEach((elm) => console.log(elm.title))
+    // );
+    // Recipe.findOneAndUpdate(
+    //   { title: "Rigatoni alla Genovese" },
+    //   { duration: 100 }
+    // );
+    // devuelve el objeto borrado
+    Recipe.deleteOne({ title: "Carrot Cake" }).then(console.log("Deleted!"));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .connection.close(function () {
+    console.log("Mongoose connection closed");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

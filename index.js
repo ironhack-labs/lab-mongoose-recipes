8c341e58959d317ddfdd6b61d3ea468369cdbc89
Andrés García <playsharp59@gmail.com>
2022-02-02 18:35:48 +0100

    done

--- index.js
+++ index.js
@@ -15,8 +15,30 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create([
+        { title: 'Perico', level: 'Easy Peasy', ingredients: ['Eggs', 'Onion', 'Tomatoe'], cuisine: 'Colombian', image: 'https://www.196flavors.com/wp-content/uploads/2018/10/perico-2-FP.jpg', duration: 10 }
+      ])
+      .then(allRecipes => {
+        // allRecipes.forEach(elm => console.log(elm.title))
+        return Recipe.insertMany(data)
+      })
+      .then(allNewRecipes => {
+        // allNewRecipes.forEach(elm => console.log(elm.title))
+        return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+      })
+      .then(() => {
+        console.log('Success update!')
+        return Recipe.deleteOne({ title: 'Carrot Cake' })
+      })
+      .then(() => {
+        console.log('Successfully deleted!')
+        mongoose.connection.close()
+      })
+      .catch(error => console.log(error))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

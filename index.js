e342f535c8493c69bbef793ef4d0ce6846ed5783
Ricardo Ronchetti <ricardoronchetti@gmail.com>
2022-02-02 21:47:45 +0100

    updated index.js and Recipe.model.js

--- index.js
+++ index.js
@@ -15,9 +15,51 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // Recipe
+    //   .create([
+    //     {
+    //       "title": "Asian Glazed Chicken Thighs",
+    //       "level": "Amateur Chef",
+    //       "ingredients": [
+    //         "1/2 cup rice vinegar",
+    //         "5 tablespoons honey",
+    //         "1/3 cup soy sauce (such as Silver Swan®)",
+    //         "1/4 cup Asian (toasted) sesame oil",
+    //         "3 tablespoons Asian chili garlic sauce",
+    //         "3 tablespoons minced garlic",
+    //         "salt to taste",
+    //         "8 skinless, boneless chicken thighs"
+    //       ],
+    //       "cuisine": "Asian",
+    //       "dishType": "main_course",
+    //       "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //       "duration": 40,
+    //       "creator": "Chef LePapu"
+    //     }
+    //   ])
+    // .then(theDish => theDish.forEach(elm => console.log(`The title of this dish is ${elm.title}`)))
+    // .catch(err => console.log('Mongoose Error', err))
+    //
+    return Recipe
+      .create(data)
+  })
+  .then(theDish => {
+    theDish.forEach(elm => console.log(`The title of this dish is ${elm.title}`))
+    const query = { title: "Rigatoni alla Genovese" }
+    console.log("The modified dish is:", theDish)
+    return Recipe.findOneAndUpdate(query, { duration: 100 }, { new: true })
+  })
+  .then((theDish => {
+    console.log("You have changed the duration of: ", theDish)
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+  }))
+  .then(deleted => {
+    console.log("You have deleted: ", deleted)
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })

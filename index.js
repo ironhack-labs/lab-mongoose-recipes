9b5776b28909427cb387e53b65f59108f7fc9cf5
Guillermo Rodríguez Calero <guillermorodriguezcalero@MacBook-Air-de-Guillermo.local>
2022-02-02 21:52:42 +0100

    lab completed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
+
+
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
@@ -15,9 +17,43 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    Recipe
+
+      .create(
+        {
+          title: 'pollo al curry',
+          level: 'Amateur Chef',
+          ingredients: ['2 pechuga de pollo', '2 cebolletas', 'curry molido', 'comino molido', ' 1/2 cup leche de coco', '1/2 cup aceite de oliva', 'pimienta negra'],
+          cuisine: 'india',
+          dishType: 'main course',
+          image: 'https://images.media-allrecipes.com/images/75131.jpg',
+          duration: 100,
+          creator: 'Tajmaguille',
+        }
+      )
+      .then(recipe => {
+        //console.log(`Esta receta se llama ${recipe.title}`)
+        return Recipe.insertMany(data)
+      })
+      .then(recipe => {
+        //recipe.forEach(elm => console.log(elm.title))
+        return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+      })
+
+      .then(recipe => {
+        console.log(`ahora la duration es ${recipe.duration}`)
+        return Recipe.deleteOne({ title: 'Carrot Cake' })
+
+      })
+      .then(() => mongoose.connection.close())
+
   })
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,55 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    unique: true,
+    type: String,
+    required: true
+
+  },
+
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+
+  },
+
+  ingredients: {
+    type: [String]
+  },
+
+  cuisine: {
+    type: String,
+    required: true
+  },
+
+  DishType: {
+    type: String,
+    enum: ['breakfast', 'main course', 'soup', 'snack', 'drink', 'dessert', 'other'],
+    default: 'main course'
+  },
+
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+
+  duration: {
+    type: Number,
+    min: 0,
+    max: 1000
+  },
+
+  creator: {
+    type: String
+
+  },
+
+  created: {
+    type: Date,
+    default: 2022 / 02 / 02
+  }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

e03ded8616e252c7a29bfb1a3e8e464e1499bf5b
Anna Ma Porras <amporras85@gmail.com>
2022-02-02 19:54:44 +0100

    done

--- index.js
+++ index.js
@@ -13,11 +13,60 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
+
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({
+        title: 'Empanada de Atún',
+        level: 'Easy Peasy',
+        ingredients: ['masa brisa', 'atun', 'cebolla', 'pimientos', 'tomate', 'huevo'],
+        cuisine: 'fácil y casera',
+        dishType: 'snack',
+        image: '',
+        duration: 40,
+        creator: 'Anna',
+        created: ''
+      })
+
+
+      .then(title => {
+        console.log('se ha creado una nueva reeceta', title)
+        return Recipe.create(data)
+      })
+      // Run your code here, after you have insured that the connection was made
+
+      .then(() => {
+        return Recipe
+          .find()
+          .then(recipes => recipes.forEach(elm => {
+            console.log(
+              elm.title)
+          }))
+      })
+      .then(() => {
+        return Recipe
+          .updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+          .then(console.log('updated'))
+      })
+      .then(() => {
+        return Recipe
+          .deleteOne({ title: 'Carrot Cake' })
+          .then(console.log('deleted'))
+      })
+
+
+
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+mongoose.connection.close(() => {
+  console.log('Mongoose default connection disconnected through app termination')
+  process.exit(0)
+
+
+})

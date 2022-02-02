8a625345d09859e27fcc1352a8d0951439bbe27d
Mikel Laiseca <mikellaiseca@gmail.com>
2022-02-02 19:31:41 +0100

    Done

--- index.js
+++ index.js
@@ -15,9 +15,47 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({
+        title: 'Bacalao al pil-pil',
+        level: 'Amateur Chef',
+        ingredients: ['Cod', 'Olive oil', 'Garlic', 'Chili pepper'],
+        cuisine: 'Basque',
+        dishType: 'main_course',
+        image: 'https://www.miscosillasdecocina.com/wp-content/uploads/2014/02/bacalao-pilpil-e1491084486535.jpg',
+        duration: 60,
+        creator: 'Mikel Laiseca',
+        creation: 02 / 02 / 2022
+      })
+
+      .then(theNewRecipe => (console.log(theNewRecipe.title)))
+      .catch(error => {
+        console.error('Error connecting to the database', error);
+      });
+
+    Recipe
+      .insertMany(data)
+      .then(NewRecipes => NewRecipes.forEach(elm => console.log(elm.title)))
+      .catch(error => {
+        console.error('Error connecting to the database', error);
+      });
+
+    Recipe
+      .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+      .then(theNewRecipe => (console.log('Success!')))
+      .catch(error => {
+        console.error('Error connecting to the database', error);
+      });
+    Recipe
+      .deleteOne({ title: "Carrot Cake" })
+      .then(theNewRecipe => (console.log('Success!')))
+      .catch(error => {
+        console.error('Error connecting to the database', error);
+      });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+mongoose.connection.close()

fefd837e0f7409f224889d38bec3a60493e59c17
riiGalaxy <riimolpeceres@gmail.com>
2022-02-02 17:57:04 +0100

    done

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
+  })
+  .then(() => Recipe.syncIndexes())
+  .then(() => {
+    // Iteracion 3
+    return Recipe.insertMany(data);
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Iteracion 4
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      {
+        duration: 100,
+      }
+    );
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    // Iteracion 5
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((deleteRecipe) => console.log("Receta eliminada es:", deleteRecipe))
+  .then(() => {
+    // Iteracion 6
+    mongoose.connection.close();
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// ITERACION 2
+// return Recipe.create({
+//   title: "Asian Glazed Chicken Thighs",
+//   level: "Amateur Chef",
+//   ingredients: [
+//     "1/2 cup rice vinegar",
+//     "5 tablespoons honey",
+//     "1/3 cup soy sauce (such as Silver Swan®)",
+//     "1/4 cup Asian (toasted) sesame oil",
+//     "3 tablespoons Asian chili garlic sauce",
+//     "3 tablespoons minced garlic",
+//     "salt to taste",
+//     "8 skinless, boneless chicken thighs",
+//   ],
+//   cuisine: "Asian",
+//   dishType: "main_course",
+//   image:
+//     "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+//   duration: 40,
+//   creator: "Chef LePapu",
+// });

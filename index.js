c2c1c8f820eeeb07ea03e7599d8a813d4bd9f3c6
sarapuertas <saritapuertas@hotmail.com>
2022-02-02 18:24:41 +0100

    done

--- index.js
+++ index.js
@@ -15,9 +15,43 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe
+      .create([
+        {
+          title: 'Vegan Curry', level: 'Amateur Chef',
+          ingredients: ['Your favorites vegetables', 'Curry', 'Soja Milk'],
+          cuisine: 'vegan', dishType: 'main_course', image: null,
+          duration: 20, creator: 'My mum', create: null
+        },
+        {
+          title: 'Spanish Tortilla', level: 'Easy Peasy',
+          ingredients: ['Eggs', 'Potatoes', 'ONION FOREVER', 'Love'],
+          cuisine: 'traditional', dishType: 'main_course', image: null,
+          duration: 40, creator: 'Who knows', create: null
+        }
+      ])
   })
+  .then((myRecipes => {
+    myRecipes.forEach(elm => console.log(`${elm.title}`))
+    return Recipe.insertMany(data)
+  }))
+  .then((allRecipes) => {
+    allRecipes.forEach(elm => console.log(`${elm.title}`))
+    return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true })
+  })
+  .then(info => {
+    console.log('Changed:', info)
+    return Recipe
+      .deleteOne({ title: 'Carrot Cake' })
+  })
+  .then((elementDeleted) => {
+    console.log(elementDeleted)
+    mongoose.connection.close()
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

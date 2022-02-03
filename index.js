ea778bc0a5cbdbe20b2dd74e1f0f3d5ab2691bfd
Zena Bos <zta.bos@gmail.com>
2022-02-03 12:43:08 +0100

    iteration 4 done

--- index.js
+++ index.js
@@ -13,26 +13,30 @@ mongoose
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
+    //   return Recipe.deleteMany();
+    // })
+    // .then(() => {
+    //   const recipe = Recipe.insertMany(data);
+    //   return recipe;
+    // })
+    // .then((result) => {
+    //   console.log(result.title);
+    // })
+    // .then(() => {
+    //   const newRecipe = {
+    //     title: "Mashed Potatoes",
+    //     cuisine: "American",
+    //   };
+    //   return Recipe.create(newRecipe);
+    //   // Run your code here, after you have insured that the connection was made
+    // })
+    // .then((result) => {
+    //   console.log(result.title);
   })
-  // .then(() => {
-  //   const recipe = Recipe.insertMany(data);
-  //   return recipe;
-  // })
-  // .then((result) => {
-  //   console.log(result.title);
-  // })
-  //.then(() => {
-  //   const newRecipe = {
-  //     title: "Mashed Potatoes",
-  //     cuisine: "American",
-  //   };
-  //   return Recipe.create(newRecipe);
-  //   // Run your code here, after you have insured that the connection was made
-  // })
-  // .then((result) => {
-  //   console.log(result.title);
-  //})
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
+
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then((result) => console.log(result.title + " is updated"))
+  .catch((error) => console.log(error));

761857424aa9b67dae95765a39a6c76dd5cbe580
GerardoRegalado <gerardo.regalado24@gmail.com>
2022-02-02 18:24:42 -0600

    se termina el modelo de recipe

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,52 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose'); //Importar Mongoose
+const Schema = mongoose.Schema;       //definir el Schema 
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type:     String,
+    required: true,
+    unique:   true
+  },
+
+  level: {
+    type:     String,
+    enum:     ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef', 'Who are u Gordon Ramsey?']
+  },
+
+  ingredients: {
+    type:     [ String ],
+  },
+
+  cuisine:{
+    type:     String,
+    required: true
+  },
+  
+  dishType: {
+    type:     String,
+    enum:     ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+
+  image: {
+    type:     String,
+    default:  "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+
+  duration: {
+    type:     Number,
+    min:      0
+  },
+
+  creator: {
+    Type:     String
+  },
+
+  created: {
+    type:     Date,
+    default:  "02 de Febrero de 2022"
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
-module.exports = Recipe;
+module.exports = Recipe

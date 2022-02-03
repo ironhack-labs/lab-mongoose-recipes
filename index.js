0ee271d85e3206d50491ac1b2208de1deaaf1553
Stephan Geyer <stephangeyer@Stephans-MacBook-Pro.local>
2022-02-03 18:31:59 +0100

    Iteration 5 done

--- index.js
+++ index.js
@@ -17,7 +17,40 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // Iteration 2
+    return Recipe.create({
+      title: 'Roasted Salmon with White Wine Sauce',
+      level: 'Easy Peasy',
+      ingredients: ['1 (1 1/2-pound) salmon fillet', '1 tablespoon unsalted butter', '2 teaspoons all-purpose flour', '1 cup dry white wine', '1 tablespoon chopped fresh chives'],
+      cuisine: 'Mediterranean',
+      dishType: 'main_course',
+      duration: 30,
+      creator: 'Hans Peter',
+      created: Date.now(),
+    })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  // Iteration 3
+  .then(() => {
+    const recipes = Recipe.insertMany(data)
+    return recipes
+      .then((recipes) => {
+        recipes.forEach((recipe) => {
+          console.log(`Recipe added: ${recipe.title}`);
+        })
+      })
+  })
+  // Iteration 4
+  .then(() => {
+    Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: "100" })
+      .then(recipe => (console.log(`Succesfully updated ${recipe.title}`)))
+  })
+  // Iteration 5
+  .then(() => {
+    Recipe.deleteOne({ title: "Carrot Cake" })
+      .then(recipe => {
+        console.log('Recipe deleted')
+      })
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  })

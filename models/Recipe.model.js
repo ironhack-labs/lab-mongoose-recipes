d20e7d8b37145eb7031c12284a2c05d0af6c4892
Bjoern Hass <bjoernhass@gmx.net>
2022-02-03 12:07:05 +0100

    Iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: String,
+  ingredients: Array,
+  cuisine: String, 
+  dishType: String,
+  image: String,
+  duration: String,
+  creator: String,
+  created: Date,
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

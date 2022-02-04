9bb2791b309b29a28f7063ad6483c984878b2dcd
Aljoscha Zynga <aljoscha.zynga@gmail.com>
2022-02-04 14:45:28 +0100

    solved lab

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { findOneAndUpdate } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -16,7 +17,42 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Iteration 2: Insert single recipe
+    return Recipe.create({
+      title: 'Wurst',
+      level: 'UltraPro Chef',
+      cuisine: 'German'
+    });
+  })
+  .then((createdRecipe) => {
+    // Iteration 2: Log single recipe
+    console.log('Recipe successfully created: ' + createdRecipe.title);
+  })
+  .then(() => {
+    // Iteration 3: Insert multiple recipes
+    return Recipe.insertMany(data);
+  })
+  .then((createdRecipes) => {
+    // Iteration 3: Log multiple recipes
+    createdRecipes.forEach(recipe => console.log('Recipe successfully created: ' + recipe.title));
+
+    // Iteration 4: Update one document
+    return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100});
+  })
+  .then((recipeBeforeUpdate) => {
+    // Iteration 4: Log success message
+    console.log('Recipe successfully updated: ' + recipeBeforeUpdate.title)
+
+    // Iteration 5: Delete one document
+    return Recipe.deleteOne({title: 'Carrot Cake'})
+  })
+  .then(() => {
+    // Iteration 5: Log success message
+    console.log('One document was sucessfully deleted')
+  })
+  .then(() => {
+    // Iteration 6: Close connection
+    mongoose.connection.close();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

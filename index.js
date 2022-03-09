f83d312bdc8063d4a99dd1f5f4527274ff13de3f
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-03-09 03:53:29 +0100

    Iteration 3 - Done

--- index.js
+++ index.js
@@ -21,34 +21,11 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    Recipe.create({
-      title: "Asian Glazed Chicken Thighs",
-      level: "Amateur Chef",
-      ingredients: [
-        "1/2 cup rice vinegar",
-        "5 tablespoons honey",
-        "1/3 cup soy sauce (such as Silver Swan®)",
-        "1/4 cup Asian (toasted) sesame oil",
-        "3 tablespoons Asian chili garlic sauce",
-        "3 tablespoons minced garlic",
-        "salt to taste",
-        "8 skinless, boneless chicken thighs",
-      ],
-      cuisine: "Asian",
-      dishType: "main_course",
-      image:
-        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-      duration: 40,
-      creator: "Chef LePapu",
-    });
+    Recipe.insertMany(data)
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
-
-const showRecipes = async () => {
-  const recipes = await Recipe.find({}, { title: 1 });
-  console.log(recipes);
-};
-
-showRecipes();
+  
+  
+  

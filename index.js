f6aa74097ca18c3324f370bc94e3b16112b21eda
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-03-10 16:15:43 -0400

    iteration 4

--- index.js
+++ index.js
@@ -17,32 +17,41 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    // Iteration 2 - Create a recipe
-    Recipe.create({
-      title: "Broccoli & Stilton soup",
-      level: "Amateur Chef",
-      ingredients: [
-        "2 tblsp Rapeseed Oil",
-        "1 finely chopped onion",
-        "1 celery", "1 sliced leek",
-        "1 medium potatoes",
-        "1 knob butter",
-        "1 litre hot vegetable stock",
-        "1 head chopped broccoli",
-        "140g stilton cheese"
-      ],
-      cuisine: "British",
-      dishType: "soup",
-      image: "https://www.themealdb.com/images/media/meals/tvvxpv1511191952.jpg",
-      duration: 45,
-      creator: "unknown"
-    }).then(newRecipe => console.log(`A new recipe is created: ${newRecipe.title}`))
-      .catch(error => console.log(`Error while creating a new recipe: ${error}`));
-    
-    // Iteration 3 - Insert multiple recipes
-    Recipe.insertMany(data).then(newRecipes => {
-      newRecipes.forEach(recipe => console.log(`A new recipe is created: ${recipe.title}`))
-    }).catch(error => console.log(`Error while creating a new recipe: ${error}`));
+    const iterations2To5 = async () => {
+      // Iteration 2 - Create a recipe
+      await Recipe.create({
+        title: "Broccoli & Stilton soup",
+        level: "Amateur Chef",
+        ingredients: [
+          "2 tblsp Rapeseed Oil",
+          "1 finely chopped onion",
+          "1 celery", "1 sliced leek",
+          "1 medium potatoes",
+          "1 knob butter",
+          "1 litre hot vegetable stock",
+          "1 head chopped broccoli",
+          "140g stilton cheese"
+        ],
+        cuisine: "British",
+        dishType: "soup",
+        image: "https://www.themealdb.com/images/media/meals/tvvxpv1511191952.jpg",
+        duration: 45,
+        creator: "unknown"
+      }).then(newRecipe => console.log(`A new recipe is created: ${newRecipe.title}`))
+        .catch(error => console.log(`Error while creating a new recipe: ${error}`));
+      
+      // Iteration 3 - Insert multiple recipes
+      await Recipe.insertMany(data).then(newRecipes => {
+        newRecipes.forEach(recipe => console.log(`A new recipe is created: ${recipe.title}`))
+      }).catch(error => console.log(`Error while creating a new recipe: ${error}`));
+
+      // Iteration 4 - Update recipe
+      await Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+        .then(updatedRecipe => console.log(`${updatedRecipe.title} duration was updated to 100!`))
+        .catch(error => console.log(`Error while updating recipe: ${error}`))
+    };
+
+    iterations2To5();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

9204766d5cf0151c3e34c97c8df32a7fe33b1882
joeydegori <96920177+joeydegori@users.noreply.github.com>
2022-03-15 17:59:58 -0700

    All

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -18,15 +18,7 @@ const recipeSchema = new Schema({
     },
   dishType: {
     type: String,
-     enum: [
-        "breakfast",
-        "main_course",
-        "soup",
-        "snack",
-        "drink",
-        "dessert",
-        "other",
-      ]
+     enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
     },
   image: {
     type: String,

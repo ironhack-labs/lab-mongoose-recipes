cbb731d7f39b07142214190b651e11d32176a9b7
Laurie Bruneau <lauriebr@hotmail.fr>
2022-03-17 22:56:56 +0100

    done

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const Recipe = require("./models/Recipe.model");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+// Import of the data from './data.json'
+const data = require("./data");
+
+const recipe = {
+  title: "Oeuf mayo",
+  level: "Easy Peasy",
+  ingredients: ["egg", "mayonnaise"],
+  cuisine: "French",
+  dishType: "other",
+  duration: 4,
+  creator: "Maïté",
+};
+
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // Remove all existing recipes
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    // Creating recipe
+    Recipe.create(recipe).then((user) =>
+      console.log("Recipe successfully created")
+    );
+    // Inserting recipes
+    Recipe.insertMany(data).then((user) =>
+      console.log("Recipe inserted successfully")
+    );
+    // Updating recipes
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    ).then((user) => console.log("Recipe updated"));
   });
+// Remove a recipe
+Recipe.deleteOne({ name: "carrot cake" }).then((user) =>
+  console.log("Recipe successfully removed!")
+);
+// Close database?
+mongoose.connection.close().catch((error) => {
+  console.error("Error connecting to the database", error);
+});
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
+const recipeSchema = new mongoose.Schema({
+  title: { type: String, unique: true, required: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: [String],
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;
+
+// // Mongoose allows us to use a callback pattern
+// // to handle the completion of the asynchronous operation
+// User.create(data, (error, user) => {
+//   if (error) {
+//     console.log('An error happened:', error);
+//     return;
+//   }
+//   console.log('The user is saved and its value is: ', user);
+// });
+
+// // The same code as above but with a Promise version
+// User.create(data)
+//   .then(user => console.log('The user is saved and its value is: ', user))
+//   .catch(error => console.log('An error happened while saving a new user:', error));

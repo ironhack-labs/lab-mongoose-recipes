eb41674da3f7da96b115ad0153139f3893feeb26
lucas-emg <lucas.gabriel3.lg@gmail.com>
2022-03-24 10:28:48 -0300

    Lab done

--- index.js
+++ index.js
@@ -15,9 +15,44 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then( async () => {
+
+    await Recipe.create({
+      title: 'Ovo mexido',
+      level: 'Easy Peasy',
+      ingredients: ['eggs', 'pepper', 'salt', 'oil'],
+      cuisine: 'American',
+      dishType: 'breakfast',
+      duration: 20,
+      creator: 'Lucas',
+    }).then(recipe => console.log('This recipe was saved as:', recipe.title))
+    .catch(error => console.log('Was not able to save recipe. Error:', error))
   })
+  .then( async () => {
+    await Recipe.insertMany(data)
+    .then(data => data.forEach(recipe => console.log('This recipe was added:' + recipe.title)))
+    .catch(error => console.log('Was not able to add recipe due to:',  error))
+  })
+  .then( async () => {
+    const filter = {title: 'Rigatoni alla Genovese'}
+    const result = await Recipe.findOneAndUpdate(filter, {$set: {duration: 100}})
+    return console.log('This recipe was successfully updated:', result.title)
+    
+  }).catch(error => console.log('Unable to updated due to:', error))
+
+  .then( async () => {
+    const removeOne = await Recipe.deleteOne({title: 'Carrot Cake'})
+
+    return console.log('This recipe was deleted:', removeOne.title)
+
+  }).catch(error => console.log('Unable to delete due to:', error))
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally( async () => 
+    await mongoose.disconnect(MONGODB_URI)
+    .then(console.log('Disconnected from DB'))
+    .catch(error => console.log('Error while disconnecting from DB: ', error))
+  )
+

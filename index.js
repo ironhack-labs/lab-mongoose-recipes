7b11ecaf80758da3296f6c4910c3078cbac7f7b9
Arundathi Jayarathne <sandyjayarathne@gmail.com>
2022-04-10 23:25:22 +0200

    lab completed

--- index.js
+++ index.js
@@ -1,23 +1,69 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
+// no recipe have the same name
+//name{
+// unique: true
+//}
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
+  /*.then(() => {
+    return Recipe.create({
+      title: "SriLankan polos curry",
+      level: "Amateur Chef",
+      ingredients: [
+        "jackfruit",
+        "coconut milk",
+        "curry powder",
+        "chili powder",
+        "onion",
+        "turmeric",
+        "giger",
+        "garlic"
+      ],
+      cuisine: "SriLankan",
+      dishType: "main_course",
+      duration: 30,
+      creator: "Sandy"
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  });*/
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    return Recipe.insertMany(data)
+      .then((recipe) => {
+        return recipe.map((result) => console.log(result.title));
+      })
+      .then(() => {
+        console.log("Update succesful");
+
+        return Recipe.findOneAndUpdate(
+          {
+            title: "Rigatoni alla Genovese"
+          },
+          { duration: 100 },
+          { new: true }
+        );
+      })
+      .then((recipe) => {
+        console.log("Rigatoni is updated", recipe.duration);
+        return Recipe.deleteOne({ title: "Carrot Cake" });
+      })
+      .then((error) => {
+        console.log("Carrot cake has been deleted");
+        return mongoose.disconnect();
+      });
   });

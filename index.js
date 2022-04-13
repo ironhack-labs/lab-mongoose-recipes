408c02f775e9517cb50f5bd1ac0f3d351cec2845
Paula Moratalla <paulamrodriguez723@gmail.com>
2022-04-13 20:17:30 +0200

    Completed lab

--- index.js
+++ index.js
@@ -15,9 +15,38 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({ title: 'Mac&Cheese', level: 'Easy Peasy', ingredients: ['Pasta', 'Cheddar Cheese', 'Milk'], cuisine: 'Italian', dishType: 'main_course', image: '', duration: 10, creator: 'Paula', created: '02/06/2020' })
   })
+
+  .then(() =>
+    Recipe.insertMany(data)
+  )
+
+  .then(() =>
+    Recipe
+      .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+    // .then(() => console.log("Exito"))
+    // .catch(err => console.log('Hubo un error', err))
+  )
+
+  .then(() =>
+    Recipe
+      .deleteOne({ title: "Carrot Cake" })
+  )
+
+  .then(() =>
+    mongoose.connection.close(() => {
+      console.log('Mongoose default connection disconnected through app termination')
+      process.exit(0)
+    }))
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+

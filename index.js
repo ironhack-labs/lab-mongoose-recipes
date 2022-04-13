cc76637e3104a4a743d26f283b08e58415607440
juancarlosdnz <dnzjuancarlos@gmail.com>
2022-04-13 20:20:12 +0200

    done

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
+
 const data = require('./data');
+
+console.log(Recipe)
+
+
+
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -15,9 +20,37 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
+ 
+/*  .then(() => {
+    Recipe
+ .create({ title: 'Juanca Masala 2', level: 'UltraPro Chef', ingredients: 'Arroz', cuisine: 'India', dishType: 'main_course', image:'https://toque.world/img/wp-blog/uploads/2021/06/148672919_1337694966586333_1381278347226852579_n-819x1024.jpg',duration:'40',creator:'Juanca',created:''})
+ .then(recipe=> console.log('La nueva receta es ',recipe.title))
+ .catch(error => console.log('Hubo un error:', error))
+
+  })*/
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log('insertando la data')
+    return Recipe.insertMany(data)
   })
+  .then(() => {
+    console.log('Recortando duration de Rigatoni')
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: '100' });
+  })
+  .then(() => {
+    console.log('borrandoooo')
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('desconectando.....')
+    mongoose.disconnect();
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+
+

7d73d8ca6620944609d3caa512dc70f94bb5ddea
Roberto D. Ezquerro <roberto.ezquerro@gmail.com>
2022-04-13 22:15:17 +0200

    [MAD DE] Roberto Ezquerro

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { modelName } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -11,12 +12,31 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+    console.log(`CONECTADO PRRO: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({ title: 'Moca', level: 'Easy Peasy', ingredients: 'love', cuisine: 'Mexican', dishType: 'snack', image: "https://images.media-allrecipes.com/images/75131.jpg", duration: 60, creator: 'Marcello', created: 2021 - 01 - 01 })
+      .then(newRecipe => console.log('La nueva receta es', newRecipe))
+      .catch(error => console.log('Hubo un error:', error))
+  })
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create(data)
+      .then(allRecipe => console.log('Todas las recetas son', allRecipe))
+      .catch(error => console.log('Hubo un error:', error))
+  })
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+    Recipe
+      .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+      .then(newRecipe => console.log('SUCCESS'))
+      .catch(error => console.log('Hubo un error:', error))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

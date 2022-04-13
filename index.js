4ba742dcad5f272d86de4b1dd7fe6f19617d57a3
Lucia de Haro <elle.dharo@gmail.com>
2022-04-13 21:12:33 +0200

    done

--- index.js
+++ index.js
@@ -15,9 +15,62 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
+
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+      return Recipe.create({
+        title: 'Paella',
+        level: 'UltraPro Chef',
+        ingredients : ['arroz', 'marisco','fumet','colorante','sofrito mediterraneo','paciencia'],
+        cuisine: 'mediterranea',
+        dishType: 'main_course',
+        Image: 'https://cucharonypasoatras.es/wp-content/uploads/2020/01/traditional-spanish-dish-paella-with-prawns-mussels_103153-153.jpg' ,
+        duration: 100 ,
+        creator: 'Algun Valenciano',
+      });
   })
-  .catch(error => {
+  .then((newRecipe) => {
+    console.log('New recipe has been created:', newRecipe.title);
+    return Recipe.find();
+  })
+
+  //
+
+  .then(()=> {
+    return Recipe.create(data);
+  })
+
+  .then((allRecipes)=>{
+      allRecipes.foreach((recipe)=>
+      console.log(`The recipes are:${recipe.title}`)
+      );
+
+      return Recipe.findOneAndUpdate(
+          { title: "Rigatoni alla Genovese" },
+          { duration: 100},
+          { new: true}
+        );
+  }) 
+
+  .then((recipe) =>{
+    console.log(`The duration of the recipe ${recipe.title} was updated to ${recipe.duration}`);
+      return Recipe.deleteOne({title: "Carrot Cake"});
+  })
+
+  .then((recipe) =>{
+    console.log(`The recipe ${recipe.title} was deleted`);
+  })
+
+  .catch((error) => {
     console.error('Error connecting to the database', error);
+  });
+
+
+  process.on("SIGINT", () => {
+    mongoose.connection.close(()=>{
+      console.log("Mongoose default connection disconnected");
+      process.exit(0);
+    });
+
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,47 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  // TODO: write the schemaname: 
+  title: { 
+    unique: true,
+    type: String,
+    required: true,
+  },
+
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+  },
+
+  ingredients: {
+     type: [String],   
+  },
+
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: ['Breakfast', 'Main_course', 'Soup', 'snack', 'drink', 'drink', 'dessert', 'other',],
+  },
+
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    ninimun: 0 ,
+  },
+  creator: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: '2022-04-13s'
+  },
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

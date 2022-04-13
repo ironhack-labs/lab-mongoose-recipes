a79307ad74ccb20ad7433448f7256d16683957b6
Andrew Lamagna <andrew.lamagna@LEGO.com>
2022-04-13 19:19:04 -0400

    Completed with bonus

--- index.js
+++ index.js
@@ -13,11 +13,45 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    //return Recipe.deleteMany()
   })
   .then(() => {
+  
+/*    
+    //Iteration 2: 
+      Recipe.create(
+      { title:       'Eggs',
+        level:       'Easy Peasy',
+        ingredients: ['eggs','salt','pepper'],
+        cuisine:     'eggy',
+        dishType:    'breakfast',
+        duration:    5,
+        creator:     'Me'})
+      .then((created) => console.log(created.title))
+      .catch(err => console.log('Create failed', err));
+  
+    //Iteration 3:
+        Recipe.insertMany(data)
+        .then((created)=>created.forEach(ins=>console.log(ins.title)))
+        .catch(err => console.log('Create failed', err));        
+
+    //Iteration 4:
+      Recipe.findOneAndUpdate(
+          { title: "Rigatoni alla Genovese"},
+          { $set:  {duration: 100 } } )
+        .then(()=>console.log('Successfully updated'))
+        .catch(err => console.log('Update failed', err));     
+
+    //Iteration 5:
+        Recipe.deleteOne( { title: "Carrot Cake"} )
+          .then(()=>console.log('Successsfully removed'))
+          .catch(err => console.log('Delete failed', err));          
+*/
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  mongoose.connection.close(()=>console.log('DB Connection Closed'));

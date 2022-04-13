7a6f4006b772e2a50b21b1ddce29d49e1154b857
María Fdez de la Rosa <7mariadlrosa@gmail.com>
2022-04-13 20:18:20 +0200

    done

--- index.js
+++ index.js
@@ -10,14 +10,37 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+  .then(recipeSchema => {
+    console.log(`Connected to the database: "${recipeSchema.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+  .then(() => Recipe.syncIndexes())
+  // .then(() => {
+  //   // Run your code here, after you have insured that the connection was made
+  //   Recipe
+  //     .create({ title: 'Risotto', level: "Amateur Chef", ingredients: ["Arroz", "setas", "vino", "queso"], cuisine: "Italiana", dishType: "main_course", duration: 90, creation: "María" })
+  // })
+
+  .then(() => 
+     Recipe.insertMany(data) //NO ENTIENDO POR QUÉ VA SIN CORCHETES
+  )
+
+  .then(() => 
+     Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })  
+    // console.log("El perro modificado es:", Recipe)
+)
+  
+  .then(() =>  
+    Recipe.deleteOne({ title: "Carrot Cake"})
+    )
+  
+.then(() => 
+  mongoose.connection.close(() => {
+    console.log()
+    // process.exit(0)
+  }))
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

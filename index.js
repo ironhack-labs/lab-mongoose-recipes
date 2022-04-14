cd7e7795058f583a6b7934b484c54966134810e1
Pablo Andreu <pablo.andreu.martin@gmail.com>
2022-04-14 09:25:48 +0200

    done

--- index.js
+++ index.js
@@ -9,15 +9,54 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
+    .connect(MONGODB_URI)
+    .then(x => {
+        console.log(`Connected to the database: "${x.connection.name}"`);
+        // Before adding any recipes to the database, let's remove all existing ones
+        return Recipe.deleteMany()
+    })
+    .then(() => Recipe.syncIndexes())
+    .then(() => {
+        Recipe
+            .create({
+                title: "spaguetis boloñesa",
+                level: "UltraPro Chef",
+                ingredients: [
+                    "spaguetis", "carne"
+                ],
+                cuisine: "American",
+                dishType: "main_course",
+                image: "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
+                duration: 160,
+                creator: "Chef pablo"
+            }, )
+            .then(newRecipe => console.log('la nueva receta es:', newRecipe))
+            .catch(error => console.log('Hubo un error:', error))
+    })
+    .then(() => {
+        Recipe
+            .insertMany(data)
+            .then(newRecipe => newRecipe.forEach(elm => console.log(`la nueva receta es:", ${elm.title}`)))
+            .catch(error => console.log('Hubo un error:', error))
+            .then(() => {
+                Recipe
+                    .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+                    .then((newRecipe => console.log(`los detalles de modificacion son ${newRecipe}`)))
+                    .catch(error => console.log('Hubo un error:', error))
+
+            })
+            .then(() => {
+                Recipe
+                    .deleteOne({ title: "Carrot Cake" })
+                    .then((newRecipe => console.log(`los detalles de modificacion son ${newRecipe}`)))
+                    .catch(error => console.log('Hubo un error:', error))
+            })
+            .then(() =>
+                mongoose.disconnect()
+            )
+    })
+    //Rigatoni alla Genovese duration to 100
+
+.catch(error => {
     console.error('Error connecting to the database', error);
-  });
+});

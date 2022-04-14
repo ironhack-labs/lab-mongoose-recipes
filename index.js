0ce5362a0cf6e00d84b0ef793cd888eca2b5716c
Cristian Calderon <cristianfab90@gmail.com>
2022-04-14 09:21:57 +0200

    Done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { insertMany } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -15,8 +16,22 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({ title: 'Roasted Chicken', level: 'Amateur Chef', ingredients: ['Chicken', 'Onions', 'Parsley'], cuisine: 'All world', dishType: 'Big one', duration: 2, creator: 'Me' })
+      .then(newDish => console.log('El nuevo plato es', newDish.title))
+
+      .then(() => Recipe.insertMany(data))
+      .then(() => Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }))
+      .then(() => Recipe.deleteOne({ title: "Carrot Cake" }))
+      .then(() => process.on('SIGINT', () => {
+        mongoose.connection.close(() => {
+          console.log('Mongoose default connection disconnected through app termination')
+          process.exit(0)
+        })
+      }))
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

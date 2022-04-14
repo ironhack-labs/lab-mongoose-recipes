aa81eb09880090b4d38b1c843d347c2650ee0577
aBoswell <A.B0ZZW3LL@gmail.com>
2022-04-14 16:41:53 -0400

    iterations added

--- index.js
+++ index.js
@@ -12,12 +12,41 @@ mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany()    //<-- Before adding any recipes to the database, let's remove all existing ones //
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
+  .catch(err => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .then(() => {                                                              
+    Recipe.insertMany(data)
+      .then(recipes => {recipes.forEach(element => console.log(`${element.title} has been created`))})
+      .catch(err => console.log('oops something went wrong creating recipe')) 
+
+        .then(() => Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, { duration: 100 }))
+          .then(message => console.log('Rigatoni updated successfully'))
+          .catch(err => console.log('whoops, something went wrong updating the Rigatoni')) 
+
+          .then(() => Recipe.deleteOne({ title: 'Carrot Cake' }))
+            .then(message => console.log('Carrot Cake removed successfully'))
+            .catch(err => console.log('Error deleting Carrot Cake'))
+
+              .then(() => {mongoose.connection.close()})
+  })
+
+
+// ITERATION 2 - Create one recipe//
+
+// .then(() => {                                                                 
+//   Recipe.create(data[0])
+//     .then(recipe => console.log(`${recipe.title} has been created`))
+//     .catch(err => console.log('oops something went wrong creating recipe'))
+// })
+// .catch(error => {
+//   console.error('Error connecting to the database', error);
+// });
+
+//ITERATION 3 - Create many recipes //
+
+
+
+ 

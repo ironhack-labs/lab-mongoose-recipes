ad2682de1f72b0aeb5ec7599b0223a6668810fa9
sherilin <sherilinm96@gmail.com>
2022-04-14 11:10:46 +0200

    done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,46 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  name: {
+    unique: true,
+    type: String
+  },
+
+  level: {
+    type: String,
+  },
+
+  ingredients: {
+    type: [String]
+  },
+
+  cuisine: {
+    type: String,
+    require: true
+  },
+
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert']
+  },
+
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+
+  duration: {
+    type: Number,
+    minlenght: 0
+  },
+
+  creator: {
+    type: String
+  },
+
+  created: {
+    Date: today
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

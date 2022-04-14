56b9d6d9583bb75ae76d53c8ded4de861c5177ca
Clara Pardo <clara.pardo@hotmail.com>
2022-04-14 09:22:21 +0200

    done

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const Recipe = require('./models/Recipe.model')   // Import of the model Recipe from './models/Recipe.model.js'
+const data = require('./data')                    // Import of the data from './data.json'
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const MONGODB_URI = 'mongodb://localhost/recipe-app'
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
+    console.log(`Connected to the database: "${x.connection.name}"`)
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
+
+
+  // // ## Iteration 3 - Insert multiple recipes
+  .then(() => Recipe.create(data))
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe
+      .find()
+      .select('title')
   })
+  .then((recipes) => console.log(recipes))
+
+
+  // // ## Iteration 4 - Update recipes
+  .then(() => Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }))
+  .then(console.log('----> UPDATE DONE <----'))
+
+
+  // // ## Iteration 5 - Remove a recipe
+  .then(() => Recipe.deleteOne({ title: "Carrot Cake" }))
+  .then(console.log('----> REMOVAL DONE <----'))
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+
+// // ## Iteration 6 - Close the Database
+process.on('SIGINT', () => {
+  then.mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination')
+    process.exit(0)
+  })
+})
+
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: Array,
+  cuisine: String,
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created: {
+    type: Date,
+    default: new Date().getDate()     //// ¿? ¿? ¿? ¿?
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

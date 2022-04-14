7fa62930ae730eb873fdb43a596957c6468e2af7
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-04-14 09:16:18 +0200

    done

--- index.js
+++ index.js
@@ -15,9 +15,40 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  .then(() => Recipe.syncIndexes())
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({
+        title: "foundee",
+        level: "Easy Peasy",
+        ingredients: ["cheese", "liquor"],
+        cuisine: "french",
+        dishType: "soup",
+        duration: 10,
+        creator: "Alejandro",
+      })
+
+      .then(() => {
+        return Recipe.insertMany(data)
+      })
+
+      .then(() => {
+        return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+      })
+      .then(() => {
+        return Recipe.deleteOne({ title: "Carrot Cake" })
+      })
+    // .then(recipes => console.log("recetas", recipes))
+    // .then(newRecipe => console.log("la nueva receta es", newRecipe))
+    //.catch(error => console.log('Hubo un error:', error))
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+mongoose.connection.close(() => {
+  console.log('Mongoose default connection disconnected through app termination')
+})
+

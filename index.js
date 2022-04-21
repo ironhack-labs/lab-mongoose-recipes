586bc019b26bc61f09338a7a97770bc1d0b18895
nash0x <nachoest@gmail.com>
2022-04-21 13:59:00 -0600

    Completed lab

--- index.js
+++ index.js
@@ -15,8 +15,56 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  // Iteration 2
+  .then(async () => {
     // Run your code here, after you have insured that the connection was made
+    await Recipe.create({
+      "title": "Aguachile",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      "cuisine": "Mexican",
+      "dishType": "main_course",
+      "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      "duration": 40,
+      "creator": "LeNache"
+    })
+      .then(recipe => console.log(recipe.title))
+      .catch(error => console.log(error))
+  })
+  //Iteration 3
+  .then(async () => {
+    //insertData()
+    await Recipe.insertMany(data)
+      .then(recipeList => {
+        recipeList.forEach(recipe => console.log(recipe.title))
+      })
+      .catch(error => console.log(error))
+  })
+  //Iteration 4
+  .then(async () => {
+    await Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+      .then(() => console.log("Your changes have been successful"))
+      .catch(() => console.log("Error in your changes"))
+  })
+  //Iteration 5
+  .then(async () => {
+    await Recipe.deleteOne({title:"Carrot Cake"})
+    .then(() => console.log("Recipe was removed"))
+    .catch(error => console.log(error))
+  })
+  .then(()=>{
+    //Iteration 6
+    mongoose.connection.close(()=>console.log("Connection closed"))
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

53258337d6977dd1ba9a6385ce60fc0ed7e6046f
jidekickpush <jeandenisdrane@gmail.com>
2022-04-21 16:09:53 +0200

    First iteration done by Alejandro and Jide

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,37 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    String,
+    required: true,
+    unique: true
+  },
+  level: {
+    type: [String],
+    enum: ["Easy Peasy","Amateur Chef","UltraPro Chef"]
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: String,
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack","drink", "dessert","other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration:{
+    type: Number,
+    min: 0
+  },
+  creator: String,
+  created:{
+    type: Date,
+    default: () => Date.now() + 7*24*60*60*1000
+  }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

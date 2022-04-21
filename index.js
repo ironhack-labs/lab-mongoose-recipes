c674426c51e11946d3ac33094bdebdf79da984ae
Georgina Aceves <aceves.georgina@hotmail.com>
2022-04-21 23:20:33 +0200

    Lab not completed

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+const { modelName, insertMany } = require('./models/Recipe.model');
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -16,8 +18,21 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+    async function insertRecets(){
+      try{
+        const NewRecet = await Recipe.create({
+          title:"Omelette au fromage",
+          level:"UltraPro Chef",
+          ingredients:["huevos","queso"],
+          cuisine:"Francesa",
+          dishType: "breakfast",
+          duration: 5,
+          creator: "Dexter",
+          created:21/04/2022
+        })
+      }
+    }
+    })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,45 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const {Schema,model} = require("mongoose");
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+
+title:{
+  type: String,
+  required: true,
+  unique: true,
+},
+level:{
+  type: String,
+  enum:["Easy Peasy","Amateur Chef","UltraPro Chef"]
+},
+ingredients:{
+  type:[String]
+},
+cuisine:{
+  type: String,
+  required: true,
+},
+dishType:{
+  type: String,
+  Default:"https://images.media-allrecipes.com/images/75131.jpg" 
+},
+duration:{
+  type: Number,
+  minimum: 0
+},
+creator:{
+  type: String
+},
+created:{
+  type: Date,
+  default: today
+}
+
+
+},{timetamps:true});
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
-module.exports = Recipe;
+module.exports = model ("Recipe",userSchema)

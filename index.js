8d316403a48c1a9ef30aec80a172e1ca4bcd423a
Ivo Silva <silvaivosoares@gmail.com>
2022-04-21 15:17:51 +0100

    Completed the first 2 iterations

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { schema } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -16,7 +17,26 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const arrabiata = {
+      title: "Arrabiata",
+      level: "Easy Peasy",
+      ingredients: ["pasta", "olive oil", "garlic", "tomatoes", "chilli flakes", "parsley"],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "./public/images/arrabiata.jpeg",
+      duration: 20,
+      creator: "Gordon Ramsey",
+      created: "2022-01-01"
+    }
+
+
+    Recipe.create(arrabiata)
+      .then(response => {
+        console.log(response.title);
+      })
+      .catch((error) => {
+        console.log("There was the following error creating the recipe:", error)
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

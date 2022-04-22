05433279ec9d622a5f1fac8f1c9085092b3c3409
Ariana <pilarsv@outlook.com>
2022-04-22 10:23:30 -0500

    Completed lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,54 @@
-const mongoose = require('mongoose');
+/* const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-});
+}); */
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const {Schema, model} = require("mongoose");
 
-module.exports = Recipe;
+/* const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe; */
+
+const recipeSchema = new Schema({
+  title:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  level:{
+    type:String,
+    enum:['Easy Peasy',' Amateur','Chef','UltraPro Chef'],
+    default:'otro'
+  },
+  ingredients:{
+    type:[ String ]
+  },
+  cuisine:{
+    type:String,
+    required:true
+  },
+  dishType:{
+    type:String,
+    enum:['breakfast',' main_course','soup','snack','drink','dessert'],
+    default:'other'
+  },
+  imagen:{
+    type:String,
+    default:"https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration:{
+    type:Number
+  },
+  creator:{
+    type:String
+  },
+  created:{
+    type:Date,
+    default:"today"
+  }
+
+},{timestamps:true})
+
+module.exports = model('Recipe', recipeSchema)

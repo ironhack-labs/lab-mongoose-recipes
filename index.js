c8aa83359e90e8a9a213a4f574831766a1572b46
cristina <cristinahurtadocm@gmail.com>
2022-05-02 23:13:06 +0200

    done

--- index.js
+++ index.js
@@ -15,9 +15,62 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async () => {
+    const recipe = {
+      title: "Delicious recipe",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu"
+    }
+    await Recipe.create(recipe)
+      .then(recipe => console.log(recipe.title))
+      .catch(error => console.log(error))
   })
+  .then(async () => {
+    await Recipe.insertMany(data)
+      .then(list => {
+        list.forEach(recipe => console.log(recipe.title))
+      })
+      .catch(error => console.log(error))
+  })
+
+  .then(async () => {
+    await Recipe.findOneAndUpdate({
+      title: "Rigatoni alla Genovese",
+    },
+      {
+        duration: 100,
+      })
+      .then(() => console.log("Success editing! :)"))
+      .catch(error => console.log(error))
+  })
+
+  .then(async () => {
+    await Recipe.deleteOne({
+      title: "Carrot Cake",
+    })
+      .then(() => console.log("Data deleted"))
+      .catch(error => console.log(error))
+  })
+
+  .then(async() => {
+   await mongoose.connection.close();
+   console.log("Bye 🫡")
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

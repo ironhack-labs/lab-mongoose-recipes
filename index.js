ae58b0f69f512277498578bc350cb9324025e8bc
Jessica Fernandez <jferasef@gmail.com>
2022-05-03 19:04:45 +0200

    final-changes

--- index.js
+++ index.js
@@ -7,44 +7,6 @@ const data = require("./data");
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-const recipes = {
-  title: {
-    type: String,
-    unique: true,
-  },
-  level: {
-    type: String,
-    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
-  },
-  ingredients: [String],
-  cuisine: {
-    type: String,
-    unique: true,
-  },
-  dishType: {
-    type: String,
-    enum: [
-      "breakfast",
-      "main_course",
-      "soup",
-      "snack",
-      "drink",
-      "dessert",
-      "other",
-    ],
-  },
-  image: {
-    type: String,
-    default: "https://images.media-allrecipes.com/images/75131.jpg",
-  },
-  duration: {
-    type: Number,
-    min: 0,
-  },
-  creator: String,
-  created: { type: Date, default: "today" },
-};
-
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -54,12 +16,12 @@ mongoose
     return Recipe.deleteMany();
   })
   .then(() => {
-    Recipe.create(recipes).then((createdRecipes) => {
+    Recipe.create(data).then((createdRecipes) => {
       console.log(`Created ${createdRecipes.length} recipes!`);
     });
   })
   .then(() => {
-    Recipe.insertMany(recipes, (error, docs));
+    Recipe.insertMany(data, (error, docs));
   })
   .then(() => {
     Recipe.findOneAndUpdate(
@@ -70,11 +32,11 @@ mongoose
     );
   })
   .then(() => {
-    Recipes.deleteOne({ title: "Carrot Cake" });
+    Recipe.deleteOne({ title: "Carrot Cake" });
     console.log("Oh no!!!, we run out of Carrot Cakes");
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
 
-// connection.end();
+connection.end();

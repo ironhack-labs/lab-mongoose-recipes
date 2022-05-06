2f1c8d3b10004a5c975a2afc1486bd468c6b5376
thaismachado31 <thaismachado31@gmail.com>
2022-05-06 20:37:27 -0300

    lab feito

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
+  })
+  // .then(() => {
+  //   const recipeCreated = Recipe.create({
+  //     title: "Dadinho de Tapioca",
+  //     level: "Easy Peasy",
+  //     ingredients: [
+  //       "Tapioca em grãos",
+  //       "Queijo coalho",
+  //       "Leite integral",
+  //       "Pimenta do reino",
+  //     ],
+  //     cuisine:
+  //       "Coloque o leite para ferver. Enquanto isso, rale o queijo coalho. Quando o leite ferver e quase vazar da panela, leve ele para um bowl e misture com a tapioca e o queijo coalho. Coloque numa assadeira envolta com um plastico filme, embrulhando-o, e depois leve a geladeira por 2h. Depois, corte a massa em cubos e leve para fritar.",
+  //     dishType: "snack",
+  //     duration: 150,
+  //     creator: "Unknown",
+  //   });
+  //   console.log("new recipe: ", recipeCreated)
+  // })
+  .then(() => {
+    const recipeList = Recipe.insertMany(data);
+    console.log("Recipe list: ", recipeList);
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const updateRigatoni = Recipe.updateOne(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } }
+    );
+    console.log("You have successfully updated the recipe!");
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    const deleteRecipe = Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log("You have successfully deleted this recipe!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+mongoose.connection.close();

6dfa49c0a42c7798a8d35f5c9c5cd7ba3f61ff12
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-06-02 17:47:08 +0200

    iteration 4,5

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ mongoose
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
+    // return Recipe.deleteMany();
   })
+  //
+  // Iteration 2
+  //
   // .then(() => {
   //   // Run your code here, after you have insured that the connection was made
   //   const newRecipe = Recipe.create({
@@ -27,9 +30,12 @@ mongoose
   //       "https://img-3.journaldesfemmes.fr/Qf4POYz6ateXL7_PU55SSeKNKf4=/750x500/smart/a5181741d55b4b3d8b54bda593ff87f5/recipe-jdf/10021667.jpg",
   //     duration: 20,
   //   });
-  //   return newRecipes;
+  //   return newRecipe;
   // })
   // .then((newRecipe) => console.log(newRecipe))
+  //
+  // Iteration 3
+  //
   // .then(() => {
   //   const newRecipes = Recipe.insertMany(data);
   //   return newRecipes;
@@ -39,6 +45,33 @@ mongoose
   //     console.log(recipe.title);
   //   })
   // )
+  //
+  // Iteration 4
+  //
+  // .then(() => {
+  //   const query = { title: "Rigatoni alla Genovese" };
+  //   console.log({ query });
+  //   const update = { duration: 100 };
+  //   console.log({ update });
+  //   const newUpdate = Recipe.findOneAndUpdate(query, update, { new: true });
+  //   return newUpdate;
+  // })
+  // .then((newUpdate) =>
+  //   console.log(`You have succeffully update your ${newUpdate.title}`)
+  // )
+
+  //
+  // Iteration 5
+  //
+  // .then(() => {
+  //   const query = { title: "Carrot Cake" };
+  //   const removedCount = Recipe.deleteOne(query);
+  //   return removedCount;
+  // })
+  // .then((removedCount) =>
+  //   console.log(`You have succeffully delete ${removedCount.deletedCount} recipe`)
+  // )
+
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  // TODO: write the schema 
+  // Iteration 1
   title: { type: String, required: true },
   level: {
     type: String,

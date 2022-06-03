bc5cba24cbc76e20639c436e88835c26824c2803
zarahsara <sarahabbasi25@aol.com>
2022-06-03 22:46:18 +0100

    test2

--- index.js
+++ index.js
@@ -17,7 +17,55 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+  
+  
+  const YorkshirePuddingRecipe = {
+    title: 'Yorkshire puddings',
+    level: 'amateur chef',
+    ingredients: ['175g plain flour','1 tsp salt',' 3 eggs, beaten','300ml cold full-fat milk'],
+    cuisine: 'English',
+    dishtype: 'maincourse',
+    images: 'https://99easyrecipes.com/english-yorkshire-pudding/',
+    duration: 1,
+    creator: 'jamieoliver',
+ };
+  
+ Recipe.create(YorkshirePuddingRecipe)
+ 
+ .then(YorkshirePuddingRecipe => console.log(`recipe added: ${YorkshirePuddingRecipe.title}`))
+ 
+ .catch(err => console.log(err));
+  
+  
+  
+ Recipe.insertMany(data)
+ .then(recipe => {
+   recipe.forEach(item => {
+     console.log(`recipe for ${item.title} inserted successfully`);
+   });
+ })
+ .catch(err => console.log(err));
+  
+  
+  
+  
+ Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+ .then(() => console.log(`The recipe is updated`))
+ .catch(err => console.log(err));
+  
+  
+  
+ Recipe.deleteOne({ title: 'Carrot Cake' })
+ .then(() => console.log(`The recipe is deleted`))
+ .catch(err => console.log(err));
+
+})
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+ 
+  mongoose.connect
+  .close()
+ console.log(`Connection closed')
+}
+};

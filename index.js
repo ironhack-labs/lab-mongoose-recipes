e6ad2df3d8e594b12a80a422cb27ee9ec6dcb506
Ulises-lab <yaftemiranda@gmail.com>
2022-06-04 01:33:05 -0500

    Iteracion 5 lista

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     //return Recipe.deleteMany() ==> Comentado para la iteracion 4
   })
-Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration:100})
-  .then(() => {
+//Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration:100})
+Recipe.deleteOne({title:'Carrot Cake'})
+.then(() => {
     // Run your code here, after you have insured that the connection was made
 	  //ITERACION 2: return Recipe.create(data[0]);
 	  //ITERACION 3: Recipe.insertMany(data)
-	  //ITERACION 4: Linea 18 y linea 24
-		console.log('La base de datos ha sido actualizada exitosamente :D')
+	  //ITERACION 4: Linea 18 y el console.log.
+	 // ITERACION 5: Linea 19 y el console.log
+	  console.log('La base de datos ha sido actualizada exitosamente :D')
 	  })
   .catch(error => {
     console.error('Error connecting to the database', error);

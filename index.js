38753afb4bd59bdbdf9f2ca40ad0846a18f79658
Ulises-lab <yaftemiranda@gmail.com>
2022-06-05 00:38:27 -0500

    reajuste el codigo pero aun no pude cerrar la base de datos y no entiendo porque el console log de la linea 35 me lo esta mostrando hasta el ultimo...

--- index.js
+++ index.js
@@ -7,30 +7,46 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const huevito = {
+	"title":"huevito con catsup",
+	"level":"Amateur Chef",
+	"ingredients": [
+	"2 huevitos",
+	"sal",
+	"catsup"
+	],
+	"cuisine":"Mexican",
+	"dishType":"breakfast",
+	"image":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.buzzfeed.com%2Fmx%2Fcrystalro%2Fque-tan-raros-son-tus-gustos-en-comida&psig=AOvVaw2PEjHNSG_QmZlZbJ9YTHUQ&ust=1654491206042000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLCMjZfClfgCFQAAAAAdAAAAABAD",
+	"duration": 30,
+	"creator":"mexican power"
+}
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    //return Recipe.deleteMany() ==> Comentado para la iteracion 4
+    return Recipe.deleteMany() //==> Comentado para la iteracion 4
   })
-//Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration:100})
-Recipe.deleteOne({title:'Carrot Cake'})
-.then(() => {
-    // Run your code here, after you have insured that the connection was made
-	  //ITERACION 2: return Recipe.create(data[0]);
-	  //ITERACION 3: Recipe.insertMany(data)
-	  //ITERACION 4: Linea 18 y el console.log.
-	 // ITERACION 5: Linea 19 y el console.log
-	  console.log('La base de datos ha sido actualizada exitosamente :D')
-	  })
+  .then(() => {
+	  //ITERACION 2: 
+	  Recipe.create(huevito)
+            .then(console.log('Se ha actualizado la base de datos'))
+	//ITERACION 3: 
+	  Recipe.insertMany(data)
+	//ITERACION 4 
+	Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration:100})
+	//ITERACION 5 
+	Recipe.deleteOne({title:'Carrot Cake'})
+
+  })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
 
-	//ITERACION 3 linea 22 y 29-31
-/* Recipe.find({title:{$exists:true}},{title:1})
-	  .then(titulos=>console.log('Los titulos son: ',titulos))
-	  .catch(error=>console.log("Surgio un error: ",error))
-*/
+
+Recipe.find({title:{$exists:true}},{title:1})
+  .then(titulos=>console.log('Los titulos son: ',titulos))
+  .catch(error=>console.log("Surgio un error: ",error))
+

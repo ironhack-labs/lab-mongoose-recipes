a1e514ede4a280edb10729e0ac37d169fd6c1ab0
emmanuelsolis <emma.ruso@outlook.com>
2022-06-11 09:09:15 -0500

    Iteration 1 y 2 solved

--- index.js
+++ index.js
@@ -15,9 +15,27 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  /* .then(() => {
+    const sojaCochinita = new Recipe({
+      title: 'Soja Cochinita',
+      level: 'Easy Peasy',
+      ingredients:['soja', 'onion','garlic', 'achiote', 'orange', 'white vinigar', 'olive oil', 'peper', 'salt'],
+      cuisine: 'Mexican',
+      dishType: ['main_course'],
+      image:'https://img-global.cpcdn.com/recipes/96007f400c84167d/680x482cq70/tacos-veganos-de-cochinita-pibil-de-soja-texturizada-foto-principal.webp',
+      duration: 30,
+      creator: 'Emmanuel Solis',
+      created: '06/07/2022'
+    });
+    //Destructuration to get the title and the level of the recipe
+    // let { title, level, ingredients, cuisine, dishType, image, duration, creator, created } = sojaCochinita;
+    sojaCochinita.save();
+
+    console.log(`Recipe "${sojaCochinita.title}" has been added to the database`)
+    return Recipe.insertMany(data);
+
     // Run your code here, after you have insured that the connection was made
-  })
+  }) */
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })

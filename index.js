28bd7a7f8c2867fd5b83685331ca415d4de8ee2a
afisman <afisman@gmail.com>
2022-06-22 16:26:32 +0200

    Recipes lab, Alejandro, Javier, Ovidiu

--- index.js
+++ index.js
@@ -4,20 +4,48 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { create } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({ title: 'Huevos fritos', level: 'Easy Peasy', ingredients: ['Huevos', 'aceite', 'sal'], cuisine: 'allAround', dishType: 'breakfast', duration: 5, creator: 'gallina' })
+  })
+  .then(() => {
+    return Recipe.create(data)
+  })
+  .then(recipesCreated => {
+
+    recipesCreated.forEach(recipe => {
+      console.log(recipe.title)
+    })
+
+    return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  })
+  .then((updatedRecipe) => {
+    console.log('ACTUALIZADA', updatedRecipe)
+
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+
+  })
+  .then((deletedRecipe) => {
+    console.log('Eliminada', deletedRecipe)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination')
+    process.exit(0)
+  })
+})

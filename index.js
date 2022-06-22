23054d6d5a7e3fd3a6baf21e9f8c78e670d07b8b
Natalia Pinzon <nanuta522@gmail.com>
2022-06-22 16:18:15 +0200

    done

--- index.js
+++ index.js
@@ -17,7 +17,36 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    Recipe
+      .create({
+        title: 'fried egg',
+        level: 'UltraPro Chef',
+        ingredients: ['egg', 'oil', 'salt'],
+        cuisine: 'Fusion',
+        dishType: 'main_course',
+        duration: 27,
+        creator: 'Santi'
+      })
+      .then(recipe => {
+        console.log(recipe.title)
+        return Recipe.create(data)
+      })
+      .then(recipes => {
+        recipes.forEach(recipe => console.log(recipe.title))
+        return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true })
+      })
+      .then(recipe => {
+        console.log(recipe)
+        return Recipe.deleteOne({ title: 'Carrot Cake' },)
+      })
+      .then(info => {
+        console.log(info)
+        mongoose.connection.close()
+      })
+      .catch(err => console.log(err))
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.error('Error connecting to the database', error)
+  })
+

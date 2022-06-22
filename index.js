694c7907c8625d2ef111460e1138aad513464ccc
Thales <dan.console.log@gmail.com>
2022-06-22 16:25:30 +0200

    done

--- index.js
+++ index.js
@@ -13,11 +13,46 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany()
+
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+
+
+// ITERATION 2
+// Recipe
+//   .create({ title: 'pesto', level: 'Easy Peasy', cuisine: 'italian', dishType: 'main_course', image: 'https://www.hogarmania.com/archivos/201704/pesto-1280x720x80xX.jpg', direction: 5, duration: 15, creator: 'Rafa', created: '2022-06-21' })
+//   .then(Recipe => {
+//     console.log(Recipe.title)
+//     // Run your code here, after you have insured that the connection was made
+//   })
+//   .catch(error => {
+//     console.error('Error connecting to the database', error);
+//   });
+
+// ITERATION 3
+// Recipe
+//   .create(data)
+//   .then(Recipe => {
+//     Recipe.forEach(elm => console.log(elm.title))
+//     // Run your code here, after you have insured that the connection was made
+//   })
+//   .catch(error => {
+//     console.error('Error connecting to the database', error);
+//   });
+
+// ITERATATION 4
+
+// Recipe
+//   .updateOne({ duration: 220 }, { duration: 110 })
+//   .then(info => console.log(info))
+//   .catch(err => console.log('Oh, la cagaste'))
+
+// ITERATION 5
+// Recipe
+//   .deleteOne({ title: 'Carrot Cake' })
+//   .then(info => console.log('Este es un objeto informativo sobre una elimiación', info))
+//   .catch(err => console.log('Se produjo un error', err))
+
+// ITERATION 6: Nos dimos cuenta de que podríamos haber anidado todo desde el principio :D... tarde
+mongoose.connection.close()

67318349285a78a5d681c160e1e06399234c8c58
HanhDinh <ms.myhanh89@gmail.com>
2022-06-30 16:57:23 -0400

    solved lab

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const dataArray = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -16,8 +16,42 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+   
+    return Recipe.create(dataArray[0])
   })
+  .then(newRecipe =>{
+    console.log(newRecipe)
+    return Recipe.insertMany(dataArray)
+  })
+  .then(recipeResult => {
+    recipeResult.forEach((element) => {
+
+      console.log(element.title)
+    })
+    
+  })
+  // .then(newRecipe=>{
+  //   newRecipe.forEach(element =>{
+  //     console.log(element.title)
+  //   })
+  // })
+
+  //4 - update recipe
+  .then(() =>{
+    return Recipe.findOneAndUpdate ({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  })
+  .then(()=> console.log(`Rigatoni alla Genovese is updated!`))
+
+//5 - remove recipe
+.then(() =>{
+  return Recipe.deleteOne ({title: 'Carrot Cake'})
+})
+.then(()=> console.log(`Carror Cake is updated!`))
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+//close
+mongoose.connect.close()
+console.log(`connection closed!`)

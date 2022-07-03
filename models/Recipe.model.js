474dbf3f5f55600dd66bb7b74256f67f83449881
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-07-03 11:35:38 +0200

    lab started

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,28 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: [true, 'Please insert a title.']
+  },
+  level: {
+    type: String,
+    enum : {
+    "Easy Peasy" : 1,
+    "Amateur Chef" : 2,
+    "UltraPro Chef" : 3
+  }
+  },
+  ingredients: [String],
+  cuisine: {
+    type: String,
+    required: [true, 'Please inster type of cuisine.']
+  },
+  dishType: String /*posibles valores "Breakfast, main-course, soup, snack, drink, dessert or other"*/,
+  image: String /*Default value "https://images.media-allrecipes.com/images/75131.jpg"*/,
+  duration: Number /*minimun value = 0*/,
+  creator: String,
+  created: Date /*by default today*/
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

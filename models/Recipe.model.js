bad980e9ae1947f98546d60b1a9b421eb0c3ca41
CrisArranz <c.arranz.arevalo@gmail.com>
2022-07-05 11:32:00 +0200

    Refactoring iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  title: String,
-  level: String,
+  title: {type: String, require: true, unique: true},
+  level: { type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"] },
   ingredients: [String],
-  cuisine: String,
-  dishType: String,
+  cuisine: {type: String, require: true},
+  dishType: {type: String, enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]},
   image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
-  duration: Number,
+  duration: {type: Number, min: 0},
   creator: String,
   created: { type: Date, default: Date.now }
 });

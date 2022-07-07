fca42020fea879ee2289ae54c5737dddb36dd6f7
Matthieu Duperrey <matth.duperrey@gmail.com>
2022-07-07 18:02:47 +0200

    changes

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,22 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  level: String,
+  ingredients: [],
+  cuisine: String,
+  dishType: String,
+  image: {
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+    type: String,
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: String,
+
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

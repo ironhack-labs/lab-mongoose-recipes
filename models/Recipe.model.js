555d9cfbf0497cc9fb1897e84b3c797e958dafa1
rubenanlo <85644491+rubenanlo@users.noreply.github.com>
2022-07-07 23:17:04 +0200

    assessment completed

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,6 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+  title: String,
+  level: String,
+  ingredients: [String],
+  cuisine: String,
+  dishType: String,
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+    duration: Number,
+    creator: String,
+    created: {
+      type: Date,
+      default: Date.now, // Source: https://stackoverflow.com/questions/29899208/mongoose-date-field-set-default-to-date-now-n-days
+  },
   // TODO: write the schema
 });
 

b1fb58a7005f3f82f99f5466480bc26d8a06b9ea
violeta atkinson <violeta.atkinson@gmail.com>
2022-07-16 14:11:50 +0200

    it-2

--- index.js
+++ index.js
@@ -16,8 +16,32 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+    const receta = {
+      title:"Empanadas",
+      level:"Easy Peasy",
+      ingredients:[ "cebolla","masa de empanadas","carne picada","moron"],
+      cuisine:"argentinian",
+      dishType:"main_course",
+      duration: 60,
+      creator:"violeta",
+      created:new Date(),
+    }
+
+    // receta es la constasnte que contiene todos los datos
+    const newRecipe = new Recipe(receta);
+
+    newRecipe
+    .save() // esto la guarda
+    .then((recipe) => {
+      console.log(recipe.title);
+    })
+    
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+    

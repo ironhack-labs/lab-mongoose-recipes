a6a088887b26de0afebf9457a7ed7cdf5f0c464b
Marina Laporte Boni <marinaolboni@gmail.com>
2022-07-18 13:12:07 +0200

    done

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
+const { insertMany } = require("./models/Recipe.model");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const steps = {
+      title: "Feijoada",
+      level: "UltraPro Chef",
+      ingredients: ["Feijão", "Cebola", "Alho", "Carnes"],
+      cuisine: "brazilian",
+      dishType: "main_course",
+    };
+
+    const recipe = new Recipe(steps);
+
+    return recipe.save().then((recipe) => console.log(recipe.title));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then((arrRecipes) => {
+    arrRecipes.forEach((recipe) => console.log(recipe.title));
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log("Recipe deleted.");
+    mongoose.connection.close(function () {
+      console.log("Mongoose disconnected on app termination");
+      process.exit(0);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

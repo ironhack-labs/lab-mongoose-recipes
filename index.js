073b4ba98936805a56e5ce2c45a17b42392a3080
Nuno <nunodpmorais@gmail.com>
2022-07-23 15:46:21 +0100

    I forget to edit the index.js

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ mongoose
         "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
       duration: 30,
       creator: "John Doe",
-      created: new Date(),
     });
     return recipeCreated;
   })

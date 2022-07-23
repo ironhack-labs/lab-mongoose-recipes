5fd41af8b27776a88c1d39054635e5f99a523cc2
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-07-23 17:18:13 +0200

    finish till iteration 3

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+// Schema (mongoose term)
+const recipeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    level: { type: String, required: true, unique: true },
+    ingredients: [String],
+    cuisine: { type: String, required: true },
+    dishType: { type: String },
+    image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg",
+    },
+    duration: { type: Number, default: 0 },
+    creator: { type: String },
+    created: { type: Date, default: new Date() },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

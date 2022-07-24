177cb9c4ce4df5118d1a11cdbb50c611d33f0486
Elvan Ipeker <eipekerdogan@hotmail.com>
2022-07-24 20:20:50 +0200

    done

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const recipes = require('./data.json');
 // Import of the data from './data.json'
-const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+// Import of the model Recipe from './models/Recipe.model.js'
+const Recipe = require("./models/Recipe.model")
 
 // Connection to the database "recipe-app"
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then(async (x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
+
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async (x) => {
+    await Recipe.create({
+      title: "Banana Cake",
+      level: "basic",
+      ingredients: ["banana", "sugar", "milk"],
+      cuisine: "American",
+      dishType: "dessert",
+      image: "https://www.delish.com/cooking/recipe-ideas/a22745193/easy-banana-cake-recipe/",
+      duration: 30,
+      creator: "elvan",
+      created: new Date(),
+    })
+    console.log(`Banana Cake Recipe`)
+  })
+  .then(async (x) => {
+    await Recipe.deleteMany()
+  })
+  .then(async (x) => {
+    await Recipe.insertMany(recipes)
+  })
+  .then(async (x) => {
+    await Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  })
+  .then(async (x) => {
+    await Recipe.deleteOne({ title: "Carrot Cake" })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
+  })
+  .finally(async () => {
+    await mongoose.connection.close()
   });
+
+

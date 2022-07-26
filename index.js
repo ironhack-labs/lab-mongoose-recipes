7c8c0f5222dacdb4061ca4e5c78e95e1c634fa99
Enrique Orta <enriqueorta1994@gmail.com>
2022-07-25 23:55:39 -0400

    done

--- index.js
+++ index.js
@@ -17,7 +17,39 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    Recipe.create({
+      title: "Arroz con pollo",
+      level: "Easy Peasy",
+      ingredients: ["arroz", "pollo", "maiz", "adobo", "agua"],
+      cuisine: "Venezolana",
+      dishType: "other",
+      duration: "20",
+      creator: "Kike",
+      created:""
+    })
+      .then(recipe => 
+        console.log(recipe))
+      .catch(err => 
+        console.log("ERROR!"))
+  
+        Recipe.insertMany(data)
+          .then(console.log("Data add"))
+          .catch(error => console.log('error creating recipe', error))
+  
+        Recipe.updateOne({title: "Rigatoni alla Genovese"}, {duration: 100})
+      .then( recipe => console.log("changes"))
+      .catch((err) => console.log(err))
+  
+      Recipe.deleteOne({title: "Carrot Cake"})
+      .then( recipe => {
+        console.log("delete")
+        mongoose.connection.close(() => {
+          console.log('disconect')
+          process.exit(0)
+        })
+      })
+      .catch((err) => console.log(err))
+ })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })

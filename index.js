641c82de25259c5d6367197cf605e157cc3188ff
Luiz Lima <luizlimadevpro@gmail.com>
2022-07-29 17:24:11 +0100

    [FTLIS072022]

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+	.connect(MONGODB_URI)
+	.then((x) => {
+		console.log(`Connected to the database: "${x.connection.name}"`);
+		// Before adding any recipes to the database, let's remove all existing ones
+		return Recipe.deleteMany();
+	})
+	.then(() => {
+		return Recipe.create({
+			title: "Feijao Tropeiro",
+			level: "Amateur Chef",
+			ingredients: [
+				"feijao",
+				"farinha",
+				"bacon",
+				"ovos",
+				"couve",
+				"linguiça",
+				"torresmo",
+			],
+			cuisine: "Mineira",
+			dishType: "main_course",
+			duration: 1,
+			creator: "Analina",
+			created: Date.now(),
+		}).then((createDish) => console.log(createDish.title));
+	})
+	.then(() => {
+		return Recipe.insertMany([...data]);
+	})
+	.then(() => {
+		return Recipe.findOneAndUpdate(
+			{ title: "Rigatoni alla Genovese" },
+			{ duration: 100 }
+		);
+	})
+	.then(() => {
+		return Recipe.deleteOne({ title: "Carrot Cake" });
+	})
+	.then(() => {
+		return mongoose.connection.close();
+	})
+	.catch((error) => console.log("Error", error));

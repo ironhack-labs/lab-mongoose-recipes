8471c9ad0296ce042f902eefccacaf84a0f7f180
Majid Boukachni <boukachni@gmail.com>
2022-07-29 18:03:05 +0200

    done

--- index.js
+++ index.js
@@ -1,23 +1,70 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const Recipe = require("./models/Recipe.model");
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const recipeDetails = {
+      title: " Soup",
+      level: "Easy Peasy",
+      ingredients: [
+        "1 tbsp butter",
+        "2 garlic cloves",
+        "1 shallot",
+        "2 broccoli heads",
+        "1/2 tsp black pepper",
+        "2 1/2 cups vegetable stock",
+      ],
+      cuisine: "soup",
+      dishType: "soup",
+      image:
+        "https://www.notenoughcinnamon.com/wp-content/uploads/2014/02/Simple-Broccoli-Soup-NotEnoughCinnamon.com1_.jpg",
+      duration: 20,
+      creator: "Majid",
+      created: new Date(),
+    };
+    return Recipe.create(recipeDetails);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((response) => {
+    console.log(response.title);
+  })
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then((response) => {
+    response.forEach((recipe) => {
+      console.log(recipe.title);
+    });
+  })
+  .then(() => {
+    const filter = { title: "Rigatoni alla Genovese" };
+    const update = { duration: 100 };
+    return Recipe.findOneAndUpdate(filter, update, {
+      returnOriginal: false,
+    });
+  })
+  .then((response) => {
+    console.log("Recipe is now updated:");
+    console.log(response);
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((response) => {
+    console.log("Recipe delete attempt:");
+    console.log(response);
+  })
+  .then(() => {
+    mongoose.connection.close();
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
   });

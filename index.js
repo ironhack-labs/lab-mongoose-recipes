74759bbea3c923fd00ae4a24e049a978d706b15a
Paulo Torres <pveg@live.com>
2022-07-29 16:30:36 +0100

    solved lab

--- index.js
+++ index.js
@@ -13,11 +13,29 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+return Recipe.create({title:'Pad Thai', level: 'UltraPro Chef', cuisine: `Rachel's cuisine`})
+.then((value) => console.log(value.title))
+.catch((err)=> console.log(err))
   })
-  .catch(error => {
+.then(() => {
+  return Recipe.insertMany([...data])
+})
+.then(() => {
+return Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+.then(() => console.log('Successfully changed!'))
+.catch(err => console.log(err))
+})
+.then(()=>{
+return Recipe.deleteOne({title: 'Carrot Cake' })
+.then(value => console.log(value))
+.catch(err => console.log(err)); 
+})  
+.then(() =>{
+return mongoose.connection.close();
+})
+.catch(error => {
     console.error('Error connecting to the database', error);
-  });
+}); 

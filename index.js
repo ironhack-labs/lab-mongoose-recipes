d5c252569789b473070c6b065285ecc454e65bbf
“Brenda <brndglopes@gmail.com>
2022-07-30 18:21:36 +0100

    lab-mongoose-recipes finished

--- index.js
+++ index.js
@@ -13,11 +13,42 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+   /* return Recipe.deleteMany()*/
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
+   /*  Recipe.create({
+      title: 'Lasagna',
+      level: 'Amateur Chef', 
+      ingredients: ['Pasta for lasagna', 'minced beef', 'mozzarella cheese', 'parmesan cheese', 'tomate sauce', 'milk', 'butter', 'all purpose flour'],
+      cuisine: 'Italian',
+      dishType: 'main_course',
+      image: 'https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg',
+      duration: 50,
+      creator: 'Brenda', 
+    })
+    .then((firstRecipe)=> console.log(firstRecipe.title))
+    .catch((err) => console.log(err));
+  }) */
+/* 
+  Recipe.insertMany(data)
+  .then((recipes) => console.log(recipes))
+  .catch((err) => console.log(err)); */
+   
+  Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
+  .then((recipe) => console.log('Recipe successfully updated', recipe))
+  .catch((err) => console.log(err))
+
+  Recipe.deleteOne({title: 'Carrot Cake'}, {new:true})
+  .then((recipe) => console.log('Recipe successfully deleted', recipe))
+  .catch((err) => console.log(err));
+})
+
+   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+ 
+  .then(() => mongoose.connection.close())
+  .catch((err) => console.log(err));
+
+  
+

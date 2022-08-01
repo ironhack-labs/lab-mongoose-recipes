07e7a2917faa73b8895434d3e827c3199e9884e8
lzaquine <zaquinelucas@gmail.com>
2022-08-01 09:35:13 +0100

    async await way

--- index.js
+++ index.js
@@ -1,61 +1,114 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose
+/* mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then((newRecipe) => {
     newRecipe = new Recipe({
-      title: 'Biryani',
-      level: 'Amateur Chef',
+      title: "Biryani",
+      level: "Amateur Chef",
       ingredients: [
-        'Basmati Rice',
-        'Lamb meat',
-        'Veggies',
-        'Garlic',
-        'Spicy Sauce'
+        "Basmati Rice",
+        "Lamb meat",
+        "Veggies",
+        "Garlic",
+        "Spicy Sauce",
       ],
-      cuisine: 'Indian',
-      dishType: 'main_course',
-      image: 'https://mejoresrecetas.me/pollo-biryani/',
+      cuisine: "Indian",
+      dishType: "main_course",
+      image: "https://mejoresrecetas.me/pollo-biryani/",
       duration: 60,
-      creator: 'Mughals',
-      created: "1600's"
-    })
+      creator: "Mughals",
+      created: "1600's", 
+    });
     console.log(newRecipe);
     return Recipe.create(newRecipe);
   })
   .then(() => {
-    return Recipe.insertMany(data)
+    return Recipe.insertMany(data);
   })
   .then(() => {
     return Recipe.findOneAndUpdate({
       title: "Rigatoni alla Genovese",
-      duration: 100
-    })
-    .then (() => {
-      console.log(`Success!`)
-    })
+      duration: 100,
+    }).then(() => {
+      console.log(`Success!`);
+    });
   })
   .then(() => {
-    return Recipe.deleteOne({ 
-      title: 'Carrot Cake',
-    })
+    return Recipe.deleteOne({
+      title: "Carrot Cake",
+    });
   })
   .then(() => {
-    return mongoose.connection.close()
+    return mongoose.connection.close();
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  }); */
+
+
+
+const myRecipe = {
+  title: "Biryani",
+  level: "Amateur Chef",
+  ingredients: [
+    "Basmati Rice",
+    "Lamb meat",
+    "Veggies",
+    "Garlic",
+    "Spicy Sauce",
+  ],
+  cuisine: "Indian",
+  dishType: "main_course",
+  image: "https://mejoresrecetas.me/pollo-biryani/",
+  duration: 60,
+  creator: "Mughals",
+  created: "1600's",
+};
+
+async function recipes() {
+  // Connecting to DB
+  try {
+  await mongoose.connect(MONGODB_URI);
+
+  //Resetting the DB
+  await Recipe.deleteMany()
+
+  // Inserting 1 recipe
+  let singleRecipe = await Recipe.create(myRecipe);
+  console.log(singleRecipe.title);
+
+  // Inserting multiple recipes
+  let multipleRecipes = await Recipe.insertMany(data)
+  multipleRecipes.forEach((recipe) => console.log(recipe.title));
+
+  // Update one recipe
+  await Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  console.log('Duration updated');
+
+  // Delete recipe
+  await Recipe.deleteOne({title: 'Carrot Cake'})
+  console.log('Cake deleted');
+
+  // mongoose.connection.close();
+  mongoose.disconnect();
+
+} catch(err) {
+  console.log(err)
+}};
+
+recipes();
+

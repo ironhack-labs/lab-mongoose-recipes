76ba3ba73093ae501d3ff24e1e9ea03b9c3354db
Tony5300 <tony_tolen@ziggo.nl>
2022-08-01 16:50:25 +0200

    made some changes to the recipe create insid of the index.js. Added the insertMany method to it. Also removed the brackets of a array inside of the index.js in the recipe.create.

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      // create a blueprint of the recipe model
+      title: "First recipe of Hamburger",
+      level: "Easy Peasy",
+      ingredients: [
+        "Meat",
+        "Tomato",
+        "Lettuce",
+        "Pickels",
+        "Onion",
+        "Sauce",
+        "Buns",
+      ],
+      cuisine: "Western Cuisine",
+      dishType: "snack",
+      image: "imgValue",
+      duration: 10,
+      creator: "Someone",
+      created: new Date(),
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then((createdRecipe) => {
+    console.log("createdRecipe:", createdRecipe);
+    return Recipe.insertMany(data); // data is the variable where we store the json file.
+    //TODO: Whenever Recipe.insertMan(data) is finished it returns 1 value. That 1 value is getting passed into the .then promise.  Because it get passed into the .then it get passed into the newlyAddedRecipe callback function.
+  })
+  .then((newlyAddedRecipes) => {
+    console.log("New added Recipes:", newlyAddedRecipes);
+    console.log("About to get a list of all the recipes");
+    return Recipe.find({});
+  })
+  .then((listOfAllRecipes) => {
+    console.log("List of all Recipes:", listOfAllRecipes);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  }); // general error message when one of the promises fail
+
+// Promises can either Resolved or reject.
+// When they resolve, if defined, they call the `.then` method
+// When they Reject, if defined they call the `.catch` method
+// If the connection is failed it immediatly goes to the .catch method
+// If the connection succeeded it will go to the next .then method
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+const recipeSchema = new Schema(
+  {
+    // TODO: write the schema
+    title: {
+      type: String,
+      unique: true,
+    },
+    level: {
+      type: String,
+      enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+    },
+    ingredients: [String],
+    cuisine: {
+      type: String,
+      required: true,
+    },
+    dishType: {
+      type: String,
+      enum: [
+        "breakfast",
+        "main_course",
+        "soup",
+        "snack",
+        "drink",
+        "dessert",
+        "other",
+      ],
+    },
+    image: {
+      type: String,
+      default: "https://images.media-allrecipes.com/images/75131.jpg",
+    },
+    duration: Number,
+    creator: String,
+    created: Date,
+  },
+  { timestamps: true }
+);
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

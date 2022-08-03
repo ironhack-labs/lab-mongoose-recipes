321019f13f468c9b4a999a53f5fec6a161773354
Tony5300 <tony_tolen@ziggo.nl>
2022-08-03 17:14:40 +0200

    done

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // TODO Iteration 2:
     return Recipe.create({
       // create a blueprint of the recipe model
       title: "First recipe of Hamburger",
@@ -38,9 +39,10 @@ mongoose
       created: new Date(),
     });
   })
+  //TODO Iteration 3:
   .then((createdRecipe) => {
     console.log("createdRecipe:", createdRecipe);
-    return Recipe.insertMany(data); // data is the variable where we store the json file.
+    return Recipe.insertMany(data); // data is the variable where we store the json file and get all the files from the json file.
     //TODO: Whenever Recipe.insertMan(data) is finished it returns 1 value. That 1 value is getting passed into the .then promise.  Because it get passed into the .then it get passed into the newlyAddedRecipe callback function.
   })
   .then((newlyAddedRecipes) => {
@@ -50,6 +52,26 @@ mongoose
   })
   .then((listOfAllRecipes) => {
     console.log("List of all Recipes:", listOfAllRecipes);
+  })
+  //TODO: Iteration 4:
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" }, // find a recipe by the title
+      { duration: 100 } // change the duration of the recipe
+    ).then((updatedRecipe) => {
+      console.log("Updated the:", updatedRecipe.title, "recipe");
+    }); // after changing the recipe  and the promise is succeded than log the change to the console
+  })
+  //TODO: Iteration 5:
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  }) // look through the Recipe schema and search for a recipe with a specific title and remove the entire recipe
+  .then((removedRecipe) => {
+    console.log("Removed the:", removedRecipe.title, "recipe");
+  }) // then if the promise is succeeded than log it to the console.
+  //TODO: Iteration 6:
+  .then(() => {
+    mongoose.connection.close(); // close the connection after all the promises are succeeded
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);

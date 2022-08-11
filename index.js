27250ae6961a752fb1db5537e682b6549aee77dc
Clark68500 <mcevoyclark@gmail.com>
2022-08-11 11:51:41 +0200

    solve lab

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+
+const recipeObj = {
+  title: "Spaghettie bolognese",
+  level: "Easy Peasy",
+  ingredients: ["Pasta", "Tomatoe sauce", "Meat"],
+  cuisine: "Modern",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/images/75131.jpg",
+  duration: 120,
+  creator: "Clark",
+};
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+const saveNewRecipe = () => {
+  const newRecipe = new Recipe(recipeObj);
+
+  newRecipe
+    .save()
+    .then((results) => console.log(`Saved new recipe: ${results}`))
+    .catch((saveErr) => console.error(`Save failed: ${saveErr}`));
+};
+
+// saveNewRecipe();
+
+const saveManyNewRecipes = () => {
+  Recipe.insertMany(data)
+    .then((results) => console.log(`Saved new recipes: ${results}`))
+    .catch((saveErr) => console.error(`Save failed: ${saveErr}`));
+};
+
+// saveManyNewRecipes();
+
+const updateRecipe = () => {
+  Recipe.findOneAndUpdate(
+    { title: "Rigatoni alla Genovese" },
+    { duration: 100 }
+  )
+    .then((results) => console.log("Recipe is updated"))
+    .catch((saveErr) => console.error(`Error`));
+};
+
+// updateRecipe();
+
+const removeRecipe = () => {
+  Recipe.findOneAndRemove({ title: "Carrot Cake" })
+    .then((results) => console.log("Recipe is removed"))
+    .catch((saveErr) => console.error(`Error`));
+};
+
+// removeRecipe();

500189d281904de4e8ab24eb6c5c63f5b41eaa91
eurico.moutinho <yungsniper@hotmail.com>
2022-08-11 13:26:54 +0100

    completed

--- index.js
+++ index.js
@@ -13,11 +13,43 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
+    
     return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Renovese",
+    level: "Easy Peasy",
+    ingredients: [
+      "2 pounds e",
+      "2 (toni",
+      "1 tarjoram leaves",
+      "1 piepper",
+      "2 tableano cheese"
+    ],
+    cuisine: "Itan",
+    dishType: "main_course",
+    image: "https://images.media-allrecipes.com/userphotos/720x405/3489951.jpg",
+    duration: 220,
+    creator: "Chef"
+    })
+    .then(recipe => {
+      console.log(recipe.title);
+    })
   })
+  .then(
+    ()=>Recipe.insertMany(data).then((recipe) => {
+    for(const val of recipe) {
+      console.log(val.title)
+  }
+  })).then(
+    ()=>Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'},{duration:100}
+    ))
+    .then(()=>Recipe.deleteOne({title: 'Carrot Cake'}
+    )).then(()=>mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  

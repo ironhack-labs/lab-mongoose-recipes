13ef6855ca4f62e2357f87ea6d6ad0808a37c677
Daniel Gonzalez <danigonzalezsierra@gmail.com>
2022-08-31 20:34:31 +0200

    solved lab

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+console.log(data)
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -15,9 +16,45 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+   return Recipe.insertMany(data)
+    }) 
+
+  .then((recetas) => recetas.forEach((el)=> console.log(el.title) ))
+
+  .then(() => {
+    const receta = new Recipe({
+     title: 'peas with ham',
+    level: 'Amateur Chef',
+    ingredients: [ 'peas', 'egg', 'ham', 'water', 'potatoes', 'salt' ],
+    cuisine: 'mediterranean cuisine',
+    dishType: 'main_course',
+    image: 'https://www.google.com/search?q=guisantes+con+jamon&rlz=1C1CHBF_esES868ES868&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiNm5C9tvH5AhVMaBoKHSC3DnYQ_AUoAnoECAIQBA&biw=1920&bih=872&dpr=1',
+    duration: 30,
+    creator: 'Cheef Daniel Gonzalez' 
   })
+    return Recipe.create(receta)
+  })
+
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
+  })
+
+  .then((receta) => console.log(receta))
+  
+  .then(()=>{
+    return Recipe.deleteOne({title:"Carrot Cake" })
+  })
+
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.error('Error connecting to the database', error)
+  })
+
+  .finally(()=>{
+    mongoose.disconnect()
+  })
+
+  
+
+  

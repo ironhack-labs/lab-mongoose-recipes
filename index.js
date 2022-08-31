ffe32e324d4cc4336b79ad5ff325a87b67bf6784
Lenis Cervantes <leniserik@gmail.com>
2022-08-31 20:36:08 +0200

    All completed

--- index.js
+++ index.js
@@ -17,7 +17,37 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const recipe1 = new Recipe({
+      title: "Pollo al horno",
+      level: 'Easy Peasy',
+      ingredients: ['pollo', 'horno'],
+      cuisine: 'hola',
+      dishType: 'main_course',
+      duration: 150,
+      creator: 'Pepe',
+    });
+    return recipe1.save()
   })
+
+  .then((recipe) => {
+    console.log(recipe);
+    return Recipe.insertMany(data)
+  })
+
+  .then((recipe) => {
+    recipe.forEach((el) => console.log(el.title))
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 },{ new: true })
+  })
+  .then((changed) => {console.log('Changed -> ', changed)
+  return Recipe.deleteOne({title: 'Carrot Cake'})
+})
+
+  
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+  .finally(() => mongoose.disconnect())
+
+

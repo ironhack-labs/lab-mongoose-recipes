0099835b418678773169b466904daa2176952bc5
RedValerian <r.pino.camacho@gmail.com>
2022-08-31 17:58:36 +0200

    Finished

--- index.js
+++ index.js
@@ -7,16 +7,29 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // console.log(data);
+    // recipe.save().then((r) => console.log(r));
+    return Recipe.insertMany(data);
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 999 }, { new: true }).then(r => console.log(r));
+  })
+  .then(() => {
+    return Recipe.findOneAndDelete({ title: "Carrot cake" });
+  })
+  .then(() => {
+    mongoose.connection.close();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: 'String', required: true, unique: true },
+  level: { type: 'String', enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
+  ingredients: [{ type: 'String' }],
+  cuisine: { type: 'String' },
+  dishType: { type: 'String' },
+  image: {
+    type: 'String', default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: { type: 'number', min: 0 },
+  creator: { type: 'String' },
+  created: { type: 'Date' }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+

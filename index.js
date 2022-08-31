d7f5522f7261cb7adc5a453c993e31b722c89cd0
Marta Morán <martamoranl06@gmail.com>
2022-08-31 18:48:29 +0200

    solved lab

--- index.js
+++ index.js
@@ -10,14 +10,55 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+  .then(mongooseConnect => {
+    console.log(`Connected to the database: "${mongooseConnect.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    //  Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+  })
+  .then(() => {
+    // const deleted = Recipe.deleteOne({ title: "Rigatoni alla Genovese" })
+    // console.log("borrado")
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+  })
+  .then((borrado) => {
+    console.log(borrado)
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    //   console.error('Error connecting to the database', error);
   });
+
+
+mongoose.disconnect();
+
+
+/*
+data.forEach(element => {
+
+  const newDish = new Recipe({
+    title: element.title,
+    level: element.level,
+    ingredients: [
+      element.ingredients
+    ],
+    cuisine: element.cuisine,
+    dishType: element.dishType,
+    image: element.image,
+    duration: element.duration,
+    creator: element.creator
+  })
+
+  newDish
+    .save()
+    .then((element) => {
+      console.log(element);
+    })
+    .catch((err) => console.log(err));
+
+}); */
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String },
+  level: { type: String },
+  ingredientes: { type: [Array] },
+  cuisine: { type: String },
+  dishType: { type: String },
+  image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
+  duration: { type: Number },
+  creator: { type: String },
+  created: { type: Date }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

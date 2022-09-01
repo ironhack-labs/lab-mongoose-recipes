c14f79fdd873c81d2c15afca46a56d3625942151
juca25 <jserranocucarella@gmail.com>
2022-09-01 09:22:51 +0200

    almost done

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
+
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
+
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
-// Connection to the database "recipe-app"
+const newRecipe = {
+      title : "Pancakes",
+      level : "Easy Peasy",
+      ingredients : ["1 huevo","120g harina", "200 ml leche", "30ml aceite", "20g azucar","15g levadura"],
+      cuisine : "Americanish",
+      dishType : "breakfast",
+      image : "https://recetasamericanas.com/wp-content/uploads/2012/01/5076901336_3995a30359_z.jpg",
+      duration : 20,
+      creator : "JC"
+    }
+
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create(newRecipe)
+  })
+  .then (()=> {
+    return Recipe.insertMany(data)  
+  } ) 
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: "Pancakes"},{duration : 30},{new : true})
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+  
+  

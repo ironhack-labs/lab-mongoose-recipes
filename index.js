53b939fc7886d56e449f5b7951aba0087f4f5906
Daniela Romero <danyrp45@gmail.com>
2022-09-03 13:46:43 -0500

    done

--- index.js
+++ index.js
@@ -17,7 +17,33 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+   
+    /*  Recipe.create(data[3])
+    .then(recipe => console.log(recipe.title))
+    .catch((err) => console.log(err)) */;
+
+    Recipe.insertMany(data)
+      .then(recipes =>{
+        recipes.forEach(recipe => {console.log(recipe.title);});
+
+        Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration:100})
+          .then(recipe => console.log("we change the duration"))
+          .catch((err) => console.log(err));
+    
+        Recipe.deleteOne({title:"Carrot Cake"})
+          .then(recipe => console.log("we delete the recipe"))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err)); 
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});

2e2b1e0bef379fa69121856f0cf68c5be8fa8346
Jpta24 <jeanpierretorres@gmail.com>
2022-09-05 11:10:42 +0200

    Lab Mongoose Done

--- index.js
+++ index.js
@@ -9,15 +9,43 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+	.connect(MONGODB_URI)
+	.then((x) => {
+		console.log(`Connected to the database: "${x.connection.name}"`);
+		// Before adding any recipes to the database, let's remove all existing ones
+		return Recipe.deleteMany();
+	})
+	.then(() => {
+		// Run your code here, after you have insured that the connection was made
+	})
+	.catch((error) => {
+		console.error('Error connecting to the database', error);
+	});
+
+Recipe.insertMany(data)
+	.then((data) => {
+		data.map((rec) => {
+			console.log(rec.title);
+		});
+	})
+	.catch((err) => console.log(err));
+
+Recipe.find()
+	.then((data) => {
+		data.map((rec) => {
+			console.log(rec.title);
+		});
+	})
+	.catch((err) => console.log(err));
+
+Recipe.findOneAndUpdate(
+	{ title: 'Rigatoni alla Genovese' },
+	{ duration: 100 },
+	{ new: true }
+)
+	.then((updated) => console.log(updated))
+	.catch((err) => console.log(err));
+
+Recipe.deleteOne({ title: 'Carrot Cake' }).then(() =>
+	console.log('succesfully deleted')
+);

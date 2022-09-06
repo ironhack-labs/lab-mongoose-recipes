25c21af0d65a69670091adbd3f63447cdba808d0
Jose Rios <jrbernabe@gmail.com>
2022-09-05 21:59:23 -0400

    Lab Finished

--- index.js
+++ index.js
@@ -17,6 +17,40 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // Iteration 2 - Create a recipe
+    // Recipe
+    //   .create(data[0])
+    //   .then(console.log(data[0].title))
+    //   .catch((error) => console.log(error));
+    
+    // Iteration 3 - Insert multiple recipes
+    Recipe
+      .insertMany(data)
+      .then((recipes) => {
+        recipes.forEach((recipe) => console.log(recipe.title));
+      })
+      .then(() => {
+        // Iteration 4 - Update recipe
+        Recipe
+          .findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100})
+          .then(console.log('Update Success'))
+          .catch((error) => console.log(error));
+      })
+      .then(() => {
+        // Iteration 5 - Remove a recipe
+        Recipe
+          .deleteOne({title: 'Carrot Cake'})
+          .then(console.log('Remove Success'))
+          .catch((error) => console.log(error));
+      })
+      .then(() => {
+        // Iteration 6
+        mongoose.connection.close(() => {
+          console.log('Mongose connection closed');
+          process.exit(0);
+        })
+      })
+      .catch((error) => console.log(error));
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

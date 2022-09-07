9ac324cf4031d0fc7d73a59ec99e5dac7f629d59
lili-cadena <anlica_99@hotmail.com>
2022-09-07 16:07:29 -0500

    completed lab

--- index.js
+++ index.js
@@ -15,9 +15,42 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  .then(async() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    
+    //Iteration 2
+
+    /*const recipe = new Recipe({
+      title: "Sandwich",
+      level: "Easy Peasy",
+      ingredients: ["Bread", "Cheese", "Ham"],
+      cuisine: "American",
+      dishType: "snack",
+      duration: 10,
+      creator: "Unknown"
+      })
+    await recipe.save()
+    })*/
+
+    //Iteration 3
+
+    /*Recipe.insertMany(data)
+      .then(() => console.log("Data inserted"))
+      .catch(err => console.error(err))*/
+
+    //Iteration 4
+
+    /*Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, { $set: { duration: 100 } }, { new: true })
+      .then(recipe => console.log(recipe))
+      catch(error => console.error(error))*/
+
+    //Iteration 5
+    
+    Recipe.deleteOne({ title: 'Carrot Cake' })
+      .then(recipe => console.log(recipe))
+      .catch(error => console.error(error))
+    })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

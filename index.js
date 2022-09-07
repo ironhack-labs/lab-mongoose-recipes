8d183259283f81420551a9efe42a055b9e68ae0b
Calvins Soonen Christopher <calvinskris@gmail.com>
2022-09-07 14:38:23 +0300

    recipes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
+
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = process.env.MONGODB_URI
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +18,39 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
+
   .then(() => {
+  Recipe.create({
+    title:'jollofRice',
+    cuisine: 'african',
+    ingredients: 'rice,tomatoes,maggi,salt,pepper',
+    dishType: 'main_course',  
+   })
+  .then(createdRecipe => console.log(createdRecipe))
+
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  
+  // Iteration 3- insert multiple recipes.  
+  
+  Recipe.insertMany(data)
+  .then(insertedData => console.log(insertedData))
+  
+  //Iteration 4- update duration field for Rigatoni alla Genovese
+  
+  // Recipe.findOneAndUpdate({duration:'220'}, {duration:'100'}, {new: true})
+  // .then(updatedRecipe => console.log(updatedRecipe))
+  // .catch(err => console.log(err))
+
+// Iteration 5- remove receipe carrot cake.
+
+  // Recipe.deleteOne({title: 'Carrot Cake'})
+  // .then(deletedRecipe => console.log(deletedRecipe))
+  
+  

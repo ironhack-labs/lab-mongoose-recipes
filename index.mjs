8ae935b797e53a38cb9391cd9427cb143512b851
hvkst <bernd@hvkst.de>
2022-09-08 21:09:50 +0200

    Iteration I

--- index.mjs
+++ index.mjs
@@ -1,23 +1,31 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+// const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+// const Recipe = require('./models/Recipe.model');
+import Recipe from 'Recipe.model';
 // Import of the data from './data.json'
-const data = require('./data');
-
+// const data = require('./data');
+import data from 'data.json';
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+await mongoose.connect('mongodb://localhost:27017/pokemons-v3');
+await mongoose.connection.db.dropDatabase();
+await mongoose.connection.close();
+
+await mongoose.connect('mongodb://localhost:27017/pokemons-v3');
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });

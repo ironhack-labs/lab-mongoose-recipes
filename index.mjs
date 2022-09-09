c2a0d4dfd8291e98a3e95da72cac8e2b85abafb5
JoaoSerafim3001 <seafim.joao@gmail.com>
2022-09-09 18:44:06 +0100

    Iterations 1 and 2 done

---
+++ index.mjs
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import Recipe from "./models/Recipe.model.js";
+// import data from "./data";
+// const mongoose = require("mongoose");
+
+// Import of the model Recipe from './models/Recipe.model.js'
+// const Recipe = require("./models/Recipe.model");
+// Import of the data from './data.json'
+// const data = require("./data");
+
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+
+await mongoose.connect(MONGODB_URI);
+await mongoose.connection.db.dropDatabase();
+await mongoose.connection.close();
+
+await mongoose.connect(MONGODB_URI);
+
+// Connection to the database "recipe-app"
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then((x) => {
+//     console.log(`Connected to the database: "${x.connection.name}"`);
+//     // Before adding any recipes to the database, let's remove all existing ones
+//     return Recipe.deleteMany();
+//   })
+//   .then(() => {
+//     // Run your code here, after you have insured that the connection was made
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to the database", error);
+//   });
+
+// const paella = new Recipe({
+//   title: "Asian Glazed Chicken Thighs",
+//   level: "Amateur Chef",
+//   ingredients: [
+//     "1/2 cup rice vinegar",
+//     "5 tablespoons honey",
+//     "1/3 cup soy sauce (such as Silver Swan®)",
+//     "1/4 cup Asian (toasted) sesame oil",
+//     "3 tablespoons Asian chili garlic sauce",
+//     "3 tablespoons minced garlic",
+//     "salt to taste",
+//     "8 skinless, boneless chicken thighs",
+//   ],
+//   cuisine: "Asian",
+//   dishType: "main_course",
+//   image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+//   duration: 40,
+//   creator: "Chef LePapu",
+// });
+
+// await paella.save();
+// console.log(paella.title);
+
+await mongoose.connection.close();

23e3625d0a897b94662b2eb43020476d9a420185
Chris Ziomek <cziomek1989@gmail.com>
2022-09-09 16:39:55 -0400

    Completed lab

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { deleteOne } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +17,56 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+
+
+  .then(savedRecipe => {
+    console.log('we printed out new recipes', savedRecipe)
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Asian Glazed Chicken Breast",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 chicken breast"
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu"
+  })
+  // 
+
+  })
+  .then(savedRecipe => {
+    console.log(savedRecipe)
+    return Recipe.insertMany(data)
+  })
+  .then(savedRecipeArray => {
+    console.log(savedRecipeArray)
+    return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {
+      duration: 100,
+    })
+  })
+  .then(deleteOne => {
+    console.log(deleteOne)
+    return Recipe.deleteOne({title: 'Carrot Cake'})
+  })
+
+  .then(updatedRecipe => {
+    console.log('updated')
+    mongoose
+    .connection.close(MONGODB_URI)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  
+  

243f279d7051ef06369dd921e379ad1f4a0e9a75
Ryan Essel <spucti19@gmail.com>
2022-09-10 09:16:53 -0400

    can get the iterations to work seperatly but can't get them to run all at once. not understanding this one

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
+
+
+
+
+
+
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
@@ -21,3 +27,117 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+  // Iteration 2 - Create a recipe
+
+  // Recipe.create({title: "Mcdoanlds", cuisine: "Fast Food", creator: "usa"}).then(createdRecipe => {
+  //   console.log("new recipe mde  ", createdRecipe);
+  //   }).then(()=> {
+  //       console.log("yo it disconnected")
+  //       mongoose.disconnect();
+  //   }).catch(err => {
+  //       console.log("yo it disconnected")
+  //       mongoose.disconnect();
+  //       throw err;
+    
+  //   })
+
+  // Iteration 3 - Insert multiple recipes
+  //   Recipe.insertMany(data)
+  //   .then(()=> {
+  //  Recipe.find({}, "title", (err, recipies) =>{
+  //       if (err) {
+  //         console.log(`Err occured getting recipies from db: ${err}`);
+  //         return
+  //     }
+  //     console.log(`got titles`);
+  //     recipies.forEach(recipie => console.log(`------> title: ${recipie.title}`))
+      
+  //     })
+
+
+
+  //   })
+
+
+  // .then(()=> {
+    
+    
+  //       console.log("yo it disconnected")
+  //       mongoose.disconnect();
+  //   }).catch(err => {
+  //       console.log("yo it disconnected")
+  //       mongoose.disconnect();
+  //       throw err;
+    
+  //   })
+
+
+
+// Iteration 4 - Update recipe
+
+
+// Recipe.findByIdAndUpdate(`631b8a716db7cb59ff6d10e3`, {duration: 100}, {new: true}).then(revisedItem => {//{new: true} is needed to see the updated data
+//     console.log(revisedItem);
+//     console.log("Success UPDATED!!!")
+
+//     mongoose.disconnect();
+// }).catch(err => {
+//     mongoose.disconnect();
+//     throw err;
+// })
+
+
+// Iteration 5 - Remove a recipe
+
+// Recipe.findByIdAndRemove("631b8a716db7cb59ff6d10e2").then(() => {
+//     console.log("You have deleted a the recipe carrot cake")
+//     mongoose.disconnect();
+// }).catch(err => {
+//     mongoose.disconnect();
+//     throw err;
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // db.recipe.find({}, {title: 1, _id: 0})
+
+  // console.log(createdRecipes)
+
+
+
+//   .then(createdRecipes => {
+
+//     for (let i = 0; i < data.length; i++) {
+      
+//   console.log("title: ", createdRecipes[i].title);
+// }
+
+
+
+
+
+// DISCONNECT ORDER ISSUE IS REVISED BUT ONLY SHOWS THE ONES THAT WERE INPUTTED
+      // .then(recipies => {
+
+      //   recipies.forEach(recipie => console.log(`------> title: ${recipie.title}`))
+      // })
+  
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,49 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+// Iteration 1 - Recipe Schema
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title:{
+    type: String,
+    unique: true,
+    require: true
+  },
+  level:  {
+    type: String,
+    default: "Easy Peasy",
+    enum: ["Easy Peasy", "Amateur Chef","UltraPro Chef"]
+  
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    type: String,
+    require: true
+  },
+  dishType:{
+    type: String,
+    default: "breakfast",
+    enum: [`breakfast`, `main_course`, `soup`, `snack`, `drink`, `dessert`, `other`]
+  
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+
+  },
+  duration: {
+    type:Number,
+    min: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

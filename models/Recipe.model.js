f6aaa5fffb5667c2100a0c0c43c4de3f24dceeb7
Juan Cordero <jcord053@fiu.edu>
2022-09-13 09:10:45 -0400

    Completed lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,15 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: { type: String },
+  level: { type: String },
+  ingredients: [ { type: String } ],
+  cuisine: { type: String },
+  dishType: { type: String },
+  image: { type: String },
+  duration: { type: Number },
+  creator: { type: String },
+  created: { type: String }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

293ba08a2984564586521ad83f438e2d62cc14a9
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-09-27 16:05:26 +0100

    adds async await starter

--- index.js
+++ index.js
@@ -7,17 +7,38 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
-mongoose
+//Method 1 : Using Async Await
+
+const manageRecipes = async () => {
+  try {
+    // Connection to the database "recipe-app"
+    const dbConnection = await mongoose.connect(MONGODB_URI);
+    console.log(`Connected to the database: "${dbConnection.connection.name}"`);
+
+    // Before adding any recipes to the database, let's remove all existing ones
+    await Recipe.deleteMany();
+
+    // Run your code here, after you have insured that the connection was made
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+manageRecipes();
+
+//Method 2: Using .then() method
+//If you want to use this method uncomment the code below:
+
+/* mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
   })
-  .catch(error => {
+  .catch((error) => {
     console.error('Error connecting to the database', error);
-  });
+  }); */

afe1bebbbbf2c42428008c498df62956bfa4b49b
HaridhaRavi <haridha.gr@gmail.com>
2022-09-29 19:35:29 +0200

    lab done

--- index.js
+++ index.js
@@ -17,6 +17,51 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    console.log("Data created with old connections are deleted!");
+    const dataRecipe = {
+      title: "French Toast",
+      level: "Easy Peasy",
+      ingredients:["8 slices of bread (brioche or challah works well)",
+      " 2 eggs",
+      "1 cup heavy cream",
+      "1 teaspoon vanilla",
+      "1 teaspoon cinnamon",
+      "1 tablespoon maple syrup, plus more for serving",
+      "1 cup fresh blueberries",
+      "1 tablespoon lemon juice",
+      "2 tablespoon sugar",
+      "vanilla ice cream"],
+      cuisine:"French",
+      dishType:"breakfast",
+      image:"https://data.thefeedfeed.com/recommended/15279499355b12aa6f35a82.jpg",
+      duration:10,
+      creator:"Chef Haridha and Joao"
+
+    }
+    return Recipe.create(dataRecipe);
+  })
+  .then((RecipeFromDb)=>{
+    console.log(RecipeFromDb.title);
+    return Recipe.insertMany(data);
+  })
+  .then((dataArray)=>{
+    dataArray.forEach(element =>{
+      console.log(element.title);
+    });
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"},{duration: 100}, {returnDocument: 'after' });
+  })
+  .then((updatedData)=>{
+    console.log(`Duration for ${updatedData.title} is updated successfully`);
+    return Recipe.find({title: "Carrot Cake"});
+  })
+  .then((dataToBeDeleted) => {
+    console.log(dataToBeDeleted[0].title);
+    return Recipe.deleteOne(dataToBeDeleted[0])
+  })
+  .then(()=>{
+    console.log("Data Deleted Successfully!");
+  }).then(()=>{
+    mongoose.connection.close();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

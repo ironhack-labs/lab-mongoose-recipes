dcd5eccda688e1fd32b15e693ac3c1174f2ad275
Saskia Peach <saskiapeach@gmail.com>
2022-09-29 17:28:23 +0100

    iteration 3 complete

--- index.js
+++ index.js
@@ -16,30 +16,36 @@ mongoose
     return Recipe.deleteMany();
   })
   .then(() => {
-    const recipeOne = {
-      title: "Asian Glazed Chicken Thighs",
-      level: "Amateur Chef",
-      ingredients: [
-        "1/2 cup rice vinegar",
-        "5 tablespoons honey",
-        "1/3 cup soy sauce (such as Silver Swan®)",
-        "1/4 cup Asian (toasted) sesame oil",
-        "3 tablespoons Asian chili garlic sauce",
-        "3 tablespoons minced garlic",
-        "salt to taste",
-        "8 skinless, boneless chicken thighs",
-      ],
-      cuisine: "Asian",
-      dishType: "main_course",
-      image:
-        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-      duration: 40,
-      creator: "Chef LePapu",
-      created: "2021-09-29T17:30:00.000+00:00",
-    };
-    return Recipe.create(recipeOne);
+    //iteration 2
+    // const recipeOne = {
+    //   title: "Asian Glazed Chicken Thighs",
+    //   level: "Amateur Chef",
+    //   ingredients: [
+    //     "1/2 cup rice vinegar",
+    //     "5 tablespoons honey",
+    //     "1/3 cup soy sauce (such as Silver Swan®)",
+    //     "1/4 cup Asian (toasted) sesame oil",
+    //     "3 tablespoons Asian chili garlic sauce",
+    //     "3 tablespoons minced garlic",
+    //     "salt to taste",
+    //     "8 skinless, boneless chicken thighs",
+    //   ],
+    //   cuisine: "Asian",
+    //   dishType: "main_course",
+    //   image:
+    //     "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //   duration: 40,
+    //   creator: "Chef LePapu",
+    //   created: "2021-09-29T17:30:00.000+00:00",
+    // };
+    // return Recipe.create(recipeOne);
+
+    return Recipe.insertMany(data);
   })
-  .then((recipefromDB) => console.log(recipefromDB.title))
+  .then((recipesfromDB) => {
+    console.log(recipesfromDB);
+    recipesfromDB.forEach((recipe) => console.log(recipe.title));
+  })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });

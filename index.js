bbedb2ce3ef9e63060143056bee66e3e72d35ddd
Walison Abram <abramwhas@gmail.com>
2022-10-08 18:28:38 -0300

    iteration 2 and 3 done

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+const myRecipe = {
+  title: "Cheese Salad",
+    level: "Amateur Chef",
+    ingredients: [
+      "1 hamburger",
+      "2 slices of cheese",
+      "2 slices of tomato",
+      "two lettuce leaves",
+    ],
+    cuisine: "American",
+    dishType: "main_course",
+    image: "https://sachefmio.blob.core.windows.net/fotos/cheese-salad-93590.jpg",
+    duration: 10,
+    creator: "Chef Barbabram"
+}
+
 const MONGODB_URI = 'mongodb+srv://abramwhas:555200@cluster0.uooc2pi.mongodb.net/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -16,7 +32,11 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    Recipe.create(data)
+    Recipe.create(myRecipe)
+    console.log(myRecipe.title)
+  })
+  .then(() => {
+    Recipe.insertMany(data)
     for(let i = 0; i < data.length; i++) {
       console.log(data[i].title)
     }

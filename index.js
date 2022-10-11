29c0d39497a90ad7ff606445d01065d10e6b27ea
Hugo Lomba <hmlomba@gmail.com>
2022-10-11 16:25:49 -0300

    iteration 5

--- index.js
+++ index.js
@@ -13,27 +13,35 @@ mongoose
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
+    // return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    // ITERATION 2
     // Recipe.create(data[0])
     //   .then((recipe) => console.log("The recipe save is: ", recipe.title))
     //   .catch((error) => console.log("error: ", error));
-    Recipe.create(data)
-      .then((recipe) => recipe.map((r) => console.log(r.title)))
 
-      // iteration 4 update
-      .then(() => {
-        Recipe.findOneAndUpdate(
-          { title: "Rigatoni alla Genovese" },
-          { duration: 100 }
-        )
-          .catch((error) => console.log(error))
-          .then(() => console.log("updated"));
-      })
-      .catch((error) => console.log("error: ", error));
-    // .then(() => {Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 } })
+    // ITERATION 3
+    // Recipe.create(data)
+    //   .then((recipe) => recipe.map((r) => console.log(r.title)))
+
+    // // ITERATION 4
+    //   .then(() => {
+    //     Recipe.findOneAndUpdate(
+    //       { title: "Rigatoni alla Genovese" },
+    //       { duration: 100 }
+    //     )
+    //       .catch((error) => console.log(error))
+    //       .then(() => console.log("updated"));
+    //   })
+    //   .catch((error) => console.log("error: ", error));
+
+    // ITERATION 5
+    Recipe.deleteOne({ title: "Carrot Cake" })
+      .then(() => console.log("Carrot Cake removed"))
+      .catch((error) => console.log(error));
   })
 
   .catch((error) => {

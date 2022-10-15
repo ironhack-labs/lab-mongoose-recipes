41baafb90309deeca65310c7a4211acd6a54dbc9
Joao Paulo <joaopaulo.bco@gmail.com>
2022-10-15 14:02:23 -0300

    index and models

--- index.js
+++ index.js
@@ -16,8 +16,27 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create(data[0])
+      .then((recipe) => console.log("The recipe save is: ", recipe.title))
+      .catch((error) => console.log("error: ", error));
+
+      Recipe.create(data)
+      .then((recipe) => recipe.map((r) => console.log(r.title)))
+
+      .then(() => {
+            Recipe.findOneAndUpdate(
+              { title: "Rigatoni alla Genovese" },
+              { duration: 100 }
+            )
+              .catch((error) => console.log(error))
+              .then(() => console.log("updated"));
+          })
+          .catch((error) => console.log("error: ", error));
+
+          
+    
+
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+ 
+
+  

0467e72c33c4b4946f6ee8c93862c0bb2bc99a0f
Shania <shaniaantezana@gmail.com>
2022-10-28 09:38:40 -0400

    iteration 3 done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import Recipe from './models/Recipe.model.js'
 //const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-//import data from './data.json'
+import data from './data.json' assert {type:"json"}
 //const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
@@ -19,16 +19,17 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-   /*Recipe.create({
-    title: "Bombom de travessa",
-    level:"Easy Peasy",
-    ingredients:["250 g de chocolate ao leite","250 g de chocolate meio amargo","2 latas de leite condensado","2 latas de creme de leite","2 colheres de margarina","2 caixas de morango"],
-    cuisine:"cuisine",
-    dishType:"dessert",
-    duration:35,
-    creator:"Shania"
-   })
-   console.log(title)*/
+    const newRecipe = {
+      title: "Bombom de travessa",
+      level:"Easy Peasy",
+      ingredients:["250 g de chocolate ao leite","250 g de chocolate meio amargo","2 latas de leite condensado","2 latas de creme de leite","2 colheres de margarina","2 caixas de morango"],
+      cuisine:"cuisine",
+      dishType:"dessert",
+      duration:35,
+      creator:"Shania"
+     }
+   Recipe.create(newRecipe,()=> console.log(newRecipe.title))
+   Recipe.insertMany(data,()=> data.forEach((recipe)=> console.log(recipe.title)))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

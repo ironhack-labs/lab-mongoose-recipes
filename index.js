1e0b33cf092da31cb3293b975ad3237b17feec5e
Shania <shaniaantezana@gmail.com>
2022-10-29 07:13:12 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 import mongoose from "mongoose";
-//const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
 import Recipe from "./models/Recipe.model.js";
-//const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 import data from "./data.json" assert { type: "json" };
-//const data = require('./data');
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
@@ -47,7 +41,10 @@ mongoose
     Recipe.deleteOne({ title: "Carrot Cake" }, () =>
       console.log("Data deleted sucessfully")
     );
-    mongoose.connection.close(() => console.log("Closed database"));
+    mongoose.connection.close(() => {
+      console.log("Closed database");
+      process.exit(0);
+    });
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);

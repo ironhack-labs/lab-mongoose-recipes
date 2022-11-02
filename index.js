941ab61106bfc7579b3b3f9482541c59fbce8f3d
cursedxp <anilozsoy@gmail.com>
2022-11-02 12:29:00 +0100

    Iteration 2 - Create a recipe

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+  .then((x) => {
+    console.log(`🏃‍♀️Connected to the database: "${x.connection.name}"`);
+    // Before adding any recipes to the database, let's remove all existing one
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    //Recipe.create(data[0]);
+    return Recipe.create(data[0]);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => mongoose.connection.close())
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

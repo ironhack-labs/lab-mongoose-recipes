ba6844a2f6f6dd55589f3c74216df22cc23eea83
Arcadio REYES <arcadioreyesph@gmail.com>
2022-11-05 17:43:55 +0100

    Collection and Recipe Model definition made

--- index.js
+++ index.js
@@ -2,8 +2,58 @@ const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require("./models/Recipe.model");
+
 // Import of the data from './data.json'
 const data = require("./data");
+
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: {
+    type: ["String"],
+  },
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default:
+      "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 

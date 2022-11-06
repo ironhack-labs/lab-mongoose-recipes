552c18c23b226a57b2a05d131c6389ba64b0c953
Arcadio REYES <arcadioreyesph@gmail.com>
2022-11-06 12:08:10 +0100

    iteration2 commented out

--- index.js
+++ index.js
@@ -59,37 +59,37 @@ const Recipe = mongoose.model("Recipe", recipeSchema);
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then((x) => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany();
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    return Recipe.create({
-      title: "Asian Glazed Chicken Thighs",
-      level: "Amateur Chef",
-      ingredients: [
-        "1/2 cup rice vinegar",
-        "5 tablespoons honey",
-        "1/3 cup soy sauce (such as Silver Swan®)",
-        "1/4 cup Asian (toasted) sesame oil",
-        "3 tablespoons Asian chili garlic sauce",
-        "3 tablespoons minced garlic",
-        "salt to taste",
-        "8 skinless, boneless chicken thighs",
-      ],
-      cuisine: "Asian",
-      dishType: "main_course",
-      image:
-        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-      duration: 40,
-      creator: "Chef LePapu",
-    });
-  })
-  .then((recipe) => console.log("Recipe was successfully created", recipe))
-  .catch((error) => {
-    console.error("Error connecting to the database", error);
-  });
+mongoose.connect(MONGODB_URI).then((x) => {
+  console.log(`Connected to the database: "${x.connection.name}"`);
+  // Before adding any recipes to the database, let's remove all existing ones
+  return Recipe.deleteMany();
+});
+
+// Iteration2 commented out
+// .then(() => {
+//   // Run your code here, after you have insured that the connection was made
+//   return Recipe.create({
+//     title: "Asian Glazed Chicken Thighs",
+//     level: "Amateur Chef",
+//     ingredients: [
+//       "1/2 cup rice vinegar",
+//       "5 tablespoons honey",
+//       "1/3 cup soy sauce (such as Silver Swan®)",
+//       "1/4 cup Asian (toasted) sesame oil",
+//       "3 tablespoons Asian chili garlic sauce",
+//       "3 tablespoons minced garlic",
+//       "salt to taste",
+//       "8 skinless, boneless chicken thighs",
+//     ],
+//     cuisine: "Asian",
+//     dishType: "main_course",
+//     image:
+//       "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+//     duration: 40,
+//     creator: "Chef LePapu",
+//   });
+// })
+// .then((recipe) => console.log("Recipe was successfully created", recipe))
+// .catch((error) => {
+//   console.error("Error connecting to the database", error);
+// });

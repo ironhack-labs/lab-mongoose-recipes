1cbce788f5cb8523a4ac496ce9f2e1edc78959d4
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-11-06 10:21:56 -0300

    Iteration 1

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,45 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+    unique: [true],
+  },
+  level: {
+    type: String,
+    enum: [Easy_Peasy, Amateur_Chef, UltraPro_Chef],
+  },
+  ingredients: { 
+    type: [String],
+  },
+
+  cuisine: {
+    type: String,
+    required: [true]
+  },
+  dishType: {
+    type: String,
+    enum: [breakfast, main_course, soup, snack, drink, dessert, other]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: Date.today
+  } 
+},
+{ timestamps: true }
+);
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 

cef7f4063cc1af2838bee80413325fe2fedfad98
Arcadio REYES <arcadioreyesph@gmail.com>
2022-11-06 11:06:12 +0100

    The first recipe waas created and sent to MongoDB Compass however, the following message is displayed: Connected to the database: recipe-app Recipe was successfully created undefined

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const mongoose = require("mongoose");
-
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require("./models/Recipe.model");
+const mongoose = require("mongoose"); // connect to mongoose
 
 // Import of the data from './data.json'
 const data = require("./data");
 
+// Collection Definition
 const recipeSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -52,7 +50,7 @@ const recipeSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
-
+// Recipe Model definition
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
@@ -67,7 +65,28 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs",
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image:
+        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu",
+    });
   })
+  .then((recipe) => console.log("Recipe was successfully created", recipe))
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });

35c3cb8cdbd6f62bb6b7252fa94e2a4b37618764
David Fdez <dav.fdez.lopez@gmail.com>
2022-11-09 16:33:00 +0100

    done

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+// console.log(data)
+
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -15,9 +18,49 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  // .then((recipes) => {
+  //   // Run your code here, after you have insured that the connection was made
+  //   console.log(recipes)
+  //   return Recipe.create({
+  //     title: "Asian Glazed Chicken Thighs",
+  //     level: "Amateur Chef",
+  //     ingredients: [
+  //       "1/2 cup rice vinegar",
+  //       "5 tablespoons honey",
+  //       "1/3 cup soy sauce (such as Silver Swan®)",
+  //       "1/4 cup Asian (toasted) sesame oil",
+  //       "3 tablespoons Asian chili garlic sauce",
+  //       "3 tablespoons minced garlic",
+  //       "salt to taste",
+  //       "8 skinless, boneless chicken thighs"
+  //     ],
+  //     cuisine: "Asian",
+  //     dishType: "main_course",
+  //     image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  //     duration: 40,
+  //     creator: "Chef LePapu"
+  //   })
+  // })
+  // .then(recipe => {
+  //   console.log(recipe.title)
+  // })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+    return Recipe.create(data)
   })
+  // .then(x => {
+  //   x.forEach(element => {
+  //     console.log(element.title)
+  //   })
+    
+  // })
+  .then(() => {
+    return Recipe.updateOne({title: 'Rigatoni alla Genovese'},{duration:100})
+  })
+  .then(() => {
+    console.log('Congratulations, duration changed')
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

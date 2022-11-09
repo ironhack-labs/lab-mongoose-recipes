341ef55cb1a3628d01e74bb167a7af9d1fffe6ca
Marta Salvador <martasbsv@gmail.com>
2022-11-09 16:34:16 +0100

    final

--- index.js
+++ index.js
@@ -2,21 +2,29 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
+/* require('./data.json') */
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
+
+
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create(data)
+  })
+
+  .then(() => {
+    return Recipe.create({ title: 'macarrones con tomate', level: 'Easy Peasy', ingredients: ['macarrones', 'tomate'], cuisine: 'italiana', dishType: 'main_course', image: 'https://images.media-allrecipes.com/images/75131.jpg', duration: 30, creator: 'Naomi y Marta' })
+
+  })
+  .then((theNewRecipe) => {
+    console.log(theNewRecipe.title)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

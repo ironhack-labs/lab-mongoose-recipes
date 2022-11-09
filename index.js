5042cfc61eabe0d113b65ff1a40d7ff063f91eb6
Teresa Chaves <ttahaves@hotmail.com>
2022-11-09 16:36:03 +0100

    donde

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
+const { create, findByIdAndUpdate } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+
+
+
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
@@ -16,8 +20,27 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Recipe
+    //   .create({
+    //     title: "Garbanzos", level: "Easy Peasy", ingredients: ["garbanzo", "pollo", "chorizo"], cuisine: "modern", dishTypes: "soup", image: "https://images.media-allrecipes.com/images/75131.jpg", duration: 4, creator: "Teresa",
+    //   })
+    //   .then(recipe => { console.log(recipe) })
+    //   .catch(err => console.log(err))
+
+    Recipe
+      .create(data)
+      .then(data => { console.log(data) })
+      .catch(err => console.log(err))
+
+      .findByIdAndUpdate("636bc6ef79e77cd07aacae59", { duration: 100 })
+      .then(recipe => { console.log(recipe) })
+      .catch(err => console.log(err))
+
+
+
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

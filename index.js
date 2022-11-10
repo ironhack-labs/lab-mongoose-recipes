d2e563e6c9ce39cd5b475c12075046accb286b5e
hongen <hongen1993@gmail.com>
2022-11-10 03:31:57 +0100

    finish all, only 5 h left to sleep ;(

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -5,19 +6,59 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+const PORT = 3000;
+const app = express();
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+  .then(mongooseConnect => {
+    console.log(`Connected to the database: ${mongooseConnect.connections[0].name}`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = new Recipe({
+      title: 'Spanish Omelette',
+      level: 'Easy Peasy',
+      ingredients: ['Potatoes', 'Onions', 'Eggs', 'Olive Oil'],
+      cuisine: 'Spanish',
+      dishType: 'main_course',
+      image: 'https://sixhungryfeet.com/wp-content/uploads/2020/10/spanish-omelette-7.jpg',
+      duration: 30,
+      creator: 'Tomás de Zumalacárregui'
+    })
+    return newRecipe
+  })
+  .then((newRecipe) => {
+    return newRecipe.save()
+  })
+  .then((newRecipe) => {
+    console.log('recipe created')
+    console.log(newRecipe)
+  })
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true })
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally(() => {
+    mongoose.disconnect
+  })
+
+app.listen(PORT, () => {
+  console.log(`Listening in port ${PORT}`)
+})

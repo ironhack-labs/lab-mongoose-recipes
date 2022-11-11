a1696ee90ecf421715f3784c030db10f1b8f9db5
Parrr Geng <parrr.geng@gmail.com>
2022-11-11 13:46:37 +0100

    Completed lab

--- index.js
+++ index.js
@@ -17,6 +17,33 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    /* --- Iteration 1 ---
+    Recipe.create(data[0])
+     .then((createdBook)=>{
+       console.log(createdBook.title)
+     })
+     .catch(error => console.log(error));
+    */
+
+     const p1 = Recipe.insertMany(data);
+     const p2 = Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100});
+     const p3 = Recipe.deleteOne({title: "Carrot Cake"});
+
+     p1
+     .then((insertedArray)=>{
+       insertedArray.forEach(recipe => console.log(recipe.title));
+     })
+     .then(()=>{
+       Promise.all([p2, p3])
+       .then(()=> {
+        console.log("Rigatoni alla Genovese is now successfully updated.");
+        console.log("Carrot Cake is now removed from the database.");
+        
+        mongoose.connection.close();
+        console.log("Closed connection to the database.");
+        })
+      })     
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

81f5c15b993ef0439aae6653ced4bef9312530e0
Phelipe Victor <phelipevictor5@gmail.com>
2022-11-11 17:43:34 -0300

    lab done

--- index.js
+++ index.js
@@ -9,15 +9,62 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+  .connect(MONGODB_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+  })
+  .then((self) => {
+    console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    self.connection.dropDatabase();
+    // inserindo receita de PUDIM
+    Recipe.create([pudim])
+      .then(() => {
+        console.log('Criado:', pudim.title);
+
+        // inserindo array de receitas do data.json
+        Recipe.insertMany(data)
+          .then((receitas) => {
+            receitas.forEach((elem) =>
+              console.log('CRIOU VARIOS:', elem.title)
+            );
+
+            // UPDATE
+            Recipe.findOneAndUpdate(
+              { title: 'Rigatoni alla Genovese' },
+              { duration: 100 },
+              { new: true }
+            )
+              .then((response) => {
+                console.log(response);
+                // DELETE
+                Recipe.deleteOne({ title: 'Carrot Cake' })
+                  .then((response) => {
+                    console.log(response);
+                    mongoose.disconnect();
+                  })
+                  .catch((error) => console.log(error));
+              })
+              .catch((error) => console.log(error));
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+.catch((error) => {
+  console.error('Error connecting to the database', error);
+});
+
+const pudim = new Recipe({
+title: 'Pudim',
+level: 'Easy Peasy',
+ingredients: ['leite consensado', 'leite integral', 'ovos', 'açucar refinado'],
+cuisine: 'Brasileira',
+dishType: 'Dessert',
+image:
+  'https://img.itdg.com.br/tdg/images/recipes/000/031/593/318825/318825_original.jpg?mode=crop&width=710&height=400',
+duration: 60,
+creator: 'Phelipe'
+});

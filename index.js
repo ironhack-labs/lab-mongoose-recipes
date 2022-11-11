2f60d08a0fcff8af3b9da9c533a1b50b9dc17178
GaYeong <rkdud5483@gmail.com>
2022-11-11 17:43:50 +0100

    first commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+const data = require('./data.json');
+console.log(data)
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -16,8 +16,23 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({title:"test"})
+  })
+  .then(()=>{ 
+    return Recipe.insertMany(data)
+  })
+  .then (() => {
+    return Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration: 100})
+  })
+  .then (() => {
+    return Recipe.deleteOne({title: "Carrot Cake"})
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+ 
+  mongoose.connection.close()
+  .then(() => {console.log("connection close")})
+  .catch ((error) => console.log(error))
+  
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,8 +3,40 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+  },
+	level: {
+    type: String,
+  },
+	ingredients: {
+    type: [String],
+  },
+	released: {
+    type: Date,
+  },
+  cuisine: {
+    type: String,
+  },
+  dishType: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  creator: {
+    type: String,
+  },
+  created: Date
+  
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+

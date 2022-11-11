e6477912b2b1fe6cb5d04f69495c462a6dd50dee
Carolina Acuna <carolina.acuna90@gmail.com>
2022-11-11 17:30:13 +0100

    solved lab

--- index.js
+++ index.js
@@ -4,20 +4,55 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { create } = require('./models/Recipe.model');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+// const MONGODB_URI = 'mongodb+srv://caroacuna90:susanahoria22@cluster0.gxu5cpk.mongodb.net/test';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log(`Connected to the database"`);
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+/*   let newRecipe = {
+    title: "empanadas",
+    cuisine: "arg",
+      }
+ */
+  /*  Recipe.create(newRecipe)
+  .then((response)=>{
+    console.log(response.title)
+      mongoose.connection.close()
+  }) 
+  .catch(err => console.log(err))
+    */
+
+
+
+/*   Recipe.insertMany(data)
+  .then((response) => {
+    console.log(response.title)
+      mongoose.connection.close()
+  })
+  .catch(err => console.log(err))
+   */
+/* 
+  Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
+  .then((response) => {
+  console.log("this is a success message: ", response)  
+    mongoose.connection.close()
+    })
+    .catch(err => console.log(err)) */
+
+/*     Recipe.deleteOne({title: "Carrot Cake"})
+    .then((response) => {
+      console.log("this is another success message: ", response)
+          mongoose.connection.close()
+    })
+    .catch(err => console.log(err)) */

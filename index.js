5ca58761254e03aed54e46d765248d8532b43836
Fiodor Gorobet <iamfiogo@gmail.com>
2022-11-11 09:45:15 +0100

    Solved lab. minor additions

--- index.js
+++ index.js
@@ -15,10 +15,32 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany();
   })
-  .then(() => {
-    return Recipe.insertMany(data).then((recipe) =>
-      recipe.forEach((r) => console.log(r.title))
-    );
+  // Iteration 1
+  // .then(() => {
+  //   return Recipe.create({
+  //     title: "Asian Glazed Chicken Thighs",
+  //     level: "Amateur Chef",
+  //     ingredients: [
+  //       "1/2 cup rice vinegar",
+  //       "5 tablespoons honey",
+  //       "1/3 cup soy sauce (such as Silver Swan®)",
+  //       "1/4 cup Asian (toasted) sesame oil",
+  //       "3 tablespoons Asian chili garlic sauce",
+  //       "3 tablespoons minced garlic",
+  //       "salt to taste",
+  //       "8 skinless, boneless chicken thighs",
+  //     ],
+  //     cuisine: "Asian",
+  //     dishType: "main_course",
+  //     image:
+  //       "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  //     duration: 40,
+  //     creator: "Chef LePapu",
+  //   });
+  // })
+  .then(async () => {
+    const recipes = await Recipe.insertMany(data);
+    return recipes.forEach((recipe) => console.log(recipe.title));
   })
   .then(() => {
     return Recipe.findOneAndUpdate(

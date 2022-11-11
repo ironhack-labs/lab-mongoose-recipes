b178e5113e332d901ea827b73a9a873b0297c1c2
Ezequiel Benitez <ezequielbv@gmail.com>
2022-11-11 17:20:07 +0100

    [BER OCT22 FT] - Ezequiel Lab DB done

--- index.js
+++ index.js
@@ -13,10 +13,36 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    // return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+
+    // // Iteration 2
+    // Recipe.create({
+    //   "title": "recipe 1",
+    //   "cuisine": "Test"
+    // })
+    //   .then( (recipe) => console.log(recipe.title) )
+    //   .catch(err => console.log(err));
+
+    // // Iteration 3
+    Recipe.insertMany(data)
+      .then((recipeAll) => {
+        recipeAll.forEach(recipe => console.log(recipe.title))
+      })
+      .catch(err => console.log(err));
+
+    // // Iteration 4
+    Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+      .then(() => console.log("Success"))
+      .catch(err => console.log(err));
+
+    // // Iteration 5
+    Recipe.deleteOne({title: "Carrot Cake"})
+      .then((deletedRecipe) => console.log(deletedRecipe))
+      .catch(err => console.log(err));
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

a0c411cd261c775be264fd7c36d11009d4bfa26c
Julio Villalvazo Carrera <juliovillalvazo26@gmail.com>
2022-11-11 17:02:15 -0600

    Completed lab

--- index.js
+++ index.js
@@ -9,15 +9,44 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI)
+    .then((x) => {
+        console.log(`Connected to the database: "${x.connection.name}"`);
+        // Before adding any recipes to the database, let's remove all existing ones
+        return Recipe.deleteMany();
+    })
+    .then(() => {
+        // Run your code here, after you have insured that the connection was made
+    })
+    .catch((error) => {
+        console.error('Error connecting to the database', error);
+    });
+
+// Recipe.create({
+//     title: 'tacos',
+//     ingredients: ['carne', 'tortilla'],
+//     cuisine: 'mexicana',
+//     image: 'no image jeje',
+// }).then((info) => console.log(`successfully created`, info));
+
+const executeTaskInOrder = async () => {
+    try {
+        const insertMany = await Recipe.insertMany([...data]);
+        insertMany.forEach((recipe) => console.log(recipe.title));
+
+        await Recipe.findOneAndUpdate(
+            { title: 'Rigatoni alla Genovese' },
+            { duration: 100 }
+        );
+        console.log('Successfully updated rigatoni');
+
+        await Recipe.deleteOne({ title: 'Carrot Cake' });
+        console.log('Successfully deleted Carrot Cake');
+
+        mongoose.disconnect();
+    } catch (err) {
+        console.log('Something went wrong: ', err);
+    }
+};
+
+executeTaskInOrder();

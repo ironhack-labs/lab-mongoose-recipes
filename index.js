9cdac922be9b431ebb2832073459ff6f44305a5c
Frances Morales Velilla <frances0688@gmail.com>
2022-11-12 15:55:06 +0100

    Frances Morales - done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -16,8 +16,27 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    let newRecipe = {title: 'Chilaquiles verdes', cuisine: 'Mexican'};
+    Recipe.create(newRecipe)
+	  console.log(newRecipe.title)
+    return Recipe.insertMany(data)
+  })
+	.then((allRecipes) => {
+    allRecipes.forEach(createdRecipe => console.log(createdRecipe.title))
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, {new: true})
+  })
+  .then((updatedRecipe) => {
+    console.log("successfully updated:", updatedRecipe.title)
+    return Recipe.deleteOne({title: 'Carrot Cake'})
+  })
+  .then(() => {
+    console.log("successfully deleted recipe")
+    return mongoose.connection.close(() => {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+      });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

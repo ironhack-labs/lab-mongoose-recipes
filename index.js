f75a299c1f0758943fd624e975283d08570d4795
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-11-15 13:25:32 -0300

    Iteration 2

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const express = require('express');
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost:27017/recipes-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -10,11 +11,26 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  const index = express ()
+  
+  index.get ('/', (req, res) => {
+    res.send ('Está funcionando')
+  })
+
+  index.get ('/:title', async (req, res) => {
+    const {title} = req.params
+    const newRecipe = await Recipe.create ({title: 'Ovo Frito', level: 'Easy Peasy', ingredients: ['Ovo', 'Sal'], cuisine: 'Brazilian', dishType: 'breakfast', duration: '2' })
+    res.json(newRecipe)
+  })
+
+  index.listen(5000, () => {
+    console.log ('server running')
+  })
+
+  

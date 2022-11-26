929f9556918ccd836ed6e7bd362e1791310f2c7f
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2022-11-25 21:45:25 -0600

    Listo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const datos = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -17,7 +17,55 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Pollo a la crema",
+      level: "Amateur Chef",
+      ingredients: ["Un pollo","Salsa de tomate","sal","Jugo de Naranja"],
+      cuisine: "Mexicana",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 30,
+      creator: "Chef OroPesa"
+    })
+    .then( recipe => console.log("Una nueva receta",recipe))
+    .catch(error=> console.log("Error al crear receta",error))
+    
+    Recipe.insertMany(datos)
+    .then( data => console.log("Datos insertados correctamente",data))
+    .catch(error=> console.log("Error al crear receta",error))
+    
+    Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"},{duration:100},{new:true})
+    .then( recipe => console.log("Success!!",recipe))
+    .catch(error=> console.log("Error al crear receta",error)) 
+    
+    Recipe.deleteOne({title:"Carrot Cake"})
+    .then( recipe => console.log("Success delete", recipe))
+    .catch(error=> console.log("Error al crear receta",error))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+/* Recipe.create({
+  title: "Pollo a la crema",
+  level: "Amateur Chef",
+  ingredients: ["Un pollo","Salsa de tomate","sal","Jugo de Naranja"],
+  cuisine: "Mexicana",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 30,
+  creator: "Chef OroPesa"
+})
+.then( recipe => console.log("Una nueva receta",recipe))
+.catch(error=> console.log("Error al crear receta",error))
+
+Recipe.insertMany(datos)
+.then( data => console.log("Datos insertados correctamente",data))
+.catch(error=> console.log("Error al crear receta",error))
+
+Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"},{duration:100},{new:true})
+.then( recipe => console.log("Success!!",recipe))
+.catch(error=> console.log("Error al crear receta",error)) 
+
+Recipe.deleteOne({title:"Carrot Cake"})
+.then( recipe => console.log("Success delete", recipe))
+.catch(error=> console.log("Error al crear receta",error)) */
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,42 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title:{
+    type:String,
+    unique:true
+  },
+  level:{
+    type:String,
+    enum:["Easy Peasy","Amateur Chef","UltraPro Chef"],
+    default:"Easy Peasy"
+  },
+  ingredients:{
+    type:Array
+  },
+  cuisine:{
+    type:String,
+    required:true
+  },
+  dishType:{
+    type:String,
+    enum:["breakfast","main_course","soup","snack","drink","dessert","other"],
+    default:"breakfast"
+  },
+  image:{
+    type:String,
+    default:"https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration:{
+    type:Number,
+    min:0
+  },
+  creator:{
+    type:String
+  },
+  created:{
+    type:Date,
+    timestamps:true,
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

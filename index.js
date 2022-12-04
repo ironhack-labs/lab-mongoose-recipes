6dee9643184f88062a77321bc953a7ded77fec2f
Jesus Gonzalez <jesusglezt27@gmail.com>
2022-12-04 16:06:34 -0600

    completed lab

--- index.js
+++ index.js
@@ -15,9 +15,47 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  // .then(() => {
+  //   Recipe.create({
+  //         "title": "Asian Glazed Chicken Thighs",
+  //         "level": "Amateur Chef",
+  //         "ingredients": [
+  //           "1/2 cup rice vinegar",
+  //           "5 tablespoons honey",
+  //           "1/3 cup soy sauce (such as Silver Swan®)",
+  //           "1/4 cup Asian (toasted) sesame oil",
+  //           "3 tablespoons Asian chili garlic sauce",
+  //           "3 tablespoons minced garlic",
+  //           "salt to taste",
+  //           "8 skinless, boneless chicken thighs"
+  //         ],
+  //         "cuisine": "Asian",
+  //         "dishType": "main_course",
+  //         "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  //         "duration": 40,
+  //         "creator": "Chef LePapu"
+  //         })
+  //       .then(recipe => console.log(recipe.title))
+  //       .catch(err => console.log(err))
+  // })
+
+  Recipe.insertMany(data)
+  .then(recipe => {
+    recipe.forEach(recipe => {
+      console.log(recipe.title)
+    })
+    })
+  .catch(err => console.log(err))
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: "100"})
+    .then(update => console.log("Updated duration"))
+    .catch(err => console.log(err))  
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  Recipe.deleteOne({title:"Carrot Cake"})
+  .then(recipe => console.log("Carrot cake deleted"))
+  .catch(error => console.log("Delete error", error))
+
+  mongoose.connection.close(() => {
+    console.log('Mongoose donnection closed')

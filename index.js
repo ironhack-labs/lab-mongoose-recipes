7d4415dfdf2117d2440e4c2903b3b13d7b63aa35
Luluyan2022 <emmymagao@gmail.com>
2022-12-15 18:54:29 +0100

    Completed lab

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +15,50 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  //Iteration 2 - Create a recipe
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title:"Lemon cheesecake",
+      level: "Amateur Chef",
+      ingredients: ["butter", "nut biscuits","eggs","vanilla","sugar","cream cheese","lemon","raspberries"],
+      cuisine: "english",
+      dishType:"dessert",
+      duration:60,
+      creator:"Jamie Oliver"
+    })
   })
+  .then((recipeFromDB) => {
+    console.log(recipeFromDB.title)
+  })
+  //Iteration 3 - Insert multiple recipes
+  .then(()=>{ 
+    return Recipe.insertMany(data)
+  })
+  .then((dataFromDate)=>{ 
+    dataFromDate.forEach((ele)=>{
+        console.log(ele.title)
+    })
+   
+  })
+  //Iteration 4 - Update recipe
+  .then(()=>{ 
+    const updateInfo = Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 });
+    return updateInfo;
+  })  
+  .then((updateInfo) =>{
+     console.log(updateInfo)
+  })
+  
+   //Iteration 5 - Remove a recipe
+  .then(()=>{ 
+     return Recipe.deleteOne({title: "Carrot Cake"})     
+    })
+  .then(recipe => console.log("Delete carrot cake"))
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  //Iteration 6 - Close the Database
+  mongoose.connection.close()

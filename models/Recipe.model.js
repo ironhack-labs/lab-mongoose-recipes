02878fd9b850d8644f6e4400efa482c7f940f4cd
Natalia Gravereaux <natalia.gravereaux@ose-engineering.fr>
2022-12-15 21:49:29 +0100

    iteration1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+    //validate: {validator ()},
+  },
+  ingridients: [String],
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+
+  duration: {
+    type: Number,
+    min: 0,
+  },
+
+  creator: String,
+  created: {
+    type: Date,
+    default: Date("2022-12-15"),
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

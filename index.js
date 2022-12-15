702f660f4228fee0ccb2e1d2b01cf0863f19d8c3
marinedrp <marine.drp@outlook.com>
2022-12-15 18:17:31 +0100

    Lab done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,8 +16,48 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+    const newRecipe = {
+      title: "Pumpkin Soup",
+      level: "Easy Peasy",
+      ingredients: ["pumpkin", "potato", "onion", "garlic", "vegetable broth", "olive oil", "salt", "pepper", "cream", "chili"],
+      cuisine: "German",
+      dishType: "soup",
+      image: "https://media.healthyfood.com/wp-content/uploads/2019/07/Creamy-pumpkin-soup-1024x638.jpg",
+      duration: 30,
+      creator: "Chef Laura"
+    }
+    
+    return Recipe.create(newRecipe)
+    
+  })
+  .then((newRecipe) => {
+    console.log(newRecipe)
+  })
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then((data) => {
+    data.forEach(object => {
+      console.log(object.title)
+    })
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100} )
+  })
+  .then(() => {
+    console.log("Duration updated!")
+  })
+  .then(() => {
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(() => {
+    console.log("Carrot Cake deleted!")
+  })
+  .then(() => {
+    mongoose.disconnect();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+ 

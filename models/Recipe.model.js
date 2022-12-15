f79a85ca5a8853e57548642236cd07ed1c58ff5f
Thalita dos Reis <dosreistha@gmail.com>
2022-12-15 17:34:20 +0100

    iteration one done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,15 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {type: String, required: true, unique: true},
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: [String],
+  cuisine: {type: String, required: true},
+  dishType: {type: String, enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']},
+  image: String("https://images.media-allrecipes.com/images/75131.jpg"),
+  duration: {type: Number, min: 0},
+  creator: String,
+  created: Date('15/12/2022')
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

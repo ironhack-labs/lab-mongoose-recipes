41876c0ef88a1e5b54ece261bf47345743d7b827
Henrique Mazzoleni <73548364+Henrique-mazzoleni@users.noreply.github.com>
2022-12-16 09:01:12 +0100

    refactored code, inserted and fixed logs

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany();
   })
-  .then(() => {
-    return Recipe.create({
+
+  //Iteration 2
+  .then(() => Recipe.create({
       title: "Potato Soup",
       level: "Easy Peasy",
       ingredients: ["Potato", "Onions", "garlic", "Cheese"],
@@ -25,31 +26,26 @@ mongoose
       duration: 60,
       creator: "Heinrich",
     });
-  })
-  .then((recipe) => {
-    console.log(recipe);
-  })
+  )
+  .then((recipe) => console.log(recipe.title))
 
   //ITERATION 3
-  .then(() => {
-    return Recipe.insertMany(data);
-  })
-  .then((recipe) => {
-    console.log(recipe);
+  .then(() => Recipe.insertMany(data))
+  .then((recipes) => {
+    for (recipe of recipes) console.log(recipe.title)
   })
 
   //ITERATION 4
-  .then(() => {
-    return Recipe.findOneAndUpdate(
+  .then(() => Recipe.findOneAndUpdate(
       { title: "Rigatoni alla Genovese" },
       { duration: 100 }
     );
-  })
+  )
+  .then(() => console.log('Recipe updated successfully!'))
 
   //ITERATION 5
-  .then(() => {
-    return Recipe.deleteOne({ title: "Carrot Cake" });
-  })
+  .then(() => Recipe.deleteOne({ title: "Carrot Cake" }))
+  .then(() => console.log('Recipe removed successfully!'))
 
   //ITERATION 6
   .then(() => {

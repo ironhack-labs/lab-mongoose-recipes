794cea83f42b4370aba933eef06aee9f6c5dbd70
Sebastian Martinez <bastianelor@hotmail.com>
2023-01-06 11:18:31 -0600

    listo

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require('./models/Recipe.model.js');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -17,6 +17,29 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: 'Pato a la Naranja',
+      level: 'Amateur Chef',
+      ingredients: ['pato','licor de naranja','sal'],
+      cuisine: 'Italiana',
+      dishType: 'main_course',
+      duration: 30,
+      creator: "Chef OroPesa"
+    })
+    .then( recipe => console.log("Una nueva receta",recipe))
+    .catch(error=> console.log("Error al crear receta",error))
+    
+    Recipe.insertMany(data)
+    .then( data => console.log("Datos insertados correctamente",data))
+    .catch(error=> console.log("Error al crear receta",error))
+    
+    Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"},{duration:100},{new:true})
+    .then( recipe => console.log("Success!!",recipe))
+    .catch(error=> console.log("Error al crear receta",error)) 
+    
+    Recipe.deleteOne({title:"Carrot Cake"})
+    .then( recipe => console.log("Success delete", recipe))
+    .catch(error=> console.log("Error al crear receta",error))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

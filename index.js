b5c8ad14ddfd11f8400d93dc73e6e78eb3509bac
Jaena Tariga <jaenatariga@gmail.com>
2023-01-12 14:58:58 -0800

    need to go over

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');//connected to database
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
@@ -9,15 +9,83 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+  .connect(MONGODB_URI)//connect
+  .then(x => {// do this once connected
+    console.log(`Connected to the database: "${x.connection.name}"`);//
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  .then(() => { //using  model.create, add a new recipe and pass details as object and console log the title
+//2. Run your code here, after you have insured that the connection was made 
+const makeRecipe = {
+
+  title:"Taiwanese Popcorn Chicken",
+  level: "Amateur Chef",
+  ingredients:["chicken cubes","corn starch", "salt", "pepper", "paprika", "basil", "oil"],
+  cuisine:"Tawainese",
+  dishType:"other",
+  image:"https://images.media-allrecipes.com/images/75131.jpg",
+  duration:60,
+  creator:"Jay",
+ };
+  return Recipe.create(makeRecipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+
+//save? and insert mulitple recipes using model.insertMAny
+
+  .then((newRecipe)=>{
+   console.log(newRecipe.title)  //console log with the title
+   return Recipe.insertMany(data);
+})
+  .then(()=>{
+    newRecipe.forEach((newRecipe)=>
+    console.log(newRecipe.title));
+ 
+
+    return Recipe.findOneAndUpdate(
+      {title:"Rigatoni alla Genovese"},
+      {duration:100}
+      
+    );
+  })
+
+    .then(()=>{
+     console.log('success!');
+   
+    return Recipe.findOneAndDelete(
+      {title:"Carrot Cake"});
+
+     })
+
+     .then(()=>{
+      console.log('successfully deleted')
+     })
+
+  //close connection:
+  mongoose.connection.close(()=>{
+    console.log("connection successfully closed")
+  })
+
+  .catch((error) => {
+    
+      console.error('Error connecting to the database', error);
   });
+
+ 
+
+  //creating a model:
+ //blueprint /model Cat--
+  //const Cat = mongoose.model('Cat', { name: String });
+// ^called 'cat' model  .... Cat is constructor function-- "Cat" decides what the name of the collections will be
+//Cat constructor function creates objects with field "name" & "String" type
+//{name.String} is 2nd arg. passed to mongoose.model called "Schema" .... {name:string=" cats all have a name thats a string"
+
+//When Cat model is used to interact with databasem it will only interact with collectin that shares name -- cats collection
+//model will have upppercase and be singular, 
+//collection will have lowercase and be plural
+//^^^^User --- users ( Model ---collections)
+
+//to make a new cat/instance to modeland how:
+//const kitty = new Cat({ name: 'Ironhack cat' });       it's like creating a class
+//newCatToCreate.save();      to save what we create.       new "Cat" matches const Cat line 34 & {name:string} need to match schema
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,8 +3,41 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+    title:{type:String, required: true , unique: true},
+    level:{type: String,enum:["Easy Peasy" , "Amateur Chef", "UltraPro Chef"]},
+    ingredients:{type:[String]},
+    cuisine:{type:String, required: true},
+    dishType:{type:String,enum:["breakfast","main_course","soup","snack","drink","dessert","other"]},
+    image:{type:String, default:"https://images.media-allrecipes.com/images/75131.jpg"},
+    duration:{type:Number,min:0},
+    creator:String,
+    created:{type:Date,default:Date.now()}
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
+
 module.exports = Recipe;
+
+//creating a model:
+ //blueprint /model Cat--
+  //const Cat = mongoose.model('Cat', { name: String });
+// ^called 'cat' model  .... Cat is constructor function-- "Cat" decides what the name of the collections will be
+//Cat constructor function creates objects with field "name" & "String" type
+//{name.String} is 2nd arg. passed to mongoose.model called "Schema" .... {name:string=" cats all have a name thats a string"
+
+//When Cat model is used to interact with databasem it will only interact with collectin that shares name -- cats collection
+//model will have upppercase and be singular, 
+//collection will have lowercase and be plural
+//^^^^User --- users ( Model ---collections)
+
+//to make a new cat/instance to modeland how:
+//const kitty = new Cat({ name: 'Ironhack cat' });       it's like creating a class
+//newCatToCreate.save();      to save what we create.       new "Cat" matches const Cat line 34 & {name:string} need to match schema
+
+
+//to delete:
+//Cat.findOneAndDelete({}) takes the object/query and takes the first and deletes itfrom db
+//to edit:
+//cat.findOneandUpdate({}) ..1 willl find first 1 .. 2nd parameter will edit 

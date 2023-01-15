b326c6d29103de340e1c7bc51ec29e33e840085f
Amanda Cuervo <amandacuervo11@gmail.com>
2023-01-15 23:53:17 +0100

    iterations 1,2,3,4,5

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+//const MONGODB_URI = 'mongodb://localhost:27017/recipe-app'; -> NO FUNCIONA POR QUE?
+const MONGODB_URI = 'mongodb://127.0.0.1/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,8 +17,29 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then((deleted) => {
+    return Recipe.create({
+      "title": "Risoto",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup light brown sugar",
+        "1 large egg",
+        "2 tablespoons milk",
+        "1 1/4 teaspoons vanilla extract",
+        "2 cups semisweet chocolate chips"
+      ],
+      "cuisine": "Italian",
+      "dishType": "main_course",
+      "duration": 60,
+      "creator": "Chef Amanda"
+    })
+  })
+  .then((created) => {
+    console.log(created.title);
+    return Recipe.insertMany(data);
+  })
+  .then(created => {
+    console.log("recipes created");
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

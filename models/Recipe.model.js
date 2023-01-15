f4205fca0da62017d1a137489b0d463cbc3c2776
unknown <roxan.m.alma18@gmail.com>
2023-01-15 12:43:55 +0100

    1º interaction

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,48 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+  },
+
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+
+  ingredients: {
+    type: [String]
+  },
+
+  cusine: {
+    type: String,
+  },
+
+  dishType: {
+    type: String,
+    required: true,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg',
+  },
+
+  duration: {
+    type: Number,
+    min: 0
+  },
+
+  creator: {
+    type: String
+  },
+
+  created: {
+    type: Date,
+    default: today
+  }
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

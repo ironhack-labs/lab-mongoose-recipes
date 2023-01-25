c450180a897cf9b62236117a9598ac4c52f9f1d1
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2023-01-25 15:54:55 -0800

    Completed lab

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
+
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
+//Iteration 2
+const newRecipe = new Recipe ({
+  title: 'Batata Recheada de Micro-Ondas',
+  level: 'Easy Peasy',
+  ingredients: [
+    '2 Batatas grandes',
+    '100g Mussarela',
+    '2 colheres de cram cheese',
+    '50g de frango desfiado',
+    '1 colher de margarina',
+    'Cebolinha picada a gosto',
+    'Sal a gosto',
+    'Pimenta do reino a gosto'
+  ],
+  cuisine: 'Brasileira',
+  dishType: 'main_course',
+  image: 'https://cdn.panelinha.com.br/receita/1467255600000-Batata-assada-no-micro-ondas-recheada-com-cogumelo.jpg',
+  duration: 20,
+  creator: 'Weslley Fortunato',
+})
+
+mongoose.connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Iteration 2
+    Recipe.create(newRecipe)
+    console.log(`Recipe ${newRecipe.title} created successfully!`, newRecipe)
+    // Iteration 3
+    return Recipe.insertMany(data)
+  })
+    // Iteration 4
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {$set: { duration: 100 }})
+  })
+  .then (() => {
+    console.log('Duration updated successfully!')
+  })
+    // Iteration 5
+  .then(() => {
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
+  })
+  .then(() => {
+    console.log('Recipe deleted successfully!')
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+    // Iteration 6
+  .finally(() => {
+    mongoose.connection.close(() => {
+      console.log('Closing the connection')
+      process.exit(0)
+    })
+  })

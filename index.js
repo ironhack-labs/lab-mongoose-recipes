1de691a4adf37567cc9ea9de99a340f65e473b48
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-03 15:57:26 +0100

    handle asynchronicity properly

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 mongoose
   .connect(MONGODB_URI)
-  .then(() => Recipe.deleteMany())
   .then(() => {
-    Recipe.insertMany(data)
+    return Recipe.deleteMany()
+      .then((recipes) => console.log("The recipes are deleted and their value is: ", recipes))
+      .catch((error) => console.log("An error happened while deleting existing recipes:", error));
+  })
+  .then(() => {
+    return Recipe.insertMany(data)
       .then((recipes) => recipes.forEach((recipe) => console.log(recipe.title)))
       .catch((error) => console.log("An error happened while saving a new recipe:", error));
-
-    Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
       .then((recipe) => console.log("The recipe is updated and its value is: ", recipe))
       .catch((error) => console.log("An error happened while updating a recipe:", error));
-
-    Recipe.deleteOne({ title: "Carrot Cake" })
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
       .then((recipe) => console.log("The recipe is deleted and its value is: ", recipe))
       .catch((error) => console.log("An error happened while deleting a recipe:", error));
   })

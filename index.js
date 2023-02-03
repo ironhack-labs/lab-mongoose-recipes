44fec237ad3b0f15dcdd187f4028a4e34aad351c
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-03 14:28:20 +0100

    complete lab

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
+const data = require("./data");
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
+  .then(() => Recipe.deleteMany())
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.insertMany(data)
+      .then((recipes) => recipes.forEach((recipe) => console.log(recipe.title)))
+      .catch((error) => console.log("An error happened while saving a new recipe:", error));
+
+    Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+      .then((recipe) => console.log("The recipe is updated and its value is: ", recipe))
+      .catch((error) => console.log("An error happened while updating a recipe:", error));
+
+    Recipe.deleteOne({ title: "Carrot Cake" })
+      .then((recipe) => console.log("The recipe is deleted and its value is: ", recipe))
+      .catch((error) => console.log("An error happened while deleting a recipe:", error));
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .catch((error) => console.error("Error connecting to the database", error))
+  .finally(() => mongoose.connection.close());

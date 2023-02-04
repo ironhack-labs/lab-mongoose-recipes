414aef6f98d6a6a05577214c8287230c4885e64e
Zergo12 <adzg96@gmail.com>
2023-02-03 18:43:59 -0600

    Primeros ajustes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,25 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(data[0])
+          .then(data => {
+            const {title} = data
+            console.log({title})
+          })
+          .catch(console.log)
+
+    Recipe.insertMany(data)
+          .then(data => {
+            data.forEach(data => console.log(data.title))
+          })
+          .catch(err => console.log(err))
+
+    Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {$inc:{duration: 100}})
+          .then(console.log)
+          .catch(err => console.log(err))
+
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,19 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {type:String},
+  level: {type:String},
+  ingredients: {type:[String]},
+  cuisine: {type:String},
+  dishType:{type:String},
+  image:{type:String,
+  default:"https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {type:Number},
+  creator: {type: String},
+  created: {type:Date,
+  default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

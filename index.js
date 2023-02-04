52f7511d34d6aafbc2f3e28a20379ac20808ba72
Pauline <sisal.dynamo-0r@icloud.com>
2023-02-04 22:23:22 +0100

    iteration 0 - 4

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+
+mongoose.set('strictQuery', true);
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then( async () => {
+    let recipe = await Recipe.create(
+      {
+        title: "Potato soup",
+        cuisine: "german",
+        level: "Amateur Chef",
+        ingredients: [
+          "1kg Potatoes",
+          "3 carrots",
+          "1 tbs salt",
+          "1 teaspoon pepper",
+          "1 tbs garlic powder",
+          "1 tbs vegetable broth powder",
+        ],
+        dishType: "main_course",
+        image:
+          "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+        duration: 60,
+        creator: "Chef Pauli",
+      },
+    );
+    console.log(recipe.title)
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(async () => {
+    let documents = await Recipe.insertMany(data);
+    documents.forEach(document => console.log(document.title))
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

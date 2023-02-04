4ee098009aacfc78b4ab0bd56a92ad054958fa62
Necrosz <g.alexisarellano@gmail.com>
2023-02-03 18:44:52 -0600

    Update Alexis Arellano

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,26 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    Recipe 
+          .create(data[0])
+          .then(data => console.log(data.title))
+          .catch(console.log)
+
+      Recipe.insertMany(data)
+            .then(data => data.forEach(data => console.log(data.title)))
+            .catch(err => console.log(err))
+
+
+      Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, {duration:100})
+          .then(console.log("We Did It!!!"))
+          .catch(err => console.log(err))
+          
+
+      Recipe.deleteOne({title:"Carrot Cake"})
+          .then(console.log("Is no longer available"))
+          .catch(err => console.log(err))
+
+  })      
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

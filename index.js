33670ed8c0888fdfc0c043297320d8fd1412bb3d
Maria Fernanda Echeverria Colorado <fernanda.ecmf@gmail.com>
2023-02-06 22:17:27 -0600

    update3

--- index.js
+++ index.js
@@ -39,8 +39,52 @@ mongoose
   return Recipe.create(newRecipe);
   })
   .then(recipe => {
-    console.log('Recipe "' + recipe.title + '" has been added to the database');
+    console.log('Recipe ' + recipe.title + ' has been added to the database');
   })
   .catch(error => {
   console.error('Error adding the recipe', error);
+  });
+  const recipeDetails = {
+    title: 'Recipe 1',
+    level: 'Easy Peasy',
+    ingredients: ['Ingredient 1', 'Ingredient 2'],
+    cuisine: 'Cuisine 1',
+    dishType: 'main_course',
+    duration: 30,
+    creator: 'Creator 1'
+  };
+  
+  mongoose
+    .connect(MONGODB_URI)
+    .then(x => {
+      console.log(`Connected to the database: "${x.connection.name}"`);
+      return Recipe.create(recipeDetails);
+    })
+    .then(recipe => {
+      console.log(`Recipe "${recipe.title}" has been added to the database`);
+    })
+    .catch(error => {
+      console.error('Error adding the recipe to the database', error);
+    });
+    mongoose
+  .connect(MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 }}, { new: true });
+  })
+  .then(recipe => {
+    console.log(`The recipe "${recipe.title}" has been updated successfully`);
+    mongoose.connection.close();
+  })
+  .catch(error => {
+    console.error(error);
+    mongoose.connection.close();
+  });
+  mongoose.connection
+  .close()
+  .then(() => {
+    console.log('The database connection has been closed');
+  })
+  .catch(err => {
+    console.error(`An error occured while closing the database connection: ${err}`);
   });

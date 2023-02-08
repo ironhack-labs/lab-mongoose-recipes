d408d5e312b379be4458037886b70800961de3a1
YadMorSal <yadmina@gmail.com>
2023-02-08 16:38:57 +0100

    Done

--- index.js
+++ index.js
@@ -5,19 +5,38 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
+  .set('strictQuery', false)
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
+
+
     return Recipe.deleteMany()
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe
+      .create({ Title: 'tortilla de patatas', level: 'Amateur Chef', ingredients: ['Aceite, patatas, huevos, sal '], cuisine: 'Mediterranea', dishType: 'main_course', image: 'https://cdn.elcocinerocasero.com/imagen/receta/1000/2022-06-15-19-04-10/tortilla-de-patata.webp', duration: 40, creator: 'Daniel de Miguel' })
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then(() => {
+    Recipe
+      .insertMany(data)
+      .then(newRecipes => console.log('se han creado las recetas', newRecipes))
+      .catch(error => {
+        console.error('Error connecting to the database', error);
+      })
+      .then(() => {
+        Recipe
+          .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+          .then(updateRecipe => console.log('se ha modificado?', updateRecipe))
+      })
+      .catch(error => {
+        console.error('Error connecting to the database', error);
+      });
+
+
   });

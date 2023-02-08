2be450e1107f0f34a62526079a20afd378be4058
Cristina Gutiérrez <cgutierrezgm@gmail.com>
2023-02-08 16:09:43 +0100

    v1

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
+  .set('strictQuery', true)
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
@@ -17,7 +18,21 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    data.forEach(element => {
+      const title = element.title
+      console.log('Los nombres de la receta son: ', title)
+    })
+    return Recipe.insertMany(data)
+  })
+  .then(() => {
+    console.log('La base de datos de recetas ha sido actualizada')
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  })
+  .then(() => {
+    console.log('Receta eliminada')
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally(() => mongoose.connection.close())

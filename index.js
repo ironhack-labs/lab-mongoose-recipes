d8dce564158ac7ae3f84104f38b090422c3e7056
“Adrian <johndoe@example.com>
2023-02-08 16:33:31 +0100

    Done

--- index.js
+++ index.js
@@ -7,8 +7,29 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const firstRecipe = {
+  "title": "Asian Glazed Chicken Thighs",
+  "level": "Amateur Chef",
+  "ingredients": [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  "cuisine": "Asian",
+  "dishType": "main_course",
+  "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  "duration": 40,
+  "creator": "Chef LePapu"
+}
+
 // Connection to the database "recipe-app"
 mongoose
+  .set('strictQuery', false)
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
@@ -17,7 +38,25 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create(firstRecipe)
+  })
+  .then(newRecipe => {
+    console.log(newRecipe.title)
+  })
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then(allRecipes => {
+    console.log(allRecipes)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally(() => mongoose.connection.close())
+

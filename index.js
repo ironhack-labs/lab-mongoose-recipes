4451669e85be8a33fbb97b8c0d9700d42087ad7e
jelloreda <jaimelloredavelasco@gmail.com>
2023-02-08 16:15:51 +0100

    iteration 6

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const recepieArr = require('./data.json')
+
 // Connection to the database "recipe-app"
 mongoose
+  .set('strictQuery', true)
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
@@ -16,8 +19,32 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+    Recipe
+      .create({
+        title: 'Lemonade',
+        level: 'Easy Peasy',
+        ingredients: ['water', 'lemons', 'sugar'],
+        cuisine: 'Basics',
+        dishType: 'drink',
+        image: 'https://www.errenskitchen.com/wp-content/uploads/2020/08/lemonade-1-3.jpg}',
+        duration: 5,
+        creator: 'Lemon tree',
+      })
     // Run your code here, after you have insured that the connection was made
+  })
+  .then(() => {
+    return Recipe
+      .insertMany(recepieArr)
+  })
+  .then(() => {
+    return Recipe
+      .findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  })
+  .then(() => {
+    return Recipe
+      .deleteOne({ title: 'Carrot Cake' })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally(() => mongoose.connection.close())

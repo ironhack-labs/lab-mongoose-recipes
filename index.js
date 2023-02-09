a3c16d748353ad37f2260bacc35cce8e092d4d52
Lukas Merlin Jürgens <108548830+lukasmerlin@users.noreply.github.com>
2023-02-09 22:13:27 +0100

    done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
+// Import of the model Recipe from './models/Recipe.model.js' 
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,44 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Moldy Raw Chicken",
+      level: "Amateur Chef",
+      ingredients: [
+        "expired Chicken",
+        "Rat Poison",
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef Diogo"
+    })
   })
+  .then(() => {
+    Recipe.insertMany(data)
+    .then((recipe) => {
+      for(let item of recipe){
+        console.log(item.title)
+      }
+    })
+    .then(()=>{
+      Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+      .then(() => {
+        console.log("update successful")
+      })
+    })
+    .then(()=>{
+      Recipe.deleteOne({title: "Carrot Cake"})
+      .then(() => {
+        console.log("deletion successful")
+        mongoose.connection.close();
+      })
+    })
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+

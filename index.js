c370c6811bb72772c72f7c4eeb91417a69dbe4e1
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-09 21:29:00 +0100

    Iteration 2 done

--- index.js
+++ index.js
@@ -5,19 +5,32 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+	.connect(MONGODB_URI)
+	.then((x) => {
+		console.log(`Connected to the database: "${x.connection.name}"`);
+		// Before adding any recipes to the database, let's remove all existing ones
+		return Recipe.deleteMany();
+	})
+	.then(() => {
+		// Run your code here, after you have insured that the connection was made
+
+		const couscous = {
+			title: 'Couscous',
+			level: 'Easy Peasy',
+			Ingredients: ['Semolina flour', 'chicken broth', 'kosher salt', 'couscous'],
+			cuisine: 'North African',
+			dishType: 'main_course',
+			image: 'https://www.onceuponachef.com/images/2016/10/Perfect-Couscous-1200x795.jpg',
+			duration: 15,
+			creator: 'God',
+		};
+
+		Recipe.create(couscous).then(console.log(couscous.title));
+	})
+	.catch((error) => {
+		console.error('Error connecting to the database', error);
+	});

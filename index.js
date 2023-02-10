e3cda031265333458819896bccfbab5b7798772e
BodyElvis <bodymoliki@gmail.com>
2023-02-10 20:25:50 +0100

    Lab-Mongoose-Recipes-Solution

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
+
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+mongoose.set('strictQuery', true);
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title: 'Southern Baked Mac and Cheese',
+      level: 'Easy Peasy',
+      ingredients: ['elbow macaroni', 'salted butter', 'eggs', 'milk', 'extra-sharp aged cheddar cheese', 'mild cheddar cheese', 'mascarpone cheese', 'dry mustard powder', 'ground nutmeg', 'black pepper'],
+      cuisine: 'American',
+      dishType: 'main_course',
+      image:
+        'https://cheneetoday.com/wp-content/uploads/2021/05/southern-baked-mac-and-cheese-with-evaporated-milk-1024x1024.jpg',
+      duration: 90,
+      creator: 'unknown'
+    };
+    return createRecipe(newRecipe);
+  })
+  .then(result => console.log(`Recipe created: ${result}`))
+  .then(() => Recipe.insertMany(data))
+  .then(result => {
+    console.log(`Created ${result.length} recipes`);
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, {duration: 100}, {new: true});
+  })
+  .then(result => {
+    console.log(`Updated ${result.title} and new duration is: ${result.duration}`);
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(result => {
+    console.log("The recipe was deleted", result);
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.error("Error: ", error);
+  })
+  .finally(() => mongoose.connection.close());
+
+function createRecipe(newRecipe) {
+  return Recipe.create(newRecipe);
+}

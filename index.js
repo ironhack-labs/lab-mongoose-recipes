d547c10d4ead52eb1d1e56798858733a2345ef18
gferreira7 <104030930+gferreira7@users.noreply.github.com>
2023-02-10 19:45:11 +0000

    finishing iterations 3 and 4

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const Recipe = require('./models/Recipe.model')
 // Import of the data from './data.json'
 const data = require('./data')
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app'
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app'
 
 // Connection to the database "recipe-app"
 mongoose
-  .set("strictQuery", true)
+  .set('strictQuery', true)
   .connect(MONGODB_URI)
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`)
@@ -18,9 +18,36 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    let newRecipe = Recipe.create(data[0]).then(() =>{
-      console.log(data[0].title)
+
+    // Recipe.create(data[0]).then( () =>{
+    //   console.log(data[0].title)
+    // })
+
+    Recipe.insertMany(data).then(() => {
+      Recipe.find({}, { title: 1, _id: 0 }, (err, result) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(result)
+        }
+      })
+
+      let opts = { new: true }
+      Recipe.findOneAndUpdate(
+        { title: 'Rigatoni alla Genovese' },
+        { duration: 100 },
+        opts,
+        (err, docs) => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(docs)
+          }
+        }
+      )
     })
+
+   
   })
   .catch((error) => {
     console.error('Error connecting to the database', error)

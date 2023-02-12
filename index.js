8ff712a518eab8e3d4995939656696f27a15e3fa
Vanya Mark <markova.vanya@gmail.com>
2023-02-12 19:07:13 +0100

    solved lab

--- index.js
+++ index.js
@@ -1,23 +1,99 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // Create a recipe
+    const banitza = {
+      title: "Banitza",
+      level: "Amateur Chef",
+      ingredients: ["feta cheese", "yogurt", "dough", "eggs"],
+      cuisine: "Bulgarian",
+      dishType: "breakfast",
+      duration: 30,
+      creator: "Vanya",
+    };
+
+    Recipe.create(banitza).then(console.log(banitza.title));
+    Recipe.insertMany(data)
+      .then(
+        data.forEach((element) => {
+          console.log(element.title);
+        })
+      )
+      .then(() => {
+        Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+        .then(console.log("update successful"))
+      })
+      .then(() => {
+        Recipe.deleteOne({title: "Carrot Cake"})
+        .then(() => {
+          console.log("deleted successfully")
+          mongoose.connection.close()
+        })
+      })
   })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  });
+
+/*
+    // SUBARNA'S CODE ------->
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Moldy Raw Chicken",
+      level: "Amateur Chef",
+      ingredients: [
+        "expired Chicken",
+        "Rat Poison",
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef Diogo"
+    })
+  })
+
+  .then(() => {
+    Recipe.insertMany(data)
+    .then((recipe) => {
+      for(let item of recipe){
+        console.log(item.title)
+      }
+    })
+    .then(()=>{
+      Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+      .then(() => {
+        console.log("update successful")
+      })
+    })
+    .then(()=>{
+      Recipe.deleteOne({title: "Carrot Cake"})
+      .then(() => {
+        console.log("deletion successful")
+        mongoose.connection.close();
+      })
+    })
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+*/
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,31 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+   //how to say it's required and unique ?
+  level: { type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"] },
+  ingredients: {type: [ String ]},
+  cuisine: {
+    type: String,
+  },
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: { type: Number, min: 0 },
+  creator: String,
+  created: {
+    type: Date, 
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

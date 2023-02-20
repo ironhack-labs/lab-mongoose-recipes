a2aa3873eb61492f5c5402bd89fdbeee0e9e27eb
Mahmoud Laban <mahmoud.laban@gmail.com>
2023-02-20 01:28:16 +0000

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,42 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    const newRecipe = {
+        title: 'my recipe',
+        level: 'expert',
+        ingredients: ['beans', 'tomato', 'cheese', 'lettuce'],
+        cuisine: 'english',
+        dishType: 'breakfast',
+        image: 'https://iamafoodblog.b-cdn.net/wp-content/uploads/2019/02/full-english-7355w-2-1024x683.webp',
+        duration: 60,
+        creator: 'Mahmoud Laban',
+        created: '2023-02-20'
+      };
+      return Recipe.create(newRecipe);
+    })
+    .then((recipe) => {
+      console.log(`new recipe: ${recipe.title}`);
+      return Recipe.insertMany(data);
+    })
+    .then((recipes) => {
+      console.log(`Add ${recipes.length} recipes to database:`);
+      recipes.forEach((recipe) => {
+        console.log(recipe.title);
+      });
+      return Recipe.findOneAndUpdate(
+        { title: 'my recipe' },
+        { duration: 60 },
+        { new: true }
+      );
+    })
+    .then((updatedRecipe) => {
+      console.log(`Update duration of "${updatedRecipe.title}" to ${updatedRecipe.duration} 20.`);
+      return Recipe.deleteOne({ title: 'my recipe' });
+    })
+    .then(() => {
+      console.log('removed recipe from database.');
+      mongoose.connection.close();
+    })
+    .catch((error) => {
+      console.error('Error connecting to the database', error);
+    });

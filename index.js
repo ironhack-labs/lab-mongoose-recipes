e72a778c1f596b5d672b9a12f0f276804920e14d
denefi <denefi@posteo.de>
2023-03-16 17:16:28 +0100

    solve it 1 and 2

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const spaghettiBolognese = {
+      title: "Spaghetti Bolognese",
+      level: "easy peasy",
+      ingredients: ["pasta", "tomato sauce", "minced meat"],
+      cuisine: "Italian",
+      dishType: "main_course",
+      duration: 20,
+      creator: "The italian people",
+    };
+    return Recipe.create(spaghettiBolognese);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

568576fa405b4de321a3aefdc82d30b1fa655575
Alex Uceda <alexucedacintas@gmail.com>
2023-03-16 17:59:10 +0100

    All done with bonus

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
@@ -19,33 +18,56 @@ mongoose
     // Run your code here, after you have insured that the connection was made
 
     console.log(response);
-
-    const data = {
-      title: "Asian Glazed Chicken Thighs",
-      level: "Amateur Chef",
-      ingredients: [
-        "1/2 cup rice vinegar",
-        "5 tablespoons honey",
-        "1/3 cup soy sauce (such as Silver Swan®)",
-        "1/4 cup Asian (toasted) sesame oil",
-        "3 tablespoons Asian chili garlic sauce",
-        "3 tablespoons minced garlic",
-        "salt to taste",
-        "8 skinless, boneless chicken thighs",
-      ],
-      cuisine: "Asian",
-      dishType: "main_course",
-      image:
-        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-      duration: 40,
-      creator: "Chef LePapu",
+// Iteration2:
+   const myData = {
+    title: "Pizza",
+    level: "Amateur Chef",
+    ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs",
+    ],
+    cuisine: "Asian",
+    dishType: "main_course",
+    image:"https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    duration: 40,
+    creator: "Chef LePapu",
     };
 
-    return Recipe.create(data);
+    return Recipe.create(myData);
+
+  
   })
   .then((response) => {
+    // Iteration3:
     console.log(response.title);
+    return Recipe.insertMany(data);
+  })
+
+  .then((response) => {
+    response.forEach(element => {
+      console.log(element.title);
+    });
+    return Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration:100})
+  })
+  .then((response) => {
+    console.log("success!!");
+    console.log(`${response.title} has been updated`)
+    return Recipe.deleteOne({title:"Carrot Cake"})
+  })
+  .then((response)=>{
+    console.log(`you just remove a Recipe`)
+    return mongoose.connection.close()
+  })
+  .then((response)=>{
+    console.log("connection was closed")
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
+  

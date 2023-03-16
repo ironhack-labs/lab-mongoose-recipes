1ae1571643031a59525ccc99e36c94efd1f812ee
Hugo Silva <dasilvapapis@protonmail.com>
2023-03-16 16:42:33 +0000

    all the iterations completed

--- index.js
+++ index.js
@@ -17,7 +17,45 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const myNewRecipe = {
+    title: "Asian Glazed Spaghetti Thighs",
+    level: "UltraPro Chef",
+    ingredients: [
+      "2 cup rice vinegar",
+      "10 tablespoons honey",
+      "2/3 cup soy sauce (such as Silver Swan®)",
+      "1/2 cup Asian (toasted) sesame oil",
+      "6 tablespoons Asian chili garlic sauce",
+      "6 tablespoons minced garlic",
+      "2 salt to taste",
+      "16 skinless, boneless chicken thighs"
+    ],
+    cuisine: "Japanesse",
+    dishType: "soup",
+    image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    duration: 80,
+    creator: "Eric&Hugo"
+    };
+    return Recipe.create(myNewRecipe)
+  })
+  .then(responseFromMongoose =>{
+    console.log(responseFromMongoose.title)
+    return Recipe.insertMany(data)
+  })
+  .then(responseFromBD =>{
+    const condition = {title: "Rigatoni alla Genovese"}
+    return Recipe.findOneAndUpdate(condition,{$set:{duration:100}})
+  })
+  .then(responseFromUpdate =>{
+    return Recipe.deleteOne({title:"Carrot Cake"})
+  })
+  .then(responseFromDelete =>{
+    console.log("Deleted!")
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+ 

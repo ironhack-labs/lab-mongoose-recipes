323cc413a9f4e401d694c15c50ec0b227b5c62bd
Pim Blom <pim.blom@icloud.com>
2023-03-16 17:57:00 +0100

    iteration 2

--- index.js
+++ index.js
@@ -14,9 +14,36 @@ mongoose
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
+
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe= [
+    {
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+          "1/2 cup rice vinegar",
+          "5 tablespoons honey",
+          "1/3 cup soy sauce (such as Silver Swan®)",
+          "1/4 cup Asian (toasted) sesame oil",
+          "3 tablespoons Asian chili garlic sauce",
+          "3 tablespoons minced garlic",
+          "salt to taste",
+          "8 skinless, boneless chicken thighs"
+        ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu"
+    }]
+    
+    return Recipe.create(newRecipe)
+    
+
+  })
+  .then((createdRecipe)=> {
+    console.log(createdRecipe.title)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

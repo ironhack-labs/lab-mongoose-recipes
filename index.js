03d1bb5569f1bb8c1f228a6c094557210d50794e
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-04-01 00:09:12 +0200

    Done

--- index.js
+++ index.js
@@ -21,3 +21,65 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+// iteration 2
+
+let newRecipe = {
+  title: 'Spaghetti Bolognese',
+  level: 'Amateur Chef',
+  ingredients: ['speghetti', 'meat', 'tomatoes'],
+  cuisine: 'Italian',
+  dishType: 'main_course',
+  image:
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/the-best-spaghetti-bolognese-7e83155.jpg?quality=90&webp=true&resize=300,272',
+  duration: 15,
+  creator: 'RS'
+};
+
+Recipe
+  .create(newRecipe)
+  .then(result => console.log(`recipe added: ${result.title}`))
+  .catch(err => console.log(err));
+
+// iteration 3
+
+Recipe
+
+  .insertMany(data)
+  .then(result => {
+    result.forEach(item => {
+      console.log(`recipe for ${item.title} inserted successfully`);
+    });
+  })
+  .catch(err => console.log(err));
+
+// iteration 4
+
+Recipe
+
+  .updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+  .then(() => console.log(`The recipe is updated`))
+  .catch(err => console.log(err));
+
+// iteration 5
+
+Recipe
+
+  .deleteOne({ title: 'Carrot Cake' })
+  .then(() => console.log(`The recipe is deleted`))
+  .catch(err => console.log(err));
+
+// iteration 6
+
+mongoose.connection
+  
+  .close()
+  .then(() => console.log(`connection closed`))
+  .catch(err =>
+    console.log(
+      `an error while closing database connection has occurred: ${err}`
+    )
+  );
+  

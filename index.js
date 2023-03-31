e63cc78818ef270a702b05bb9b9ec2921f4bb605
Wiebke Lueneburg <git@fraulueneburg.de>
2023-03-31 14:28:00 +0200

    solved iteration 2

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
+const hotChocolate = {
+  title: 'Hot Chocolate',
+  level: 'Easy Peasy',
+  ingredients: [ 'milk', 'cocoa powder' ],
+  cuisine: 'international',
+  dishType: 'drink',
+  image: 'https://images.unsplash.com/photo-1608735484559-50aa06c9478c' ,
+  duration: '5',
+  creator: 'probably the same that invented fire. or cocoa',
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -17,6 +29,10 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create(hotChocolate);
+  })
+  .then(() => {
+    console.log(hotChocolate.title);
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

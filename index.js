c0a5880967efbb6c8b71dea492582b3cfa4f3b76
Nicolas_M <nicolas.meot@live.fr>
2023-04-08 10:34:02 +0200

    first iteration

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
+
 // Import of the data from './data.json'
 const data = require('./data');
 
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+let date = new Date()
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title : String,
+  level : {
+    type : String,
+    enum : ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients : [ String ],
+  cuisine : {
+    type : String,
+    required : true
+  },
+  dishType : {
+    type : String,
+    enum : ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+  },
+  image : {
+    type : String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration : {
+    type : String,
+    min : 0
+  },
+  creator : String,
+  created : {
+    type : Date,
+    default : date
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

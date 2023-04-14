80c4fc10b21c0c809a88b2373167b8775d12c864
Diana Kalstein <dianakalstein@gmail.com>
2023-04-14 15:11:36 +0200

    Finished Lab

--- index.js
+++ index.js
@@ -5,19 +5,56 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+    console.log(`Connected to the database: ${x.connection.name}`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+    "title": "Asian Glazed Chicken Thighs",
+    "level": "Amateur Chef",
+    "ingredients": [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+    "cuisine": "Asian",
+    "dishType": "main_course",
+    "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    "duration": 40,
+    "creator": "Chef LePapu"})
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then(()=> {
+    Recipe.insertMany(data)
+    .then(() => { 
+      return Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100 }, { new: true });
+    })
+    .then(updatedRecipe => {
+      console.log(updatedRecipe);
+      return Recipe.findOneAndDelete({ title: "Carrot Cake" });
+    })
+    .then(deletedCake => {
+      console.log(deletedCake);
+    })
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    })
+    .finally(() => {
+      // Close connection
+      mongoose.connection.close()
+    })
+    
+  })  
+    
+    

f39e0ff5e9e758094781b2c155f850488cdf131c
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2023-04-14 16:26:51 +0200

    lab ready

--- index.js
+++ index.js
@@ -16,8 +16,30 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({ title: "New Recipe" , cuisine: "italian"})
+    .then(createdRecipe => console.log(createdRecipe))
+    .catch(err => console.log(err))
+
+    Recipe.insertMany(data)
+      .then(createdRecipe => console.log(createdRecipe))
+      .catch(err => console.log(err))
+
+    Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 220})
+    .then(createdRecipe => console.log(createdRecipe))
+    .catch(err => console.log(err))
+
+    Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100}, { new: true })
+    .then(newRecipe => console.log(newRecipe))
+    .catch(err => console.log(err))
+
+    Recipe.findOneAndDelete({ title: "Carrot Cake" })
+    .then(deletedRecipe => console.log(deletedRecipe))
+    .catch(err => console.log(err))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally(() => {
+		// Close connection
+		mongoose.connection.close()
+  })

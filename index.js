f55fac7047fc50205d7a28bd49c8b215939964a1
Hector Riveros <89.hars@gmail.com>
2023-04-21 01:17:13 +0200

    not finish

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', false);
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,8 +16,12 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    let Reseta = {title: 'dish', level:'Easy Peasy', cousine: 'stuff'}
+    return Recipe.create(Reseta)
+    
+
   })
+  .then((newRecepi)=>{console.log (newRecepi)}) 
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

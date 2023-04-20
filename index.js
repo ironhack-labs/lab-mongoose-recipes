7bbd80fe998326d63a2c1b83cef514f495a4da8a
Ian Mason <ianmason5@googlemail.com>
2023-04-20 15:07:25 +0100

    Done

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://0.0.0.0:27017/recipe-app";
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "test",
+      level: "Amateur Chef",
+      ingredients: ["test"],
+      cuisine: "test",
+      dishType: "main_course",
+      duration: 30,
+      creator: "Chef Test",
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((recipe) => {
+    console.log(`Recipe created: ${recipe.title}`);
+    return Recipe.insertMany(data);
+  })
+  .then((recipes) => {
+    recipes.forEach((recipe) => {
+      console.log(`Recipe created: ${recipe.title}`);
+    });
+
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then((updatedRecipe) => {
+    console.log(
+      `Recipe: ${updatedRecipe.title} Duration: ${updatedRecipe.duration}`
+    );
+
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((deleteResult) => {
+    console.log("Carrot Cake removed successfully:", deleteResult);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  })
+  .finally(() => {
+    mongoose.connection.close().then(() => {
+      console.log("Database connection closed.");
+    });
   });

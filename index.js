7441dcfaf3cd3f2ff42d46e714a18d2cba10b86e
Victor Villalba Besada <129146894+Hieronimous@users.noreply.github.com>
2023-05-03 16:47:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 const mongoose = require('mongoose');
 
-// Import of the model Recipe from './models/Recipe.model.js'
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'hbs')
+
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
+
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
-// Connection to the database "recipe-app"
+
+
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe
+      .create({ title: "Pasta con huevo y ketchup", level: 'Easy Peasy', ingredietns: "Pasta, huevos, ketchup", cuisine: "Italian", dishType: 'main_course', image: "https://img-global.cpcdn.com/recipes/1c5ce9956b195e58/640x640sq70/photo.webp", duration: 20, creator: "Jaime", created: 2023 / 01 / 06 })
+
+  })
+  .then(() => {
+    return Recipe
+      .create(data)
+
+  })
+  .then(() => {
+    return Recipe
+      .findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+  })
+  .then(() => {
+    return Recipe.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+app.listen(5005, () => console.log('SERVIDOR LEVANTADO EN 5005'))
+

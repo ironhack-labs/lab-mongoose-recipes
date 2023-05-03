b40cc9dd348fd698f4d9d6c43b23664dbbbc4373
lortes <lortes95@gmail.com>
2023-05-03 16:47:44 +0200

    Done Fernando y Lorena

--- index.js
+++ index.js
@@ -13,11 +13,60 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
+
     return Recipe.deleteMany()
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    return Recipe.create({
+      title: "Asian Glazed Chicken NUDDLES",
+      level: "Amateur Chef",
+      ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+     cuisine: "Asian",
+     dishType: "main_course",
+     image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+     duration: 40,
+     creator: "Chef LePapu" 
+    })
+
+    
+  })
+  .then((firstRecipe) =>{
+     console.log("The title of the first recipe is", firstRecipe.title)
+  })
+  .then(()=>{
+   return Recipe.insertMany(data)
+  })
+  .then((recipesFromBD)=>{
+    recipesFromBD.forEach((eachRecipe)=>{
+      console.log("TITULO DE RECETA-----------",eachRecipe.title)
+    })
+  })
+  .then(()=>{
+    console.log("SUCESSSSSSSSSSSSS, SE HA ACTUALIZADO!!!!")
+   return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+  })
+  .then(()=>{
+    console.log("SUCESSSSSSSSSSSSS, SE HA ELIMINADO LA CARROT CAKE!!!!")
+
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(()=>{
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+
+  

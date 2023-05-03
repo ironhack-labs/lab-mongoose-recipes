b4645e39bc831aa415653fc6a4700ed2908cd94f
Carloslt <carloslt.proyectos@gmail.com>
2023-05-03 16:39:11 +0200

    done

--- index.js
+++ index.js
@@ -17,7 +17,54 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe
+      .create({
+        title: "Asian Glazed Chicken Thighs",
+        level: "Amateur Chef",
+        ingredients: [
+          "1/2 cup rice vinegar",
+          "5 tablespoons honey",
+          "1/3 cup soy sauce (such as Silver Swan®)",
+          "1/4 cup Asian (toasted) sesame oil",
+          "3 tablespoons Asian chili garlic sauce",
+          "3 tablespoons minced garlic",
+          "salt to taste",
+          "8 skinless, boneless chicken thighs"
+        ],
+        cuisine: "Asian",
+        dishType: "main_course",
+        image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+        duration: 40,
+        creator: "Chef LePapu"
+      })
+      .then(createdRecipe => {
+        console.log('El titulo de la receta es:', createdRecipe.title)
+      })
+      .catch(err => {
+        console.log('ERROR -->', err)
+      })
   })
+  .then(() => {
+    return Recipe
+      .insertMany(data)
+      .then(() => {
+        data.forEach(elem => {
+          console.log('LOS TITULOS DE LAS RECETAS SON:', elem.title)
+        })
+      })
+      .catch(console.log('ERROR -->', err))
+  })
+
+  .then(() => {
+    return Recipe
+      .findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+      .then(data => console.log("La receta modificada es:", data))
+      .catch(err => console.log('Hubo un error', err))
+  })
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+

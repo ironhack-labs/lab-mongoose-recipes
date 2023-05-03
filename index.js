cf1bca8f813459257badb50526b5ff8e7d3ba403
Belen Sanchez <belensezrami@gmail.com>
2023-05-03 16:36:47 +0200

    lab medio hecho

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,17 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({ 
+      title: 'Popino a la brasa', level: 'UltraPro Chef', 
+      ingredients: ['Fresh Popino Meat', 'onions', 'pepper', 'salt'], 
+      cuisine: 'spanish', dishType: 'main_course', image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 100, creator: 'Germán'
+    }) 
+  })
+  .then(() => {
+    return Recipe.insertMany(data)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  

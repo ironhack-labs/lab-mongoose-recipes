16a60a79292d23762fb68afa5b552580197a5d96
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-05-11 22:21:42 -0400

    Started iteration #3

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  title: {type: String, required: true},
+  title: {type: String, required: true, unique: true},
   level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
   ingredients: {type: [String]},
   cuisine: {type: String, required: true},
@@ -10,9 +10,7 @@ const recipeSchema = new Schema({
   image: {type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
   duration: {type: Number, min: 0},
   creator: String,
-  created: {type: Date, default: Date.now}
-
-});
+  created: {type: Date, default: Date.now}});
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 

0e7ce8c17d2027700509bb9e442899c258b825d7
Bernard Cardoso <be1.0@hotmail.com>
2023-05-25 20:16:45 +0100

    Iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String
+  },
+  level: {
+    type: String
+  },
+  ingredients: {
+    type: [ String ]
+  },
+  cuisine: {
+    type: String
+  },
+  dishType: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  duration: {
+    type: Number
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date
+  },
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

890e5b544dc4eb28b71e86df7ea2f7e3a51df6d9
Patricia <marie.lou.trips@gmail.com>
2023-05-26 22:10:00 +0200

    Add multiple recipes sucessfully

--- index.js
+++ index.js
@@ -12,25 +12,44 @@ mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
+
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
   .then(() =>  Recipe.create ({
     title: "Rote Linsel-Dal", 
     level: "Easy Peasy", 
-    ingredients: "Zwiebeln, Knoblauchzehen, Ingwer, rote Linsen, Koriander oder Petersilie, Kurkuma gemahlen, Koriander gemahlen, Kreuzkümmel gemahlen, Garam Masala, Rapsöl, gehackte Tomaten, Kokosmilch, Limettensaft, Pfeffer",
+    ingredients: [
+      "2 Zwiebeln",
+      "4 Knoblauchzehen",
+      "1 Stk Ingwer",
+      "250g rote Linsen",
+      "1 Bd. Koriander oder Petersilie", 
+      "1 TL Kurkuma gemahlen",
+      "1 TL Koriander gemahlen",
+      "1 TL Kreuzkümmel gemahlen",
+      "1TL Garam Masala",
+      "2 EL Rapsöl", 
+      "200g gehackte Tomaten",
+      "200 ml Kokosmilch",
+      "2 EL Limettensaft", 
+      "Pfeffer und salz",
+    ],
     cuisine: "Orientalish",
     dishType: "main_course",
     image: "https://images.media-allrecipes.com/images/75131.jpg",
     duration: 30,
-    creator: "EatBetter",
+    creator: "Chef Natascha",
     created: Date.now()
-  }) )
+  }))
 
-  .then((printTitle => { 
-  console.log(`Recipe created: ${printTitle.title}`);
-}))
+  .then((printTitle => console.log(`Recipe created: ${printTitle.title}`)))
 
-.catch(error => {
-  console.error('Error generating a recipe', error);
-});
+  .then(() => {
+  return Recipe.insertMany(data)
+  })
+
+  .then((createdmultipleRecipes) => console.log('Multiple recipes added'))
+
+  .catch(err => console.log('Error generating a recipe', err));

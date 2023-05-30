c81bebb9cc51071f1f081694ef81f0ad7f352432
Vialoha <vi62469@gmail.com>
2023-05-30 12:26:50 +0100

    iteration 4 and 5 missing

--- index.js
+++ index.js
@@ -10,14 +10,50 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
+
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.insertMany(data, function (error, recipies){
+      if (error) {
+        console.log(`Error occurred during getting recipes: ${error}`);
+        return;
+      }
+      console.log('Got all the RECIPES Titles!');
+      return recipies.forEach(doc => console.log(` --> recipe title: ${doc.title}`));
+    })
   })
+
+
+  //wasn't able to update the duration - iteration 4
+
+  /* .then (() => {
+    let filter = {title:'Rigatoni alla Genovese'};
+    let update = {duration: 100};
+
+    Recipe.findOneAndUpdate(filter, update, {
+      returnOriginal: true
+    });
+    /*.then(updatetime => console.log('updatetime', updatetime))
+  }) */
+
+
+
+//wasn't able to delete - iteration 5
+
+  /* .then(() => {
+    await Recipe.deleteOne({title: 'Carrot Cake' });
+  }) */
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+
+  mongoose.connection.close()

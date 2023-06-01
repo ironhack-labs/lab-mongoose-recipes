6d0300951e1000d05dc019acc01217ed11923451
Fernando Ramos <fndo.ramos@gmail.com>
2023-06-01 18:00:52 +0200

    add iterations 1 and 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,19 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const recipeOne = {
+      title: "bean soup",
+      level: "Amateur Chef",
+      ingredients: ["beans", "selderij", "carrots", "onions", "olive oil", "tomato juice"],
+      cuisine: "mediterranean",
+      dishType: "soup",
+      duration: 20,
+      creator: "Fernando&Greg"
+    }
+
+    return Recipe.create(recipeOne)
   })
+  .then((response) => console.log(`the title of the recipe is:`, response.title))
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

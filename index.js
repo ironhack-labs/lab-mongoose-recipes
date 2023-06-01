4b873f6d9b9faca8cd172eaf1945bd9e29106296
linhvnde <linh.nh.tyty@gmail.com>
2023-06-01 23:00:08 +0200

    adjusted code to do iteration 3 in a different approach

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -21,46 +21,72 @@ mongoose
     const recipeOne = {
       title: "Curry Chicken",
       level: "UltraPro Chef",
-      ingredients: ["curry sauce", "chicken", "rice", "basil", "pepper", "salt", "onion", "carrots"],
+      ingredients: [
+        "curry sauce",
+        "chicken",
+        "rice",
+        "basil",
+        "pepper",
+        "salt",
+        "onion",
+        "carrots",
+      ],
       cuisine: "India",
       dishType: "main_course",
       image: "/public/images/currychicken.jpeg",
       duration: 60,
       creator: "Jamie Oliver",
-      created: '2023-06-01',
-    }
+      created: "2023-06-01",
+    };
     return Recipe.create(recipeOne);
   })
   .then((recipeOne) => {
-    console.log(recipeOne.title)
+    console.log(recipeOne.title);
     //Iteration 3
     return Recipe.insertMany(data);
   })
-  .then (() => {
-    return Recipe.find({},{
-      title: 1
-    })
+  .then((recipeArr) => {
+    recipeArr.forEach((recipe) => {
+      console.log(recipe.title);
+    });
+    //
+    //
+    //at first, we tried the below because the forEach didnt work
+    /*
+    return Recipe.find(
+      {},
+      {
+        title: 1,
+      }
+    );
   })
-  .then ((result) => {
+  .then((result) => {
     console.log(result);
+    */
+    
+    //
+    //
     // Iteration 4
-    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"},{
-       duration: 100
-    })
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      {
+        duration: 100,
+      }
+    );
   })
-  .then (() => {
-    console.log("Rigatoni is done faaaast")
+  .then(() => {
+    console.log("Rigatoni is done faaaast");
     //Iteration 5
-    return Recipe.deleteOne({title: "Carrot Cake"})
-})
-  .then (() => {
-  console.log("Carrot cake all gone")
-  //Iteration 6
-  mongoose.connection.close()
-}) 
-.then (() => {
-  console.log("We are closed, come by tomorrow")
-})
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log("Carrot cake all gone");
+    //Iteration 6
+    mongoose.connection.close();
+  })
+  .then(() => {
+    console.log("We are closed, come by tomorrow");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });

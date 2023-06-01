605d5d064e166f5430364fe1d7cc10afad41ea8a
Emily Stevenson <e.stevenson6@hotmail.co.uk>
2023-06-01 17:38:40 +0100

    done - Jani and Emily

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,8 +17,38 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+
+    const newRecipe = {
+      title: "Vegan Thai Curry",
+      level: "Easy Peasy",
+      ingredients: "Vegan stuff",
+      cuisine: "Vegan Thai",
+    }
+    return Recipe.create(newRecipe)
     // Run your code here, after you have insured that the connection was made
+  })
+  .then((newRecipe) => {
+    console.log(newRecipe.title)
+    return Recipe.insertMany(data)
+  })
+  .then((dataDb) => {
+    dataDb.forEach((element) => {
+      console.log(element.title)
+    })
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+    }) 
+  .then(() => {
+    console.log("You have updated the duration of Rigatoni")
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(() => {
+    console.log("You have run out of Carrot Cake")
+    mongoose.connection.close()
+  })
+  .then(() => {
+    console.log("database is closed")
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+

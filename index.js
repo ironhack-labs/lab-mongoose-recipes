886df0af0f32aa9dc4474e1a2853b974cc4001ec
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-06-03 08:48:26 -0600

    Done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,37 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const oneRecipe = {
+      title: "chocolate cookies",
+      level: "Easy Peasy",
+      ingredients: [""],
+      cuisine: "american",
+      dishType: "breakfast",
+      image: "",
+      duration: 1,
+      creator: "chef Fabio",
+    };
+
+    return Recipe.create(oneRecipe);
+  })
+  .then((recipe) => {
+    console.log(recipe.title);
+  })
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then((array) => {
+    array.forEach((element) => {
+      console.log(element.title);
+    });
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  

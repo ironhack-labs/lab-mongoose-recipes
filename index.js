4643e8653f3f5774a952d3ba9d947e77c3967492
Vialoha <vi62469@gmail.com>
2023-06-03 14:48:11 +0100

    not yet

--- index.js
+++ index.js
@@ -8,37 +8,26 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI)
+        .then(x => console.log(`Connected to the database: "${x.connection.name}"`))
+                Recipe.deleteMany()
+        .catch(err => {console.error('Error connecting to the database', err)});
 
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
 
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    Recipe.insertMany(data, function (recipies){
-      console.log('Got all the RECIPES Titles!');
-      return recipies;
-    })
-  })
+  Recipe.insertMany(data)
+    .then(data => {console.log(data)})
+    .then(recipes => console.log(recipes))
+    .catch(error => console.log('no recipes added'));
 
-  //wasn't able to update the duration - iteration 4
-
-  .then (() => {
-    Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, {duration: 100}, {new: true})
-  })
   
-  .then(updatetime => console.log('updatetime', updatetime))
+  Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, {duration: 100}, {new:true})
+    .then(updatetime => console.log('updatetime', updatetime))
+    .catch(error => console.log('Not updated recipe'));
 
-//wasn't able to delete - iteration 5
 
-  /* .then(() => {
-    await Recipe.deleteOne({title: 'Carrot Cake' });
-  }) */
-
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  })
+  Recipe.deleteOne({title: 'Carrot Cake'})
+    .then(deleted => console.log('deleted', deleted))
+    .catch(error => console.log('Not deleted recipe'));
+  
+// It is not updating the Rigatoni recipe duration on MongoDB
+// And neather deleting the carrot Cake 

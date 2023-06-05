eea48324e81526221ee8a7488b533e7790bef559
churrundo <churrundo@gmail.com>
2023-06-05 11:10:20 -0600

    iterations 1 and 2

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+const newRecipe = {
+  title: 'Chocolate Chip Cookies',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup light brown sugar', '1 large egg', '2 tablespoons milk', '1 1/4 teaspoons vanilla extract', '2 cups semisweet and/or milk chocolate chips'],
+  cuisine: 'American',
+  dishType: 'snack',
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 30,
+  creator: 'Chef Jennifer'
+};
+
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -16,7 +27,12 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create(newRecipe)
+  })
+  .then(recipe => {
+    console.log(`Recipe created: ${recipe.title}`);
+    // Close the DB connection when done
+    mongoose.connection.close();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

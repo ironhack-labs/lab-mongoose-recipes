b1ff7a4ee3ba9685d020e781bf038ab166bf6e08
alexanderley <alexander.ley.inbox@gmail.com>
2023-06-23 15:27:26 +0200

    Lab Done

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-// Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
+  .connect("mongodb://127.0.0.1:27017/recipes")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
+
+// Creates a new Recipe, using the data from ou json file
+//  1. Inser new Recipe to data base
+// const newRecipe = new Recipe(data[0]);
+// for (let i = 0; i < data.length; i++) {
+//   const newRecipe = new Recipe(data[i]);
+//   newRecipe
+//     .save()
+//     .then((savedRecipe) => console.log("savedRecipe: ", savedRecipe))
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// -----------------------------------------------------
+
+//  1. Insert new Recipe to data base
+// Recipe.insertMany(data)
+//   .then((resp) => console.log(resp))
+//   .catch((err) => console.log(err));
+
+// -----------------------------------------------------
+
+// 2. New Duraiton
+// Recipe.updateOne(
+//   { title: "Rigatoni alla Genovese" },
+//   { $set: { duration: 100 } }
+// ).then(() => {
+//   console.log("NEW DURATION");
+// });
+
+// -----------------------------------------------------
+
+// 3. Delete one element from the database
+// Recipe.deleteOne({ title: "Carrot Cake" }).then(() => {
+//   console.log("Element got deleted");
+// });
+
+// 4.Close the database
+mongoose.connection
+  .close()
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log("Database connection closed successfully");
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error closing database connection:", error);
   });

410b9b0c8f35ebd74db7aebc08d567744747fdb5
Sirkem112 <meingithub01@gmail.com>
2023-06-23 17:46:41 +0200

    in progress

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const recipesData = require("./data.json");
+
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const recipeDetails = {
+      title: "New Recipe",
+      description: "Delicious Egusi",
+      cuisine: "italian",
+      ingredients: [
+        "Redoil",
+        "water",
+        "fish",
+        "meat",
+        "pepper",
+        "spices",
+        "Egusi",
+        "salt",
+      ],
+      instructions: "put water in the pot, mix the egusi with pepper and salt",
+    };
+
+    Recipe.create(recipeDetails).then((createdRecipe) => {
+      console.log(`Inserted recipe: ${createdRecipe.title}`);
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+Recipe.insertMany(recipesData)
+  .then((createdRecipes) => {
+    createdRecipes.forEach((recipe) => {
+      console.log(`Inserted recipe: ${recipe.title}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(3000, () => {
+  console.log("Engine started");
+});

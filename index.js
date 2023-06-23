4ab97fa1c2f917a1dd9ee4ab64dface6c93e660f
Andy Cockroft <acockroft1994@gmail.com>
2023-06-23 15:48:03 +0200

    lab complete

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.deleteOne({ title: "Carrot Cake" }).then(() =>
+      console.log("DELETED")
+    );
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+mongoose.connection.close();
+/* INSERT ONE RECIPE
+const newRecipe = new Recipe(data[2]);
+
+Recipe.create(newRecipe)
+  .then((resp) => console.log(resp))
+  .catch((err) => console.log(err));
+*/
+
+/* INSERT ARRAY
+Recipe.insertMany(data)
+  .then((resp) => console.log(resp))
+  .catch((err) => console.log(err));
+*/
+
+/* UPDATE DURATION
+Recipe.updateOne(
+  { title: "Rigatoni alla Genovese" },
+  {
+    $set: { duration: 100 },
+  }
+).then(() => console.log("NEW DURATION"));
+*/
+/* DELETE RECIPE
+Recipe.deleteOne({ title: "Carrot Cake" }).then(() => console.log("DELETED"));
+*/

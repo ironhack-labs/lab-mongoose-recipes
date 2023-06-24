0f5cca36b4c30ff56fb7f4bee0f98a906349a263
Sirkem112 <meingithub01@gmail.com>
2023-06-24 16:58:17 +0200

    solved mogoose recipe lab

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
 const mongoose = require("mongoose");
-const express = require("express");
-const app = express();
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
 const data = require("./data");
 
-const recipesData = require("./data.json");
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
+let newRecipe = {
+  title: "miXto quente",
+  level: "Easy Peasy",
+  ingredients: ["pão francês", "queijo", "presunto"],
+  cuisine: "Brasileira",
+  dishType: "snack",
+  image:
+    "http://culinaria.culturamix.com/blog/wp-content/gallery/misto-quente-3/Misto-Quente-6.jpg",
+  duration: 5,
+  creator: "JOC",
+};
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
+    // Before adding any documents to the database, let's delete all previous entries
     return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-    const recipeDetails = {
-      title: "New Recipe",
-      description: "Delicious Egusi",
-      cuisine: "italian",
-      ingredients: [
-        "Redoil",
-        "water",
-        "fish",
-        "meat",
-        "pepper",
-        "spices",
-        "Egusi",
-        "salt",
-      ],
-      instructions: "put water in the pot, mix the egusi with pepper and salt",
-    };
-
-    Recipe.create(recipeDetails).then((createdRecipe) => {
-      console.log(`Inserted recipe: ${createdRecipe.title}`);
-    });
+    // iteration 2. newRecipe will be created in our DB
+    return Recipe.create(newRecipe); // returning the promise allows us to resolve it in the next .then(), instead of nested promises.
   })
-  .catch((error) => {
-    console.error("Error connecting to the database", error);
-  });
-
-Recipe.insertMany(recipesData)
-  .then((createdRecipes) => {
-    createdRecipes.forEach((recipe) => {
-      console.log(`Inserted recipe: ${recipe.title}`);
-    });
+  .then((result) => {
+    console.log(`recipe added: ${result.title}`); // a console.log to verify previous promise resolved correctly
+    // iteration 3. all recipes in data will be inserted in our DB
+    return Recipe.insertMany(data);
   })
-  .catch((error) => {
-    console.log(error);
-  });
+  .then((result) => {
+    result.forEach((item) =>
+      console.log(`recipe for ${item.title} inserted successfully`)
+    ); // a console.log to verify previous promise resolved correctly
+    // iteration 4. To find a recipe by its title and update one attribute
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then((result) => {
+    console.log(`The recipe ${result.title} is updated`); // a console.log to verify previous promise resolved correctly
+    // iteration 5. To find a recipe by its title and delete it
+    return Recipe.findOneAndDelete({ title: "Carrot Cake" });
+  })
+  .then((result) => {
+    console.log(`The recipe ${result.title} is deleted`); // a console.log to verify previous promise resolved correctly
+    // iteration 6. To close the DB.
+    return mongoose.connection.close();
+  })
+  .then(() => console.log(`connection closed`)) // a console.log to verify previous promise resolved correctly
+  .catch((err) => console.log(`an error has occurred: ${err}`)); // to catch errors in promises
 
 app.listen(3000, () => {
   console.log("Engine started");

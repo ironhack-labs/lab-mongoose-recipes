553ca6e8a4346507a812a4d68f51bbe88948db74
Biljana Kotevska <biljanakotev@gmail.com>
2023-07-06 23:10:36 +0200

    updated the newRecipe variable to extract the data directly from the data variable that contains the import data from the ./data.json file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
 const data = require("./data");
+console.log(data.length);
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
@@ -16,25 +17,15 @@ mongoose
     return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
     const newRecipe = {
-      title: "Asian Glazed Chicken Thighs",
-      level: "Amateur Chef",
-      ingredients: [
-        "1/2 cup rice vinegar",
-        "5 tablespoons honey",
-        "1/3 cup soy sauce (such as Silver Swan®)",
-        "1/4 cup Asian (toasted) sesame oil",
-        "3 tablespoons Asian chili garlic sauce",
-        "3 tablespoons minced garlic",
-        "salt to taste",
-        "8 skinless, boneless chicken thighs",
-      ],
-      cuisine: "Asian",
-      dishType: "main_course",
-      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-      duration: 40,
-      creator: "Chef LePapu",
+      title: data[0].title,
+      level: data[0].level,
+      ingredients: data[0].ingredients,
+      cuisine: data[0].cuisine,
+      dishType: data[0].dishType,
+      image: data[0].image,
+      duration: data[0].duration,
+      date: data[0].creator,
     };
     return Recipe.create(newRecipe);
   })

51060c4569af66f62a7f68a18e35e0fa8d0f21f8
Biljana Kotevska <biljanakotev@gmail.com>
2023-07-07 20:00:21 +0200

    done with iteration 4

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
 const data = require("./data");
-console.log(data.length);
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
@@ -18,20 +18,47 @@ mongoose
   })
   .then(() => {
     const newRecipe = {
-      title: data[0].title,
-      level: data[0].level,
-      ingredients: data[0].ingredients,
-      cuisine: data[0].cuisine,
-      dishType: data[0].dishType,
-      image: data[0].image,
-      duration: data[0].duration,
-      date: data[0].creator,
+      title: "Strawberry Cake",
+      level: "Amateur Chef",
+      ingredients: ["2 cups sugar", "1 cup butter", "4 large eggs", "2 cups flour", "1 cup milk", "1/2 cup strawberries"],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu",
     };
+
     return Recipe.create(newRecipe);
   })
   .then((createdRecipe) => {
     console.log("Recipe title:", createdRecipe.title);
   })
+  .then(() => {
+    Recipe.insertMany(data);
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].title);
+    }
+  })
   .catch((error) => {
     console.error("Error connecting to the database", error);
+  });
+
+// Recipe.insertMany(data)
+//   .then((recipes) => {
+//     console.log(recipes);
+
+//     for (let i = 0; i < data.length; i++) {
+//       console.log(data[i].title);
+//     }
+//   })
+// .catch((err) => {
+//   console.log(err);
+// });
+
+Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 }, { new: true })
+  .then((updatedRecipe) => {
+    console.log("UpdatedRecipe: Rigatoni alla Genovese", updatedRecipe.duration);
+  })
+  .catch((err) => {
+    console.log(err);
   });

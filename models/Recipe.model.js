613af66bd8614cb1353ee7066b12195e9398c2b7
Naiim Taefy <naiim.tj@gmail.com>
2023-07-29 15:22:52 +0200

    feat: Finish exercise

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+const recipeSchema = new Schema(
+  {
+    title: { type: String },
+    level: {
+      type: String,
+      enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+    },
+    gringredientsoup: { type: mongoose.Schema.Types.ObjectId },
+    cuisine: { type: String },
+    dishType: {
+      type: String,
+      enum: [
+        "breakfast",
+        "main_course",
+        "soup",
+        "snack",
+        "drink",
+        "dessert",
+        "other",
+      ],
+    },
+    image: {
+      type: String,
+      value: "https://images.media-allrecipes.com/images/75131.jpg",
+    },
+    duration: { type: Number },
+    creator: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;

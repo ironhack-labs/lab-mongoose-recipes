78ae0f60cee7d21299693f1a70dda078aa3cd97e
Carlos A Martin Aparicio <carlosktm08@gmail.com>
2023-08-01 21:19:49 +0200

    Feat: lab recipes Carlos Martin

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
+    console.log(`Connected to the database: "${mongoose.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data);
   })
+
+  .then((newRecipe) => {
+    console.log('Recipe added:', newRecipe);
+  })
+
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration : 100 },
+      { new: true}
+    );
+  })
+
+  .then((updateRecipe) => {
+    console.log('Receta eliminada con exito', updateRecipe);
+    return Recipe.deleteOne( {name: 'Carrot Cake'} )
+  })
+
+  .then(() => {
+    console.log('Receta actualiza con exito');
+    mongoose.connection.close();
+  })
+
+  
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

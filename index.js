c2130e8bea9a6759eae8183902d5ea819eb7ad46
Irene Buceta <irenebuzalocen@gmail.com>
2023-08-02 15:47:48 +0200

    Irene y Fer casi done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -16,8 +17,38 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+    Recipe
+      .create({ title: "donuts", level: "Amateur Chef", ingredients: ["harina", "agua", "azucar", "huevos", "chocolate"], cuisine: "american", dishType: "snack", duration: 15, creator: "us" })
+      .then(createdRecipe => {
+        console.log('el donut esta aqui', createdRecipe)
+      })
+      .catch(err => {
+        console.log('ERROR', err)
+      })
     // Run your code here, after you have insured that the connection was made
   })
+  .then(() => {
+    Recipe
+      .insertMany(data)
+      .then(dataBase => {
+        console.log('la data esta aqui', dataBase)
+      })
+      .catch(err => {
+        console.log('ERROR', err)
+      })
+    // Run your code here, after you have insured that the connection was made
+  })
+  .then(() => {
+    Recipe
+      .findByIdAndUpdate('64ca5a04fa441cb8bc187b14', { duration: 100 }, { new: true })
+      .then(rigatoniUpdate => {
+        console.log('rigatoni succes', rigatoniUpdate)
+      })
+      .catch(err => {
+        console.log('ERROR', err)
+      })
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

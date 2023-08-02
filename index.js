01312070e9f627af45deca462565d0eea9b0102f
Lianshuang PUCHEU <lianshuang.pucheu@gmail.com>
2023-08-02 11:08:12 +0200

    Completed lab

--- index.js
+++ index.js
@@ -17,7 +17,50 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title: 'Delicious Pancakes',
+      level: 'Easy Peasy',
+      ingredients: ['flour', 'milk', 'eggs', 'sugar', 'baking powder'],
+      cuisine: 'Breakfast',
+      dishType: 'breakfast',
+      image: 'https://example.com/pancakes.jpg',
+      duration: 20,
+      creator: 'John Doe'
+    };
+
+    return Recipe.create(newRecipe);
   })
+
+  .then(createdRecipe => {
+    console.log(`Added recipe: ${createdRecipe.title}`);
+    return Recipe.insertMany(data);
+  })
+
+  .then(insertedRecipes => {
+    insertedRecipes.forEach(recipe => {
+      console.log(`Inserted recipe: ${recipe.title}`);
+    });
+
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 }
+    );
+  })
+  .then(updatedRecipe => {
+    console.log(`Updated duration of recipe: ${updatedRecipe.title}`);
+
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('Removed "Carrot Cake" recipe');
+    
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    console.log('Database connection closed.');
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
+    mongoose.connection.close();
   });

eb71b6a4fe281c8a09ab97f3079173b324120926
Johanna Geßner <johanna-gessner@web.de>
2023-08-24 18:18:24 +0200

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,7 +16,49 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title: "Carrot Cake",
+      level: "Amateur Chef",
+      ingredients: [
+        "6 cups grated carrots",
+        "1 cup brown sugar",
+        "1 cup raisins",
+        "4 eggs",
+        "1 1/2 cups white sugar",
+        "1 cup vegetable oil",
+        "2 teaspoons vanilla extract",
+        "1 cup crushed pineapple, drained",
+        "3 cups all-purpose flour",
+        "1 1/2 teaspoons baking soda",
+        "1 teaspoon salt",
+        "4 teaspoons ground cinnamon"
+      ],
+      cuisine: "International",
+      dishType: "dessert",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/3605684.jpg",
+      duration: 130,
+      creator: "Chef Nadia"
+    };
+    console.log(`${newRecipe.title}`)
+    return Recipe.create(newRecipe);
+  })
+  .then(() => {
+    const allRecipes = data;
+    allRecipes.forEach(recipe => {
+        console.log(recipe.title)
+    })
+    return Recipe.insertMany(allRecipes);
+  })
+  .then(() => {
+    console.log("success")
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  })
+  .then(() => {
+    console.log("success")
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+  })
+  .then(() => {
+    return mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);

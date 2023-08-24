71078c9c40550a0efa01d6a739f7aa82ecf7447d
HannaInIT <ann10062016@gmail.com>
2023-08-24 18:02:48 +0200

    itreation 1,2,3 done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,34 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+  const ourRecipe = {
+    title: "pasta salad",
+    level: "Easy Peasy",
+    ingredients: ["pasta", "tomato", "olives", "lettuce", "mozarella"],
+    cuisine: "Italian",
+    dishType: "main_course",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 30,
+    creator: "Merna & Hanna", 
+  }
+  console.log(ourRecipe.title)
+  return Recipe.create(ourRecipe)
+  })
+  .then(() => {
+    console.log("Your salad is ready")
+  })
+  .then(() =>{
+    console.log(data.title)
+    return Recipe.insertMany(data)
+  })
+  .then(() =>{
+    const multipleRecipes = JSON.parse(data);
+    multipleRecipes.forEach((recipe) => {
+      console.log("Recipe's title: ", recipe.title)
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+

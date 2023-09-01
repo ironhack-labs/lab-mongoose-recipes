19f1ae4e93e7c51ff0513a986bb4b64359181613
Mirabela Tiugan <mirabelatiugan@gmail.com>
2023-09-01 22:50:35 +0200

    Lab partially solved

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
@@ -17,7 +17,26 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title: "Tiramisu",
+      level: "Easy Peasy",
+      ingredients: ["Ladyfingers", "mascarpone", "eggs","expresso", "cocoa" ,"sugar"],
+      cuisine:"Italiann",
+      dishType: "dessert",
+      image:"https://www.recipesfromitaly.com/wp-content/uploads/2019/10/authentic-Italian-tiramisu-600x900-1.jpg.webp",
+      duration: 30,
+      creator: "Unknown"
+    }
+
+  return Recipe.create(newRecipe);
   })
+  .then((dataFromApi)=> {
+    console.log(`This recipes title is ${dataFromApi.title}`);
+    return Recipe.insertMany(data);
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+

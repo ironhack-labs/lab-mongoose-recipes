ee60433f0cf8cf8b40e61fbc2e2e49d75f205f5b
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-09-01 16:35:37 +0200

    wip

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-
+const express = require("express");
+const app = express()
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
@@ -17,7 +18,23 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    let myRecipe = {
+    title : "Torta",
+    level : "Okey",
+    ingredients : "Palta",
+    cuisine : "High",
+    dishType : "Asian",
+    image : "Yes",
+    duration : "10",
+    creator : "Paris Hilton",
+    created: "yes"
+
+    }
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+

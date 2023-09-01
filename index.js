b15d3cea21c2e38297b5525da10b0e97ea0cc332
shani <shani@shanis-MacBook-Pro.local>
2023-09-01 17:21:23 +0200

    Solved

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
+// Import model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
@@ -16,8 +15,82 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create(
+      {
+        title: 'Mango curry',
+        level: 'Amateur Chef',
+        ingredients: '["onion", "mango", "currey powder", "coconut milk"]',
+        cuisine: "Asian",
+        dishType: "main_course",
+        duration: 60,
+        creator: "Mum"
+      });
+  })
+  .then(recipeFromDB => {
+    console.log(`*** --> *** recipe: ${recipeFromDB}`)
+  })
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then(recipesFromDB => {
+    console.log(`---- --> All recipes: ${recipesFromDB}`)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { $set: { "duration": 100 } }, { new: true });
+  })
+  .then(updatedRecipe => {
+    console.log('The recipe was updated! ', updatedRecipe)
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(deletedRecipe => {
+    console.log('The recipe was deleted! ', deletedRecipe)
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally(() => {
+    mongoose.disconnect();
+  })
+
+
+// Connection to the database "recipe-app"
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(x => {
+//     console.log(`Connected to the database: "${x.connection.name}"`);
+//     return Recipe.deleteMany()
+//   })
+//   .catch(error => {
+//     console.error('Error connecting to the database', error);
+//   });
+
+// Recipe.create({
+//   title: 'Mango curry',
+//   level: 'Amateur Chef',
+//   ingredients: '["onion", "mango", "currey powder", "coconut milk"]',
+//   cuisine: "Asian",
+//   dishType: "main_course",
+//   duration: 60,
+//   creator: "Mum"
+// })
+//   .then(recipeOne => console.log('The recipe is saved and its value is: ', recipeOne))
+//   .catch(error => console.log('An error happened while saving:', error));
+
+// Recipe.insertMany(data)
+//   .then(allRecipes => console.log('The recipes are saved and values are: ', allRecipes))
+//   .catch(error => console.log('An error happened while saving :', error));
+
+// Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { $set: { "duration": 100 } }, { new: true })
+//   .then(updatedOne => console.log('The recipe was updated ', updatedOne))
+//   .catch(error => console.log('An error happened while udpating:', error));
+
+// Recipe.deleteOne({ title: "Carrot Cake" })
+//   .then(deletedOne => console.log('The recipe was deleted ', deletedOne))
+//   .catch(error => console.log('An error happened while delected:', error));
+
+// mongoose.disconnect()
+//   .then((disconnect) => console.log('The recipe was updated ', disconnect))
+//   .catch(error => console.log('An error happened while saving a new user:', error));
+

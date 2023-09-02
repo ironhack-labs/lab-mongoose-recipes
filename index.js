97ed64f0061b986d69a6e0a0b5e0178f401ec74c
Mirabela Tiugan <mirabelatiugan@gmail.com>
2023-09-02 22:46:53 +0200

    Lab solved

--- index.js
+++ index.js
@@ -21,20 +21,38 @@ mongoose
       title: "Tiramisu",
       level: "Easy Peasy",
       ingredients: ["Ladyfingers", "mascarpone", "eggs","expresso", "cocoa" ,"sugar"],
-      cuisine:"Italiann",
+      cuisine: "italian",
       dishType: "dessert",
       image:"https://www.recipesfromitaly.com/wp-content/uploads/2019/10/authentic-Italian-tiramisu-600x900-1.jpg.webp",
       duration: 30,
       creator: "Unknown"
     }
-
   return Recipe.create(newRecipe);
   })
   .then((dataFromApi)=> {
     console.log(`This recipes title is ${dataFromApi.title}`);
     return Recipe.insertMany(data);
   })
-
+  .then(() => {
+   return Recipe.findOneAndUpdate({ title:"Rigatoni alla Genovese"}, { $set: { "duration": 100 } }, { new: true }); 
+  })
+  .then((update) => {
+    console.log(`Update successful, new duration is` , update.duration);
+  })
+  .then(()=> {
+    return Recipe.deleteOne({ title: "Carrot Cake"});
+  })
+  .then((result) => {
+    if (result.deletedCount === 1) {
+      console.log("Recipe successfully deleted");
+    } else {
+      console.log("Error deleting recipe");
+    }
+  })
+  .then(()=> {
+    mongoose.connection.close()
+    console.log("Lab done! Connection closed")
+  })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

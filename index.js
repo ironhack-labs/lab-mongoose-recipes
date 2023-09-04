1ee7fc5ebefa6b9073b15f466e4f8714f0e88d21
Jorlyjoseph <141339657+Jorlyjoseph@users.noreply.github.com>
2023-09-04 13:47:49 +0200

    allmost done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany();
   })
-  
+
   .then(() => {
     const newRecipe = {
       title: "pepper coffee",
@@ -32,10 +32,36 @@ mongoose
       avatarUrl: "https://images.app.goo.gl/PXGfBzUjxGWWNbuf8",
       duration: 10,
       creator: "unknown",
-    }
+    };
     return Recipe.create(newRecipe);
     // Run your code here, after you have insured that the connection was made
   })
+
+  .then((dataFromDB) => {
+    console.log(dataFromDB.title);
+    Recipe.insertMany(data);
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } }
+    );
+  })
+  .then((update) => {
+    console.log(`Update successful, new duration is`, update.duration);
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+
+  .then((result) => {
+    if (result.deletedCount === 1) {
+      console.log("Recipe successfully deleted");
+    } else {
+      console.log("Error deleting recipe");
+    }
+  })
+
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });

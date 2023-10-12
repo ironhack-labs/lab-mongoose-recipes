a2f1d75e6a8cec2a3ac8887ed6f7f1547d826365
Ian IJmkers <ianymkers@gmail.com>
2023-10-12 21:10:56 +0200

    Up until now everything is quite straightforward, just have to use the right method for each step of the iteration

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,38 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  }
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  }
+  ingredients: {
+    Array: String,
+  }
+  cuisine: {
+    type: String,
+  }
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert" "or other"]
+  }
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  }
+  duration: {
+    type: Number,
+    min: 0,
+  }
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

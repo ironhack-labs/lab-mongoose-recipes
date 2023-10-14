9d23b3afafc72fa3f1e19b4760406a35aadb8ae7
chloe <marin.ducarme@gmail.com>
2023-10-14 02:57:19 +0200

    done

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then((data) => 
+   {
+     console.log(data.title)
+     return Recipe.insertMany(data);
+   })
+   .then(() => {
+    return Recipe.create
+   })
+   .then(() => 
+  {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese", duration: 100})
+  })
+  .then(() => 
+  {
+    return Recipe.deleteOne({title: "Carrot Cake"})
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  
+
+  
+   

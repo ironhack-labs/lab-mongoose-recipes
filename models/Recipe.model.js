d88139efa36b06492475cb5d4505b171e476e55f
alvaro sanchez <alvaro_piro_@hotmail.com>
2023-11-01 16:43:18 +0100

    ?¿

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    require: true,
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: [String],
+  cuisine: {
+    type: String,
+    require: true
+  },
+  disType: {
+    type: String,
+    enum: ['breakfast', 'main course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+
+
+  duration: {
+    type: Number,
+    min: 0
+  },
+
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

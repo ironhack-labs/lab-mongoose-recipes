f8fa87291ada7fbe2073b66a947ec389022e845d
agdoy <145876715+agdoy@users.noreply.github.com>
2023-11-01 16:41:37 +0100

    done

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+const databaseName = 'recipesDB'
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipesDB';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,8 +17,24 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Recipe
+    //   .create({ title: 'Huevos Rellenos', level: 'Easy Peasy', ingredients: ['huevos', 'mayonesa', 'tomate'], cuisine: 'spanish', dishType: 'snack', image: "https://elronqueo.es/wp-content/uploads/2021/10/Receta-de-huevos-rellenos-de-atun-El-Ronqueo.jpg", duration: 100, creator: 'Arzak' })
+    //   .then(newRecipe => console.log('La nueva receta es', newRecipe.title))
+    //   .catch(err => console.log('da error', err))
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+// Recipe
+//   .create({ title: 'Huevos Rellenos', level: 'Easy Peasy', ingredients: ['huevos', 'mayonesa', 'tomate'], cuisine: 'spanish', dishType: 'snack', image: "https://elronqueo.es/wp-content/uploads/2021/10/Receta-de-huevos-rellenos-de-atun-El-Ronqueo.jpg", duration: 100, creator: 'Arzak' })
+//   .then(newRecipe => console.log('La nueva receta es', newRecipe.title))
+//   .catch(err => console.log('da error', err))
+
+Recipe
+  .insertMany(data)
+  .then(newRecipe2 => console.log('Se han creado las siguientes recetas', newRecipe2.title))
+  .catch(err2 => console.log('Esto da error', err2))
+
+

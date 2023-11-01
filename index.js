aca130ebc72032f90267b70f749fb12013eed9a8
Marina Viejo <100945399+marinavp8@users.noreply.github.com>
2023-11-01 16:56:03 +0100

    done

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+
+
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
@@ -16,8 +18,30 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+    return Recipe
+      .create({ title: "huevos Rotos", level: "Easy Peasy", ingredients: ["huevos", "patatas", "jamón", "aceite"], cuisine: "española", dishType: "main_course", duration: 40, creator: "Fabio" })
     // Run your code here, after you have insured that the connection was made
+  })
+  .then((recipe) => {
+    const prueba = recipe.title
+    console.log(prueba)
+  })
+
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+process.on('SIGINT', () => {
+  mongoose.connection.close()
+  console.log('Mongoose default connection disconnected through app termination')
+})

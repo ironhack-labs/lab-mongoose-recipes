df8aa03fb6742c12424f7e345c9cd87a5b1a70d1
Louise Gastardi <l.gastardi@hotmail.com>
2023-11-10 17:00:39 +0100

    lab done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String, unique: true },
+  level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'], default: 'Easy Peasy' },
+  ingredients: { type: [String] },
+  cusine: { type: String },
+  dishType: { type: String, enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other'], default: 'other' },
+  image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.now },
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

933de10bfa65856c8ff658cf057c9537830519ef
PMisar <147728448+PMisar@users.noreply.github.com>
2023-11-10 17:11:48 +0100

    done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -16,8 +15,56 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const garlicSoupRecipe = {
+      title: 'Simple Garlic Soup',
+      level: 'Easy Peasy',
+      ingredients: [
+        '6 cloves garlic, minced',
+        '2 tablespoons olive oil',
+        '6 cups chicken or vegetable broth',
+        '1 teaspoon dried thyme',
+        'Salt and pepper to taste',
+        'Croutons for garnish (optional)',
+        'Fresh parsley for garnish (optional)',
+      ],
+      cuisine: 'International',
+      dishType: 'soup',
+      image: 'https://example.com/garlic_soup.jpg',
+      duration: 30,
+      creator: 'Chef Jane',
+    };
+
+    return Recipe.create(garlicSoupRecipe);
   })
-  .catch(error => {
+  .then((createdRecipe) => {
+    console.log(`Recipe created: ${createdRecipe.title}`);
+    
+    return Recipe.insertMany(data);
+  })
+  .then((insertedRecipes) => {
+    insertedRecipes.forEach((recipe) => {
+      console.log(`Recipe: ${recipe.title}`);
+    });
+
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 },
+      { new: true } 
+    );
+  })
+  .then((updatedRecipe) => {
+    console.log(`Updated duration of Rigatoni alla Genovese to ${updatedRecipe.duration} minutes`);
+
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('Carrot Cake recipe removed');
+
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    console.log('Database connection closed');
+  })
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });

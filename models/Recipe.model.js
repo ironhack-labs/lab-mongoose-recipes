00ffb244236170213aad955e156894a3c0fc0e89
Eva Kalbfell <e.kalbfell@web.de>
2023-11-10 15:55:58 +0100

    done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,31 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+const {Schema, model } = require("mongoose")
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+  },
+  ingredients: [String],
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: Number,
+  creator: String,
+  created: {
+    type: Date,
+    default: Date.now,
+  }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = model('Recipe', recipeSchema);
 
 module.exports = Recipe;

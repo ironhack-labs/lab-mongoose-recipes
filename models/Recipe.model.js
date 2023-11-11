4373564a835e841b7824810314c784737c1149ff
steeldragonn <sweetsourr.sos@gmail.com>
2023-11-10 18:08:31 -0800

    DONELAB

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: {
+    type: [String],
+  },
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert"],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-
+const Recipe = mongoose.model("recipe", recipeSchema);
 module.exports = Recipe;

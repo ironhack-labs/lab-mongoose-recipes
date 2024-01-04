88b8c5b78d2bdda9e28f0cc0d8f83cf916a674df
Marcelo Via <mvia@outlook.com>
2024-01-04 01:01:31 -0500

    [MIA-PT-OCT2023] Marcelo Via - Lab Completed

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,21 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {type: String},
+  level: {type: String, enum: ['Easy Peasy','Amateur Chef','UltraPro Chef']},
+  ingredients: {type: [String]},
+  cuisine: {type: String},
+  dishType: {type: String},
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {type: Number},
+  creator: {type: String},
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

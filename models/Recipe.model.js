864ebe3ad0f35e4e88586b7a93ecf296457b3662
Jesús Sánchez <jsxexu38@gmail.com>
2024-02-19 14:38:55 +0100

    solved lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
+const recipeSchema = new Schema(
+
+{
   title: {
     type: String,
     required: true,

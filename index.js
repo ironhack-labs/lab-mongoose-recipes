873de6203649ae005f16b1a885c44364fa2560d7
Raulpsdos <raulpscoder@gmail.com>
2024-02-20 15:21:10 +0100

    Almost done!

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-const mongoose = require('mongoose');
+require('dotenv').config();
+require('./configs/db.config');
 
-// Import of the model Recipe from './models/Recipe.model.js'
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
-
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+Recipe.deleteMany();
+  .then (() => {
+    return Recipe.create({
+        'title': 'White Chocolate Brownie',
+        'level': 'Amateur Chef',
+        'ingredients': ['White chocolate - 200g', 'Flour - 125g', 'Butter - 100g', '2 eggs', 'Vanilla essence - 2 tablespoons'],
+        'cuisine': 'American',
+        'dishType': 'snack',
+        'image': 'https://sugarspunrun.com/wp-content/uploads/2022/09/easy-White-chocolate-Brownies-recipe-5-of-9.jpg',
+        'duration': 50,
+        'creator': 'Raúl Pérez Sánchez',
+        'created': 18
+    });
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then((recipes) => {
+    recipes.forEach((recipe) = console.log(`${recipe.title}'s recipe has been created`));
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese'},
+      { duration: 100 }
+    );
+  })
+  .then((recipeUpdated) => {
+    console.log(`${recipeUpdated.title}'s recipe has been successfully updated`);
+    return Recipe.deleteOne(
+      { title: 'Carrot Cake'}
+    );
+  })
+  .then(() => {
+    console.log(`The ${recipe.title}'s recipe has been removed form the database`);
+  })
+  .catch((error) => {
+    console.log
+  })

f83476e14a5794757d38843d9d42b04f83cdb0f7
Vitor193 <vitormiguel135@gmail.com>
2024-02-26 15:11:56 +0000

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', false);
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -16,8 +16,39 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title:'Bitoque',
+      level:'Amateur Chef',
+      ingredients:['200g White Rice','250g Beef Steak','200g Potatoes','1 Egg','1 garlic clove','2 Bay leaves','olive oil','mustard','50ml milk','dry coriander'],
+      cuisine:'Portuguese',
+      dishType:'main_course',
+      image:'https://oseubitoque.pt/wp-content/uploads/2018/05/bitoque_frango_manteiga.jpg',
+      duration:20,
+      creator:'Xô Manel',
+    }
+    console.log(Recipe.title)
+    return Recipe.create(newRecipe);
+
   })
+
+  .then(() =>{
+    
+    return Recipe.insertMany(data);
+  })
+
+  .then(()=>{
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+
+  })
+  .then(()=>{
+    return Recipe.deleteOne({ title: 'Carrot Cake' })
+  })
+  .then(() => {
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    console.log('Connection is closed');})
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });

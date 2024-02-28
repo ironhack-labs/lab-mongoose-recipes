7a4c36a101d7cf0b26aeac3e550288fb954de453
martab <mbsuarez.92@gmail.com>
2024-02-28 18:49:22 +0100

    lab done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
+const recipeSchema = new Schema(
+  {
+  title: {
+    type: String,
+    
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);

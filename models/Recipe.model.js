608f3702bc5fc4030e4197a4ead91a8815a53402
martab <mbsuarez.92@gmail.com>
2024-02-28 19:23:38 +0100

    finished

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -5,7 +5,37 @@ const recipeSchema = new Schema(
   {
   title: {
     type: String,
-    
+    required: true,
+    unique: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+  },
+  ingredients: {
+    type: [String]
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: String
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now()
   }
 });
 
